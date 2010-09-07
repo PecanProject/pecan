202a796fbe4ec0a68d83ea97d20a87a555753d16
David LeBauer <dlebauer@illinois.edu>
2010-09-07 16:21:49 -0500

    no changes

--- R/query.bety.pft_species.R
+++ R/query.bety.pft_species.R
@@ -1,3 +1,9 @@
-query.bety.pft_species <-
-function().
-
+query.bety.pft_species <- function(pft){
+## select plant id's associated with pft
+  con <- query.bety.con()
+  query <- paste("select id, genus, species.species from species where id in ( select specie_id from pfts_species where pft_id = (select id from pfts where name = '",pft,"'));", sep = "")
+  q    <- dbSendQuery(con, query)
+  species <- NA
+  species <- fetch ( q, n = -1 )
+  return(species)
+}
--- R/query.bety.priors.R
+++ R/query.bety.priors.R
@@ -1,9 +1,10 @@
-query.bety.priors <- function(pft, prstr){
+query.bety.priors <- function(pft){
+    con <- query.bety.con()
 ## prior traits will be used in meta-analysis for traits w/ data
 ## or to query priors for traits w/o data
-  dvr <- dbDriver ("MySQL")
-  con <- dbConnect(dvr, group  = "biofuel" )
-  query <- paste("SELECT Priors.PriorID, PriorPFT, VarID, PriorDistn, PriorParamA, PriorParamB, PriorN from Priors, Priors_has_PFT where Priors.PriorID = Priors_has_PFT.PriorID and Priors_has_PFT.pftID in ('", pft, "') and VarID in (", prstr, ");", sep = "")
+  trstr <- "'mort2','cuticular_cond','dark_respiration_factor','plant_min_temp','growth_resp_factor','leaf_turnover_rate','leaf_width','nonlocal_dispersal','q','root_respiration_factor','root_turnover_rate','seedling_mortality','SLA','stomatal_slope','Vm_low_temp','quantum_efficiency','f_labile','leafN','water_conductance','Vm0','r_fract','storage_turnover_rate'"
+  query <- paste("SELECT Priors.PriorID, PriorPFT, VarID, PriorDistn, PriorParamA, PriorParamB, PriorN from Priors, Priors_has_PFT where Priors.PriorID = Priors_has_PFT.PriorID and Priors_has_PFT.pftID in ('", pft, "') and VarID in (", trstr, ");", sep = "")
+  print(query)
   q    <- dbSendQuery(con, query)
   priors <- NA
   priors <- fetch ( q, n = -1 )
