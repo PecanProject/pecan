9956a680d63ee0a6aad53beb8988aa8aaaf6e18b
David LeBauer <dlebauer@illinois.edu>
2010-09-13 09:14:22 -0500

    a few changes to the test scrpt pecantest.R associated with rev. 4

--- pecantest.R
+++ pecantest.R
@@ -2,28 +2,27 @@ setwd('/home/dlebauer/pecan/dev/R/')
 library('RMySQL')
 
 ##the next 'source' lines will be replaced by library('PECAn')
-source('query.bety.pft_species.R')
-source('query.bety.priors.R') #given pft, query available priors
-source('query.bety.traits.R') 
-source('vecpaste.R')
-source('query.bety.con.R')
+source('query.bety.traits.R')
+source('query.bety.priors.R')
+
 
 ##input variables
-pft <- 'ebifarm.nfixer'
+pft <- 'c4crop'
+
+##edtraits is the comprehensive list of traits that could be in the database 
+edtraits <- c('mort2','cuticular_cond','dark_respiration_factor','plant_min_temp','growth_resp_factor','leaf_turnover_rate','leaf_width','nonlocal_dispersal','q','root_respiration_factor','root_turnover_rate','seedling_mortality','SLA','stomatal_slope','Vm_low_temp','quantum_efficiency','f_labile','leafN','water_conductance','Vm0','r_fract','storage_turnover_rate')
+
+parms  <- query.bety.parameters(pft, edtraits)
+
+prstr  <- vecpaste(parms)
+priors <- query.bety.priors(pft, prstr)
+
+trstr  <- vecpaste(gsub('Vm0', 'Vcmax', prstr))
+data   <- query.bety.traits(pft, trstr)
 
 
-## 1. get species list based on pft
-spp <- query.bety.pft_species(pft)
-spstr <- vecpaste(spp$plant_id)
-
-## 2. get priors available for pft
-priors <- query.bety.priors(pft)
-
-prvec <- priors$VarID     # vector of traits with prior distributions for pft 
-prstr <- vecpaste(prvec)  # string of " " " " used to query priors
-
-trvec <- gsub('Vm0', 'Vcmax', prvec)  
-trstr <- gsub('Vm0', 'Vcmax', prstr) #used to query trait data
+##command to disconnect MySQL connections
+lapply(dbListConnections(MySQL()), dbDisconnect)
 
 
 
