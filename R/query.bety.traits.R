4881f38a8e5271995be49b2c6d992b6525075bcc
David LeBauer <dlebauer@illinois.edu>
2010-09-13 13:25:55 -0500

    created new query.bety.traits.R to extract trait data given spstr and trvec

--- R/query.bety.traits.R
+++ R/query.bety.traits.R
@@ -1,20 +1,18 @@
-query.bety.traits <- function(pft, trstr){
-<<<<<<< TREE
+query.bety.traits <- function(spstr, trvec){
   con <- query.bety.con()
-  #query <- paste("select VarID from Priors where Priors.PriorID in (select Priors_has_PFT.PriorID from Priors_has_PFT where Priors_has_PFT.PFTID = '",pft,"' and VarID in (", trstr,"));", sep = '')
-=======
-  dvr <- dbDriver ("MySQL")
-  con <- dbConnect(dvr, group  = "biofuel" )
-  query <- paste("select VarID from Priors where Priors.PriorID in (select Priors_has_PFT.PriorID from Priors_has_PFT where Priors_has_PFT.PFTID = '",pft,"' and VarID in (", trstr,"));", sep = '')
-
-  #for ebi_analysis
-  #con <- dbConnect(dvr, group  = "ebi_analysis" )
->>>>>>> MERGE-SOURCE
-  #query <- paste('select distinct variable_id from traits where traits.specie_id in (select specie_id from pfts_species where pft_id = (select pfts.id from pfts where pfts.name = ',pft,'));')
-  query.result <- dbSendQuery(con, query)
-  prvec <- fetch(query.result, n = -1)
-  return(prvec)
+  trait.data <- list()
+  for (i.tr in trvec) {
+    if (i.tr != 'Vcmax') {
+    query <- paste("select traits.site_id, treatments.name, traits.mean, traits.statname, traits.stat, traits.n from traits left join treatments on (traits.treatment_id = treatments.id) where specie_id in (", spstr,") and variable_id in ( select id from variables where name = '",i.tr,"');", sep = "")
+  } else if (i.tr == 'Vcmax') {
+    query <- paste("select traits.site_id, treatments.name, traits.mean, traits.statname, traits.stat, traits.n, tdhc1.level AS 'temp', tdhc2.level AS 'canopy_layer' FROM traits LEFT JOIN treatments ON traits.treatment_id = treatment.id JOIN covariates AS tdhc1 ON (traits.id = covariates.trait_id) JOIN covariates AS tdhc2 ON (traits.id = covariates.trait_id) WHERE (SELECT true from variables WHERE tdhc1.name = 'temp' and tdhc2.TraitCovID = 'canopy_layer' and (tdhc2.TraitCovLevel >= .8 or tdhc2.TraitCovLevel IS NULL) and tdhc2.TraitCovLevel IS NOT NULL and MONTH(TraitDate) between 4 and 7 and USDASymbol in (", spstr, ") and TraitVarID = 'Vcmax'; ", sep = "")
+  }
+    query.result <- dbSendQuery(con, query)
+    result <- fetch(query.result, n = -1)
+    if (dim(result)[1] > 0) trait.data[[i.tr]] <- result 
+  }
+  return(trait.data)
 }
 
-
+ e
 
