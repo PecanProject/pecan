04142b363147cbc2c4f3305543bf01bc1cde87b8
David LeBauer <dlebauer@illinois.edu>
2010-09-16 13:02:41 -0500

    slight changes to nomenclature in ma.model; added ma.model.ghs.R as an alternative to ma.model.R when data come from both greenhouse and field studies. Need to include an if...then statement in PECAn to choose which file based on pres/abs. of ghs data. Not sure what to do if data come from only greenhouse studies????

--- R/ma.model.ghs.R
+++ R/ma.model.ghs.R
@@ -2,7 +2,9 @@
 ma.model <- function(){
   for (k in 1:LENGTHK){
     Y[k] ~ dnorm( Z[k] , tau.y[k])              # observed site x trt means and uncertainties
-    Z[k] <- b.trt[trt[k]] + b.site[site[k]]      # linear model with random treatment and site effects
+    Z[k] <- b.trt[trt[k]] + b.site[site[k]] + b.ghs[ghs[k]]
+                                                # linear model with random effects of
+                                                # treatment, site, and greenhouse
     tau.y[k] <- prec.y*n[k]                     # precision from obs.prec
     u1[k] <- n[k]/2                             
     u2[k] <- n[k]/(2*prec.y)
@@ -13,11 +15,14 @@ ma.model <- function(){
     b.trt[j] ~ dnorm(0, tau.trt)                # treatment effects, random
   }
   for (g in 1:LENGTHG){
-    theta[g] ~ dnorm(beta.o, tau.site)         # site effects, random
+    b.site[g] ~ dnorm(beta.o, tau.site)         # site effects, random
   }
+  b.ghs[0]  ~ dnorm(0, tau.ghs)                 # greenhouse effect, random
+  b.ghs[1]  ~ dnorm(0, tau.ghs)                    
   beta.o    ~ PRIORDIST (PRIORPARAMA, PRIORPARAMB) # informed prior on global mean
-  tau.site ~ dgamma(0.1, 0.1)                  # global precision
+  tau.site  ~ dgamma(0.1, 0.1)                  # global precision
   tau.trt   ~ dgamma(0.1, 0.1)                  # treatment effect precision
+  tau.ghs   ~ dgamma(0.1, 0.1)
   prec.y    ~ dgamma(0.1, 0.1)                  # observation Y precision
   thetaSD  <- 1 / sqrt(tau.site)               # across site variance
   ySD      <- 1 / sqrt(prec.y)                  # within site variance
