8116e432ffd57a5fcaa8878933a43449067b3ee4
David LeBauer <dlebauer@illinois.edu>
2010-10-01 11:15:38 -0500

    created documentation for query.bety.trait.data, updated documentation for query.bety.traits

--- R/query.bety.traits.R
+++ R/query.bety.traits.R
@@ -1,7 +1,10 @@
 query.bety.traits <- function(spstr, trvec){
+  ## check which traits in trvec (those for which priors exist)
+  ##    have trait data available for species in spstr
   con <- query.bety.con()
   trait.data <- list()
   query <- paste("select distinct variables.name from traits join variables on (traits.variable_id = variables.id) where specie_id in (", spstr,") and variable_id in (select id from variables where name in (", vecpaste(trvec),"));", sep = "")
+  ##*TODO
   ## Need to write query for:
   ## first check pft_species for grass or tree 
   ##      if grass, root and shoot to calculate q
