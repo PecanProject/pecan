e89e659d867f155a37ae57c09d06a98e0ff0a145
David LeBauer <dlebauer@illinois.edu>
2010-10-05 16:21:46 -0500

    added \"chmod +x pecan.ed.batchjobs.sh\" to pecan.send.jobs.sh

--- bash/pecan.send.jobs.sh
+++ bash/pecan.send.jobs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 rsync saconfigs.tgz ebi-cluster:/home/dlebauer/EDBRAMS/ED/run/
 rsync ./bash/pecan.ed.batchjobs.sh ebi-cluster:/home/dlebauer/EDBRAMS/ED/run/
-ssh ebi-cluster '/home/dlebauer/EDBRAMS/ED/run/pecan.ed.batchjobs.sh'
+ssh ebi-cluster 'chmod +x ~/EDBRAMS/ED/run/pecan.ed.batchjobs.sh'
+ssh ebi-cluster 'bash /home/dlebauer/EDBRAMS/ED/run/pecan.ed.batchjobs.sh'
 DATE=`date +%Y%m%d`
-ssh cp saconfigs.tgz saconfigs.$DATE.tgz
+cp saconfigs.tgz saconfigs.$DATE.tgz
 
