31fb1d6a0fc8b670df2385b5126d427002798089
David LeBauer <dlebauer@illinois.edu>
2010-10-07 16:37:55 -0500

    divided batchjobs into a ed2in.create and batchjobs script to facilitate re-submission of jobs; added rm *log and rm c.* before creating the ed2in files and submitting jobs

--- bash/pecan.ed.batchjobs.sh
+++ bash/pecan.ed.batchjobs.sh
@@ -1,32 +1,9 @@
 #!/bin/bash
-cd $HOME/EDBRAMS/ED/RUN
-DATE=`date +%Y%m%d`
-
-OUTDIR=/home/scratch/pecan/$USER/out$DATE
-if [ ! -d $OUTDIR ] #if [output directory] exists 
-then
-    mkdir $OUTDIR    #if not, make new directory
-fi
-
-tar -zxf saconfigs.tgz #unzip config files
-
-##Make new ED2IN file for each config file
-cp aED2IN ED2IN
-sed -i 's/YYYYMMDD/'$DATE'/g' ED2IN
-
-for f in c.*
-do 
-    cp ED2IN ED2IN$f
-    sed -i 's/CONFIGFILE/'$f'/g' ED2IN$f
-    sed -i 's/OUTFILE/out'$f'/g' ED2IN$f 
-    sed -i 's/outconfig./out./g' ED2IN$f
-    sed -i 's/HISTFILE/hist'$f'/g' ED2IN$f
-    sed -i 's/histconfig./hist./g' ED2IN$f
-done
-
-rm ED2IN
-
 ## run jobs
+rm *.log
+DATE=`date +%Y%m%d`
+OUTDIR=/home/scratch/pecan/$USER/out$DATE
+
 for f in ED2IN*; do
   LOG="$f-`date +%Y.%m.%d-%H.%M`.log" #name with date tag for log files, one per ED2IN file 
   CMD="qsub -cwd -pe mpich 1 -j y -m eas -M dlebauer@illinois.edu -o $LOG ./run 1 $f" #defines command to be run
