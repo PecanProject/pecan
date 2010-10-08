2ff55d4e0c9e1747bf4fe19ed2a3818492579ffb
David LeBauer <dlebauer@illinois.edu>
2010-10-08 18:27:47 -0500

    end of week commit, still working on pecan.ed2in.create.sh

--- bash/pecan.send.jobs.sh
+++ bash/pecan.send.jobs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## send files to cluster
-tar -zcf pecan.edrunfiles.tgz saconfigs.tgz pecan.ed2in.create pecan.batchjobs.sh
+tar -zcf pecan.edrunfiles.tgz saconfigs.tgz bash/pecan.ed2in.create bash/pecan.batchjobs.sh
 rsync -outi pecan.edrunfiles.tgz ebi-cluster:/home/$USER/EDBRAMS/ED/run/pecan.edrunfiles.tgz
 
 ## unzip config files, set env vars, write ED2IN files, run ED ensemble, 
