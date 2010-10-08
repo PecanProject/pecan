a35708c2cd6c70ec9ffa4adb122c0afabfda04ea
David LeBauer <dlebauer@illinois.edu>
2010-10-08 17:05:14 -0500

    working on pecan.send.jobs.sh still need to fix, e.g.. add a wait step while ed2in files are being written.

--- bash/pecan.send.jobs.sh
+++ bash/pecan.send.jobs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ## send files to cluster
-tar -zcf pecan.edrunfiles.tgz saconfigs.tgz pecan.env.vars.sh pecan.ed2in.create pecan.batchjobs.sh
-ssh put pecan.edrunfiles.tgz ebi-cluster:/home/$USER/EDBRAMS/ED/run/pecan.edrunfiles.tgz
+tar -zcf pecan.edrunfiles.tgz saconfigs.tgz pecan.ed2in.create pecan.batchjobs.sh
+rsync -outi pecan.edrunfiles.tgz ebi-cluster:/home/$USER/EDBRAMS/ED/run/pecan.edrunfiles.tgz
 
 ## unzip config files, set env vars, write ED2IN files, run ED ensemble, 
-ssh ebi-cluster "cd $HOME/EDBRAMS/ED/run; tar -zxf pecan.edrunfiles.tgz; tar-zxf saconfigs.tgz; chmod +x *.sh; ./pecan.env.vars.sh; ./pecan.ed2in.create.sh; ./pecan.ed.batchjobs.sh"
+ssh ebi-cluster "cd /home/$USER/EDBRAMS/ED/run; tar -zxf pecan.edrunfiles.tgz; tar-zxf saconfigs.tgz; chmod +x *.sh; ./pecan.ed2in.create.sh; ./pecan.ed.batchjobs.sh"
 
 
