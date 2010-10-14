8f698d77eb4651fda8ed6bfef1a5fc7ef97e9846
David LeBauer <dlebauer@illinois.edu>
2010-10-14 13:07:37 -0500

    fixing SA calculations, added save transformed.samps, a list with all mcmc chains and prior samples, as transformed.samps.Rdata

--- R/pecan.SA.R
+++ R/pecan.SA.R
@@ -5,7 +5,7 @@ pecan.SA <- function(M, yr0, yrf, date, outdir) {
   ## theta are the n trait inputs defined in 'trait' dataframe
   ## f are the model outputs at different quantiles of theta
 
-####Import filenames from ED2 output 
+  ##Import filenames from ED2 output 
   trait.defs <- trait.dictionary()
 
   ## .f.yr is a list of files with annual output
@@ -79,5 +79,7 @@ pecan.SA <- function(M, yr0, yrf, date, outdir) {
       satables[[.r]][[.i]] <- pecan.SAcalcs(.r,  .i, dat, trait.defs, trait.samps)
     }
   }
-  return(satables)
+
+  return(list(satables=satables,
+              transformed.samps = dat))
 }
