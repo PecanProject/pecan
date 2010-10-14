a4199adc446b0fba32ebad4e605cf4a980275d20
David LeBauer <dlebauer@illinois.edu>
2010-10-14 14:46:00 -0500

    fixed pecan.SA.runscript to save transformed.samps and satables in Rdata format

--- rscripts/pecan.SA.runscript.R
+++ rscripts/pecan.SA.runscript.R
@@ -4,9 +4,9 @@ yr0 <- as.numeric(system("echo $YR0", intern =TRUE))
 yrf   <- as.numeric(system("echo $YRF", intern =TRUE))
 date <- read.table("DATE")
 user <- system("echo $USER", intern=TRUE)
-outdir <- paste('/home/scratch/pecan/',user,'/out',date,sep='')
+outdir <- paste('/home/scratch/',user,'/pecan/out',date,sep='')
 saout <- pecan.SA(M, yr0, yrf, date, outdir)
 satables <- saout[['satables']]
 transformed.samps <- saout[['corrected.samps']] 
 save(satables, file = paste(outdir,'/satables.Rdata', sep = ''))
-save(transformed.samps, file = paste(outdir,'/transformed.samps.Rdata')
+save(transformed.samps, file = paste(outdir,'/transformed.samps.Rdata',sep=''))
