eec80e14fa8b5172b83d6cd5ab368429236b9836
David LeBauer <dlebauer@illinois.edu>
2010-10-18 18:13:59 -0500

    end of day commit; changing output file locations, integration with Kepler, moving toward use of R-2.12.0; almost there

--- bash/pecan.send.jobs.sh
+++ bash/pecan.send.jobs.sh
@@ -4,7 +4,4 @@
 tar -zcf pecan.edrunfiles.tgz saconfigs.tgz bash/pecan.ed2in.create bash/pecan.batchjobs.sh
 rsync -outi pecan.edrunfiles.tgz ebi-cluster:/home/$USER/EDBRAMS/ED/run/pecan.edrunfiles.tgz
 
-## unzip config files, set env vars, write ED2IN files, run ED ensemble, 
-ssh ebi-cluster "cd /home/$USER/EDBRAMS/ED/run; tar -zxf pecan.edrunfiles.tgz; tar-zxf saconfigs.tgz; chmod +x *.sh; ./pecan.ed2in.create.sh; ./pecan.ed.batchjobs.sh"
-
 
