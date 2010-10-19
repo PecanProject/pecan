4e19ae45017d3965ad1325d1e5456ddaf3436b8d
David LeBauer <dlebauer@illinois.edu>
2010-10-19 16:55:53 -0500

    added Rd files, pecan.send.jobs, kepler/pecan.kar, pecanbuild.sh

--- bash/pecan.send.jobs.sh
+++ bash/pecan.send.jobs.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
+tar -zcf pecan.edrunfiles.tgz saconfigs.tgz bash/pecan.ed2in.create.sh bash/pecan.ed.batchjobs.sh
 
-## send files to cluster
-tar -zcf pecan.edrunfiles.tgz saconfigs.tgz bash/pecan.ed2in.create.sh bash/pecan.batchjobs.sh
-echo 'pecan.edrunfiles.tgz, saconfigs.tgz, bash/pecan.ed2in.create
 rsync -outi pecan.edrunfiles.tgz ebi-cluster:/home/$USER/EDBRAMS/ED/run/pecan.edrunfiles.tgz
 
 
