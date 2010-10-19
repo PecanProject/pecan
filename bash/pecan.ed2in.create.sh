b31c4fda6623b4ede7d81fe5c94bd2f0a278c83b
David LeBauer <dlebauer@illinois.edu>
2010-10-19 18:12:14 -0500

    editing send.jobs, and related files

--- bash/pecan.ed2in.create.sh
+++ bash/pecan.ed2in.create.sh
@@ -1,17 +1,22 @@
 DATE=`date +%Y%m%d`
-echo $DATE > /home/$USER/pecan/DATE
-OUTDIR=/home/scratch/$USER/out$DATE
-ED_RUN=$HOME/EDBRAMS/ED/run
-cd $ED_RUN
-rm c.*
 
+OUTDIR=/home/scratch/$USER/pecan/out$DATE
+EDIN=/home/scratch/$USER/pecan/edin
+echo $DATE > /home/scratch/$USER/pecan/DATE
 if [ ! -d $OUTDIR ] #if [output directory] exists 
 then
     mkdir $OUTDIR    #if not, make new directory
 fi
 
-tar -zxf saconfigs.tgz #unzip config files
+for i in $EDIN/*c.p* 
+do 
+rm $i 
+done
 
+
+cd $EDIN
+
+tar -zxf saconfigs.tgz #unzip config files
 ##Make new ED2IN file for each config file
 cp aED2IN ED2IN
 sed -i 's/YYYYMMDD/'$DATE'/g' ED2IN
