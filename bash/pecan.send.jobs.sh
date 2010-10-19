021a315b844fc55c5660f1d1eb595f35280dcee6
David LeBauer <dlebauer@illinois.edu>
2010-10-19 16:33:12 -0500

    minor edits to pecan.dtheta.R, writeconfigs, send.jobs.sh

--- bash/pecan.send.jobs.sh
+++ bash/pecan.send.jobs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ## send files to cluster
-tar -zcf pecan.edrunfiles.tgz saconfigs.tgz bash/pecan.ed2in.create bash/pecan.batchjobs.sh
+tar -zcf pecan.edrunfiles.tgz saconfigs.tgz bash/pecan.ed2in.create.sh bash/pecan.batchjobs.sh
+echo 'pecan.edrunfiles.tgz, saconfigs.tgz, bash/pecan.ed2in.create
 rsync -outi pecan.edrunfiles.tgz ebi-cluster:/home/$USER/EDBRAMS/ED/run/pecan.edrunfiles.tgz
 
 
