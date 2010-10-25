29a2b11fd6218529f22215723820783f34d817d2
David LeBauer <dlebauer@illinois.edu>
2010-10-25 14:15:11 -0500

    beginning of week commit

--- rscripts/pecan.SA.runscript.R
+++ rscripts/pecan.SA.runscript.R
@@ -1,14 +1,16 @@
 library('PECAn', lib.loc = '~/lib/R')
-load('pecan.samps.Rdata')
+
+date <-  as.numeric(system("echo $DATE", intern =TRUE))
+pecanhome <- system("echo $PECANHOME", intern=TRUE)
+pecanout <- paste(pecanhome, '/out', sep='')
+load(paste(pecanout, '/pecan.samps.Rdata', sep=''))
+
+##need to be loaded after pecan.samps.Rdata unless values included there
 yr0 <- as.numeric(system("echo $YR0", intern =TRUE))
 yrf   <- as.numeric(system("echo $YRF", intern =TRUE))
-date <-  as.numeric(system("echo $DATE", intern =TRUE))
-user <- system("echo $USER", intern=TRUE)
-outdir <- paste('/home/scratch/',user,'/pecan/out',date,sep='')
-
 dtheta.q <- list(prior = prior.dtheta.q, post = post.dtheta.q)
-saout <- pecan.SA(M, yr0, yrf, date, outdir,dtheta.q)
+saout <- pecan.SA(M, yr0, yrf, date, pecanout,dtheta.q)
 satables <- saout[['satables']]
 transformed.samps <- saout[['transformed.samps']] 
-save(satables, file = paste(outdir,'/satables.Rdata', sep = ''))
-save(transformed.samps, file = paste(outdir,'/transformed.samps.Rdata',sep=''))
+save(satables, file = paste(pecanout,'/satables.Rdata', sep = ''))
+save(transformed.samps, file = paste(pecanout,'/transformed.samps.Rdata',sep=''))
