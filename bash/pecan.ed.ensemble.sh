c690b3e2a24599b905d25824a8e76c15ac793a48
David LeBauer <dlebauer@illinois.edu>
2010-10-26 17:52:17 -0500

    added ensemble bash scripts

---
+++ bash/pecan.ed.ensemble.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+R --vanilla < ./rscripts/pecan.writeconfigs.R
+
+for i in config*xml; do mv "${i}" "${i/config/c}"; done
+for i in *_*xml; do mv "${i}" "${i/_/}"; done
+for i in *_*xml; do mv "${i}" "${i/_/}"; done
+for i in *factor*xml; do mv "${i}" "${i/factor/}"; done
+for i in *root*xml; do mv "${i}" "${i/root/rt}"; done
+for i in *turnover*xml; do mv "${i}" "${i/turnover/tnvr}"; done
+for i in *conductance*xml; do mv "${i}" "${i/conductance/cndctnc}"; done
+for i in *respiration*xml; do mv "${i}" "${i/respiration/resp}"; done
+for i in *nonlocaldispersal*xml; do mv "${i}" "${i/nonlocaldispersal/nldisprs}"; done
+for i in *quantumefficiency*xml; do mv "${i}" "${i/quantumefficiency/quantef}"; done
+for i in *water*xml; do mv "${i}" "${i/water/h2o}"; done
+for i in *stomatalslope*xml; do mv "${i}" "${i/stomatalslope/stmslope}"; done
+for i in c*xml; do mv "${i}" "${i/.xml/}"; done
+
+tar zcf saconfigs.tgz c*  
