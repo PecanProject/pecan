4e6f93d4491aac885f276582448e95e1cc06b184
David LeBauer <dlebauer@illinois.edu>
2010-10-26 17:39:59 -0500

    save trait.defs as file out/trait.defs.Rdata

--- rscripts/pecan.MA.runscript.R
+++ rscripts/pecan.MA.runscript.R
@@ -3,9 +3,9 @@ pft   <- system("echo $PFT", intern = TRUE)
 ITER  <- as.numeric(system("echo $ITER", intern = TRUE)) 
 M     <- as.numeric(system("echo $ENSN", intern = TRUE))
 outdir   <- system("echo $PECANOUT", intern = TRUE)
-outfile1 <- paste(outdir, 'pecan.parms.Rdata', sep = '')
+outfile1 <- paste(outdir, '/pecan.parms.Rdata', sep = '')
 save.image(outfile1)
-
+ 
 print(cat("PECAn run with ",pft,
           "\nmeta-analysis has",ITER,"iterations",
           "\nensemble has",M,"config files"))
@@ -23,8 +23,9 @@ prvec <- rownames(priors) # vector of traits with prior distributions for pft
 prstr <- vecpaste(prvec)  # string of " " " " used to query priors
 
 trvec <- gsub('Vm0', 'Vcmax', prvec)  
-
-
+traits <- trvec
+trait.defs <- trait.dictionary(traits)
+save(trait.defs, file = paste(outdir, '/trait.defs.Rdata', sep=''))
 ## now it is time to query the data
 trait.data <- query.bety.traits(spstr,trvec) 
 ## returns list 'trait.data' with one dataframe per variable 
