7c9c6ed5bf54b5ba04d706765a5979b70082a827
David LeBauer <dlebauer@illinois.edu>
2010-11-02 13:53:43 -0500

    fixed typo left over from yesterday's fixes

--- R/pecan.ma.summary.R
+++ R/pecan.ma.summary.R
@@ -1,7 +1,6 @@
 pecan.ma.summary <- function(mcmc.object, pft){
-  ## G-R diagnostics to ensure convergence
-  pdf(paste('out/ma.summaryplots.',pft, '.pdf', sep = ''))
   for (trait in names(mcmc.object)){
+    ## G-R diagnostics to ensure convergence    
     gd<-gelman.diag(mcmc.object[[trait]])
     mpsrf<-round(gd$mpsrf,digits=4)
     if(mpsrf<1.1){
@@ -9,16 +8,24 @@ pecan.ma.summary <- function(mcmc.object, pft){
                     "\nGD MPSRF = ",mpsrf,"\n", sep=" ")
     } else {
       note <- cat ("JAGS model did not converge for", pft, trait,
-                     "\nGD MPSRF = ",mpsrf,"\n", sep=" ")
+                   "\nGD MPSRF = ",mpsrf,"\n", sep=" ")
     }
-    maparms <- names(trait.mcmc[['SLA']][1,][1][[1]])
+    mtext(text = note, line = 3)
+
+    ## reordering maparms so that beta.o etc not sent to end
+    .maparms <- names(trait.mcmc[['SLA']][1,][1][[1]])
+    .parms <- c('beta.o', 'thetaSD', 'trtSD', 'ySD')
+    maparms <- .maparms[ c(which(.maparms %in% .parms), which(!.maparms %in% .parms))]
+
+    ## plots for mcmc diagnosis
+    pdf(paste('out/ma.summaryplots.',pft, trait, '.pdf', sep = ''))
     for (i in maparms) {
       plot(mcmc.object[[trait]][,i], trace = FALSE, density = TRUE,
            main = paste('summary plots of',i ,'for', pft, trait))
-      plot(trait.mcmc[[trait]][,i],density = FALSE, xlim =c(1, 50000))
+      plot(trait.mcmc[[trait]][,i],density = FALSE, xlim =c(1, 50000),
+           main = note)
       autocorr.plot(mcmc.object[[trait]][,i][1])
-      mtext(text = note, line = 3)
     }
+    dev.off()
   }
-  dev.off()
 }
