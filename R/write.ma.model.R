4241be15d6454fdf0cbc7d15dcf6d67ca1e185e4
David LeBauer <dlebauer@illinois.edu>
2010-11-05 18:30:05 +0000

    added dynamic regression model - parameters based on data set

--- R/write.ma.model.R
+++ R/write.ma.model.R
@@ -2,14 +2,15 @@
 ##      inserts prior distribution name and parameter values
 ##      adapted from R2WinBUGS write.model(
 ## The function is currently defined as
-write.ma.model <- function (ma.model, con = "model.bug",
+write.ma.model <- function (modelfile, outfile, reg.model,
                             pr.dist, pr.param.a, pr.param.b,
                             n, trt.n, site.n, ghs.n) {
 
-  model.text <- scan(file=".temp.jags",what="character",sep="@")
+  model.text <- scan(file=modelfile, what="character",sep="@")
   ## chose an uncommon separator in order to capture whole lines
 
   model.text <- gsub("%_%", "", model.text)
+  model.text <- gsub("REGMODEL", reg.model, model.text)
   model.text <- gsub("PRIORDIST", paste("d", pr.dist,sep=""), model.text)
   model.text <- gsub("PRIORPARAMA", pr.param.a, model.text)
   model.text <- gsub("PRIORPARAMB", pr.param.b, model.text)
@@ -22,5 +23,5 @@ write.ma.model <- function (ma.model, con = "model.bug",
   if(ghs.n > 1)   model.text <- gsub("\\#GGG", '', model.text)
   if(site.n > 1)  model.text <- gsub("\\#SSS", '', model.text)
   if(trt.n > 1)   model.text <- gsub("\\#TTT", '', model.text)
-  writeLines(model.text, con = con)
+  writeLines(model.text, con = outfile)
 }
