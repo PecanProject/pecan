7592ac49e5b70286c888068667fcd4aab2054189
David LeBauer <dlebauer@illinois.edu>
2010-11-09 18:16:09 +0000

    reduced length of samples to min chain length

--- R/pecan.samps.R
+++ R/pecan.samps.R
@@ -1,15 +1,16 @@
 pecan.samps <- function(trait.mcmc, priors) {
 
   trait.mat <- lapply(trait.mcmc, as.matrix)
+  n.samp <- min(sapply(trait.mat, function(x) nrow(x)))
   traits <- names(trait.mcmc)
   priors$n <- nrow(trait.mat[[1]])
   colnames(priors)[which(colnames(priors) %in% c('parama','paramb'))] <- c('a', 'b')
 
-  prior.samps <- sapply(1:nrow(priors), function(x) do.call(pr.samp,priors[x,]))
+  prior.samps <- sapply(1:n.samp, function(x) do.call(pr.samp,priors[x,]))
   colnames(prior.samps) <- rownames(priors)
 
   post.samps <- prior.samps
-  for (tri in traits) post.samps[,tri] <- trait.mat[[tri]][,1]
+  for (tri in traits) post.samps[1:n.samp,tri] <- trait.mat[[tri]][1:n.samp,1]
 
   ## Convert variables with different units in DB and ED
   ## Convert leaf width in mm to leaf width in m
