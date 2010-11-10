8c62c0315dbd63ed01d881f102255ab20419a469
David LeBauer <dlebauer@illinois.edu>
2010-11-09 19:24:12 -0600

    corrected elasticity calculation

--- R/pecan.SAcalcs.R
+++ R/pecan.SAcalcs.R
@@ -1,29 +1,9 @@
 pecan.SAcalcs <- function(runname, outvar, dat, dtheta.q, trait.defs, trait.samps) {
-                                        # runname <- c('post', 'prior')
-                                        # outvar <- c('agb', 'ssc')
 
   ## Values of Theta
   ## #######################
   ## Load dtheta values used in sensitivity analysis
   ## dtheta.q has cbind(lcl, ucl, mean, var, cv)
-                                        #!!need to fix error?
-
-  ## f is the output variable
-  if(runname == 'post') {
-    f <- dat[[outvar]][['output']][which(dat[["runtype"]] == "postsamp"),]
-  } else if (runname == 'prior') {
-    f <- dat[[outvar]][['output']][which(dat[["runtype"]] == "priorsamp"),]
-  } else { print ('runname id not valid')
-         }      
- 
-  ## Calculate overall mean and variance
-  ## average each run across years before calculations 
-  mean.f <- mean(rowMeans(f, na.rm=TRUE),  na.rm = TRUE)
-  var.f  <- var(rowMeans(f, na.rm=TRUE),  na.rm = TRUE)
-
-  mean.rowname <- paste(runname, 'means', sep = "") #identify row in f with mean data
-
-
   dq <- as.data.frame(signif(dtheta.q[[runname]], 4))
     
   colnames(dq) <- c('lcl.theta', 'ucl.theta', 'mean.theta', 'var.theta', 'cv.theta') 
@@ -33,30 +13,44 @@ pecan.SAcalcs <- function(runname, outvar, dat, dtheta.q, trait.defs, trait.samp
   .vmlt <- dq['Vm_low_temp', c('lcl.theta', 'ucl.theta', 'mean.theta') ]
   dq['Vm_low_temp', c('lcl.theta', 'ucl.theta', 'mean.theta')] <- c(.vmlt+273.15)
 
-  ## Start making table for Sensitivity Analysis
+  ## Calculate Values of 'f'
+  ## f is the model output variable, either 'agb' or 'ssc' for now
+  
+  ##  define 'f'
+  if(runname == 'post') {
+    f <- dat[[outvar]][['output']][which(dat[["runtype"]] == "postsamp"),]
+  } else if (runname == 'prior') {
+    f <- dat[[outvar]][['output']][which(dat[["runtype"]] == "priorsamp"),]
+  } else { print ('runname id not valid')
+         }      
+  
+  ## Calculate overall mean and variance
+  ## mean: average each run across years and ensemble runs
+  mean.f <- mean(rowMeans(f, na.rm=TRUE),  na.rm = TRUE)
+  ## var(f) from ensemble runs
+  var.f  <- var(rowMeans(f, na.rm=TRUE),  na.rm = TRUE)
+
+  #identify row in f with mean data for the runname = 'post' or 'prior'
+  mean.rowname <- paste(runname, 'means', sep = "") 
+
+  ## Make table of values used in Sensitivity Analysis
   satable <-  merge(dq, trait.defs, by = 'id')
   rownames(satable) <- satable$id
   
-  ## Values of f
-  ## #################
-  ## df from SA for change in f at mean
-  ## var(f) from ensemble runs
-  
-  ## all mean "f's" are from the same run
+  ## all mean "f's" are the same
   satable$mean.f <- mean(dat[[outvar]][['output']][mean.rowname,],na.rm=T)
+  ## calculate the values of f +/- df
   for (.j in seq(satable$id)) {
     for (.k in c('lcl', 'ucl')) {
       .j1 <- as.character(satable$fileid[.j])
       .j2 <- as.character(satable$id[.j])
       sa.rowname <- paste(runname, .k, '.', .j1, '-', sep="")
       if (sa.rowname %in% rownames(dat[[outvar]][['output']])){
-        satable[.j2, paste(.k, '.f', sep = '')]<- mean(dat[[outvar]][['output']][sa.rowname, ],na.rm=T)
+        satable[.j2, paste(.k, '.f', sep = '')] <- mean(dat[[outvar]][['output']][sa.rowname, ], na.rm=T)
       }
     }
   }
- 
-
-  
+   
   ##~~~~~~~~~~~~~~~~~~~~~~~
   ## Data Organization Done
   ##~~~~~~~~~~~~~~~~~~~~~~~
@@ -71,17 +65,20 @@ pecan.SAcalcs <- function(runname, outvar, dat, dtheta.q, trait.defs, trait.samp
   ymax <- max(satable[,c('mean.f', 'lcl.f', 'ucl.f')])*1.05
   if(is.na(ymin)) ymin <- min(0, mean.f - sqrt(var.f))
   if(is.na(ymax)) ymax <- mean.f + sqrt(var.f)
-  for (.jid in which(!is.na(satable$lcl.f))) {
-    fig.title <- as.character(satable$figid[.jid])
-    id <- satable$id[.jid]
-    dtheta.j <- as.numeric(satable[.jid, c('lcl.theta', 'mean.theta', 'ucl.theta')])
-    df.ij <- as.numeric(satable[.jid, c('lcl.f', 'mean.f', 'ucl.f')])
-    dfdth.terms <- dfdth(f = df.ij, th = dtheta.j, yrange = c(ymin, ymax), name = fig.title)
-    satable$df[.jid]  <- dfdth.terms$dfdth1
-    satable$d2f[.jid] <- dfdth.terms$dfdth2
-    ## elasticity
-    satable$elast[.jid] <- satable$d2f[.jid]*dtheta.j[2]/df.ij[2] 
-  }
+
+  dfdth.terms <- sapply(satable$id,
+                        function(x) {
+                          dfdth(f = satable[x, c('lcl.f', 'mean.f', 'ucl.f')],
+                                th = satable[x, c('lcl.theta', 'mean.theta', 'ucl.theta')],
+                                yrange = c(ymin, ymax),
+                                name = x)})
+  rownames(dfdth.terms) <- c('df', 'd2f')
+
+  satable <- cbind(satable, t(dfdth.terms))
+  
+  ## elasticity = (df/dth)/(mean.f/mean.theta)
+  satable$elast <- with(satable,
+                        as.numeric(df)/(mean.f/mean.theta))
   dev.off()
 
   sum.var <- sum(satable$var.theta)
