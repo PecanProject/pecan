da8e54d3ad75cac5a0336197bfa1471366881785
David LeBauer <dlebauer@illinois.edu>
2010-11-16 13:37:09 -0600

    added pdf plots

--- rscripts/pecan.results.R
+++ rscripts/pecan.results.R
@@ -6,15 +6,50 @@ agb <-edout[['agb']][['output']]
 
 data <-data.frame(prior = agb[pr.samp,3],post = agb[po.samp,3]) 
 
-
-ggplot(data=data) +
+## Manuscript Figures
+agbens <- ggplot(data=data) +
   opts(title = 'Switchgrass Yield (grey = prior, black = posterior)') +
   geom_density(aes(x = post)) +
   geom_density(aes(x = prior),colour = 'grey') +
-  scale_x_continuous('Aboveground Biomass Nov 2002 Mg ha-1') +
+  scale_x_continuous('November Aboveground Biomass (Mg ha-1 y-1) average over years 5-10') +
   scale_y_continuous('Density') +
   theme_bw()
-  
+
+pdf('out/agbens.pdf')
+agbens
+dev.off()
+
+## Trait Pdfs
+load('out/pecan.samps.Rdata')
+load('out/pecan.MA.Rdata')
+traits <- colnames(prior.samps)
+
+trait.plots <- list()
+for (i in seq(traits)) {
+  tri<-traits[i]
+  data <- data.frame(prior = prior.samps[,tri], post = post.samps[,tri])
+
+  plot <- ggplot(data=data) +
+    geom_density(aes(x=prior), color='grey') +
+      theme_bw() +
+        scale_x_continuous('units') +
+          scale_y_continuous('Density') +
+            opts(title = as.character(trait.defs[trait.defs$id == tri, 'figid']))
+
+  tr <- trait.data[[tri]]
+  if(!is.null(nrow(tr))) {
+    plot <- plot + geom_density(aes(x = post))
+    tri <- ifelse(tri == 'Vm0', 'Vcmax', tri)
+    plot <- plot + geom_point(aes(x=Y, y=0), data=tr) 
+  }
+  trait.plots[[tri]] <- plot
+}
+
+pdf('traitpdfs.pdf')
+trait.plots
+dev.off()
+
+## Diagnostic Plots
 load('out/satables.Rdata')
 postagb <- satables[['post']][['agb']]
 prioragb <- satables[['prior']][['agb']]
