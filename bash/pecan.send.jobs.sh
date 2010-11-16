d12e326c183bdf849b2a10c5fe6731eca09fc526
David LeBauer <dlebauer@illinois.edu>
2010-11-16 16:57:01 -0600

    simplified pecan.send.jobs by replacing long quoted string of commands with ssh -T ebi-cluster < script.sh, and putting the commands inside the script

--- bash/pecan.send.jobs.sh
+++ bash/pecan.send.jobs.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 EDIN=/home/scratch/$USER/pecan/edin
-cp out/saconfigs.tgz ./
-tar -zcf pecan.edrunfiles.tgz saconfigs.tgz bash/pecan.ed2in.create.sh bash/pecan.ed.batchjobs.sh
-
-rsync -outi pecan.edrunfiles.tgz ebi-cluster:$EDIN/pecan.edrunfiles.tgz
-wait
-ssh ebi-cluster "cd $EDIN; tar -zxf pecan.edrunfiles.tgz; wait; tar -zxf saconfigs.tgz; chmod +x bash/*sh; wait; bash/pecan.ed2in.create.sh"
+cd $PECANHOME
+rsync -outi saconfigs.tgz ebi-cluster:$EDIN/
+ssh -T ebi-cluster < bash/pecan.ed2in.create.sh
 wait
 ## unzip config files, set env vars, write ED2IN files, run ED ensemble, 
-ssh ebi-cluster "cd $EDIN; bash/pecan.ed.batchjobs.sh"
-
-
+ssh -T ebi-cluster < bash/pecan.ed.batchjobs.sh
