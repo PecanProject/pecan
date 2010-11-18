23e4288e2eb88c6e67b810db7cc6444d79574ff0
David LeBauer <dlebauer@illinois.edu>
2010-11-18 09:07:55 -0600

    minor edits to reading variables and formatting

--- rscripts/pecan.postrun.R
+++ rscripts/pecan.postrun.R
@@ -1,11 +1,14 @@
 source('R/trait.dictionary.R')
 source('R/pecan.edout.R')
+
 pecanhome <- system("echo $PWD", intern=TRUE)
-pecanout <- paste(pecanhome, '/out', sep = '')
-date <-  read.table(paste(pecanhome, '/edin/DATE', sep=''))
+pecanout  <- paste(pecanhome, '/out', sep = '')
+date      <-  read.table(paste(pecanhome, '/edin/DATE', sep=''))
 load(paste(pecanout, '/M.Rdata', sep=''))
-outdir   <- paste(pecanout, date, sep = '')
-yr0 <- as.numeric(system("echo $YR0", intern =TRUE))
-yrf   <- as.numeric(system("echo $YRF", intern =TRUE))
-edout <- pecan.edout(M, yr0, yrf, outdir)
+
+outdir    <- paste(pecanout, date, sep = '')
+yr0       <- as.numeric(system("echo $YR0", intern =TRUE))
+yrf       <- as.numeric(system("echo $YRF", intern =TRUE))
+edout     <- pecan.edout(M, yr0, yrf, outdir)
+
 save(edout, file=paste(pecanout, '/edout.Rdata', sep=''))
