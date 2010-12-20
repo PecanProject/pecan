d0b8afd82209ae102b4e4a41fecde70c5cd262d0
David LeBauer <dlebauer@illinois.edu>
2010-12-20 23:23:40 +0000

    changed renaming loops to 'rename' command for simplicity and speed

--- bash/pecan.writeconfigs.sh
+++ bash/pecan.writeconfigs.sh
@@ -4,19 +4,19 @@ R --vanilla < $PECANHOME/rscripts/pecan.writeconfigs.R
 cd $PECANOUT
 echo "finished writing configs"
 echo "renaming config files"
-for i in config*xml; do mv "${i}" "${i/config/c}"; done
-for i in *_*xml; do mv "${i}" "${i/_/}"; done
-for i in *_*xml; do mv "${i}" "${i/_/}"; done
-for i in *factor*xml; do mv "${i}" "${i/factor/}"; done
-for i in *root*xml; do mv "${i}" "${i/root/rt}"; done
-for i in *turnover*xml; do mv "${i}" "${i/turnover/tnvr}"; done
-for i in *conductance*xml; do mv "${i}" "${i/conductance/cndctnc}"; done
-for i in *respiration*xml; do mv "${i}" "${i/respiration/resp}"; done
-for i in *nonlocaldispersal*xml; do mv "${i}" "${i/nonlocaldispersal/nldisprs}"; done
-for i in *quantumefficiency*xml; do mv "${i}" "${i/quantumefficiency/quantef}"; done
-for i in *water*xml; do mv "${i}" "${i/water/h2o}"; done
-for i in *stomatalslope*xml; do mv "${i}" "${i/stomatalslope/stmslope}"; done
-for i in c*xml; do mv "${i}" "${i/.xml/}"; done
+rename config c *xml
+rename _ '' *xml
+rename factor '' *xml
+rename root rt *xml
+rename turnover tnvr *xml
+rename conductance cndctnc *xml
+rename respiration resp *xml
+rename nonlocaldispersal nldisprs *xml
+rename quantumefficiency quantef *xml
+rename water h2o *xml
+rename stomatalslope stmslope *xml
+rename .xml '' *xml
+
 echo "zipping config files to saconfigs.tgz"
 tar zcf saconfigs.tgz c.*
 rm c.*
