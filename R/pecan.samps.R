651a1bec97fc02307ae764227ecbe555f64b0486
David LeBauer <dlebauer@illinois.edu>
2011-01-14 17:39:51 -0600

    removed transformation of samps since data and priors are now transformed before meta-analysis

--- R/pecan.samps.R
+++ R/pecan.samps.R
@@ -12,28 +12,6 @@ pecan.samps <- function(trait.mcmc, priors) {
   post.samps <- prior.samps
   for (tri in traits) post.samps[1:n.samp,tri] <- trait.mat[[tri]][1:n.samp, 'beta.o']
 
-  ## Convert variables with different units in DB and ED
-  ## Convert leaf width in mm to leaf width in m
-  post.samps[,'leaf_width'] <- post.samps[,'leaf_width']/1000
-  prior.samps[,'leaf_width'] <- prior.samps[,'leaf_width']/1000
-  priors['leaf_width', "a"] <-  priors['leaf_width', "a"] - log(1000)
-
-  ## Transform leafN -> c2n_leaf
-  post.samps[,'leafN'] <- 48/post.samps[,'leafN']
-  prior.samps[,'leafN'] <- 48/prior.samps[,'leafN']
-  colnames(post.samps)[which(colnames(post.samps) == 'leafN')] <- 'c2n_leaf'
-  colnames(prior.samps)[which(colnames(prior.samps) == 'leafN')] <- 'c2n_leaf'
-
-  c2n_leaf <- do.call(pr.samp, as.list(c(priors['leafN',1:3],100000)))
-  .parms <- signif(fitdistr(c2n_leaf, "lognormal")$estimate, 2)
-  priors["c2n_leaf", 1:3] <- c("lnorm", .parms)
-  priors <- priors[-which(rownames(priors) == "leafN"),]
-
-  ## root maint resp = 50% of total root resp 
-  prior.samps[,'root_respiration_factor'] <- 0.5 * prior.samps[,'root_respiration_factor']
-  post.samps[,'root_respiration_factor'] <- 0.5 * post.samps[,'root_respiration_factor']
-  priors['root_respiration_factor', "a"] <- as.numeric(priors['root_respiration_factor', "a"]) - log(2)
-
   return(list(post.samps = post.samps,
               prior.samps = prior.samps,
               priors = priors)) 
