744917e2e6683453da632edef8a683b2cce82fcf
David LeBauer <dlebauer@illinois.edu>
2011-01-14 12:27:11 -0600

    added transformation of leaf width prior from mm to m

--- R/query.bety.priors.R
+++ R/query.bety.priors.R
@@ -1,10 +1,7 @@
-query.bety.priors <- function(pft){
+query.bety.priors <- function(pft, trstr){
   con <- query.bety.con()
   ## prior traits will be used in meta-analysis for traits w/ data
   ## or to query priors for traits w/o data
-
-  ## trstr is a list of the traits that ED can use
-  trstr <- "'mort2','cuticular_cond','dark_respiration_factor','plant_min_temp','growth_resp_factor','leaf_turnover_rate','leaf_width','nonlocal_dispersal','q','root_respiration_factor','root_turnover_rate','seedling_mortality','SLA','stomatal_slope','Vm_low_temp','quantum_efficiency','f_labile','leafN','water_conductance','Vm0','r_fract','storage_turnover_rate'"
   
   ## query 1: query the prior_id s assoc. with pft
   query1 <- paste("select pfts_priors.prior_id from pfts_priors where pfts_priors.pft_id in (select pfts.id from pfts where pfts.name in ('",pft,"'));", sep = "")
@@ -16,8 +13,18 @@ query.bety.priors <- function(pft){
   query2 <- paste("select distinct variables.name, distn, parama, paramb, n from priors join variables on priors.variable_id = variables.id where priors.id in (",pr.id.str,") and variables.name in (",trstr,");", sep = "")
   q2 <- dbSendQuery(con, query2)
   priors <- fetch ( q2, n = -1 )
+  priors$name[priors$name == 'SLA_gC_per_m2'] <- 'SLA'
   rownames(priors) <- priors$name
-  priors <- priors[,-1]
+  priors <- priors[, -which(colnames(priors)=='name')]
+  if(priors['leaf_width', 'distn']=='lnorm') {
+    priors['leaf_width','parama'] <- priors['leaf_width','parama'] - log(1000)
+  } else {
+    stop (paste('leaf_width prior not transformed \n
+           change prior distribution on leaf_width to lognormal \n
+           or add transformation of ', priors['leaf_width', 'distn'],
+                ' distribution to query.bety.priors.R',
+                sep = '' ))
+  }
   sink(file = 'out/priors.tex', split = FALSE)
   xtable(priors, caption="Raw table of priors")
   sink()
