76a03daa09063813de6b38672fe2017db141e91a
Carl Davidson <davids14@illinois.edu>
2011-02-25 00:38:28 -0600

    Attempt to integrate satest.R into the rest of the code. DOES NOT COMPILE.

--- R/write.configs.R
+++ R/write.configs.R
@@ -1,106 +1,36 @@
-write.configs <- function(M, SA, pft, prior.samps, post.samps, outdir) {
+write.configs <- function(ensemble_size, sensitivity_analysis, pft, ens.samps, quantile.samples, outdir, quantiles) {
+
+  priors$distn[priors$distn=='weib'] <- 'weibull'
 
   const <- pecan.config.constants(pft)
   PFT <- const$PFT
   CONFIG <- const$CONFIG
-  tr <- colnames(post.samps)
+  traits <-  colnames(ens.samps[[1]])
   filenames <- list()
 
-  if (M>0) { #if M==0, only do sens.anal.runs
-    seqM <- seq(1,M)
-    ## add leading zeroes to the file names to avoid confusion
-    zerosM <- sprintf("%04.0f", seqM) #"%05.0f" if > 10^5 runs, etc.
+  if(ensemble_size > 0 ) { # write files for ensemble
+    ens.sequence <- seq(1, ensemble_size)
+    zeros.ens <- sprintf("%04.0f", ens.sequence) #"%05.0f" if > 10^5 runs, etc.
+
+    samps <- halton(n = ensemble_size, dim = length(traits)) 
+    colnames(samps) <-  traits
 
 
-    samps <- halton(n = M, dim = ncol(post.samps)) 
-    colnames(samps) <- tr
-    
-    ## Insert samples from trait posteriors into config.xml files
-    for (m in seqM) {
-      zm <- zerosM[m]
-      PFTi <- PFT
-      for (k in tr) {
-        samp <- quantile( post.samps[,k], samps[m,k])
-        PFTi <- append.xmlNode(PFTi, xmlNode(k, samp))
+    foo <- lapply(ens.samps, function(x) subset(x, subset = )
+                
 
-      }
-      CONFIGi <- append.xmlNode(CONFIG, PFTi)
-      file <- paste(outdir, "/config.postsamp",zm,".xml",sep="")
-      saveXML(CONFIGi, file = file, indent = TRUE, prefix = '<?xml version=\"1.0\"?>\n<!DOCTYPE config SYSTEM \"ed.dtd\">\n')
-      filenames[['post.ensemble']][m]<-file
-      rm(PFTi)
-      
-
-      ## Insert samples from trait priors into config.xml files
-      PFTi <- PFT
-      for (k in tr) {
-        samp <- quantile(prior.samps[,k], samps[m,k])
-        PFTi <- append.xmlNode(PFTi, xmlNode(k, samp))
-      }
-      CONFIGi <- append.xmlNode(CONFIG, PFTi)
-      file <- paste(outdir, "/config.priorsamp",zm,".xml",sep="")
-      saveXML(CONFIGi, file = file, indent = TRUE, prefix = '<?xml version=\"1.0\"?>\n<!DOCTYPE config SYSTEM \"ed.dtd\">\n')
-      filenames[['prior.ensemble']][m]<-file
-    }
-    
-    if (SA) { #if SA is true    
-      
+  }
+  if (sensitivity_analysis) {
+  ## Create the config for median runs
+    for(runname in names(quantile.samples)) {
       PFTm <- PFT
-      for (tri in tr) {
-        PFTm <- append.xmlNode(PFTm, xmlNode(tri, post.dtheta.q[tri, 'mean']))
-      }
-      CONFIGm <- append.xmlNode(CONFIG, PFTm)
-      file <- paste(outdir, "/config.postmeans.xml", sep = '')
-      filenames[['postmeans']] <- file
-      saveXML(CONFIGm, file = file, indent = TRUE, prefix = '<?xml version=\"1.0\"?>\n<!DOCTYPE config SYSTEM \"ed.dtd\">\n')
-
-      for ( j in seq(tr)){
-        notj <- seq(tr)[-j]
-        PFTl <- append.xmlNode(PFT, xmlNode(tr[j], post.dtheta.q[tr[j], 'lcl']))
-        PFTu <- append.xmlNode(PFT, xmlNode(tr[j], post.dtheta.q[tr[j], 'ucl']))
-        for (k in notj) {
-          pmean <- post.dtheta.q[tr[k], 'mean']
-          PFTl <- append.xmlNode(PFTl, xmlNode(tr[k], pmean))
-          PFTu <- append.xmlNode(PFTu, xmlNode(tr[k], pmean))
-        }
-        CONFIGl <- append.xmlNode(CONFIG, PFTl)
-        CONFIGu <- append.xmlNode(CONFIG, PFTu)
-        filel <- paste(outdir, "/config.postlcl.", tr[j],".xml", sep="")
-        fileu <- paste(outdir, "/config.postucl.", tr[j],".xml", sep="")
-        filenames[['postSA']] <- c(filel, fileu)
-        saveXML(CONFIGl, file = filel, indent = TRUE, prefix = '<?xml version=\"1.0\"?>\n<!DOCTYPE config SYSTEM \"ed.dtd\">\n')
-        saveXML(CONFIGu, file = fileu, indent = TRUE, prefix = '<?xml version=\"1.0\"?>\n<!DOCTYPE config SYSTEM \"ed.dtd\">\n')
-      }
-      
-      PFTm <- PFT
-      for (tri in tr) {
-        PFTm <- append.xmlNode(PFTm, xmlNode(tri, prior.dtheta.q[tri, 'mean']))
+      for (tri in traits) { 
+        PFTm <- append.xmlNode(PFTm, xmlNode(tri, quantile.samples[[runname]][[tri]]))
       }
       CONFIGm <- append.xmlNode(CONFIG, PFTm)
       file <- paste(outdir, "/config.priormeans.xml", sep = '')
       filenames[['priormeans']] <- file
       saveXML(CONFIGm, file = file, indent = TRUE, prefix = '<?xml version=\"1.0\"?>\n<!DOCTYPE config SYSTEM \"ed.dtd\">\n')
-      
-      ##following will test +/- 15% CI
-      ##make prior<ucl|lcl><trait>
-      for ( j in seq(tr)){
-        notj <- seq(tr)[-j]
-        PFTl <- append.xmlNode(PFT, xmlNode(tr[j], prior.dtheta.q[tr[j], 'lcl']))
-        PFTu <- append.xmlNode(PFT, xmlNode(tr[j], prior.dtheta.q[tr[j], 'ucl']))
-        for (k in notj) {
-          pmean <- prior.dtheta.q[tr[k], 'mean']
-          PFTl <- append.xmlNode(PFTl, xmlNode(tr[k], pmean))
-          PFTu <- append.xmlNode(PFTu, xmlNode(tr[k], pmean))
-        }
-        CONFIGl <- append.xmlNode(CONFIG, PFTl)
-        CONFIGu <- append.xmlNode(CONFIG, PFTu)
-        filel <- paste(outdir, "/config.priorlcl.", tr[j],".xml", sep="")
-        fileu <- paste(outdir, "/config.priorucl.", tr[j],".xml", sep="")
-        filenames[['priorSA']] <- c(filel, fileu)
-        saveXML(CONFIGl, file = filel, indent = TRUE, prefix = '<?xml version=\"1.0\"?>\n<!DOCTYPE config SYSTEM \"ed.dtd\">\n')
-        saveXML(CONFIGu, file = fileu, indent = TRUE, prefix = '<?xml version=\"1.0\"?>\n<!DOCTYPE config SYSTEM \"ed.dtd\">\n')
-      }
-    }
+
   }
-  save(filenames, file = 'filenames.Rdata')
 }
--- rscripts/pecan.MA.runscript.R
+++ rscripts/pecan.MA.runscript.R
@@ -1,16 +1,13 @@
-require(XML)
 #settings.file = "~/pecan/settings.xml"
 settings.file <- system("echo $PECANSETTINGS", intern = TRUE)
 settings.xml = xmlTreeParse(settings.file)
 settings = xmlToList(settings.xml)
 if(!is.null(settings$Rlib)){ .libPaths(settings$Rlib)} 
-#pft   <- system("echo $PFT", intern = TRUE)
-#ITER  <- as.numeric(system("echo $ITER", intern = TRUE)) 
-#M     <- as.numeric(system("echo $ENSN", intern = TRUE))
-#outdir   <- system("echo $PECANOUT", intern = TRUE)
+
 pft    = settings$pft
-ITER   = as.numeric(settings$ma_iter)
-M      = as.numeric(settings$ensemble_size)
+ma_iter   = as.numeric(settings$ma_iter)
+ensemble_size = as.numeric(settings$ensemble_size)
+sensitivity_analysis = as.logical(settings$sensitivity_analysis)
 outdir = settings$outdir
 outfile1 <- paste(outdir, '/pecan.parms.Rdata', sep = '')
 save.image(outfile1)
@@ -25,19 +22,27 @@ if(settings$database$location == 'localhost'){
 }
 
 ## 1. get species list based on pft
-spstr <- query.bety.pft_species(pft,con=con)
+spstr <- query.bety.pft_species(pft, con=con)
 
 
 ## 2. get priors available for pft
 ## trstr is a list of the traits that ED can use
-  trstr <- "'mort2','cuticular_cond','dark_respiration_factor','plant_min_temp','growth_resp_factor','leaf_turnover_rate','leaf_width','nonlocal_dispersal','q','root_respiration_factor','root_turnover_rate','seedling_mortality','SLA_gC_per_m2','stomatal_slope','Vm_low_temp','quantum_efficiency','f_labile','c2n_leaf','water_conductance','Vm0','r_fract','storage_turnover_rate'" #SLA_gC_per_m2 is converted to SLA in query.bety.priors
+  trstr <- "'mort2','cuticular_cond','dark_respiration_factor','plant_min_temp','growth_resp_factor','leaf_turnover_rate','leaf_width','nonlocal_dispersal','q','root_respiration_factor','root_turnover_rate','seedling_mortality','SLA_m2_per_gC','stomatal_slope','Vm_low_temp','quantum_efficiency','f_labile','c2n_leaf','water_conductance','Vm0','r_fract','storage_turnover_rate'" #SLA_gC_per_m2 is converted to SLA in query.bety.priors
 
 priors <- query.bety.priors(pft, trstr,out=outdir,con=con)
 print(priors)
 
-trvec <- rownames(priors) # vector of traits with prior distributions for pft 
 
-traits <- trvec
+## Set gamma distribution prior on
+prior.var <- function(x) do.call(pdf.stats, list(x$distn, x$parama, x$paramb))['var']
+prior.variances <- data.frame(var = sapply(1:nrow(priors), function(i) prior.var(priors[i,])),
+                              row.names = rownames(priors))
+
+taupriors <- list(tauA = 0.01,
+                  tauB = apply(prior.variances, 1, function(x) min(0.01, x)))
+
+traits <- trvec <- rownames(priors) # vector of traits with prior distributions for pft 
+
 trait.defs <- trait.dictionary(trvec)
 
 save(trait.defs, file = paste(outdir, '/trait.defs.Rdata', sep=''))
@@ -46,8 +51,7 @@ trait.data <- query.bety.traits(spstr,trvec,con=con)
 ## returns list 'trait.data' with one dataframe per variable 
 
 ## run the meta-analysis
-trait.mcmc <- pecan.ma(trait.data, priors, j.iter = ITER,settings)
-
+trait.mcmc <- pecan.ma(trait.data, priors, taupriors, j.iter = ITER, settings)
 pecan.ma.summary(trait.mcmc, pft,outdir)
 outfile2 <- paste(outdir, '/pecan.MA.Rdata', sep = '')
 save.image(outfile2)
