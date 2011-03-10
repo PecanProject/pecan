1253ed288dca2d5cd3670f8e0720785036afff93
David LeBauer <dlebauer@illinois.edu>
2011-03-10 15:37:42 -0600

    changing pecan.ma.sh, pecan.MA.runscript.R

--- bash/pecan.ma.sh
+++ bash/pecan.ma.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
+env PECANSETTINGS=$1 R --vanilla < ./rscripts/settings.R
+R --vanilla < ./rscripts/query.bety.R
 echo "start meta-analysis"
-#echo PFT is $1
-#echo MCMC chain length is $2
-#echo Ensemble Size is $3 
-#env PFT=$1 ITER=$2 ENSN=$3 PECANOUT=$4 R --vanilla < ./rscripts/pecan.MA.runscript.R
-env PECANSETTINGS=$1 R --vanilla < ./rscripts/pecan.MA.runscript.R
+R --vanilla < ./rscripts/metaanalysis.runscript.R
 wait
 echo "Meta-analysis complete"
--- rscripts/pecan.MA.runscript.R
+++ rscripts/pecan.MA.runscript.R
@@ -1,33 +1,7 @@
-
-require(XML)
-setwd('~/pecan')  
-settings.file <- '~/pecan/settings.xml'
-
-settings.xml <- xmlTreeParse(settings.file)
-settings <- xmlToList(settings.xml)
-if(!is.null(settings$Rlib)){ .libPaths(settings$Rlib)} 
-
-pft                  <- settings$pft
-ma_iter              <- as.numeric(settings$ma_iter)
-ensemble_size        <- as.numeric(settings$ensemble_size)
-sensitivity_analysis <- as.logical(settings$sensitivity_analysis)
-outdir               <- settings$outdir
-runnames <- settings$runnames
-require(PECAn)
-
-  ## connect to database
-  con <- settings$database
-  if(settings$database$location == 'localhost'){
-    con <- query.bety.con(dbname=con$name,password=con$passwd,username=con$userid)
-  }
-
-## 1. get species list based on pft
-  spstr <- query.bety.pft_species(pft, con=con)
   
-  
-  ## 2. get priors available for pft
-  ## trstr is a list of the traits that ED can use
-  trstr <- "'mort2','cuticular_cond','dark_respiration_factor','plant_min_temp','growth_resp_factor','leaf_turnover_rate','leaf_width','nonlocal_dispersal','q','root_respiration_factor','root_turnover_rate','seedling_mortality','SLA_m2_per_gC','stomatal_slope','Vm_low_temp','quantum_efficiency','f_labile','c2n_leaf','water_conductance','Vm0','r_fract','storage_turnover_rate'" #SLA_gC_per_m2 is converted to SLA in query.bety.priors
+## 2. get priors available for pft
+## trstr is a list of the traits that ED can use
+trstr <- "'mort2','cuticular_cond','dark_respiration_factor','plant_min_temp','growth_resp_factor','leaf_turnover_rate','leaf_width','nonlocal_dispersal','q','root_respiration_factor','root_turnover_rate','seedling_mortality','SLA_m2_per_gC','stomatal_slope','Vm_low_temp','quantum_efficiency','f_labile','c2n_leaf','water_conductance','Vm0','r_fract','storage_turnover_rate'" #SLA_gC_per_m2 is converted to SLA in query.bety.priors
   
   priors <- query.bety.priors(pft, trstr,out=outdir,con=con)
 
