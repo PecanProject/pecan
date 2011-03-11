8aa97a15c6ac8773778b057b55cfebe2c31e5fd4
David LeBauer <dlebauer@illinois.edu>
2011-03-11 17:12:19 -0600

    end of week commit

--- tests/test.query.bety.trait.data.R
+++ tests/test.query.bety.trait.data.R
@@ -1,4 +1,5 @@
-spstr <- "'938'"
+
+test.query.bety.trait.data <- function(spstr <- "'938'"
 
 query.bety.trait.data('LMA', spstr) # should return error message
 query.bety.trait.data('SLA', spstr) # should return data
