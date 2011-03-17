6bf5f82c8388ffc6768848b6cda1d63f09b27e11
David LeBauer <dlebauer@illinois.edu>
2011-03-17 15:55:27 -0500

    added new (blank) functions for start run and write.config discussed in meeting with mike and carl

---
+++ R/start.run.ed.R
@@ -0,0 +1,19 @@
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} .. starts one run for one site with one set of parameter settings. Minimalistic function for starting a single run. called once for each run.
+##' @title 
+##' @param model.settings
+##' @param pecan.settings
+##' @param config.file  from file.name from write.config()
+##' @param site 
+##' ##' @return runs
+##' @author David
+start.run.ed <- function(model.settings, pecan.settings, config.file, site) {
+# move scripts to remote host
+
+# call write.config
+  write.config(list, filename, configheader))
+# run script on remote machine
+
+# figure out when runs are finished
+}
