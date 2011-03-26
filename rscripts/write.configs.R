e23b3aef0d8f317c75728110cf36fc702451fa1a
Carl Davidson <davids14@illinois.edu>
2011-03-26 18:28:36 -0500

    Implemented model specific convert function.

--- rscripts/write.configs.R
+++ rscripts/write.configs.R
@@ -21,7 +21,6 @@ outdirs <- unlist(xpathApply(settings.xml, '//pfts//pft//outdir', xmlValue))
 ## for each pft
 ## traits, prior distributions, trait.mcmc
 
-##TODO make sure that all of the traits with priors end up in the trait.samples and other lists and config files
 trait.samples <- list()
 sa.samples <- list()
 ensemble.samples <- list()
@@ -39,27 +38,21 @@ for (i in seq(pft.names)){
   traits <- names(trait.mcmc)
   #KLUDGE: assumes mcmc for first trait is the same size as others
   samples.num <- nrow(as.matrix(trait.mcmc[[1]]))
-  browser()
   priors<-rownames(prior.distns)
-  ## convert trait.mcmc for each pft to dataframes in trait.samples
   trait.samples <- list()
   for (prior in priors) {
     if (prior %in% traits)
-      sample <- as.matrix(trait.mcmc[[prior]][,'beta.o'])
+      samples <- as.matrix(trait.mcmc[[prior]][,'beta.o'])
     else
-      sample <- get.sample(prior.distns[prior,], samples.num)
-    trait.samples[[pft.name]][[prior]] <- sample
+      samples <- get.sample(prior.distns[prior,], samples.num)
+    trait.samples[[pft.name]][[prior]] <- samples
   }
-  ## TODO ed specific tranform function
-  ## convert SLA from kg leaf / m2 to kg C / m2
-  ## result[, c('mean','stat')] <- result[, c('mean','stat')] / 0.48 
-  ## convert leaf width / 1000
-  
+
   ## subset the trait.samples to ensemble size using Halton sequence 
   ensemble.samples[[pft.name]] <- get.ensemble.samples(settings$ensemble$size, trait.samples[[pft.name]])
   run.ids[[pft.name]] <- write.ensemble.configs(pft.xml, ensemble.samples[[pft.name]], outdirs[i])
   
-  if(!is.null(settings$sensitivity.analysis)) {
+  if('sensitivity.analysis' %in% names(settings)) {
     quantiles <- get.quantiles(settings$sensitivity.analysis$quantiles)
     sa.samples[[pft.name]] <-  get.sa.samples(trait.samples[[pft.name]], quantiles)
     run.ids <- write.sa.configs(pft.xml, sa.samples[[pft.name]], outdirs[i])
