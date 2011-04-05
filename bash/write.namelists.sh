cdba515be005686e196d71a7fe9fc5793d13e03f
David LeBauer <dlebauer@illinois.edu>
2011-04-04 20:21:46 -0500

    added output directory that is model.output/YYYY.MM.DD-HH.MM

--- bash/write.namelists.sh
+++ bash/write.namelists.sh
@@ -1,29 +1,24 @@
-EDIN=/home/scratch/$USER/pecan/edin
-DATE=`date +%Y%m%d`
-OUTDIR=/home/scratch/pecan/$USER/out$DATE
-ED_RUN=$HOME/EDBRAMS/ED/run
-
-# make new directory
-mkdir --parents $OUTDIR   
-
+EDOUTDIR=$1
+cd $EDOUTDIR
 # remove old config and ED2IN files
-rm $ED_RUN/ED2INc*
-rm $ED_RUN/c*
-rm $EDIN/c.* 
+rm ED2INc*
+rm c.*
 
-cd $EDIN
+#Create Date
+RUNTIME=`date +%Y.%m.%d-%H.%M`
+mkdir $RUNTIME
 
 # unzip new config files
-tar -zxf saconfigs.tgz 
+tar -zxf configs.tgz 
 
 # create new ED2IN file for each config file
 for f in c.*
 do 
+    echo $f
   #Create ED2IN file from template
-  ED2INc=$ED_RUN/ED2IN$f
-  cp $f $ED_RUN/$f
+  ED2INc=ED2IN$f
   cp aED2IN $ED2INc
-  sed -i 's/YYYYMMDD/'$DATE'/g' $ED2INc
+  sed -i 's/RUNTIME/'$RUNTIME'/g' $ED2INc
   sed -i 's/ENSNAME/'$f'/g' $ED2INc
   sed -i 's/USER/'$USER'/g' $ED2INc
   sed -i 's/CONFIGFILE/'$f'/g' $ED2INc
@@ -32,3 +27,5 @@ do
   sed -i 's/HISTFILE/hist'$f'/g' $ED2INc
   sed -i 's/histconfig./hist./g' $ED2INc
 done
+## Need to rewrite in R to facilitate naming out-directory from settings.pavi.xml
+## or get xpath installed (???)
