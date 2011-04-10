c451af56d97f15b97b4d1c49b8c54bde8cdbfa90
Carl Davidson <davids14@illinois.edu>
2011-04-10 16:13:47 -0500

    Merged read.output.R with sensitivity.analysis.R

--- rscripts/sensitivity.analysis.R
+++ rscripts/sensitivity.analysis.R
@@ -12,6 +12,9 @@ if(interactive()){
   settings.file <- system("echo $PECANSETTINGS", intern = TRUE)
 }
 
+print('Enter runtime: ')
+run.time <- readline()
+
 settings.xml <- xmlParse(settings.file)
 settings <- xmlToList(settings.xml)
 
@@ -20,42 +23,23 @@ library(PECAn)
 
 pft <- settings$pfts[[1]]
 outdir <- settings$outdir
+host<- settings$run$host
 
-load(paste(outdir, 'trait.samples.Rdata', sep=''))
-load(paste(outdir, 'sa.output.Rdata', sep=''))
-load(paste(outdir, 'sa.samples.Rdata', sep=''))
-#load(paste(outdir,'ensemble.output.Rdata', sep='')
+#retrieve output from host machine
+system(paste('rsync -outi ', 
+        host$name, ':', host$outdir, run.time, '/* ',
+        pft$outdir, sep = ''))
 
+#ensemble.output <- read.ensemble.output(settings$ensemble$size, pft$outdir)
 
-
-sensitivity.analysis <- function(trait.samples, sa.samples, sa.output){
-  traits <- names(trait.samples)
-  sa.splinefuns <- sapply(traits, function(trait) sa.splinefun(sa.samples[[trait]], sa.output[[trait]]))
-  
-  
-  saplots <-  lapply(traits, function(x) sensitivity.plot(sa.samples[[x]], sa.splinefuns[[x]], x))
-  pdf('sensitivity.analysis.pdf', height = 12, width = 20)
-  saplots#left='Aboveground Biomass', main='Parameter Sensitivity', nrow=3,ncol=5) 
-  dev.off()
-  
-  spline.estimates <- sapply(traits, function(trait) sa.splinefuns[[trait]](trait.samples[[trait]]))
-  spline.estimates <- zero.truncate(spline.estimates)
-  sensitivities <- sapply(traits, function(trait) get.sensitivity(trait.samples[[trait]], sa.splinefuns[[trait]]))
-  elasticities <- sapply(traits, 
-      function(trait) get.elasticity(sensitivities[[trait]], trait.samples[[trait]], spline.estimates[,trait]))
-  explained.variances <- get.explained.variances(spline.estimates)
-  
-  #TODO: move unit conversions to their own method, called before sensitivity analysis 
-  if('Vm_low_temp' %in% traits)
-    trait.samples[which(traits == 'Vm_low_temp')] <- trait.samples[which(traits == 'Vm_low_temp')] + 273.15
-  coef.vars <- sapply(trait.samples, get.coef.var)
-  
-  pdf('variancedecomposition.pdf', width = 12, height = 8)
-  grid.arrange(qplot(names(explained.variances), coef.vars) + coord_flip(),
-      qplot(1:length(explained.variances), elasticities) + coord_flip(),
-      qplot(1:length(explained.variances), explained.variances) + coord_flip(),
-      ncol = 3)
-  dev.off()
+if('sensitivity.analysis' %in% names(settings)) {
+  load(paste(outdir, 'trait.samples.Rdata', sep=''))
+  load(paste(outdir, 'sa.samples.Rdata', sep=''))
+  traits <- names(trait.samples[[pft$name]])
+  quantiles.str <- rownames(sa.samples[[pft$name]])
+  quantiles.str <- quantiles.str[which(quantiles.str != '50')]
+  quantiles <- as.numeric(quantiles.str)/100
+  sa.agb <- read.sa.output(traits, quantiles, pft$outdir)
+  sensitivity.analysis(trait.samples[[pft$name]], sa.samples[[pft$name]], sa.agb)
 }
 
-sensitivity.analysis(trait.samples[[1]], sa.samples[[1]], sa.agb)
