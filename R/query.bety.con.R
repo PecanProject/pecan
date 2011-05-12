7083bdc115d49b9f096588e69e7634740e6f04e4
David LeBauer <dlebauer@illinois.edu>
2011-05-12 13:52:51 -0500

    added documentation, example to query.bety.con

--- R/query.bety.con.R
+++ R/query.bety.con.R
@@ -1,6 +1,35 @@
+##' This is a generic function that sets a connection to BETYdb
+##'
+##' \details{
+##'   This function requires a file in $HOME/.my.cnf that allows user to automatically log in to BETYdb. This file is used to specify the database name, <username>, and <password>. The contents of the file should be:
+##' \code{
+##'    [ebi_analysis]
+##'    user = <username>
+##'    password = <password>
+##'    host = ebi-forecast.igb.uiuc.edu
+##'    database = ebi_analysis
+##'  }
+##'  or, if database is on localhost:
+##'  \code{
+##'    [ebi_analysis]
+##'    user = <username>
+##'    password = <password>
+##'    host = localhost
+##'    database = ebi_analysis
+##'  }
+##' @title Setup connection to BETYdb
+##' @param ... 
+##' @return con, database connection 
 query.bety.con <- function(...){
   lapply(dbListConnections(MySQL()), dbDisconnect) #first kill all connections
   dvr <- dbDriver ("MySQL")
   con <- dbConnect(dvr, group  = 'ebi_analysis',...)
   return(con)
 }
+##' @examples
+##' ## query all SLA data from database
+##' con <- query.bety.con()
+##' query <- "select mean from traits where variable_id in (select id from variables where name like 'SLA');"
+##' sla.data <- fetch(dbSendQuery(con, query), n=-1) 
+##' hist(sla.data)
+
