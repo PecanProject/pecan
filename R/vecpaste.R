5125d563f875159fd649c0f3e2e1dde6bd20f84d
Carl Davidson <davids14@illinois.edu>
2011-05-16 18:07:05 -0500

    Addressed bug introduced by last revision.

--- R/vecpaste.R
+++ R/vecpaste.R
@@ -1,11 +1,2 @@
 ## vecpaste, turns vector into comma del. string 
-vecpaste <- function(x){
-  y <- paste("'",x[1],sep="")
-  if(length(x) > 1){
-    for(i in 2:length(x)){history 
-                          y <- paste(y,x[i],sep="','")
-                        }
-  }
-  y <- paste(y,"'",sep="")
-  y
-}
+vecpaste <- function(x) paste(paste("'", x, "'", sep=''), collapse=',')
