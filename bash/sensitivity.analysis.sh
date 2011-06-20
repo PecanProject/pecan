972b0dc0463d4423183096335d006b38db3aa292
David LeBauer <dlebauer@illinois.edu>
2011-06-20 10:18:14 -0500

    changed name of sensitivity analysis bash file and name of the R script that it calls

--- bash/sensitivity.analysis.sh
+++ bash/sensitivity.analysis.sh
@@ -3,6 +3,6 @@ echo "begin Sensitivity Analysis"
 #rsync ebi-cluster:/home/scratch/$USER/pecan/edin/DATE $PWD/out/
 DATE=`cat $PWD/out/DATE`
 
-R --vanilla < rscripts/pecan.SA.runscript.R
+R --vanilla < rscripts/sensitivity.analysis.R
 echo "end Sensitivity Analysis"
 
