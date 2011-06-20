2fe7afe8618e58642bad587fe5d59c7dc908f7f1
David LeBauer <dlebauer@illinois.edu>
2011-06-20 10:51:54 -0500

    fixed sensitivity analysis scripts to call settings file

--- bash/sensitivity.analysis.sh
+++ bash/sensitivity.analysis.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 echo "begin Sensitivity Analysis"
-#rsync ebi-cluster:/home/scratch/$USER/pecan/edin/DATE $PWD/out/
-DATE=`cat $PWD/out/DATE`
-
-R --vanilla < rscripts/sensitivity.analysis.R
+env PECANSETTINGS=$1 R --vanilla < rscripts/sensitivity.analysis.R
 echo "end Sensitivity Analysis"
 
