a37a81ac74ae1d73b1f2e77eabaa6657b6269783
David LeBauer <dlebauer@illinois.edu>
2011-07-05 18:14:37 -0500

    moved functions from pr.samp.R to utils.R

--- R/pr.samp.R
+++
@@ -1,8 +0,0 @@
-
-pr.samp <- function(distn, parama, paramb, n) {
-    do.call(paste('r', distn, sep=""), list(n, parama, paramb))
-}
-
-get.sample <- function(prior, n) {
-  do.call(paste('r', prior$distn, sep=""), list(n, prior$parama, prior$paramb))
-}
--- R/utils.R
+++ R/utils.R
@@ -44,3 +44,9 @@ get.units <- function(traits) {
   return(ans)
 }
  
+pr.samp <- function(distn, parama, paramb, n) {
+    do.call(paste('r', distn, sep=""), list(n, parama, paramb))
+}
+get.sample <- function(prior, n) {
+  do.call(paste('r', prior$distn, sep=""), list(n, prior$parama, prior$paramb))
+}
