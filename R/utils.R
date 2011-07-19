da4805743b5604bb7e0ae96b46bfd3dd118e2a12
David LeBauer <dlebauer@illinois.edu>
2011-07-19 15:50:53 -0500

    refs #122 added transfer.bety() function to mimic db_copy.sh script. currently does not work

--- R/utils.R
+++ R/utils.R
@@ -121,3 +121,34 @@ zero.bounded.density <- function (x, bw = "SJ") {
   g$x <- c(0, xgrid)
   return(g)
 }
+##' Update ebi_analysis with information in ebi_production
+##'
+##' Backs up ebi_analysis, copies ebi_production to ebi_analysis. This function is based on the db_copy.sh script, and is useful 
+##' @title 
+##' @param tables.to.exclude list of large tables that do not need to be backed up (excluding default values add 1 hr to backup time)
+##' @return updated ebi_analysis
+##' @author David LeBauer, Patrick Mulrooney
+transfer.bety <- function(tables.to.exclude =
+                          c('counties', 'county_boundaries',
+                            'county_paths', 'location_yields')){
+  
+  USER="ebi_user"
+  PASSWORD="mScGKxhPhdq"
+  DB="ebi_analysis"
+  backup.dir <- "/home/share/ebi_analysis-backup.sql"
+  ignore.tables <- paste(paste(' --ignore-table=', tables.to.exclude, sep = ''), collapse = " ")
+  print("Backing up $DB to $DB-backup.sql")
+  backup.db <- paste("mysqldump ", DB, ignore.tables, " -u ", USER, " -p", PASSWORD," >> ", backup.dir, sep = '')
+  system(backup.db)
+  
+  print("Dropping tables from $DB")
+  drop.tables <- paste("mysqldump ", DB, " -u ", USER, " -p", PASSWORD, ' ', ignore.tables," --add-drop-table --no-data | grep ^DROP | mysql -u ", USER, " -p", PASSWORD, ' ', DB, sep = '')
+  system(drop.tables)
+  
+
+  print("Transfering tables from ebi_production to $DB")
+  transfer.tables <- paste("mysqldump -u", USER, " -p", PASSWORD, ' ', ignore.tables, " ebi_production | mysql -u ", USER, " -p", PASSWORD, ' ', DB, sep = '')
+  system(transfer.tables)
+                                        #mysqldump -u $DB_USER -p$DB_PASSWORD ebi_production | mysql -u $DB_USER -p$DB_PASSWORD $DB                                                                     
+  print("Done!") 
+}
