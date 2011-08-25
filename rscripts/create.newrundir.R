7b61f3358c4fb785cdb5eaa7cad6a25a96654692
David LeBauer <dlebauer@illinois.edu>
2011-08-25 11:08:12 -0500

    finished create.newrundir scripts

--- rscripts/create.newrundir.R
+++ rscripts/create.newrundir.R
@@ -1,35 +1,62 @@
 library(XML)
 
-settings.template.filename <- Sys.getenv("SETTINGSTEMPLATE")
+if(interactive()){
+  Sys.setenv(SETTINGSTEMPLATE='template.settings.ebifarm.pavi.xml')
+  Sys.setenv(RUNDIRNAME='test')
+}
+# load variables
+template.settings.filename <- Sys.getenv("SETTINGSTEMPLATE")
 rundirname <- Sys.getenv("RUNDIRNAME")
 
-settings <- xmlToList(xmlParse(settings.template.filename))
+# load template settings file (has everything except rundirname)
+template.settings <- xmlToList(xmlParse(paste('edin/', template.settings.filename, sep = '')))
 
-outdir <- paste(settings$pecanDir, rundirname, sep = '')
+# load utility commands
+source(paste(template.settings$pecanDir, 'R/utils.R', sep = ''))
 
-sed <- function(find, replace, dirname = getwd(), filename){
-  system(paste("sed -i 's/", find, "/", replace, "/g' ", dirname, '/', filename, sep = ''))
-}
-cp  <- function(option = '', from = getwd(), to = getwd(), oldfilename, newfilename) {
-  system(paste('cp', option, paste(from, oldfilename, sep = '/'), paste(to, newfilename, sep = '/')))
-}
-mkdir <- function(dir) {
-  system(paste('mkdir', dir))
-}
 
-if(!rundirname %in% dir(settings$pecanDir)) mkdir(outdir)
-settings.filename <- paste('settings.', settings$pfts$pft$name, '.xml', sep = '')
-ed2in.filename    <- paste('templateED2IN-', settings$pfts$pft$name, sep = '')
+############
+# Local Host
+############
+# name output directory on localhost
+outdir <- paste(template.settings$pecanDir, rundirname, '/', sep = '')
 
-cp(from         = paste(settings$pecanDir, '/edin/', sep = ''),
+## create new run directory
+if(!rundirname %in% dir(template.settings$pecanDir)) mkdir(outdir)
+
+## name settings and ed2in files
+settings.filename <- paste('settings.', template.settings$pfts$pft$name, '.xml', sep = '')
+ed2in.filename    <- paste('templateED2IN-', template.settings$pfts$pft$name, sep = '')
+
+## move settings file to new rundir
+cp(from         = paste(template.settings$pecanDir, 'edin/', sep = ''),
    to           = outdir,
-   oldfilename  = 'template.settings.xml',
+   oldfilename  = template.settings.filename,
    newfilename  = settings.filename)
+## find and replace rundirname in settings file (sed -i /RUNDIRNAME/rundirname/ )
 sed(find     = 'RUNDIRNAME',
     replace  = rundirname,
     dirname  = outdir,
     filename = settings.filename)
-cp(from         = paste(settings$pecanDir, '/edin/', sep = ''),
+# copy ed2in file over
+cp(from         = paste(template.settings$pecanDir, 'edin/', sep = ''),
    to           = outdir,
    oldfilename  = ed2in.filename,
    newfilename  = ed2in.filename)
+
+
+############################
+# Model Inputs and Outputs
+############################
+
+settings <- xmlToList(xmlParse(paste(outdir, settings.filename, sep = '')))
+# create outdir on model host
+system(paste("ssh -T ", settings$run$host$name,
+             " '",'mkdir -p ', settings$run$host$outdir,"'",sep=''))
+
+## make symbolic link from met data to model rundirectory
+
+hostrundir <- gsub('out', '', settings$run$host$outdir) #needs revision in settings file
+copy.met.command <- paste("cp -lr ", settings$run$host$inputs, settings$run$site$name,
+                          ' ', hostrundir , sep = '')
+system(paste("ssh -T ", settings$run$host$name, "'", copy.met.command, "'"))
