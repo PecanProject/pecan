918a57064406b79e30432d3830c026e19cbf975f
David LeBauer <dlebauer@illinois.edu>
2011-08-30 18:47:41 -0500

    added automatic repository selection for R, moved xml install to bash script

--- rscripts/pecan.init.R
+++ rscripts/pecan.init.R
@@ -1,11 +1,14 @@
 
 # these packages are required by PEcAn:
-list.of.packages <- c('XML', 'MASS', 'randtoolbox','rjags', 'ggplot2', 'RMySQL', 'gridExtra','xtable')
+list.of.packages <- c('MASS', 'randtoolbox','ggplot2', 'RMySQL', 'gridExtra','xtable')
 # of the required packages, these are not yet installed:
 new.packages <- list.of.packages[!(list.of.packages %in%
                                    installed.packages(lib = c(.Library, '~/lib/R'))[,"Package"])]
 # install required packages:
-if(length(new.packages)) install.packages(new.packages[new.packages != c('XML', 'rjags')], 
-			 lib = '~/lib/R', 
-			 repos = "http://lib.stat.cmu.edu/R/CRAN")
-if('XML' %in% 'new.packages') system('apt-get install r-core-xml')
+r <- getOption("repos")
+r["CRAN"] <- "http://cran.us.r-project.org"
+options(repos = r)
+rm(r)
+
+if(length(new.packages)) install.packages(new.packages[new.packages != c('XML')], 
+                                          lib = '~/lib/R')
