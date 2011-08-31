bb7f1efebebac97b1eb545dbe39652a1491f01b4
David LeBauer <dlebauer@illinois.edu>
2011-08-31 21:29:21 +0000

    changed from installing packages to in local folder to default folder

--- rscripts/install.dependencies.R
+++ rscripts/install.dependencies.R
@@ -2,13 +2,11 @@
 # these packages are required by PEcAn:
 list.of.packages <- c('MASS', 'randtoolbox','ggplot2', 'RMySQL', 'gridExtra','xtable')
 # of the required packages, these are not yet installed:
-new.packages <- list.of.packages[!(list.of.packages %in%
-                                   installed.packages(lib = c(.Library, '~/lib/R'))[,"Package"])]
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 # install required packages:
 r <- getOption("repos")
 r["CRAN"] <- "http://cran.us.r-project.org"
 options(repos = r)
 rm(r)
 
-if(length(new.packages)) install.packages(new.packages[new.packages != c('XML')], 
-                                          lib = '~/lib/R')
+if(length(new.packages)) install.packages(new.packages[new.packages != c('XML')])
