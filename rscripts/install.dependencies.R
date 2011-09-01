68a8e03aaa492916027f0fb93c2b7c672ac30904
David LeBauer <dlebauer@illinois.edu>
2011-09-01 16:50:30 -0500

    merged from dlebauer branch

--- rscripts/install.dependencies.R
+++ rscripts/install.dependencies.R
@@ -5,15 +5,15 @@ list.of.packages <- c('randtoolbox','ggplot2', 'RMySQL', 'gridExtra')
 # of the required packages, these are not yet installed:
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 
-new.packages <- list.of.packages[!(list.of.packages %in%
-                                   installed.packages(lib = c(.Library))[,"Package"])]
-
 # install required packages:
 r <- getOption("repos")
 r["CRAN"] <- "http://cran.us.r-project.org"
 options(repos = r)
 rm(r)
 
+
 cat(paste('packages to be installed \n', new.packages))
 
-if(length(new.packages)) install.packages(new.packages[new.packages != c('XML')])
+
+
+if(length(new.packages)) install.packages(new.packages)
