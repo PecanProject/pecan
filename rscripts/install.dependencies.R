5a51abab3eef6368acd3c94458e882885dcc2713
David LeBauer <dlebauer@illinois.edu>
2011-09-01 15:04:29 -0500

    new packages will be installed in the standard directory rather than in ~/lib/R which can be used as a workaround when there is no root access

--- rscripts/install.dependencies.R
+++ rscripts/install.dependencies.R
@@ -3,12 +3,12 @@
 list.of.packages <- c('MASS', 'randtoolbox','ggplot2', 'RMySQL', 'gridExtra','xtable')
 # of the required packages, these are not yet installed:
 new.packages <- list.of.packages[!(list.of.packages %in%
-                                   installed.packages(lib = c(.Library, '~/lib/R'))[,"Package"])]
+                                   installed.packages(lib = c(.Library))[,"Package"])]
 # install required packages:
 r <- getOption("repos")
 r["CRAN"] <- "http://cran.us.r-project.org"
 options(repos = r)
 rm(r)
 
-if(length(new.packages)) install.packages(new.packages[new.packages != c('XML')], 
-                                          lib = '~/lib/R')
+cat(paste('packages to be installed \n', new.packages))
+if(length(new.packages)) install.packages(new.packages[new.packages != c('XML')])
