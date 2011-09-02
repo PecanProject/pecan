a50dcf6582d2043d060e7f8a32781241af2d64ee
David LeBauer <dlebauer@illinois.edu>
2011-09-02 18:32:28 -0500

    refs #218; generalizes table import

--- bash/create.db.subset.sh
+++ bash/create.db.subset.sh
@@ -2,10 +2,10 @@
 
 # to be run after dump.db.subset.sh
 
-NEWDB={$1:-"pecan_subset"}
+NEWDB=$1
 mysqladmin create $NEWDB
 
 for table in citation cultivar covariate pft pfts_prior pfts_specie prior site specie trait treatment variable yield
 do
-    mysql pecan_subset < ${table}s.sql
+    mysql $NEWDB < ${table}s.sql
 done
