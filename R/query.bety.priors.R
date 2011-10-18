08c22c97fe2c61d4e31783ec8c8527be62493757
Carl Davidson <davids14@illinois.edu>
2011-10-17 23:15:34 -0500

    Fixed several bugs from last commit that were obscured from an unsuccessful build routine.

--- R/query.bety.priors.R
+++ R/query.bety.priors.R
@@ -8,22 +8,22 @@ query.bety.priors <- function(pft, trstr, out=NULL,con=NULL,...){
     return(NULL)
   }
     
-  query.text <- paste("select variables.name, distn, parama, paramb, n ",
+  query.text <- paste("select variables.name, distn, parama, paramb, n",
                   "from priors",
                   "join variables on priors.variable_id = variables.id",
-                  "join pft_priors on pfts_priors.prior_id = priors.id",
-                  "join pfts on pfts.id = pfts_priors.prior_id",
-                  "where pfts.name in (", pft, "')",
+                  "join pfts_priors on pfts_priors.prior_id = priors.id",
+                  "join pfts on pfts.id = pfts_priors.pft_id",
+                  "where pfts.name in (", vecpaste(pft), ")",
                   "and variables.name in (", trstr, ");")
   query    <- dbSendQuery(con, query.text)
-  priors <- fetch ( query.text, n = -1 )
+  priors <- fetch ( query, n = -1 )
   
   #HACK: this rename does not belong here. 
   #There should be a separate function for this called after query.bety.priors. 
   #-carl
   priors$name[priors$name == 'SLA_m2_per_gC'] <- 'SLA'
 
-  if(nrow(priors) > 0){
+  if(nrow(priors) <= 0){
     warning(paste("No priors found for pft(s): ", pft))
     priors <- priors[, which(colnames(priors)!='name')]
     return(priors)
@@ -31,6 +31,7 @@ query.bety.priors <- function(pft, trstr, out=NULL,con=NULL,...){
   else {    
     rownames(priors) <- priors$name
     priors <- priors[, which(colnames(priors)!='name')]
+    return(priors)
   }
 
 #  if(!is.null(out)){
@@ -39,5 +40,4 @@ query.bety.priors <- function(pft, trstr, out=NULL,con=NULL,...){
 #    sink()
 #  }
   
-  return(priors)
 }
