822c86882da512ab2c41e05584498d19847eba8f
David LeBauer <dlebauer@illinois.edu>
2011-10-28 11:08:25 -0500

    fixes #370

--- R/model.specific.R
+++ R/model.specific.R
@@ -81,11 +81,18 @@ write.config.ED <- function(pft, trait.samples, settings, outdir, run.id){
   saveXML(xml, file = paste(outdir, xml.file.name, sep=''), 
       indent=TRUE, prefix = PREFIX_XML)
   
+  startdate <- as.Date(settings$run$start.date)
+  enddate <- as.Date(settings$run$end.date)
   ed2in.text <- scan(file = pft$edin, 
       what="character",sep='@', quote=NULL, quiet=TRUE)
+  ed2in.text <- gsub('START_MONTH', format(startdate, "%m"), ed2in.text)
+  ed2in.text <- gsub('START_DAY', format(startdate, "%d"), ed2in.text)
+  ed2in.text <- gsub('START_YEAR', format(startdate, "%Y"), ed2in.text)
+  ed2in.text <- gsub('END_MONTH', format(enddate, "%m"), ed2in.text)
+  ed2in.text <- gsub('END_DAY', format(enddate, "%d"), ed2in.text)
+  ed2in.text <- gsub('END_YEAR', format(enddate, "%Y"), ed2in.text)
   ed2in.text <- gsub('OUTDIR', settings$run$host$outdir, ed2in.text)
   ed2in.text <- gsub('ENSNAME', run.id, ed2in.text)
-  ed2in.text <- gsub('USER', system('echo $USER', intern=TRUE), ed2in.text)
   ed2in.text <- gsub('CONFIGFILE', xml.file.name, ed2in.text)
   ed2in.text <- gsub('OUTFILE', paste('out', run.id, sep=''), ed2in.text)
   ed2in.text <- gsub('HISTFILE', paste('hist', run.id, sep=''), ed2in.text)
