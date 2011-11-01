de4c4bb8de56ad7ca72ca2a03275c8eea506b49c
David LeBauer <dlebauer@illinois.edu>
2011-11-01 13:52:00 -0500

    added pdf.stats function, for calculating basic statistics from parameterized distributions

--- R/utils.R
+++ R/utils.R
@@ -191,3 +191,33 @@ summarize.result <- function(result) {
   return(rbind(ans1, ans2))
 }
   
+##' @title calculate mean and variance statistics from a known distribution 
+##' @param distn name of distribution used by R (beta, f, gamma, lnorm, norm, weibull) 
+##' @param A first parameter 
+##' @param B second parameter
+##' @return list with mean, variance, and 95% CI
+##' @author David LeBauer
+pdf.stats <- function(distn, A, B) {
+  mean <- switch(distn,
+                 gamma   = A/B,
+                 lnorm   = exp(A + 1/2 * B^2),
+                 beta    = A/(A+B),
+                 weibull = B * gamma(1 + 1/A),
+                 norm    = A,
+                 f       = ifelse(B>2, B/(B - 2), mean(rf(10000, A, B)))
+                 )
+  var <- switch(distn,
+                gamma   = A/B^2,
+                lnorm   = exp(2*A + B^2) * (exp(B^2) - 1),
+                beta    =  A*B/((A+B)^2 * (A + B + 1)),
+                weibull = B^2 * (gamma(1 + 2 / A) - gamma(1 + 1/A)^2),
+                norm    = B^2,
+                f       = ifelse(B>4, 2*B^2*(A+B-2) / (A*(B-2)^2*(B-4)), var(rf(100000, A, B)))
+                )
+  qci  <- get(paste("q", distn, sep = ""))
+  ci <- qci(c(0.025, 0.975), A, B)
+  lcl <- ci[1]
+  ucl <- ci[2]
+  out  <- unlist(list(mean = mean, var = var, lcl = lcl, ucl = ucl)) 
+  return(out)
+}
