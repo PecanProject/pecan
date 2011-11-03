c866dd0735f22fa102bee445c6abc5df24e010d5
David LeBauer <dlebauer@illinois.edu>
2011-11-03 13:18:14 -0500

    fixes #394 by handling sensitivity analysis runs that do not complete and sending a warning

--- rscripts/sensitivity.analysis.R
+++ rscripts/sensitivity.analysis.R
@@ -3,7 +3,7 @@ if(interactive()){
   user <- Sys.getenv('USER')
   options(error = browser)
   if(user == 'dlebauer'){
-    settings.file = '~/out/2011.08.26/settings.ebifarm.pavi.xml'
+    settings.file = '~/in/ebifarm/post/ebifarm.pavi.xml'
   } else if(user == 'davids14') {
     settings.file = '~/pecan/tundra.xml'
   } else {
@@ -27,22 +27,32 @@ load(paste(outdir, 'output.Rdata', sep=''))
 load(paste(outdir, 'samples.Rdata', sep=''))
 
 for(pft in settings$pfts){
-  traits <- names(trait.samples[[pft$name]])
-  quantiles.str <- rownames(sa.samples[[pft$name]])
-  quantiles.str <- quantiles.str[which(quantiles.str != '50')]
-  quantiles <- as.numeric(quantiles.str)/100
-                                        #ensemble.output <- read.ensemble.output(settings$ensemble$size, outdir, pft.name=pft$name)
-  
   if('sensitivity.analysis' %in% names(settings)) {
-    sensitivity.results <- sensitivity.analysis(trait.samples = trait.samples[[pft$name]],
-                                                sa.samples = sa.samples[[pft$name]],
-                                                sa.output = sa.output[[pft$name]],
+
+    quantiles.str <- rownames(sa.samples[[pft$name]])
+    quantiles.str <- quantiles.str[which(quantiles.str != '50')]
+    quantiles <- as.numeric(quantiles.str)/100
+    ## ensemble.output <- read.ensemble.output(settings$ensemble$size, outdir, pft.name=pft$name)
+
+    ## only perform sensitivity analysis on traits where no more than 2 results are missing
+    traits <- names(trait.samples[[pft$name]])
+    good.saruns <- sapply(sensitivity.output[[pft$name]], function(x) sum(is.na(x)) <=2)
+    if(!all(good.saruns)) { # if any bad saruns, reduce list of traits and print warning
+      bad.saruns <- !good.saruns
+      warning(paste('missing >2 runs for', vecpaste(traits[bad.saruns]),
+                    '\n no sensitivity analysis or variance decomposition will be performed on these trait(s)',
+                    '\n it is likely that the runs did not complete, this should be fixed !!!!!!'))
+    }
+    
+    sensitivity.results <- sensitivity.analysis(trait.samples = trait.samples[[pft$name]][traits],
+                                                sa.samples = sa.samples[[pft$name]][ ,traits],
+                                                sa.output = sensitivity.output[[pft$name]][ ,traits],
                                                 outdir = pft$outdir)
-    sa.plots <- plot.sensitivities(sensitivity.results$sensitivity.plot.inputs,
+    sensitivity.plots <- plot.sensitivities(sensitivity.results$sensitivity.plot.inputs,
                                    linesize = 1,
                                    dotsize = 3)
     pdf(paste(outdir, 'sensitivityanalysis.pdf', sep = ''), height = 12, width = 9)
-    sa.plots
+    sensitivity.plots
     dev.off()
 
     vd.plots <- plot.variance.decomposition(sensitivity.results$variance.decomposition.plot.inputs)
