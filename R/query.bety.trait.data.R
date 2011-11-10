75e97ab8906fb9db18edca97c123be9041175894
Carl Davidson <davids14@illinois.edu>
2011-11-09 19:19:16 -0600

    Cut down the number of nested for loops for fineroot2leaf code in query.bety.trait.data.R

--- R/query.bety.trait.data.R
+++ R/query.bety.trait.data.R
@@ -270,49 +270,48 @@ query.bety.trait.data <- function(trait, spstr,con=NULL,...){
     ## query Q or FRC_RC
     query <- paste("select traits.citation_id, traits.id, variables.name as vname, traits.site_id, treatments.name, treatments.control, sites.greenhouse, traits.mean, traits.statname, traits.stat, traits.n, traits.date, traits.time, traits.cultivar_id, traits.specie_id from traits left join treatments on  (traits.treatment_id = treatments.id) left join sites on (traits.site_id = sites.id) left join variables on (traits.variable_id = variables.id) where specie_id in (", spstr,")  and variables.name in ('fineroot2leaf', 'FRC_RC');", sep = "")
     data <- fetch.stats2se(con, query)
+    print(data)
+    browser()
 
     ## query fine root biomass and leaf biomass
-    query <- paste("select traits.citation_id, traits.id, variables.name as vname, traits.site_id, treatments.name, treatments.control, sites.greenhouse, traits.mean, traits.statname, traits.stat, traits.n, traits.date, traits.time, traits.cultivar_id, traits.specie_id, traits.specie_id from traits left join treatments on  (traits.treatment_id = treatments.id) left join sites on (traits.site_id = sites.id) left join variables on (traits.variable_id = variables.id) where specie_id in (", spstr,")  and variables.name in ('fine_root_biomass','leaf_biomass');", sep = "")
+    query <- paste("select traits.citation_id, traits.id, variables.name as vname, traits.site_id, treatments.name, treatments.control, sites.greenhouse, traits.mean, traits.statname, traits.stat, traits.n, traits.date, traits.time, traits.cultivar_id, traits.specie_id from traits left join treatments on  (traits.treatment_id = treatments.id) left join sites on (traits.site_id = sites.id) left join variables on (traits.variable_id = variables.id) where specie_id in (", spstr,")  and variables.name in ('fine_root_biomass','leaf_biomass', 'Leaf Biomass', 'root_biomass');", sep = "")
     data2 <- fetch.stats2se(con, query)
 
     ## match above and below ground biomass
     ## match on citation_id, site_id, treatment_id, specie_id where different variables.name
     data3 = NULL
     if(nrow(data2) > 0){
-#      pair = list(); counter = 0;
-      for(cite in unique(data2$citation_id)){
-        selC = which(data2$citation_id == cite)
-        for(site in unique(data2$site_id[selC])){
-          selS = selC[which(data2$site_id[selC] == site)]
-          for(spp in unique(data2$specie_id[selS])){
-            selSp = selS[which(data2$specie_id[selS] == spp)]
-            for(tmt in unique(data2$name[selSp])){
-              selT = selSp[which(data2$name[selSp] == tmt)]
-
-              #ok, after all this you have a unique data -- does it constitute a root/leaf pair?
-              roots  = selT[which(data2$vname[selT] == "fine_root_biomass")]
-              leaves = selT[which(data2$vname[selT] == "leaf_biomass")]
-
-              if(length(roots) == 1 & length(leaves) == 1){
-                newrow = data2[roots,]; newrow$vname = 'fineroot2leaf'
-                if(is.na(data2$stat[leaves])){
-                  newrow$mean = newrow$mean/data2$mean[leaves]
-                  newrow$stat = newrow$stat/data2$mean[leaves]
-                } else {
-                  ## approximate by numerical simulation
-                  if(is.na(newrow$stat)) newrow$stat = 0
-                  x = rnorm(10000,newrow$mean,newrow$stat)/rnorm(10000,data2$mean[leaves],data2$stat[leaves])
-                  newrow$mean = mean(x)
-                  newrow$stat = sd(x)
-                }
-                newrow$n    = min(newrow$n,data2$n[leaves])
-                if(is.null(data3)){ data3 = newrow} else {data3 = rbind(data3,newrow)}
-                
-              }
-            }
+      # pair = list(); counter = 0;
+      unique.groups <- split(data2,
+          list(cite=data2$citation_id, 
+               site=data2$site_id, 
+               spp=data2$specie_id))
+      unique.groups <- unique.groups[lapply(unique.groups, nrow) > 0]
+      print(unique.groups)
+      
+      for(unique.group in unique.groups){
+        
+        roots <- unique.group[unique.group$vname %in% c("fine_root_biomass"),]#t/ha
+        leaves<- unique.group[unique.group$vname=="leaf_biomass",]#t/ha
+        
+        if(nrow(roots) == 1 & nrow(leaves) == 1){
+          newrow <- roots
+          newrow$vname = 'fineroot2leaf'
+          if(is.na(data2$stat[leaves])){
+            newrow$mean = newrow$mean/data2$mean[leaves]
+            newrow$stat = newrow$stat/data2$mean[leaves]
+          } else {
+            ## approximate by numerical simulation
+            if(is.na(newrow$stat)) newrow$stat = 0
+            x = rnorm(10000,newrow$mean,newrow$stat)/rnorm(10000,data2$mean[leaves],data2$stat[leaves])
+            newrow$mean = mean(x)
+            newrow$stat = sd(x)
           }
-        }        
+          newrow$n    = min(newrow$n,data2$n[leaves])
+          if(is.null(data3)){ data3 = newrow} else {data3 = rbind(data3,newrow)}
+        }
       }
+
       if(!is.null(data3)) data3 <- drop.columns(data3, "specie_id")
     }
     result <- rbind(data,data3)
