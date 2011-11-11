00b6a25b102477aded241193675df78761280b92
David LeBauer <dlebauer@illinois.edu>
2011-11-11 09:56:22 -0600

    (minor) added extra bracket for if(meta.analysis) else statement

--- rscripts/meta.analysis.R
+++ rscripts/meta.analysis.R
@@ -30,31 +30,31 @@ if('meta.analysis' %in% names(settings)) {
   pft.names <- unlist(xpathApply(settings.xml, '//pfts//pft//name', xmlValue))
   
   if(length(settings$pfts) < 1) stop('no PFT specified')
-  
-  ## loop over pfts
-  for(pft in settings$pfts){
-    load(paste(pft$outdir, 'trait.data.Rdata', sep=''))
-    load(paste(pft$outdir, 'prior.distns.Rdata', sep=''))
-    trait.average <- sapply(trait.data,function(x){mean(x$Y,na.rm=TRUE)})
-    
-    ## Set gamma distribution prior
-    prior.variances = as.data.frame(rep(1,nrow(prior.distns)))
-    row.names(prior.variances) = row.names(prior.distns)
-    prior.variances[names(trait.average),] = 0.001*trait.average^2 
-    prior.variances["seedling_mortality",1] = 1.0
-    taupriors <- list(tauA = 0.01,
-                      tauB = apply(prior.variances, 1, function(x) min(0.01, x)))
-    
-    ## run the meta-analysis
-    trait.mcmc  <- pecan.ma(trait.data, prior.distns, taupriors, j.iter = ma.iter, settings, pft$outdir)
-    post.distns <- approx.posterior(trait.mcmc,prior.distns,trait.data,pft$outdir)
+    ## loop over pfts
+    for(pft in settings$pfts){
+      load(paste(pft$outdir, 'trait.data.Rdata', sep=''))
+      load(paste(pft$outdir, 'prior.distns.Rdata', sep=''))
+      trait.average <- sapply(trait.data,function(x){mean(x$Y,na.rm=TRUE)})
+      
+      ## Set gamma distribution prior
+      prior.variances = as.data.frame(rep(1,nrow(prior.distns)))
+      row.names(prior.variances) = row.names(prior.distns)
+      prior.variances[names(trait.average),] = 0.001*trait.average^2 
+      prior.variances["seedling_mortality",1] = 1.0
+      taupriors <- list(tauA = 0.01,
+                        tauB = apply(prior.variances, 1, function(x) min(0.01, x)))
+      
+      ## run the meta-analysis
+      trait.mcmc  <- pecan.ma(trait.data, prior.distns, taupriors, j.iter = ma.iter, settings, pft$outdir)
+      post.distns <- approx.posterior(trait.mcmc,prior.distns,trait.data,pft$outdir)
 
-    pecan.ma.summary(trait.mcmc, pft$name, pft$outdir)
+      pecan.ma.summary(trait.mcmc, pft$name, pft$outdir)
 
-    save(trait.mcmc, file = paste(pft$outdir, 'trait.mcmc.Rdata', sep=''))
-    save(post.distns, file = paste(pft$outdir, 'post.distns.Rdata', sep = ''))  
+      save(trait.mcmc, file = paste(pft$outdir, 'trait.mcmc.Rdata', sep=''))
+      save(post.distns, file = paste(pft$outdir, 'post.distns.Rdata', sep = ''))  
 
-  } ## end loop over pfts
-} else {
-  print('settings file does not call for meta-analysis')
+    } ## end loop over pfts
+  } else {
+    print('settings file does not call meta-analysis')
+  }
 }
