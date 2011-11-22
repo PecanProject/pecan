3b80f8bf520fb500931cef999ed07122c25fc641
Michael Dietze <mdietze@illinois.edu>
2011-11-22 13:10:43 -0600

    Added a new feature for estimating allometric coefficients from information in the 'raws' table.
    right now bash/allom.sh just calls rscripts/AllomAve.R
    
    AllomAve loops over PFTS and stem vs leaf, queries the allometric equations, and fits a power law function to imputed data.
    
    query.allom.data does the actual querying of the database
    allom.BayesFit is the Gibbs sampler for the model (fit in R not JAGS because of the need for the data imputation)
    
    Output is a mcmc.list object for the model and a pdf of the mcmc diagnosics
    
    The code is not yet fully functional, a number of features need to be added to handle raw field data on allometries, allometries beyond leaf and stem (height, canopy radius, etc.), the ability to update trait posteriors with the parameter estimates, and the ability to query the previous posterior as the prior. These will be added as redmine feature requests shortly

---
+++ rscripts/AllomAve.R
@@ -0,0 +1,74 @@
+## overall script for allom
+
+## load pecan settings
+if(interactive()){
+ settings.file = "/home/mdietze/pecan/tests/settings.bartlett.xml"
+} else {
+  settings.file <- Sys.getenv("PECANSETTINGS")
+}
+library(XML)
+settings.xml <- xmlParse(settings.file)
+settings <- xmlToList(settings.xml)
+if(!is.null(settings$Rlib)){ .libPaths(settings$Rlib)} 
+require(PECAn)
+
+## libraries & database connection
+library(mvtnorm)
+library(MCMCpack)
+haveMPI <- require(Rmpi)
+library(RMySQL)
+dvr <- dbDriver("MySQL")
+con <- query.bety.con(dbname   = settings$database$name,
+                                    password = settings$database$passwd,
+                                    username = settings$database$userid,
+                                    host     = settings$database$host)
+
+## mcmc settings
+ngibbs = nu(settings$meta.analysis$iter)
+nchain = 3
+sel    =  floor(seq(ngibbs*0.25,ngibbs,length=min(ngibbs*0.75,5000)))
+
+########## BAYESIAN VERSION ###############
+for(pft in settings$pfts){  ## loop over PFTs
+
+  for(component in c(6,18)){ ## 6=stem, 18 = leaf 
+
+    print(c(pft,component))
+    
+    ## load data
+    allom <- query.allom.data(pft$name,component,con)
+    mc <- list()
+    for(i in 1:nchain){
+      mc[[i]] <- as.mcmc(allom.BayesFit(allom,ngibbs)[sel,])
+    }
+    mc <- as.mcmc.list(mc)
+
+    ## Model Selection
+    D = as.array(mc)[,'D',]
+    pD = mean(D) - min(D)
+    DIC = mean(D) + pD
+    
+    Dg = as.array(mc)[,'Dg',]
+    pDg = mean(Dg) - min(Dg)
+    DICg = mean(Dg) + pDg
+    
+    ## Save MCMC objects (Pass to MCMC diagnostics module)
+    save(mc,DIC,DICg,pD,pDg,
+         file=paste(pft$outdir,paste("Allom",pft$name,component,"RData",sep="."),sep=""))
+        
+    
+    ## Save Posterior information (Pass to update.posterior module)
+    if(FALSE){
+
+    }
+    
+    ## Analysis/visualization
+    pdf(paste(pft$outdir,paste("Allom",pft$name,component,"MCMC","pdf",sep="."),sep=""))
+    plot(mc)
+    summary(mc)
+    print(c("DIC",DIC,"pD",pD))
+    print(c("DICg",DICg,"pDg",pDg))    
+    dev.off()
+    
+  } ## end component loop
+} ## end PFT loop
