511d0a6f654203bab63891430a6d18695f027667
David LeBauer <dlebauer@illinois.edu>
2011-12-07 14:41:59 -0600

    added prior.fn to utils.R

--- R/utils.R
+++ R/utils.R
@@ -355,3 +355,66 @@ read.sa.output <- function(traits, quantiles, outdir, pft.name='',
 }
 
 isFALSE <- function(x) !isTRUE(x)
+
+##' Prior fitting function for optimization
+##'
+##' This function is used within \cite{\link{DEoptim}} to parameterize a distribution to the central tendency and confidence interval of a parameter. This function is not very robust; currently it needs to be tweaked when distributions require starting values (e.g. beta, f, 
+##' @title prior.fn 
+##' @param parms target for optimization
+##' @param x vector with c(lcl, ucl, ct) lcl / ucl = confidence limits, ct = entral tendency 
+##' @param alpha quantile at which lcl/ucl are estimated (e.g. for a 95% CI, alpha = 0.5)
+##' @param distn named distribution, one of 'lnorm', 'gamma', 'beta'; support for other distributions not currently implemented 
+##' @param central.tendency one of 'mode', 'median', and 'mean' 
+##' @param trait name of trait, can be used for exceptions (currently used for trait == 'q')
+##' @return parms
+##' @author David LeBauer
+##' @examples
+##' DEoptim(fn = prior.fn, 
+##'                 lower = c(0, 0), 
+##'                 upper = c(1000, 1000), 
+##'                 x=c(2, 6, 3.3), 
+##'                 alpha = 0.05, 
+##'                 distn = 'lnorm')$optim$bestmem
+
+prior.fn <- function(parms, x, alpha, distn, central.tendency = NULL, trait = NULL) {
+  if(distn == 'lnorm') {
+    mu <- parms[1]
+    sigma <- parms[2]         
+    lcl <- mu + qnorm(alpha/2)*sigma
+    ucl <- mu + qnorm(1-alpha/2)*sigma
+    ct <-  mu - sigma^2 
+    x <- log(x)
+  }
+  if(distn == 'gamma'){
+    lcl <- qgamma(alpha/2,   parms[1], parms[2])
+    ucl <- qgamma(1-alpha/2, parms[1], parms[2])
+    if(central.tendency == 'median'){
+      ct <- qgamma(0.5, parms[1], parms[2])
+    } else if (central.tendency == 'mean') {
+      ct <- parms[1]/parms[2]
+    } else if (central.tendency == 'mode') {
+      ct <- ifelse (parms[1]>1, (parms[1]-1)/parms[2], 0)
+    }
+  }
+  if (distn == 'beta') {
+    a <- parms[1]
+    if(central.tendency == 'mean' & trait == 'q'){ ## fixed mean, optimize for a
+      b <- a * (1/x[3] - 1)
+    } else {
+      b <- parms[2]
+    }
+    lcl <- qbeta(alpha/2,   a, b)  
+    ucl <- qbeta(1-alpha/2, a, b) 
+    if(is.null(central.tendency)) {
+      ct <- x[3]
+    } else if (central.tendency == 'mean'){
+      ct <- a/(a+b)
+    } else if (central.tendency == 'median'){
+      ct <- qbeta(0.5, a, b)  ## median
+    } else if (central.tendency == 'mode') {
+      ct <- ifelse(a>1 & b>1,(a-1)/(a+b-2) , 0) ## mode
+    } 
+  } 
+  return(sum(abs(c(lcl, ucl, ct) - x)))
+}
+
