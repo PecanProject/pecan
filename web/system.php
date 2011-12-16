36610582e9f5650207f6fa1d1cc5bbd658bb32df
Rob Kooper <kooper@illinois.edu>
2011-12-15 21:29:44 -0600

    move all system specifics to system.php

--- web/system.php
+++ web/system.php
@@ -1,5 +1,15 @@
 <?
 $pecan_home="/home/kooper/projects/EBI/pecan/";
+#$output_folder=sys_get_temp_dir();
+$output_folder="/home/kooper/tmp/pecan";
+
+# should come from database
+$site_files=array(610 => array( "met"  => array("/home/kooper/projects/EBI/ebifarm/met/ED_MET_DRIVER_HEADER" => "ebifarm"),
+                                "site" => array("/home/kooper/projects/EBI/ebifarm/ebifarm." => "ebifarm")
+                              ),
+                 );
+
+# should come from database
 $ed_binary="/usr/local/bin/ed2";
 $ed_veg="/home/kooper/projects/EBI/oge2OLD/OGE2_";
 $ed_soil="/home/kooper/projects/EBI/faoOLD/FAO_";
