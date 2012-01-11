15183f20f6c6fd367974119fff047b953ca80660
David LeBauer <dlebauer@illinois.edu>
2012-01-10 18:50:13 -0600

    moved pecan.transformstats function to utils, renamed transformstats

--- R/utils.R
+++ R/utils.R
@@ -308,7 +308,7 @@ capitalize <- function(x) {
 fit.dist <- function(trait.data, trait = colnames(trait.data), dists = c('weibull', 'lognormal', 'gamma'), n = NULL) {
   if(class(trait.data) == 'data.frame') trait.data <- trait.data[,1]
   ## warning(immediate. = TRUE)
-  nostart.dists <- dists[dists %in% c('weibull', 'lognormal', 'gamma', 'norm')]
+  nostart.dists <- dists[dists %in% c('weibull', 'lognormal', 'gamma', 'normal')]
   a <- lapply(nostart.dists, function(x) suppressWarnings(fitdistr(trait.data,x)))
   names(a) <- nostart.dists
   if('f' %in% dists){
@@ -332,7 +332,8 @@ fit.dist <- function(trait.data, trait = colnames(trait.data), dists = c('weibul
   print(result)
   bestfitdist <- names(which.min(aicvalues))
   parms <- tabnum(a[[bestfitdist]]$estimate)
-  return(data.frame(distribution = bestfitdist, 
+  dist <- ifelse(bestfitdist == 'normal', 'norm', bestfitdist)
+  return(data.frame(distribution = dist, 
                     a = as.numeric(parms[1]), 
                     b = as.numeric(parms[2]), 
                     n = ifelse(is.null(n), length(trait.data), n)))
@@ -510,3 +511,69 @@ bibtexify <- function (author, year, title) {
   acronym <- abbreviate(title, minlength = 3, strict=TRUE)
   paste(author, year, acronym, sep='')
 }
+
+
+##' Transform misc. statistics to SE
+##'
+##' Automates transformations of SD, MSE, LSD, 95\%CI, HSD, and MSD to conservative estimates of SE.
+##' @title Transform Stats 
+##' @param data data frame with mean, statistic, n, and statistic name: \code{example data <- data.frame(Y=rep(1,5), stat=rep(1,5), n=rep(4,5), statname=c('SD', 'MSE', 'LSD', 'HSD', 'MSD'))}
+##' @return dataframe with statistics transformed to SE
+##' @author David LeBauer
+##' @export
+transformstats <- function(data) {
+  if(!"SE" %in% levels(data$statname)){
+    data$statname <- factor(data$statname, levels = c(levels(data$statname), "SE"))
+  }
+  ## Transformation of stats to SE
+  ## transform SD to SE
+  if (max(c("SD","sd") %in% data$statname)) {
+    sdi <- which(data$statname %in% c("SD","sd"))
+    data$stat[sdi] <- data$stat[sdi] / sqrt(data$n[sdi])
+    data$statname[sdi] <- "SE"
+  }
+  ## transform MSE to SE
+  if ("MSE" %in% data$statname) {
+    msei <- which(data$statname == "MSE")
+    data$stat[msei] <- sqrt (data$stat[msei]/data$n[msei])
+    data$statname[msei] <- "SE"
+  }
+  ## 95%CI measured from mean to upper or lower CI
+  ## SE = CI/t
+  if ("95%CI" %in% data$statname) {
+    cii <- which(data$statname == '95%CI')
+    data$stat[cii] <- data$stat[cii]/qt(0.975,data$n[cii])
+    data$statname[cii] <- "SE"
+  }
+  ## Fisher's Least Significant Difference (LSD)
+  ## conservatively assume no within block replication
+  if ("LSD" %in% data$statname) {
+    lsdi <- which(data$statname == "LSD")
+    data$stat[lsdi] <- data$stat[lsdi] / (qt(0.975,data$n[lsdi]) * sqrt( (2 * data$n[lsdi])))
+    data$statname[lsdi] <- "SE"
+  }
+  ## Tukey's Honestly Significant Difference (HSD),
+  ## conservatively assuming 3 groups being tested so df =2
+  if ("HSD" %in% data$statname) {
+    hsdi <- which(data$statname == "HSD")
+    n = data$n[hsdi]
+    n[is.na(n)] = 2 ## minimum n that can be used if NA
+    data$stat[hsdi] <- data$stat[hsdi] / (qtukey(0.975, n, df = 2))
+    data$statname[hsdi] <- "SE"
+    data$n[hsdi] <- n
+  }              
+  ## MSD Minimum Squared Difference
+  ## MSD = t_{\alpha/2, 2n-2}*SD*sqrt(2/n)
+  ## SE  = MSD*n/(t*sqrt(2))
+  if ("MSD" %in% data$statname) {
+    msdi <- which(data$statname == "MSD")
+    data$stat[msdi] <- data$stat[msdi] * data$n[msdi] / ( qt(0.975,2*data$n[msdi]-2)*sqrt(2))
+    data$statname[msdi] <- "SE"
+  }
+  if (FALSE %in% c('SE','none') %in% data$statname) {
+    print(paste(trait, ': ERROR!!! data contains untransformed statistics'))
+  }
+  return(data)
+}
+##' @example statdf <- data.frame(Y=rep(1,5), stat=rep(1,5), n=rep(4,5), statname=c('SD', 'MSE', 'LSD', 'HSD', 'MSD'))
+##' transformstats(statdf)
