b1c25b3a490cb1beb1032d01e73cdb467efc643c
David LeBauer <dlebauer@illinois.edu>
2012-02-17 12:28:27 -0600

    added notes

--- bash/pecanbuild.sh
+++ bash/pecanbuild.sh
@@ -4,6 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../..
 
 ## allow optional input
+## alternative would be to use default value syntax, remove ifelse and replace $pecandir with ${1:-pecan}
 if [ ! -z $1 ] 
 then 
     pecandir=$1
