eb9b083d91775c69133601fe999da21bd110f962
David LeBauer <dlebauer@illinois.edu>
2012-02-17 11:43:35 -0600

    fixes #650

--- bash/pecanbuild.sh
+++ bash/pecanbuild.sh
@@ -2,6 +2,15 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../..
-R CMD build pecan
+
+## allow optional input
+if [ ! -z $1 ] 
+then 
+    pecandir=$1
+else
+    pecandir=pecan
+fi
+
+R --vanilla CMD build pecandir
 PECAn=`ls -v PECAn*tar.gz | tail -n 1`
-R CMD INSTALL $PECAn
+R --vanilla CMD INSTALL $PECAn
