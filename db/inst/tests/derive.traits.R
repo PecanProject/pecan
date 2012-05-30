48adfd433c5173ccd34134d8b30e5060c03232bb
David LeBauer <dlebauer@illinois.edu>
2012-05-30 17:29:59 -0500

    added test for derive.trait function

--- db/inst/tests/derive.traits.R
+++ db/inst/tests/derive.traits.R
@@ -1,7 +1,17 @@
 test_that("take.samples works";{
-  expect_equal(take.samples(trait = data.frame(mean = 1, stat = NA)), 1)
+  expect_equal(take.samples(summary = data.frame(mean = 1, stat = NA)), 1)
   set.seed(0)
-  test.sample <- take.samples(trait = data.frame(mean = 1, stat = 1),
+  test.sample <- take.samples(summary = data.frame(mean = 1, stat = 1),
                                sample.size = 2)
   expect_equal(test.sample, c(2.26295428488079, 0.673766639294351))
+})
+
+test_that("derive.trait works";{
+  set.seed(0)
+  input <- list(x = data.frame(mean = 1, stat = 1, n = 1))
+  test.derived <- derive.trait(FUN = function(x){1/x},
+                              input = input,
+                              var.name = 'x')
+  expect_equal(test.derived,
+               structure(list(mean = 0.0944129994366609, stat = 687.395104414576, n = 1, vname = "x"), .Names = c("mean", "stat", "n", "vname"), row.names = c(NA, -1L), class = "data.frame"))
 })
