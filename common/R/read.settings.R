0668d7bcaf28f528ac01aa35cc5df77d11bae263
Shawn P. Serbin <sserbin@illinois.edu>
2012-05-31 11:18:08 -0500

    Updates to read.settings.R and get.trait.data.R in prep for running the db query

--- common/R/read.settings.R
+++ common/R/read.settings.R
@@ -1,41 +1,47 @@
-##' Generic function to open and parse PEcAn XML config/settings file 
+#--------------------------------------------------------------------------------------------------#
+##' 
+##' @name read.settings.R
+##' @title Generic function to open and parse PEcAn XML config/settings file 
 ##'
 ##' NEED MORE HERE. WORKING DRAFT OF SCRIPT TO OPEN PECAN SETTINGS
 ##'
 ##'
+##' @return list object with information from input PEcAn XML settings file 
 ##'
-##'
+##' @author Shawn Serbin
 #==================================================================================================#
 
 
 #---------------- Load requirements for function. -------------------------------------------------#
 # Info: Load required libraries for running this function inside the PEcAn workflow.
 ok = require(XML); if (! ok) stop("Package XML is not available...")      # R XML library
-#require(db)      # PEcAn db library
-#--------------------------------------------------------------------------------------------------#
-
-
-#---------------- Import command line arguments. --------------------------------------------------#
-args <- commandArgs(trailingOnly = TRUE)
-#print(args)       # echo arguments
 #--------------------------------------------------------------------------------------------------#
 
 
 #---------------- Define function. ----------------------------------------------------------------#
-# Info: This function is called by other modules to open and parse XML config file.
+# Info: Function to read in and parse settings file. Returns XML info as R list object.
+#       This function is called by other modules to open and parse XML config file.
 
-# Define settings file - debug hack. REMOVE?
-#pecan.settings.file = Sys.getenv("PECANSETTINGS")
-#pecan.settings.file = '/Users/serbin/DATA/pecan_in/US-WCr.settings.xml'
-pecan.settings.file = args[1]     # Define path to settings file
-
-#message(paste("---- Path to settings file: ",pecan.settings.file))
-
-# Function to read in and parse settings file. Returns XML info as R list object.
-read.settings = function(pecan.settings.file){
+read.settings <- function(pecan.settings.file){
   settings.xml <- xmlParse(pecan.settings.file)
   settings <- xmlToList(settings.xml)
   if(!is.null(settings$Rlib)){ .libPaths(settings$Rlib)}
+  
+  # Create main output directory if it doesn't already exist
+  if (! file.exists(settings$outdir)) dir.create(settings$outdir)
+  
+  # Move settings file to main output directory
+  if (! file.exists(file.path(pecan.settings.file,settings$outdir))){
+    file.copy(from = pecan.settings.file, to = settings$outdir)
+  }
+  
+  # Re-open settings.xml in output directory
+  out.path = list.files(path = settings$outdir, pattern = "*.xml",full.names=TRUE)
+  settings.xml <- xmlParse(out.path)
+  settings <- xmlToList(settings.xml)
+  if(!is.null(settings$Rlib)){ .libPaths(settings$Rlib)}
+  
+  # Return settings file as a list
   return(settings)
 }
 #--------------------------------------------------------------------------------------------------#
