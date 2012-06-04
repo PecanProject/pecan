36b021b38374ae16e423351a25b395e7e893ba68
Shawn P. Serbin <sserbin@illinois.edu>
2012-06-04 00:11:49 -0500

    Updates to run.write.configs.ed.R

--- models/ed/R/run.write.configs.ed.R
+++ models/ed/R/run.write.configs.ed.R
@@ -9,6 +9,16 @@
 #--------------------------------------------------------------------------------------------------#
 run.write.configs.ed <- function() {
   
+  ### Read in settings file if not in workspace
+  if(!exists("settings")){
+    settings = read.settings(pecan.settings.file)
+    print("-------------------------------------------------------------------")
+    print(paste("Using PEcAn settings file: ",pecan.settings.file, sep = ""))
+    print("-------------------------------------------------------------------")
+    print(" ")
+    print(" ")
+  }
+  
   ### Identify PFTs in the input settings.xml file
   num.pfts <- length(settings$pfts)
   pft.names=as.list(rep(NA,num.pfts))
@@ -32,6 +42,9 @@ run.write.configs.ed <- function() {
   print("-------------------------------------------------------------------")
   print(" ")
   
+  ### Prepare for model output.  Cleanup any old config files (if exists)
+  remove.config()
+  
   ### Generate empty list arrays for output.
   trait.samples <- list()
   sa.samples <- list()
@@ -39,30 +52,114 @@ run.write.configs.ed <- function() {
   env.samples <- list()
   ###
   
-  ### Prepare for model output.  Cleanup any old config files (if exists)
-  remove.config()
+  ### Define main output directory and host for SA/Ensemble run.
+  main.outdir   <- settings$outdir
+  host <- settings$run$host
+  
+  ### Load PFT priors and posteriors
+  for (i in seq(pft.names)){
+    print(i)
+    
+    ### Load priors
+    load(paste(outdirs[i], 'prior.distns.Rdata', sep=''))
+    
+    ### Load trait mcmc data (if exists)
+    if("trait.mcmc.Rdata" %in% dir(unlist(outdirs))) {
+      load(paste(outdirs[i], 'trait.mcmc.Rdata', sep=''))
+    }
+    
+    pft.name <- unlist(pft.names[i])
+    
+    ### When no ma for a trait, sample from  prior
+    ### Trim all chains to shortest mcmc chain, else 20000 samples
+    if(exists('trait.mcmc')) {
+      traits <- names(trait.mcmc)
+      samples.num <- min(sapply(trait.mcmc, function(x) nrow(as.matrix(x))))
+    } else {
+      traits <- NA
+      samples.num <- 20000
+    }
+    
+    priors <- rownames(prior.distns)
+    for (prior in priors) {
+      if (prior %in% traits) {
+        samples <- as.matrix(trait.mcmc[[prior]][,'beta.o'])
+      } else {
+        samples <- get.sample(prior.distns[prior,], samples.num)
+      }
+      trait.samples[[pft.name]][[prior]] <- samples
+    }
+  } ### End for loop
+  
+  
+  ### NEED TO IMPLEMENT: 
+  ## Load Environmental Priors and Posteriors
+  ###
+  
   
   ### Sensitivity Analysis
   if('sensitivity.analysis' %in% names(settings)) {
     
-    ### Get info on the quantiles to be run in the sensitivity analysis (if requested)
-    quant <- get.quantiles(settings$sensitivity.analysis$quantiles)
-    ### Get info on the years to run the sensitivity analysis (if requested)
-    sa.years = data.frame(sa.start = settings$sensitivity.analysis$start.year, 
-                          sa.end = settings$sensitivity.analysis$end.year)
-    
-    print(" ")
-    print(" ")
-    print("-------------------------------------------------------------------")
-    print("Selected Quantiles: ")
-    #print("Lower             Mid             Upper")
-    print(round(quant,3))
-    print("-------------------------------------------------------------------")
-    print(" ")
-    print(" ")
-    
-    
+    if( is.null(settings$sensitivity.analysis)) {
+      print(paste('sensitivity analysis settings are NULL'))
+    } else {
+      
+      ### Get info on the quantiles to be run in the sensitivity analysis (if requested)
+      quant <- get.quantiles(settings$sensitivity.analysis$quantiles)
+      ### Get info on the years to run the sensitivity analysis (if requested)
+      sa.years = data.frame(sa.start = settings$sensitivity.analysis$start.year, 
+                            sa.end = settings$sensitivity.analysis$end.year)
+      
+      print(" ")
+      print(" ")
+      print("-------------------------------------------------------------------")
+      print("Selected Quantiles: ")
+      #print("Lower             Mid             Upper")
+      print(round(quant,3))
+      print("-------------------------------------------------------------------")
+      print(" ")
+      print(" ")
+      
+      ### Generate list of sample quantiles for SA run
+      sa.samples <-  get.sa.sample.list(trait.samples, 
+                                        env.samples,
+                                        quant)
+      
+      ### Write out SA config files
+      write.sa.configs(settings$pfts, sa.samples, 
+                       host, outdir, settings)
+    }
   } ### End of SA
+  
+  ### Write ENSEMBLE
+  if('ensemble' %in% names(settings) && settings$ensemble$size > 0) {
+    ## subset the trait.samples to ensemble size using Halton sequence 
+    ensemble.samples <- get.ensemble.samples(settings$ensemble$size, 
+                                             trait.samples, env.samples)
+    write.ensemble.configs(settings$pfts, ensemble.samples, 
+                           host, outdir, settings)
+  } ### End of Ensemble
+  
+  ######################## Finish up runs ########################
+  ### Save output from SA/Ensemble runs
+  save(ensemble.samples, trait.samples, sa.samples, settings,
+       file = paste(outdir, 'samples.Rdata', sep=''))
+  
+  ### Make outdirectory, send samples to outdir
+  if(host$name == 'localhost'){
+    if(!host$outdir == outdir) {
+      dir.create(host$outdir)
+      file.copy(from = paste(outdir, 'samples.Rdata', sep=''),
+                to   = paste(host$outdir, 'samples.Rdata', sep = ''),
+                overwrite = TRUE)
+    }
+  } else {  
+    mkdir.cmd <- paste("'if ! ls ", host$outdir, " > /dev/null ; then mkdir -p ", 
+                       host$outdir," ; fi'",sep='')
+    system(paste("ssh", host$name, mkdir.cmd))
+    system(paste('rsync -routi ', paste(outdir, 'samples.Rdata', sep=''),
+                 paste(host$name, ':', host$outdir, sep='')))
+  }
   
 } ### End of function: run.write.configs.ed.R
 #==================================================================================================#
