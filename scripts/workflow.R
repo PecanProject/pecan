1f4a83292b1891cbb95503e80364f7e58af9576b
David LeBauer <dlebauer@illinois.edu>
2012-06-13 11:06:03 -0500

    replaced individual package loading with PEcAn.all

--- scripts/workflow.R
+++ scripts/workflow.R
@@ -1,15 +1,5 @@
 #---------------- Load libraries. -----------------------------------------------------------------#
-pecan.pkgs <- lapply(c("DB", "common", "utils", "MA", "uncertainty"),
-                     function(x) paste("PEcAn.", x, sep = ''))
-try.load <- function(pkg = NULL){
-  ifelse(!require(pkg, character.only = TRUE), FALSE, TRUE)
-}
-loaded <- sapply(pecan.pkgs, try.load)  #loads available packages,
-                                        # returns logical vector of success
-if(any(!loaded)) {
-  print(paste("Could not load", pecan.pkgs[!loaded]))
-  stop()
-}
+require(PEcAn.all)
 #--------------------------------------------------------------------------------------------------#
 
 
