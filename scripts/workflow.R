19d9b942438453b54f0d89376383f98218dd60e9
David LeBauer <dlebauer@illinois.edu>
2012-06-13 12:48:42 -0500

    updated workflow.R for Rob's new read.settings function refs #980, #985

--- scripts/workflow.R
+++ scripts/workflow.R
@@ -4,21 +4,8 @@ require(PEcAn.all)
 
 
 #---------------- Load PEcAn settings file. -------------------------------------------------------#
-default.settings <- Sys.getenv(x = c("SETTINGS","USER","HOME"))   # Import default location
-args <- commandArgs(trailingOnly = TRUE)                          # Import command argument
-
-if (is.na(args[1])==TRUE){
-  pecan.settings.file <- default.settings[1]
-  print(paste("---- PEcAn settings file: ",pecan.settings.file, sep = ""))
-  print(" ")
-} else {
-  pecan.settings.file <- args[1]
-  print(paste("---- PEcAn settings file: ",pecan.settings.file, sep = ""))
-  print(" ")
-}
-
 # Open and read in settings file for PEcAn run.
-settings = read.settings(pecan.settings.file)
+settings <- read.settings()
 #--------------------------------------------------------------------------------------------------#
 
 
@@ -27,7 +14,7 @@ get.trait.data()        # Query the trait database for data and priors
 
 run.meta.analysis()     # Run the PEcAn meta.analysis
 
-#write.model.config()   # calls model specific write.configs e.g. write.config.ed.R
+# write.model.config()   # calls model specific write.configs e.g. write.config.ed.R
 
-#run.sa()
+# run.sensitivity.analysis()
 #--------------------------------------------------------------------------------------------------#
