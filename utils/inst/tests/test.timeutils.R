170ace0e7e3ea33d69d2197d26fdb1b748578bf4
David LeBauer <dlebauer@illinois.edu>
2012-06-13 18:51:51 -0500

    added tests to utils package, all tests pass, fixes #1063

--- utils/inst/tests/test.timeutils.R
+++ utils/inst/tests/test.timeutils.R
@@ -1,13 +1,13 @@
 test_that("time utils work",{
-  eng.months  <- c("jan","feb","mar","apr","may","jun",
-                  "jul","aug","sep","oct","nov","dec")
-  port.months <- c("jan","feb","mar","apr","may","jun",
-                 "jul","aug","sep","oct","nov","dec")
+  eng.months  <- c("jan", "feb", "mar", "apr", "may", "jun",
+                  "jul", "aug", "sep", "oct", "nov", "dec")
+  port.months <- c("jan", "fev", "mar", "abr", "mai", "jun",
+                   "jul", "ago", "set", "out", "nov", "dez")
   expect_equal(mon2mmm(1), 'jan')
   expect_equal(mmm2mon('jan'), 1)
   expect_equal(mon2mmm(1:12), eng.months)
-  expect_equal(mon2mmm(1:12, "Portugese"), port.months)
-  expect_equal(mmm2mon(port.months, "Portugese"), 1:12)
+  expect_equal(mon2mmm(1:12, "Portuguese"), port.months)
+  expect_equal(mmm2mon(port.months, "Portuguese"), 1:12)
   expect_equal(mmm2mon(eng.months), 1:12)
 })
   
