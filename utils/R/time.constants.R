321e75fdd9c7935ac0e451d9b81922d947d017e2
David LeBauer <dlebauer@illinois.edu>
2012-06-13 21:02:40 -0500

    fixed documentation; exported lots of functions so that check('utils') would work; moved objects from time.constants.Rdata and time.constants.R

---
+++ utils/R/time.constants.R
@@ -0,0 +1,14 @@
+#==========================================================================================#
+#==========================================================================================#
+# Time conversion units                                                                    #
+#------------------------------------------------------------------------------------------#
+yr.day  <<- 365.2425         # # of days in a year                              [   day/yr]
+day.sec <<- 86400.           # # of seconds in a day                            [    s/day]
+day.min <<- 1440.            # # of minutes in a day                            [  min/day]
+day.hr  <<- 24.              # # of hours in a day                              [   hr/day]
+hr.sec  <<- 3600.            # # of seconds in an hour                          [     s/hr]
+hr.min  <<- 60.              # # of minutes in an hour                          [   min/hr]
+min.sec <<- 60.              # # of seconds in a minute                         [    s/min]
+yr.sec  <<- yr.day * day.sec # # of seconds in a year                           [     s/yr]
+#==========================================================================================#
+
