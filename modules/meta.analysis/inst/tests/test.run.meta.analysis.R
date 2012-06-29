f55d1fe762d027ef7f30bc9b03a749028c178bd6
David LeBauer <dlebauer@illinois.edu>
2012-06-29 13:07:06 -0500

    added test for check.prior.v.mapost refs #88

---
+++ modules/meta.analysis/inst/tests/test.run.meta.analysis.R
@@ -0,0 +1,8 @@
+test_that("check.prior.v.mapost function is functional",{
+  set.seed(0)
+  prior <- list(distn = "norm", parama = 0, paramb = 1)
+  expect_error(check.prior.v.mapost(prior,
+                                    ma.post = rnorm(1000, 3, 1)))
+  expect_message(check.prior.v.mapost(prior,0),
+                 "prior and posterior are consistent") 
+})c
