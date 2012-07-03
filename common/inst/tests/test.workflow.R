d2c9b657742b1a98d4c5e13efc6982a8d237a370
David LeBauer <dlebauer@illinois.edu>
2012-07-03 13:41:51 -0500

    first draft of integration test refs #1125

---
+++ common/inst/tests/test.workflow.R
@@ -0,0 +1,7 @@
+settings <- read.settings("test.settings.xml")
+#get.trait.data()
+#run.meta.analysis()     
+#run.write.configs("ED2")
+#clear.scratch(settings)
+#start.model.runs("ED2")
+#get.model.output("ED2")
