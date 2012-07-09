5b460c485429e62dc6aaf727552da77a12c612c1
David LeBauer <dlebauer@illinois.edu>
2012-07-09 17:25:16 -0500

    added tests for test.approx.posterior function

---
+++ utils/inst/tests/test.approx.posterior.R
@@ -0,0 +1,27 @@
+context("test approx.posterior function")
+
+data("trait.mcmc")
+data("prior.distns")
+
+test_that("test data are as expected", {
+  expect_equal(names(trait.mcmc),
+               c("SLA", "leaf_width", "Vcmax", "fineroot2leaf", "root_respiration_rate",
+                 "root_turnover_rate", "stomatal_slope"))
+  expect_is(trait.mcmc, "list")
+  expect_is(trait.mcmc[[1]], "mcmc.list")
+  expect_is(prior.distns, "data.frame")
+  expect_equal(dim(prior.distns), c(15, 4))
+  expect_is(prior.distns$distn, "character")
+  expect_is(prior.distns$parama, "numeric")
+  expect_is(prior.distns$paramb, "numeric")
+})
+
+test_that("approx.posterior function returns expected dataframe given test data", {
+  test.ap <- approx.posterior(trait.mcmc, priors = prior.distns)
+  expect_is(test.ap, "data.frame")
+  expect_equal(dim(test.ap), c(15, 4))
+  expect_equal(dim(test.ap), dim(prior.distns))
+  expect_equal(colnames(test.ap), colnames(prior.distns))
+  expect_equal(rownames(test.ap), rownames(prior.distns))
+  expect_equal(test.ap$n, prior.distns$n)
+})
