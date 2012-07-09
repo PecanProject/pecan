1a2b9649c8687f740c6dbcf023e353c838ebc585
David LeBauer <dlebauer@illinois.edu>
2012-07-09 17:48:43 -0500

    updated test.bety.depreciation

--- all/inst/tests/test.bety.depreciation.R
+++ all/inst/tests/test.bety.depreciation.R
@@ -2,6 +2,6 @@
 ## test for feature 896 
 test_that("all references to BETY have been removed from source code",{
   betygrep <- system("find ../../../ -type f | grep -v depreciated | grep -v bzr | grep -v Rhistory | grep -v test |  xargs grep -s bety", intern = TRUE)
-  warning("the following code still references bety\n")
+  warning("PEcAn still references bety\nsee list of occurances above")
   writeLines(betygrep)
 })
