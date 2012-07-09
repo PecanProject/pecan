0b23f34726c1a5e7e0831f5d42f4e232950974ec
David LeBauer <dlebauer@illinois.edu>
2012-07-09 17:35:57 -0500

    updated test for removal of bety depreciation warnings

--- all/inst/tests/test.bety.depreciation.R
+++ all/inst/tests/test.bety.depreciation.R
@@ -1,6 +1,7 @@
 
 ## test for feature 896 
 test_that("all references to BETY have been removed from source code",{
-  suppressWarnings(betygrep <- system("find ../../../ -type f | grep -v depreciated | grep -v bzr | xargs grep -s bety", intern = TRUE))
-#  expect_equal(length(betygrep), 1) # BETY only shows up in this line
+  betygrep <- system("find ../../../ -type f | grep -v depreciated | grep -v bzr | grep -v Rhistory | grep -v test |  xargs grep -s bety", intern = TRUE)
+  warning("the following code still references bety\n")
+  writeLines(betygrep)
 })
