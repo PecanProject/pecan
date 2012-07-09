4438d50c6851970c06a4d3d4e93cd929a19d1649
David LeBauer <dlebauer@illinois.edu>
2012-07-09 16:19:04 -0500

    added first two modules to tests of PEcAn.all refs #1132

--- all/inst/tests/test.workflow.R
+++ all/inst/tests/test.workflow.R
@@ -1,7 +1,10 @@
-settings <- read.settings("test.settings.xml")
-#get.trait.data()
-#run.meta.analysis()     
-#run.write.configs("ED2")
-#clear.scratch(settings)
-#start.model.runs("ED2")
-#get.model.output("ED2")
+context("tests of overall workflow")
+
+settings <<- read.settings("test.settings.xml")
+
+get.trait.data()
+run.meta.analysis()     
+# run.write.configs("ED2")
+# clear.scratch(settings)
+# start.model.runs("ED2")
+# get.model.output("ED2")
