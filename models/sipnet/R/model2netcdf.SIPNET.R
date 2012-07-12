94e73c91fa3192b98172cde7491f57f595c25888
Shawn P. Serbin <sserbin@illinois.edu>
2012-07-12 07:43:31 -0500

    Completion of model2netcdf.SIPNET.R

--- models/sipnet/R/model2netcdf.SIPNET.R
+++ models/sipnet/R/model2netcdf.SIPNET.R
@@ -31,32 +31,59 @@ model2netcdf.SIPNET <- function(outdir,run.id) {
     sub.sipnet.output <- subset(sipnet.output,year==y)
     sub.sipnet.output.dims <- dim(sub.sipnet.output)
     dayfrac = 1/out.day
+    step <- seq(0,0.99,1/out.day)
 
     ### Setup outputs for netCDF file in appropriate units
     output <- list()
     output[[1]] <- sub.sipnet.output$year                       # Year
-    output[[2]] <- (sub.sipnet.output$day)                      # Fractional day
+    output[[2]] <- sub.sipnet.output$day+step                   # Fractional day
     output[[3]] <- (sub.sipnet.output$gpp*0.001)/timestep.s     # GPP in kgC/m2/s
-    output[[4]] <- (sub.sipnet.output$npp*0.001)/timestep.s     # NPP in kgC/m2/s
+    #output[[4]] <- (sub.sipnet.output$npp*0.001)/timestep.s     # NPP in kgC/m2/s. Internal SIPNET calculation
+    output[[4]] <- (sub.sipnet.output$gpp*0.001)/timestep.s -
+      ((sub.sipnet.output$rAboveground*0.001)/timestep.s +
+      (sub.sipnet.output$rRoot*0.001)/timestep.s)               # NPP in kgC/m2/s. Post SIPNET calculation
     output[[5]] <- (sub.sipnet.output$rtot*0.001)/timestep.s    # Total Respiration in kgC/m2/s
     output[[6]] <- (sub.sipnet.output$rAboveground*0.001)/timestep.s +
       (sub.sipnet.output$rRoot*0.001)/timestep.s                # Autotrophic Respiration in kgC/m2/s
     output[[7]] <- (sub.sipnet.output$rSoil*0.001)/timestep.s   # Heterotropic Respiration in kgC/m2/s
     output[[8]] <- (sub.sipnet.output$nee*0.001)/timestep.s     # NEE in kgC/m2/s
-
+    #output[[9]] <- rep(-999,sipnet.output.dims[1])             # CarbPools
+    output[[9]] <- (sub.sipnet.output$plantWoodC*0.001)         # Above ground wood kgC/m2
+    output[[10]] <- (sub.sipnet.output$plantLeafC*0.001)        # Leaf C kgC/m2
+    output[[11]] <- (sub.sipnet.output$plantWoodC*0.001)+
+      (sub.sipnet.output$plantLeafC*0.001)+
+      (sub.sipnet.output$coarseRootC*0.001)+
+      (sub.sipnet.output$fineRootC*0.001)                       # Total living C kgC/m2
+    output[[12]] <- (sub.sipnet.output$soil*0.001)+
+      (sub.sipnet.output$litter*0.001)                          # Total soil C kgC/m2
+    output[[13]] <- (sub.sipnet.output$fluxestranspiration*0.001)/timestep.s  #Transpiration kgW/m2/s
+    output[[14]] <- (sub.sipnet.output$soilWater*10)            # Soil moisture kgW/m2
+    output[[15]] <- (sub.sipnet.output$soilWetnessFrac)         # Fractional soil wetness
+    output[[16]] <- (sub.sipnet.output$snow*10)                 # SWE
+      
     #******************** Declar netCDF variables ********************#
     t <- dim.def.ncdf("time",paste("seconds since ",y,"-","01","-","01 ","00:00:00 -6:00",sep=""),
                       (1:sub.sipnet.output.dims[1]*timestep.s)) #cumulative time
+    ### ***** Need to dynamically update the UTC offset here *****
     
     var <- list()
-    var[[1]]  <- var.def.ncdf("Year","YYYY",t,-999)           # year
-    var[[2]]  <- var.def.ncdf("JulianDay","",t,-999)          # Day
-    var[[3]]  <- var.def.ncdf("GPP","kgC/m2/s",t,-999)        # GPP in kgC/m2/s
-    var[[4]]  <- var.def.ncdf("NPP","kgC/m2/s",t,-999)        # NPP in kgC/m2/s
-    var[[5]]  <- var.def.ncdf("TotalResp","kgC/m2/s",t,-999)  # Total Respiration in kgC/m2/s
-    var[[6]]  <- var.def.ncdf("AutoResp","kgC/m2/s",t,-999)   # Autotrophic respiration in kgC/m2/s
-    var[[7]]  <- var.def.ncdf("HeteroResp","kgC/m2/s",t,-999) # Heterotrophic respiration in kgC/m2/s
-    var[[8]]  <- var.def.ncdf("NEE","kgC/m2/s",t,-999)  # NEE in kgC/m2/s
+    var[[1]]  <- var.def.ncdf("Year","YYYY",t,-999,"Year of model output")                # year
+    var[[2]]  <- var.def.ncdf("FracJulianDay","",t,-999,"Fractional Julian Date")         # Fractional Julian Day
+    var[[3]]  <- var.def.ncdf("GPP","kgC/m2/s",t,-999,"Gross Primary Productivity")       # GPP in kgC/m2/s
+    var[[4]]  <- var.def.ncdf("NPP","kgC/m2/s",t,-999,"Net Primary Productivity")         # NPP in kgC/m2/s
+    var[[5]]  <- var.def.ncdf("TotalResp","kgC/m2/s",t,-999,"Total Respiration")          # Total Respiration in kgC/m2/s
+    var[[6]]  <- var.def.ncdf("AutoResp","kgC/m2/s",t,-999,"Autotrophic Respiration")     # Autotrophic respiration in kgC/m2/s
+    var[[7]]  <- var.def.ncdf("HeteroResp","kgC/m2/s",t,-999,"Heterotrophic Respiration") # Heterotrophic respiration in kgC/m2/s
+    var[[8]]  <- var.def.ncdf("NEE","kgC/m2/s",t,-999,"Net Ecosystem Exchange")           # NEE in kgC/m2/s
+    #var[[9]]  <- var.def.ncdf("CarbPools","kgC/m2/s",t,-999,"Size of each carbon pool")   # Carbon Pools
+    var[[9]]  <- var.def.ncdf("AbvGrndWood","kgC/m2",t,-999,"Above ground woody biomass") # Above ground woody biomass KgC/m2
+    var[[10]]  <- var.def.ncdf("LeafC","kgC/m2",t,-999,"Leaf carbon")                     # Leaf carbon KgC/m2
+    var[[11]]  <- var.def.ncdf("TotLivBiom","kgC/m2",t,-999,"Total living biomass")       # Total living C kgC/m2
+    var[[12]]  <- var.def.ncdf("TotSoilCarb","kgC/m2",t,-999,"Total soil carbon")         # Total soil C kgC/m2
+    var[[13]]  <- var.def.ncdf("TVeg","kgC/m2/s",t,-999,"Transpiration")                  # Transpiration
+    var[[14]]  <- var.def.ncdf("SoilMoist","kg/m2",t,-999,"Average Soil Moisture")        # Soil Moisture
+    var[[15]]  <- var.def.ncdf("SoilMoistFrac","-",t,-999,"Average Fraction of Saturation") # Fractional soil wetness
+    var[[16]]  <- var.def.ncdf("SWE","kg/m2",t,-999,"Snow Water Equivalent")              # SWE
     
     #******************** Declar netCDF variables ********************#
     nc <- create.ncdf(paste(outdir,run.id,".",y,".nc",sep=""),var)
