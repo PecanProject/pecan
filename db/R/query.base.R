8678b7ab6853c900377f0b9a4066e242796ecf30
David LeBauer <dlebauer@illinois.edu>
2012-07-13 17:50:14 -0500

    fixed typo in source:db/inst/tests/test.query.base.R: changed ';' to ','. refs #769

--- db/R/query.base.R
+++ db/R/query.base.R
@@ -13,7 +13,7 @@
 ##' \dontrun{
 ##' query.base('select count(id) from traits;')
 ##' }
-query.base <- function(query,con=NULL,...){
+query.base <- function(query, con=NULL,...){
   iopened <- 0
   if(is.null(con)){
     con <- query.base.con(settings)
