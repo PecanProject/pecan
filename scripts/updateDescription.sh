7c0dd2d1f4e10499b96444059fe932b0cd93f702
Rob Kooper <kooper@illinois.edu>
2012-07-15 16:05:45 -0500

    document script to set version/date/license (fixes #982)

--- scripts/updateDescription.sh
+++ scripts/updateDescription.sh
@@ -1,40 +1,23 @@
 #!/bin/bash
 
+# version of pecan
 VERSION="1.2"
 DATE=`date +"%Y-%m-%d"`
 
-UPDATE_REQUIRE="yes"
-UPDATE_COLLATE="yes"
-
+# if no arguments passed in update all DESCRIPTION files
 if [ $# -gt 0 ]; then
   FILES="$*"
 else
   FILES=$( find . -name DESCRIPTION -print )
 fi
 
-echo $FILES
+echo "Version   : $VERSION"
+echo "Date      : $DATE"
 
 for d in $FILES; do
-  DIR=`dirname $d`
-
-  echo "Modifying $d"
-  echo "Version: $VERSION"
-  echo "Date: $DATE"
-  sed -i -e "s/^Version: .*$/Version: $VERSION/" \
-         -e "s/^Date: .*$/Date: $DATE/" \
-         -e "s/^License: .*/License: FreeBSD + file LICENSE/" $d
-
-  if [ "$UPDATE_REQUIRE" == "yes" ]; then
-    REQUIRE=$( echo `grep -h 'require\|library[\s]*(' $DIR/R/* | sed -e 's/.*require[\s]*(\([^)]*\)).*/\1 /' -e 's/.*library[\s]*(\([^)]*\)).*/\1 /' | sort -u` )
-    echo "Require: $REQUIRE"
-    sed -i -e "s/^Require: .*/Require: $REQUIRE/" $d
-  fi
-
-  if [ "$UPDATE_COLLATE" == "yes" ]; then
-    COLLATE=$( echo `ls -1 $DIR/R` )
-    echo "Collate: $COLLATE"
-    sed -i -e "s/^Collate: .*/Collate: $COLLATE/" $d
-  fi
-
-  echo ""
+  # update DESCRIPTION file version/date/license
+  echo "Modifying : $d"
+  sed -i.bak -e "s/^Version: .*$/Version: $VERSION/" \
+             -e "s/^Date: .*$/Date: $DATE/" \
+             -e "s/^License: .*/License: FreeBSD + file LICENSE/" $d
 done
