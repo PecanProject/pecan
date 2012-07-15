a0a4e0af397ef8077d3074c3edcefbe19718fbe1
Rob Kooper <kooper@illinois.edu>
2012-07-15 17:01:00 -0500

    script to create and install list of dependencies (fixes #941)

---
+++ scripts/check.dependencies.sh
@@ -0,0 +1,35 @@
+for f in `find . -type d -name R`; do
+  NAME=$( echo $f | sed -e 's#\./##' -e 's#/R##' )
+  LIB=$( grep 'library(.*)' $f/*.R 2>/dev/null | grep -v 'PEcAn' | grep -v '^#' | sed -e 's/.*library(\("*[A-Za-z0-9\.]*"*\).*/\1/' | sort -u )
+  LIB=$( echo $LIB )
+
+  REQ=$( grep 'require(.*)' $f/*.R 2>/dev/null | grep -v 'PEcAn' | grep -v '^#' | sed -e 's/.*require(\("*[A-Za-z0-9\.]*"*\).*/\1/' | sort -u )
+  REQ=$( echo $REQ )
+
+  PACKAGE=$( echo $LIB $REQ | tr -s [:space:] \\n | sort -u )
+  PACKAGE=$( echo $PACKAGE)
+  if [ ! -z "$PACKAGE" ]; then
+    echo "packages needed for $NAME : $PACKAGE"
+  fi
+
+  ALL="${ALL} ${PACKAGE}"
+done
+
+if [ ! -z "$ALL" ]; then
+  ALL=$( echo $ALL | tr -s [:space:] \\n | sort -u )
+  ALL=$( echo $ALL )
+  ALL=$( echo "'$ALL'" | sed -e "s/ /', '/g" )
+  echo "Make sure following packages are installed : "
+  echo "$ALL"
+  echo ""
+
+  cat << EOF
+list.of.packages <- c($ALL)
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) {
+  print("installing : ")
+  print(new.packages)
+  install.packages(new.packages, repos="http://cran.us.r-project.org")
+}
+EOF
+fi
