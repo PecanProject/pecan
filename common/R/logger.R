80e9bf9e578a37d7bf73eb9c1f3f88c09a03b315
Rob Kooper <kooper@illinois.edu>
2012-07-15 15:42:20 -0500

    license file (#926)

--- common/R/logger.R
+++ common/R/logger.R
@@ -1,3 +1,11 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2012 University of Illinois, NCSA.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the 
+# University of Illinois/NCSA Open Source License
+# which accompanies this distribution, and is available at
+# http://opensource.ncsa.illinois.edu/license.html
+#-------------------------------------------------------------------------------
 log.variables <- list("filename"="", "console"=TRUE, "DEBUG"=TRUE, "INFO"=TRUE, "WARN"=TRUE, "ERROR"=TRUE)
 
 ##' Prints a debug message.
--- web/system.php
+++ web/system.php
@@ -1,4 +1,12 @@
-<?
+<?php
+/**
+ * Copyright (c) 2012 University of Illinois, NCSA.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the 
+ * University of Illinois/NCSA Open Source License
+ * which accompanies this distribution, and is available at
+ * http://opensource.ncsa.illinois.edu/license.html
+ */
 $pecan_home="/home/kooper/projects/EBI/pecan/";
 #$output_folder=sys_get_temp_dir();
 $output_folder="/home/kooper/tmp/pecan";
