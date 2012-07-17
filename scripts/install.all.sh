29d87cbedef8cb670bf70959ba5dbefbb815645f
Shawn P. Serbin <sserbin@illinois.edu>
2012-07-17 14:47:20 -0500

    minor updates

--- scripts/install.all.sh
+++ scripts/install.all.sh
@@ -1,4 +1,3 @@
-for f in common utils db modules/meta.analysis modules/uncertainty models/ed models/sipnet all
 #-------------------------------------------------------------------------------
 # Copyright (c) 2012 University of Illinois, NCSA.
 # All rights reserved. This program and the accompanying materials
@@ -7,6 +6,8 @@ for f in common utils db modules/meta.analysis modules/uncertainty models/ed mod
 # which accompanies this distribution, and is available at
 # http://opensource.ncsa.illinois.edu/license.html
 #-------------------------------------------------------------------------------
+
+for f in common utils db modules/meta.analysis modules/uncertainty models/ed models/sipnet all
 do
   R CMD build $f && R CMD INSTALL $f
 done
