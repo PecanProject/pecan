4421bec05b806abcf512e678bcb684272e7b23c1
David LeBauer <dlebauer@illinois.edu>
2012-07-19 15:34:45 -0500

    fixed some error, updated documentation

--- modules/uncertainty/R/run.sensitivity.analysis.R
+++ modules/uncertainty/R/run.sensitivity.analysis.R
@@ -72,7 +72,7 @@ run.sensitivity.analysis <- function(){
         ## arrange plots  http://stackoverflow.com/q/10706753/199217
         ncol <- floor(sqrt(length(sensitivity.plots)))
         print(do.call("grid.arrange", c(sensitivity.plots, ncol=ncol)))
-        lapply(sensitivity.plots, function(x) print(x + opts() # old method.  depreciated.
+        print(sensitivity.plots) # old method.  depreciated.
         dev.off()
 
         ### Generate VD diagnostic plots
