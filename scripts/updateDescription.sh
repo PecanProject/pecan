ec1b4acab40d291e866341cab366a5061e357cd6
Rob Kooper <kooper@illinois.edu>
2012-07-20 15:07:27 -0500

    bump version to 1.2.5

--- scripts/updateDescription.sh
+++ scripts/updateDescription.sh
@@ -9,7 +9,7 @@
 #-------------------------------------------------------------------------------
 
 # version of pecan
-VERSION="1.2"
+VERSION="1.2.5"
 DATE=`date +"%Y-%m-%d"`
 
 # if no arguments passed in update all DESCRIPTION files
