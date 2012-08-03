8b8e787d577d9070404a61afe8af7943218b912f
Michael Dietze <mdietze@illinois.edu>
2012-08-02 21:12:12 -0500

    bug fix on diametergrow

--- modules/data.land/R/Read_Tuscon.R
+++ modules/data.land/R/Read_Tuscon.R
@@ -4,6 +4,7 @@ Read_Tuscon <- function(folder){
   library(dplR)
   
   filenames <- dir(folder,pattern =  "TXT",full.names=TRUE)
+  filenames <- c(filenames,dir(folder,pattern =  "rwl",full.names=TRUE))
   filedata <- list()
   for (file in filenames){
     filedata[[file]] <- read.tucson(file, header = FALSE)
