7d79969741322744e50a995a28f6b33c40805608
Michael Dietze <mdietze@illinois.edu>
2012-08-08 09:51:30 -0500

    implementation of fuse_plot_treering

--- modules/data.land/R/read.plot.R
+++ modules/data.land/R/read.plot.R
@@ -2,9 +2,9 @@ read.plot <- function(file){
   dat <- read.csv(file)
   plot <- dat[,which(toupper(names(dat)) == "PLOT")]
   tree <- dat[,which(toupper(names(dat)) == "TREE")]
-  spp <- dat[,which(toupper(names(dat)) == "SPECIES")]
+  spp <- as.character(dat[,which(toupper(names(dat)) == "SPECIES")])
   dbh <- dat[,which(toupper(names(dat)) == "DBH")]
 
-  cbind(plot,tree,spp,dbh)
+  data.frame(plot=plot,tree=tree,spp=spp,dbh=dbh)
   
 }
