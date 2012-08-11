c3101b693fa0bd866381a2a77aa3ae8567888aca
Mike Dietze <dietze@bu.edu>
2012-08-10 17:04:59 -0700

    Refinement of sda.particle for ESA talk 2012

--- modules/assim.sequential/R/sda.particle.R
+++ modules/assim.sequential/R/sda.particle.R
@@ -8,25 +8,55 @@
 
 sda.particle <- function(model){
   sda.demo <- TRUE
+  unit.conv <-  0.001*2#  kgC/ha/yr to Mg/ha/yr
   
-  library(Hmisc)
+  wtd.quantile <- function(x,wt,q){
+    ord <- order(x)
+    wstar <- cumsum(wt[ord])/sum(wt)
+    qi <- findInterval(q,wstar)
+    return(x[ord[qi]])
+  }
+  
+#  library(Hmisc)
 
-  Year <- read.output("ENS00001",settings$outdir,variables="Year",model=model)
+  Year <- read.output("ENS00001",settings$outdir,variables="Year",model=model)$Year
   time <- as.numeric(names(table(Year)))
   
 ### Load Ensemble
-  ensp = read.ensemble.ts(model)[[1]]
-  ensp = t(apply(ensp,1,tapply,Year,mean))
+  ensp.all = read.ensemble.ts(model)
+  ensp = ensp.all[[1]]
+  ensp = t(apply(ensp,1,tapply,Year,mean))*unit.conv
   np = nrow(ensp)      ## number of particles
   nt = ncol(ensp)      ## number of time steps
   w = matrix(1,np,nt)  ## matrix of weights
-
+ 
 ### Load Data
   if(sda.demo){
     ## use one of the ensemble members as the true data
     sd <- apply(ensp,2,sd)*0.3  ## pseudo data uncertainty
     ref <- sample(1:np,1)       ## choose an ensemble member
     y <- rnorm(nt,ensp[ref,],sd) ## add noise
+  } else {
+    load(paste(outdir,"plot2AGB.Rdata",sep="/"))
+    mch = which(yrvec %in% time)
+    y = mNPP[1,mch]   ## data mean
+    sd = sNPP[1,mch]  ## data uncertainty 
+  }
+  
+  if(FALSE){
+    ## plot ensemble
+    plot(0,0,type='n',ylim=range(ensp),xlim=range(time),ylab="Mg/ha/yr",xlab="year")
+    for(i in 1:np){
+      lines(time,ensp[i,])
+    }
+    ## plot data
+    for(i in 1:nrow(mNPP)){
+      up = mNPP[i,]+sNPP[i,]*1.96
+      low = mNPP[i,]-sNPP[i,]*1.96
+      lines(yrvec[-1],mNPP[i,],ylim=range(c(up,low)),col=2 ) 
+      lines(yrvec[-1],up)
+      lines(yrvec[-1],low)
+    }
   }
   
 ###analysis: generate log-weights
@@ -47,7 +77,7 @@ XapCI = matrix(NA,2,nt)
 for(i in 1:nt){
   Xap[i] = weighted.mean(ensp[,i],wc[,i])
   XapCI[,i] = wtd.quantile(ensp[,i],wc[,i]/wbar[i],c(0.025,0.975))
-  Pap[i] = wtd.var(ensp[,i],wc[,i]/wbar[i])
+#  Pap[i] = wtd.var(ensp[,i],wc[,i]/wbar[i])
 }
 Pap[Pap < 0] <- 0
 Xbar = apply(ensp,2,mean)
@@ -55,8 +85,15 @@ Xci = apply(ensp,2,quantile,c(0.025,0.975))
 
 ### HERE
   
-#pdf("ParticleFilter.pdf")
-plot(time,y,ylim=range(Xci),type='b')
+pdf(paste(outfolder,"ParticleFilter.pdf",sep="/"))
+  par(mfrow=c(1,1))
+plot(time,y,ylim=range(c(y+1.96*sd,y-1.96*sd)),type='b',xlab="time",ylab="Mg/ha/yr")
+  lines(time,y+1.96*sd,col=2)
+  lines(time,y-1.96*sd,col=2)
+  
+  plot(time,y,ylim=range(Xci),type='b',xlab="time",ylab="Mg/ha/yr")
+  lines(time,y+1.96*sd,col=2)
+  lines(time,y-1.96*sd,col=2)
 if(sda.demo) lines(time,ensp[ref,],col=2,lwd=2)
 #for(i in 1:min(500,np)){
 # lines(t2,ensp[,i],lty=3,col="grey")
@@ -66,7 +103,7 @@ if(sda.demo) lines(time,ensp[ref,],col=2,lwd=2)
 lines(time,Xbar[1:nt],col=6)
 lines(time,Xci[1,1:nt],col=6,lty=2)
 lines(time,Xci[2,1:nt],col=6,lty=2)
-legend("topleft",c("True","Data","ens","ensmean"),col=c(1:2,"grey",6),lty=c(1,0,3,1),pch=c(1,19,1,1),cex=1.5)
+#legend("topleft",c("True","Data","ens","ensmean"),col=c(1:2,"grey",6),lty=c(1,0,3,1),pch=c(1,19,1,1),cex=1.5)
 
 lines(time,Xap,col=3,type='b',lwd=2)
 lines(time,XapCI[1,],col=3,lty=2,lwd=2)
@@ -76,5 +113,38 @@ lines(time,XapCI[2,],col=3,lty=2,lwd=2)
 legend("topleft",c("True","Data","PF","ens","ensmean"),col=c(1:3,"grey",6),lty=c(1,0,1,3,1),pch=c(1,19,1,1,0),cex=1.5)
   
 ## need to add figures for covariances and predictions for covariates
+  unit <- rep(unit.conv,5);unit[3] = 1
+  ensp.conv <- list()
+  for(i in 1:length(ensp.all)){
+    ensp.conv[[i]] <- t(apply(ensp.all[[i]],1,tapply,Year,mean))*unit[i]
+  }  
+  par(mfrow=c(2,2))
+  for(i in 2:5){
+    plot(ensp.conv[[1]][,nt],ensp.conv[[i]][,nt],xlab=names(ensp.all)[1],ylab=names(ensp.all)[i])
+  }
+  
+  ##unweighted distributions
+  for(i in c(1,2,4,5)){
+    hist(ensp.conv[[i]][,nt],main=names(ensp.all)[i],probability=TRUE)
+  }  
+  
+  ## Weighted distributions
+  library(plotrix)
+  for(i in c(1,2,4,5)){
+    weighted.hist(ensp.conv[[i]][,nt],wc[,nt]/sum(wc[,nt]),main=names(ensp.all)[i])
+  }
+  
+  for(i in c(1,2,4,5)){
+    if(i == 5){
+      weighted.hist(ensp.conv[[i]][,nt],wc[,nt]/sum(wc[,nt]),main=names(ensp.all)[i],col=2)
+    }else{
+      weighted.hist(ensp.conv[[i]][,nt],wc[,nt]/sum(wc[,nt]),main=names(ensp.all)[i],xlim=range(ensp.conv[[i]][,nt])*c(0.9,1.1),col=2)
+    }
+    hist(ensp.conv[[i]][,nt],main=names(ensp.all)[i],probability=TRUE,add=TRUE)
+  }
+  
+  dev.off()
+  
+  save.image(paste(settings$outdir,"sda.particle.Rdata"))
   
 }
