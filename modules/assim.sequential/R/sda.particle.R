e80b8508eb57fddb685c0a14a93534647f153599
Mike Dietze <dietze@bu.edu>
2012-08-13 23:33:33 -0500

    cleaning up sda.particle for public consumption

--- modules/assim.sequential/R/sda.particle.R
+++ modules/assim.sequential/R/sda.particle.R
@@ -7,18 +7,21 @@
 ### Prerequisite:assumes that you have run an ensemble run
 
 sda.particle <- function(model){
-  sda.demo <- TRUE
+  sda.demo <- FALSE  ## debugging flag
   unit.conv <-  0.001*2#  kgC/ha/yr to Mg/ha/yr
   
-  wtd.quantile <- function(x,wt,q){
+
+  ## internal functions
+  #  library(Hmisc)
+  wtd.quantile <- function(x,wt,q){  ## weighted quantile
     ord <- order(x)
     wstar <- cumsum(wt[ord])/sum(wt)
     qi <- findInterval(q,wstar)
     return(x[ord[qi]])
   }
   
-#  library(Hmisc)
 
+  ## extract time from one ensemble member
   Year <- read.output("ENS00001",settings$outdir,variables="Year",model=model)$Year
   time <- as.numeric(names(table(Year)))
   
@@ -43,6 +46,7 @@ sda.particle <- function(model){
     sd = sNPP[1,mch]  ## data uncertainty 
   }
   
+  ## diagnostic figures, not printed as part of normal operation
   if(FALSE){
     ## plot ensemble
     plot(0,0,type='n',ylim=range(ensp),xlim=range(time),ylab="Mg/ha/yr",xlab="year")
@@ -68,7 +72,7 @@ sda.particle <- function(model){
     }
   }
 
-###post-hoc processing of weights
+### calculate weighted mean and CI of the ensemble
 wc = t(exp(apply(w,1,cumsum)))
 wbar = apply(wc,2,mean)
 Xap = 0
@@ -83,11 +87,12 @@ Pap[Pap < 0] <- 0
 Xbar = apply(ensp,2,mean)
 Xci = apply(ensp,2,quantile,c(0.025,0.975))
 
-### HERE
-  
+### Diagnostic graphs  
 pdf(paste(outfolder,"ParticleFilter.pdf",sep="/"))
+  
+  ## plot ensemble, filter, and data mean's and CI's
   par(mfrow=c(1,1))
-plot(time,y,ylim=range(c(y+1.96*sd,y-1.96*sd)),type='b',xlab="time",ylab="Mg/ha/yr")
+  plot(time,y,ylim=range(c(y+1.96*sd,y-1.96*sd)),type='b',xlab="time",ylab="Mg/ha/yr")
   lines(time,y+1.96*sd,col=2)
   lines(time,y-1.96*sd,col=2)
   
@@ -112,12 +117,16 @@ lines(time,XapCI[2,],col=3,lty=2,lwd=2)
 #lines(t,Xap-1.96*sqrt(Pap),col=3,lty=2)
 legend("topleft",c("True","Data","PF","ens","ensmean"),col=c(1:3,"grey",6),lty=c(1,0,1,3,1),pch=c(1,19,1,1,0),cex=1.5)
   
-## need to add figures for covariances and predictions for covariates
+### Plots demonstrating how the constraint of your target variable 
+### impacts the other model pools and fluxes
+  
+  ## Calculate long-term means for all ensemble extracted variables
   unit <- rep(unit.conv,5);unit[3] = 1
   ensp.conv <- list()
   for(i in 1:length(ensp.all)){
     ensp.conv[[i]] <- t(apply(ensp.all[[i]],1,tapply,Year,mean))*unit[i]
   }  
+  ## plot scatter plots of outputs
   par(mfrow=c(2,2))
   for(i in 2:5){
     plot(ensp.conv[[1]][,nt],ensp.conv[[i]][,nt],xlab=names(ensp.all)[1],ylab=names(ensp.all)[i])
@@ -145,6 +154,7 @@ legend("topleft",c("True","Data","PF","ens","ensmean"),col=c(1:3,"grey",6),lty=c
   
   dev.off()
   
+  ## save all outputs
   save.image(paste(settings$outdir,"sda.particle.Rdata"))
   
 }
