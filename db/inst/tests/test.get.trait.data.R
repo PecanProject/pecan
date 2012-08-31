edac540b8887e1a6607989e0aca2f72ad2f412f9
David LeBauer <dlebauer@illinois.edu>
2012-08-31 15:42:32 -0500

    fixed typo in test.get.trait.data.R

--- db/inst/tests/test.get.trait.data.R
+++ db/inst/tests/test.get.trait.data.R
@@ -12,7 +12,7 @@ test_that("trait dictionary loads and has expected columns",{
   data(trait.dictionary)
   expect_true(exists("trait.dictionary"))
   expect_true(all(c("id", "figid", "units", "model.id") %in%
-                  colnames(ed.trait.dictionary)))
+                  colnames(trait.dictionary)))
   expect_true(ncol(trait.dictionary) >= 4) # dim = 49 x 4 at time of writing
   expect_true(nrow(trait.dictionary) >=49)
-}
+})
