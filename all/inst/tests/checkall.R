1aeb9e45bd0fea5304624a9d613da498522d1a6e
David LeBauer <dlebauer@illinois.edu>
2012-08-31 16:45:10 -0500

    fixes #1073 and tests that packages listed in check.all.sh match packages in all/data/pecan.packages.csv

--- all/inst/tests/checkall.R
+++
@@ -1,9 +0,0 @@
-data(pecan.packages, package = "PEcAn.all")
-pkgs <- pecan.packages[!pecan.packages == "all"]
-setwd("../../../")
-test_that("documentation is valid",{
-  for (pkg in pkgs){
-    devtools::check(pkg, document = TRUE)
-    test(pkg)
-  }
-})
