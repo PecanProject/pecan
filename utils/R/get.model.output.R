5876e4501b75a62ca11ab6efce12a1fe897fe14c
David LeBauer <dlebauer@illinois.edu>
2012-09-04 10:21:33 -0500

    removed depreciated / commented out code that only printed messages anyway

--- utils/R/get.model.output.R
+++ utils/R/get.model.output.R
@@ -39,37 +39,6 @@ get.model.output <- function(model){
   }
 
   return()
-  
-  ### ALL BELOW SEEMS DEPRECIATED ###
-  
-#   if (model=="ED2"){
-#     print(" ")
-#     print("-------------------------------------------------------------------")
-#     print(" Retrieving ED2 model output")
-#     print("-------------------------------------------------------------------")
-#     print(" ")
-#     Sys.sleep(1)
-#     
-#     ### Retrieve model output from local or remote server
-#     get.model.output.ed()
-#   
-#     ### Parse model output
-#     
-#   }else if (model=="SIPNET") {
-#     #write.configs.sipnet()
-#     print(" ")
-#     print("-------------------------------------------------------------------")
-#     print(" Generating SIPNET model run and configuration files")
-#     print("-------------------------------------------------------------------")
-#     print(" ")
-#     
-#     print("!!!! Not yet implemented !!!!")
-#     
-#   } ### End of if/else
-#   
-} ### End of function
-#==================================================================================================#
-
 
 ####################################################################################################
 ### EOF.  End of R script file.            	
