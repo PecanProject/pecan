ea44842189c0bfb1bfa229ee94a666b867d8b597
David LeBauer <dlebauer@illinois.edu>
2012-09-04 17:14:10 -0500

    refactored / corrected source:utils/R/get.results.R

--- models/c4photo/inst/workflow.R
+++ models/c4photo/inst/workflow.R
@@ -24,8 +24,6 @@ run.meta.analysis()     	# Run the PEcAn meta.analysis
 
 run.write.configs(model)        # Calls model specific write.configs e.g. write.config.ed.R
 
-clear.scratch(settings)         # Clear any old model output in ebi-cluster scratch/$USER on worker nodes
-
 start.model.runs(model, write.to.db = FALSE)         # Start ecosystem model runs
 
 get.model.output(model)         # Get results of model runs
