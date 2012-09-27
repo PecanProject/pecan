834f1563cfc37c95622857f843d6e945877c8e7c
David LeBauer <dlebauer@illinois.edu>
2012-09-27 14:45:10 -0500

    fixed unresolved write.configs.ed.R error

--- models/ed/R/write.configs.ed.R
+++ models/ed/R/write.configs.ed.R
@@ -215,7 +215,6 @@ write.config.ED2 <- function(defaults, trait.values, settings, outdir, run.id){
 
   if(is.null(settings$model$phenol.scheme)){
     print(paste("no phenology scheme set; \n",
-<<<<<<< TREE
                 "need to add <phenol.scheme> tag under <model> tag in settings file"))
   } else if(settings$model$phenol.scheme==1) {
     ## Set prescribed phenology switch in ED2IN
@@ -228,9 +227,6 @@ write.config.ED2 <- function(defaults, trait.values, settings, outdir, run.id){
     ed2in.text <- gsub('@PHENOL_END@', settings$model$phenol.end, ed2in.text)
     
     ## If not prescribed set alternative phenology scheme.
-=======
-                "need to add <phenol.scheme> tag under <model> tag in settings file"))
->>>>>>> MERGE-SOURCE
   } else {
     ed2in.text <- gsub(' @PHENOL_SCHEME@', settings$model$phenol.scheme, ed2in.text)
   }
