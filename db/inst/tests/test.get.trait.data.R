8dba80b8378263a556579c9df0ece81a86ecac43
David LeBauer <dlebauer@illinois.edu>
2012-10-16 15:21:43 -0500

    wrote test for issue #1348 that get.trait.data does not depend on meta-analysis tag; reinstated old tests for this function

---
+++ db/inst/tests/test.get.trait.data.R
@@ -0,0 +1,46 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2012 University of Illinois, NCSA.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the 
+# University of Illinois/NCSA Open Source License
+# which accompanies this distribution, and is available at
+# http://opensource.ncsa.illinois.edu/license.html
+#-------------------------------------------------------------------------------
+
+test_that("trait dictionary loads and has expected columns",{
+  rm(list = ls())
+  data(trait.dictionary)
+  expect_true(exists("trait.dictionary"))
+  expect_true(all(c("id", "figid", "units", "model.id") %in%
+                  colnames(ed.trait.dictionary)))
+  expect_true(ncol(trait.dictionary) >= 4) # dim = 49 x 4 at time of writing
+  expect_true(nrow(trait.dictionary) >=49)
+test.stats <- data.frame(Y=rep(1,5),
+                           stat=rep(1,5),
+                           n=rep(4,5),
+                           statname=c('SD', 'MSE', 'LSD', 'HSD', 'MSD'))
+})
+
+test_that("get.trait.data will return trait data even when there is no meta.analysis tag in the settings file", {
+  writeLines(" 
+<pecan>
+  <pfts>
+    <pft>
+      <name>ebifarm.pavi</name>
+      <outdir>/tmp/test/</outdir>
+    </pft>
+  </pfts>
+  <outdir>/tmp/test/</outdir>
+  <database>
+    <userid>ebi_analys_user</userid>
+    <passwd>b742xsAu</passwd>
+    <location>localhost</location>
+    <name>ebi_analysis</name>
+  </database>
+</pecan>", con = "/tmp/test/test.xml")
+
+  settings <- read.settings("/tmp/test/test.xml")
+  get.trait.data()
+  
+  expect_true(file.exists("/tmp/out/trait.data.Rdata"))
+})
