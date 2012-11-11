8a01cfd715170ebe0bd2438a64086a7ced6590e6
Shawn P. Serbin <sserbin@illinois.edu>
2012-11-11 12:52:56 -0600

    Updates to shp2kml.R code.  Now runs and produces .kml files with all layer and object attributes.

--- modules/data.land/R/plot2AGB.R
+++ modules/data.land/R/plot2AGB.R
@@ -1,3 +1,11 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2012 University of Illinois, NCSA.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the 
+# University of Illinois/NCSA Open Source License
+# which accompanies this distribution, and is available at
+# http://opensource.ncsa.illinois.edu/license.html
+#-------------------------------------------------------------------------------
 ## convert composite ring & census data into AGB
 
 plot2AGB <- function(unit.conv=0.02){
