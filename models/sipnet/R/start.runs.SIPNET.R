3c977c43991bc5c4baadc56abdcb5624e1fb5391
Rob Kooper <kooper@illinois.edu>
2012-11-13 01:44:58 -0600

    Write runs table, use standard names (sipnet.*), output copied to out folder, nc files are called year.nc

--- models/sipnet/R/start.runs.SIPNET.R
+++ models/sipnet/R/start.runs.SIPNET.R
@@ -23,73 +23,81 @@ start.run.SIPNET <- function(run.id){
 
 }
 
-start.runs.SIPNET <- function(){
+start.runs.SIPNET <- function(runid){
+  host <-  settings$run$host
 
-  host     <-  settings$run$host
-  
-  #Run model from user made bash script 
-  if(host$name == 'localhost') {
+  if (settings$run$host$name == "localhost") {
+    cwd = getwd()
+    setwd(file.path(settings$run$host$rundir, as.character(runid)))
+    system2(settings$model$binary)
+    file.rename(file.path(settings$run$host$rundir, runid, "sipnet.out"), file.path(settings$run$host$outdir, runid, "sipnet.out"))
+    file.copy(file.path(settings$run$host$rundir, runid, "README.txt"), file.path(settings$run$host$outdir, runid, "README.txt"))
+    setwd(cwd)
+  } else {  
 
-    ## generate list of runs
-    runs <- dir(settings$outdir,full.names=TRUE)
-    runs <- runs[file.info(runs)$isdir]
-    
-    ### Eliminate non-run dirs --------------------------------------------
+    #Run model from user made bash script 
+    if(host$name == 'localhost') {
 
-## This one is removing all the SA runs (since the pft name is in the SA name)    
-#    pft.dir <- strsplit(settings$pfts$pft$outdir,"/")[[1]]
-#    ln <- length(pft.dir)
-#    pft.dir <- pft.dir[ln]
-#    runs <- runs[-grep(pft.dir,runs)]
+      ## generate list of runs
+      runs <- dir(settings$outdir,full.names=TRUE)
+      runs <- runs[file.info(runs)$isdir]
+      
+      ### Eliminate non-run dirs --------------------------------------------
 
-## these two are removing all the run directories    
-#    run.dir <- strsplit(settings$run$host$rundir,"/")[[1]]
-#    ln <- length(run.dir)
-#    run.dir <- run.dir[ln]
-#    runs <- runs[-grep(run.dir,runs)]
-    
-#    out.dir <- strsplit(settings$run$host$outdir,"/")[[1]]
-#    ln <- length(out.dir)
-#    out.dir <- out.dir[ln]
-#    runs <- runs[-grep(paste("/",out.dir,sep=""),runs)]
-    ### Done -------------------------------------------------------------
-    
-    ## run SIPNET for each 
-    for(run in runs){
-      print(paste("---- SIPNET model run: ",run,sep=""))
-      if("sipnet.in" %in% dir(run)){  ## make sure directory is a SIPNET folder
-        system(paste('(cd ', run, '; ',settings$model$binary," )", sep = ''))
+  ## This one is removing all the SA runs (since the pft name is in the SA name)    
+  #    pft.dir <- strsplit(settings$pfts$pft$outdir,"/")[[1]]
+  #    ln <- length(pft.dir)
+  #    pft.dir <- pft.dir[ln]
+  #    runs <- runs[-grep(pft.dir,runs)]
+
+  ## these two are removing all the run directories    
+  #    run.dir <- strsplit(settings$run$host$rundir,"/")[[1]]
+  #    ln <- length(run.dir)
+  #    run.dir <- run.dir[ln]
+  #    runs <- runs[-grep(run.dir,runs)]
+      
+  #    out.dir <- strsplit(settings$run$host$outdir,"/")[[1]]
+  #    ln <- length(out.dir)
+  #    out.dir <- out.dir[ln]
+  #    runs <- runs[-grep(paste("/",out.dir,sep=""),runs)]
+      ### Done -------------------------------------------------------------
+      
+      ## run SIPNET for each 
+      for(run in runs){
+        print(paste("---- SIPNET model run: ",run,sep=""))
+        if("sipnet.in" %in% dir(run)){  ## make sure directory is a SIPNET folder
+          system(paste('(cd ', run, '; ',settings$model$binary," )", sep = ''))
+        }
+        
       }
       
-    }
-    
-  }else{
-    warning("REMOTE RUNNING OF SIPNET NOT YET IMPLEMENTED")
-    stop()
-    
-    ## Priority only needs to be set for very big jobs
-    ## priority can be NULL in settings, if null,
-    ## only batch.jobs.sh is required to exist
-    if(is.null(settings$run$priority)){
-      batch.jobs.script <- system.file("inst", "batch.jobs.sh", package="PEcAn.SIPNET")
+    }else{
+      warning("REMOTE RUNNING OF SIPNET NOT YET IMPLEMENTED")
+      stop()
       
-    } else if (as.numeric(settings$run$priority) < 0) {
-      batch.jobs.script <- system.file("inst", "batch.jobs.lowp.sh", package="PEcAn.SIPNET")
+      ## Priority only needs to be set for very big jobs
+      ## priority can be NULL in settings, if null,
+      ## only batch.jobs.sh is required to exist
+      if(is.null(settings$run$priority)){
+        batch.jobs.script <- system.file("inst", "batch.jobs.sh", package="PEcAn.SIPNET")
+        
+      } else if (as.numeric(settings$run$priority) < 0) {
+        batch.jobs.script <- system.file("inst", "batch.jobs.lowp.sh", package="PEcAn.SIPNET")
+        
+      } else if (as.numeric(settings$run$priority) > 0){
+        stop("need admin rights to set higher priority")
+      }
       
-    } else if (as.numeric(settings$run$priority) > 0){
-      stop("need admin rights to set higher priority")
+      ## write runscript that rsyncs at the end
+      if(any(grep("SIPNET", settings$run$host$rundir))){
+        write.run.SIPNET(settings)
+      }
+      
+      system(paste("echo 'cd ", host$rundir, "' | ",
+                   "cat - ", batch.jobs.script, " | ",
+                   'ssh -T ', host$name, sep = ''))
     }
-    
-    ## write runscript that rsyncs at the end
-    if(any(grep("SIPNET", settings$run$host$rundir))){
-      write.run.SIPNET(settings)
-    }
-    
-    system(paste("echo 'cd ", host$rundir, "' | ",
-                 "cat - ", batch.jobs.script, " | ",
-                 'ssh -T ', host$name, sep = ''))
   }
-  
 } ### End of function
 #==================================================================================================#
 
