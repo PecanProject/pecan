59b5c36b77c68af216688059a5f3297b03564d7f
dlebauer <dlebauer@gmail.com>
2013-01-22 15:21:02 -0600

    updated tests; refs #1469

--- models/biocro/inst/test/test.model2netcdf.BIOCRO.R
+++ models/biocro/inst/test/test.model2netcdf.BIOCRO.R
@@ -1,10 +1,25 @@
-result <- read.csv(system.file("result.csv", package = "PEcAn.BIOCRO"))
+settings.xml <- system.file("pecan.biocro.xml", package = "PEcAn.BIOCRO")
+settings <- read.settings(settings.xml)
 
-outdir <- "~/R-dev/pecan/models/biocro/inst/"
-#test_that{"we can create a netcdf file in PEcAn default format",
-model2netcdf.BIOCRO(outdir=outdir)
-ensemble.output <- read.ensemble.output(1,outdir = outdir, 
-                                                  start.year=2004,
-                                                  end.year=2004,
-                                                  variables="Stem")          
+## put test output in settings$outdir (/tmp/out/run/)
+result.csv <- system.file("result.csv", package = "PEcAn.BIOCRO")
+file.copy(from = result.csv, to = settings$outdir)
+
+test_that("model2netcdf.BIOCRO reads a .csv and writes a netcdf file",{
+  model2netcdf.BIOCRO(outdir = settings$outdir)
+  expect_true(file.exists(file.path(settings$outdir, "2004.nc")))
+})
+
+
+test_that("model2netcdf.BIOCRO wrotes netCDF file in PEcAn format",{
+  
+
 }
+          
+test_that("read.ensemble.output works with BIOCRO output", {
+  ensemble.output <- read.ensemble.output(1, outdir  = settings$outdir, 
+                                          start.year = 2004,
+                                          end.year   = 2004,
+                                          variables  = "Stem")          
+  
+})
