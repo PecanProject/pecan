03ee7d60fb7b71d09a2fb5d4aa1cdcb86679b03a
dlebauer <dlebauer@gmail.com>
2013-01-29 17:30:35 -0600

    added ability to use inputs and dbfiles table when weather data is available

--- models/biocro/R/start.runs.BIOCRO.R
+++ models/biocro/R/start.runs.BIOCRO.R
@@ -28,20 +28,37 @@ start.runs.BIOCRO <- function(runid) {
   lon <- as.numeric(settings$run$site$lon)
   start.date <- settings$run$start.date
   end.date <- settings$run$end.date
-  site_id = settings$run$site$id
+  site.id = settings$run$site$id
   
+  site.info <- query.base(paste0("select * from sites where id = ", site.id, ";"))
+  site.exists <- nrow(site.info) == 1
+  if(!site.exists){
+    query.base(paste0("insert into sites (sitename, lat, lon) values(",
+                      vecpaste(c(settings$run$site$name, lat, lon)), ");"))
+  }
   
   ### TODO the following code should be run during write_configs and the file name passed to the start.runs function
   metfiles <- query.base(paste("select start_date, end_date, file_name, file_path ",
                    "from inputs join dbfiles on dbfiles.file_id = inputs.file_id ",
                    "where start_date <= '", start.date, 
                    "' and end_date >= '", end.date, 
-                   "' and site_id =", site_id, ";", sep = ""))
+                   "' and site_id =", site.id, ";", sep = ""))
 
   if(nrow(metfiles == 1)){
     weather <- read.csv(file.path(metfiles$file_path, metfiles$file_name), row.names = NULL)
   } else {
     weather <- InputForWeach(lat, lon, year(start.date), year(end.date))
+    write.csv(weather, file = file.path(outdir, "weather.csv"))
+    file.id <- 1+ max(query.base(paste0("select max(inputs.file_id), max(dbfiles.file_id) ",
+                                 " from inputs right join dbfiles on inputs.file_id = dbfiles.file_id;")))
+    query.base(paste0("insert into dbfiles (file_name, file_path, created_at, file_id) ",
+                      "values('weather.csv', '", outdir, "', now(),", file.id,");"))
+    query.base(paste0("insert into inputs ",
+                      "(notes, created_at, site_id, file_id, start_date, ",
+                      "end_date, access_level, format_id) ",
+                      "values(,'downloaded from NCEP', now(),", 
+                      vecpaste(c(site.id, file.id, start.date, end.date, 4, 28)), ");"))
+
   }
 
   weather2 <- weachNEW(weather, lati = lat, ts = 1, 
@@ -49,13 +66,11 @@ start.runs.BIOCRO <- function(runid) {
                        ws.units="mph", pp.units="in")
 
   # run model
-  config <- xmlToList(xmlParse(file.path(settings$outdir, runid, "data.xml")))
+  config <- xmlToList(xmlParse(file.path(outdir, runid, "data.xml")))
   pp <- do.call(photoParms, list(unlist(config$parms)))
   
   BioGro_result <- BioGro(weather2, photoControl=pp)
 
-  # save results
-  write.csv(weather, file = file.path(outdir, "weather.csv"))
   write.csv(with(BioGro_result, data.frame(DayofYear, Hour, ThermalT, Stem, Leaf, Root)), 
             file=file.path(outdir, "result.csv"))
   file.copy(file.path(rundir, "README.txt"), file.path(outdir, "README.txt"))
