ecb25f15c52fca4d6d022ba2f8aad7595b920690
David LeBauer <dlebauer@gmail.com>
2013-01-30 15:14:06 -0600

    updated read.ensemble and read.sensitivity functions to robs edits that had been lost during a merge (https://github.com/PecanProject/pecan/blob/807a90638876b0f4fb5abfc6d4faa4c0a6bd1cba/utils/R/utils.R)

--- modules/uncertainty/R/read.sensitivity.output.R
+++ modules/uncertainty/R/read.sensitivity.output.R
@@ -1,4 +1,11 @@
-
+#-------------------------------------------------------------------------------
+# Copyright (c) 2012 University of Illinois, NCSA.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the 
+# University of Illinois/NCSA Open Source License
+# which accompanies this distribution, and is available at
+# http://opensource.ncsa.illinois.edu/license.html
+#-------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------#
 ##' Reads output of sensitivity analysis runs
 ##'
@@ -18,28 +25,21 @@
 read.sa.output <- function(traits, quantiles, outdir, pft.name='', 
                            start.year, end.year, variables, model){
   
+  if (!exists('runs.samples')) {
+    load(file.path(settings$outdir, 'samples.Rdata'))    
+  }
+
   sa.output <- matrix(nrow = length(quantiles),
                       ncol = length(traits),
                       dimnames = list(quantiles, traits))
   for(trait in traits){
     for(quantile in quantiles){
-      if(!quantile == "50"){
-        run.id <- get.run.id('SA', round(as.numeric(quantile)/100, 3),
-                             trait = trait, pft.name = pft.name)
-        print(run.id)
-        sa.output[quantile, trait] <-
-          sapply(read.output(run.id, outdir,
-                             start.year, end.year,
-                             variables, model),
-                 mean, na.rm=TRUE)
-      } else if (quantile == "50") {
-        sa.output[quantile, trait] <- sapply(read.output(get.run.id('SA', 'median'),
-                                                         outdir,
-                                                         start.year, end.year,
-                                                         variables, model),
-                                             mean,na.rm=TRUE)
-      } ## end loop over quantiles
-    }
+      run.id <- runs.samples$sa[[pft.name]][quantile, trait]
+      sa.output[quantile, trait] <- sapply(read.output(run.id, file.path(outdir, run.id),
+                                                       start.year, end.year,
+                                                       variables, model),
+                                           mean, na.rm=TRUE)
+    } ## end loop over quantiles
   } ## end loop over traits
   sa.output <- as.data.frame(sa.output)
   return(sa.output)
