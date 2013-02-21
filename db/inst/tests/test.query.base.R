ed56d18f8877394bac3b234138633532620c4cd2
dlebauer <dlebauer@gmail.com>
2013-02-21 15:54:09 -0600

    updated PEcAn.DB tests

--- db/inst/tests/test.query.base.R
+++ db/inst/tests/test.query.base.R
@@ -6,22 +6,21 @@
 # which accompanies this distribution, and is available at
 # http://opensource.ncsa.illinois.edu/license.html
 #-------------------------------------------------------------------------------
-
-test_that("settings variable is passed from db/tests/run.all.R",{
-  expect_true(exists("settings"))
-  expect_is(settings, "list")
-  expect_equal(names(settings), "database")
-  expect_equal(names(settings$database), c("userid", "passwd", "location", "name"))
-})
-
-test_that("query base can execute a trivial SQL statement and return results",{  
-  ans <- query.base("select count(*) from traits;")
-  expect_is(ans, "data.frame")
-  expect_is(ans[,1], "numeric")
-  expect_true(length(ans) == 1)
-  
-  tables <- query.base('show tables;')
-  expect_true(is.data.frame(tables))
-  expect_true(is.character(tables[,1]))
-  expect_true(ncol(tables) == 1)
-})
+settings <- list(database = 
+                   list(userid = "bety", 
+                        passwd = "bety", 
+                        location = "localhost",
+                        name = "bety"))
+if(db.exists()){
+  test_that("query base can execute a trivial SQL statement and return results",{  
+    ans <- query.base("select count(*) from traits;")
+    expect_is(ans, "data.frame")
+    expect_is(ans[,1], "numeric")
+    expect_true(length(ans) == 1)
+    
+    tables <- query.base('show tables;')
+    expect_true(is.data.frame(tables))
+    expect_true(is.character(tables[,1]))
+    expect_true(ncol(tables) == 1)
+  })
+}
