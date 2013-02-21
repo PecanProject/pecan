7f7d920ccf9da2a48f0d0e86aa6db0273205b178
dlebauer <dlebauer@gmail.com>
2013-02-20 19:27:37 -0600

    fixed errors in tests of biocro-specific functions resulting from use of runs.sample (95022d2a)

--- models/biocro/inst/tests/test.model2netcdf.BIOCRO.R
+++ models/biocro/inst/tests/test.model2netcdf.BIOCRO.R
@@ -2,27 +2,36 @@ settings.xml <- system.file("extdata/pecan.biocro.xml", package = "PEcAn.BIOCRO"
 settings <- read.settings(settings.xml)
 
 ## put test output in tempdir
-outdir <- tempdir()
+settings$outdir <- tempdir()
 result.csv <- system.file("extdata/result.csv", package = "PEcAn.BIOCRO")
-file.copy(from = result.csv, to = outdir)
+runs.samples <- list(ensemble = data.frame(id = 1))
+runoutdir <- file.path(settings$outdir, "1")
+dir.create(runoutdir, recursive = TRUE, showWarnings = FALSE)
 
+file.copy(from = result.csv, to = runoutdir)
+
+save(runs.samples, 
+     file = file.path(settings$outdir, 'samples.Rdata'))
+     
 test_that("model2netcdf.BIOCRO reads a .csv and writes a netcdf file",{
-  model2netcdf.BIOCRO(outdir = outdir)
-  biocro.nc <- file.path(outdir, "2004.nc")
-  expect_true(file.exists(biocro.nc))
+  model2netcdf.BIOCRO(outdir = runoutdir)
+  biocro.ncfile <- file.path(runoutdir, "2004.nc")
+  expect_true(file.exists(biocro.ncfile))
+ 
 })
 
+biocro.nc <- nc_open(file.path(runoutdir, "2004.nc"))
 
-test_that("model2netcdf.BIOCRO wrotes netCDF file in PEcAn format",{
-  biocro.nc <- nc_open(biocro.nc)
+test_that("model2netcdf.BIOCRO wrote netCDF file in PEcAn format",{
   expect_true(all(c("Stem", "Leaf", "Root") %in% names(biocro.nc$var)))
-  
-}
+})
           
 test_that("read.ensemble.output works with BIOCRO output", {
-  ensemble.output <- read.ensemble.output(1, outdir  = outdir, 
+  ensemble.output <- read.ensemble.output(ensemble.size = 1, 
+                                          outdir  = settings$outdir, 
                                           start.year = 2004,
                                           end.year   = 2004,
-                                          variables  = "Stem")          
+                                          variables  = "Stem",
+                                          model = "BIOCRO")          
   
 })
