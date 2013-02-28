2e67e66597e65e855e75f651c1f13bae015c21af
Jing Xia <jingxia615@gmail.com>
2013-02-28 15:49:16 -0600

    new.taylor function

--- qaqc/inst/test/test.taylor.plot.R
+++ qaqc/inst/test/test.taylor.plot.R
@@ -7,11 +7,12 @@
 # http://opensource.ncsa.illinois.edu/license.html
 #-------------------------------------------------------------------------------
 
-#--------------------------------------------------------------------------------------------------#
-##' 
-##' generate benchmarking inputs table
-##' @title 
-##' @param runid the id of the run (folder in runs) to execute
+
+library("plotrix")
 set.seed(1)
-data=data.frame(site=c(1,1,1,2,2,3),date=c(2001,2001,2002,2003,2004,2005),obs=rnorm(6,10,2),model=rnorm(6,10.3)+2)
-write.csv(data,file="pecan/qaqc/inst/data.csv")
+testdata <- data.frame(site=c(1,1,1,2,2,3),
+                   date=c(2001,2001,2002,2003,2004,2005),
+                   obs=rnorm(6,10,2),model1=rnorm(6,10,3)+2,model2=rnorm(6,11,3)+2)
+
+taylor.diagram(testdata$obs,testdata$model1,pos.cor=FALSE)
+taylor.diagram(testdata$obs,model=testdata$model2,add=TRUE,col="blue")
