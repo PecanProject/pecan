111b56a9e8e44fb2201a228ef4359cce44ba37c2
David LeBauer <dlebauer@illinois.edu>
2013-06-03 17:12:47 -0500

    added variables to test.settings.xml; minor changes to test.read.output

--- utils/inst/tests/test.read.output.R
+++ utils/inst/tests/test.read.output.R
@@ -2,11 +2,12 @@ extdata <- system.file("extdata", package = "PEcAn.utils")
 outdir <- tempdir()
 settings <- read.settings(file.path(extdata, "test.settings.xml"))
 
-test_that("example output is available",{
-  expect_true(all(file.copy(dir(extdata, pattern = "testENS-000[1-4].nc", full.names = TRUE), outdir)))
-  read.output(run.id = 1, 
-              outdir = outdir, 
-              start.year=1999, end.year=2000,
-              variables="NPP", model = "SIPNET")
+test_that("example SIPNET  output is available",{
+  expect_true(all(file.copy(dir(extdata, pattern = "sipnet.out",
+                                full.names = TRUE), outdir)))
+  result <- read.output(run.id = 1, 
+                        outdir = outdir, 
+                        start.year=1999, end.year=2000,
+                        variables="GPP", model = "SIPNET")
   
 })
