49de27ee2bd0661a297f34ea4f61a754afa2af4b
Rob Kooper <kooper@illinois.edu>
2013-06-20 10:50:12 -0500

    Resolved circular dependency and disabled broken tests
    read.settings needs access to the database code, resulting in a
    dependency on PEcAn.DB which in turn depends on PEcAn.utils
    resulting in a circular dependency. Broke dependency by moving
    read.settings to PEcAn.DB
    
    disabled some read.output test in PEcAn.utils since it was testing
    model specific code. (github #38)

---
+++ db/inst/tests/test.read.settings.R
@@ -0,0 +1,25 @@
+#----------------------------------------------------------------------------
+
+## Copyright (c) 2012 University of Illinois, NCSA.
+## All rights reserved. This program and the accompanying materials
+## are made available under the terms of the 
+## University of Illinois/NCSA Open Source License
+## which accompanies this distribution, and is available at
+## http://opensource.ncsa.illinois.edu/license.html
+## #-------------------------------------------------------------------------------
+
+
+context("tests for read.settings and related functions")
+
+settings <- read.settings(system.file("tests/pecan.sipnet.xml", package = "PEcAn.db"))
+
+test_that("read.settings returned correctly", {
+	expect_true(file.exists(settings$outdir))
+	expect_true(file.info(settings$outdir)$isdir)
+	expect_true(file.exists(file.path(settings$outdir, "pecan.xml")))
+})
+
+test_that("read settings returns error if no settings file found (issue #1124)",{
+	## TODO RK : test does not work yet
+	## expect_message(read.settings("nofile.xml"), "Could not find a pecan.xml file")
+})
