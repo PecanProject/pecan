d869f02d4c36c1f7896f53ba9e383fe9ef7d7a59
Rob Kooper <kooper@illinois.edu>
2013-07-08 18:00:43 -0500

    use string versions

--- scripts/updatedb.sh
+++ scripts/updatedb.sh
@@ -29,7 +29,7 @@ fi
 
 # add models
 echo "INSERT INTO models (model_name, model_type, model_path, revision, created_at, updated_at) VALUES
-    ('ED2.2', 'ED2', '${HOSTNAME}:/usr/local/bin/ed2.r46', 46, NOW(), NOW()),
-    ('ED2.2', 'ED2', '${HOSTNAME}:/usr/local/bin/ed2.r82', 82, NOW(), NOW()),
-    ('SIPNET', 'SIPNET', '${HOSTNAME}:/usr/local/bin/sipnet.runk', 0, NOW(), NOW()),
-    ('BIOCRO', 'BIOCRO', '${HOSTNAME}:/bin/true', 1, NOW(), NOW());" | ${CMD} bety
+    ('ED2.2', 'ED2', '${HOSTNAME}:/usr/local/bin/ed2.r46', '46', NOW(), NOW()),
+    ('ED2.2', 'ED2', '${HOSTNAME}:/usr/local/bin/ed2.r82', '82', NOW(), NOW()),
+    ('SIPNET', 'SIPNET', '${HOSTNAME}:/usr/local/bin/sipnet.runk', 'unk', NOW(), NOW()),
+    ('BIOCRO', 'BIOCRO', '${HOSTNAME}:/bin/true', '0.0.1', NOW(), NOW());" | ${CMD} bety
