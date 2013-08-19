936def1bfd0e927d3b615731c6bcd40e83d6b883
Rob Kooper <kooper@illinois.edu>
2013-08-19 15:46:08 -0500

    update code to reflect changes to get.trait.data and run.meta.analysis

--- scripts/workflow.R
+++ scripts/workflow.R
@@ -17,10 +17,10 @@ settings <- read.settings()
 
 #---------------- Run PEcAn workflow. -------------------------------------------------------------#
 # Query the trait database for data and priors
-get.trait.data()
+settings$pfts <- get.trait.data(settings$pfts, settings$run$dbfiles, settings$database, settings$meta.analysis$update)
 
 # Run the PEcAn meta.analysis
-run.meta.analysis()
+run.meta.analysis(settings$pfts, settings$meta.analysis$iter, settings$run$dbfiles, settings$database)
 
 # Calls model specific write.configs e.g. write.config.ed.R
 run.write.configs(settings$model$name, settings$bety$write)
