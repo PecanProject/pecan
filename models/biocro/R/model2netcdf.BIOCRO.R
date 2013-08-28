76db37a195c0bf695a96142592af13d42085d0aa
David LeBauer <dlebauer@illinois.edu>
2013-08-28 09:02:57 -0500

    added 'require(BioCro) to start.runs.BIOCRO, plus minor edits

--- models/biocro/R/model2netcdf.BIOCRO.R
+++ models/biocro/R/model2netcdf.BIOCRO.R
@@ -28,7 +28,7 @@ model2netcdf.BIOCRO <- function(outdir, sitelat, sitelon, start_date, end_date) 
     load(file.path(outdir, "result.RData")) ## contains genus and result
 
     ##result$s <- with(result, ((DayofYear -1) * 24 + Hour) * 3600)
-    if(genus == "Saccharum"){
+    if(config$pft$genus == "Saccharum"){
         for(variable in c("Leaf", "Root", "Stem", "LAI", "DayofYear")) {
             x <- result[[variable]]
             result[[variable]] <- c(x[1], rep(x[-1], 24, each = TRUE))
