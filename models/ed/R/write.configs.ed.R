43dae1a9e03eea2fd1a513de488f0ad2d7c6a132
Rob Kooper <kooper@illinois.edu>
2013-09-12 11:23:32 -0500

    use scratch folder using settings$host$run$scratchdir clearn scratch folder using settings$host$run$clearscratch (true by default) (redmine #1129)

--- models/ed/R/write.configs.ed.R
+++ models/ed/R/write.configs.ed.R
@@ -111,12 +111,29 @@ write.config.ED2 <- function(defaults, trait.values, settings, run.id){
     outdir <- file.path(settings$modeloutdir, as.character(run.id))
   }
 
+  # command if scratch is used
+  if (is.null(settings$run$host$scratchdir)) {
+    modeloutdir <- outdir
+    copyscratch <- "# no need to copy from scratch"
+    clearscratch <- "# no need to clear scratch"
+  } else {
+    modeloutdir <- file.path(settings$run$host$scratchdir, as.character(run.id))
+    copyscratch <- paste("rsync", "-a", file.path(modeloutdir, "*"), outdir)
+    if (is.null(settings$run$host$clearscratch) || is.na(as.logical(settings$run$host$clearscratch)) || as.logical(settings$run$host$clearscratch)) {
+      clearscratch <- paste("rm", "-rf", modeloutdir)
+    } else {
+      clearscratch <- "# scratch is not cleared"
+    }
+  }
+
   # create launch script
   writeLines(c("#!/bin/bash",
-             paste("mkdir -p", outdir),
+             paste("mkdir -p", modeloutdir),
              paste("cd", rundir),
              "export GFORTRAN_UNBUFFERED_PRECONNECTED=yes",
-             settings$model$binary,
+             #settings$model$binary,
+             copyscratch,
+             clearscratch,
              paste("cp ", file.path(rundir, "README.txt"), file.path(outdir, "README.txt"))),
              con=file.path(settings$rundir, run.id, "job.sh"))
   Sys.chmod(file.path(settings$rundir, run.id, "job.sh"))
@@ -254,24 +271,13 @@ write.config.ED2 <- function(defaults, trait.values, settings, run.id){
   ed2in.text <- gsub('@END_YEAR@', format(enddate, "%Y"), ed2in.text)
 
   ##----------------------------------------------------------------------
-  ed2in.text <- gsub('@OUTDIR@', settings$run$host$outdir, ed2in.text)
+  ed2in.text <- gsub('@OUTDIR@', modeloutdir, ed2in.text)
   ed2in.text <- gsub('@ENSNAME@', run.id, ed2in.text)
   ed2in.text <- gsub('@CONFIGFILE@', file.path(settings$run$host$rundir, run.id, "config.xml"), ed2in.text)
   
-  ## Generate a numbered suffix for scratch output folder.  Useful for cleanup.  TEMP CODE. NEED TO UPDATE.
-  ## cnt = counter(cnt) # generate sequential scratch output directory names 
-  ## print(cnt)
-  ## scratch = paste(Sys.getenv("USER"),".",cnt,"/",sep="")
-  scratch <- file.path(Sys.getenv("USER"),settings$run$scratch)
-  ## ed2in.text <- gsub('@SCRATCH@', paste('/scratch/', settings$run$scratch, sep=''), ed2in.text)
-  ed2in.text <- gsub('@SCRATCH@', paste0('/scratch/', scratch), ed2in.text)
-  ##
-  
-#  ed2in.text <- gsub('@OUTFILE@', file.path(settings$run$host$outdir, run.id, "analysis"), ed2in.text)
-#  ed2in.text <- gsub('@OUTFILE@', file.path(settings$run$host$outdir, run.id, "analysis"), ed2in.text)
-
-  ed2in.text <- gsub('@FFILOUT@', file.path(settings$run$host$outdir, run.id, "analysis"), ed2in.text)
-  ed2in.text <- gsub('@SFILOUT@', file.path(settings$run$host$outdir, run.id, "history"), ed2in.text)
+  ##----------------------------------------------------------------------
+  ed2in.text <- gsub('@FFILOUT@', file.path(modeloutdir, "analysis"), ed2in.text)
+  ed2in.text <- gsub('@SFILOUT@', file.path(modeloutdir, "history"), ed2in.text)
   
   ##----------------------------------------------------------------------
   writeLines(ed2in.text, con = file.path(settings$rundir, run.id, "ED2IN"))
