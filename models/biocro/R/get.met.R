179a34b08ccfcbff3bf9123a3caf70c761accfca
Rob Kooper <kooper@illinois.edu>
2013-09-17 20:51:12 -0500

    update biocro for jobrun (redmin #1560 #1750)

--- models/biocro/R/get.met.R
+++ models/biocro/R/get.met.R
@@ -28,18 +28,30 @@ get.ncepmet <- function(lat = as.numeric(settings$run$site$lat),
         query.base(paste0("insert into sites (sitename, lat, lon) values(",
                           vecpaste(c(settings$run$site$name, lat, lon)), ");"), con = con)
     }
+
+  mimetype <- "text/csv"
+  formatname <- "biocromet"
+  formatid <- db.query(paste0("SELECT id FROM formats WHERE mime_type='", mimetype, "' AND name='", formatname, "'"), con)[['id']]
+  if (is.null(formatid)) {
+    # insert format
+    db.query(paste0("INSERT INTO formats (mime_type, name, created_at, updated_at) VALUES ('", mimetype, "', '", formatname, "', NOW(), NOW())"), con)
+    formatid <- db.query(paste0("SELECT id FROM formats WHERE mime_type='", mimetype, "' AND name='", formatname, "'"), con)[['id']]
+  }
+
     
 ### TODO the following code should be run during write_configs and the file name passed to the start.runs function
 ### the next set of code using queries will be passed to a new function called "query.met"
+db.showQueries(TRUE)
     metfiles <- db.query(
         paste("select start_date, end_date, hostname, file_name, file_path ",
-              "from inputs join dbfiles on dbfiles.container_id = inputs.file_id ",
+              "from inputs join dbfiles on dbfiles.container_id = inputs.file_id and dbfiles.container_type='Input' ",
               "join machines on dbfiles.machine_id = machines.id ",
               "join formats on inputs.format_id = formats.id ",
-              "where formats.name = 'biocromet' ",
+              "where inputs.format_id = ", formatid,
               " and start_date <= '", start.date, 
               "' and end_date >= '", end.date, 
               "' and site_id =", site.id, ";", sep = ""), con = con)
+db.showQueries(FALSE)
     
     if(nrow(metfiles) == 0){
         metfile.exists <- FALSE
@@ -67,23 +79,21 @@ get.ncepmet <- function(lat = as.numeric(settings$run$site$lat),
         write.csv(weather,
                   file = file.path(weather.dir, "weather.csv"), row.names = FALSE)
         machine.id <- db.query(paste0("select id from machines where hostname = '", hostname, "';"), con = con)
+        # TODO this could be a race condition, select/insert is not atomic
         if(nrow(machine.id) == 0){
             machine.id <- db.query("select max(id) + 1 as id from machines;", con = con)
             db.query(paste0("insert into machines (id, hostname, created_at) values(",
                             vecpaste(c(machine.id, hostname, format(Sys.time()))), ");"), con = con)
         }
-        file.id <- 1 + db.query(paste0(
-            "select 1 + greatest(max(inputs.file_id), max(dbfiles.container_id)) as id ",
-            " from inputs right join dbfiles on inputs.file_id =",
-            "dbfiles.container_id;"), con = con)
-        
-        db.query(paste0("insert into dbfiles (file_name, file_path, created_at, machine_id, container_id) ",
-                        "values(", vecpaste(c('weather.csv', weather.dir, format(Sys.time()), machine.id, file.id)),");"), con = con)
+        # TODO MAKE SURE FORMAT_ID IS CHECKED
         db.query(paste0("insert into inputs ",
-                        "(notes, created_at, site_id, file_id, start_date, ",
+                        "(notes, created_at, updated_at, site_id, start_date, ",
                         "end_date, access_level, format_id) ",
-                        "values('downloaded from NCEP', now(),",
-                        vecpaste(c(site.id, file.id, start.date, end.date, 4, 28)), ");"), con = con)
+                        "values('downloaded from NCEP', NOW(), NOW(),",
+                        vecpaste(c(site.id, start.date, end.date, 4, formatid)), ");"), con = con)
+        inputid <- db.query(paste0("SELECT id FROM inputs WHERE site_id=", siteid, " AND format_id=", formatid, " AND start_date='", start.date, "' AND end_date='", end.date, "';"), con)[['id']]
+        db.query(paste0("insert into dbfiles (file_name, file_path, created_at, updated_at, machine_id, container_id, container_type) ",
+                        "values(", vecpaste(c('weather.csv', weather.dir, 'NOW()', 'NOW()', machine.id, inputid, 'Input')),");"), con = con)
         
     }
     query.close(con)
