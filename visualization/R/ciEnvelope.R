100c4dc0c99aad5140033f5051206bdef494c548
Mike Dietze <dietze@bu.edu>
2013-10-24 16:10:13 -0400

    Tree Rings
    Moved JAGS code and exectution to its own function, InventoryGrowthFusion, as well as a helper function, parse.MatrixNames, which extracts the row and column info out of matrices output by JAGS

--- visualization/R/ciEnvelope.R
+++ visualization/R/ciEnvelope.R
@@ -1,3 +1,4 @@
+#plots a confidence interval around an x-y plot (e.g. a timeseries)
 ciEnvelope <- function(x,ylo,yhi,...){
   polygon(cbind(c(x, rev(x), x[1]), c(ylo, rev(yhi),
                                       ylo[1])), border = NA,...) 
