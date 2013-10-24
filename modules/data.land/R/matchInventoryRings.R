bad61e6a131e494e1eb66be2e4b065bf03652034
Mike Dietze <dietze@bu.edu>
2013-10-24 16:27:15 -0400

    tree rings:
    encapsulated the code to match the forest inventory data with the tree ring data into a single data frame
    
    be aware, the code currently drops all trees that are not matched to rings by default. This needs to be generalized again in order to estimate plot-level statistics

---
+++ modules/data.land/R/matchInventoryRings.R
@@ -0,0 +1,26 @@
+matchInventoryRings <- function(trees,rings,extractor=from.TreeCode){
+
+  ## build tree codes
+  names(trees) = toupper(names(trees))
+  tree.ID = to.TreeCode(trees$SITE,trees$PLOT,trees$SUB,trees$TAG)
+
+  ## build tree ring codes
+  if(is.list(rings)){
+    ring.file <- rep(names(rings),times=sapply(rings,ncol))
+    rings <- combine.rwl(rings)
+  }
+  ring.ID <- names(rings)
+  ring.info <- extract.stringCode(ring.ID,extractor)
+
+  ## matching up data sets by tree
+  mch = match(tree.ID,ring.ID)
+  cored = apply(!is.na(trees[,grep("DATE_CORE_COLLECT",names(trees))]),1,any)
+  unmatched = which(cored & is.na(mch))
+  write.table(tree.ID[unmatched],file="unmatched.txt")
+  mch[duplicated(mch)] <- NA  ## if there's multiple stems, match the first
+
+  ## combine data into one table
+  combined = cbind(trees,t(as.matrix(rings))[mch,-30:0 + nrow(rings)])
+  combined = combined[!is.na(combined$"2000"),]
+  return(combined)
+}
