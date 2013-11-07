2881cafdc0e075cb4c761337457747c4d7e3ce40
dlebauer <dlebauer@gmail.com>
2013-11-07 17:46:40 -0600

    added find.land function to data.land (identifies lat / lon coordinates that are land)

--- modules/data.land/R/find.land.R
+++ modules/data.land/R/find.land.R
@@ -1,21 +1,28 @@
 ## from Josh O'Brien http://stackoverflow.com/a/19148078/513006
 
-library(maptools)
-data(wrld_simpl)
-library(data.table)
-load("/home/dlebauer/met/ncep/latlon.RData")
-
-## Create a SpatialPoints object
-points <- data.table(expand.grid(Lon-180, Lat))
-setnames(points, c("lon", "lat"))
-pts <- SpatialPoints(points, proj4string=CRS(proj4string(wrld_simpl)))
-
-## Find which points fall over land
-landmask <- cbind(points, data.table(land = !is.na(over(pts, wrld_simpl)$FIPS)))
-
-## Check that it worked
-plot(wrld_simpl) 
-landmask[,points(lon, lat, col=1+land, pch=16)]
-ncep_landmask <- landmask
-setkeyv(landmask, c("lat", "lon"))
-save(Lat, Lon, landmask, file = "../data/ncep_landmask.RData")
+##' find land
+##'
+##' extract terrestrial lat and lon coordinates from a grid
+##'
+##' @title find.land
+##' @param lat vector of latitudes
+##' @param lon vector of longitudes
+##' @return data frame with numeric lat, lon and logical value "land"
+##' @author David LeBauer
+find.land <- function(lat, lon, plot = FALSE){
+    library(maptools)
+    data(wrld_simpl)
+    
+    ## Create a SpatialPoints object
+    points <- expand.grid(lon, lat)
+    colnames(points) <- c("lat", "lon")
+    pts <- SpatialPoints(points, proj4string=CRS(proj4string(wrld_simpl)))
+    
+    ## Find which points fall over land
+    landmask <- cbind(points, data.frame(land = !is.na(over(pts, wrld_simpl)$FIPS)))
+    if(plot){
+        plot(wrld_simpl) 
+        landmask[,points(lon, lat, col=1+land, pch=16)]
+    }
+    return(landmask)
+}
