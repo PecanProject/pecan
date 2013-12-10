89100fbf0c7f17b41792d0b2201b411ac3a57b0d
hardimanb <brady.hardiman@gmail.com>
2013-12-10 11:14:12 -0500

    Extract PALSAR from ChEAS FIA coords. Build tall matrix of output (w/o NAs).

--- modules/data.remote/scripts/ChEAS_FIA.R
+++ modules/data.remote/scripts/ChEAS_FIA.R
@@ -1,8 +1,32 @@
 ##Author Brady S. Hardiman 11/12/2013
 
-##Read in fuzzed FIA coordinates from Andy Finley (MSU)
-require(sp)
+##Read in fuzzed FIA coordinates supplied by Andy Finley (MSU)
 
+################################
+## Load Required Packages
+################################
+require(rgdal)
+require(raster)
+require(sp)
+require(RgoogleMaps)
+require(maptools)
+require(ggplot2)
+require(car)
+require(spatsta)
+
+################################
+## Options
+################################
+
+#generate and save kml files?
+
+
+################################
+## Read in FIA data for calibrationof PALSAR backscatter returns
+##    Uses PALSAR metadata file to determine geographic extent of available PALSAR data and crops FIA data 
+##      to match PALSAR extent.
+##    Creates and saves KML file to show FIA points in Google Earth.
+################################
 calib_inpath <-"/home/bhardima/git/pecan/modules/data.remote/biometry/Link_to_Forest_Biomass_Calibration_Coords"
 
 calib_infile <-read.csv(file.path(calib_inpath,"wi-biomass-fuzzed.csv"), sep=",", header=T) #Wisconsin FIA plots
@@ -17,12 +41,15 @@ FIA.points <- SpatialPointsDataFrame(Sr1, wi.fia) #convert to class="SpatialPoin
 
 # writeOGR(FIA.points, layer=1, "WI_FIA.kml", driver="KML") #export as kml (this puts in in the Home folder) 
 
-ChEAS_PLASAR_extent<-rbind(c(-88.446974,46.936532), 
-c(-88.023077,45.343856),
-c(-91.057452,45.032895),
-c(-91.454983,46.725886),
-c(-88.446974,46.936532)) #corner coords for palsar data in cheas domain. DEVELOP METHOD TO BUILD THIS AUTOMATICALLY--METADATA TABLE???
+metadata<- read.csv("~/git/pecan/modules/data.remote/output/metadata/output_metadata.csv", sep="\t", header=T)
 
+## corner coords for cheas domain based on avaialable PALSAR data.
+ChEAS_PLASAR_extent <-rbind(cbind(min(metadata$scn_nwlon),max(metadata$scn_nwlat)),
+                            cbind(max(metadata$scn_nelon),max(metadata$scn_nelat)),
+                            cbind(max(metadata$scn_selon),min(metadata$scn_selat)),
+                            cbind(min(metadata$scn_swlon),min(metadata$scn_swlat)),
+                            cbind(min(metadata$scn_nwlon),max(metadata$scn_nwlat))) 
+                            
 ChEAS_PLASAR_extent<- Polygon(ChEAS_PLASAR_extent) #spatial polygon from cheas-palsar extent
 Srs1<- Polygons(list(ChEAS_PLASAR_extent),"ChEAS_PLASAR_extent") #spatial polygons (plural)
 ChEAS_PLASAR_extent<-SpatialPolygons(list(Srs1),proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")) 
@@ -31,11 +58,97 @@ FIA.in.cheas<-as.vector(over(FIA.points,ChEAS_PLASAR_extent)) #subset of FIA plo
 
 FIA.in.cheas[is.na(FIA.in.cheas)]<-0 #replace na's with 0's for indexing
 
-cheasFIA<-FIA.points[FIA.in.cheas>0,]
+cheasFIA<-Sr1@coords[FIA.in.cheas==1,] 
+spcheasFIA <- SpatialPoints(cheasFIA,proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
 
 # writeOGR(cheasFIA, layer=1, "cheas_FIA.kml", driver="KML") #export as kml (this puts in in the Home folder) 
 
-## Use "over" function to ID palsar scenes that intersect with ChEAS bounding box
+################################
+## Begin extracting PALSAR values at FIA plot coordinates
+################################
+palsar_inpath <- file.path("/home/bhardima/git/pecan/modules/data.remote/palsar_scenes/Link_to_cheas/geo_corrected_single_sigma")
+
+# file.info<-read.table(file="/home/bhardima/git/pecan/modules/data.remote/output/metadata/output_metadata.csv",header=T,sep="\t")
+
+# date<-as.Date(metadata$scndate, format='%Y%m%d')
+# col_names<-c(rbind(paste(date, "HH",sep="_"),paste(date, "HV",sep="_")))
+
+pol_bands<-c("HH", "HV")
+numfiles<-length(date.time)
+
+# lake_extracted<-matrix(NA, nrow(lake_coords),length(pol_bands)*numfiles)
+# disturbance_extracted_40m<-matrix(NA, nrow(disturbance_coords),length(pol_bands)*numfiles)
+# 
+# colnames(lake_extracted)<-col_names
+# colnames(disturbance_extracted)<-col_names
+# colnames(disturbance_extracted_40m)<-col_names
+
+# extracted_48m<-matrix(NA, nrow=length(spcheasFIA)*numfiles, ncol=length(pol_bands)) #df to store extracted palsar values. nrow=number of coordinates being extracted. ncol=# of pol_bands
+# extracted_60m<-matrix(NA, nrow=length(spcheasFIA)*numfiles, ncol=length(pol_bands)) #df to store extracted palsar values. nrow=number of coordinates being extracted. ncol=# of pol_bands
+extracted_48m<-matrix(NA, ncol=6) #df to store extracted palsar values. nrow=number of coordinates being extracted. ncol=# of pol_bands
+extracted_60m<-matrix(NA, ncol=6) #df to store extracted palsar values. nrow=number of coordinates being extracted. ncol=# of pol_bands
+
+# colnames(extracted_48m)<-pol_bands
+# colnames(extracted_60m)<-pol_bands
+
+for(i in 1:numfiles){
+#   for(j in 1:length(pol_bands)){        
+
+#     filelist<-as.vector(list.files(file.path(palsar_inpath, pol_bands[j]), pattern=".tif" ,recursive=F))
+#     inpath<-file.path(palsar_inpath,pol_bands[j],filelist[i])
+#     rast<-raster(inpath)
+        HH_filelist<-as.vector(list.files(file.path(palsar_inpath, pol_bands[1]), pattern=".tif" ,recursive=F))
+        HH_inpath<-file.path(palsar_inpath, pol_bands[1],HH_filelist[i])
+        HH_rast<-raster(HH_inpath)
+        
+        HV_filelist<-as.vector(list.files(file.path(palsar_inpath, pol_bands[2]), pattern=".tif" ,recursive=F))
+        HV_inpath<-file.path(palsar_inpath, pol_bands[2],HV_filelist[i])
+        HV_rast<-raster(HV_inpath)
+    
+        ################################
+        ##calibration data from FIA plots 48m BUFFER MEAN
+        ################################
+        HH_data_48m<-extract(HH_rast, spcheasFIA, method="simple",buffer=48, small=T, fun=mean)
+        HV_data_48m<-extract(HV_rast, spcheasFIA, method="simple",buffer=48, small=T, fun=mean)
+#           cols<-seq(j,ncol(extracted_48m),by=2) #columns to be filled with palsar data (if j is odd=HH, if j is even=HV)
+#           extracted_48m[,cols[i]]<-data_48m
+        cleaned_HH_data_48m<-as.vector(na.omit(HH_data_48m))
+        cleaned_HV_data_48m<-as.vector(na.omit(HV_data_48m))
+        
+        filename<-matrix(substr(as.character(HV_filelist[i]),1,15),nrow=length(cleaned_HH_data_48m),ncol=1)
+        colnames(filename)<-"scnid"
+        palsar_date<-matrix(as.character(as.Date(substr(as.character(metadata$scndate[metadata$scnid==filename[1]]),1,8),"%Y%m%d")),nrow=length(cleaned_HH_data_48m),ncol=1)
+        colnames(palsar_date)<-"scndate"
+        extracted_coords<-spcheasFIA@coords[!is.na(HH_data_48m),]
+                
+        both_48<-cbind(filename,palsar_date,extracted_coords,cleaned_HH_data_48m,cleaned_HV_data_48m)
+        extracted_48m<-rbind(extracted_48m,both_48)
+        
+        ###############################
+        #calibration data from FIA plots 60m BUFFER MEAN
+        ###############################
+        HH_data_60m<-extract(HH_rast, spcheasFIA, method="simple",buffer=60, small=T, fun=mean)
+        HV_data_60m<-extract(HV_rast, spcheasFIA, method="simple",buffer=60, small=T, fun=mean)
+        #           cols<-seq(j,ncol(extracted_60m),by=2) #columns to be filled with palsar data (if j is odd=HH, if j is even=HV)
+        #           extracted_60m[,cols[i]]<-data_60m
+        cleaned_HH_data_60m<-as.vector(na.omit(HH_data_60m))
+        cleaned_HV_data_60m<-as.vector(na.omit(HV_data_60m))
+        
+        filename<-matrix(substr(as.character(HV_filelist[i]),1,15),nrow=length(cleaned_HH_data_60m),ncol=1)
+        colnames(filename)<-"scnid"
+        palsar_date<-matrix(as.character(as.Date(substr(as.character(metadata$scndate[metadata$scnid==filename[1]]),1,8),"%Y%m%d")),nrow=length(cleaned_HH_data_60m),ncol=1)
+        colnames(palsar_date)<-"scndate"
+        extracted_coords<-spcheasFIA@coords[!is.na(HH_data_60m),]
+        
+        both_60<-cbind(filename,palsar_date,extracted_coords,cleaned_HH_data_60m,cleaned_HV_data_60m)
+        extracted_60m<-rbind(extracted_60m,both_60)
+      
+    print(paste("i=",i,sep=""))
+#     print(paste("j=",j,sep=""))
+#   }
+}
+extracted_48m<-extracted_48m[2:dim(extracted_48m)[1],]
+extracted_60m<-extracted_60m[2:dim(extracted_60m)[1],]
 
 
 
--- modules/data.remote/scripts/extract_palsar_metadata_function.R
+++ modules/data.remote/scripts/extract_palsar_metadata_function.R
@@ -33,53 +33,53 @@ scncf <- as.numeric(str_extract_all(scncf_raw[1,1],"\\(?[0-9,.-]+\\)?")[[1]])
 ## To convert a line (L) and pixel (P) position to the map projection frame of reference, say (E, N) where:
 ##   E = A11 + A12*L + A13*P + A14*L*P
 ##   N = A21 + A22*L + A23*P + A24*L*P
-options(digits=8)
-scn_pix2coord_a11 <- as.numeric(as.character(txtfile$V2[grep("a11", txtfile$V1)]))
-scn_pix2coord_a12 <- as.numeric(as.character(txtfile$V2[grep("a12", txtfile$V1)]))
-scn_pix2coord_a13 <- as.numeric(as.character(txtfile$V2[grep("a13", txtfile$V1)]))
-scn_pix2coord_a14 <- as.numeric(as.character(txtfile$V2[grep("a14", txtfile$V1)]))
-scn_pix2coord_a21 <- as.numeric(as.character(txtfile$V2[grep("a21", txtfile$V1)]))
-scn_pix2coord_a22 <- as.numeric(as.character(txtfile$V2[grep("a22", txtfile$V1)]))
-scn_pix2coord_a23 <- as.numeric(as.character(txtfile$V2[grep("a23", txtfile$V1)]))
-scn_pix2coord_a24 <- as.numeric(as.character(txtfile$V2[grep("a24", txtfile$V1)]))
-
-##Coefficients to convert a projection reference to a line & pixel
-## To convert from the map projection (E, N) of the pixel at the upper left to line (L) and pixel (P) position in the image, say (L, P) where: corner and (E, N) show alongitude (deg.) and a latitude
-##  L = B11 + B12*E + B13*N + B14*E*N 
-##  P = B21 + B22*E + B23*N + B24*E*N
-scn_coord2pix_b11 <- as.numeric(as.character(txtfile$V2[grep("b11", txtfile$V1)]))
-scn_coord2pix_b12 <- as.numeric(as.character(txtfile$V2[grep("b12", txtfile$V1)]))
-scn_coord2pix_b13 <- as.numeric(as.character(txtfile$V2[grep("b13", txtfile$V1)]))
-scn_coord2pix_b14 <- as.numeric(as.character(txtfile$V2[grep("b14", txtfile$V1)]))
-scn_coord2pix_b21 <- as.numeric(as.character(txtfile$V2[grep("b21", txtfile$V1)]))
-scn_coord2pix_b22 <- as.numeric(as.character(txtfile$V2[grep("b22", txtfile$V1)]))
-scn_coord2pix_b23 <- as.numeric(as.character(txtfile$V2[grep("b23", txtfile$V1)]))
-scn_coord2pix_b24 <- as.numeric(as.character(txtfile$V2[grep("b24", txtfile$V1)]))
-
-##More coordinates to convert dec degree LatLon to line and pixel
-##P = a0＋a1*N＋a2*E＋a3*N*E＋a4*N^2＋a5*E^2＋a6*N^2*E＋a7*N*E^2＋a8*N^3＋a9*E^3
-##L = b0＋b1*N＋b2*E＋b3*N*E＋b4*N^2＋b5*E^2＋b6*N^2*E＋b7*N*E^2＋b8*N^3＋b9*E^3
-
-scn_coord2pix_a0 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.0.", txtfile$V1)]))
-scn_coord2pix_a1 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.1.", txtfile$V1)]))
-scn_coord2pix_a2 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.2.", txtfile$V1)]))
-scn_coord2pix_a3 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.3.", txtfile$V1)]))
-scn_coord2pix_a4 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.4.", txtfile$V1)]))
-scn_coord2pix_a5 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.5.", txtfile$V1)]))
-scn_coord2pix_a6 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.6.", txtfile$V1)]))
-scn_coord2pix_a7 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.7.", txtfile$V1)]))
-scn_coord2pix_a8 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.8.", txtfile$V1)]))
-scn_coord2pix_a9 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.9.", txtfile$V1)]))
-scn_coord2pix_b0 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.0.", txtfile$V1)]))
-scn_coord2pix_b1 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.1.", txtfile$V1)]))
-scn_coord2pix_b2 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.2.", txtfile$V1)]))
-scn_coord2pix_b3 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.3.", txtfile$V1)]))
-scn_coord2pix_b4 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.4.", txtfile$V1)]))
-scn_coord2pix_b5 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.5.", txtfile$V1)]))
-scn_coord2pix_b6 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.6.", txtfile$V1)]))
-scn_coord2pix_b7 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.7.", txtfile$V1)]))
-scn_coord2pix_b8 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.8.", txtfile$V1)]))
-scn_coord2pix_b9 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.9.", txtfile$V1)]))
+# options(digits=8)
+# scn_pix2coord_a11 <- as.numeric(as.character(txtfile$V2[grep("a11", txtfile$V1)]))
+# scn_pix2coord_a12 <- as.numeric(as.character(txtfile$V2[grep("a12", txtfile$V1)]))
+# scn_pix2coord_a13 <- as.numeric(as.character(txtfile$V2[grep("a13", txtfile$V1)]))
+# scn_pix2coord_a14 <- as.numeric(as.character(txtfile$V2[grep("a14", txtfile$V1)]))
+# scn_pix2coord_a21 <- as.numeric(as.character(txtfile$V2[grep("a21", txtfile$V1)]))
+# scn_pix2coord_a22 <- as.numeric(as.character(txtfile$V2[grep("a22", txtfile$V1)]))
+# scn_pix2coord_a23 <- as.numeric(as.character(txtfile$V2[grep("a23", txtfile$V1)]))
+# scn_pix2coord_a24 <- as.numeric(as.character(txtfile$V2[grep("a24", txtfile$V1)]))
+# 
+# ##Coefficients to convert a projection reference to a line & pixel
+# ## To convert from the map projection (E, N) of the pixel at the upper left to line (L) and pixel (P) position in the image, say (L, P) where: corner and (E, N) show alongitude (deg.) and a latitude
+# ##  L = B11 + B12*E + B13*N + B14*E*N 
+# ##  P = B21 + B22*E + B23*N + B24*E*N
+# scn_coord2pix_b11 <- as.numeric(as.character(txtfile$V2[grep("b11", txtfile$V1)]))
+# scn_coord2pix_b12 <- as.numeric(as.character(txtfile$V2[grep("b12", txtfile$V1)]))
+# scn_coord2pix_b13 <- as.numeric(as.character(txtfile$V2[grep("b13", txtfile$V1)]))
+# scn_coord2pix_b14 <- as.numeric(as.character(txtfile$V2[grep("b14", txtfile$V1)]))
+# scn_coord2pix_b21 <- as.numeric(as.character(txtfile$V2[grep("b21", txtfile$V1)]))
+# scn_coord2pix_b22 <- as.numeric(as.character(txtfile$V2[grep("b22", txtfile$V1)]))
+# scn_coord2pix_b23 <- as.numeric(as.character(txtfile$V2[grep("b23", txtfile$V1)]))
+# scn_coord2pix_b24 <- as.numeric(as.character(txtfile$V2[grep("b24", txtfile$V1)]))
+# 
+# ##More coordinates to convert dec degree LatLon to line and pixel
+# ##P = a0＋a1*N＋a2*E＋a3*N*E＋a4*N^2＋a5*E^2＋a6*N^2*E＋a7*N*E^2＋a8*N^3＋a9*E^3
+# ##L = b0＋b1*N＋b2*E＋b3*N*E＋b4*N^2＋b5*E^2＋b6*N^2*E＋b7*N*E^2＋b8*N^3＋b9*E^3
+# 
+# scn_coord2pix_a0 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.0.", txtfile$V1)]))
+# scn_coord2pix_a1 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.1.", txtfile$V1)]))
+# scn_coord2pix_a2 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.2.", txtfile$V1)]))
+# scn_coord2pix_a3 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.3.", txtfile$V1)]))
+# scn_coord2pix_a4 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.4.", txtfile$V1)]))
+# scn_coord2pix_a5 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.5.", txtfile$V1)]))
+# scn_coord2pix_a6 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.6.", txtfile$V1)]))
+# scn_coord2pix_a7 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.7.", txtfile$V1)]))
+# scn_coord2pix_a8 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.8.", txtfile$V1)]))
+# scn_coord2pix_a9 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..a.9.", txtfile$V1)]))
+# scn_coord2pix_b0 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.0.", txtfile$V1)]))
+# scn_coord2pix_b1 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.1.", txtfile$V1)]))
+# scn_coord2pix_b2 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.2.", txtfile$V1)]))
+# scn_coord2pix_b3 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.3.", txtfile$V1)]))
+# scn_coord2pix_b4 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.4.", txtfile$V1)]))
+# scn_coord2pix_b5 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.5.", txtfile$V1)]))
+# scn_coord2pix_b6 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.6.", txtfile$V1)]))
+# scn_coord2pix_b7 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.7.", txtfile$V1)]))
+# scn_coord2pix_b8 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.8.", txtfile$V1)]))
+# scn_coord2pix_b9 <- as.numeric(as.character(txtfile$V3[grep("Map to line/sample coefficients..b.9.", txtfile$V1)]))
   
 ##Extract scene corner and center coordinates (UTM)
 scn_nwlat  <- as.numeric(as.character(workreport$V2[grep("Img_ImageSceneLeftTopLatitude",workreport$V1)]))
@@ -99,42 +99,6 @@ scn_vector <- c(scnid,
                 scnUTMzone, 
                 scnpix,
                 scncf,
-                scn_pix2coord_a11,
-                scn_pix2coord_a12,
-                scn_pix2coord_a13,
-                scn_pix2coord_a14,
-                scn_pix2coord_a21,
-                scn_pix2coord_a22,
-                scn_pix2coord_a23,
-                scn_pix2coord_a24,
-                scn_coord2pix_b11,
-                scn_coord2pix_b12,
-                scn_coord2pix_b13,
-                scn_coord2pix_b14,
-                scn_coord2pix_b21,
-                scn_coord2pix_b22,
-                scn_coord2pix_b23,
-                scn_coord2pix_b24,
-                scn_coord2pix_a0,
-                scn_coord2pix_a1,
-                scn_coord2pix_a2,
-                scn_coord2pix_a3,
-                scn_coord2pix_a4,
-                scn_coord2pix_a5,
-                scn_coord2pix_a6,
-                scn_coord2pix_a7,
-                scn_coord2pix_a8,
-                scn_coord2pix_a9,
-                scn_coord2pix_b0,
-                scn_coord2pix_b1,
-                scn_coord2pix_b2,
-                scn_coord2pix_b3,
-                scn_coord2pix_b4,
-                scn_coord2pix_b5,
-                scn_coord2pix_b6,
-                scn_coord2pix_b7,
-                scn_coord2pix_b8,
-                scn_coord2pix_b9,
                 scn_nwlat,          
                 scn_nwlon,
                 scn_nelat,   
