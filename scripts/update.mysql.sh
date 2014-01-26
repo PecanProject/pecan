2708fe5bdeaa25008c6cbda95494beca6acc4cf1
Rob Kooper <kooper@illinois.edu>
2014-01-26 14:44:10 -0600

    export CMD for addsites

--- scripts/update.mysql.sh
+++ scripts/update.mysql.sh
@@ -4,12 +4,15 @@
 cd $(dirname $0)/../..
 set -x
 
+# command to connect to database
+export CMD="mysql -u bety -pbety"
+
 # load latest dump of the database
 curl -o betydump.gz https://ebi-forecast.igb.illinois.edu/pecan/dump/betydump.mysql.gz
 
-echo "drop database if exists bety; create database bety;" | mysql -u bety -pbety
+echo "drop database if exists bety; create database bety;" | ${CMD}
 
-gunzip -c betydump.gz | grep -v 'DEFINER' | mysql -u bety -pbety bety
+gunzip -c betydump.gz | grep -v 'DEFINER' | ${CMD} bety
 rm betydump.gz
 
 # remove old runs
@@ -30,5 +33,5 @@ else
 	    ('ED2.2', 'ED2', '${HOSTNAME}:/usr/local/bin/ed2.r46', '46', NOW(), NOW()),
 	    ('ED2.2', 'ED2', '${HOSTNAME}:/usr/local/bin/ed2.r82', '82', NOW(), NOW()),
 	    ('SIPNET', 'SIPNET', '${HOSTNAME}:/usr/local/bin/sipnet.runk', 'unk', NOW(), NOW()),
-	    ('BIOCRO', 'BIOCRO', '${HOSTNAME}:/bin/true', '0.0.1', NOW(), NOW());" | mysql -u bety -pbety bety
+	    ('BIOCRO', 'BIOCRO', '${HOSTNAME}:/bin/true', '0.0.1', NOW(), NOW());" | ${CMD} bety
 fi
--- scripts/update.psql.sh
+++ scripts/update.psql.sh
@@ -4,13 +4,16 @@
 cd $(dirname $0)/../..
 set -x
 
+# command to connect to database
+export CMD="psql -U bety"
+
 # load latest dump of the database
 curl -o betydump.gz http://isda.ncsa.illinois.edu/~kooper/EBI/betydump.psql.gz
 
-dropdb bety
-createdb -O bety bety
+sudo -u postgres dropdb bety
+sudo -u postgres createdb -O bety bety
 
-gunzip -c betydump.gz | psql -U bety bety
+gunzip -c betydump.gz | ${CMD} bety
 rm betydump.gz
 
 # remove old runs
@@ -31,5 +34,5 @@ else
 	    ('ED2.2', 'ED2', '${HOSTNAME}:/usr/local/bin/ed2.r46', '46', NOW(), NOW()),
 	    ('ED2.2', 'ED2', '${HOSTNAME}:/usr/local/bin/ed2.r82', '82', NOW(), NOW()),
 	    ('SIPNET', 'SIPNET', '${HOSTNAME}:/usr/local/bin/sipnet.runk', 'unk', NOW(), NOW()),
-	    ('BIOCRO', 'BIOCRO', '${HOSTNAME}:/bin/true', '0.0.1', NOW(), NOW());" | psql -U bety bety
+	    ('BIOCRO', 'BIOCRO', '${HOSTNAME}:/bin/true', '0.0.1', NOW(), NOW());" | ${CMD} bety
 fi
