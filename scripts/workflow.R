b028f76e0e331ecfa00f54f1c7acfe5f5ca6f678
Rob Kooper <kooper@illinois.edu>
2014-02-12 15:11:30 -0800

    add status file to workflow.R (redmine #1924)

--- scripts/workflow.R
+++ scripts/workflow.R
@@ -6,6 +6,22 @@
 # which accompanies this distribution, and is available at
 # http://opensource.ncsa.illinois.edu/license.html
 #-------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------#
+# functions used to write STATUS used by history
+#--------------------------------------------------------------------------------#
+options(warn = 1, keep.source = TRUE, error = quote({
+            status.end("ERROR")
+         }))
+
+status.start <- function(name) {
+  cat(paste(name, format(Sys.time(), "%F %T"), sep="\t"), file=file.path(settings$outdir, "STATUS"), append=TRUE)      
+}
+
+status.end <- function(status="DONE") {
+  cat(paste("", format(Sys.time(), "%F %T"), status, "\n", sep="\t"), file=file.path(settings$outdir, "STATUS"), append=TRUE)      
+}
+
 #---------------- Load libraries. -----------------------------------------------------------------#
 require(PEcAn.all)
 #--------------------------------------------------------------------------------------------------#
@@ -15,37 +31,69 @@ require(PEcAn.all)
 settings <- read.settings()
 #--------------------------------------------------------------------------------------------------#
 
+# start with a clean status
+unlink(file.path(settings$outdir, "STATUS"))
+
 #---------------- Run PEcAn workflow. -------------------------------------------------------------#
 # Query the trait database for data and priors
+status.start("TRAIT")
 settings$pfts <- get.trait.data(settings$pfts, settings$run$dbfiles, settings$database, settings$meta.analysis$update)
+status.end()
 
 # Run the PEcAn meta.analysis
+status.start("META")
 run.meta.analysis(settings$pfts, settings$meta.analysis$iter, settings$run$dbfiles, settings$database)
+status.end()
 
 # Calls model specific write.configs e.g. write.config.ed.R
+status.start("CONFIG")
 run.write.configs(settings$model$name, settings$bety$write)
-
-# Clear any old model output in ebi-cluster scratch/$USER on worker nodes
-clear.scratch(settings)
+status.end()
 
 # Start ecosystem model runs
+status.start("MODEL")
 start.model.runs(settings$model$name, settings$bety$write)
+status.end()
 
 # Convert output
+status.start("OUTPUT")
 convert.outputs(settings$model$name, settings)
+
+# special for web, print all nc vars
+data(mstmip_vars, package="PEcAn.utils")
+for (runid in readLines(con=file.path(settings$rundir, "runs.txt"))) {
+  for(file in list.files(path=file.path(settings$modeloutdir, runid), pattern="*.nc")) {
+    nc <- nc_open(file.path(settings$modeloutdir, runid, file))
+    for(v in sort(names(nc$var))) {
+      name <- mstmipvar(v, silent=TRUE)['longname']
+      cat(paste(v, name), file=file.path(settings$modeloutdir, runid, paste(file, "var", sep=".")), append=TRUE, sep="\n")
+    }
+    nc_close(nc)
+  }
+}
 
 # Get results of model runs
 get.model.output(settings$model$name, settings)
+status.end()
 
 # Run sensitivity analysis and variance decomposition on model output
+status.start("SENSITIVITY")
 run.sensitivity.analysis()
+status.end()
 
 # Run ensemble analysis on model output. 
+status.start("ENSEMBLE`")
 run.ensemble.analysis()
 
 # OPTIONAL: to get an esemble time-series output for the target variables set in the PEcAn.xml file
 #run.ensemble.analysis(plot.timeseries=TRUE)
+status.end()
 
 ### PEcAn workflow run complete
+status.start("FINISHED")
+if (settings$workflow$id != 'NA') {
+  query.base(paste("UPDATE workflows SET finished_at=NOW() WHERE id=", settings$workflow$id, "AND finished_at IS NULL"))
+}
+status.end()
 print("---------- PEcAn Workflow Complete ----------")
 #--------------------------------------------------------------------------------------------------#
