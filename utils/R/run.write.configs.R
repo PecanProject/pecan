9c0e46c494b26607d9cf209387d2f88353e8a703
David LeBauer <dlebauer@gmail.com>
2014-03-17 22:51:43 -0500

    extracted get.samples from run.write.configs
    get.samples is useful with meta-analysis, independent of running a particular model. this change should isolate these two functions. However, it would require either a) workflows specifically call `get.parameter.samples` or this function be added to the `run.meta.analysis` function

--- utils/R/run.write.configs.R
+++ utils/R/run.write.configs.R
@@ -36,77 +36,9 @@ run.write.configs <- function(model, write = TRUE) {
   
   # TODO RK : need to write to runs_inputs table
   
-  ### Identify PFTs in the input settings.xml file
-  num.pfts <- length(settings$pfts)
-  pft.names <- list()
-  outdirs <- list()
-  for (i.pft in 1:num.pfts){
-    pft.names[i.pft] <- settings$pfts[i.pft]$pft$name
-    
-    ### If no PFT(s) are specified insert NULL to warn user 
-    if(length(pft.names)==0) pft.names[1] <- "NULL" 
-    
-    ### Get output directory info
-    outdirs[i.pft] <- settings$pfts[i.pft]$pft$outdir
-    
-  } ### End of for loop to extract pft names
-  
-  logger.info("Selected PFT(s): ", pft.names)
-  
-  ## Generate empty list arrays for output.
-  trait.samples <- sa.samples <- ensemble.samples <- env.samples <- runs.samples <- list()
-  
   ## Prepare for model output.  Cleanup any old config files (if exists)
   do.call(paste("remove.config", model, sep="."), args = list(settings$rundir, settings))
 
-  ## Load PFT priors and posteriors
-  for (i in seq(pft.names)){
-
-    ## Load priors
-    load(file.path(outdirs[i], 'prior.distns.Rdata'))
-    
-    ### Load trait mcmc data (if exists)
-    if("trait.mcmc.Rdata" %in% dir(unlist(outdirs))) {
-      ma.results <- TRUE
-      load(file.path(outdirs[i], 'trait.mcmc.Rdata'))
-    }
-    
-    pft.name <- unlist(pft.names[i])
-    
-    ### When no ma for a trait, sample from  prior
-    ### Trim all chains to shortest mcmc chain, else 20000 samples
-    priors <- rownames(prior.distns)
-    if(exists('trait.mcmc')) {
-      ma.traits <- names(trait.mcmc)
-      samples.num <- min(sapply(trait.mcmc, function(x) nrow(as.matrix(x))))
-      
-      ## report which traits use MA results, which use priors 
-      if(length(ma.traits) > 0){
-        logger.info("PFT",  pft.names[i], "has meta analysis results for:\n", paste0(ma.traits, collapse = "\n "))        
-      }
-      if(!all(priors %in% ma.traits)){
-        logger.info("PFT", pft.names[i], "will use prior distributions for:\n", paste0(priors[!priors %in% ma.traits], collapse = "\n "))        
-      }
-    } else {
-      ma.traits <- NULL
-      samples.num <- 20000
-      logger.info("No meta analysis results for PFT",  pft.names[i])  
-      logger.info("PFT", pft.names[i], "will use prior distributions for", priors )
-    }
-
-
-    logger.info("using ", samples.num, "samples per trait")
-    for (prior in priors) {
-      if (prior %in% ma.traits) {
-        samples <- as.matrix(trait.mcmc[[prior]][,'beta.o'])
-      } else {
-        samples <- get.sample(prior.distns[prior,], samples.num)
-      }
-      trait.samples[[pft.name]][[prior]] <- samples
-    }
-    
-  } ### End for loop
-  
   ### NEED TO IMPLEMENT: 
   ## Load Environmental Priors and Posteriors
   
@@ -162,12 +94,75 @@ run.write.configs <- function(model, write = TRUE) {
       logger.info('not writing config files for ensemble, settings are NULL')
   } ### End of Ensemble
   logger.info("\n  ######################## Finished writing model run config files ########################")
-  ### Save output from SA/Ensemble runs
-  save(ensemble.samples, trait.samples, sa.samples, runs.samples, file = file.path(settings$outdir, 'samples.Rdata'))
 }
 #==================================================================================================#
 
 
+##' Convert priors / MCMC samples to chains that can be sampled for model parameters 
+##' 
+##' @name get.parameter.samples
+##' @title Sample from priors or posteriors
+##' @param pfts the pfts node of the list of pecan settings
+##' @export
+##'
+##' @author David LeBauer, Shawn Serbin
+get.parameter.samples <- function(pfts = settings$pfts){
+  
+  trait.samples <- sa.samples <- ensemble.samples <- list()
+  env.samples <- runs.samples <- ma.samples <- list()
+  for (i in seq(pfts)){
+    pft.name <- pfts[[i]]$name
+    trait.samples[[pft.name]] <- list()
+    ma.samples[[pft.name]] <- list()
+    ## Load priors
+    load(file.path(pfts[[i]]$outdir, 'prior.distns.Rdata'))
+    
+    ### Load trait mcmc data (if exists)
+    if("trait.mcmc.Rdata" %in% dir(pfts[[i]]$outdir)) {
+      ma.results <- TRUE
+      load(file.path(pfts[[i]]$outdir, 'trait.mcmc.Rdata'))
+    }
+    
+
+    ### When no ma for a trait, sample from  prior
+    ### Trim all chains to shortest mcmc chain, else 20000 samples
+    priors <- rownames(prior.distns)
+    if(exists('trait.mcmc')) {
+      ma.traits <- names(trait.mcmc)
+      samples.num <- min(sapply(trait.mcmc, function(x) nrow(as.matrix(x))))
+      
+      ## report which traits use MA results, which use priors 
+      if(length(ma.traits) > 0){
+        logger.info("PFT",  pft.name, "has meta analysis results for:\n", 
+                    paste0(ma.traits, collapse = "\n "))        
+      }
+      if(!all(priors %in% ma.traits)){
+        logger.info("PFT", pft.name, "will use prior distributions for:\n", 
+                    paste0(priors[!priors %in% ma.traits], collapse = "\n "))        
+      }
+    } else {
+      ma.traits <- NULL
+      samples.num <- 20000
+      logger.info("No meta analysis results for PFT",  pft.name)  
+      logger.info("PFT", pft.name, "will use prior distributions for", priors )
+    }
+    
+    
+    logger.info("using ", samples.num, "samples per trait")
+    for (prior in priors) {
+      if (prior %in% ma.traits) {
+        samples <- as.matrix(trait.mcmc[[prior]][,'beta.o'])
+        ma.samples[[pft.name]][[prior]] <- as.matrix(trait.mcmc[[prior]])
+      } else {
+        samples <- get.sample(prior.distns[prior,], samples.num)
+      }
+      trait.samples[[pft.name]][[prior]] <- samples
+    }
+    
+  } ### End for loop
+  save(ensemble.samples, trait.samples, sa.samples, runs.samples, 
+       file = file.path(settings$outdir, 'samples.Rdata'))
+}
 ####################################################################################################
 ### EOF.  End of R script file.          		
 ####################################################################################################
