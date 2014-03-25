b420a20343020233c83388d7484d0b66da27bb1f
Rob Kooper <kooper@illinois.edu>
2014-03-24 21:55:57 -0700

    converted workflow.R into r script.

--- tests/workflow.R
+++ tests/workflow.R
@@ -1,50 +1,15 @@
+#!/usr/bin/Rscript
+
 ## See README in tests/ folder for details
 require("PEcAn.all")
 
-# ----------------------------------------------------------------------
-# debug options
-# ----------------------------------------------------------------------
-options(warn = 1, keep.source = TRUE, error =
-          quote({
+#--------------------------------------------------------------------------------#
+# functions used to write STATUS used by history
+#--------------------------------------------------------------------------------#
+options(warn = 1, keep.source = TRUE, error = quote({
             status.end("ERROR")
+         }))
 
-            db.print.connections()
-            cat("Environment:\n", file=stderr());
-            
-            # TODO: setup option for dumping to a file (?)
-            # Set `to.file` argument to write this to a file for post-mortem debugging
-            dump.frames();  # writes to last.dump
-            
-            #
-            # Debugging in R
-            #   http://www.stats.uwo.ca/faculty/murdoch/software/debuggingR/index.shtml
-            #
-            # Post-mortem debugging
-            #   http://www.stats.uwo.ca/faculty/murdoch/software/debuggingR/pmd.shtml
-            #
-            # Relation functions:
-            #   dump.frames
-            #   recover
-            # >>limitedLabels  (formatting of the dump with source/line numbers)
-            #   sys.frame (and associated)
-            #   traceback
-            #   geterrmessage
-            #
-            # Output based on the debugger function definition.
-            
-            n <- length(last.dump)
-            calls <- names(last.dump)
-            cat(paste("  ", 1L:n, ": ", calls, sep = ""), sep = "\n", file=stderr())
-            cat("\n", file=stderr())
-            
-            if (!interactive()) {
-              q()
-            }
-          }))
-
-# ----------------------------------------------------------------------
-# status functions
-# ----------------------------------------------------------------------
 status.start <- function(name) {
   print("----------------------------------------------------------------------")
   print(paste("STARTING", name))
