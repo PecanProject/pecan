7e5b6383c2cd1569a8e83b2bd9496157c74e9b9b
Josh Mantooth <jam2767@gmail.com>
2014-04-03 13:03:27 -0400

    added conversion for U and V, precip, and RH to SH. Unresolved error in code that is supposed to write new netCDF variables to an existing file.

--- modules/data.atmosphere/inst/scripts/met2CF.Ameriflux.R
+++ modules/data.atmosphere/inst/scripts/met2CF.Ameriflux.R
@@ -49,50 +49,60 @@ met2CF.Ameriflux <- function(in.path,in.prefix,outfolder){
     ncatt_put(nc=nc,varid='TA',attname='units',attval='degrees K') 
     #can do the same for long_name but server that hosts netCDF 
     #CF standard names is down
-    
     nc <- ncvar_rename(nc=nc,'TA','air_temperature')
-    nc <- ncvar_rename(nc=nc,'WS','wind_speed')
     
+    #convert wind speed and wind direction to U and V
+    #assumes we have both wind speed and direction at the same time 
+    ws <- ncvar_get(nc=nc,varid='WD') #wind direction
+    wd <- ncvar_get(nc=nc,varid='WS') #wind speed
+    ws.sub <- which(ws > -6999)
+    wd.sub <- wd[ws.sub] #use wind direction coincident with windspeed
+    u <- ws[ws.sub]*cos(wd.sub*(pi/180))
+    v <- ws[ws.sub]*sin(wd.sub*(pi/180))
+    nc <- ncvar_rename(nc=nc,'WS','wind_speed') #CF name
+    
+    #create new netCDF variables u and v
+
+    
+    #convert air pressure to CF standard
     press <- ncvar_get(nc=nc,varid="PRESS")
     press.pa <- which(press > -6999)
     press.new <- press[press.pa] * 1000 #kilopascals to pascals
     press <- replace(x=press,list=press.pa,values=press.new)
     ncvar_put(nc=nc, varid='PRESS',vals=press)
     ncatt_put(nc=nc,varid='PRESS',attname='units',attval='Pa') 
-    
     nc <- ncvar_rename(nc=nc,'PRESS','air_pressure')
+    
     nc <- ncvar_rename(nc=nc,'Rg','surface_downwelling_shortwave_flux')
     nc <- ncvar_rename(nc=nc,'Rgl','surface_downwelling_longwave_flux')
+    
+    #convert precipitation to CF standard
+    dtime <- ncvar_get(nc=nc,varid="DTIME")
+    sec <- 0.02083/30 #0.02083 DTIME = 30 minutes
+    timestep <- round(x=mean(diff(dtime))/sec,digits=1) #round to nearest 0.1 second
+    prec <- ncvar_get(nc=nc,varid="PREC")
+    prec.sub <- which(prec > -6999)
+    prec.new <- prec[prec.sub]/timestep #mm/s = kg/m2/s
+    prec <- replace(x=prec,list=prec.sub,values=prec.new)
+    ncvar_put(nc=nc, varid='PREC',vals=prec)
+    ncatt_put(nc=nc,varid='PREC',attname='units',attval='Kg m-2 s-1') 
     nc <- ncvar_rename(nc=nc,'PREC','precipitation_flux')
     
-    # SH still hasn't been calculated
-    # cannot delete variables using ncdf4
-    # can build new files, but need to recreate the dimensions, variables and then create file
-    # can we just delete what we don't need using nco?
-#     nc.new <- nc_create(filename=paste(in.prefix,02004,sep="."),
-#               vars=c(ncvar_get(nc=nc,varid='air_temperature'),
-#                      ncvar_get(nc=nc,varid='wind_speed'),
-#                      ncvar_get(nc=nc,varid='air_pressure'),
-#                      ncvar_get(nc=nc,varid='surface_downwelling_shortwave_flux'),
-#                      ncvar_get(nc=nc,varid='surface_downwelling_longwave_flux'),
-#                      ncvar_get(nc=nc,varid='precipitation_flux'),
-#                      ncvar_get(nc=nc,varid='YEAR'),
-#                      ncvar_get(nc=nc,varid='GAP'),
-#                      ncvar_get(nc=nc,varid='DTIME'),
-#                      ncvar_get(nc=nc,varid='DOY'),
-#                      ncvar_get(nc=nc,varid='HRMIN'),
-#                      ncvar_get(nc=nc,varid='UST'),
-#                      ncvar_get(nc=nc,varid='WD'),
-#                      ncvar_get(nc=nc,varid='TS1'),
-#                      ncvar_get(nc=nc,varid='TSdepth1'),
-#                      ncvar_get(nc=nc,varid='TS2'),
-#                      ncvar_get(nc=nc,varid='TSdepth2'),
-#                      ncvar_get(nc=nc,varid='RH'),
-#                      ncvar_get(nc=nc,varid='VPD'),
-#                      ncvar_get(nc=nc,varid='SWC1'),
-#                      ncvar_get(nc=nc,varid='SWC2'),
-#                      ncvar_get(nc=nc,varid='PAR')
-#                      ))
+    
+    # convert RH to SH
+    rh <- ncvar_get(nc=nc,varid='RH')
+    rh.sub <- which(rh > -6999) #select non-missing data
+    rh.sh <- as.vector(rh.sub/100) #percent to proportion: needed for conversion
+    rh.sh <- replace(x=rh,list=rh.sub,values=rh.sh) #insert proportion values into RH vector
+    ta.rh <- ta[rh.sub] # use T coincident with RH
+    sh.miss <- rh2rv(rh=rh.sh[rh.sub],T=ta.rh) #conversion, doesn't include missvals
+    sh <- replace(x=rh,list=rh.sub,values=sh.miss) #insert Kelvin values into vector
+#     sh.dim <- ncdim_def(name='specific_humidity',units='ratio',vals=sh,
+#                         create_dimvar=TRUE,longname='specific humidity') #define netCDF dimension
+#     sh.var <- ncvar_def(name='specific_humidity',units='ratio',dim=sh.dim) #define netCDF variable, doesn't include longname and comments
+#     ncvar_add(nc=nc,v=sh.var,indefine=TRUE,verbose=TRUE) #add variable to existing netCDF file
+#     this doesn't work yet
+
   
   }  ## end loop over files
  
