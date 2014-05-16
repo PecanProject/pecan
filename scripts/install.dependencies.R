8d0d57f9fd8ee70c0828b3863ecabd137edddeb9
Rob Kooper <kooper@illinois.edu>
2014-05-16 14:07:50 -0700

    install postgresql driver

--- scripts/install.dependencies.R
+++ scripts/install.dependencies.R
@@ -12,21 +12,10 @@ list.of.packages <- c('abind', 'car', 'chron', 'coda', 'data.table', 'doSNOW', '
                       'knitr', 'lubridate', 'MASS', 'MCMCpack', 'mvtnorm', 'ncdf4',
                       'plotrix', 'plyr', 'raster', 'randtoolbox', 'rjags',
                       'rgdal', 'tgp', 'DBI', 'roxygen2', 'stringr', 'testthat',
-                      'XML', 'RNCEP', 'foreign', 'RCurl', 'udunits2')
+                      'XML', 'RNCEP', 'foreign', 'RCurl', 'udunits2', 'RPostgreSQL')
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) {
   print("installing : ")
   print(new.packages)
   install.packages(new.packages, repos="http://cran.rstudio.com/")
-}
-
-
-# Check for R database drivers.
-if (!any(c('RMySQL', 'RPostgreSQL') %in% installed.packages()[,"Package"])) {
-  print("No database drivers installed, please select appropriate driver")
-  print("RMySQL      : for MySQL database")
-  print("RPostgreSQL : for PostgreSQL database")
-  print("For PEcAn, PostgreSQL is recommended")
-  print("You can install the driver using the following command in R.")
-  print("install.packages('DRIVER', repos='http://cran.rstudio.com/')")
 }
