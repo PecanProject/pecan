a2860a99ed7bc9d95dc257a3d120ea67310f4700
Mike Dietze <dietze@bu.edu>
2014-05-17 08:52:49 -0400

    met2CF.csv - added debug flag and implemented air_temperature, air_pressure, and precipitation_flux

--- modules/data.atmosphere/R/met2CF.csv.R
+++ modules/data.atmosphere/R/met2CF.csv.R
@@ -13,12 +13,13 @@ if(FALSE){
   in.path = "~/Downloads/"
   in.file = "WR_E"
   outfolder = "/tmp/"
-  format = data.frame(orig="TA",units="celsius",bety="airT")  
+  format = data.frame(orig=c("TA","PRECIP"),units=c("celsius","mm"),bety=c("airT","precipitation_flux"))  
   lat = 40
   lon = -80
 }
 
 met2CF.csv(in.path,in.file,outfolder,format,lat=NULL,lon=NULL){
+  debug=TRUE
   require(ncdf4)
   require(udunits2)
   require(PEcAn.utils)
@@ -40,35 +41,45 @@ met2CF.csv(in.path,in.file,outfolder,format,lat=NULL,lon=NULL){
 #    dat <- read.csv(files[i],skip=17,na.strings=c(-9999,-6999)) #example file
     dat <- read.csv(files[i],skip=7,na.strings=c(-9999,-6999,9999),as.is=TRUE) #example file
     units <- dat[1,]
-    dat <- dat[-1,]
+    dat <- as.data.frame(sapply(dat[-1,],as.numeric))
     
-    ### Variable defintions
-#    vars <- c("WS","TA","PRESS","Rg","Rgl","PREC")
-#    new.vars <- c("wind_speed","air_temperature","air_pressure",
-#              "surface_downwelling_shortwave_flux",
-#              "surface_downwelling_longwave_flux","precipitation_flux") #netCDF CF long variable names
-
- #   names(dat)[names(dat) %in% vars] <- new.vars #replace column names with netCDF CF names
-
     ### time 
 ## HACK!!!
-    tdim = ncdim_def("time","day of year",seq(0,365,length=nrow(dat))) #define netCDF dimensions for variables
+    tdim = ncdim_def("time","day of year",seq(0,365,length=nrow(dat)),verbose=debug) #define netCDF dimensions for variables
 
 
     ## create new netCDF file
-    x = ncdim_def("Lon","degreesE",lon) #define netCDF dimensions for variables
-    y = ncdim_def("Lat","degreesN",lat)
-    co2.var = ncvar_def(name="CO2",units="ppm",dim=list(x,y))
-    nc_create(new.file, vars=co2.var) #create netCDF file
+    x = ncdim_def("Lon","degreesE",lon,verbose=debug) #define netCDF dimensions for variables
+    y = ncdim_def("Lat","degreesN",lat,verbose=debug)
+    co2.var = ncvar_def(name="CO2",units="ppm",dim=list(x,y),verbose=debug)
+    nc = nc_create(new.file, vars=co2.var,verbose=debug) #create netCDF file
 
-    ## air_temperature
+    ## air_temperature / airT
     if("airT" %in% format$bety){
       k = which(format$bety=="airT")
-      airT.var = ncvar_def(name="air_temperature",units="K",dim=tdim)
-      nc = ncvar_add(nc=nc,v=airT.var,verbose=TRUE) #add variable to existing netCDF file
-      ncvar_put(nc,varid='specific_humidity',
-                vals=met.conv(dat[,as.character(format$orig[k])],format$units[k],"celsius","K"))
-      
+      airT.var = ncvar_def(name="air_temperature",units="K",dim=tdim,verbose=debug)
+      nc = ncvar_add(nc=nc,v=airT.var,verbose=debug) #add variable to existing netCDF file
+      ncvar_put(nc,varid='air_temperature',
+                vals=met.conv(dat[,as.character(format$orig[k])],format$units[k],"celsius","K"))  
+    }
+
+    ## air_pressure / Psurf
+    if("air_pressure" %in% format$bety){
+      k = which(format$bety=="air_pressure")
+      Psurf.var = ncvar_def(name="air_pressure",units="Pa",dim=tdim,verbose=debug)
+      nc = ncvar_add(nc=nc,v=Psurf.var,verbose=debug) #add variable to existing netCDF file
+      ncvar_put(nc,varid='air_pressure',
+            vals=met.conv(dat[,as.character(format$orig[k])],format$units[k],"Pa","Pa"))
+  
+    }
+
+    ## precipitation_flux / rain
+    if("precipitation_flux" %in% format$bety){
+      k = which(format$bety=="precipitation_flux")
+      rain.var = ncvar_def(name="precipitation_flux",units="Kg/m2/s",dim=tdim,verbose=debug)
+      nc = ncvar_add(nc=nc,v=rain.var,verbose=debug) #add variable to existing netCDF file
+      ncvar_put(nc,varid='precipitation_flux',
+            vals=met.conv(dat[,as.character(format$orig[k])],format$units[k],"Kg/m2/s","Kg/m2/s"))  
     }
 
     uwind  = ncvar_def(name="eastward_wind",units="m s-1",dim) #define netCDF variables
@@ -97,6 +108,7 @@ met.conv <- function(x,orig,bety,CF){
   orig = as.character(orig)
   bety = as.character(bety)
   CF   = as.character(CF)
+  if(nchar(orig) == 0) orig = bety; ## if units not provided, default is that they were the same units as bety
   if(ud.is.parseable(orig)){
     if(ud.are.convertible(orig,bety)){
       return(ud.convert(ud.convert(x,orig,bety),bety,CF))
