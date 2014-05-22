3a1e7d77fd173aefcd29ac7127a7428cf8da9111
Betsy Cowdery <emcowdery@gmail.com>
2014-05-21 20:11:14 -0400

    ongoing changes

--- modules/data.atmosphere/inst/scripts/met.workflow.NARR.R
+++ modules/data.atmosphere/inst/scripts/met.workflow.NARR.R
@@ -4,44 +4,37 @@ require(RPostgreSQL)
 require(ncdf4)
 #--------------------------------------------------------------------------------------------------#
 
+# For initial NARR setup, run download.NARR.R on geo
 # Update NARR from the internet
 outfolder <- "/projectnb/cheas/pecan.data/input/NARR/"
 start_year <- 1979
 end_year   <- 2013
-met2cf.NARR(outfolder,start_year,end_year) 
 
+download.NARR(outfolder,start_year,end_year) 
+
+#--------------------------------------------------------------------------------------------------#
 # Update NARR_CF
-system(paste("pecan/modules/data.atmosphere/inst/scripts/nc_formatting.sh")) ## How to have it find the path?
+input.id  <-  285
+outfolder <- "/projectnb/cheas/pecan.data/input/NARR_CF/"
+pkg       <- "PEcAn.data.atmosphere"
+fcn       <- "met2cf.NARR"
+write     <-  FALSE
+username  <- ""
 
+convert.input(input.id,outfolder,pkg,fcn,write,username)
+
+#--------------------------------------------------------------------------------------------------#
 # Extract for location
+input.id <-  288
+newsite  <-  768
 
-input.id = 288
+outfolder <-  paste0("/projectnb/cheas/pecan.data/input/NARR_CF_site_",newsite,"/")
+pkg       <- "PEcAn.data.atmosphere"
+fcn       <- "extract.NARR"
+write     <-  FALSE
+username  <- ""
 
-newsite = 768
-year = TRUE
-
-outfolder = paste0("/projectnb/cheas/pecan.data/input/NARR_CF_site_",newsite,"/")
-pkg = "PEcAn.data.atmosphere"
-fcn = "extract.NARR"
-write = FALSE
-username = "ecowdery"
-
-convert.input (input.id,outfolder,pkg,fcn,write,username,...)
-
-
-
-
-
-
-
-
-
-if point == TRUE{
-	# one coordinate
-	pnt.input <- function(input.id,outfolder,pkg,fcn,write,username,newsite,year=TRUE)
-	}else{
-		# deal with region here
-	}
+convert.input (input.id,outfolder,pkg,fcn,write,username,newsite)
 
 
 
