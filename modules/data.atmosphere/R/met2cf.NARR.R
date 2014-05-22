012059d006ec00ed29034a4b3973c036652adb7d
Betsy Cowdery <emcowdery@gmail.com>
2014-05-21 20:10:17 -0400

    download.NARR.R and met2cf.NARR.R made serparate

--- modules/data.atmosphere/R/met2cf.NARR.R
+++ modules/data.atmosphere/R/met2cf.NARR.R
@@ -1,24 +1,8 @@
-met2cf.NARR <- function(outfolder,start_year,end_year){
+met2cf.NARR <- function(in.path,in.prefix,outfolder){
 
-# Defaults
-start_year <- 1979 
-end_year <- 2013
-outfolder <- "/projectnb/cheas/pecan.data/input/NARR/"
+  cmdArgs = paste(c(in.path,in.prefix,outfolder),collapse=" ")
+  
+  fcn = system.file("inst/scripts/nc_formatting.sh", package = "PEcAn.data.atmosphere")
+  system(paste(fcn,cmdArgs))
 
-
-vlist <- c("pres.sfc", "dswrf", "dlwrf", "air.2m", "shum.2m", "prate","vwnd.10m","uwnd.10m")
-
-# system(paste("module load netcdf"))
-
-# Get original Data
-for (v in vlist){
-  for (year in seq(end_year,start_year,by=-1)){
-    system(paste("wget -c -P ", outfolder ," ftp://ftp.cdc.noaa.gov/Datasets/NARR/monolevel/",v,".", year,".nc",sep=""))
-  }    
 }
-
-# system(paste("cd ", outfolder))
-# system(paste("/projectnb/cheas/gapmacro/NARR/NewNARR/nc_formatting.sh"))
-}
-
-
