5000027bcffeecbb3f4828248417785937c2e6eb
Betsy Cowdery <emcowdery@gmail.com>
2014-05-29 19:33:13 -0400

    sync

--- scripts/Rfcn.R
+++ scripts/Rfcn.R
@@ -6,8 +6,6 @@
 
 args <- commandArgs(trailingOnly = TRUE)
 
-print(args)
-
 ## check args
 if(length(args)<2){
   print(c("Insufficient args",args))
@@ -33,8 +31,6 @@ if(exists(args[2])){
     }
   }
 
-  print(fcn.args)
-
   ## call function
-  do.call(args[2], fcn.args) 
+  output <- do.call(args[2], fcn.args) 
 }
