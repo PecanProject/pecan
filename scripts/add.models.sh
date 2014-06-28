d9884edbe5280fc0f963a18c14ee24eeea02b5c8
Rob Kooper <kooper@illinois.edu>
2014-06-28 08:47:50 -0700

    spinet should be called sipnet.runk

--- scripts/add.models.sh
+++ scripts/add.models.sh
@@ -39,6 +39,6 @@ addmodel() {
 # Add models
 addmodel "${FQDN}" "ED2.2" "ED2" "46" "ed2.r46" "/usr/local/bin"
 addmodel "${FQDN}" "ED2.2" "ED2" "82" "ed2.r82" "/usr/local/bin"
-addmodel "${FQDN}" "SIPNET" "SIPNET" "unk" "sipnet.unk" "/usr/local/bin"
+addmodel "${FQDN}" "SIPNET" "SIPNET" "unk" "sipnet.runk" "/usr/local/bin"
 addmodel "${FQDN}" "BioCro" "BIOCRO" "" "true" "/bin"
 
