9a42312a4bff0424ce5af34e62db79179d40271e
Betsy Cowdery <emcowdery@gmail.com>
2014-06-30 21:56:12 -0400

    Continued work to get met2model.ED2 working with convert.input

--- modules/data.atmosphere/inst/scripts/met.workflow.NARR.R
+++ modules/data.atmosphere/inst/scripts/met.workflow.NARR.R
@@ -1,9 +1,12 @@
 #---------------- Load libraries. -----------------------------------------------------------------#
 require(PEcAn.all)
 require(RPostgreSQL)
+
+for (i in dbListConnections(PostgreSQL())) db.close(i)
+
 #--------------------------------------------------------------------------------------------------#
 # Download raw NARR from the internet 
-
+rm(list = setdiff(ls(), lsf.str()))
 outfolder  <- "/projectnb/cheas/pecan.data/input/NARR/"
 start_year <- 1979
 end_year   <- 2013
@@ -15,6 +18,7 @@ NARR_raw.id <- raw.NARR(outfolder,start_year,end_year,pkg,NARR.host)
 
 #--------------------------------------------------------------------------------------------------#
 # Update NARR_CF
+# input.id  <-  285
 input.id  <-  NARR_raw.id
 outfolder <- "/projectnb/cheas/pecan.data/input/NARR_CF/"
 pkg       <- "PEcAn.data.atmosphere"
@@ -37,7 +41,7 @@ NARR_perm.id <- permute.nc(input.id,outfolder,write)
 #--------------------------------------------------------------------------------------------------#
 # Extract for location
 input.id <- NARR_perm.id
-newsite  <- 1161
+newsite  <- 766
 str_ns   <- paste0(newsite %/% 1000000000, "-", newsite %% 1000000000)
 
 outfolder <- paste0("/projectnb/cheas/pecan.data/input/NARR_CF_site_",str_ns,"/")
@@ -62,12 +66,10 @@ lst <- GNtimezone(sc[[1]],sc[[2]], radius = 0)$dstOffset
 input.id <- NARR_extract.id
 
 outfolder <- paste0("/projectnb/cheas/pecan.data/input/NARR_ED_site_",str_ns,"/")
-pkg       <- "PEcAn.data.atmosphere"
-fcn       <- "met"
+pkg       <- "PEcAn.ED2"
+fcn       <- "met2model.ED"
 write     <- TRUE
 overwrite <- FALSE
 username  <- ""
 
 NARR_ED.id <- convert.input (input.id,outfolder,pkg,fcn,write,username,lst=lst,overwrite=overwrite)
-
-
