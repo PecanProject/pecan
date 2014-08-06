b707e16a43e71b675d9b3fc72670e615e888a52f
Michael Dietze <dietze@bu.edu>
2014-08-06 11:57:01 -0700

    First half of updated treering assimilation workflow. Updated the files used, switched to the new functions for tree ring processing, added allometries, added database connection. Tested up through plot2AGB, which runs but the JAGS code in InventoryGrowthFusion seem to balk at large n.iter and there's some bug in the JAGS visualization that hasn't been tracked down yet

--- scripts/workflow.treering.R
+++ scripts/workflow.treering.R
@@ -1,22 +1,66 @@
+#--------------------------------------------------------------------------------#
+# functions used to write STATUS used by history
+#--------------------------------------------------------------------------------#
+options(warn = 1, keep.source = TRUE, error = quote({
+  status.end("ERROR")
+}))
+
+status.start <- function(name) {
+  cat(paste(name, format(Sys.time(), "%F %T"), sep="\t"), file=file.path(settings$outdir, "STATUS"), append=TRUE)      
+}
+
+status.end <- function(status="DONE") {
+  cat(paste("", format(Sys.time(), "%F %T"), status, "\n", sep="\t"), file=file.path(settings$outdir, "STATUS"), append=TRUE)      
+}
+
 #---------------- Load libraries. -----------------------------------------------------------------#
-.libPaths("~/lib/R")
-require(PEcAn.all)
-#--------------------------------------------------------------------------------------------------#
+      require(PEcAn.all)
+      #--------------------------------------------------------------------------------------------------#
+      #
+      #  dir.create("~/demo.sda")
+      #  clean.settings("~/demo.pda/demo.xml","~/demo.sda/demo.xml")
+      
+      #---------------- Load PEcAn settings file. -------------------------------------------------------#
+      # Open and read in settings file for PEcAn run.
+      settings <- read.settings("~/demo.sda/demo.xml")
+      #--------------------------------------------------------------------------------------------------#
+      
+      ## Read tree data
+      trees <- read.csv("/home/carya/Camp2014/PecanInputs/H 2012 Adult Field Data.csv")
+      
+      ## Read tree ring data
+      rings <- Read_Tuscon("/home/carya/Camp2014/PecanInputs/Revised 2/")
+      
+      ## Match observations & format for JAGS
+      combined <- matchInventoryRings(trees,rings,nyears=30,coredOnly=FALSE)
+      data <- buildJAGSdata_InventoryRings(combined)
+      
+      ## Tree Ring model
+      n.iter = 300
+      jags.out = InventoryGrowthFusion(data,n.iter=n.iter)
+      save(trees,rings,combined,data,jags.out,
+           file=file.path(settings$outdir,"treering.Rdata"))
 
-#---------------- Load PEcAn settings file. -------------------------------------------------------#
-# Open and read in settings file for PEcAn run.
-settings <- read.settings("~/inputs/sylvania.SIPNET.xml")
-#--------------------------------------------------------------------------------------------------#
+pdf(file.path(settings$outdir,"treering.Diagnostics.pdf"))
+InventoryGrowthFusionDiagnostics(combined,jags.out,combined)
+dev.off()
 
-plot.data <- read.plot("~/inputs/plot/Sylvania_Plot_Sam.csv")
-inc.dataV <- read.velmex("~/inputs/plot/velmex/")
-inc.dataT <- Read_Tuscon("~/inputs/plot/tuscon/")
+## Allometry Model
+library(PEcAn.allometry)
+con <- db.open(settings$database$bety)
+pft.data = list()
+for(ipft in 1:length(settings$pfts)){  ## loop over PFTs
+  pft_name = settings$pfts[[ipft]]$name
+  query <- paste0("SELECT s.spcd,",'s."Symbol"'," as acronym from pfts as p join pfts_species on p.id = pfts_species.pft_id join species as s on pfts_species.specie_id = s.id where p.name like '%",pft_name,"%'")  
+  pft.data[[pft_name]] <- db.query(query, con)
+}
+allom.stats = AllomAve(pft.data,outdir = settings$outdir,ngibbs=n.iter)
+save(allom.stats,file=file.path(settings$outdir,"allom.stats.Rdata"))
 
-fd <- fuse_plot_treering(plot.data,inc.dataV,inc.dataT)
-
-diametergrow(fd$diameters,fd$increments,fd$survival)
-
-plot2AGB(unit.conv=0.02)
+## Convert tree-level growth & diamter to stand-level NPP & AGB
+out = as.matrix(jags.out)
+sel = grep('x[',colnames(out),fixed=TRUE)
+state = plot2AGB(combined,out[,sel],settings$outdir,allom.stats,unit.conv=0.02)
 
 ## run ensemble
 # Query the trait database for data and priors
@@ -38,4 +82,5 @@ sda.particle(settings$model$name)
 
 sda.enkf(settings$model$name)
 
+db.close(con)
 #--------------------------------------------------------------------------------------------------#
