c93c27a97582ccd18a8199f9342f4862db614fa5
Michael Dietze <dietze@bu.edu>
2014-08-11 15:46:25 -0700

    workflow.treering: added obs, IC, and priors

--- scripts/workflow.treering.R
+++ scripts/workflow.treering.R
@@ -42,7 +42,7 @@ status.end <- function(status="DONE") {
            file=file.path(settings$outdir,"treering.Rdata"))
 
 pdf(file.path(settings$outdir,"treering.Diagnostics.pdf"))
-InventoryGrowthFusionDiagnostics(combined,jags.out,combined)
+InventoryGrowthFusionDiagnostics(jags.out,combined)
 dev.off()
 
 ## Allometry Model
@@ -61,6 +61,65 @@ save(allom.stats,file=file.path(settings$outdir,"allom.stats.Rdata"))
 out = as.matrix(jags.out)
 sel = grep('x[',colnames(out),fixed=TRUE)
 state = plot2AGB(combined,out[,sel],settings$outdir,allom.stats,unit.conv=0.02)
+obs = data.frame(mean = apply(state$NPP[1,,],2,mean,na.rm=TRUE),
+                 sd = apply(state$NPP[1,,],2,sd,na.rm=TRUE))
+
+## Build Initial Conditions
+#### write INITAL CONDITIONS here ####
+# settings$assim.sequential$n.ensemble = 10
+ne = settings$assim.sequential$n.ensemble
+plantWood = ifelse(rep("AGB" %in% names(state),ne),
+                   state$AGB[1,sample.int(ncol(state$AGB),ne),1]*0.1,
+                   runif(ne,0,300)) ## prior
+lai = ifelse(rep("LAI" %in% names(state),ne),
+             state$LAI[1,sample.int(ncol(state$LAI),ne),1],
+             runif(ne,0,7)) ## prior
+litter = ifelse(rep("litter" %in% names(state),ne),
+             state$litter[1,sample.int(ncol(state$litter),ne),1],
+             runif(ne,0,4)) ## prior
+soil   = ifelse(rep("soil" %in% names(state),ne),
+                state$soil[1,sample.int(ncol(state$soil),ne),1],
+                runif(ne,0,10)) ## prior
+litterWFrac = ifelse(rep("litterW" %in% names(state),ne),
+                state$litterW[1,sample.int(ncol(state$litterW),ne),1],
+                runif(ne)) ## prior
+soilWFrac = ifelse(rep("soilW" %in% names(state),ne),
+                state$soilW[1,sample.int(ncol(state$soilW),ne),1],
+                runif(ne)) ## prior
+snow = ifelse(rep("snow" %in% names(state),ne),
+                state$snow[1,sample.int(ncol(state$snow),ne),1],
+                runif(ne,0,1)) ## prior
+microbe = ifelse(rep("microbe" %in% names(state),ne),
+                state$microbe[1,sample.int(ncol(state$microbe),ne),1],
+                runif(ne,0,1)) ## prior                  
+IC = data.frame(plantWood,lai,litter,soil,litterWFrac,soilWFrac,snow,microbe)
+
+#---------------- Load Priors ----------------------------------------------------------------------#
+status.start("PRIORS")
+## grab posteriors from database
+if(is.null(settings$assim.sequential$prior)){
+  pft.id =  db.query(paste0("SELECT id from pfts where name = '",settings$pfts$pft$name,"'"),con)
+  priors =  db.query(paste0("SELECT * from posteriors where pft_id = ",pft.id),con)
+  ## by default, use the most recent posterior as the prior
+  settings$assim.sequential$prior = priors$id[which.max(priors$updated_at)]
+}
+## load prior
+prior.db = db.query(paste0("SELECT * from dbfiles where container_type = 'Posterior' and container_id = ",
+                           settings$assim.sequential$prior),con)
+prior.db = prior.db[grep("post.distns.Rdata",prior.db$file_name),]
+load(file.path(prior.db$file_path,"post.distns.Rdata"))
+## sample from priors
+nvar <- nrow(post.distns)
+prior = as.data.frame(matrix(numeric(),ne,nvar))
+for(i in 1:nvar){
+  if(post.distns$distn[i] == 'exp'){
+    prior[,i] <- eval(parse(text=paste0("rexp(",ne,",",post.distns$parama[i],")")))
+  }else{
+    prior[,i] <- eval(parse(text=paste0("r",post.distns$distn[i],"(",ne,",",post.distns$parama[i],",",post.distns$paramb[i],")")))
+  }
+}
+colnames(prior) = rownames(post.distns)
+status.end()
 
 ## run ensemble
 # Query the trait database for data and priors
@@ -80,7 +139,7 @@ get.results(settings)
 
 sda.particle(settings$model$name)
 
-sda.enkf(settings$model$name)
+sda.enkf(settings,IC,prior,obs)
 
 db.close(con)
 #--------------------------------------------------------------------------------------------------#
