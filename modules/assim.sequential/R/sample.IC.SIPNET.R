69d1e33cc6487d324edc95139a8d837b8caf20c7
Michael Dietze <dietze@bu.edu>
2014-08-14 13:05:59 -0700

    sample.IC.SIPNET: added units, updated priors based on ranges in default param file

--- modules/assim.sequential/R/sample.IC.SIPNET.R
+++ modules/assim.sequential/R/sample.IC.SIPNET.R
@@ -1,22 +1,29 @@
 sample.IC.SIPNET <- function(ne,state){
+  ## g C * m-2 ground area in wood (above-ground + roots)
   plantWood = ifelse(rep("AGB" %in% names(state),ne),
-                     state$AGB[1,sample.int(ncol(state$AGB),ne),1]*0.1,
-                     runif(ne,0,300)) ## prior
+                     state$AGB[1,sample.int(ncol(state$AGB),ne),1]*50, ## unit Mg/ha
+                     runif(ne,0,14000)) ## prior
+  ## initial leaf area, m2 leaves * m-2 ground area (multiply by leafCSpWt to get initial plant leaf C)
   lai = ifelse(rep("LAI" %in% names(state),ne),
                state$LAI[1,sample.int(ncol(state$LAI),ne),1],
                runif(ne,0,7)) ## prior
+  ## g C * m-2 ground area
   litter = ifelse(rep("litter" %in% names(state),ne),
                   state$litter[1,sample.int(ncol(state$litter),ne),1],
-                  runif(ne,0,4)) ## prior
+                  runif(ne,0,1200)) ## prior
+  ## g C * m-2 ground area
   soil   = ifelse(rep("soil" %in% names(state),ne),
                   state$soil[1,sample.int(ncol(state$soil),ne),1],
-                  runif(ne,0,10)) ## prior
+                  runif(ne,0,19000)) ## prior
+  ## unitless: fraction of litterWHC
   litterWFrac = ifelse(rep("litterW" %in% names(state),ne),
                        state$litterW[1,sample.int(ncol(state$litterW),ne),1],
                        runif(ne)) ## prior
+  ## unitless: fraction of soilWHC
   soilWFrac = ifelse(rep("soilW" %in% names(state),ne),
                      state$soilW[1,sample.int(ncol(state$soilW),ne),1],
                      runif(ne)) ## prior
+  ## cm water equiv
   snow = ifelse(rep("snow" %in% names(state),ne),
                 state$snow[1,sample.int(ncol(state$snow),ne),1],
                 runif(ne,0,1)) ## prior
