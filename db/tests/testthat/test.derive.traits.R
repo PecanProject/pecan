0890f562b2af77034e3f70f08856546c8a152cd4
David LeBauer <dlebauer@gmail.com>
2014-08-19 19:08:35 -0500

    fixed tests in PEcAn.DB

--- db/tests/testthat/test.derive.traits.R
+++ db/tests/testthat/test.derive.traits.R
@@ -6,7 +6,7 @@
 # which accompanies this distribution, and is available at
 # http://opensource.ncsa.illinois.edu/license.html
 #-------------------------------------------------------------------------------
-
+con <- db.open(dbparms)
 test_that("take.samples works",{
   expect_equal(take.samples(summary = data.frame(mean = 1, stat = NA)), 1)
   set.seed(0)
--- db/tests/testthat/test.query.base.R
+++ db/tests/testthat/test.query.base.R
@@ -6,16 +6,11 @@
 # which accompanies this distribution, and is available at
 # http://opensource.ncsa.illinois.edu/license.html
 #-------------------------------------------------------------------------------
+con <- db.open(dbparms)
 
-
-test_that("query base can execute a trivial SQL statement and return results",{  
+test_that("db.query can execute a trivial SQL statement and return results",{  
     ans <- db.query("select count(*) from traits;", con = con)
     expect_is(ans, "data.frame")
     expect_is(ans[,1], "numeric")
     expect_true(length(ans) == 1)
-
-    tables <- db.query('show tables;', con = con)
-    expect_true(is.data.frame(tables))
-    expect_true(is.character(tables[,1]))
-    expect_true(ncol(tables) == 1)
 })
