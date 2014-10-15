c23ba03de787d211dda98a55cc9266411acd1922
Josh Mantooth <jam2767@bu.edu>
2014-10-15 12:43:38 -0400

    Script to Download Ameriflux L2 netCDF files, with gaps

---
+++ download.Ameriflux.R
@@ -0,0 +1,17 @@
+download.Ameriflux <- function(start_year, end_year, site_id, in.prefix, outfolder){
+  
+  if(!file.exists(outfolder)){
+    dir.create(outfolder)
+  }
+  
+  start_year <- as.numeric(start_year)
+  end_year   <- as.numeric(end_year)
+  
+  # Download Ameriflux L2 .nc from internet
+  for(i in start_year:end_year){
+    year <- i 
+    url <- paste0("http://cdiac.ornl.gov/ftp/ameriflux/data/Level2/Sites_ByID/",
+                  site_id,"with_gaps/",in.prefix,"_",year,"_L2_WG_V005.nc")
+    download.file(url,paste0(outfolder,in.prefix,"_",year,"_L2_WG_V005.nc"))
+  }
+}
