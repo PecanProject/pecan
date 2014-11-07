PecanProject/pecan

commit fe65518bebe3be0546a7a4eea4a015635d671fab

removed duplicate vcmax temperature scaling fixes gh-83

models/ed/R/write.configs.ed.R
@@ -58,15 +58,10 @@ convert.samples.ED <- function(trait.samples){
   ## Convert leaf_respiration_rate_m2 to dark_resp_factor
   if('leaf_respiration_rate_m2' %in% names(trait.samples)) {
     leaf_resp = trait.samples[['leaf_respiration_rate_m2']]
-    vcmax <- trait.samples[['Vcmax']]
     
     ## First scale variables to 15 degC
     trait.samples[['leaf_respiration_rate_m2']] <- 
-      arrhenius.scaling(leaf_resp, old.temp = 25, new.temp = 15)
-    vcmax_15 <- arrhenius.scaling(vcmax, old.temp = 25, new.temp = 15)
-    
-    ## Output leaf_respiration_rate @ 15C as Rd0 -- New way to input leaf resp into ED2.  Dark Resp Factor is no longer used (see below)
-    #trait.samples[['Rd0']] <- trait.samples[['leaf_respiration_rate_m2']] ## Added by SPS 05/06/2013 -- NO LONGER NEEDED
+    arrhenius.scaling(leaf_resp, old.temp = 25, new.temp = 15)
     
     ## Calculate dark_resp_factor -- Will be depreciated when moving from older versions of ED2
     trait.samples[['dark_respiration_factor']] <- trait.samples[['leaf_respiration_rate_m2']]/

commit 0919270ad9ccb592a28067a4eb66fd8dd77af54c

added vcmax to dark_respiration_factor calculation
This was a bug introduced in fe65518; also discussed in PecanProject/pecan#83

When I committed fe65518 I forgot to fix the calculation of dark_respiration_factor, which depends on vcmax

models/ed/R/write.configs.ed.R
@@ -53,26 +53,25 @@ convert.samples.ED <- function(trait.samples){
   if('Vcmax' %in% names(trait.samples)) {
     vcmax <- trait.samples[['Vcmax']]
     trait.samples[['Vcmax']] <- arrhenius.scaling(vcmax, old.temp = 25, new.temp = 15)
-  }
 
-  ## Convert leaf_respiration_rate_m2 to dark_resp_factor
-  if('leaf_respiration_rate_m2' %in% names(trait.samples)) {
-    leaf_resp = trait.samples[['leaf_respiration_rate_m2']]
+    ## Convert leaf_respiration_rate_m2 to dark_resp_factor; requires Vcmax
+    if('leaf_respiration_rate_m2' %in% names(trait.samples)) {
+      leaf_resp = trait.samples[['leaf_respiration_rate_m2']]
     
-    ## First scale variables to 15 degC
-    trait.samples[['leaf_respiration_rate_m2']] <- 
-    arrhenius.scaling(leaf_resp, old.temp = 25, new.temp = 15)
+      ## First scale variables to 15 degC
+      trait.samples[['leaf_respiration_rate_m2']] <- 
+      arrhenius.scaling(leaf_resp, old.temp = 25, new.temp = 15)
     
-    ## Calculate dark_resp_factor -- Will be depreciated when moving from older versions of ED2
-    trait.samples[['dark_respiration_factor']] <- trait.samples[['leaf_respiration_rate_m2']]/
-      vcmax_15
+      ## Calculate dark_resp_factor -- Will be depreciated when moving from older versions of ED2
+      trait.samples[['dark_respiration_factor']] <- trait.samples[['leaf_respiration_rate_m2']]/
+         trait.samples[['Vcmax']]
     
-    ## Remove leaf_respiration_rate from trait samples -- NO LONGER NEEDED
-    #remove <- which(names(trait.samples)=='leaf_respiration_rate_m2')
-    #trait.samples = trait.samples[-remove]
+      ## Remove leaf_respiration_rate from trait samples -- NO LONGER NEEDED
+      #remove <- which(names(trait.samples)=='leaf_respiration_rate_m2')
+      #trait.samples = trait.samples[-remove]
     
-  } ## End dark_respiration_factor loop
-  
+    } ## End dark_respiration_factor loop
+  } ## End Vcmax  
   # for debugging conversions
   #save(trait.samples, file = file.path(settings$outdir, 'trait.samples.Rdata'))
 