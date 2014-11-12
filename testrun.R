169ad7e2da26e56de86755abe4feec917312720b
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2014-11-11 19:07:40 -0500

    Updated testrun script to have large (10^6) number of samples.

--- testrun.R
+++ testrun.R
@@ -3,6 +3,6 @@
 source("prospect_bayesinv.R")
 beech <- read.table("data/beech.txt", header=TRUE, quote="\"")
 
-beech.inv <- pinvbayes(beech)
+beech.inv <- pinvbayes(beech, ngibbs=10^6)
 bi <- data.frame(beech.inv)
 write.csv(bi, file="test_beech.csv")
