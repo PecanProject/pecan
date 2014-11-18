c2d64e7c08680e4296065a16d5c662ed0fae3f01
ANN RAIHO <araiho@nd.edu>
2014-11-18 16:37:31 -0500

    added linkages

---
+++ models/linkages/R/model2netcdf.LINKAGES.R
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2012 University of Illinois, NCSA.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the 
+# University of Illinois/NCSA Open Source License
+# which accompanies this distribution, and is available at
+# http://opensource.ncsa.illinois.edu/license.html
+#-------------------------------------------------------------------------------
+
+##-------------------------------------------------------------------------------------------------#
+##' Convert MODEL output into the NACP Intercomparison format (ALMA using netCDF)
+##' 
+##' @name model2netcdf.MODEL
+##' @title Code to convert MODELS's output into netCDF format
+##'
+##' @param outdir Location of model output
+##' @param sitelat Latitude of the site
+##' @param sitelon Longitude of the site
+##' @param start_date Start time of the simulation
+##' @param end_date End time of the simulation
+##' @export
+##'
+##' @author Rob Kooper
+model2netcdf.MODEL <- function(outdir, sitelat, sitelon, start_date, end_date) {
+  logger.severe("NOT IMPLEMENTED")
+}
