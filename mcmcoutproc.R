061c5ba3a5516866640649b8def53f84eaaa41fb
Alexey Shiklomanov <ashiklom@geo.bu.edu>
2014-11-19 17:04:07 -0500

    Fixed missing paren.

--- mcmcoutproc.R
+++ mcmcoutproc.R
@@ -7,6 +7,6 @@ diag <- function(fname, beg=1000){
   for(cn in colnames(dat)[1:5]){
     plot(dat[-beg:0,cn], type='l')
     hist(dat[-beg:0,cn])
-    print(sprintf("%s AR %.2f", cn, length(unique(dat[,cn]))/length(dat[,cn]))
+    print(sprintf("%s AR %.2f", cn, length(unique(dat[,cn]))/length(dat[,cn])))
   }  
 }
