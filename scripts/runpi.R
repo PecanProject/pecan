40cb88a8bf04ba2abd947dad3d409a859aa01435
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2014-11-21 15:02:27 -0500

    Added random effects and runid character arguments

--- scripts/runpi.R
+++ scripts/runpi.R
@@ -3,9 +3,16 @@
 args <- commandArgs(trailingOnly=TRUE)
 jrsd <- as.numeric(args[1])
 species <- args[2]
-precision <- as.numeric(args[3])
-ngibbs <- as.numeric(args[4])
-filename <- sprintf("run_results/%s_%g_%s.dat", species, jrsd, as.character(precision>0))
+precarg <- as.numeric(args[3])
+if(precarg){
+        precision <- "sp"
+} else {
+        precision <- "pwl"
+}
+rearg <- args[4]
+ngibbs <- as.numeric(args[5])
+runid <- args[6]
+filename <- sprintf("run_results/%s_%g_%s_%s_%s_%s.dat", species, jrsd, precision, rearg, runid)
 
 source("inv_bayes.R")
 source("specdataproc.R")
@@ -16,4 +23,5 @@ smat <- specmatrix(specdat, cond)
 
 pinvbayes(smat, ngibbs=ngibbs, JumpRSD=jrsd, fname=filename,
           local.store=FALSE, 
-          single.precision=precision)
+          single.precision=precision, 
+          random.effects=rearg)
