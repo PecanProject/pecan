88e8ff343d404e4a175bef1928fed20ec068d2de
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2014-11-25 15:06:57 -0500

    Simplified. Optimization based on mean spectrum.

--- mle_inversion.R
+++ mle_inversion.R
@@ -4,40 +4,19 @@
 
 source("prospect.R")
 
-# Some test data
-beech <- read.table("data/beech.txt", header=TRUE, quote="\"")
-clover <- read.table("data/clover.txt", header=TRUE, quote="\"")
-corn <- read.table("data/corn.txt", header=TRUE, quote="\"")
-poplar <- read.table("data/poplar.txt", header=TRUE, quote="\"")
-
-# Vector of initial conditions based on min/max Shawn's LOPEX 93 data
-
-ic1 <- unlist(testdata['min.f', ])   # min f
-ic2 <- unlist(testdata['max.f', ])   # max f
-ic3 <- unlist(testdata['min.d', ])   # min d
-ic4 <- unlist(testdata['max.d', ])   # max d
-
-# Minimum and maximum values from Shawn's LOPEX 93 data (for DEoptim)
-mins <- as.numeric(unlist(apply(testdata, 2, min)[2:5]))
-maxs <- as.numeric(unlist(apply(testdata, 2, max)[2:5]))
-
 # Merit function based on sum-of-squares difference
 ssd <- function(P, ref.obs, func=prospect4){
-  prospect <- do.call(func, as.list(P))
-  ref.mod <- prospect$R
-  ssd <- min(log(sum((ref.mod - ref.obs)^2)), 1e10)
+  P <- as.list(P)
+  prospect <- func(P$N, P$Cab, P$Cw, P$Cm, n.a, cab.a, w.a, m.a)
+  ssd <- log(sum((prospect - ref.obs)^2))
   return(ssd)
 }
 
-p.invert <- function(observed, func=prospect4, inits=ic1){
-  a1 <- optim(inits, ssd, ref.obs=observed$Refl, func=func)
-  p <- do.call(func, as.list(a1$par))
-  d <- p$R - observed$R
-  plot(p$wavelength, p$R, type='l')
-  lines(observed$Wavelength, observed$R, col=2)
-  plot(p$wavelength, d, type='l')
-  abline(h=0)
-  a1$residuals <- d
-  return(a1)
+p.invert <- function(observed, func=prospect4, inits=initv){
+  observed <- rowMeans(observed[,-1])
+  a1 <- optim(inits, ssd, ref.obs=observed, func=func)
+return(a1$par)
 }
+
+initv <- unlist(inits)[1:4]
 
