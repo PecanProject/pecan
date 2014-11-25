a296fb9c71d03646f7b967dc6936b82e20b38028
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2014-11-25 15:11:58 -0500

    Added diagnostic plot and printing of all acceptance rates.

--- scripts/localtest.R
+++ scripts/localtest.R
@@ -3,11 +3,17 @@
 source("inv_bayes.R")
 g2 <- pinvbayes(grapedat,
                 local.store=TRUE,
-                ngibbs=600,
+                ngibbs=20,
                 random.effects='none',
                 random.inits=1,
-                ar.step=10,
-                ar.min=0.4)
+                ar.step=100,
+                ar.min=0.4,
+                JumpRSD=1)
 
-plot(g2$N, type='l')
-length(unique(g2$N))/length(g2$N)
+plot(g2$N/max(g2$N), type='l')
+lines(g2$Cab/max(g2$Cab), col=2)
+lines(g2$Cw/max(g2$Cw), col=3)
+lines(g2$Cm/max(g2$Cm), col=4)
+for(i in 1:4){
+  print(length(unique(g2[[i]]))/length(g2[[i]]))
+}
