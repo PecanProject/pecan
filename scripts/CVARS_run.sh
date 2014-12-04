88da2983cedf1332dabdb29e0987ce19faa4ba5a
Alexey Shiklomanov <ashiklom@geo.bu.edu>
2014-12-03 20:54:29 -0500

    Fixed argument handling in CVARS script

--- scripts/CVARS_run.sh
+++ scripts/CVARS_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 REFARG=$1
 SIARG=$2
-qsub -v SPECIES="Grape",REF=REFARG,SI=SIARG multirun.qsub
-qsub -v SPECIES="CAAN-Red Pepper",REF=REFARG,SI=SIARG multirun.qsub
-qsub -v SPECIES="LemonTree",REF=REFARG,SI=SIARG multirun.qsub
-qsub -v SPECIES="ShortDatePalm",REF=REFARG,SI=SIARG multirun.qsub
-qsub -v SPECIES="MandarinOrange",REF=REFARG,SI=SIARG multirun.qsub
+qsub -v SPECIES="Grape",REF=$REFARG,SI=$SIARG multirun.qsub
+qsub -v SPECIES="CAAN-Red Pepper",REF=$REFARG,SI=$SIARG multirun.qsub
+qsub -v SPECIES="LemonTree",REF=$REFARG,SI=$SIARG multirun.qsub
+qsub -v SPECIES="ShortDatePalm",REF=$REFARG,SI=$SIARG multirun.qsub
+qsub -v SPECIES="MandarinOrange",REF=$REFARG,SI=$SIARG multirun.qsub
 
