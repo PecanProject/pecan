9e6835e315a5226e006f55021f6c5391f4f022ba
Rob Kooper <kooper@illinois.edu>
2014-12-04 13:46:44 -0800

    clean up gapfill

--- modules/data.atmosphere/R/metgapfill.R
+++ modules/data.atmosphere/R/metgapfill.R
@@ -3,15 +3,17 @@
 ##' Currently 
 ##' Future version: Choose which variables to gap fill
 ##' Future version will first downscale and fill with NARR, then REddyProc
-##
-##' @param in.path
-##' @param in.prefix
-##' 
-##' @param outfolder
+##'
+##' @export
+##' @param start_year first year to be gapfiled
+##' @param end_year last year to be gapfiled
+##' @param in.path location on disk where inputs are stord
+##' @param in.prefix prefix of input and output files
+##' @param outfolder location on disk where outputs will be stored
+##' @param overwrite should existing files be overwritten
 ##' @author Ankur Desai
 ##'
-
-metgapfill <- function(in.path,in.prefix,outfolder){
+metgapfill <- function(start_year, end_year, in.path, in.prefix, outfolder, overwrite=FALSE){
   require(REddyProc)  
   require(ncdf4)
 ##  require(udunits2)
@@ -20,25 +22,41 @@ metgapfill <- function(in.path,in.prefix,outfolder){
   #REddyProc installed to ~/R/library by install.packages("REddyProc", repos="http://R-Forge.R-project.org", type="source")
   #dependency minpack.lm may not install automatically, so install it first
 
-  ## Step 1. Read met variables from Ameriflux NetCDF CF in.file
-  files = dir(in.path,in.prefix)
-  files = files[grep(pattern="*.nc",files)]
-  
-  if(length(files) == 0) {
-    ## send warning
-    return(NULL)
-  }  
-  
   if(!file.exists(outfolder)){
     dir.create(outfolder)
   }
   
-  for(i in 1:length(files)){
+  rows <- end_year - start_year + 1
+  results <- data.frame(file=character(rows), host=character(rows),
+                        mimetype=character(rows), formatname=character(rows),
+                        startdate=character(rows), enddate=character(rows),
+                        stringsAsFactors = FALSE)
+  
+  for(year in start_year:end_year) {
+    old.file <- file.path(in.path, paste(in.prefix, year, "nc", sep="."))
+    new.file <- file.path(outfolder, paste(in.prefix, year, "nc", sep="."))
     
-    new.file =file.path(outfolder,files[i])
+    # check if input exists
+    if (!file.exists(old.file)) {
+      logger.sever("Missing input file for year", year, "in folder", in.path)
+    }
+        
+    # create array with results
+    row <- year - start_year + 1
+    results$file[row] <- new.file
+    results$host[row] <- fqdn()
+    results$startdate[row] <- paste0(year,"-01-01 00:00:00")
+    results$enddate[row] <- paste0(year,"-12-31 23:59:59")
+    results$mimetype[row] <- 'application/x-netcdf'
+    results$formatname[row] <- 'CF (gapfilled)'
+    
+    if (file.exists(new.file) && !overwrite) {
+      logger.debug("File '", new.file, "' already exists, skipping to next file.")
+      next
+    }
     
     ## copy old file to new directory
-    system2("cp",paste(file.path(in.path,files[i]),new.file))
+    file.copy(old.file, new.file, overwrite=TRUE)
     
     ## Let's start with reading a few variables
     nc <- nc_open(new.file,write=TRUE)
@@ -47,22 +65,23 @@ metgapfill <- function(in.path,in.prefix,outfolder){
     Tair <- ncvar_get(nc=nc,varid='air_temperature')
     Rg <- ncvar_get(nc=nc,varid='surface_downwelling_shortwave_flux')
     rH <- ncvar_get(nc=nc,varid='relative_humidity')
-    PAR <- ncvar_get(nc=nc,varid='PAR')
+    PAR <- ncvar_get(nc=nc,varid='surface_downwelling_photosynthetic_photon_flux_in_air')
     precip <- ncvar_get(nc=nc,varid='precipitation_flux')
-    Rn <- ncvar_get(nc=nc,varid='Rn')
+    #Rn <- ncvar_get(nc=nc,varid='Rn')
     sHum <- ncvar_get(nc=nc,varid='specific_humidity')
     Lw <- ncvar_get(nc=nc,varid='surface_downwelling_longwave_flux')
-    Ts1 <-ncvar_get(nc=nc,varid='TS1')
-    Ts2 <-ncvar_get(nc=nc,varid='TS2')
-    VPD <-ncvar_get(nc=nc,varid='VPD')
+    Ts1 <-ncvar_get(nc=nc,varid='soil_temperature')
+    #Ts2 <-ncvar_get(nc=nc,varid='TS2')
+    VPD <-ncvar_get(nc=nc,varid='water_vapor_saturation_deficit')
     ws <-ncvar_get(nc=nc,varid='wind_speed')
-    co2 <- ncvar_get(nc=nc,varid='CO2')
+    #co2 <- ncvar_get(nc=nc,varid='CO2')
     press <- ncvar_get(nc=nc,varid='air_pressure')
     east_wind <- ncvar_get(nc=nc,varid='eastward_wind')
     north_wind <- ncvar_get(nc=nc,varid='northward_wind')
     
     ## make a data frame, convert -9999 to NA, convert to degrees C
-    EddyData.F <- data.frame(Tair,Rg,rH,PAR,precip,Rn,sHum,Lw,Ts1,Ts2,VPD,ws,co2,press,east_wind,north_wind)
+    #EddyData.F <- data.frame(Tair,Rg,rH,PAR,precip,Rn,sHum,Lw,Ts1,Ts2,VPD,ws,co2,press,east_wind,north_wind)
+    EddyData.F <- data.frame(Tair,Rg,rH,PAR,precip,sHum,Lw,Ts1,VPD,ws,press,east_wind,north_wind)
     EddyData.F[EddyData.F <= -9999] = NA
     EddyData.F['Tair'] = EddyData.F['Tair']-273.15
 
@@ -75,14 +94,14 @@ metgapfill <- function(in.path,in.prefix,outfolder){
     n_rH <- sum(is.na(EddyData.F['rH']))
     n_PAR <- sum(is.na(EddyData.F['PAR']))
     n_precip <- sum(is.na(EddyData.F['precip']))
-    n_Rn <- sum(is.na(EddyData.F['Rn']))
+    #n_Rn <- sum(is.na(EddyData.F['Rn']))
     n_sHum <- sum(is.na(EddyData.F['sHum']))
     n_Lw <- sum(is.na(EddyData.F['Lw']))
     n_Ts1 <- sum(is.na(EddyData.F['Ts1']))
-    n_Ts2 <- sum(is.na(EddyData.F['Ts2']))
+    #n_Ts2 <- sum(is.na(EddyData.F['Ts2']))
     n_VPD <- sum(is.na(EddyData.F['VPD']))
     n_ws <- sum(is.na(EddyData.F['ws']))
-    n_co2 <- sum(is.na(EddyData.F['co2']))
+    #n_co2 <- sum(is.na(EddyData.F['co2']))
     n_press <- sum(is.na(EddyData.F['press']))
     n_east_wind <- sum(is.na(EddyData.F['east_wind']))
     n_north_wind <- sum(is.na(EddyData.F['north_wind']))
@@ -100,8 +119,9 @@ metgapfill <- function(in.path,in.prefix,outfolder){
     EddyData.F <- fConvertTimeToPosix(EddyData.F, 'YDH', Year.s='Year', Day.s='DoY', Hour.s='Hour')
     
     ## Create EddyProc object 
-    EddyProc.C <- sEddyProc$new('Site', EddyData.F, c('Tair','Rg','rH','PAR','precip','Rn','sHum','Lw','Ts1','Ts2','VPD','ws','co2','press','east_wind','north_wind'))
-                                
+    #EddyProc.C <- sEddyProc$new('Site', EddyData.F, c('Tair','Rg','rH','PAR','precip','Rn','sHum','Lw','Ts1','Ts2','VPD','ws','co2','press','east_wind','north_wind'))
+    EddyProc.C <- sEddyProc$new('Site', EddyData.F, c('Tair','Rg','rH','PAR','precip','sHum','Lw','Ts1','VPD','ws','press','east_wind','north_wind'))
+    
     ## Gap fill with default (see below for examples of advanced options)
     ## Have to do Rg, Tair, VPD first
     if(n_Rg>0&&n_Rg<nelem) EddyProc.C$sMDSGapFill('Rg', FillAll.b=TRUE,V1.s='Rg',V2.s='VPD',V3.s='Tair')
@@ -110,13 +130,13 @@ metgapfill <- function(in.path,in.prefix,outfolder){
     if(n_rH>0&&n_rH<nelem) EddyProc.C$sMDSGapFill('rH', FillAll.b=TRUE,V1.s='Rg',V2.s='VPD',V3.s='Tair')
     if(n_PAR>0&&n_PAR<nelem) EddyProc.C$sMDSGapFill('PAR', FillAll.b=TRUE,V1.s='Rg',V2.s='VPD',V3.s='Tair')
     if(n_precip>0&&n_precip<nelem) EddyProc.C$sMDSGapFill('precip', FillAll.b=TRUE,V1.s='Rg',V2.s='VPD',V3.s='Tair')
-    if(n_Rn>0&&n_Rn<nelem) EddyProc.C$sMDSGapFill('Rn', FillAll.b=TRUE,V1.s='Rg',V2.s='VPD',V3.s='Tair')
+    #if(n_Rn>0&&n_Rn<nelem) EddyProc.C$sMDSGapFill('Rn', FillAll.b=TRUE,V1.s='Rg',V2.s='VPD',V3.s='Tair')
     if(n_sHum>0&&n_sHum<nelem) EddyProc.C$sMDSGapFill('sHum', FillAll.b=TRUE,V1.s='Rg',V2.s='VPD',V3.s='Tair')
     if(n_Lw>0&&n_Lw<nelem) EddyProc.C$sMDSGapFill('Lw', FillAll.b=TRUE,V1.s='Rg',V2.s='VPD',V3.s='Tair')
     if(n_Ts1>0&&n_Ts2<nelem) EddyProc.C$sMDSGapFill('Ts1', FillAll.b=TRUE,V1.s='Rg',V2.s='VPD',V3.s='Tair')
-    if(n_Ts2>0&&n_Ts2<nelem) EddyProc.C$sMDSGapFill('Ts2', FillAll.b=TRUE,V1.s='Rg',V2.s='VPD',V3.s='Tair')
+    #if(n_Ts2>0&&n_Ts2<nelem) EddyProc.C$sMDSGapFill('Ts2', FillAll.b=TRUE,V1.s='Rg',V2.s='VPD',V3.s='Tair')
     if(n_ws>0&&n_ws<nelem) EddyProc.C$sMDSGapFill('ws', FillAll.b=TRUE,V1.s='Rg',V2.s='VPD',V3.s='Tair')
-    if(n_co2>0&&n_co2<nelem) EddyProc.C$sMDSGapFill('co2', FillAll.b=TRUE,V1.s='Rg',V2.s='VPD',V3.s='Tair')
+    #if(n_co2>0&&n_co2<nelem) EddyProc.C$sMDSGapFill('co2', FillAll.b=TRUE,V1.s='Rg',V2.s='VPD',V3.s='Tair')
     if(n_press>0&&n_press<nelem) EddyProc.C$sMDSGapFill('press', FillAll.b=TRUE,V1.s='Rg',V2.s='VPD',V3.s='Tair')
     if(n_east_wind>0&&n_east_wind<nelem) EddyProc.C$sMDSGapFill('east_wind', FillAll.b=TRUE,V1.s='Rg',V2.s='VPD',V3.s='Tair')
     if(n_north_wind>0&&n_north_wind<nelem) EddyProc.C$sMDSGapFill('north_wind', FillAll.b=TRUE,V1.s='Rg',V2.s='VPD',V3.s='Tair')
@@ -129,7 +149,6 @@ metgapfill <- function(in.path,in.prefix,outfolder){
     ##Write back to NC file, convert air T to Kelvin
     if(n_Tair>0&&n_Tair<nelem) {
       Tair_f <- Extracted[,'Tair_f']
-      Tair_f = Tair_f + 273.15
       ncvar_put(nc,varid='air_temperature',vals=Tair_f)
     }
     if(n_Rg>0&&n_Rg<nelem) {
@@ -142,16 +161,16 @@ metgapfill <- function(in.path,in.prefix,outfolder){
     }
     if(n_PAR>0&&n_PAR<nelem) {
       PAR_f <- Extracted[,'PAR_f']
-      ncvar_put(nc,varid='PAR',vals=PAR_f)
+      ncvar_put(nc,varid='surface_downwelling_photosynthetic_photon_flux_in_air',vals=PAR_f)
     }
     if(n_precip>0&&n_precip<nelem) {
       precip_f <- Extracted[,'precip_f']
       ncvar_put(nc,varid='precipitation_flux',vals=precip_f)
     }
-    if(n_Rn>0&&n_Rn<nelem) {
-      Rn_f <- Extracted[,'Rn_f']
-      ncvar_put(nc,varid='Rn',vals=Rn_f)
-    }
+    #if(n_Rn>0&&n_Rn<nelem) {
+    #  Rn_f <- Extracted[,'Rn_f']
+    #  ncvar_put(nc,varid='Rn',vals=Rn_f)
+    #}
     if(n_sHum>0&&n_sHum<nelem) {
       sHum_f <- Extracted[,'sHum_f']
       ncvar_put(nc,varid='specific_humidity',vals=sHum_f)
@@ -161,25 +180,25 @@ metgapfill <- function(in.path,in.prefix,outfolder){
       ncvar_put(nc,varid='surface_downwelling_longwave_flux',vals=Lw_f)
     }
     if(n_Ts1>0&&n_Ts1<nelem) {
-      Ts1_f <- Extracted[,'Ts1_f']
-      ncvar_put(nc,varid='TS1',vals=Ts1_f)
+      Ts1 <- Extracted[,'soil_temperature']
+      ncvar_put(nc,varid='soil_temperature',vals=Ts1)
     }
-    if(n_Ts2>0&&n_Ts2<nelem) {
-      Ts2_f <- Extracted[,'Ts2_f']
-      ncvar_put(nc,varid='TS2',vals=Ts2_f)
-    }
+    #if(n_Ts2>0&&n_Ts2<nelem) {
+    #  Ts2_f <- Extracted[,'Ts2_f']
+    #  ncvar_put(nc,varid='TS2',vals=Ts2_f)
+    #}
     if(n_VPD>0&&n_VPD<nelem) {
       VPD_f <- Extracted[,'VPD_f']
-      ncvar_put(nc,varid='VPD',vals=VPD_f)
+      ncvar_put(nc,varid='water_vapor_saturation_deficit',vals=VPD_f)
     }
     if(n_ws>0&&n_ws<nelem) {
       ws_f <- Extracted[,'ws_f']
       ncvar_put(nc,varid='wind_speed',vals=ws_f)
     }
-    if(n_co2>0&&n_co2<nelem) {
-      co2_f <- Extracted[,'co2_f']
-      ncvar_put(nc,varid='CO2',vals=co2_f)
-    }
+    #if(n_co2>0&&n_co2<nelem) {
+    #  co2_f <- Extracted[,'co2_f']
+    #  ncvar_put(nc,varid='CO2',vals=co2_f)
+    #}
     if(n_press>0&&n_press<nelem) {
       press_f <- Extracted[,'press_f']
       ncvar_put(nc,varid='air_pressure',vals=press_f)
@@ -238,5 +257,6 @@ metgapfill <- function(in.path,in.prefix,outfolder){
   ## Step 6.Replace gaps with debiased time series (perhaps store statistics of fit somewhere as a measure of uncertainty?)
   ## Step 7. Write to outfolder the new NetCDF file
 
+  invisible(results)
 } ## End function
 
