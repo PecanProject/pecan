6efbd685601f8bd45637ac336b429a4682441153
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2014-12-04 02:19:18 -0500

    Updated species list generator (and complementary list)

--- specdataproc.R
+++ specdataproc.R
@@ -19,3 +19,16 @@ specmatrix <- function(Species,
         colnames(refl.mat) <- fset
         return(refl.mat)
 }
+
+gen.spec.list <- function(path="data/SE_spectra/Reflectance/",
+                          out=FALSE){
+        flist <- list.files(path)
+        flist.split <- strsplit(flist, "_")
+        spec.list <- unique(sapply(flist.split, "[", 5))
+        if(out) {
+                return(spec.list)
+        } else {
+                write(spec.list, "scripts/species_list.txt",
+                      ncolumns = 1, append=FALSE)
+        }
+}
