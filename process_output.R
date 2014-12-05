a7a2a140ae3f9a464b23ac7a53fe58ef85e0569c
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2014-12-05 11:16:30 -0500

    Rewritten as function. Added interactive plot viewing.

--- process_output.R
+++ process_output.R
@@ -1,23 +1,49 @@
 library(coda)
 
-path <- "run_results/2014_12_2/"
-flist <- list.files(path)
-flist <- flist[which(nchar(flist) > 5)]
-species.list <- unique(gsub("(.*)_[0-9][.][0-9]+_.*", "\\1", flist))
+get.results <- function(path, burnin=2000, thin=16){
+        flist <- list.files(path)
+        flist <- flist[which(nchar(flist) > 5)]
+        species.list <- unique(gsub("(.*)_[0-9][.][0-9]+_.*", "\\1", flist))
+        
+        relist <- c("none", "plot", "leaf")
+        
+        for (species in species.list){
+                for (re in relist){
+                        fset <- flist[grep(sprintf("%s_.*_%s_.*", species, re), flist)]
+                        print(fset)
+                        if(length(fset) > 0){
+                                speclist <- lapply(fset, function(x) read.csv(paste(path, x, sep=''), 
+                                                                              header=TRUE))
+                                min.spec <- min(sapply(speclist, nrow))
+                                samples <- seq(burnin, min.spec, by=thin)
+                                speclist <- lapply(speclist, "[", samples,,drop=FALSE)
+                                speclist <- lapply(speclist, mcmc)
+                                assign(sprintf("%s.%s.l", species, re), mcmc.list(speclist), envir = .GlobalEnv)
+                        }
+                }
+        }
+}
 
-relist <- c("none", "plot", "leaf")
-
-burnin <- 2000
-
-for (species in species.list){
-  for (re in relist){
-    fset <- flist[grep(sprintf("%s_.*_%s_.*", species, re), flist)]
-    print(fset)
-    speclist <- lapply(fset, function(x) read.csv(paste(path, x, sep=''), 
-                                                  header=TRUE))
-    min.spec <- min(sapply(speclist, nrow))
-    speclist <- lapply(speclist, "[", burnin:min.spec,,drop=FALSE)
-    speclist <- lapply(speclist, mcmc)
-    assign(sprintf("%s.%s.l", species, re), mcmc.list(speclist))
-  }
+chain.plots <- function(mcmclist){
+        plot(mcmclist)
+        t1 <- readline("View details? : ")
+        if(t1 == 1){
+                l.m <- length(mcmclist)
+                out <- NULL
+                for (i in 1:l.m){
+                        plot(mcmclist[[i]])
+                        t2 <- readline("Discard? ")
+                        if(t2 == 1){
+                                out <- c(out, i)
+                        }
+                }
+                if(length(out) > 0 {
+                        return(mcmclist[-out])
+                } else {
+                        return(mcmclist)
+                }
+        } else {
+                return(mcmclist)
+        }
+        
 }
