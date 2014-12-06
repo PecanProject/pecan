3d8cb2c46cd16d62e4a11e2b04b3e6a6bdb0dc32
Michael Dietze <dietze@bu.edu>
2014-12-05 23:59:24 -0500

    Modified download.Ameriflux.site to use an external file to look up site.id to FLUXNET.id mapping. Moved this file from inst to data in data.atmosphere module so that system.file could find it.

--- modules/data.atmosphere/R/download.Ameriflux.R
+++ modules/data.atmosphere/R/download.Ameriflux.R
@@ -1,16 +1,8 @@
 # lookup the site based on the site_id
 download.Ameriflux.site <- function(site_id) {
-  sites <- c("622"="US-Syv", "676"="US-WCr", "678"="US-PFa", "679"="US-Los",
-             "751"="US-ARM", "752"="US-Blo", "753"="US-Bo1", "754"="US-Brw", 
-             "755"="US-Dk2", "756"="US-Dk3", "757"="US-FPe", "758"="US-Ha1", 
-             "759"="US-Ho1", "760"="US-IB1", "761"="US-IB2", "762"="US-Ivo",
-             "763"="US-Me2", "764"="US-Me3", "766"="US-Me5", "767"="US-MMS",
-             "768"="US-MOz", "769"="US-Ne1", "770"="US-Ne2", "771"="US-Ne3",
-             "772"="US-NR1", "773"="US-Shd", "774"="US-SO2", "775"="US-Ton",
-             "776"="US-UMB", "777"="US-Var", "778"="US-Atq", "796"="US-Bar",
-             "899"="US-WI8", "900"="US-WI1", "906"="US-WI7", "907"="US-WI0",
-             "908"="US-WI9")
-  sites[as.character(site_id)]
+  sites <- read.csv(system.file("data/FLUXNET.sitemap.csv",package="PEcAn.data.atmosphere"),
+                    stringsAsFactors=FALSE)
+  sites$FLUX.id[which(sites$site.id == site_id)]
 }
 
 ##' Download Ameriflux L2 netCDF files
