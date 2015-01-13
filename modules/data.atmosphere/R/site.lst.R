a52794884a257c8256765faf4d95bc8484a09e12
Betsy Cowdery <emcowdery@gmail.com>
2015-01-13 17:26:09 -0500

    Changed geonames username to carya

--- modules/data.atmosphere/R/site.lst.R
+++ modules/data.atmosphere/R/site.lst.R
@@ -7,7 +7,7 @@ site.lst <- function(site.id,con){
   }else{
     site <- db.query(paste("SELECT ST_X(ST_CENTROID(geometry)) AS lon, ST_Y(ST_CENTROID(geometry)) AS lat FROM sites WHERE id =",site.id),con)
     require(geonames)
-    options(geonamesUsername="ecowdery")
+    options(geonamesUsername="carya")
     lst <- GNtimezone(site$lat, site$lon, radius = 0)$dstOffset 
   }
   return(lst)
