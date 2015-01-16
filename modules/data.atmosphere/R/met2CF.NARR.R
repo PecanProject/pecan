ca162912da114507fc5f4ea0c5ea231c1dc37d04
Betsy Cowdery <emcowdery@gmail.com>
2015-01-16 00:31:50 -0500

    cf variable name changes

--- modules/data.atmosphere/R/met2CF.NARR.R
+++ modules/data.atmosphere/R/met2CF.NARR.R
@@ -12,15 +12,17 @@ met2CF.NARR <- function(in.path, in.prefix, outfolder) {
   
   require(ncdf4)
   
+  system(paste0("mkdir -p ", outfolder))
+  
   vars=c("pres.sfc", "dswrf", "dlwrf", "air.2m", "shum.2m", "prate", "uwnd.10m", "vwnd.10m")
   svars=c("pres", "dswrf", "dlwrf", "air", "shum", "prate", "uwnd", "vwnd")
-  cfvars=c("air_pressure", "surface_downwelling_shortwave_flux", "surface_downwelling_longwave_flux", "air_temperature", "specific_humidity", "precipitation_flux", "eastward_wind", "northward_wind" )
+  cfvars=c("air_pressure", "surface_downwelling_shortwave_flux_in_air", "surface_downwelling_longwave_flux_in_air", "air_temperature", "specific_humidity", "precipitation_flux", "eastward_wind", "northward_wind" )
   
   years = 1979:2014
   
   for(y in years){
     for(i in 1:length(vars)){
-      file <- paste0(in.path,"/",in.prefix,"/",vars[i],".",y,".nc")
+      file <- paste0(in.path,"/",vars[i],".",y,".nc")
       newfile <-paste0(outfolder,cfvars[i],".",y,".nc")
       
       print(file)
@@ -37,15 +39,13 @@ met2CF.NARR <- function(in.path, in.prefix, outfolder) {
   }
   
   for(y in years){
-    j=0
+    newfile <- paste0(outfolder,"NARR.",y,".nc")
+    
     for(i in 1:length(vars)){
       file <-paste0(outfolder,cfvars[i],".",y,".nc")
       if(file.exists(file)){
-        j=j+1
-        newfile <- paste0(outfolder,"NARR.",y,".nc")
-        if (j==1){
+        if (!file.exists(newfile)){
           file.copy(file, newfile)
-          
           system(paste("ncks -O --fl_fmt=netcdf4", newfile, newfile))   # netCDF4
           system(paste("ncpdq -O -U" , newfile, newfile)) 
         }else{
@@ -59,6 +59,10 @@ met2CF.NARR <- function(in.path, in.prefix, outfolder) {
       }
       
     }
+    nc <- nc_open(newfile, write=TRUE)
+    ncvar_rename(nc, "lat", "latitude")
+    ncvar_rename(nc, "lon", "longitude")
+    nc_close(nc)
   }
   
   
