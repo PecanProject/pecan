fc5924a793c90df5ddb024facb1c44a19b2eeec7
Betsy Cowdery <emcowdery@gmail.com>
2015-01-27 18:34:48 -0500

    return result dataframe

--- modules/data.atmosphere/R/extract.nc.R
+++ modules/data.atmosphere/R/extract.nc.R
@@ -35,12 +35,26 @@ extract.nc <- function(in.path,in.prefix,outfolder,start_date,end_date,slat,slon
   y <- close$y
   print(c(x,y))
   
+  start_year <- year(input$start_date)
+  end_year <- year(input$end_date)
+  rows <- length(files)
+  results <- data.frame(file=character(rows), host=character(rows),
+                        mimetype=character(rows), formatname=character(rows),
+                        startdate=character(rows), enddate=character(rows),
+                        stringsAsFactors = FALSE)
+  
   for(i in 1:length(files)){    
     infile = file.path(in.path,files[i])
     outfile = file.path(outfolder,files[i])
     if(file.exists(infile)==TRUE && file.exists(outfile)==FALSE){
-      system(paste0("ncks -d x,",x,",",x, " -d y,",y,",",y," ",infile," ",outfile))
+      system(paste0("ncks -d x,",x,",",x, " -d y,",y,",",y," ",infile," ",outfile))      
     }
+    results$file[i] <- outfile
+    results$host[i] <- fqdn()
+    results$startdate[i] <- start_date
+    results$enddate[i] <- end_date
+    results$mimetype[i] <- 'application/x-netcdf'
+    results$formatname[i] <- 'CF'
   }
-  print(dir(outfolder))
+  invisible(results)
 }
