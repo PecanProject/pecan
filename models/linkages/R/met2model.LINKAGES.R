b01431f3d2e1505447309adcabd20bf3a0e8152b
Betsy Cowdery <emcowdery@gmail.com>
2015-02-11 16:00:55 -0500

    added results to met2model.LINKAGES along with a change in arguments and small fixes

--- models/linkages/R/met2model.LINKAGES.R
+++ models/linkages/R/met2model.LINKAGES.R
@@ -18,19 +18,55 @@
 ##' @param outfolder location where model specific output is written.
 ##' @return OK if everything was succesful.
 ##' @export
-##' @author Ann Raiho
+##' @author Ann Raiho, Betsy Cowdery
 ##-------------------------------------------------------------------------------------------------#
-met2model.LINKAGES <- function(in.path, in.prefix=NULL, start.year, end.year, outfolder) {
+met2model.LINKAGES <- function(in.path, in.prefix, outfolder, start_date, end_date, ..., overwrite=FALSE,verbose=FALSE) {
    
-  out.file <- file.path(paste0(outfolder,"test_text1.txt"))
+  start_date <- as.POSIXlt(start_date, tz = "GMT")
+  end_date<- as.POSIXlt(end_date, tz = "GMT")
+  out.file <- file.path(outfolder, paste(in.prefix,
+                                         strptime(start_date, "%Y-%m-%d"),
+                                         strptime(end_date, "%Y-%m-%d"),
+                                         "dat", sep="."))
   
-  year = seq(start.year,end.year,1)
+  results <- data.frame(file=c(out.file),
+                        host=c(fqdn()),
+                        mimetype=c('text/plain'),
+                        formatname=c('DALEC meteorology'),
+                        startdate=c(start_date),
+                        enddate=c(end_date),
+                        stringsAsFactors = FALSE)
+  print("internal results")
+  print(results)
+  
+  if (file.exists(out.file) && !overwrite) {
+    logger.debug("File '", out.file, "' already exists, skipping to next file.")
+    return(invisible(results))
+  }
+  
+  require(ncdf4)
+  require(lubridate)
+  require(PEcAn.data.atmosphere)
+  #  require(ncdf)
+  
+  ## check to see if the outfolder is defined, if not create directory for output
+  if(!file.exists(outfolder)){
+    dir.create(outfolder)
+  }
+  
+  out <- NULL
+  
+  # get start/end year since inputs are specified on year basis
+  start_year <- year(start_date)
+  end_year <- year(end_date)
+  
+  year = seq(start_year,end_year,1)
   
   month_matrix_precip = matrix(NA,length(year),12)
   julian_vec_hr = c(1,c(32,60,91,121,152,182,213,244,274,305,335,365)*4)
   
   for(i in 1:length(year)){
-    ncin <- nc_open(paste(in.path,year[i],".nc",sep=""))
+    ncin <- nc_open(file.path(in.path,paste0(in.prefix,".",year[i],".nc")))
     #print(ncin)
     ncprecipf = ncvar_get(ncin, "precipitation_flux") #units are kg m-2 s-1    
     for(m in 1:12){
@@ -46,8 +82,8 @@ met2model.LINKAGES <- function(in.path, in.prefix=NULL, start.year, end.year, ou
   
   for(i in 1:length(ipolat_nums)){
     for(m in 1:12){
-      mean_ncprecipf_mm[i,m] = mean(month_matrix[ipolat_nums[i],m],month_matrix[ipolat_nums[i]-1,m])
-      sd_ncprecipf_mm[i,m] = sd(c(month_matrix[ipolat_nums[i],m],month_matrix[ipolat_nums[i]-1,m]))
+      mean_ncprecipf_mm[i,m] = mean(month_matrix_precip[ipolat_nums[i],m],month_matrix_precip[ipolat_nums[i]-1,m])
+      sd_ncprecipf_mm[i,m] = sd(c(month_matrix_precip[ipolat_nums[i],m],month_matrix_precip[ipolat_nums[i]-1,m]))
     }  
   }
   
@@ -57,7 +93,7 @@ met2model.LINKAGES <- function(in.path, in.prefix=NULL, start.year, end.year, ou
   month_matrix_temp_mean = matrix(NA,length(year),12)
   
   for(i in 1:length(year)){
-    ncin <- nc_open(paste(in.path,year[i],".nc",sep=""))
+    ncin <- nc_open(file.path(in.path,paste0(in.prefix,".",year[i],".nc")))
     #print(ncin)
     nctemp = ncvar_get(ncin, "air_temperature") #units are kg m-2 s-1    
     for(m in 1:12){
@@ -79,5 +115,5 @@ met2model.LINKAGES <- function(in.path, in.prefix=NULL, start.year, end.year, ou
   sd_nctemp_C <- round(sd_nctemp, digits = 1)
   
   write.table(rbind(mean_nctemp_C,sd_nctemp_C,mean_ncprecipf_cm,sd_ncprecipf_cm),out.file,quote = FALSE,sep=",",col.names=FALSE,row.names=FALSE)
-  
+  invisible(results)
 }
