6e7cc5da6359f9003704e83931b7d95fa4c80a47
Alexey Shiklomanov <ashiklom@geo.bu.edu>
2015-02-12 18:44:48 -0500

    Temporary change for autoburnin. Non-functional

--- modules/rtm/R/results_get.R
+++ modules/rtm/R/results_get.R
@@ -17,28 +17,27 @@
 library(coda)
 library(data.table)
 
-get.results <- function(path, burnin=5e4, thin=100, species="All"){
-        flist <- list.files(path)
-        if(species != "All") flist <- flist[grep(species, flist)]
-        flist <- flist[which(nchar(flist) > 5)]
-        species.list <- unique(gsub("(.*)_[0-9][.][0-9]+_.*", "\\1", flist))
-        
-        relist <- c("none", "plot", "leaf")
-        
-        for (species in species.list){
-                for (re in relist){
-                        fset <- flist[grep(sprintf("%s_.*_%s_.*", species, re), flist)]
-                        print(fset)
-                        if(length(fset) > 0){
-                                speclist <- lapply(fset, function(x) data.frame(fread(paste(path, x, sep=''), 
-                                                                              header=TRUE)))
-                                min.spec <- min(sapply(speclist, nrow))
-                                samples <- seq(burnin, min.spec, by=thin)
-                                print(sprintf("%d samples", length(samples)*length(fset)))
-                                speclist <- lapply(speclist, "[", samples,,drop=FALSE)
-                                speclist <- lapply(speclist, mcmc)
-                                assign(sprintf("%s.%s.l", species, re), mcmc.list(speclist), envir = .GlobalEnv)
-                        }
-                }
+get.results <- function(path, species, burnin=5e4, thin=100){
+    print(sprintf("Loading species %s...", species))
+    flist <- list.files(path)
+    speciesre <- sprintf("^%s_.*\\.dat", species)
+    fset <- flist[grep(speciesre, flist)]
+    fset <- fset[which(nchar(fset) > 5)]
+    if(length(fset) > 0){
+        speclist <- lapply(fset, function(x) data.frame(fread(paste(path, x, sep=''), 
+                                                              header=TRUE)))
+        min.spec <- min(sapply(speclist, nrow))
+        if(min.spec < 5000){
+            print("Insufficient samples found. Bad run")
+            return(NA)
         }
+        samples <- seq(burnin, min.spec, by=thin)
+        print(sprintf("%d chains x %d samples = %d total",
+                      length(fset),
+                      length(samples),
+                      length(samples)*length(fset)))
+        speclist <- lapply(speclist, "[", samples,,drop=FALSE)
+        speclist <- lapply(speclist, mcmc)
+        return(speclist)
+    }
 }
