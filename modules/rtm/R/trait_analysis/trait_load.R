2a729df367e06e029a7e1b2d8a0c26487d14107d
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2015-03-04 02:42:16 -0500

    Finished "trait_load" script.
    Identified missing inversion spectra. Put them in missing.txt.

--- modules/rtm/R/trait_analysis/trait_load.R
+++ modules/rtm/R/trait_analysis/trait_load.R
@@ -1,5 +1,6 @@
 ##' Load traits into table
 library(reshape2)
+library(data.table)
 
 ### Setup paths
 PATH.data = "~/Documents/Dropbox/NASA_TE_PEcAn-RTM_Project/Data"
@@ -8,8 +9,8 @@ PATH.data.FFT.d15N = file.path(PATH.data.FFT, "NASA_FFT_d15N_ANALYZED_DATA_UPDAT
 PATH.data.FFT.lignin = file.path(PATH.data.FFT, "NASA_FFT_FIB_LIG_CELL_RESULTS_FINAL_4R.csv")
 PATH.data.FFT.CN = file.path(PATH.data.FFT, "NASA_FFT_Project_CN_Data_4R.csv")
 PATH.data.FFT.SLA_LMA = file.path(PATH.data.FFT, "NASA_FFT_SLA_LMA_Data_v2_4R_updated_new.csv")
-PATH.results = "~/Documents/Dropbox/NASA_TE_PEcAn-RTM_Project/Results/FFT no random effects/"
-PATH.results.summary = file.path(PATH.results, "fft_summary.csv")
+PATH.results = "~/Documents/Dropbox/run_results/results_FFT0203.Rdata"
+PATH.spectra = "~/Documents/Dropbox/FFT_spectra/NASA_FFT_LC_Refl_Spectra_v4.csv"
 
 ### Read data
 FFT.d15N <- read.csv(PATH.data.FFT.d15N, header=TRUE, stringsAsFactors = FALSE)
@@ -25,21 +26,9 @@ FFT.SLA$EWT_g_cm2[FFT.SLA$EWT_g_cm2 < 0] <- NA
 FFT.SLA$LMA_g_DW_cm2[FFT.SLA$LMA_g_DW_cm2 < 0] <- NA
 
 ### Subset necessary columns
-mergeby.caps <- c("SITE",
-                  "PLOT",
-                  "SITE_PLOT",
-                  "SPECIES",
-                  "HEIGHT",
-                  "AGE",
-                  "SAMPLE_NAME",
+mergeby.caps <- c("SAMPLE_NAME",
                   "SAMPLE_YEAR")
-mergeby.lower <- c("Site",
-                   "Plot",
-                   "Site_Plot",
-                   "Species",
-                   "Height",
-                   "Age",
-                   "Sample_Name",
+mergeby.lower <- c("Sample_Name",
                    "Sample_Year")
 
 FFT.d15N.sub <- FFT.d15N[,c(mergeby.caps,
@@ -59,8 +48,19 @@ FFT.SLA.sub <- FFT.SLA[, c(mergeby.lower,
 ### Merge into large data file
 FFT.p1 <- merge(FFT.d15N.sub, FFT.lignin.sub, by=mergeby.caps, all=TRUE)
 FFT.p2 <- merge(FFT.CN.sub, FFT.SLA.sub, by=mergeby.lower, all=TRUE)
-FFT.all <- merge(FFT.p1, FFT.p2, by.x=mergeby.caps, by.y=mergeby.lower, all=TRUE)
+FFT.all <- merge(FFT.p2, FFT.p1, by.x=mergeby.lower, by.y=mergeby.caps, all=TRUE)
+FFT.all <- data.table(FFT.all)
+
+## Average duplicates
+FFT2 <- FFT.all[, lapply(.SD, mean, na.rm=TRUE), by=c("Sample_Name", "Sample_Year")]
 
 #### Load results
-#FFT.results <- read.table(PATH.results.summary, header=TRUE)
-#FFT.all <- merge(FFT.results, FFT.sp.mean, by="Group.1")
+## Append information from original spectra to results table
+load(PATH.results)
+setnames(results, "leaf", "Spectra")
+info.orig <- fread(PATH.spectra, header=TRUE, select = c(1:9,12,13))
+results <- merge(info.orig, results, by="Spectra")
+
+## Merge results with traits
+fftdat <- merge(x=results, y=FFT2, by=mergeby.lower, all=TRUE)
+
