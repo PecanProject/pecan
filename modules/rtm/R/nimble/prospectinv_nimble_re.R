6883ab4ec1b8acac36627adf8b62539740db1bb6
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2015-03-10 10:52:53 -0400

    Working but slow random effects in NIMBLE.

--- modules/rtm/R/nimble/prospectinv_nimble_re.R
+++ modules/rtm/R/nimble/prospectinv_nimble_re.R
@@ -1,125 +1,74 @@
 ### PROSPECT code and inversion, implemented in Nimble
-library(nimble)
-load("../data/prospect4.Rdata")
+library(data.table)
+source("nimble/nimblefuncs/nim_setup_re.R")
 
-### Initial setup
-n.iter <- 1
+### Load spectrum
+if (!exists("TEST")){
+        args <- commandArgs(trailingOnly=TRUE)
+        specname <- args[1]
+        ngibbs <- as.numeric(args[2])
+        foldername <- args[3]
+        runid <- args[4]
+} else {
+        specname <- "JUNI"
+        foldername <- "NIMBLE_TEST_RE"
+        runid <- "TEST_RE"
+        ngibbs <- 100
+}
+
+path.to.spec <- "../data/FFT_spectra/NASA_FFT_LC_Refl_Spectra_v4.csv"
+fullspec <- fread(path.to.spec, header=TRUE, drop=22:71)
+obs.spec <- t(as.matrix(fullspec[Species == specname, 22:ncol(fullspec),
+                                 with=FALSE]))
+leaf.names <- fullspec[Species == specname,
+                     sprintf("%s_%s", Spectra, Sample_Year)]
+dir.create(sprintf("../run_results/%s", foldername), showWarnings = FALSE)
+filename <- sprintf("../run_results/%s/%s_%s.dat", foldername, specname, runid)
+
 AI <- 3
 
-### Bayesian inversion
-prospectCode <- nimbleCode({
-        ### Priors
-        # Global mean PROSPECT parameters
-        Ni ~ dlnorm(-0.916, 2.2)
-        N.m <- Ni + 1
-        Cab.m ~ dlnorm(3.4, 0.9)
-        Cw.m ~ dlnorm(-6.377, 0.5)
-        Cm.m ~ dlnorm(-5.116, 0.9)
-
-        # Random effects
-        for(i in 1:nspec){
-                sp.N[i] ~ dnorm(0, sp.N.t)
-                sp.Cab[i] ~ dnorm(0, sp.Cab.t)
-                sp.Cw[i] ~ dnorm(0, sp.Cw.t)
-                sp.Cm[i] ~ dnorm(0, sp.Cm.t)
-        }
-        sp.N.t ~ dgamma(0.1, 0.1)
-        sp.Cab.t ~ dgamma(0.1, 0.1)
-        sp.Cw.t ~ dgamma(0.1, 0.1)
-        sp.Cm.t ~ dgamma(0.1, 0.1)
-
-        # Residual
-        resp ~ dgamma(0.001, 0.001)
-
-        ### Model variables
-        for (sp in 1:nspec){
-                N[sp] <- N.m + sp.N[sp]
-                if(N[sp] < 1.001){
-                        N[sp] <- 1.001
-                }
-                Cab[sp] <- Cab.m + sp.Cab[sp]
-                Cw[sp] <- Cw.m + sp.Cw[sp]
-                Cm[sp] <- Cm.m + sp.Cm[sp]
-        }
-
-
-})
-
-prospectConstants <- list(Cab_abs = prospect4.dat$Cab_abs,
-                          Cw_abs = prospect4.dat$Cw_abs,
-                          Cm_abs = prospect4.dat$Cm_abs,
-                          tao1 = prospect4.dat$tao1,
-                          tao2 = prospect4.dat$tao2,
-                          rho1 = prospect4.dat$rho1,
-                          rho2 = prospect4.dat$rho2,
-                          x = prospect4.dat$x,
-                          y = prospect4.dat$y,
-                          e1 = expint_coefs$e1,
-                          e2 = expint_coefs$e2,
-                          observed = obs.spec,
-                          nspec = ncol(obs.spec),
-                          wl = nrow(obs.spec))
-
-prospectInits <- list(N.m = 1.4,
-                      Cab.m = 30,
-                      Cw.m = 0.01,
-                      Cm.m = 0.006,
-                      resp = 0.5,
-                      sp.N.t = 0.1,
-                      sp.Cab.t = 3,
-                      sp.Cw.t = 0.001,
-                      sp.Cm.t = 0.0006)
+prospectConstants$observed <- obs.spec
+prospectConstants$nspec <- ncol(obs.spec)
+prospectConstants$wl <- nrow(obs.spec)
 
 prospect <- nimbleModel(code = prospectCode,
                         name = "prospect",
                         constants = prospectConstants,
                         inits = prospectInits)
+
 prospectNodes <- prospect$getNodeNames()
 
-prospectSpec <- configureMCMC(prospect,
-                              nodes = prospectNodes[grep("*\\.t", prospectNodes)],
-                              print=TRUE)
-
-samplers.to.add <- prospectNodes[grep("(Ni)|(Cab\\.m)|(Cw\\.m)|(Cm\\.m)|\
-                                 (sp\\.N\\[)|(sp\\.Cab\\[)|(sp\\.Cw\\[)|(sp\\.Cm\\[)",
-                                      prospectNodes)]
+prospectSpec <- configureMCMC(prospect, print=TRUE)
+prospectSpec$removeSamplers(prospectNodes %in% prospectNodes)
 
 ### Load custom samplers
-source("nimblefuncs/LL_mod_re.R")
-source("nimblefuncs/resp_long_re.R")
+source("nimble/nimblefuncs/LL_mod_re.R")
+source("nimble/nimblefuncs/resp_long_re.R")
 
 specialized_prospect_LL <- prospect_LL(prospect, prospectConstants)
 
-for (s in samplers.to.add){
+sampler.pars <- c("mu.N", "mu.Cab", "mu.Cw", "mu.Cm")
+random.effects <- sprintf("alpha.%s[%d]",
+                          rep(c("N", "Cab", "Cw", "Cm"), each=ncol(obs.spec)),
+                          1:ncol(obs.spec))
+                          
+for (i in c(sampler.pars, random.effects)){
         prospectSpec$addSampler(type = "RW_llFunction",
-                        control = list(targetNode = s,
-                                       llFunction = specialized_prospect_LL,
-                                       includesTarget = FALSE,
-                                       adaptInterval = AI))
+                                control = list(targetNode = i,
+                                               llFunction = specialized_prospect_LL,
+                                               includesTarget = FALSE,
+                                               adaptInterval = AI))
 }
-
 prospectSpec$addSampler(type = "resp", control = list(targetNode = "resp"))
 
-prospectSpec$addMonitors(c("N.m", "Cab.m", "Cw.m", "Cm.m", "resp", "sp.N[1]", "sp.N[2]"))
+prospectSpec$addMonitors(c("alpha.N", "alpha.Cab", "alpha.Cw", "alpha.Cm"))
 prospectMCMC <- buildMCMC(prospectSpec, project = prospect)
 
-### Compilation
-print("Begin compilation")
 prosProj <- compileNimble(prospect, prospectMCMC)
-print("Compilation complete!")
 
 ### Run MCMC
-print("Begin MCMC...")
-sw <- system.time(prosProj$prospectMCMC$run(n.iter))
-print("MCMC complete!")
-print(sw)
+timer <- system.time(prosProj$prospectMCMC$run(ngibbs))
+samples <- as.matrix(prosProj$prospectMCMC$mvSamples)
 
-### Check output
-samples1 <- as.matrix(prosProj$prospectMCMC$mvSamples)
-print(samples1)
-par(mfrow=c(3,2))
-plot(samples1[,"N.m"], type='l')
-plot(samples1[,"Cab.m"], type='l')
-plot(samples1[,"Cw.m"], type='l')
-plot(samples1[,"Cm.m"], type='l')
-plot(samples1[,"resp"], type='l')
+write.csv(samples, filename, row.names=FALSE)
+
