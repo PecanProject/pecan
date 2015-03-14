db048ff3e40ef71a0f751e9bce24d748854a4edb
David LeBauer <dlebauer@gmail.com>
2015-03-13 20:39:46 -0500

    added new function 'get.id' to simplify code used to query ids from various tables in the db.insert.input functions

---
+++ db/tests/testthat/test.db.utils.R
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2012 University of Illinois, NCSA.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the 
+# University of Illinois/NCSA Open Source License
+# which accompanies this distribution, and is available at
+# http://opensource.ncsa.illinois.edu/license.html
+#-------------------------------------------------------------------------------
+context("Testing utility functions")
+
+con <- db.open(list(driver = "PostgreSQL", user = "bety", dbname = "bety", password = "bety"))
+
+test_that("get.id works on some tables, and with different inputs", {
+  pftid <- get.id("pfts", "name", "salix", con)
+  expect_is(pftid, "numeric")
+  
+  pftname <- 'ebifarm.salix'
+  modeltypeid <- 1
+  pftid <- get.id("pfts", c("name", "modeltype_id"), c(pftname, modeltypeid), con)
+  pft <- db.query(paste0("select name, modeltype_id from pfts where id = ", pftid), con)
+  expect_equal(pft$name, pftname)
+  expect_equal(pft$modeltype_id, modeltypeid)
+}
