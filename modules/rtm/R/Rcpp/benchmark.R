86a42754de99951849586da4a72e1706715fd9be
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2015-03-16 14:56:28 -0400

    Some optimization of basic script, with benchmarking.

---
+++ modules/rtm/R/Rcpp/benchmark.R
@@ -0,0 +1,6 @@
+# Benchmarking 
+
+# Default R implementation
+SpecError_R = function(Model, Observed) Model - Observed
+
+# 
--- modules/rtm/R/Rcpp/rcpp_inversion.R
+++ modules/rtm/R/Rcpp/rcpp_inversion.R
@@ -34,13 +34,30 @@ pinvbayes <- function(ngibbs, Observed){
     priorCm = function(logCm) dnorm(logCm, -5.116, 0.9)
 
     # Shortcut functions
-    SpecError = function(Model, Observed) Model - Observed
-    cppFunction("double Likelihood(NumericMatrix Error, double rsd){
+#     SpecError = function(Model, Observed) Model - Observed
+    cppFunction("NumericMatrix SpecError(NumericVector Model,
+										NumericMatrix Observed){
+				int nspec = Observed.ncol();
+				NumericMatrix E(2101,nspec);
+				for(int i; i<nspec; i++){
+					E(_,i) = Model - Observed(_,i);
+				}
+				return E;
+    			}")
+    
+	cppFunction("double Likelihood(NumericMatrix Error, double rsd){
             NumericVector LL;
             LL = dnorm(Error, 0, rsd, true);
             return sum(LL);
     }")
-        #function(Error, rsd) sum(dnorm(Error, 0, rsd, TRUE))
+    
+	cppFunction("double sample_rsd(NumericMatrix PrevError, double rp1){
+    			double rinv, rp2;
+				rp2 = 0.001 + sum(PrevError*PrevError)/2;
+    			rinv = rgamma(1, rp1, rp2)[0];
+    			return 1/sqrt(rinv);
+    			}")
+    
     rp1 = 0.001 + nspec*wl/2
 
     # Precalculate first model
@@ -65,18 +82,33 @@ pinvbayes <- function(ngibbs, Observed){
             N = TN
             PrevError = TryError
         }
-
+        
+        # Sample Cab
+        TlogCab = rnorm(1, logCab, JumpCab)
+        TCab = exp(TlogCab) + 1
+        TrySpec = prospect4(N, TCab, Cw, Cm)
+        TryError = SpecError(TrySpec, Observed)
+        TryPost = Likelihood(TryError, rsd) + priorCab(TlogCab)
+        PrevPost = Likelihood(PrevError, rsd) + priorCab(logCab)
+        a = exp(TryPost - PrevPost)
+        if(a > runif(1)){
+        	logCab = TlogCab
+        	Cab = TCab
+        	PrevError = TryError
+        }
+        
         # Sample rsd
-        rp2 = 0.001 + sum(PrevError*PrevError)/2
-        rinv = rgamma(1, rp1, rp2)
-        rsd = 1/sqrt(rinv)
+#         rp2 = 0.001 + sum(PrevError*PrevError)/2
+#         rinv = rgamma(1, rp1, rp2)
+#         rsd = 1/sqrt(rinv)
+		rsd = sample_rsd(PrevError, rp1)
 
         Results[ng, ] = c(N, Cab, Cw, Cm, rsd)
     }
     return(Results)
 }
 
-Rprof(memory.profiling = TRUE)
-z <- pinvbayes(1000, obs.spec)
+Rprof(memory.profiling = TRUE, line.profiling = TRUE)
+z <- pinvbayes(200, obs.spec)
 Rprof(NULL)
 print(summaryRprof())
