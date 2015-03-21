e4a5cd319d171cbd16fe5f6e793377e72e7c6123
Alexey Shiklomanov <ashiklom@geo.bu.edu>
2015-03-20 22:59:31 -0400

    Removed non-package files.
    Moved into prospect_bayes repository, which is now exclusively for analysis (loads this package for the process).

--- modules/rtm/dev/old_scripts/scripts/install_nimble.R
+++
@@ -1,5 +0,0 @@
-require(devtools)
-#install_github('nimble-dev/nimble/packages/nimble@devel',
-#               args="--enable-lib=true")
-install.packages("~/Downloads/nimble_0.3-1.tar.gz", repos=NULL, type="source",
-                 configure.args="--enable-lib=true")
--- modules/rtm/dev/old_scripts/scripts/sync_results.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-## Synchronize (rsync) results between cluster and Dropbox run_results folder.
-
-rsync -r --progress ashiklom@geo.bu.edu:ashiklom-dietzelab/pecan/modules/rtm/run_results/FFT_Jan27/ $HOME/Documents/Dropbox/run_results/FFT_Jan27
-echo Done!
