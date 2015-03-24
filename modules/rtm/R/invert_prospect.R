2142b9937f24ebf6af58fcd9cc336ab3c1cb2540
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2015-03-24 12:52:03 -0400

    Added basic documentation to R functions

--- modules/rtm/R/invert_prospect.R
+++ modules/rtm/R/invert_prospect.R
@@ -1,4 +1,22 @@
-## Default inversion
+##' @name invert_prospect
+##' @title Basic Bayesian inversion of PROSPECT4 model
+##' @author Alexey Shiklomanov
+##' @param spectra A matrix of reflectance spectra with dimensions 2101 x n,
+##'     where n is the number of spectra. Vectors are coerced to matrices.
+##' @param ngibbs The number of MCMC steps for the iteration. Default is 15000.
+##'     Convergence for good, individual spectra happens around 5000 steps.
+##' @param adapt How often to adapt the Jump distribution, in MCMC steps. Default = 25
+##' @param min_adapt Minimum value to adapt by. This prevents the Jump distribution variance
+##'     from going to zero when no values are accepted. Default is 0.05.
+##' @return Matrix of samples of each PROSPECT parameter and residual variance
+##' @examples
+##' data(testspec)
+##' samples <- invert_prospect(testspec_ACRU[,1], 1000)
+##' par(mfrow=c(2,2))
+##' plot(samples[,1], type='l', ylab="N")
+##' plot(samples[,2], type='l', ylab="Cab")
+##' plot(samples[,3], type='l', ylab="Cw")
+##' plot(samples[,4], type='l', ylab="Cm")
 
 invert_prospect <- function(spectra,
                              ngibbs = 15000,
@@ -10,6 +28,28 @@ invert_prospect <- function(spectra,
     return(out)
 }
 
+##' @name invert_prospect_re
+##' @title Bayesian inversion of PROSPECT4 model with random effects
+##' @author Alexey Shiklomanov
+##' @param spectra A matrix of reflectance spectra with dimensions 2101 x n,
+##'     where n is the number of spectra. Vectors are coerced to matrices.
+##' @param ngibbs The number of MCMC steps for the iteration. Default is 15000.
+##'     Convergence for good, individual spectra happens around 5000 steps.
+##' @param adapt How often to adapt the Jump distribution, in MCMC steps. Default = 25
+##' @param min_adapt Minimum value to adapt by. This prevents the Jump distribution variance
+##'     from going to zero when no values are accepted. Default is 0.05.
+##' @return Matrix of samples of each PROSPECT parameter, residual variance, and
+##'     random effects variance for each parameter, and random effects values
+##'     for each leaf.
+##' @examples
+##' data(testspec)
+##' samples <- invert_prospect_re(testspec_ACRU[,1:5], 1000)
+##' dim(samples)
+##' par(mfrow=c(2,2))
+##' plot(samples[,1], type='l', ylab="N")
+##' plot(samples[,2], type='l', ylab="Cab")
+##' plot(samples[,3], type='l', ylab="Cw")
+##' plot(samples[,4], type='l', ylab="Cm")
 invert_prospect_re <- function(spectra,
 							   ngibbs = 15000,
 							   adapt = 25,
@@ -19,10 +59,26 @@ invert_prospect_re <- function(spectra,
 	return(out)
 }
 
-invert_prospect_MLE <- function(observed){
+##' @name invert_prospect_MLE
+##' @title Simple maximum likelihood inversion of PROSPECT4
+##' @details MLE inversion based on log of sum of squares of error.
+##' @author Alexey Shiklomanov
+##' @param spectra A matrix of reflectance spectra with dimensions 2101 x n,
+##'     where n is the number of spectra.
+##' @return Vector of maximum likelihood values of PROSPECT parameters
+##' @examples
+##' data(testspec)
+##' fit <- invert_prospect_MLE(testspec_ACRU[,1])
+##' plot(400:2100, prospect4([fit[1], fit[2], fit[3], fit[4]),
+##'     type='l',
+##'     xlab="Wavelength (nm)",
+##'     ylab="Reflectance")
+##' lines(400:2100, testspec_ACRU[,1], col=2)
+
+invert_prospect_MLE <- function(spectra){
 	merit <- function(pars){
 		spec <- prospect4(pars[1], pars[2], pars[3], pars[4])
-		return(log(sum((spec-observed)^2)))
+		return(log(sum((spec-spectra)^2)))
 	}
 	fit <- optim(c(1.4, 30, 0.01, 0.01, 0.5), merit)
 	return(fit$par)
--- modules/rtm/R/prospect.R
+++ modules/rtm/R/prospect.R
@@ -1,8 +1,12 @@
-## Function to generate PROSPECT spectra
-
-# transmittance=FALSE (default) returns reflectance; 
-# 	otherwise, returns reflectance
-
+##' @name prospect4
+##' @title PROSPECT4 model
+##' @author Alexey Shiklomanov
+##' @param N Effective number of leaf layers (>1)
+##' @param Cab Leaf chlorophyll content (>0)
+##' @param Cw Leaf water content (>0)
+##' @param Cm Leaf dry matter content (>0)
+##' @param transmittance Return transmittance instead of reflectance (default = FALSE)
+##' @return Vector (length 2100) of simulated reflectance/transmittance values from 400:2100 nm
 prospect4 <- function(N, Cab, Cw, Cm, transmittance=FALSE){
 	reflectance <- as.integer(!transmittance)
 	data(prospect4)
