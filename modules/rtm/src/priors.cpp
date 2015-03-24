6c86a8391d58c907524c0dc3ce4d77d69b1070c6
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2015-03-24 13:15:17 -0400

    Confined PROSPECT4 priors to a single function.

--- modules/rtm/src/priors.cpp
+++ modules/rtm/src/priors.cpp
@@ -2,23 +2,17 @@
 #include <Rmath.h>
 using namespace Rcpp;
 
-// N is halfnormal, rest are lognormal
-double priorN(double N){
-    double p =  R::dnorm(N-1, 0.916, 2.2, 1) + log(2);
-    return p;
-}
-
-double priorCab(double Cab){
-    double p = R::dnorm(log(Cab), 3.4, 0.9, 1);
-    return p;
-}
-
-double priorCw(double Cw){
-    double p= R::dnorm(log(Cw), -6.377, 0.5, 1);
-    return p;
-}
-
-double priorCm(double Cm){
-    double p = R::dnorm(log(Cm), -5.116, 0.9, 1);
+// Priors for PROSPECT4 model
+double prospect4_priors(int param, double value){
+    double p;
+    if(param == 0){         // N -- Halfnormal
+        p = R::dnorm(value-1, 0.916, 2.2, 1) + log(2);
+    } else if(param == 1){  // Cab -- Lognormal
+        p = R::dnorm(log(value), 3.4, 0.9, 1);
+    } else if(param == 2){  // Cw -- Lognormal
+        p= R::dnorm(log(value), -6.377, 0.5, 1);
+    } else if(param == 3){  // Cm -- Lognormal
+        p = R::dnorm(log(value), -5.116, 0.9, 1);
+    }
     return p;
 }
