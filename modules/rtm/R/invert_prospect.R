bbb765530318fa2e71d83fe61cf15e6e3ac1bb67
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2015-03-24 17:13:23 -0400

    Fixed invert_prospect to pass correct arguments to inversion.

--- modules/rtm/R/invert_prospect.R
+++ modules/rtm/R/invert_prospect.R
@@ -19,11 +19,12 @@
 ##' plot(samples[,4], type='l', ylab="Cm")
 
 invert_prospect <- function(spectra,
-                             ngibbs = 15000,
-                             adapt = 25,
-                             min_adapt = 0.05){
+                            ngibbs = 15000,
+                            inits = c(1.4, 30, 0.01, 0.01),
+                            adapt = 25,
+                            min_adapt = 0.05){
     data(prospect4)
-    out <- pinvbayes(ngibbs, as.matrix(spectra), adapt, min_adapt, P4data)
+    out <- invert_RTM("prospect4", as.matrix(spectra), ngibbs, adapt, min_adapt, inits, P4data)
     colnames(out) <- c("N", "Cab", "Cw", "Cm", "rsd")
     return(out)
 }
