75ea54bdb6120bfcffbb7db8a931154f998e369f
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2015-03-25 00:58:23 -0400

    Fixed random effects convergence bug!
    One issue was that the minimum of my truncated normal was not set properly (pmin instead of pmin-value).
    
    Another was that I was not resetting my acceptance rate to zero when adjusting alpha_Jump, causing it to balloon as more cumulative values got accepted.

--- modules/rtm/src/statistical_inversion_RE.cpp
+++ modules/rtm/src/statistical_inversion_RE.cpp
@@ -37,7 +37,8 @@ NumericMatrix invert_RTM_re(
     int nre = re_values.ncol();
     int nwl = Observed.nrow();
     int npars = values.size();
-    NumericMatrix results(ngibbs, (npars+1)*nre);   // Set up results matrix
+    int ng;
+    NumericMatrix results(ngibbs, npars*(nre+2)+1);   // Set up results matrix
     printf("Results matrix is %d rows by %d columns \n", results.nrow(), results.ncol());
 
     // Pick RTM based on specified string
@@ -59,29 +60,35 @@ NumericMatrix invert_RTM_re(
 
     // Initialize Jump distribution
     NumericVector Jump = values * 0.05;  // Jump distribution vector - starts at 5% of initial conditions
-    NumericMatrix alpha_Jump = re_values * 0.05;    // Jump matrix for random effects
+    NumericVector alpha_Jump = Jump * 0.1;    // Jump vector for random effects
 
     // Define sampler parameters
-    double Tpar, JN, JD, a;
+    double Tpar, Tmin, JN, JD, a;
     NumericVector Tvec(npars);
-    NumericVector Talpha(nre);
     NumericMatrix TrySpec(nwl, nre);
     NumericMatrix TryError(nwl, nspec);
+    NumericVector TrySpec_alpha(nwl), TryError_alpha(nwl);
     double TryPost, PrevPost;
 
     // Define acceptance monitoring parameters
     NumericVector ar(npars), adj(npars);
+    NumericVector alpha_ar(npars), alpha_adj(npars);
     float adapt_count = 0;
 
     // MCMC loop
     printf("Begin MCMC \n");
-    for(int ng = 0; ng<ngibbs; ng++){
+    for(ng = 0; ng<ngibbs; ng++){
         // Adapt
         if(adapt - adapt_count < 1){
             adj = ar / adapt / 0.75;
             adj = ifelse(adj < adj_min, adj_min, adj);
             Jump = Jump * adj;
             ar = ar * 0;
+
+            alpha_adj = alpha_ar / nre / adapt / 0.75;
+            alpha_adj = ifelse(alpha_adj < adj_min, adj_min, alpha_adj);
+            alpha_Jump = alpha_Jump * alpha_adj;
+            alpha_ar = alpha_ar * 0;
             adapt_count = 0;
         }
 
@@ -96,34 +103,47 @@ NumericMatrix invert_RTM_re(
             JN = dtnorm(Tvec[p], values[p], Jump[p], pmin[p]);
             JD = dtnorm(values[p], Tvec[p], Jump[p], pmin[p]);
             a = exp((TryPost - JN) - (PrevPost - JD));
-            printf("Tpar: %g  values: %g  Jump: %g \n TL: %g  PL: %g \n",
-                Tvec[p],
-                values[p],
-                Jump[p],
-                Likelihood(TryError, rsd),
-                Likelihood(PrevError, rsd));
             if(a > runif(1)[0]){
                 values[p] = Tvec[p];
                 PrevError = TryError;
                 ar[p] = ar[p] + 1;
             }
-            results(ng, p) = values[p];
+            results(ng, p*(nre+2)) = values[p];
 
             // Sample model random effects
             for(int r = 0; r<nre; r++){
                 // Sample
-                Talpha = rtnorm_c(re_values(t,r), alpha_Jump(p), pmin(p)-values(p));
-                TrySpec_alpha = Model(#VECTOR#, );
-
+                Tmin = pmin[p] - values[p];
+                Tpar = rtnorm_c(re_values(p,r), alpha_Jump[p], Tmin);
+                Tvec = values + re_values(_,r);
+                Tvec[p] = values[p] + Tpar;
+                TrySpec_alpha = Model(Tvec, func_data);
+                TryError_alpha = TrySpec_alpha - Observed(_,r);
+                TryPost = Likelihood(TryError_alpha, rsd) + dtnorm_c(Tpar, 0, tau[p], Tmin);
+                PrevPost = Likelihood(PrevError(_,r), rsd) + dtnorm_c(re_values(p,r), 0, tau[p], Tmin);
+                JN = dtnorm_c(Tpar, re_values(p,r), alpha_Jump[p], Tmin);
+                JD = dtnorm_c(re_values(p,r), Tpar, alpha_Jump[p], Tmin);
+                a = exp((TryPost - JN) - (PrevPost - JD));
+                if(a > runif(1)[0]){
+                    re_values(p,r) = Tpar;
+                    PrevError(_,r) = TryError_alpha;
+                    alpha_ar[p] = alpha_ar[p] + 1;
+                }
+                results(ng, p*(nre+2)+r+1) = re_values(p,r);
             }
-        }
 
+            // Sample random effects SD
+            alpha_rp2[p] = 0.001 + sum(re_values(p,_)*re_values(p,_))/2;
+            tinv[p] = rgamma(1, alpha_rp1, 1/alpha_rp2[p])[0];
+            tau[p] = 1/sqrt(tinv[p]);
+            results(ng, (p+1)*(nre+2)-1) = tau[p];
+            }
+        
         // Sample residual SD
         rp2 = 0.001 + sum(PrevError * PrevError)/2;
         rinv = rgamma(1, rp1, 1/rp2)[0];
         rsd = 1/sqrt(rinv);
-        printf(" RSD: %f \n =======================\n", rsd);
-        results(ng, npars) = rsd;
+        results(ng, npars*(nre+2)) = rsd;
         adapt_count = adapt_count + 1;
     }
 
--- modules/rtm/tests/quicktest_re.R
+++ modules/rtm/tests/quicktest_re.R
@@ -1,7 +1,7 @@
 ## Quick test
 library(PEcAnRTM)
 data(testspec)
-ngibbs = 50
+ngibbs = 500
 tt <- system.time(z2 <- invert_prospect_re(testspec_ACRU[,1:5], ngibbs, 10))
 print(tt)
 
@@ -10,9 +10,6 @@ Thin = 2
 bt <- seq(ngibbs/2, ngibbs, by=Thin)
 z <- z2[bt,]
 
+plt <- c(1:6,8:13, 15:20, 22:27)
 par(mfrow=c(3,2))
-plot(z2[,1], type='l')
-plot(z2[,10], type='l')
-plot(z2[,14], type='l')
-plot(z2[,18], type='l')
-plot(z2[,22], type='l')
+for(p in plt){plot(z2[,p], type='l')}
