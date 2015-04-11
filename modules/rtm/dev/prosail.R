87460313c1caf8af7f3b00e2045f799654b18ca9
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2015-04-11 18:50:33 -0400

    Primitive R wrapper for PROSAIL.
    Doesn't quite work yet.

---
+++ modules/rtm/dev/prosail.R
@@ -0,0 +1,29 @@
+## Simple wrapper for PROSAIL
+dyn.load("src/prosail.so")
+
+prosp.def <- list(1.5, 40, 8, 0, 0.01, 0.009)
+
+sail.def = c(prosp.def,
+			 list(-0.35, -0.15, as.integer(1), 
+			 	 3, 0.01,
+			 	 30, 10, 0,
+			 	 1))
+
+
+prospect <- function(params){
+	r <- matrix(0,nrow=2101,ncol=2)
+	p <- c(list("PROSPECT_5B"), params, list(r))
+	f <- do.call(.Fortran,p)
+	out <- f[[7]]
+	return(out)
+}
+
+
+pro4sail <- function(params = sail.def){
+	r <- numeric(2101)
+	p <- c(list("PRO4SAIL"), params, rep(list(r),4))
+	lp <- length(p)
+	f <- do.call(.Fortran, p)
+	out <- matrix(f[[lp-3]], f[[lp-2]], f[[lp-1]], f[[lp]])
+	return(out)
+}
