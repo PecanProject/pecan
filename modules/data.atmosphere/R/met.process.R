515f57e648c744c72ff069b24072e7daeb143cb8
Betsy Cowdery <emcowdery@gmail.com>
2015-05-05 14:37:22 -0400

    met2CF functions can now take in additional arguments (helpful for when different met types require different arguments)

--- modules/data.atmosphere/R/met.process.R
+++ modules/data.atmosphere/R/met.process.R
@@ -114,9 +114,7 @@ met.process <- function(site, input_met, start_date, end_date, model, host, dbpa
   
   #--------------------------------------------------------------------------------------------------#
   # Change to  CF Standards
-  
-  # all met2CF funtions need to take additional arguments
-  
+    
   print("### Change to CF Standards")
   
   input.id  <-  raw.id[1]
