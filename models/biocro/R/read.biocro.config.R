af1f28c90abdfdf72f2f3628fd31b6b1aacd2b6f
David LeBauer <dlebauer@gmail.com>
2015-06-01 22:54:55 -0500

    began re-factoring regional biocro Rscript, export functions run.biocro and read.biocro.config

---
+++ models/biocro/R/read.biocro.config.R
@@ -0,0 +1,19 @@
+##' Read BioCro config file
+##'
+##' @title Read BioCro Config 
+##' @param config.file 
+##' @return list of run configuration parameters for PEcAn
+##' @export
+##' @author David LeBauer
+read.biocro.config <- function(config.file = "config.xml"){
+    config <- xmlToList(
+        xmlTreeParse(
+            file = file.path(rundir, "config.xml"),
+            handlers = list("comment" = function(x){NULL}),
+            asTree = TRUE)
+        )
+    config$pft$canopyControl$mResp <- ## hacky way of importing a vector from xml to a list
+        unlist(
+            strsplit(
+                config$pft$canopyControl$mResp, split = ","))
+}
