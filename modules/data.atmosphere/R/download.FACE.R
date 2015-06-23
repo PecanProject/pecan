2682241fe31e92937fd5e44d7d3140bb0c40c155
Betsy Cowdery <emcowdery@gmail.com>
2015-06-22 20:27:52 -0400

    updated FACE downloading function

--- modules/data.atmosphere/R/download.FACE.R
+++ modules/data.atmosphere/R/download.FACE.R
@@ -11,58 +11,46 @@
 ##' @author Betsy Cowdery
 
 download.FACE <- function(sitename,outfolder, start_date, end_date, overwrite=FALSE){
-# download.FACE <- function(data.set,outfolder,pkg,raw.host,start_year,end_year,site.id,dbparams,con){
+  # download.FACE <- function(data.set,outfolder,pkg,raw.host,start_year,end_year,site.id,dbparams,con){
   
-  n <- nchar(outfolder)
-  if(substr(outfolder,n,n) != "/") outfolder = paste0(outfolder,"/")
+  require(lubridate) #is this necessary?
+  start_date <- as.POSIXlt(start_date, tz = "GMT")
+  end_date <- as.POSIXlt(end_date, tz = "GMT")
   
-  # Check database for file, and instert if not already there.
-
-  site <- unlist(strsplit(data.set,"_"))[[2]]
+  site <- site_from_tag(sitename, "FACE")
+  
+  # make sure output folder exists
+  if(!file.exists(outfolder)){
+    dir.create(outfolder, showWarnings=FALSE, recursive=TRUE)
+  }
   
   raw.file <- paste0(site,"_forcing_h.nc")
-
-  cmd1 <- paste0("mkdir -p ", outfolder)
-  cmd2 <- paste0("wget -c ftp://cdiac.ornl.gov/.private/eCO2_Modelling/Site_Data/",site,"/", raw.file, " -O ",outfolder,"FACE_",raw.file)
+  out.file <- file.path(outfolder, paste0("FACE_",raw.file))
   
-  Rfcn     <- "pecan/scripts/Rfcn.R"
-  host     <- system("hostname",intern=TRUE)
+  # url where Ameriflux data is stored
+  url <- paste0("ftp://cdiac.ornl.gov/.private/eCO2_Modelling/Site_Data/",site,"/", raw.file)
+  system(paste(url, " -O ", out.file))
   
-  if(raw.host %in% c("localhost",host)){
-    ## if the machine is local, run conversion function
-    system(cmd1); system(cmd2)
-  }else{
-    ## if the machine is remote, run conversion remotely
-    usr = ifelse(username==NULL | username=="","",paste0(username,"@"))
-    system2("ssh",paste0(usr,paste(raw.host,cmd1)))
-    system2("ssh",paste0(usr,paste(raw.host,cmd2)))
-    years <- system2("ssh",paste0(usr,paste0(raw.host," ncdump -v 'YEAR' ",outfolder,"FACE_",raw.file)),stdout=TRUE)
-  }
+  results <- data.frame(file=out.file, 
+                        host=fqdn(),
+                        mimetype='application/x-netcdf', 
+                        formatname='FACE',
+                        startdate=start_date, 
+                        enddate=end_date,
+                        dbfile.name = 'FACE',
+                        stringsAsFactors = FALSE)
   
-  check <- input.name.check(data.set, con, dbparams)
-  if(is.null(check)==FALSE){
-    logger.error('Input is already in the database.')
-    raw.id <- check
-    return(raw.id)
-  }
+  invisible(results)
+  ######################
   
-  if(is.na(start_year) |is.na(end_year)){
-    j <- grep("YEAR =",years)
-    start_year <- as.numeric(substr(unlist(strsplit(years[j],","))[1],nchar(unlist(strsplit(years[j],","))[1])-4,nchar(unlist(strsplit(years[j],","))[1]) ))
-    end_year <- as.numeric(unlist(strsplit(years[length(years)-1],";"))[1])
-  }
+  #   if(is.na(start_year) |is.na(end_year)){
+  #     j <- grep("YEAR =",years)
+  #     start_year <- as.numeric(substr(unlist(strsplit(years[j],","))[1],nchar(unlist(strsplit(years[j],","))[1])-4,nchar(unlist(strsplit(years[j],","))[1]) ))
+  #     end_year <- as.numeric(unlist(strsplit(years[length(years)-1],";"))[1])   
+  #     start_date <- paste0(start_year,"-01-01 00:00:00")
+  #     end_date   <- paste0(end_year,"-12-31 23:59:00")
+  #   }
   
-  start_date <- paste0(start_year,"-01-01 00:00:00")
-  end_date   <- paste0(end_year,"-12-31 23:59:00")
   
-  type <- 'Input'  
-  formatname <- 'FACE'
-  mimetype <- 'application/x-netcdf'
   
-  #(filename, siteid, startdate, enddate, mimetype, formatname, parentid=NA, con, hostname=fqdn())
-  newinput <- dbfile.input.insert(outfolder,data.set, site.id, start_date, end_date, mimetype,formatname,parentid=NA,con=con,raw.host) 
-  
-  raw.id <- c(newinput$input.id)
-
-return(raw.id)
 } 
