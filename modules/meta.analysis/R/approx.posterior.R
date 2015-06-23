PecanProject/pecan

commit da4b2176b7dd0e0b44612f4d7d31e8d25bce4c6a

do not fit gamma for approx.posterior if trait = cuticular_cond; this is a hack until gh-223 is resolved

modules/meta.analysis/R/approx.posterior.R
@@ -10,6 +10,8 @@
 ##' Approximate the posterior MCMC with a closed form pdf
 ##'
 ##' returns priors where posterior MCMC are missing
+##' 
+##' NOTE: this function is similar to PEcAn.priors::fit.dist
 ##' @title Approximate posterior
 ##' @param trait.mcmc meta analysis outputs
 ##' @param priors dataframe of priors used in meta analysis
@@ -76,10 +78,13 @@ approx.posterior <- function(trait.mcmc, priors, trait.data=NULL, outdir=NULL){
       fit <- list()
       fit[[1]] <- suppressWarnings(fitdistr(dat,"exponential"))
       ## fit[[2]] <- fitdistr(dat,"f",list(df1=10,df2=2*mean(dat)/(max(mean(dat)-1,1))))
-      fit[[2]] <- suppressWarnings(fitdistr(dat, "gamma"))
-      fit[[3]] <- suppressWarnings(fitdistr(dat, "lognormal"))
-      fit[[4]] <- suppressWarnings(fitdistr(dat, "weibull"))
-      fit[[5]] <- suppressWarnings(fitdistr(dat, "normal"))
+      fit[[2]] <- suppressWarnings(fitdistr(dat, "lognormal"))
+      fit[[3]] <- suppressWarnings(fitdistr(dat, "weibull"))
+      fit[[4]] <- suppressWarnings(fitdistr(dat, "normal"))
+      if(!trait == 'cuticular_cond'){
+        fit[[5]] <- suppressWarnings(fitdistr(dat, "gamma"))
+      } 
+      
       fparm <- lapply(fit,function(x){as.numeric(x$estimate)})
       fAIC  <- lapply(fit,function(x){AIC(x)})
       
@@ -125,8 +130,7 @@ approx.posterior <- function(trait.mcmc, priors, trait.data=NULL, outdir=NULL){
       posteriors[trait,"parama"] <- mean(dat)
       posteriors[trait,"paramb"] <- sd(dat)
       if(do.plot){
-        rng <- range(dat)
-        if(!is.null(trait.data) && length(trait.data[[trait]]$Y)>1) rng = range(trait.data[[trait]]$Y)
+        rng <- quantile(dat, c(0.01, 0.99))
         x <- seq(rng[1], rng[2], length=1000)
         plot(density(dat), col=2, lwd=2, main = trait, xlim = rng)
         if(!is.null(trait.data)) {

commit 49cf8afbffdbf15cc57935989a336d5c81336000

fix approx.posterior.R
Commit da4b217 inadvertently caused fitted posterior to be misnamed.
Fixed here.

Also added try() statements to allow code to proceed even if one or
more candidate distributions could not be fit.

modules/meta.analysis/R/approx.posterior.R
@@ -54,7 +54,7 @@ approx.posterior <- function(trait.mcmc, priors, trait.data=NULL, outdir=NULL){
     }
     
     ## first determine the candidate set of models based on any range restrictions
-    zerobound = c("exp", "gamma", "lnorm", "weibull")
+    zerobound <- c("exp", "gamma", "lnorm", "weibull")
     if(pdist %in% "beta"){
       m <- mean(dat)
       v <- var(dat)
@@ -76,22 +76,26 @@ approx.posterior <- function(trait.mcmc, priors, trait.data=NULL, outdir=NULL){
       posteriors[trait,"parama"] <- fit$estimate[1]
       posteriors[trait,"paramb"] <- fit$estimate[2]
     } else if(pdist %in% zerobound | (pdist == "unif" & pparm[1] > 0)){
-
+      dist.names <- c("exp", "lnorm", "weibull", "norm")
       fit <- list()
-      fit[[1]] <- suppressWarnings(fitdistr(dat,"exponential"))
+      fit[[1]] <- try(suppressWarnings(fitdistr(dat,"exponential")), silent=TRUE)
       ## fit[[2]] <- fitdistr(dat,"f",list(df1=10,df2=2*mean(dat)/(max(mean(dat)-1,1))))
-      fit[[2]] <- suppressWarnings(fitdistr(dat, "lognormal"))
-      fit[[3]] <- suppressWarnings(fitdistr(dat, "weibull"))
-      fit[[4]] <- suppressWarnings(fitdistr(dat, "normal"))
+      fit[[2]] <- try(suppressWarnings(fitdistr(dat, "lognormal")), silent=TRUE)
+      fit[[3]] <- try(suppressWarnings(fitdistr(dat, "weibull")), silent=TRUE)
+      fit[[4]] <- try(suppressWarnings(fitdistr(dat, "normal")), silent=TRUE)
       if(!trait == 'cuticular_cond'){
-        fit[[5]] <- suppressWarnings(fitdistr(dat, "gamma"))
+        fit[[5]] <- try(suppressWarnings(fitdistr(dat, "gamma")), silent=TRUE)
+        dist.names <- c(dist.names, "gamma")
       } 
+      failfit.bool <- sapply(fit, class) == "try-error"
+      fit[failfit.bool] <- NULL
+      dist.names <- dist.names[!failfit.bool]
       
       fparm <- lapply(fit,function(x){as.numeric(x$estimate)})
       fAIC  <- lapply(fit,function(x){AIC(x)})
       
       bestfit <- which.min(fAIC)
-      posteriors[ptrait,"distn"] <- c(zerobound,"norm")[bestfit]
+      posteriors[ptrait,"distn"] <- dist.names[bestfit]
       posteriors[ptrait,"parama"] <- fit[[bestfit]]$estimate[1]
       if(bestfit == 1){
         posteriors[ptrait,"paramb"] <- NA