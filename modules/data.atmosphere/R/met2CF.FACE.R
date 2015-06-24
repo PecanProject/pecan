7580aa6d77a089e23227efb9388efa4a5cc659d3
Betsy <emcowdery@gmail.com>
2015-06-24 15:55:56 -0400

    changed from renaming variables to copying variables into new ncdf file because the time dim in the original file didn't have units.

--- modules/data.atmosphere/R/met2CF.FACE.R
+++ modules/data.atmosphere/R/met2CF.FACE.R
@@ -24,59 +24,102 @@ met2CF.FACE <- function(in.path,in.prefix,outfolder){
     outfolder <- paste(unlist(strsplit(outfolder, "FACE")), collapse =paste0("FACE_", treatment))
     if(!file.exists(outfolder)) dir.create(outfolder)
     f.cf <- paste0(outfolder,file)
-    if(!file.exists(outfolder)){file.copy(f,f.cf)}
+    if(!file.exists(outfolder)){ #file.copy(f,f.cf)
 
-    # remove the unwanted treatment 
-    if(treatment == "a"){rm.vars <- c("eCO2", "eO3")}
-    else if (treatment == "e"){rm.vars <- c("aCO2", "aO3")}
-    else{logger.error("Need a CO2 levels treatment")}
-  
-    paste("ncks -x -v", paste0(rm.vars,collapse = ","), f.cf, f.cf)
 
-    # Change to CF variable names 
 
-    nc <- nc_open(f.cf,write=TRUE)
-    nc.vars <-  nc.get.variable.list(nc)
-    
-    vars <- c("nav_lat", "nav_lon", "Rainf", "Tair", "RH", "VPD", "Qair", 
-      "Wind", "SWdown", "PAR", "LWdown", "Psurf", "aCO2", "eCO2", "aO3", 
-      "eO3", "SolarElevation")
-    
-    nvars <- c("latitude", "longitude", "precipitation_flux", "air_temperature", "relative_humidity", 
-     "water_vapor_saturation_deficit", "specific_humidity", "wind_speed", 
-     "surface_downwelling_shortwave_flux", "surface_downwelling_photosynthetic_radiative_flux_in_air", 
-     "surface_downwelling_longwave_flux", "air_pressure", "mass_concentration_of_carbon_dioxide_in_air", 
-     "mass_concentration_of_carbon_dioxide_in_air", "mass_concentration_of_ozone_in_air", 
-     "mass_concentration_of_ozone_in_air", "solar_elevation_angle")
-    
-    l <- length(vars)
-    for (k in 1:l){   
-      if(vars[k] %in% nc.vars){nc <- tncar_rename(nc,vars[k],nvars[k])}
-    }
-    
-    # Split into annual files 
-    
-    year <- ncvar_get(nc, 'YEAR')
-    y <- year[1]:year[length(year)]
-    n <- length(y)
-    t <- -1 
-    for(j in 1:n){
-      new.file <- paste0(outfolder,in.prefix,".",y[j],".nc")
-      if(!file.exists(new.file)){ 
-        s <- t + 1; print(s)
-        e <- t + sum(year == y[j]); print(e)      
-        if(file.exists(f.cf)==TRUE && file.exists(new.file)==FALSE){
-          system(paste0("ncks -d tstep,",s,",",e," ",f.cf," ",new.file))
-        } 
+
+
+      # paste("ncks -x -v", paste0(rm.vars,collapse = ","), f.cf, f.cf)
+
+      # Change to CF variable names 
+
+      nc <- nc_open(f.cf,write=TRUE)
+      nc.vars <-  nc.get.variable.list(nc)
+      
+      lat <- ncdim_def(name='latitude', units='', vals=1:1, create_dimvar=FALSE)
+      lon <- ncdim_def(name='longitude', units='', vals=1:1, create_dimvar=FALSE)
+      time <- ncdim_def(name='time', units= *********************, vals=tdim$vals, create_dimvar=TRUE, unlim=TRUE)
+      dim=list(lat,lon,time)
+
+
+      var <- ncvar_def(name="latitude",
+       units="degree_north",
+       dim=list(lat,lon), missval=as.numeric(-9999))
+
+      nc2 <- nc_create(filename=new.file, vars=var, verbose=verbose)
+      
+      ncvar_put(nc=nc2, varid='latitude', vals=ncvar_get(nc,"nav_lat"))
+
+        # copy lon attribute to longitude
+      var <- ncvar_def(name="longitude",
+       units="degree_east",
+       dim=list(lat,lon), missval=as.numeric(-9999))
+      nc2 <- ncvar_add(nc=nc2, v=var, verbose=verbose)
+      ncvar_put(nc=nc2, varid='longitude', vals=ncvar_get(nc,"nav_lon"))
+
+
+      vars_all <- c("Rainf", "Tair", "RH", "VPD", "Qair", 
+        "Wind", "SWdown", "PAR", "LWdown", "Psurf", "aCO2", "eCO2", "aO3", 
+        "eO3", "SolarElevation")
+
+       # remove the unwanted treatment 
+      if(treatment == "a"){rm.vars <- c("eCO2", "eO3")}
+      else if (treatment == "e"){rm.vars <- c("aCO2", "aO3")}
+      else{logger.error("Need a CO2 levels treatment")}
+      vars <- vars_all[-rm.vars]
+      
+      nvars <- c("precipitation_flux", "air_temperature", "relative_humidity", 
+       "water_vapor_saturation_deficit", "specific_humidity", "wind_speed", 
+       "surface_downwelling_shortwave_flux_in_air", "surface_downwelling_photosynthetic_radiative_flux_in_air", 
+       "surface_downwelling_longwave_flux_in_air", "air_pressure", "mass_concentration_of_carbon_dioxide_in_air",
+       "mass_concentration_of_ozone_in_air", "solar_elevation_angle")
+      
+      if(!(length(nvars) == length(vars))){logger.error("Variable mismatch")}
+
+      l <- length(vars)
+      for (k in 1:l){   
+        if(vars[k] %in% nc.vars){#nc <- tncar_rename(nc,vars[k],nvars[k])
+
+          vals <- ncvar_get(nc, vars[k])
+
+
+          units <- ncatt_get(nc, varid=vars[k], attname='units', verbose=verbose)$value
+            
+          var <- ncvar_def(name=nvars[k], units=units, dim=dim, verbose=verbose)
+          nc2 <- ncvar_add(nc=nc2, v=var, verbose=verbose)
+          ncvar_put(nc=nc2, varid=nvars[k], vals=vals)
+
+          att <- ncatt_get(nc, vars[k], 'long_name')
+          if (att$hasatt) {
+            val <- att$value
+            ncatt_put(nc=nc2, varid=nvars[k], attname='long_name', attval=val)
+          }
+        }
       }
-      t <- e
-    } 
+      
+      # Split into annual files 
+      
+      year <- ncvar_get(nc, 'YEAR')
+      y <- year[1]:year[length(year)]
+      n <- length(y)
+      t <- -1 
+      for(j in 1:n){
+        new.file <- paste0(outfolder,in.prefix,".",y[j],".nc")
+        if(!file.exists(new.file)){ 
+          s <- t + 1; print(s)
+          e <- t + sum(year == y[j]); print(e)      
+          if(file.exists(f.cf)==TRUE && file.exists(new.file)==FALSE){
+            system(paste0("ncks -d tstep,",s,",",e," ",f.cf," ",new.file))
+          } 
+        }
+        t <- e
+      } 
+    } # end make new file
+  } # end loop over treatments
 
-  }
+nc_close(nc) 
+file.remove(paste0(in.path,file))
 
-  nc_close(nc) 
-  file.remove(paste0(in.path,file))
-
-
-  }
+}
 
