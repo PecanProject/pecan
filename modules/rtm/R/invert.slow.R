6d0643e1dcffcf336addd775522365e7f8b89cfd
Alexey Shiklomanov (Arch VM) <alexey.shiklomanov@gmail.com>
2015-07-21 11:43:53 -0400

    Simplified priors, updated docs
    Also, added quiet option for slow

--- modules/rtm/R/invert.slow.R
+++ modules/rtm/R/invert.slow.R
@@ -1,43 +1,40 @@
 #' @name invert.slow
 #' @title Bayesian inversion of a model
-#' @details Performs a Bayesian inversion of an arbitrary function
-#'      using the Metropolis-Hastings algorithm.
-#'      This is substantially (~80x) slower than 
-#'      `invert.fast` because it is implemented as a basic R loop,
-#'      but it is also more versatile and is intended for use with
-#'      custom models.
-#' @param observed Vector, matrix, or data frame (coerced to matrix) of
-#'      observed values. For spectral data, wavelengths are rows and 
-#'      spectra are columns.
-#' @param inits Vector of initial values of model parameters to be
-#'      inverted.
+#' @details Performs an inversion of an arbitrary model using a modified 
+#' Metropolis Hastings algorithm with block sampling. This may be slightly 
+#' slower than the implementation in Fortran, but is much more customizable, as 
+#' the model can be any R function.
+#' @param observed Vector, matrix, or data frame (coerced to matrix) of 
+#' observed values. For spectral data, wavelengths are rows and spectra are 
+#' columns.
+#' @param inits Vector of initial values of model parameters to be inverted.
 #' @param constants Vector of model constants.
 #' @param ngibbs Number of MCMC iterations
-#' @param prior List of functions used as priors (e.g. dnorm(x, 3, 0.5, 1)).
-#'      NOTE: These should return the LOG density of the value
-#'      (i.e. log=TRUE should be set)
+#' @param prior Function for use as prior. Should take a vector of parameters 
+#' as input and return a single value -- the sum of their log-densities -- as 
+#' output.
 #' @param pm Vector of minimum values for inversion parameters
-#' @param model The model to be inverted. This should be an R function 
-#'      that takes `inits` and `constants` as input and returns 
-#'      one column of `observed` (nrows should be the same).
+#' @param model The model to be inverted. This should be an R function that 
+#' takes `inits` and `constants` as input and returns one column of `observed` 
+#' (nrows should be the same).
 #' @param adapt Number of steps for adapting covariance matrix (i.e. adapt 
 #' every 'n' steps. Default=100
 #' @param adj_min Minimum threshold for rescaling Jump standard deviation.  
-#' Default = 0.1
+#' Default = 0.1.
 #' @param target Target acceptance rate. Default=0.44
 #' @param lsq.first Perform least squares optimization first, and use outputs 
 #' to initialize Metropolis Hastings. This dramatically improves the mixing 
-#' time, but may prevent full exploration of the parameter space and may result 
-#' in getting caught in a local minimum. Default=TRUE
-invert.slow <- function(observed, inits, constants, ngibbs, prior, pm,
-                        model, adapt=100, adj_min=0.1, target=0.44, do.mle=TRUE){
+#' time. Default=TRUE
+#' @param quiet Don't print steps and status messages. Default=FALSE
+invert.slow <- function(observed, inits, constants, ngibbs, prior, pm, model, 
+                        adapt=100, adj_min=0.1, target=0.44, do.mle=TRUE, quiet=FALSE){
     observed <- as.matrix(observed)
     nspec <- ncol(observed)
     nwl <- nrow(observed)
     npars <- length(inits)
     if(do.mle){
-        fit <- invert.lsq(observed, inits, constants, model, lower=pm)$par
-        print(fit)
+        fit <- invert.lsq(observed, inits, constants, model, lower=pm)
+        if(!quiet) print(fit)
         inits <- fit$par
     }
     rp1 <- 0.001 + nspec*nwl/2
@@ -50,9 +47,9 @@ invert.slow <- function(observed, inits, constants, ngibbs, prior, pm,
     ar <- 0
     for(ng in 1:ngibbs){
         if(ng %% adapt < 1){
-            print(ng)
+            if(!quiet) print(ng)
             if(ar < 2){
-                rescale <- diag(rep(adj_min,4))
+                rescale <- diag(rep(adj_min,npars))
                 Jump <- rescale %*% Jump %*% rescale
             } else{
                 adj <- max(ar / adapt / target, adj_min)
@@ -69,10 +66,8 @@ invert.slow <- function(observed, inits, constants, ngibbs, prior, pm,
         if(all(tvec > pm)){
             TrySpec <- model(tvec, constants)
             TryError <- TrySpec - observed
-            TryPrior <- sum(sapply(1:npars, function(p) prior[[p]](tvec[p])))
-            TryPost <- sum(dnorm(TryError,0,rsd,1)) + TryPrior
-            PrevPrior <- sum(sapply(1:npars, function(p) prior[[p]](inits[p])))
-            PrevPost <- sum(dnorm(PrevError,0,rsd,1)) + PrevPrior
+            TryPost <- sum(dnorm(TryError,0,rsd,1)) + prior(tvec)
+            PrevPost <- sum(dnorm(PrevError,0,rsd,1)) + prior(inits)
             a <- exp(TryPost - PrevPost)
             if(is.na(a)) a <- -1
             if(a > runif(1)){
