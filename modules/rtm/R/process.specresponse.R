ba9c6c77d3829024382c4dbca34ecdac81f4e99d
Alexey Shiklomanov (Arch VM) <alexey.shiklomanov@gmail.com>
2015-07-22 13:53:57 -0400

    Finished SRFs for remote sensing platforms

--- modules/rtm/R/process.specresponse.R
+++ modules/rtm/R/process.specresponse.R
@@ -1,5 +1,6 @@
 #' Functions for generating spectral response matrices
-read.landsat <- function(dir.path){
+read.rsr.folder <- function(dir.path, type){
+    type <- tolower(type)
     flist <- list.files(dir.path)
     nbands <- length(flist)
     bandnames <- gsub("(.*)[.]csv", "\\1", flist)
@@ -7,7 +8,12 @@ read.landsat <- function(dir.path){
     for(i in 1:nbands){
         fpath <- file.path(dir.path, flist[i])
         raw.input <- read.csv(fpath)
-        out.dat <- raw.input[,1:2]
+        if(type == "landsat"){
+            out.dat <- raw.input[,1:2]
+        } else if (type == "avhrr"){
+            raw.input$Wavelength <- raw.input$Wavelength.._m. * 1000
+            out.dat <- raw.input[, c("Wavelength", "Relative.SRF")]
+        }
         colnames(out.dat) <- c("Wavelength", bandnames[i])
         band.list[[i]] <- out.dat
     }
@@ -16,6 +22,5 @@ read.landsat <- function(dir.path){
         band.full <- merge(band.full, band.list[[i]], by="Wavelength", all=TRUE)
     }
     band.full[is.na(band.full)] <- 0
-    return(band.full)
+    return(as.matrix(band.full))
 }
-    
