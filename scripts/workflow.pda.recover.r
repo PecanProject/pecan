3bf3b59b356a472ae0e3f408e70c77525d000c01
Ryan Kelly <rkelly@life.illinois.edu>
2015-07-22 01:18:49 -0400

    Add experimental pda.recover workflow

---
+++ scripts/workflow.pda.recover.r
@@ -0,0 +1,15 @@
+library(PEcAn.all)
+
+# Insert settings file path here. Use the same one supplied to the original failed workflow
+settings.file = ""
+
+# Read in settings
+settings <- read.settings(settings.file)
+
+# Clean and wrap up failed PDA MCMC
+settings$assim.batch <- pda.mcmc.recover(settings)
+
+# If desired, now proceed to complete the run
+status.start("PDA")
+settings$assim.batch <- pda.mcmc(settings)
+status.end()
