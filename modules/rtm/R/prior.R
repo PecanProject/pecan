ffaed3e06c38ea98a8ee4315b3535a49bd39f084
Alexey Shiklomanov (Arch VM) <alexey.shiklomanov@gmail.com>
2015-07-23 16:19:45 -0400

    Added prior function.

---
+++ modules/rtm/R/prior.R
@@ -0,0 +1,29 @@
+#' Functions for default priors
+lognorm.mu <- function(mean, sd) log(mean / sqrt(1 + (mean/sd)^2))
+lognorm.sigma <- function(mean, sd) sqrt(log(1 + (mean/sd)^2))
+
+prior.defaultvals.prospect <- function(sd.inflate = 3){
+    pmean <- c("N" = 0.7,
+               "Cab" =32.81,
+               "Car" = 8.51,
+               "Cw" =0.0129,
+               "Cm" =0.0077)
+    psd <- c("N" = 0.6, 
+             "Cab" = 17.87,
+             "Car" = 3.2,
+             "Cw" = 0.0073,
+             "Cm" = 0.0035)
+    psd <- psd * sd.inflate
+    pmu <- lognorm.mu(pmean, psd)
+    psigma <- lognorm.sigma(pmean, psd)
+    return(list(mu = pmu, sigma = psigma))
+}
+
+priorfunc.prospect <- function(pmu, psigma){
+    prior <- function(params){
+        params["N"] <- params["N"] - 1
+        priors <- mapply(dlnorm, params, pmu, psigma, log=TRUE)
+        return(sum(priors))
+    }
+    return(prior)
+}
