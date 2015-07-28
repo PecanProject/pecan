c041e5f3be3e39b5cb5728cf8aa7e706eafeb464
Ryan Kelly <rkelly@life.illinois.edu>
2015-07-27 23:46:54 -0400

    update workflow.pda.recover

--- scripts/workflow.pda.recover.r
+++ scripts/workflow.pda.recover.r
@@ -1,7 +1,7 @@
 library(PEcAn.all)
 
 # Insert settings file path here. Use the same one supplied to the original failed workflow
-settings.file = ""
+settings.file = "/fs/data2/rykelly/PDA/Tests/nr1.test.07/pecan.pda1000001559.xml"
 
 # Read in settings
 settings <- read.settings(settings.file)
@@ -11,5 +11,5 @@ settings$assim.batch <- pda.mcmc.recover(settings)
 
 # If desired, now proceed to complete the run
 status.start("PDA")
-settings$assim.batch <- pda.mcmc(settings)
+settings <- assim.batch(settings)
 status.end()
