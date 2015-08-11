2e6aebb311a1f0fa5f83292850fcbe8b5278d856
Alexey Shiklomanov (Lenovo Lubuntu VM) <alexey.shiklomanov@gmail.com>
2015-08-11 14:42:45 -0400

    Added proper sensor names

--- modules/rtm/R/spectral-response.R
+++ modules/rtm/R/spectral-response.R
@@ -50,6 +50,11 @@ sensor.list <- c("identity", "aviris.ng", "aviris.classic",
                  "hyperion", "chris.proba", "landsat5", "landsat7",
                  "landsat8", "modis", "viirs", "avhrr")
 
+sensor.proper <- c("ASD Field Spec", "AVIRIS NG", "AVIRIS Classic",
+                   "Hyperion", "CHRIS-Proba", "Landsat 5", "Landsat 7",
+                   "Landsat 8", "MODIS", "VIIRS", "AVHRR")
+names(sensor.proper) <- sensor.list
+
 spectral.response <- function(spec, sensor){
     sensor <- tolower(sensor)
     stopifnot(sensor %in% sensor.list)
