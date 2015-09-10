84c05c8b2c7d9ea82b727bbf29d39acf655eb12f
Betsy Cowdery <emcowdery@gmail.com>
2015-09-10 11:24:54 -0400

    outline of calc.metrics

---
+++ modules/benchmark/R/calc.metrics.R
@@ -0,0 +1,22 @@
+calc.metrics <- function(input_path, format_table, vars_names_units, model_run, metrics, start_year=NA, end_year=NA, site=NA){
+
+  # create results table
+  results <- metrics
+  results$score <- rep(NA, dim(metrics)[1])
+  
+  model <- read.output(model_run$id, model_run$outdir, start_year=NA, end_year=NA, var=vars_names_units$bety_names) #which names????
+  # model output list object with values, variable name and units 
+  
+  obvs <- load.data(input_path, format_table, vars_names_units, start_year = NA, end_year=NA, site=NA)
+  # observation list object with values, variable name and units 
+  
+  dat <- align.data(model, obvs)
+  
+  for(i in 1:length(metrics$name){
+    fcn <- paste("metric.",metrics$name[i])
+    results$score[i] <- apply(fcn, dat)
+  }
+  
+  return(results)
+  
+}
