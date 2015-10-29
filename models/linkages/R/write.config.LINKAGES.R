4427385baa253b0ea1fd1817d904be35448b352d
ANN RAIHO <araiho@nd.edu>
2015-10-29 16:53:24 -0400

    rename climate matrices

--- models/linkages/R/write.config.LINKAGES.R
+++ models/linkages/R/write.config.LINKAGES.R
@@ -74,8 +74,8 @@ write.config.LINKAGES <- function(defaults=NULL, trait.values=NULL, settings, ru
   clat <- read.csv(system.file("clat.csv", package = "PEcAn.LINKAGES"),header = FALSE)
   load(system.file("switch.mat.Rdata", package = "PEcAn.LINKAGES"))
   
-  temp.mat <- matrix(c(-8.6,-7.6,-1.9,6.9,13.7,19,21.6,20.5,15.9,9.6,.8,-6.1),nyear,12,byrow = TRUE)
-  precip.mat <- matrix(c(2.9,2.7,4.2,7,9.2,11.2,8,8.9,8.9,5.7,5.5,2.9),nyear,12,byrow=TRUE)
+  #temp.mat <- matrix(c(-8.6,-7.6,-1.9,6.9,13.7,19,21.6,20.5,15.9,9.6,.8,-6.1),nyear,12,byrow = TRUE)
+  #precip.mat <- matrix(c(2.9,2.7,4.2,7,9.2,11.2,8,8.9,8.9,5.7,5.5,2.9),nyear,12,byrow=TRUE)
   
   #linkages.out <- linkages(iplot = iplot, nyear = nyear,nspec = nspec, fc = fc, dry = dry,
    #                        bgs = bgs, egs = egs, max.ind=max.ind, plat = plat, temp.mat = temp.mat,
