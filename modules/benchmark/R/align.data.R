3d28c6455b4a8dc25a3324ebb6bd76aad7b3bee9
Betsy Cowdery <emcowdery@gmail.com>
2015-11-05 18:43:56 -0500

    Shell of align.data

---
+++ modules/benchmark/R/align.data.R
@@ -0,0 +1,19 @@
+##-------------------------------------------------------------------------------------------------#
+##' For each benchmark id, calculate metrics and update benchmarks_ensemble_scores
+##'  
+##' @name calc.benchmark 
+##' @title Calculate benchmarking statistics
+##' @param model list 
+##' @param obvs list
+##' @export 
+##' 
+##' @author Betsy Cowdery 
+
+align.data <- function(model, obvs){
+  
+  model$time
+  obvs$time
+  
+  
+  
+}
