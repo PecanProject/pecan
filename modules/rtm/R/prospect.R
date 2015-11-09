e275d76fde6b85839d021ddb697614827d7834a0
Alexey Shiklomanov (LVM64) <alexey.shiklomanov@gmail.com>
2015-11-09 17:41:32 -0500

    Force manual version in prospect function

--- modules/rtm/R/prospect.R
+++ modules/rtm/R/prospect.R
@@ -9,15 +9,18 @@
 #'     (5B) Cbrown: Leaf brown matter content (ug/cm2) (>0)
 #'     Cw: Leaf water content (cm) (>0)
 #'     Cm: Leaf dry matter content (ug/cm2) (>0)
-#' @param version PROSPECT version: 4, 5, or "5B" (default)
+#' @param version PROSPECT version: 4, 5, or "5B"
 #' @return Matrix (2101 x 2) of simulated reflectance (column 1) 
 #'      and transmittance (column 2) values from 400:2100 nm
 
-prospect <- function(param, version="5B"){
+prospect <- function(param, version){
     version <- as.character(version)
     plist <- as.list(param)
     plist$RT <- matrix(0, 2101, 2)
-    if (version == "4") inlist <- c("prospect_4", plist)
+    if (version == "4"){
+        stopifnot(length(plist) == 4)
+        inlist <- c("prospect_4", plist)
+    }
     else if (version == "5") inlist <- c("prospect_5", plist)
     else if (version == "5B") inlist <- c("prospect_5b", plist)
     else stop("Version must be 4, 5, or 5B") 
