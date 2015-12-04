daaee6ff45a2ef3a9c140a25cf937dbc935d56d7
jsimkins2 <jsimkins2@wisc.edu>
2015-12-04 13:07:14 -0600

    First version of R wrapper for python CRUNCPE

---
+++ modules/data.atmosphere/R/download.CRUNCEP_Global.R
@@ -0,0 +1,19 @@
+download.CRUNCEP <- function(outfolder, start_date, end_date, lat, lon, overwrite=FALSE, verbose=FALSE, ...){
+  
+  require(PEcAn.utils)
+  require(lubridate)
+  start_date <- as.POSIXlt(start_date, tz = "GMT")
+  end_date <- as.POSIXlt(end_date, tz = "GMT")
+  start_year <- year(start_date)
+  end_year   <- year(end_date)
+  
+# start_year = 2001
+#  end_year = 2005
+#  lat = 45.0
+#  lon = -90.2
+  ylist <- seq(start_year,end_year,by=1)
+    for (year in ylist) {
+      system(paste('python ../python/download_Global_MsTMIP_CRUNCEP.py ',lat,lon,year)
+
+    }
+}
