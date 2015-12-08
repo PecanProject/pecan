9800c7c6075101e97b5b5558453ae69c30614575
araiho <araiho@nd.edu>
2015-12-08 15:19:30 -0500

    making spinup.LINKAGES more generalized

--- models/linkages/R/spinup.LINKAGES.R
+++ models/linkages/R/spinup.LINKAGES.R
@@ -1,10 +1,11 @@
 spinup.LINKAGES <- function(start.year,end.year,temp.mat,precip.mat){
-  start.year <- start.year - 500
+  spin.num <- round(length(stary.year:end.year)/2)
+  start.year <- start.year - spin.num
   year <- seq(start.year,end.year,1)
   nyear <- length(year)
   
-  temp.mat = rbind(temp.mat[1:(nrow(temp.mat)/2),],temp.mat)
-  precip.mat = rbind(precip.mat[1:(nrow(temp.mat)/2),],precip.mat)
+  temp.mat = rbind(temp.mat[1:spin.num,],temp.mat)
+  precip.mat = rbind(precip.mat[1:spin.num,],precip.mat)
   
   return(list(start.year=start.year,nyear=nyear,temp.mat=temp.mat,precip.mat=precip.mat))
   
