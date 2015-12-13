0fccc78eca027d9f844b080f5750f3d658499f28
Michael Dietze <dietze@bu.edu>
2015-12-12 19:22:54 -0500

    update download.CRUNCEP to give a more full results object

--- modules/data.atmosphere/R/download.CRUNCEP_Global.R
+++ modules/data.atmosphere/R/download.CRUNCEP_Global.R
@@ -19,11 +19,25 @@ download.CRUNCEP <- function(outfolder, start_date, end_date, lat, lon, overwrit
   
   dir.create(outfolder, showWarnings=FALSE, recursive=TRUE)
   
-  ylist <- seq(start_year,end_year,by=1)
-    for (year in ylist) 
-      {
-      results <- system2(system.file('scripts/download_Global_MsTMIP_CRUNCEP.sh',package = "PEcAn.data.atmosphere"),args = c(system.file('python/download_Global_MsTMIP_CRUNCEP.py',package = "PEcAn.data.atmosphere"),lat,lon,year,outfolder),stdout=TRUE)
-    }
+  results <- data.frame(file=character(rows), host=character(rows),
+                        mimetype=character(rows), formatname=character(rows),
+                        startdate=character(rows), enddate=character(rows),
+                        dbfile.name = "CRUNCEP",
+                        stringsAsFactors = FALSE)
   
-  invisible(results[length(results)])
+  ylist <- seq(start_year,end_year,by=1)
+  for (i in 1:length(ylist)){
+    year = ylist[i]    
+    new.file <- system2(system.file('scripts/download_Global_MsTMIP_CRUNCEP.sh',package = "PEcAn.data.atmosphere"),args = c(system.file('python/download_Global_MsTMIP_CRUNCEP.py',package = "PEcAn.data.atmosphere"),lat,lon,year,outfolder),stdout=TRUE)
+    
+    results$file[i] <- new.file
+    results$host[i] <- fqdn()
+    results$startdate[i] <- paste0(year,"-01-01 00:00:00")
+    results$enddate[i] <- paste0(year,"-12-31 23:59:59")
+    results$mimetype[i] <- 'application/x-netcdf'
+    results$formatname[i] <- 'CF Meteorology'
+    
+  }
+  
+  invisible(results)
 }
