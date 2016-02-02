9f1af1dfddfc3a3102b3f44fec6c53c5514338fc
araiho <araiho@nd.edu>
2016-02-02 13:18:09 -0500

    add paleon specific spinup

--- models/linkages/R/spinup.LINKAGES.R
+++ models/linkages/R/spinup.LINKAGES.R
@@ -1,12 +1,23 @@
-spinup.LINKAGES <- function(start.year,end.year,temp.mat,precip.mat){
-  spin.num <- round(length(start.year:end.year)/2)
-  start.year <- start.year - spin.num
-  year <- seq(start.year,end.year,1)
-  nyear <- length(year)
-  
-  temp.mat = rbind(temp.mat[1:spin.num,],temp.mat)
-  precip.mat = rbind(precip.mat[1:spin.num,],precip.mat)
-  
+spinup.LINKAGES <- function(start.year,end.year,temp.mat,precip.mat,paleon=NULL){
+  if(is.null(paleon)) paleon = TRUE
+  if(paleon==TRUE){
+    spin.num <- 20
+    start.year <- start.year - 1000
+    
+    temp.mat = rbind(temp.mat[rep(1:spin.num,length = 1000),],temp.mat)
+    precip.mat = rbind(precip.mat[rep(1:spin.num,length=1000),],precip.mat) 
+    nyear <- nrow(temp.mat)
+    
+  } else {
+    spin.num <- round(length(start.year:end.year)/2)
+    start.year <- start.year - spin.num
+    year <- seq(start.year,end.year,1)
+    nyear <- length(year)
+    
+    temp.mat = rbind(temp.mat[1:spin.num,],temp.mat)
+    precip.mat = rbind(precip.mat[1:spin.num,],precip.mat)
+  }
+
   return(list(start.year=start.year,nyear=nyear,temp.mat=temp.mat,precip.mat=precip.mat))
   
 }
