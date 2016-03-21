17d1faa662be169507bc4166639d474bd89b80b6
Betsy Cowdery <emcowdery@gmail.com>
2016-03-21 16:45:30 -0400

    New function: load.csv

---
+++ modules/benchmark/R/load.csv.R
@@ -0,0 +1,29 @@
+##' @name load.csv
+##' @title load.csv
+##' @export
+##' @param data.path
+##' @param format
+##' @param start_year
+##' @param end_year
+##' @param site
+##' 
+##' @author Betsy Cowdery
+
+load.csv <- function(data.path, format, start_year, end_year, site){
+  
+  
+  if (format$header == 0 | format$header == 1){
+    dat <- read.csv(data.path, skip = format$skip, na.strings = format$na.strings, as.is=TRUE,
+                    check.names = FALSE, header = as.logical(format$header))
+  }else if (format$header > 1){
+    dat <- read.csv(data.path, skip = format$skip, na.strings = format$na.strings, as.is=TRUE, 
+                    check.names = FALSE, header = TRUE)
+    dat <- dat[-c(1:header-1),]
+  }else{
+    dat <- read.csv(data.path, skip = format$skip, na.strings = format$na.strings, as.is=TRUE, 
+                    check.names = FALSE)
+  }
+  
+  return(dat)
+  
+}
