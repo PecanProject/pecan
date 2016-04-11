e6564a2a8f5a4eabdf8d62eb8a0a778293f9b2ce
Alexey Shiklomanov (test-pecan) <ashiklom@bu.edu>
2016-04-11 10:39:55 -0400

    Bugfixes in load data step.
    Was missing the stringr package.
    
    Also, loading traits by entity loaded covariates in addition to traits. New change in script loads traits only.

--- db/inst/import-try/06.load.data.R
+++ db/inst/import-try/06.load.data.R
@@ -1,6 +1,7 @@
 # 6. Add TRY data to BETY
 source("common.R")
 load("try.5.RData")
+library(stringr)
 
 setkey(try.dat, ObservationID)
 try.entities <- try.dat[, .GRP, by=ObservationID]
@@ -32,7 +33,8 @@ for(i in 1:nrow(try.entities)){
   entity.name <- fixquote(paste0("TRY_OBSERVATION_", entity))
   check <- db.query(sprintf("SELECT id FROM entities WHERE name LIKE '%s'", entity.name), con)
   if(length(check) > 0) next
-  try.sub <- try.dat[ObservationID == entity]
+  try.sub <- try.dat[ObservationID == entity & type == "t"]   # Select only traits -- for now, ignore covariates
+  if(nrow(try.sub) == 0) next
   entity.notes <- fixquote(try.sub[, paste(unique(DatasetID),
                                            unique(Dataset),
                                            unique(ObservationID),
--- db/inst/import-try/common.R
+++ db/inst/import-try/common.R
@@ -1,11 +1,13 @@
 # Common definitions and packages
 
 # Load packages
-library(data.table)
-library(bit64)
-library(PEcAn.DB)
-library(RPostgreSQL)
-
+suppressMessages({
+  library(data.table)
+  library(bit64)
+  library(PEcAn.DB)
+  library(RPostgreSQL)
+})
+  
 # Database parameters
 user_id <- "1000000013"     # Alexey Shiklomanov
 dbparms <- list(driver="PostgreSQL" , user = "ashiklom", dbname = "bety_ashiklom")
