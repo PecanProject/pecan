08a8d1dbf0439d6269640fb26681f6b482c3ae54
istem fer <fer.istem@gmail.com>
2016-04-13 08:59:58 -0400

    load.pda.data related minor changes

--- modules/assim.batch/R/pda.mcmc.bs.R
+++ modules/assim.batch/R/pda.mcmc.bs.R
@@ -46,7 +46,7 @@ pda.mcmc.bs <- function(settings, params.id=NULL, param.names=NULL, prior.id=NUL
   n.param.all  <- nrow(prior)
 
   ## Load data to assimilate against
-  inputs <- load.pda.data(settings$assim.batch$inputs, con)
+  inputs <- load.pda.data(settings, con)
   n.input <- length(inputs)
 
   ## Set model-specific functions
