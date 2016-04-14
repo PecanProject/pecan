78a08015805fc2cc1895efa085de4893b6676666
istem fer <fer.istem@gmail.com>
2016-04-14 17:07:29 -0400

    minor updates

--- modules/assim.batch/R/pda.bayesian.tools.R
+++ modules/assim.batch/R/pda.bayesian.tools.R
@@ -165,8 +165,11 @@ pda.bayesian.tools <- function(settings, params.id=NULL, param.names=NULL, prior
   if(sampler=="Metropolis"){
     params <- matrix(rep(parm,n.row), nrow=n.row, ncol=n.param.all, byrow=T)
     params[, prior.ind] <- out$chain[,1:length(prior.ind)]
-  } else if(sampler %in% c("M","AM","DR","DRAM","DE","DEzs")) {
+  } else if(sampler %in% c("M","DR","DE","DEzs")) {
     params <- matrix(rep(parm,n.row), nrow=n.row, ncol=n.param.all, byrow=T)
+    params[, prior.ind] <- out$chain
+  } else if(sampler %in% c("AM","DRAM")) {
+    params <- matrix(rep(parm,(n.row+1000)), nrow=(n.row+1000), ncol=n.param.all, byrow=T)
     params[, prior.ind] <- out$chain
   } else if(sampler=="SMC") {
     params <- matrix(rep(parm,bt.settings$initialParticles[[2]]), nrow=bt.settings$initialParticles[[2]], ncol=n.param.all, byrow=T)
