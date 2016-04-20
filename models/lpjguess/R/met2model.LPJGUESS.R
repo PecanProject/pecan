eb04907428a1d5929b770196a939db80e424e464
istem fer <fer.istem@gmail.com>
2016-04-20 12:34:54 -0400

    read-in the climate data

--- models/lpjguess/R/met2model.LPJGUESS.R
+++ models/lpjguess/R/met2model.LPJGUESS.R
@@ -35,9 +35,54 @@ met2model.LPJGUESS <- function(in.path, in.prefix, outfolder, start_date, end_da
   end_date='2002/12/31'
   
   library(PEcAn.utils)
+  require(ncdf4)
   
   print("START met2model.LPJGUESS")
   start_date <- as.POSIXlt(start_date, tz = "GMT")
   end_date <- as.POSIXlt(end_date, tz = "GMT")
+  start_year <- year(start_date)
+  end_year <- year(end_date)
   
+  year = sprintf("%04d",seq(start_year,end_year,1))
+  #month = sprintf("%02d",seq(1,12,1))
+  
+  nyear = length(year) #number of years to simulate
+  
+  ## LPJ-GUESS looks for different input files for different climate variables
+  out.files <- list()
+  out.names=c("tmp","pre","cld")
+  for(i in 1:length(out.names)) out.files[[i]] <- paste(in.prefix, start_year, end_year, out.names[[i]], "nc", sep=".")
+  
+  month_matrix_tmp = matrix(NA,nyear,12)
+  month_matrix_pre = matrix(NA,nyear,12)
+  month_matrix_cld = matrix(NA,nyear,12)
+  DOY_vec_hr = c(1,c(32,60,91,121,152,182,213,244,274,305,335,365)*4)
+
+  
+  for(i in 1:nyear){ 
+    ## open netcdf file
+    ncin <- nc_open(file.path(in.path,paste(in.prefix,year[i],"nc",sep=".")))
+    
+    ## convert time to seconds
+    sec   <- ncin$dim$time$vals  
+    #sec = udunits2::ud.convert(sec,unlist(strsplit(ncin$dim$time$units," "))[1],"seconds")
+    ifelse(leap_year(as.numeric(year[i]))==TRUE,
+           dt <- (366*24*60*60)/length(sec), #leap year
+           dt <- (365*24*60*60)/length(sec)) #non-leap year
+    tstep = 86400/dt
+    
+    nctmp = ncvar_get(ncin, "air_temperature") #units are K 
+    ncpre = ncvar_get(ncin, "precipitation_flux") #units are kg m-2 s-1  
+    nccld = ncvar_get(ncin, "surface_downwelling_shortwave_flux_in_air") #units are W m-2
+    
+    for(m in 1:12){
+      month_matrix_tmp[i,m] = (mean(nctmp[DOY_vec_hr[m]:(DOY_vec_hr[m+1]-1)])) 
+      month_matrix_pre[i,m] = (sum(ncpre[DOY_vec_hr[m]:(DOY_vec_hr[m+1]-1)]) * dt /10)
+      month_matrix_cld[i,m] = (mean(nccld[DOY_vec_hr[m]:(DOY_vec_hr[m+1]-1)])) 
+    }  
+    
+    ## close netcdf file
+    nc_close(ncin)
+  } ## end nyear for loop
+   
 } ### end met2model.LPJGUESS
