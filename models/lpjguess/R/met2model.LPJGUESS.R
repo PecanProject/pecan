498d9e0bd2ed7b55513d3f213672ca7a95e7c61a
istem fer <fer.istem@gmail.com>
2016-04-21 13:10:53 -0400

    changes to use daily time step for LPJ-GUESS inputs

--- models/lpjguess/R/met2model.LPJGUESS.R
+++ models/lpjguess/R/met2model.LPJGUESS.R
@@ -28,9 +28,9 @@
 met2model.LPJGUESS <- function(in.path, in.prefix, outfolder, start_date, end_date, ..., overwrite=FALSE,verbose=FALSE){
   
   ## function arguments for development
-  in.path='/fs/data5/pecan.models/LPJ-GUESS/build/CRUNCEP_site_0-622'
-  in.prefix='CRUNCEP'
-  outfolder='/fs/data5/pecan.models/LPJ-GUESS/build/CRUNCEP_LPJGUESS_site_0-622/'
+  in.path='/fs/data5/pecan.models/LPJ-GUESS/build/NARR_CF_site_0-622'
+  in.prefix='NARR'
+  outfolder='/fs/data5/pecan.models/LPJ-GUESS/build/NARR_LPJGUESS_site_0-622/'
   start_date='2002/01/01'
   end_date='2003/12/31'
   verbose=FALSE
@@ -45,8 +45,6 @@ met2model.LPJGUESS <- function(in.path, in.prefix, outfolder, start_date, end_da
   end_year <- year(end_date)
   
   year = sprintf("%04d",seq(start_year,end_year,1))
-  month = sprintf("%02d",seq(1,12,1))
-  
   nyear = length(year) #number of years to simulate
   
   ## LPJ-GUESS looks for different input files for different climate variables
@@ -62,56 +60,51 @@ met2model.LPJGUESS <- function(in.path, in.prefix, outfolder, start_date, end_da
     dir.create(outfolder)
   }
 
-  month_array=array(NA,dim=c(nyear,12,3))
-  DOY_vec_hr = c(1,c(32,60,91,121,152,182,213,244,274,305,335,365)*4)
-
-  
-  for(i in 1:nyear){ 
-    ## open netcdf file
-    ncin <- nc_open(file.path(in.path,paste(in.prefix,year[i],"nc",sep=".")))
+    ## open netcdf files
+    ncin <- lapply(file.path(in.path,paste(in.prefix,year,"nc",sep=".")),nc_open)
     
-    lon=ncvar_get(ncin,"longitude")
-    lat=ncvar_get(ncin,"latitude")
+    ## retrieve lat/lon 
+    lon=ncvar_get(ncin[[1]],"longitude")
+    lat=ncvar_get(ncin[[1]],"latitude")
     
-    ## convert time to seconds
-    sec   <- ncin$dim$time$vals  
-    days = udunits2::ud.convert(sec,unlist(strsplit(ncin$dim$time$units," "))[1],"days")
-    ifelse(leap_year(as.numeric(year[i]))==TRUE,
-           dt <- (366*24*60*60)/length(sec), #leap year
-           dt <- (365*24*60*60)/length(sec)) #non-leap year
-    tstep = 86400/dt
-    
+    ## calculate time step from the time-dimension length, check for leap year
+    tstep=ifelse(ncin[[1]]$dim$time$len%%365==0, ncin[[1]]$dim$time$len/365, ncin[[1]]$dim$time$len/366)
     
     ## read climate data
-    nc.vars <- list()
-    for(v in 1:n.var){
-      nc.vars[[v]] = ncvar_get(ncin, long.names[v]) 
-      for(m in 1:12) month_array[i,m,v] = (mean(nc.vars[[v]][DOY_vec_hr[m]:(DOY_vec_hr[m+1]-1)])) 
+    nc.tmp <- lapply(ncin, ncvar_get, long.names[1])
+    nc.pre <- lapply(ncin, ncvar_get, long.names[2])
+    nc.cld <- lapply(ncin, ncvar_get, long.names[3])
+    
+    ## aggregate to daily time steps
+    tmp.list <- pre.list <- cld.list <- list()
+    for(y in 1:nyear){
+      if(as.numeric(year[y])%%4 == 0) ind.vec=rep(1:366,each=tstep) else ind.vec=rep(1:365,each=tstep)
+      tmp.list[[y]] <- tapply(nc.tmp[[y]],ind.vec,mean)
+      pre.list[[y]] <- tapply(nc.pre[[y]],ind.vec,mean)
+      cld.list[[y]] <- tapply(nc.cld[[y]],ind.vec,mean)
     }
-
-    var.units=c("K","kg m-2 s-1","W m-2")
-     
- 
+    
+    var.list=list(unlist(tmp.list),unlist(pre.list),unlist(cld.list))
+    
     ## write climate data
     ## define dimensions
     latdim <- ncdim_def(name='lat', units='', vals=1:length(lon), create_dimvar=FALSE)
     londim <- ncdim_def(name='lon', units='', vals=1:length(lon), create_dimvar=FALSE)
-    timedim=ncdim_def("time", units='',vals=1:12,create_dimvar=FALSE)
+    timedim=ncdim_def("time", units='',vals=1:length(var.list[[1]]),create_dimvar=FALSE)
     
 
     fillvalue=9.96920996838687e+36
     
 
-
     for(n in 1:n.var) {
       
       var <- ncvar_def(name="time",
-                       units="days since 1900-1-1",
+                       units=paste0("days since ",start_year,"-1-1"),
                        dim=timedim, missval=as.numeric(-9999))
       
       ncout<- nc_create(filename=file.path(outfolder,paste(out.files[[n]],sep=".")), vars=var, verbose=verbose)
-      #ncout<- nc_create(filename=out.files[[n]], vars=var, verbose=verbose)
-      ncvar_put(nc=ncout, varid='time', vals=month)
+
+      ncvar_put(nc=ncout, varid='time', vals=1:length(var.list[[1]]))
       
       var <- ncvar_def(name="lon",
                        units="degree_east",
@@ -128,7 +121,7 @@ met2model.LPJGUESS <- function(in.path, in.prefix, outfolder, start_date, end_da
       var <- ncvar_def(name=var.names[n],units=var.units[n],dim=(list(latdim,londim,timedim)),fillvalue,long.names[n],verbose=verbose,prec="float")
       
       ncout=ncvar_add(nc=ncout, v=var,verbose=verbose)
-      ncvar_put(nc=ncout,varid=var.names[n],vals=month_array[,,n])
+      ncvar_put(nc=ncout,varid=var.names[n],vals=var.list[[n]])
       ncatt_put(nc=ncout, varid=var.names[n],attname="standard_name", long.names[n])
       
       ncatt_put(nc=ncout, varid="lon",attname="axis", "X")
@@ -141,9 +134,9 @@ met2model.LPJGUESS <- function(in.path, in.prefix, outfolder, start_date, end_da
       nc_close(ncout)
     } # end n.var loop
     
-    ## close netcdf file
-    nc_close(ncin)
-  } ## end nyear loop
+    ## close netcdf files
+    sapply(ncin,nc_close)
+
    
   
 } ### end met2model.LPJGUESS
