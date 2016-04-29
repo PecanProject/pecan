2a0d66526fe53a0ea4767554b0eaef1cba88123d
Betsy Cowdery <emcowdery@gmail.com>
2016-04-29 01:00:05 -0400

    Rewriting practically the entire function - now it should be generalized and it uses the first align function. Not generalized to more than one align function yet.

--- modules/benchmark/R/align.data.R
+++ modules/benchmark/R/align.data.R
@@ -6,73 +6,87 @@
 ##' @author Betsy Cowdery
 
 
-align.data <- function(model, obvs, vars_used, start_year, end_year){
-
-  t <- which(vars_used$pecan_name == "time")
+align.data <- function(model_full, obvs_full, dat_vars, start_year, end_year){
   
   
-  # We know that the model output time is days since the beginning of the year.
-  # Make a column of years to supplement the column of days of the year.
+  rng_model <- range(model_full$posix)
+  rng_obvs <- range(obvs_full$posix)
+  rng_dat <- sort(c(rng_obvs, rng_model))[c(2,3)]
   
-  years <- start_year:end_year
-  Diff <- diff(model$time)
-  n <- model$time[c(which(Diff < 0), length(model$time))]
-  y <- c()
-  for(i in 1:length(n)){
-    y <- c(y, rep(years[i], n[i]))
-  }
-  model$year <- y
+  model <- model_full[model_full$posix >= rng_dat[1] & model_full$posix <= rng_dat[2],]
+  obvs <- obvs_full[obvs_full$posix >= rng_dat[1] & obvs_full$posix <= rng_dat[2],]
   
+  diff.m <- diff(model$posix)
+  diff.o <- diff(obvs$posix)
+  units(diff.m) = units(diff.o) = "secs"
   
-  ##############################
+  mode.m <- as.numeric(diff.m[which.max(tabulate(match(unique(diff.m), diff.m)))])
+  mode.o <- as.numeric(diff.o[which.max(tabulate(match(unique(diff.o), diff.o)))])
   
-  obvs_time <- obvs[,vars_used$orig_name[t]]
-  # Should we make a lookup table that converts between udunits and
-  m_obvs <- min(diff(strptime(as.character(sort(unique(obvs_time))), format = "%Y")))
-  units(m_obvs) <- "secs"
-  m_obvs <- as.numeric(m_obvs)
-  mode_obvs <- m_obvs[which.max(tabulate(match(unique(m_obvs), m_obvs)))]
-
-  m_model <- diff(strptime(paste(model$time, model$year), format = "%j %Y"))
-  # DALEC isn't outputting the right number of days whyyyyyyyy
-  units(m_model) <- "secs"
-  m_model <- as.numeric(m_model)
-  mode_model <- m_model[which.max(tabulate(match(unique(m_model), m_model)))]
-
-
-  # Compare timestep sizes,
-  # choose the smaller of the two
-  # then choose the appropriate conversion function
-
-  min(mode_model,mode_obvs)
-  
-  ###################################################
-
-  # Then big theoretical leap to get me here O.o
-  
-  out_model <- as.data.frame(matrix(NA, ncol = length(vars_used$pecan_name), nrow = length(unique(model$year))))
-  colnames(out_model) <- paste(vars_used$pecan_name, "model", sep = "_")
-  out_model$years <- sort(unique(model$year))
-  for(i in 1:nrow(vars_used)){
-    v <- vars_used$pecan_name[i]
-    out_model[,paste(v,"model",sep="_")] <- aggregate(model[,v], by=list(model$year), FUN=mean, na.rm=TRUE)[,2]
+  if(mode.m > mode.o){
+    date.coarse = model$posix
+    date.fine = obvs$posix
+    data.fine = obvs[,dat_vars]
+    colnames(data.fine) <- paste0(colnames(data.fine), ".o")
+    out1 <- model[,dat_vars]
+    colnames(out1) <- paste0(colnames(out1), ".m")
+  }else if(mode.o > mode.m){
+    date.coarse = obvs$posix
+    date.fine = model$posix
+    data.fine = model[,dat_vars]
+    colnames(data.fine) <- paste0(colnames(data.fine), ".m")
+    out1 <- obvs[,dat_vars]
+    colnames(out1) <- paste0(colnames(out1), ".o")
   }
   
-  colnames(obvs)[which(names(obvs)=="YEAR")] <- "time"
-  out_obvs <- as.data.frame(matrix(NA, ncol = length(vars_used$pecan_name), nrow = length(unique(obvs$time))))
-  colnames(out_obvs) <- paste(vars_used$pecan_name, "obvs", sep = "_")
-  out_obvs$years <- sort(unique(obvs$time))
-  for(i in 1:nrow(vars_used)){
-    v <- vars_used$pecan_name[i]
-    print(v)
-    print(paste(v,"obvs",sep="_"))
-    out_obvs[,paste(v,"obvs",sep="_")] <- aggregate(obvs[,v], by=list(obvs$time), FUN=mean, na.rm=TRUE)[,2]
-  }
+  # There will be other functions eventually
+  
+  out2 <-apply(data.fine,2,function(x) mean.over.larger.timestep(date.coarse, date.fine, x))  
+
+  dat <- cbind(out1,out2)
+  dat$posix <- date.coarse
   
   
-  out <- merge(out_model, out_obvs, by = "years", all = TRUE)
-  colnames(out)[which(names(out)=="years")] <- "time" 
-   out$time_model  <- NULL
-   out$time_obvs  <- NULL
-  return(out)
+  
+  return(dat)
 }
+  
+  # # Compare timestep sizes,
+  # # choose the smaller of the two
+  # # then choose the appropriate conversion function
+  # 
+  # which.min(c(mode_model,mode_obvs))
+  # 
+  # date.coarse <- obvs$time
+  # date.fine <- strptime(paste(model$time, model$year), format = "%j %Y")
+  # data.fine <- model$NPP
+  # mean.over.larger.timestep(date.coarse, date.fine, data.fine)
+  # 
+  # ###################################################
+  # 
+  # # Then big theoretical leap to get me here O.o
+  # 
+  # out_model <- as.data.frame(matrix(NA, ncol = length(vars_used$pecan_name), nrow = length(unique(model$year))))
+  # colnames(out_model) <- paste(vars_used$pecan_name, "model", sep = "_")
+  # out_model$years <- sort(unique(model$year))
+  # for(i in 1:nrow(vars_used)){
+  #   v <- vars_used$pecan_name[i]
+  #   out_model[,paste(v,"model",sep="_")] <- aggregate(model[,v], by=list(model$year), FUN=mean, na.rm=TRUE)[,2]
+  # }
+  # 
+  # colnames(obvs)[which(names(obvs)=="YEAR")] <- "time"
+  # out_obvs <- as.data.frame(matrix(NA, ncol = length(vars_used$pecan_name), nrow = length(unique(obvs$time))))
+  # colnames(out_obvs) <- paste(vars_used$pecan_name, "obvs", sep = "_")
+  # out_obvs$years <- sort(unique(obvs$time))
+  # for(i in 1:nrow(vars_used)){
+  #   v <- vars_used$pecan_name[i]
+  #   print(v)
+  #   print(paste(v,"obvs",sep="_"))
+  #   out_obvs[,paste(v,"obvs",sep="_")] <- aggregate(obvs[,v], by=list(obvs$time), FUN=mean, na.rm=TRUE)[,2]
+  # }
+  # 
+  # 
+  # out <- merge(out_model, out_obvs, by = "years", all = TRUE)
+  # colnames(out)[which(names(out)=="years")] <- "time"
+  # out$time_model  <- NULL
+  # out$time_obvs  <- NULL
