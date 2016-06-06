6865b154a67e5055b6ff892755dc93847c4dc455
Michael Dietze <dietze@bu.edu>
2016-06-06 13:16:23 -0400

    icon: updated require to include. Modified man to clarify that id can be any text

--- visualization/R/add_icon.R
+++ visualization/R/add_icon.R
@@ -1,18 +1,19 @@
 ##' @name add_icon
 ##' @param x x-coordinate of logo
 ##' @param y y-coordinate of logo
+##' @param id additional plot identificaition (URL, database ID, etc)
 ##' @export 
 ##' @author Mike Dietze
 ##' 
 add_icon <- function(id=NULL,x=0,y=0){
-  require(png)
-  require(grid)
+  include(png)
+  include(grid)
   icon <- readPNG(system.file("favicon.png",package = "PEcAn.visualization"))
-  dims = dim(icon)
-  logo = rasterGrob(icon,unit(x,"npc"),unit(y,"npc"),unit(dims[1],"points"),unit(dims[2],"points"),just=c("left","bottom"))
+  dims <- dim(icon)
+  logo <- rasterGrob(icon,unit(x,"npc"),unit(y,"npc"),unit(dims[1],"points"),unit(dims[2],"points"),just=c("left","bottom"))
   grid.draw(logo)
 
-  lab = textGrob(label = paste("PEcAn",id), x = unit(x, "npc") + unit(dims[1],"points"), y = unit(y, "npc"),just = c("left", "bottom"))
+  lab <- textGrob(label = paste("PEcAn",id), x = unit(x, "npc") + unit(dims[1],"points"), y = unit(y, "npc"),just = c("left", "bottom"))
   grid.draw(lab)
   
 }
