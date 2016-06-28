af21473bfcb11552326e04519a8eebe340b968f0
Ryan Kelly <rkelly@neptuneinc.org>
2016-06-28 15:27:36 -0400

    set test db to psql-pecan.bu.edu for PEcAn.db testthat tests

--- db/tests/testthat/test.contents_sanity.R
+++ db/tests/testthat/test.contents_sanity.R
@@ -6,7 +6,7 @@
 # which accompanies this distribution, and is available at
 # http://opensource.ncsa.illinois.edu/license.html
 #-------------------------------------------------------------------------------
-con <- db.open(list(driver = "PostgreSQL", user = "bety", dbname = "bety", password = "bety"))
+con <- db.open(list(host="psql-pecan.bu.edu", driver = "PostgreSQL", user = "bety", dbname = "bety", password = "bety"))
 
 context("Basic Sanity tests for PEcAn functions that query BETYdb")
 
--- db/tests/testthat/test.derive.traits.R
+++ db/tests/testthat/test.derive.traits.R
@@ -7,7 +7,7 @@
 # http://opensource.ncsa.illinois.edu/license.html
 #-------------------------------------------------------------------------------
 
-con <- db.open(list(driver = "PostgreSQL", user = "bety", dbname = "bety", password = "bety"))
+con <- db.open(list(host="psql-pecan.bu.edu", driver = "PostgreSQL", user = "bety", dbname = "bety", password = "bety"))
 test_that("take.samples works",{
   expect_equal(take.samples(summary = data.frame(mean = 1, stat = NA)), 1)
   set.seed(0)
