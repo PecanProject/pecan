5e0d1fb5e386c6e2a15a8e02509bbedd56ee591f
Betsy Cowdery <emcowdery@gmail.com>
2016-07-01 02:17:43 -0400

    Updating calc.bemchmark, replacing what I initially thought would be a settings object (wrong) to info queried using the bemchmark ensemble id. Adding in looping. Not fully functional because I need to fix the inputs section.

--- modules/benchmark/R/calc.benchmark.R
+++ modules/benchmark/R/calc.benchmark.R
@@ -3,53 +3,57 @@
 ##'  
 ##' @name calc.benchmark 
 ##' @title Calculate benchmarking statistics
-##' @param settings object from start.benchmark.runs (for now - we many only need a few variables)
+##' @param bm.ensemble object, either from create.BRR or start.bm.ensemle
 ##' @param con database connection
 ##' @export 
 ##' 
 ##' @author Betsy Cowdery 
 
-calc.benchmark <- function(settings, con){ #settings file is output from start.benchmark.runs
+calc.benchmark <- function(bm.ensemble, con){ 
   
   require(RPostgreSQL)
   require(XML)
   require(data.table)
   
   
-  # Query BRR for benchmark ids
+  ens <- db.query(paste("SELECT * FROM ensembles where id = ",bm.ensemble$ensemble_id,";"), con) 
+  wf <- db.query(paste("SELECT w.* FROM workflows as w join ensembles as e on w.id = e.workflow_id where e.id = ",bm.ensemble$ensemble_id,";"), con)
+  site  <- query.site(wf$site_id, con)
   
-  bm.id <- settings$benchmark$id 
-  obvs.id <- settings$benchmark$input_id
+  # All benchmarking records for the given benchmarking ensemble id
+  bms <- db.query(paste("SELECT * FROM benchmarks as b join benchmarks_benchmarks_reference_runs as r on b.id=r.benchmark_id  join  benchmarks_ensembles as be on r.reference_run_id = be.reference_run_id where be.ensemble_id = ",bm.ensemble$ensemble_id,";"), con) 
   
-  
-  # For each bm.id - don't have this written as a loop yet. 
-  #  Query database for: input path, variables, site, start/end dates, metrics, formats
-  
-  data.path <- query.file.path(obvs.id,settings$run$host$name,con)
-  format <- query.format.vars(obvs.id,con)  
-  
-  site  <- query.site(settings$run$site$id, con)
-  
-  metrics <- db.query(paste("SELECT m.name from metrics as m JOIN benchmarks_metrics as b 
-                              ON m.id = b.metric_id WHERE b.benchmark_id = ", bm.id),con)
-  
-  model_run <- dir(settings$run$host$outdir, full.names=TRUE)[1]
+  # Determine how many data sets inputs are associated with the benchmark id's
+  inputs <- unique(bms$input_id)
+  # This thought isn't done yet - what needs to happen is that the number of bm.ids need to be split up in to groups according to their input data. So that the observations are only loaded once. 
+
+  data.path <- query.file.path(obvs.id,wf$hostname,con)
+  model_run <- dir(file.path(wf$folder,"out"), full.names=TRUE,  include.dirs = TRUE)[1]
   #How are we dealing with ensemble runs? Right now I've hardcoded to select the first run.
-  
-  # Local or remote
+
+  start_year <- year(wf$start_date)
+  end_year <- year(wf$end_date)
+  format_full <<- format <- query.format.vars(obvs.id,con) 
   # format is a list, metrics is a dataframe, both can be long. How to pass to calc.metrics remotely? 
   # Maybe as an Rdata file?
   
-  start_year <- year(settings$run$start.date)
-  end_year <- year(settings$run$end.date)
   
-  results <- calc.metrics(data.path, format, vars_used, model_run, metrics, start_year, end_year, site)
+  results.list <- list()
+  bm.ids <- bms$id
+  # Loop over benchmark ids
+  for(i in 1:length(bm.ids)){
+    bm <- db.query(paste("SELECT * from benchmarks where id =", bm.ids[i]), con)
+    format$vars <- format_full$vars[nchar(format_full$vars$storage_type) > 0 | 
+                                      format_full$vars$variable_id == bm$variable_id,]
+    metrics <- db.query(paste("SELECT m.name, m.id from metrics as m JOIN benchmarks_metrics as b 
+                              ON m.id = b.metric_id WHERE b.benchmark_id = ", bm.ids[i]),con)
+    
+    r <- calc.metrics(data.path, format, model_run, metrics, start_year, end_year, site,bm,ens)
+    results.list <- append(results.list, list(r))
+  } #end loop over benchmark ids
+  
+  results <- Reduce(function(...) merge(..., by="metric", all=T), results.list)
+  return(results)
   
   
-  #  Update benchmark ensemble scores table
-  
-
-# Return results
-return(results)
-
 }
