f10139e79d97d3d3cdc5b9c2d7ce526c8ec0cb67
tonygardella <tonygard@bu.edu>
2016-07-05 12:59:58 -0400

    Corrected units of VPD in met2CF.Ameriflux from mol/m2/s1 to Pa

--- modules/data.atmosphere/R/met2CF.Ameriflux.R
+++ modules/data.atmosphere/R/met2CF.Ameriflux.R
@@ -202,7 +202,7 @@ met2CF.Ameriflux <- function(in.path, in.prefix, outfolder, start_date, end_date
     # convert VPD to water_vapor_saturation_deficit
     # HACK : conversion will make all values < 0 to be NA
     copyvals(nc1=nc1, var1='VPD',
-             nc2=nc2, var2='water_vapor_saturation_deficit', units2='mol m-2 s-1', dim2=dim, 
+             nc2=nc2, var2='water_vapor_saturation_deficit', units2='Pa', dim2=dim, 
              conv=function(x) { ifelse(x<0, NA, x*1000) }, verbose=verbose)
 
     # copy Rg to surface_downwelling_shortwave_flux_in_air
