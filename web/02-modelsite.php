3ae8dc52f14de675aeda406183f727412cf474d4
Gene Roeder <eroeder@illinois.edu>
2016-07-05 12:13:23 -0500

    Model changes for add site field, still need map and insert hook to db.

--- web/02-modelsite.php
+++ web/02-modelsite.php
@@ -80,11 +80,117 @@ while ($row = @$stmt->fetch(PDO::FETCH_ASSOC)) {
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 <link rel="stylesheet" type="text/css" href="sites.css" />
+<style>
+	body {font-size: 62.5%;}
+	label, input {display:block}
+	input.text { margin-bottom:12px; width:95%; padding:10px;}
+	fieldset { padding:0; border:0; margin-top:25px; }
+	h1 { font-size: 1.2em; margin: .6em 0; }
+	.ui-dialog .ui-state-error { padding: .3em; } 
+	.validateTips { border: 1px solid transparent; padding: 0.3em; } 
+	#container { display: table; }
+	#row { display: table-row; }
+	#left, #right, #middle { display: table-cell; }
+</style>
 <script type="text/javascript" src="jquery-1.7.2.min.js"></script>
 <?php if (!$offline) {?>
 <script type="text/javascript" src="//www.google.com/jsapi"></script>
 <?php }?>
+<?php if (!$offline) {?>
+<script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.js"></script>
+<?php }?>
+<?php if (!$offline) {?>
+<script type="text/javascript" src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<?php }?>
+<?php if (!$offline) {?>
+<link rel="stylesheet" type="text/css" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css"/>
+<?php }?>
 <script type="text/javascript">
+
+	$(function()  {
+	var dialog, form,
+		sitename = $( "#txtsitename" ),
+		elevation =$( "#txtelevation" ),
+		allFields = $( [] ).add( sitename ).add( elevation ),
+		tips = $( ".validateTips" );
+
+	function updateTips( t ) {
+		tips
+			.text( t )
+			.addClass( "ui-state-highlight" );
+		setTimeout( function() {
+			tips.removeClass( "ui-state-highlight", 1500 );
+		}, 500 );
+	}
+
+	function checkLength( o, n, min, max ) {
+		if (o.val().length > max || o.val().length < min ) {
+			o.addClass( "ui-state-error" );
+			updateTipes ( "Length of " + n + " must be between " + min + " and " + max + "." );
+			return false; 
+		} else { 
+			return true;
+		}
+
+	}
+
+ 	function checkRegexp ( o, regexp, n ) {
+		if ( !(regexp.text( o.val() ) ) ) {
+			o.addClass( "ui-state-error" );
+			updateTips( n );
+			return false;
+		} else { 
+			return true;
+		}
+	}
+
+	function addSite() {
+		var valid = true;
+		allFields.removeClass( "ui-state-error" );
+
+		valid = valid && checkLength ( sitename, "sitename", 3, 16);
+		valid = valid && checkLength ( email, "email", 6, 80);
+
+		valid = valid && checkRegexp ( sitename, /^[a-z]([0-9a-z_\s])+$/i, "Site name may consist of a-z, 0-9, underscores, spaces and must begin with a letter.");
+		
+		if ( valid ) {
+			$( "#users tbody" ).append( "<tr>" +
+				"<td>" + sitename.val() + "</td>" +
+				"<td>" + sitename.val() + "</td>" +
+			"</tr>" );
+			dialog.dialog( "close" );
+		}
+		return valid;
+
+	}
+
+	dialog = $( "#dialog-form" ).dialog({
+		autoOpen: false,
+		height: 800,
+		width: 850,
+		modal: true,
+		buttons: {
+			"Create a site": addSite,
+			Cancel: function() {
+				dialog.dialog( "close" );
+			}
+		},
+		close: function () {
+			form[ 0 ].reset();
+			allFields.removeClass( "ui-state-error" );
+		}
+	});
+
+	form = dialog.find( "form" ).on( "submit", function( event ) {
+		event.preventDefault();
+		addSite();
+	});
+
+	$( "#create-site" ).button().on( "click", function() {
+		dialog.dialog( "open" );
+	});
+	});
+
   var markersArray = [];
   
   function validate() {
@@ -385,6 +491,89 @@ while ($row = @$stmt->fetch(PDO::FETCH_ASSOC)) {
       <span id="error" class="small"></span>
       <input id="prev" type="button" value="Prev" onclick="prevStep();" />
       <input id="next" type="button" value="Next" onclick="nextStep();" />    
+      <div class="spacer"></div>
+
+
+
+
+      <input id="create-site" type="button" value="Add Site"/>
+	
+	<div id="divAddSite">
+		<div id="dialog-form" class="ui-widget" title="Create new site">
+			<p class="validateTips">All form fields are required.</p>
+			<form>
+				<fieldset>
+					<div id="container">
+						<div id="row">
+						<label for="sitename" id="lblsitename">Site name</label>
+						<input id="txtsitename" size="30" type="text"></input>
+						</div>
+						<div id="row">
+							<div id="left">
+								<label id="lblelevation">Elevation (m)</label>
+								<input id="txtelevation" size="30" type="text"></input>
+							</div>
+							<div id="middle">
+								<label id="lblmap">Mean Annual Precipitation (mm/yr)</label>
+								<input id="txtmap" size="30" type="text"></input>
+							</div>
+							<div id="right">
+								<label id="lblmat">Mean Annual Temperature (C)</label>
+								<input id="txtmat" size="30" type="text"></input>
+							</div>
+						</div>
+						<div id="row">
+							<div id="left">
+								<label id="lblcity">City</label>
+								<input id="txtcity" size="30" type="text"></input>
+							</div>
+							<div id="middle">
+								<label id="lblstate">State</label>
+								<input id="txtstate" size="30" type="text"></input>
+							</div>
+							<div id="right">
+								<label id="lblcountry">Country</label>
+								<input id="txtcountry" size="30" type="text"></input>
+							</div>
+						</div>
+						<div id="row">
+						<!-- Column 1 -->
+							<label id="lbllat">Lat</label>
+							<input id="txtlat" size="30" type="text"></input>
+							<label id="lbllong">Long</label>
+							<input id="txtlong" size="30" type="text"></input>
+							<label id="lblpctsoil">% Soil</label>
+							<input id="txtpctsoil" size="30" type="text"></input>
+							<label id="lblpctclay">% Clay</label>
+							<input id="txtpctclay" size="30" type="text"></input>
+							<label id="lblpctsand">% Sand</label>
+							<input id="txtpctsand" size="30" type="text"></input>
+							<label id="lblsom">SOM</label>
+							<input id="txtsom" size="30" type="text"></input>
+							<label id="lblgreenhouse">Greenhouse</label>
+							<input id="txtgreenhouse" size="30" type="text"></input>
+							<!-- Column 2 -->
+							<!-- MAP HERE -->
+						</div>
+						<div id="row">
+							<div id="left">
+								<label id="lblnotes">Notes</label>
+								<textarea id="txtnotes" cols="40" rows="10" type="text"></textarea>
+							</div>
+							<div id="middle">
+								<label id="lblsoilnotes">Soil Notes</label>
+								<textarea id="txtsoilnotes" cols="40" rows="10" type="text"></textarea>
+							</div>
+						</div>
+					</div>					
+					<!-- Allow form submission with keyboard without duplicating the dialog button -->
+					<input type="submit" tabindex="-1" style="position:absolute; top:-1000px">
+				</fieldset>
+			</form>
+		</div>
+
+	<!--	<button id="create-site">Create new site</button> -->
+	</div>
       <div class="spacer"></div>
     </form>
 <?php whoami(); ?>    
