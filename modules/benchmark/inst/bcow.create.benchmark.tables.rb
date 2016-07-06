ddacb037169562a801a356410cd84c531f434649
Betsy Cowdery <emcowdery@gmail.com>
2016-07-06 15:33:59 -0400

    Changed `ON DELETE CASCADE` to `ON DELETE RESTRICT`

--- modules/benchmark/inst/bcow.create.benchmark.tables.rb
+++ modules/benchmark/inst/bcow.create.benchmark.tables.rb
@@ -23,13 +23,13 @@ class CreateBenchmarksTables < ActiveRecord::Migration
       ALTER TABLE "benchmarks"
       ADD CONSTRAINT "benchmarks_input_id_fkey" 
         FOREIGN KEY ("input_id") REFERENCES inputs("id")
-        ON DELETE CASCADE ON UPDATE CASCADE,
+        ON DELETE RESTRICT ON UPDATE CASCADE,
       ADD CONSTRAINT "benchmarks_site_id_fkey" 
         FOREIGN KEY ("site_id") REFERENCES sites("id")
-        ON DELETE CASCADE ON UPDATE CASCADE,
+        ON DELETE RESTRICT ON UPDATE CASCADE,
       ADD CONSTRAINT "benchmarks_variable_id_fkey" 
         FOREIGN KEY ("variable_id") REFERENCES variables("id")
-        ON DELETE CASCADE ON UPDATE CASCADE;
+        ON DELETE RESTRICT ON UPDATE CASCADE;
     }
 
     create_table :metrics do |t|
@@ -66,13 +66,13 @@ class CreateBenchmarksTables < ActiveRecord::Migration
       ALTER TABLE "benchmarks_ensembles"
       ADD CONSTRAINT "benchmarks_ensembles_ensemble_id_fkey" 
         FOREIGN KEY ("ensemble_id") REFERENCES ensembles("id")
-        ON DELETE CASCADE ON UPDATE CASCADE,
+        ON DELETE RESTRICT ON UPDATE CASCADE,
       ADD CONSTRAINT "benchmarks_ensembles_model_id_fkey" 
         FOREIGN KEY ("model_id") REFERENCES models("id")
-        ON DELETE CASCADE ON UPDATE CASCADE,
+        ON DELETE RESTRICT ON UPDATE CASCADE,
       ADD CONSTRAINT "benchmarks_ensembles_reference_run_id_fkey" 
         FOREIGN KEY ("reference_run_id") REFERENCES reference_runs("id") 
-        ON DELETE CASCADE ON UPDATE CASCADE;
+        ON DELETE RESTRICT ON UPDATE CASCADE;
     }
 
     create_table :benchmarks_ensembles_scores, :id => false do |t|
@@ -92,13 +92,13 @@ class CreateBenchmarksTables < ActiveRecord::Migration
       ALTER TABLE "benchmarks_ensembles_scores"
       ADD CONSTRAINT "benchmarks_ensembles_scores_benchmark_id_fkey" 
         FOREIGN KEY ("benchmark_id") REFERENCES benchmarks("id")
-        ON DELETE CASCADE ON UPDATE CASCADE,
+        ON DELETE RESTRICT ON UPDATE CASCADE,
       ADD CONSTRAINT "benchmarks_ensembles_scores_benchmarks_ensemble_id_fkey" 
         FOREIGN KEY ("benchmarks_ensemble_id") REFERENCES benchmarks_ensembles("id")
-        ON DELETE CASCADE ON UPDATE CASCADE,
+        ON DELETE RESTRICT ON UPDATE CASCADE,
       ADD CONSTRAINT "benchmarks_ensembles_scores_metric_id_fkey" 
         FOREIGN KEY ("metric_id") REFERENCES metrics("id")
-        ON DELETE CASCADE ON UPDATE CASCADE;
+        ON DELETE RESTRICT ON UPDATE CASCADE;
     }
 
     create_table :reference_runs do |t|
@@ -118,7 +118,7 @@ class CreateBenchmarksTables < ActiveRecord::Migration
       ALTER TABLE "reference_runs"
       ADD CONSTRAINT "reference_runs_model_id_fkey" 
         FOREIGN KEY ("model_id") REFERENCES models("id")
-        ON DELETE CASCADE ON UPDATE CASCADE;
+        ON DELETE RESTRICT ON UPDATE CASCADE;
     }
 
     create_table :benchmark_sets do |t|
@@ -149,10 +149,10 @@ class CreateBenchmarksTables < ActiveRecord::Migration
       ALTER TABLE "benchmarks_metrics"
       ALTER COLUMN "benchmarks_metrics_benchmark_id_fkey" 
         FOREIGN KEY ("benchmark_id") REFERENCES benchmarks("id")
-        ON DELETE CASCADE ON UPDATE CASCADE,
+        ON DELETE RESTRICT ON UPDATE CASCADE,
       ALTER COLUMN "benchmarks_metrics_metric_id_fkey" 
         FOREIGN KEY ("metric_id") REFERENCES metrics("id")
-        ON DELETE CASCADE ON UPDATE CASCADE;
+        ON DELETE RESTRICT ON UPDATE CASCADE;
     }
 
     create_table :benchmarks_benchmarks_reference_runs, :id => false do |t|
@@ -167,10 +167,10 @@ class CreateBenchmarksTables < ActiveRecord::Migration
 
       ALTER COLUMN "benchmarks_benchmarks_reference_runs_benchmark_id_fkey" 
         FOREIGN KEY ("benchmark_id") REFERENCES benchmarks("id")
-        ON DELETE CASCADE ON UPDATE CASCADE,
+        ON DELETE RESTRICT ON UPDATE CASCADE,
       ALTER COLUMN "benchmarks_benchmarks_reference_runs_reference_run_id_fkey" 
         FOREIGN KEY ("reference_run_id") REFERENCES reference_runs("id")
-        ON DELETE CASCADE ON UPDATE CASCADE;
+        ON DELETE RESTRICT ON UPDATE CASCADE;
     }
 
     create_table :benchmark_sets_benchmark_reference_runs, :id => false do |t|
@@ -185,10 +185,10 @@ class CreateBenchmarksTables < ActiveRecord::Migration
 
       ALTER COLUMN "benchmark_sets_benchmark_reference_runs_benchmark_set_id_fkey" 
         FOREIGN KEY ("benchmark_set_id") REFERENCES benchmark_sets("id")
-        ON DELETE CASCADE ON UPDATE CASCADE,
+        ON DELETE RESTRICT ON UPDATE CASCADE,
       ALTER COLUMN "benchmark_sets_benchmark_reference_runs_reference_run_id_fkey" 
         FOREIGN KEY ("reference_run_id") REFERENCES reference_runs("id")
-        ON DELETE CASCADE ON UPDATE CASCADE;
+        ON DELETE RESTRICT ON UPDATE CASCADE;
     }
     end
 
