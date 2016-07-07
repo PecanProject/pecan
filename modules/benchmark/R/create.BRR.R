12ac45978437a13878279f26cbe589dd67ad32ea
Betsy Cowdery <emcowdery@gmail.com>
2016-07-07 01:41:14 -0400

    Function returns BRR, adding error message

--- modules/benchmark/R/create.BRR.R
+++ modules/benchmark/R/create.BRR.R
@@ -10,14 +10,14 @@
 ##' @author Betsy Cowdery 
 
 create.BRR <- function(ensemble.id, workflow, con){
-
-cnd1 <- workflow$hostname == fqdn() 
-cnd2 <- workflow$hostname == 'test-pecan.bu.edu' & fqdn() == 'pecan2.bu.edu'
-cnd3 <- workflow$hostname == 'pecan2.bu.edu' & fqdn() == 'test-pecan.bu.edu'
+  
+  cnd1 <- workflow$hostname == fqdn() 
+  cnd2 <- workflow$hostname == 'test-pecan.bu.edu' & fqdn() == 'pecan2.bu.edu'
+  cnd3 <- workflow$hostname == 'pecan2.bu.edu' & fqdn() == 'test-pecan.bu.edu'
   
   
-if(cnd1|cnd2|cnd3){  # If the ensemble run was done on localhost, turn into a BRR
-  
+  if(cnd1|cnd2|cnd3){  # If the ensemble run was done on localhost, turn into a BRR
+    
     BRR <- db.query(paste0("INSERT INTO reference_runs (model_id, settings, user_id, created_at, updated_at) VALUES(",workflow$model_id,", '",workflow$folder,"' , ",user_id,", NOW() , NOW()) RETURNING *;"),con)
     
     bm.ensemble <- db.query(paste0("INSERT INTO benchmarks_ensembles (reference_run_id, ensemble_id, model_id, user_id, created_at, updated_at, citation_id) VALUES(",BRR$id,",",ensemble.id,",", BRR$model_id,", ",user_id,", NOW() , NOW(), 1000000001 ) RETURNING *;"),con)
@@ -25,7 +25,9 @@ if(cnd1|cnd2|cnd3){  # If the ensemble run was done on localhost, turn into a BR
     warning("Because you are creating a new reference run you will need to manually relate benchmarks.  This is done by 1) creating records for new data sources using the formats, inputs and dbfiles tables 2) Relating benchmarks with the reference run in the benchmarks_benchmarks_reference_runs table.")
     
     return(BRR)
-  }
+  }else{logger.error(sprintf("Cannot create a benchmark reference run for a run on hostname: %s",  workflow$hostname))}
+}
+  
   
   # Case in which we would need a remote connection to get the pecan.xml file - not functional
   
@@ -46,9 +48,4 @@ if(cnd1|cnd2|cnd3){  # If the ensemble run was done on localhost, turn into a BR
   #   BRR <- db.query(paste0("INSERT INTO reference_runs (model_id, settings, user_id, created_at, updated_at) VALUES(",settings$model$id,", '",settings$outdir,"' , ",user_id,", NOW() , NOW()) RETURNING *;"),con)
   #   
   #   bm.ensemble <- db.query(paste0("INSERT INTO benchmarks_ensembles (reference_run_id, ensemble_id, model_id, user_id, created_at, updated_at, citation_id) VALUES(",BRR$id,",",ensemble$id,",", BRR$model_id,", ",user_id,", NOW() , NOW(), 1000000001 ) RETURNING *;"),con)
-  #   
-  # }
-  
-  return(bm.ensemble)
-  
-}
+  #   }
