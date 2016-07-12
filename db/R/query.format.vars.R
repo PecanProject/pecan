8713fdba2259611d1ec4369f94da098bb2a40793
ankurdesai <desai@aos.wisc.edu>
2016-07-12 00:54:51 -0500

    Fix for query.format to work with regional met

--- db/R/query.format.vars.R
+++ db/R/query.format.vars.R
@@ -34,53 +34,63 @@ query.format.vars <- function(input.id,con,format.id){
   
   # get variable names and units of input data
   fv <- db.query(paste("SELECT variable_id,name,unit,storage_type,column_number from formats_variables where format_id = ", f$id),con)
-  colnames(fv) <- c("variable_id", "orig_name", "orig_units", "storage_type", "column_number")
-  fv$variable_id <- as.numeric(fv$variable_id)
+  if (nrow(fv)>0) {
+    colnames(fv) <- c("variable_id", "orig_name", "orig_units", "storage_type", "column_number")
+    fv$variable_id <- as.numeric(fv$variable_id)
   
-  n <- dim(fv)[1]
+    n <- dim(fv)[1]
   
-  vars <- as.data.frame(matrix(NA, ncol=3, nrow=n))
+    vars <- as.data.frame(matrix(NA, ncol=3, nrow=n))
   
-  # get bety names and units 
+    # get bety names and units 
 
-  for(i in 1:n){
-    vars[i,] <- as.matrix(db.query(paste("SELECT id, name, units from variables where id = ",
-                               fv$variable_id[i]),con))
+    for(i in 1:n){
+      vars[i,] <- as.matrix(db.query(paste("SELECT id, name, units from variables where id = ",
+                                 fv$variable_id[i]),con))
+    }
+    colnames(vars) <- c("variable_id", "bety_name", "bety_units")
+    vars$variable_id <- as.numeric(fv$variable_id)
+  
+  
+    # Fill in CF vars
+    # This will ultimately be useful when looking at met variables where CF != Bety
+    # met <- read.csv(system.file("/data/met.lookup.csv", package= "PEcAn.data.atmosphere"), header = T, stringsAsFactors=FALSE)
+  
+    #Fill in MstMIP vars 
+    #All pecan output is in MstMIP variables 
+  
+    bety_mstmip <- read.csv(system.file("bety_mstmip_lookup.csv", package= "PEcAn.DB"), header = T, stringsAsFactors=FALSE)
+  
+    vars_full <- merge(fv, merge(vars, bety_mstmip, by = "bety_name", all.x = TRUE), by="variable_id", all=TRUE) # not sure if all=TRUE is appropriate here or not.
+  
+    vars_full$pecan_name <- vars_full$mstmip_name
+    vars_full$pecan_units <- vars_full$mstmip_units
+    ind <- is.na(vars_full$pecan_name)
+    vars_full$pecan_name[ind] <- vars_full$bety_name[ind]
+    vars_full$pecan_units[ind] <- vars_full$bety_units[ind]
+  
+    header <- as.numeric(f$header)
+    skip <- ifelse(is.na(as.numeric(f$skip)),0,as.numeric(f$skip))
+  
+    # Final format list
+    format <- list(file_name = f$name,
+                   mimetype = f$mimetype,
+                   vars = vars_full,
+                   skip = skip, 
+                   header = header,
+                   na.strings=c("-9999","-6999","9999"), # This shouldn't be hardcoded in, but not specified in format table ?
+                   site = site.id,
+                   lat = site.lat,
+                   lon = site.lon
+                   )
+  } else {
+    format <- list(file_name = f$name,
+                   mimetype = f$mimetype,
+                   na.strings=c("-9999","-6999","9999"), # This shouldn't be hardcoded in, but not specified in format table ?
+                   site = site.id,
+                   lat = site.lat,
+                   lon = site.lon
+    )
   }
-  colnames(vars) <- c("variable_id", "bety_name", "bety_units")
-  vars$variable_id <- as.numeric(fv$variable_id)
-  
-  
-  # Fill in CF vars
-  # This will ultimately be useful when looking at met variables where CF != Bety
-  # met <- read.csv(system.file("/data/met.lookup.csv", package= "PEcAn.data.atmosphere"), header = T, stringsAsFactors=FALSE)
-  
-  #Fill in MstMIP vars 
-  #All pecan output is in MstMIP variables 
-  
-  bety_mstmip <- read.csv(system.file("bety_mstmip_lookup.csv", package= "PEcAn.DB"), header = T, stringsAsFactors=FALSE)
-  
-  vars_full <- merge(fv, merge(vars, bety_mstmip, by = "bety_name", all.x = TRUE), by="variable_id", all=TRUE) # not sure if all=TRUE is appropriate here or not.
-  
-  vars_full$pecan_name <- vars_full$mstmip_name
-  vars_full$pecan_units <- vars_full$mstmip_units
-  ind <- is.na(vars_full$pecan_name)
-  vars_full$pecan_name[ind] <- vars_full$bety_name[ind]
-  vars_full$pecan_units[ind] <- vars_full$bety_units[ind]
-  
-  header <- as.numeric(f$header)
-  skip <- ifelse(is.na(as.numeric(f$skip)),0,as.numeric(f$skip))
-  
-  # Final format list
-  format <- list(file_name = f$name,
-                 mimetype = f$mimetype,
-                 vars = vars_full,
-                 skip = skip, 
-                 header = header,
-                 na.strings=c("-9999","-6999","9999"), # This shouldn't be hardcoded in, but not specified in format table ?
-                 site = site.id,
-                 lat = site.lat,
-                 lon = site.lon
-                 )
   return(format)
 }
