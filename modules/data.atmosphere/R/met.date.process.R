bbe0348b4f4e5c6cbb6afe6a540ab3d27bb61888
tonygardella <tonygard@bu.edu>
2016-08-02 18:23:09 -0400

    initial script to find dates

---
+++ modules/data.atmosphere/R/met.date.process.R
@@ -0,0 +1,31 @@
+##' @title met.data.process
+##' @export
+##'
+##' @param dates, list of date information from db and run
+##'
+##' @author Tony Gardella, Elizabeth Cowdery
+
+dates2process <- function(dates{
+
+  if(run_start < db_start & run_end < db_run){
+    # run dates are for years before db run dates
+    dates$new_start = run_start
+    dates$new_end = db_start
+    dates$update_new = run_start
+    dates$update_end = db_end
+  }else(run_start > db_start & run_end > db_run){
+    #runs dates are for years after db dates
+      dates$new_start = db_end
+      dates$new_end = run_end
+      dates$update_new = db_start
+      dates$update_end = run _end
+  }else(run_start < db_start & run_end > db_run){
+    #run dates envelope existing db dates
+      dates$new_start = run_start
+      dates$new_end = run_end
+      dates$update_new = run_start
+      dates$update_end = run_end
+  }
+}
+
+
