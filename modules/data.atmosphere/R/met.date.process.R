807b5c54de0728e65790ba913a3f915265228379
tonygardella <tonygard@bu.edu>
2016-08-03 18:07:20 -0400

    added subset case to met.dates.process

--- modules/data.atmosphere/R/met.date.process.R
+++ modules/data.atmosphere/R/met.date.process.R
@@ -6,25 +6,31 @@
 
 dates2process <- function(dates){
 
-  if(run_start < db_start & run_end < db_run){
+  if(run_start < db_start & run_end < db_end){
     # run dates are for years before db run dates
-    dates$new_start = run_start
-    dates$new_end = db_start
-    dates$update_new = run_start
+    dates$new_start = run_start,
+    dates$new_end = db_start,
+    dates$update_new = run_start,
     dates$update_end = db_end
-  }else(run_start > db_start & run_end > db_run){
+  }else(run_start > db_start & run_end > db_end){
     #runs dates are for years after db dates
-      dates$new_start = db_end
-      dates$new_end = run_end
-      dates$update_new = db_start
+      dates$new_start = db_end,
+      dates$new_end = run_end,
+      dates$update_new = db_start,
       dates$update_end = run_end
-  }else(run_start < db_start & run_end > db_run){
+  }else(run_start < db_start & run_end > db_end){
     #run dates envelope existing db dates
-      dates$new_start = run_start
-      dates$new_end = run_end
-      dates$update_new = run_start
+      dates$new_start = run_start,
+      dates$new_end = run_end,
+      dates$update_new = run_start,
       dates$update_end = run_end
-  }
+  }else(run_start > db_start & run_end < db_end){
+     #run dates are between db dates(subset case)
+      dates$new_start = run_start,
+      dates$new_end = run_end,
+      dates$update_new = run_start,
+      dates$update_end = run_end
+
 }
 
 
