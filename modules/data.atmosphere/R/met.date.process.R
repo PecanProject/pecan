03a0f045411fcbb269a930d58db4218562b72766
Betsy Cowdery <emcowdery@gmail.com>
2016-08-05 13:32:14 -0400

    Fixed typo errors in met.date.process

--- modules/data.atmosphere/R/met.date.process.R
+++ modules/data.atmosphere/R/met.date.process.R
@@ -5,32 +5,32 @@
 ##' @author Tony Gardella, Elizabeth Cowdery
 
 dates2process <- function(dates){
-
+  
   if(run_start < db_start & run_end < db_end){
     # run dates are for years before db run dates
-    dates$new_start = run_start,
-    dates$new_end = db_start,
-    dates$update_new = run_start,
+    dates$new_start = run_start
+    dates$new_end = db_start
+    dates$update_new = run_start
     dates$update_end = db_end
-  }else(run_start > db_start & run_end > db_end){
+  }else if(run_start > db_start & run_end > db_end){
     #runs dates are for years after db dates
-      dates$new_start = db_end,
-      dates$new_end = run_end,
-      dates$update_new = db_start,
-      dates$update_end = run_end
-  }else(run_start < db_start & run_end > db_end){
+    dates$new_start = db_end
+    dates$new_end = run_end
+    dates$update_new = db_start
+    dates$update_end = run_end
+  }else if(run_start < db_start & run_end > db_end){
     #run dates envelope existing db dates
-      dates$new_start = run_start,
-      dates$new_end = run_end,
-      dates$update_new = run_start,
-      dates$update_end = run_end
-  }else(run_start > db_start & run_end < db_end){
-     #run dates are between db dates(subset case)
-      dates$new_start = run_start,
-      dates$new_end = run_end,
-      dates$update_new = run_start,
-      dates$update_end = run_end
-
+    dates$new_start = run_start
+    dates$new_end = run_end
+    dates$update_new = run_start
+    dates$update_end = run_end
+  }else if(run_start > db_start & run_end < db_end){
+    #run dates are between db dates(subset case)
+    dates$new_start = run_start
+    dates$new_end = run_end
+    dates$update_new = run_start
+    dates$update_end = run_end
+  }
 }
-
-
+  
+  
