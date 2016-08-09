1dc2d815057622ba304de2e1ddcf63ebb264180f
Betsy Cowdery <emcowdery@gmail.com>
2016-08-09 00:33:42 -0400

    Removing any db.queries and the sections that load input and model data. This now happens in calc.benchmarks.

--- modules/benchmark/R/calc.benchmark.R
+++ modules/benchmark/R/calc.benchmark.R
@@ -17,7 +17,9 @@ calc.benchmark <- function(bm.ensemble, con){
   
   
   ens <- db.query(paste("SELECT * FROM ensembles where id = ",bm.ensemble$ensemble_id,";"), con) 
-  wf <- db.query(paste("SELECT w.* FROM workflows as w join ensembles as e on w.id = e.workflow_id where e.id = ",bm.ensemble$ensemble_id,";"), con)
+  wf <- db.query(paste(
+    "SELECT w.* FROM workflows as w join ensembles as e on w.id = e.workflow_id",
+    "WHERE e.id = ",bm.ensemble$ensemble_id,";"), con)
   site  <- query.site(wf$site_id, con)
   start_year <- year(wf$start_date)
   end_year <- year(wf$end_date)
@@ -25,7 +27,11 @@ calc.benchmark <- function(bm.ensemble, con){
   #How are we dealing with ensemble runs? Right now I've hardcoded to select the first run.
   
   # All benchmarking records for the given benchmarking ensemble id
-  bms <- db.query(paste("SELECT * FROM benchmarks as b join benchmarks_benchmarks_reference_runs as r on b.id=r.benchmark_id  join  benchmarks_ensembles as be on r.reference_run_id = be.reference_run_id where be.ensemble_id = ",bm.ensemble$ensemble_id,";"), con) 
+  bms <- db.query(paste(
+    "SELECT * FROM benchmarks as b",
+    "JOIN benchmarks_benchmarks_reference_runs as r on b.id=r.benchmark_id",
+    "JOIN benchmarks_ensembles as be on r.reference_run_id = be.reference_run_id",
+    "WHERE be.ensemble_id = ",bm.ensemble$ensemble_id,";"), con) 
   
   
   # Determine how many data sets inputs are associated with the benchmark id's
@@ -40,29 +46,83 @@ calc.benchmark <- function(bm.ensemble, con){
     bm.ids <- bms$id[which(bms$input_id == obvs.id)]
     data.path <- query.file.path(obvs.id,wf$hostname,con)
     format_full <- format <- query.format.vars(obvs.id,con) 
-    # format is a list, metrics is a dataframe, both can be long. How to pass to calc.metrics remotely? 
-    # Maybe as an Rdata file?
+
+    # ---- LOAD INPUT DATA ---- #
+    # Right now I'm making the inappropriate assumption that storage type will be 
+    # empty unless it's a time variable. 
+    # This is because I haven't come up for a good way to test that a character string is a date format
+
+    format$vars <- format_full$vars %>% filter(., variable_id %in% db.query(paste(
+      "SELECT variable_id from benchmarks WHERE id IN (", paste(bm.ids, collapse = ","),");"),con)[[1]] | 
+        nchar(storage_type) > 0)  
+    st <- format$vars$storage_type
+    time.row <- which(nchar(st)>1 & substr(st, 1,1) == "%")
+    # vars.used.index is redundant and will be removed when I'm sure it won't break the PDA code. 
+    vars.used.index <- which(nchar(st)==0) 
     
+    obvs <- load.data(data.path, format, start_year, end_year, site, vars.used.index, time.row)
+    dat_vars <- format$vars$pecan_name
+    obvs_full = obvs
+    
+    # ---- LOAD MODEL DATA ---- #
+
+    model_vars <- format$vars$pecan_name[-time.row]
+    model <- as.data.frame(read.output(runid=basename(model_run), outdir=model_run, 
+                                       start.year=start_year, end.year=end_year, 
+                                       c("time",model_vars)))
+    vars.used.index <- which(format$vars$pecan_name %in% names(model)[!names(model)=="time"])
+    
+    # We know that the model output time is days since the beginning of the year.
+    # Make a column of years to supplement the column of days of the year.
+    years <- start_year:end_year
+    Diff <- diff(model$time)
+    n <- model$time[c(which(Diff < 0), length(model$time))]
+    y <- c()
+    for(i in 1:length(n)){
+      y <- c(y, rep(years[i], n[i]))
+    }
+    model$year <- y
+    model$posix <- strptime(paste(model$time, model$year), format = "%j %Y")
+    model_full = model
+
+
+    ####################################################
     results.list <- list()
-    vars.list <- list()
-    metrics.list <- list()
-    
+
     # Loop over benchmark ids
     for(i in 1:length(bm.ids)){
       bm <- db.query(paste("SELECT * from benchmarks where id =", bm.ids[i]), con)
-      vars.list <- append(vars.list,format_full$vars[nchar(format_full$vars$storage_type) > 0 | 
-                                        format_full$vars$variable_id == bm$variable_id,])
-      metrics.list <- append(metrics.list,db.query(paste(
-        "SELECT m.name, m.id from metrics as m JOIN benchmarks_metrics as b ON m.id = b.metric_id WHERE b.benchmark_id = ",
-        bm.ids[i]),con))
+      metrics <- db.query(paste(
+        "SELECT m.name, m.id from metrics as m",
+        "JOIN benchmarks_metrics as b ON m.id = b.metric_id",
+        "WHERE b.benchmark_id = ", bm.ids[i]),con)
+      var <- filter(format$vars, variable_id == bm$variable_id)[,"pecan_name"]
+      
+      obvs.bm <- obvs_full %>% select(., one_of(c("posix", var )))
+      model.bm <- model_full %>% select(., one_of(c("posix", var )))
+
+      r <- calc.metrics(model.bm, obvs.bm, var, metrics, start_year, end_year, bm, ens, model_run)
+      
+      benchmarks_ensemble_id <- db.query(paste("SELECT id FROM benchmarks_ensembles where ensemble_id = ",
+                                               ens$id),con )[[1]]
+      # for(j in 1:nrow(r)){
+      # db.query(paste0(
+      #   "INSERT INTO benchmarks_ensembles_scores",
+      #   "(score, benchmarks_ensemble_id, benchmark_id, metric_id, created_at, updated_at) VALUES ",
+      #   "('",score[j],"',",benchmarks_ensemble_id,", ",bm$id,",",metrics$id[j],", NOW(), NOW())"),con)
+      # }
+      
+      results.list <- append(results.list, list(r))
+      
     } #end loop over benchmark ids
     
-    r <- calc.metrics(data.path, format, model_run, metrics, start_year, end_year, site,bm,ens)
-    results.list <- append(results.list, list(r))
     
     
-    results <- append(results, list(list(Reduce(function(...) merge(..., by="metric", all=T), 
-                                                results.list), data.path, format_full)))
+    results <- append(results,
+                      list(list( bench.results = Reduce(function(...) merge(..., by="metric", all=T), 
+                                                results.list),
+                                 data.path = data.path, 
+                                 format = format_full)))
   }
   
   names(results) <- sprintf("input.%0.f",inputs)
--- modules/benchmark/R/calc.metrics.R
+++ modules/benchmark/R/calc.metrics.R
@@ -1,58 +1,25 @@
 ##' @name calc.metrics
 ##' @title calc.metrics
 ##' @export
-##' @param data.path
-##' @param format
-##' @param model_run
+##' @param model.bm
+##' @param obvs.bm
+##' @param var
 ##' @param metrics
 ##' @param start_year
 ##' @param end_year
+##' @param bm
+##' @param ens
+##' @param model_run
 ##' 
 ##' 
 ##' @author Betsy Cowdery
 
-calc.metrics <- function(data.path, format, model_run, metrics, start_year, end_year, site, bm, ens){
-  
-  # Right now I'm making the inappropriate assumption that storage type will be 
-  # empty unless it's a time variable. 
-  # This is because I haven't come up for a good way to test that a character string is a date format
-  st <- format$vars$storage_type
-  
-  time.row <- which(nchar(st)>1 & substr(st, 1,1) == "%")
-  
-  # ---- MODEL DATA ---- #
-  
-  model_vars <- format$vars$pecan_name[-time.row]
-  model <- as.data.frame(read.output(runid=basename(model_run), outdir=model_run, 
-                                     start.year=start_year, end.year=end_year, 
-                                     c("time",model_vars)))
-  vars.used.index <- which(format$vars$pecan_name %in% names(model)[!names(model)=="time"])
-  
-  # We know that the model output time is days since the beginning of the year.
-  # Make a column of years to supplement the column of days of the year.
-  years <- start_year:end_year
-  Diff <- diff(model$time)
-  n <- model$time[c(which(Diff < 0), length(model$time))]
-  y <- c()
-  for(i in 1:length(n)){
-    y <- c(y, rep(years[i], n[i]))
-  }
-  model$year <- y
-  model$posix <- strptime(paste(model$time, model$year), format = "%j %Y")
-  model_full = model
-  
-  # ---- INPUT DATA ---- #
-  
-  obvs <- load.data(data.path, format, start_year, end_year, site, vars.used.index, time.row)
-  dat_vars <- format$vars$pecan_name[vars.used.index]
-  obvs_full = obvs
-  
-  #########################
+calc.metrics <- function(model.bm, obvs.bm, var, metrics, start_year, end_year, bm, ens, model_run){
 
-  dat <- align.data(model_full, obvs_full, dat_vars, start_year, end_year)
+  dat <- align.data(model.bm, obvs.bm, var, start_year, end_year)
   
-  results <- as.data.frame(matrix(NA, nrow = length(metrics$name), ncol = length(dat_vars)+1))
-  colnames(results) <- c("metric", dat_vars)
+  results <- as.data.frame(matrix(NA, nrow = length(metrics$name), ncol = length(var)+1))
+  colnames(results) <- c("metric", var)
   rownames(results) <- metrics$name
   results$metric <- metrics$name
   
@@ -60,24 +27,27 @@ calc.metrics <- function(data.path, format, model_run, metrics, start_year, end_
   for(i in 1:length(file.sources)){
     source(file.sources[i])
   }
-  
-  for(v in 1:length(dat_vars)){
-    metric_dat <- dat[,c(paste(dat_vars[v], c("m", "o"),sep = "." ),"posix")]
+
+    metric_dat <- dat[,c(paste(var, c("m", "o"),sep = "." ),"posix")]
     colnames(metric_dat)<- c("model","obvs","time")
     
     for(m in 1:length(metrics$name)){
       
       fcn <- paste0("metric.",metrics$name[m])
-      score <- as.character(do.call(fcn, args <- list(metric_dat,dat_vars[v])))
-      results[metrics$name[m],dat_vars[v]] <- score
       
-      benchmarks_ensemble_id <- db.query(paste("SELECT id FROM benchmarks_ensembles where ensemble_id = ", ens$id),con )[[1]]
-      
-      db.query(sprintf("INSERT into benchmarks_ensembles_scores (score, benchmarks_ensemble_id, benchmark_id, metric_id, created_at, updated_at) VALUES ('%s', %.0f, %.0f, %.0f, NOW(), NOW())",
-                       score, benchmarks_ensemble_id, bm$id, metrics$id[m]),con)
+      if(tail(unlist(strsplit(fcn, "[.]")),1) =="plot"){
+        filename = file.path(dirname(dirname(model_run)), 
+                             paste("benchmark",metrics$name[m],var,ens$id,"pdf", sep = "."))
+        do.call(fcn, args <- list(metric_dat,var,filename))
+        score <- filename
+        results[metrics$name[m],var] <- score
+      }else{
+        score <- as.character(do.call(fcn, args <- list(metric_dat,var)))
+        results[metrics$name[m],var] <- score
+      }
       
     } #end loop over metrics
-  } #end loop over variables
+
 
 return(results)
 }
