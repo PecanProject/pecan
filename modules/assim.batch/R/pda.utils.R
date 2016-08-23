67e8e06e5078ebe5105b8b7eab8bf932108d4018
istem fer <fer.istem@gmail.com>
2016-08-23 16:35:40 -0400

    change to GMT for model output too

--- modules/assim.batch/R/pda.utils.R
+++ modules/assim.batch/R/pda.utils.R
@@ -614,7 +614,7 @@ pda.get.model.output <- function(settings, run.id, inputs) {
   
     # seq.POSIXt returns class "POSIXct"
     # the model output is since the beginning of the year but 'settings$run$start.date' may not be the first day of the year, using lubridate::floor_date
-    model$posix <- seq.POSIXt(from = lubridate::floor_date(as.POSIXct(settings$run$start.date), "year"), by = diff(model.secs)[1], length.out = length(model$time))
+    model$posix <- seq.POSIXt(from = lubridate::floor_date(as.POSIXlt(settings$run$start.date, tz="GMT"), "year"), by = diff(model.secs)[1], length.out = length(model$time))
   
     dat <- align.data(model_full = model, obvs_full = inputs[[k]]$data, dat_vars = vars.used, 
                       start_year = start.year, end_year = end.year)
