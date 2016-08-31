47655c60417fec8f209d12f3e9d8151eb6ad763a
Ryan Kelly <rkelly@neptuneinc.org>
2016-08-31 15:36:08 -0400

    Allow papply to proceed with remaining Settings when one or more fails. Also added testthat tests for papply

--- settings/R/papply.R
+++ settings/R/papply.R
@@ -1,7 +1,32 @@
 ##' @export
-papply <- function(settings, fn, ...) {
+papply <- function(settings, fn, stop.on.error=FALSE, ...) {
   if(is.MultiSettings(settings)) {
-    result <- lapply(settings, fn, ...)
+    result <- list()
+    for(i in seq_along(settings)) {
+      logger.debug(paste0("papply executing ", deparse(substitute(fn)), 
+        " on element ", i, " of a MultiSettings object."))
+      
+      result.i <- try(fn(settings[[i]], ...), silent = TRUE)
+                      
+      if(!is(result.i, "try-error")) {
+        ind <- length(result) + 1
+        result[[ind]] <- result.i
+        if(!is.null(settingNames(settings))) {
+          names(result)[ind] <- settingNames(settings)[i]
+        }
+      } else {
+        if(stop.on.error) {
+          stop(paste0("papply threw an error for element ", i, 
+            ", and is aborting since stop.on.error=TRUE. Message was: '", 
+            as.character(result.i), "'"))
+        } else {
+          warning(paste0("papply threw an error for element ", i, 
+            ", but is continuing since stop.on.error=FALSE", 
+            " (there will be no results for this element, however). Message was: '", 
+            as.character(result.i), "'"))
+        }
+      }
+    }
     if(all(sapply(result, is.Settings)))
       result <- MultiSettings(result)
     return(result)
