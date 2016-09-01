772c954980bb00cc0955b4ba2fabf4f6f2676cea
Shawn P. Serbin <sserbin@bnl.gov>
2016-09-01 17:49:45 -0400

    Updates to met2model.MAAT. Currently working Next step to push xml to run dir

--- models/maat/R/met2model.MAAT.R
+++ models/maat/R/met2model.MAAT.R
@@ -206,16 +206,13 @@ met2model.MAAT <- function(in.path, in.prefix, outfolder, start_date, end_date, 
     #</met_data_translator>
     
     # Create leaf_user_met.xml
-    main.tag <- newXMLNode("met_data_translator")
     # TODO: make this dynamic with names above!
     # TODO: add the additional met variables, make dynamic
     leaf_user_met_list <- list(leaf = list(env = list(temp = "Tair_degC", par = "PAR_umol_m2_s")))
-    leaf_user_met_xml <- listToXml(li)
+    leaf_user_met_xml <- listToXml(leaf_user_met_list,"met_data_translator")
     
     # output XML file
-    saveXML(leaf_user_met_xml, file = file.path(settings$rundir, run.id, "leaf_user_met.xml"), indent=TRUE, prefix = PREFIX_XML)
-    # for testing
-    #saveXML(leaf_user_met_xml, file = file.path(outfolder, "test_xml/", "leaf_user_met.xml"), indent=TRUE, prefix = PREFIX_XML)
+    saveXML(leaf_user_met_xml, file = file.path(outfolder, "leaf_user_met.xml"), indent=TRUE, prefix = PREFIX_XML)
     
     invisible(results)
     
