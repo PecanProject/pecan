e68c2dae8b7491f3b89a8f58aa1541d40bafdaab
Mike Dietze <dietze@bu.edu>
2016-09-20 10:04:36 -0500

    FATES work done yesterday aimed at having PEcAn set up the whole file structure of input data that CLM expects. Decided to go this way after realizing that the DOMAIN file wasn't in the run directory and would get overwritten constantly in the reference case unless we moved it there. So now, at a high level, the run directory for a CLM/FATES run will have two folders, case and inputs, the former being where the model is run the latter being where the inputs are. Started by creating a function that would recursively copy the file structure of the reference case, creating folders and symlinks to files in the reference case. In BETY, added 'defaults' as a required input for CLM/FATES and the input record will be a pointer to the FOLDER of the reference case inputs, just as the pointer for the MODEL is the folder of the reference case itself. Need to finish and test. If this works the next step is, for each file that is MODIFIED by PEcAn, to have write.configs delete the default symlink and replace it with the PEcAn generated file/link. JUST REALISED THIS HAS TO BE DONE IN JOBS.SH because those symlinks may be on a remote machine!

---
+++ models/fates/R/recurse.create.R
@@ -0,0 +1,25 @@
+##' @name recurse.create
+##' @title recurse.create
+##' @description recursively follow the file structure in 'ins' and create all the same folders in 'path' as well as symbolic links to all the file. This is done, rather than creating a symbolic link to the whole structure, so individual files can later be unlinked and replaced with different files/links.
+##' @author Mike Dietze
+##' @export
+recurse.create <- function(path,ins){
+  files <- list.files(ins,full.names=TRUE)
+  if(length(files)==0) return()
+  is.dir = dir.exists(files)  ## determine which files are actually folders
+  ## create links to true files
+  curr <- files[!is.dir]
+  for(i in seq_along(curr)){
+    file.symlink(curr[i],file.path(path,basename(curr[i])))
+  }
+  
+  down <- files[is.dir]
+  for(i in seq_along(down)){
+    ndir <- file.path(path,basename(down[i]))
+    ## create folders
+    dir.create(ndir)
+    
+    ## recurse to fill folders
+    recurse.create(ndir,down[i])
+  }     
+}
