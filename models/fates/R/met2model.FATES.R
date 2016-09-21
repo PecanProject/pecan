ab846dbf82335dbc975b13196ded28872241f194
Mike Dietze <dietze@bu.edu>
2016-09-20 22:26:39 -0500

    met2model.FATES: Initial code to change variable names and split files from annual to monthly. Still need code to write the header files/namelists

--- models/fates/R/met2model.FATES.R
+++ models/fates/R/met2model.FATES.R
@@ -23,121 +23,132 @@
 ##' @param verbose should the function be very verbosefor(year in start_year:end_year)
 met2model.FATES <- function(in.path,in.prefix,outfolder,start_date, end_date, lst=0,lat,lon,..., overwrite=FALSE,verbose=FALSE){
   
-  #General Structure- FATES Uses Netcdf so for now just need to rename vars.(Many not is CF standard. Need to Check that out)
+  #General Structure- FATES Uses Netcdf so we need to rename vars, split files from years into months, and generate the header file
   #Get Met file from inpath.
   #Loop over years (Open nc.file,rename vars,change dimensions as needed,close/save .nc file)
   #close
   #defining temporal dimension needs to be figured out. If we configure FATES to use same tstep then we may not need to change dimensions
   
-#   require("PEcAn.data.atmosphere")
-#   require("PEcAn.utils")
-#   require("ncdf4")
-#   require("udunits2")
-#   
-#   #Process start and end dates
-#   start_date<-as.POSIXlt(start.date,tz="GMT")
-#   end_date<-as.POSIXlt(end.date,tz="GMT")
-#   
-#   start_year <- year(start_date)
-#   end_year <- year(end_date)
-#   
-#   timestep.s<-86400 #Number of seconds in a day
-#   
-#   ## Build met
-#   met <- NULL
-#   for(year in start_year:end_year){
-#     
-#     met.file.y = paste(met.file,year,"nc",sep=".")
-#     
-#     if(file.exists(met.file.y)){
-#       
-#       ## Open netcdf file
-#       nc=nc_open(met.file.y)
-#       
-#       
-#       ## convert time to seconds
-#       sec   <- nc$dim$time$vals  
-#       sec = udunits2::ud.convert(sec,unlist(strsplit(nc$dim$time$units," "))[1],"seconds")
-#       
-#       
-#       
-#       ##build day and  year
-#       
-#       ifelse(leap_year(year)==TRUE,
-#              dt <- (366*24*60*60)/length(sec), #leap year
-#              dt <- (365*24*60*60)/length(sec)) #non-leap year
-#       tstep = round(timestep.s/dt) #time steps per day
-#       
-#       doy <- rep(1:365,each=tstep)[1:length(sec)] 
-#       if(year %% 4 == 0){  ## is leap
-#         doy <- rep(1:366,each=tstep)[1:length(sec)]
-#       }
-#       
-  
-  ## extract variables. These need to be read in and converted to FATES standards
-  
-#   ncvar_rename(ncfile,varid="LONGXY")
-#   ncvar_rename(ncfile,varid="LATIXY")
-#   #     double ZBOT(time, lat, lon) ;
-#   #     ZBOT:long_name = "observational height" ;
-#   #     ZBOT:units = "m" ;
-#   ZBOT = ncvar_rename(ncfile,"ZBOT","ZBOT")
-#   #     
-#   #     double EDGEW(scalar) ;
-#   #     EDGEW:long_name = "western edge in atmospheric data" ;
-#   #     EDGEW:units = "degrees E" ;
-#   EDGEW = ncvar_rename(ncfile,"EDGEW","EDGEW")
-#   
-#   #     double EDGEE(scalar) ;
-#   #     EDGEE:long_name = "eastern edge in atmospheric data" ;
-#   #     EDGEE:units = "degrees E" ;
-#   EDGEE = ncvar_rename(ncfile,"EDGEE","EDGEE")
-#   
-#   #     double EDGES(scalar) ;
-#   #     EDGES:long_name = "southern edge in atmospheric data" ;
-#   #     EDGES:units = "degrees N" ;
-#   EDGES = ncvar_rename(ncfile,"EDGES","EDGES") 
-#   #     
-#   #     double EDGEN(scalar) ;
-#   #     EDGEN:long_name = "northern edge in atmospheric data" ;
-#   #     EDGEN:units = "degrees N" ;
-#   EDGEN = ncvar_rename(ncfile,"EDGEN","air_temperature")
-#   #     double TBOT(time, lat, lon) ;
-#   #     TBOT:long_name = "temperature at the lowest atm level (TBOT)" ;
-#   #     TBOT:units = "K" ;
-#   TBOT  = ncvar_rename(ncfile,"TBOT","specific_humidity")
-#   #     double RH(time, lat, lon) ;   
-#   #     RH:long_name = "relative humidity at the lowest atm level (RH)" ;
-#   #     relative_humidity
-#   #     RH:units = "%" ;
-#   RH    = ncvar_rename(ncfile,"RH","relative_humidity")
-#   #     double WIND(time, lat, lon) ;
-#   #     WIND:long_name = "wind at the lowest atm level (WIND)" ;
-#   #     wind_speed
-#   #     WIND:units = "m/s" ;
-#   WIND  = ncvar_rename(ncfile,"WIND","wind_speed")
-#   #     double FSDS(time, lat, lon) ;
-#   #     FSDS:long_name = "incident solar (FSDS)" ;
-#   #     FSDS:units = "W/m2" ;
-#   FSDS  = ncvar_rename(ncfile,"FSDS","FSDS")
-#   #     double FLDS(time, lat, lon) ;
-#   #     FLDS:long_name = "incident longwave (FLDS)" ;
-#   #     FLDS:units = "W/m2" ;
-#   FLDS  = ncvar_rename(ncfile,"FLDS","")
-#   #     double PSRF(time, lat, lon) ;   
-#   #     PSRF:long_name = "pressure at the lowest atm level (PSRF)" ;
-#   #     PSRF:units = "Pa" ;
-#   PSRF  = ncvar_rename(ncfile,"PSRF","air_pressure")
-#   #     double PRECTmms(time, lat, lon) ;
-#   #     PRECTmms:long_name = "precipitation (PRECTmms)" ;
-#   #     PRECTmms:units = "mm/s" ;
-#   PRECTmms =ncvar_rename(ncfile,"PRECTmmc","precipitation_flux")
-  
- #nc_close(ncfiles)
+   require("PEcAn.utils")
+   require("ncdf4")
+   insert <- function(ncout,name,unit,data){
+     var <- ncvar_def(name=name, units=unit, dim=dim, missval=-6999.0, verbose=verbose)
+     ncout <- ncvar_add(nc=ncout, v=var, verbose=verbose)
+     ncvar_put(nc=ncout, varid=name, vals=data)
+     invisible(ncout)
+   }
+   sm <- c(0,31,59,90,120,151,181,212,243,273,304,334,365)*86400 ## day of year thresholds
 
-#} ### end loop over met files
+   ## Create output directory
+   dir.create(outfolder)
+  
+   #Process start and end dates
+   start_date<-as.POSIXlt(start.date,tz="GMT")
+   end_date<-as.POSIXlt(end.date,tz="GMT")
+   start_year <- year(start_date)
+   end_year <- year(end_date)
 
-#print("Done with met2model.FATES")
+   ## Build met
+   for(year in start_year:end_year){
+     
+     in.file <- file.path(in.path,paste(in.prefix,year,"nc",sep="."))
+     
+     if(file.exists(in.file)){
+       
+       ## Open netcdf file
+       nc<-nc_open(in.file)
+       
+       ## extract variables. These need to be read in and converted to CLM names (all units are correct)
+       time <- ncvar_get(nc,"time")
+       latitude <- ncvar_get(nc,"latitude")
+       longitude <- ncvar_get(nc,"longitude")
+       FLDS <- ncvar_get(nc,"surface_downwelling_longwave_flux_in_air") ## W/m2
+       FSDS <- ncvar_get(nc,"surface_downwelling_shortwave_flux_in_air") ## W/m2
+       PRECTmms <- ncvar_get(nc,"precipitation_flux") ## kg/m2/s -> mm/s (same val, diff name)
+       PSRF <- ncvar_get(nc,"air_pressure") ## Pa
+       SHUM <- ncvar_get(nc,"specific_humidity") ## g/g -> kg/kg
+       TBOT <- ncvar_get(nc,"air_temperature") ## K
+       WIND <- sqrt(ncvar_get(nc,"eastward_wind")^2 + ncvar_get(nc,"northward_wind")^2) ## m/s
+       
+       ## CREATE MONTHLY FILES
+       for(mo in 1:12){
+         tsel <- which(time > sm[mo] & time <= sm[mo+1])
+         outfile <- file.path(outfolder,paste0(formatC(year, width=4, flag="0"),"-",formatC(mo, width=2, flag="0"),".nc"))
+         if(file.exists(outfile) & overwrite == FALSE) next
+         
+         lat.dim <- ncdim_def(name='latitude', units='', vals=1:1, create_dimvar=FALSE)
+         lon.dim <- ncdim_def(name='longitude', units='', vals=1:1, create_dimvar=FALSE)
+         time.dim <- ncdim_def(name='time', units="seconds", vals=time, create_dimvar=TRUE, unlim=TRUE)
+         dim=list(lat.dim,lon.dim,time.dim)
+         
+         # LATITUDE
+         var <- ncvar_def(name="latitude",
+                          units="degree_north",
+                          dim=list(lat.dim,lon.dim), missval=as.numeric(-9999))
+         ncout <- nc_create(outfile,vars=var,verbose=verbose)
+         ncvar_put(nc=ncout, varid='latitude', vals=latitude)
+         
+         # LONGITUDE
+         var <- ncvar_def(name="longitude",
+                          units="degree_east",
+                          dim=list(lat.dim,lon.dim), missval=as.numeric(-9999))
+         ncout <- ncvar_add(nc=ncout, v=var, verbose=verbose)
+         ncvar_put(nc=ncout, varid='longitude', vals=longitude)
+         
+         ## surface_downwelling_longwave_flux_in_air
+         ncout <- insert(ncout,"FLDS","W m-2",FLDS)
+         
+         ## surface_downwelling_shortwave_flux_in_air
+         ncout <- insert(ncout,"FSDS","W m-2",FSDS)
+
+         ## precipitation_flux
+         ncout <- insert(ncout,"PRECTmms","mm/s",PRECTmms)
+         
+         ## air_pressure
+         ncout <- insert(ncout,"PSRF","Pa",PSRF)
+         
+         ## specific_humidity
+         ncout <- insert(ncout,"SHUM","kg/kg",SHUM)
+         
+         ## air_temperature
+         ncout <- insert(ncout,"TBOT","K",TBOT)
+         
+         ## eastward_wind & northward_wind
+         ncout <- insert(ncout,"WIND","m/s",WIND)
+         
+         nc_close(ncout)
+         
+         #   ncvar_rename(ncfile,varid="LONGXY")
+         #   ncvar_rename(ncfile,varid="LATIXY")
+         #   #     
+         #   #     double EDGEW(scalar) ;
+         #   #     EDGEW:long_name = "western edge in atmospheric data" ;
+         #   #     EDGEW:units = "degrees E" ;
+         #   EDGEW = ncvar_rename(ncfile,"EDGEW","EDGEW")
+         #   
+         #   #     double EDGEE(scalar) ;
+         #   #     EDGEE:long_name = "eastern edge in atmospheric data" ;
+         #   #     EDGEE:units = "degrees E" ;
+         #   EDGEE = ncvar_rename(ncfile,"EDGEE","EDGEE")
+         #   
+         #   #     double EDGES(scalar) ;
+         #   #     EDGES:long_name = "southern edge in atmospheric data" ;
+         #   #     EDGES:units = "degrees N" ;
+         #   EDGES = ncvar_rename(ncfile,"EDGES","EDGES") 
+         #   #     
+         #   #     double EDGEN(scalar) ;
+         #   #     EDGEN:long_name = "northern edge in atmospheric data" ;
+         #   #     EDGEN:units = "degrees N" ;
+         #   EDGEN = ncvar_rename(ncfile,"EDGEN","EDGEN")
+       }
+
+       nc_close(nc)
+       
+     } ## end file exists
+
+} ### end loop over met files
+
+logger.info("Done with met2model.FATES")
 
 } ### end met2model.FATES
 
