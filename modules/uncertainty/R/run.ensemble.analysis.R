e9c263187625eaaad494d0e6d37b306fb847fe7b
Mike Dietze <dietze@bu.edu>
2016-09-28 17:55:01 -0500

    run.ensemble.analysis: do a better job of catching missing output

--- modules/uncertainty/R/run.ensemble.analysis.R
+++ modules/uncertainty/R/run.ensemble.analysis.R
@@ -56,9 +56,6 @@ run.ensemble.analysis <- function(settings,plot.timeseries=NA, ensemble.id=NULL,
     logger.warn(paste0("Currently performs ensemble analysis on only one variable at a time. Using first (", variable, ")"))
   }
 
-  cflux <- c("GPP","NPP","NEE","TotalResp","AutoResp","HeteroResp","DOC_flux","Fire_flux") #converted to gC/m2/s
-  wflux <- c("Evap","TVeg","Qs","Qsb","Rainf") #kgH20 m-2 s-1
-
   print(paste("----- Variable: ",variable,sep=""))
 
   #units <- lapply(variable, function(x) { paste0(x, " (", mstmipvar(x, silent=TRUE)$units, ")") })
@@ -68,7 +65,12 @@ run.ensemble.analysis <- function(settings,plot.timeseries=NA, ensemble.id=NULL,
   fname <- ensemble.filename(settings, "ensemble.output", "Rdata", all.var.yr=FALSE,
     ensemble.id=ensemble.id, variable=variable, start.year=start.year, end.year=end.year)
   load(fname)
-
+  my.dat = unlist(ensemble.output)
+  if(is.null(my.dat)){
+    logger.warning("no data in ensemble.output")
+    return()
+  }
+  
   
   ### ------------------- Start ensemble analysis -------------------
   ensemble.results <- list()
@@ -84,12 +86,12 @@ run.ensemble.analysis <- function(settings,plot.timeseries=NA, ensemble.id=NULL,
   
   pdf(file=fname,width=13,height=6)
   par(mfrow=c(1,2),mar=c(4,4.8,1,2.0)) # B, L, T, R
-
-  hist(unlist(ensemble.output),xlab=units,
+  
+  hist(my.dat,xlab=units,
        main="",cex.axis=1.1,cex.lab=1.4,col="grey85")
   box(lwd=2.2)
   
-  boxplot(unlist(ensemble.output),ylab=units,
+  boxplot(my.dat,ylab=units,
           boxwex=0.6,col="grey85", cex.axis=1.1,range=2,pch=21,cex=1.4, bg="black",cex.lab=1.5)
   box(lwd=2.2)
 
