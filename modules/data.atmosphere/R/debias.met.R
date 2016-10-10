bc0e38a961fe66d2592e9945b2528e51550d2494
jsimkins2 <jsimkins2@wisc.edu>
2016-10-10 15:11:10 -0500

    linear regression and blank variables

--- modules/data.atmosphere/R/debias.met.R
+++ modules/data.atmosphere/R/debias.met.R
@@ -18,13 +18,15 @@ debias.met <- function(outfolder, source_met, train_met, site_id, de_method='mea
                    CF.name = c("air_temperature","surface_downwelling_longwave_flux_in_air","air_pressure","surface_downwelling_shortwave_flux_in_air","eastward_wind","northward_wind","specific_humidity","precipitation_flux"),
                    units = c('Kelvin',"W/m2","Pascal","W/m2","m/s","m/s","g/g","kg/m2/s")
   )
-  
+
   #Need to pull the year from the string filename, this assumes that the year is always placed right before '.nc'
   substrRight <- function(x, n){
     substr(x, nchar(x)-n+1, nchar(x))
   }
   sub_str= substrRight(source_met, 7)
   year = substr(sub_str,1, 4)
+  
+  
   #Read in the two datasets, with the dimensions of the source dataset being named sou.list
   sou = list()
   sou.list = list()
@@ -39,9 +41,7 @@ debias.met <- function(outfolder, source_met, train_met, site_id, de_method='mea
   year = as.numeric(year)
   nc_close(tem)
   
-#Need to try and find the closest ameriflux site and year based on the lat/lon/year given 
-#download function (lat_sou,lon_sou,year)
-#   find closest fluxnet site and then download year, if not available, try next year up from 2006 to 2015? 
+# Load in the 'better' data that will be used to train the source. Most of the time this will be observed data. 
   train = list()
   tow = nc_open(train_met)
   for (j in 1:length(var$CF.name)){
@@ -56,15 +56,26 @@ debias.met <- function(outfolder, source_met, train_met, site_id, de_method='mea
   colnames(train) = c("air_temperature","surface_downwelling_longwave_flux_in_air","air_pressure","surface_downwelling_shortwave_flux_in_air","eastward_wind","northward_wind","specific_humidity","precipitation_flux")
   
   #Grab the means/medians of the source and train, find the difference, and correct the source dataset accordingly
-
+  #The following separate variables based on properties so we can appropriately debias based on means/medians
   sou_add = data.frame(sou$air_temperature, sou$air_pressure, sou$eastward_wind, sou$northward_wind)
   sou_mult = data.frame(sou$surface_downwelling_longwave_flux_in_air, sou$surface_downwelling_shortwave_flux_in_air, sou$specific_humidity, sou$precipitation_flux)
   train_add = data.frame(train$air_temperature, train$air_pressure, train$eastward_wind, train$northward_wind)
   train_mult = data.frame(train$surface_downwelling_longwave_flux_in_air, train$surface_downwelling_shortwave_flux_in_air, train$specific_humidity, train$precipitation_flux)
   add_var = c("air_temperature", "air_pressure", "eastward_wind", "northward_wind")
   mult_var = c("surface_downwelling_longwave_flux_in_air","surface_downwelling_shortwave_flux_in_air","specific_humidity","precipitation_flux")
-    
-  if (de_method == 'mean'){
+  # These are for the linear regression argument, the for loop upscales the training dataset to match the length of the source dataset because they must be equal lengths
+  reso = nrow(sou)
+  step = floor(nrow(train)/nrow(sou))
+  daily.met = data.frame()
+  for (n in 1:length(var$DAP.name)){
+    for (x in 1:reso){
+      daily.met[x,n] <- mean(sou[(x*step-step+1):(x*step),n])}
+  }
+  colnames(daily.met) = c("air_temperature","surface_downwelling_longwave_flux_in_air","air_pressure","surface_downwelling_shortwave_flux_in_air","eastward_wind","northward_wind","specific_humidity","precipitation_flux")
+  
+  ### De_method routines!!!!! ###
+  for (n in 1:length(var$DAP.name)){
+    if (de_method == 'mean'){
     mean_sou_add = apply(sou_add,2,mean)
     mean_train_add = apply(train_add,2,mean)
     mean_diff = mean_train - mean_sou
@@ -101,9 +112,27 @@ debias.met <- function(outfolder, source_met, train_met, site_id, de_method='mea
       debi_mult = data.frame(debi_mult)
       colnames(debi_mult) = mult_var
       debi = data.frame(debi_add,debi_mult)
+      } else {
+      if(de_method == 'linear'){
+        debi = list()
+        for(i in 1:length(var$CF.name)){
+          if(all(is.na(sou[[i]]))== FALSE & all(is.na(daily.met[[i]])) == FALSE){
+            lin = lm(sou[[i]] ~ daily.met[[i]])
+            x = as.numeric(lin$coefficients[2])
+            b = as.numeric(lin$coefficients[1])
+            m = sou[[i]]
+          if (var$CF.name[[i]] == 'precipitation_flux'){b = 0}
+          debi[[i]] = (m*x + b)}
+          else {
+            if(all(is.na(sou[[i]])) == TRUE | all(is.na(daily.met[[i]])) == TRUE){
+              debi[[i]] = NA}}}
+        debi = data.frame(debi)
+        colnames(debi) = var$CF.name}
       }
-    } 
-  
+  }
+  }
+
+  # This step just ensures that we aren't breaking laws of nature by having negative precipitation or negative specific humidity 
   debi$precipitation_flux[debi$precipitation_flux < 0] <- 0
   debi$specific_humidity[debi$specific_humidity < 0] <- 0
 
@@ -134,4 +163,4 @@ debias.met <- function(outfolder, source_met, train_met, site_id, de_method='mea
   invisible(results)
 }
 
-
+#debias.met('debi','GFDL', 'US-WCr.2006.nc', 4, de_method = 'linear')
