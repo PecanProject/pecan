514ad4750ed9d8fc51517bac86aa461a6eabdfaf
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2016-10-10 23:24:24 -0400

    Un-screwing-up `met2netcdf.MAESPA.R`

--- models/maespa/R/model2netcdf.MAESPA.R
+++ models/maespa/R/model2netcdf.MAESPA.R
@@ -29,9 +29,7 @@ model2netcdf.MAESPA <- function(outdir, sitelat, sitelon, start_date, end_date, 
   library(Maeswrap)
   
   ### Read in model output using Maeswrap. Dayflx.dat, watbalday.dat
-  
-  dayflx.dataframe <- readdayflux(filename = "Dayflx.dat")
-  
+  dayflx.dataframe    <- readdayflux(filename = "Dayflx.dat")
   watbalday.dataframe <- readwatbal(filename = "watbalday.dat")
   
   # moles of Carbon to kilograms
@@ -52,7 +50,7 @@ model2netcdf.MAESPA <- function(outdir, sitelat, sitelon, start_date, end_date, 
     }
     print(paste("---- Processing year: ", y))  # turn on for debugging
     
-    ## Setup outputs for netCDF file in appropriate units
+    ## Set up outputs for netCDF file in appropriate units
     output <- list()
     output[[1]] <- (dayflx.dataframe$totPs) * mole2kg_C * stem_density  # (GPP) gross photosynthesis. mol tree-1 d-1 -> kg C m-2 s-1
     output[[2]] <- (dayflx.dataframe$netPs) * mole2kg_C * stem_density  # (NPP) photosynthesis net of foliar resp mol tree-1 d-1 -> kg C m-2 s-1
@@ -85,63 +83,8 @@ model2netcdf.MAESPA <- function(outdir, sitelat, sitelon, start_date, end_date, 
     var[[4]] <- ncvar_def("Qh", units = ("W m-2"), dim = list(lat, lon, t), missval = -999, longname = "Sensible Heat Flux")
     var[[5]] <- ncvar_def("Qle", units = ("W m-2"), dim = list(lat, lon, t), missval = -999, longname = "latent Heat Flux")
     
-    dayflx.dataframe <- readdayflux(filename = "Dayflx.dat")
-    
-    watbalday.dataframe <- readwatbal(filename = "watbalday.dat")
-    
-    # moles of Carbon to kilograms
-    mole2kg_C <- 0.0120107
-    # Seconds in a day
-    secINday <- 60 * 60 * 24
-    
-    ### Determine number of years and output timestep
-    start_date <- as.POSIXlt(start_date, tz = "UTC")
-    end_date <- as.POSIXlt(end_date, tz = "UTC")
-    start_year <- year(start_date)
-    end_year <- year(end_date)
-    years <- start_year:end_year
-    
-    
-    for (y in years) {
-        if (file.exists(file.path(outdir, paste(y, "nc", sep = ".")))) {
-            next
-        }
-        print(paste("---- Processing year: ", y))  # turn on for debugging
-        
-        
-        ## Setup outputs for netCDF file in appropriate units
-        output <- list()
-        output[[1]] <- (dayflx.dataframe$totPs) * mole2kg_C * stem_density  #(GPP) gross photosynthesis. mol tree-1 d-1 -> kg C m-2 s-1
-        output[[2]] <- (dayflx.dataframe$netPs) * mole2kg_C * stem_density  #(NPP) photosynthesis net of foliar resp mol tree-1 d-1 -> kg C m-2 s-1
-        output[[3]] <- (watbalday.dataframe$et)/secINday  #(Tveg) modeled canopy transpiration   mm -> kg m-2 s-1
-        output[[4]] <- (watbalday.dataframe$qh) * 1e+06  #(Qh) Sensible heat flux MJ m-2 day-1 -> W m-2
-        output[[5]] <- (watbalday.dataframe$qe) * 1e+06  #(Qle)latent Heat flux MJ m-2 day-1 -> W m-2
-        
-        # ******************** Declare netCDF variables ********************#
-        t <- ncdim_def(name = "time", units = paste0("days since ", y, "-01-01 00:00:00"), vals = (dayflx.dataframe$DOY), 
-            calendar = "standard", unlim = TRUE)
-        lat <- ncdim_def("lat", "degrees_east", vals = as.numeric(sitelat), longname = "station_latitude")
-        lon <- ncdim_def("lon", "degrees_north", vals = as.numeric(sitelon), longname = "station_longitude")
-        
-        for (i in seq_along(output)) {
-            if (length(output[[i]]) == 0) 
-                output[[i]] <- rep(-999, length(t$vals))
-        }
-        
-        mstmipvar <- PEcAn.utils::mstmipvar
-        var <- list()
-        var[[1]] <- ncvar_def("GPP", units = ("kg C m-2 s-1"), dim = list(lat, lon, t), missval = -999, 
-            longname = "Gross Primary Production")
-        var[[2]] <- ncvar_def("NPP", units = ("kg C m-2 s-1"), dim = list(lat, lon, t), missval = -999, 
-            longname = " Net Primary Production")
-        var[[3]] <- ncvar_def("TVeg", units = ("kg m-2 s-1"), dim = list(lat, lon, t), missval = -999, 
-            longname = "EvapoTranpiration")
-        var[[4]] <- ncvar_def("Qh", units = ("W m-2"), dim = list(lat, lon, t), missval = -999, longname = "Sensible Heat Flux")
-        var[[5]] <- ncvar_def("Qle", units = ("W m-2"), dim = list(lat, lon, t), missval = -999, longname = "latent Heat Flux")
-        
-        
     ### Output netCDF data
-    nc <- nc_create(file.path(outdir, paste(y, "nc", sep = ".")), var)
+    nc      <- nc_create(file.path(outdir, paste(y, "nc", sep = ".")), var)
     varfile <- file(file.path(outdir, paste(y, "nc", "var", sep = ".")), "w")
     for (i in seq_along(var)) {
       # print(i)
