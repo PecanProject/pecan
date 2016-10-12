c398f0481d4b886d81778a6691aa2601bec55a4d
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2016-10-12 14:13:59 -0400

    Syntax fix

--- modules/assim.batch/R/get.da.data.R
+++ modules/assim.batch/R/get.da.data.R
@@ -116,8 +116,8 @@ get.da.data <- function(out.dir, ameriflux.dir, years, be, bu, ensemble.size=199
   dtime <- do.call(c, lapply(years,
           function(year) {
             nodays <- 365 + lubridate::leap_year(year)
-            year + seq(1, nodays, by = 1 / points.per.day)[-1] / nodays))
-          }
+            year + seq(1, nodays, by = 1 / points.per.day)[-1] / nodays
+          }))
   
   #run.ids<-ensemble.run.ids
   #x <- ensemble.x
