14d1b13893e5dded38f0e1f1285a7cad410fd18a
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2016-10-13 07:38:31 -0400

    Style cleanup for `modules/emulator/R/*.R` scripts
    Ran through formatR::tidy_dir with arrow=TRUE and indent=2; made sure
    all if blocks have braces. Tried to make sure formatR didn’t make
    things too jaggy. Substituted `seq_len`, `seq_along`, and `paste0`, and
    `file.path` as necessary.

--- modules/emulator/R/calcSpatialCov.list.R
+++ modules/emulator/R/calcSpatialCov.list.R
@@ -11,25 +11,18 @@
 ##' @param tau spatial var
 ##' 
 ##' @author Michael Dietze
-`calcSpatialCov.list` <-
-function(d,psi,tau){
-  m <- length(d)
+calcSpatialCov.list <- function(d, psi, tau) {
+  m  <- length(d)
   nl <- nrow(d[[1]])
-  H <- matrix(0,nl,nl)
-  if(length(psi) == 1 && m > 1){
-    ##apply same psi to all directions
-    psi <- rep(psi,m)
+  H  <- matrix(0, nl, nl)
+  if (length(psi) == 1 && m > 1) {
+    ## apply same psi to all directions
+    psi <- rep(psi, m)
   }
-  #for(i in 1:nl){
-  #  for(j in i:nl){
-  #    tmp <- 0
-  #    for(k in 1:m){tmp <- tmp - psi[k]*d[[k]][i,j]}
-  #    H[i,j] <- tau*exp(tmp)
-  #    H[j,i] <- H[i,j]
-  #  }
-  #}
-  for(k in 1:m) H <- H - psi[k]*d[[k]]
-  H <- tau*exp(H)
-  return(H)
-}
-
+  # for(i in 1:nl){ for(j in i:nl){ tmp <- 0 for(k in 1:m){tmp <- tmp -
+  # psi[k]*d[[k]][i,j]} H[i,j] <- tau*exp(tmp) H[j,i] <- H[i,j] } }
+  for (k in seq_len(m)) {
+    H <- H - psi[k] * d[[k]]
+  }
+  tau * exp(H)
+} # calcSpatialCov.list
