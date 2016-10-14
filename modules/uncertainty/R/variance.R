ff43300244b772d2af0364f916faac661760115e
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2016-10-13 21:25:55 -0400

    Style cleanup for `modules/uncertainty/R/*.R` scripts
    Ran through formatR::tidy_dir with arrow=TRUE and indent=2; made sure
    all if blocks have braces. Tried to make sure formatR didn’t make
    things too jaggy. Substituted `seq_len`, `seq_along`, and `paste0`, and
    `file.path` as necessary.

--- modules/uncertainty/R/variance.R
+++ modules/uncertainty/R/variance.R
@@ -6,6 +6,7 @@
 # which accompanies this distribution, and is available at
 # http://opensource.ncsa.illinois.edu/license.html
 #-------------------------------------------------------------------------------
+
 ##' Variance and SD(variance)
 ##'
 ##' calculates variance and sd of variance using the \code{var} from base R and \code{\link{sd.var}} from PEcAn.
@@ -13,9 +14,10 @@
 ##' @param x numeric vector
 ##' @return list with variance and sd of variance
 ##' @author David LeBauer
-variance.stats <- function(x){
+variance.stats <- function(x) {
   list(var = var(x), sd = sd.var(x))
-}
+} # variance.stats
+
 
 ##' Calculate distribution of function of a variable
 ##'
@@ -27,31 +29,32 @@ variance.stats <- function(x){
 ##' @param maxn maximum number of parameter sets to evaluate
 ##' @return matrix of spline estimates of model output for each of n parameter sets 
 ##' @author David LeBauer
-get.gi.phii <- function(splinefuns, trait.samples, maxn = NULL){
+get.gi.phii <- function(splinefuns, trait.samples, maxn = NULL) {
   ## check inputs
-  if(class(trait.samples) == 'list'){
-    trait.samples <- matrix(unlist(trait.samples), 
-                            ncol = length(names(trait.samples)))
+  if (class(trait.samples) == "list") {
+    trait.samples <- matrix(unlist(trait.samples), ncol = length(names(trait.samples)))
     colnames(trait.samples) <- names(splinefuns)
-    if(!is.null(maxn) & maxn < nrow(trait.samples)){
-      j <- sample(1:nrow(trait.samples), maxn)
+    if (!is.null(maxn) & maxn < nrow(trait.samples)) {
+      j <- sample(seq_len(nrow(trait.samples), maxn))
       trait.samples <- trait.samples[j, ]
     }
   }
-  if(class(trait.samples) != 'matrix'){
-    stop(paste('variance.decomposition currently does not handle trait.samples of class', class(trait.samples), '\n please convert to list or matrix'))
+  if (class(trait.samples) != "matrix") {
+    stop(paste("variance.decomposition currently does not handle trait.samples of class", 
+               class(trait.samples), 
+               "\n please convert to list or matrix"))
   }
-  if(!all(names(splinefuns) %in% colnames(trait.samples))){
-    stop('mismatch between splinefuns and samples')
+  if (!all(names(splinefuns) %in% colnames(trait.samples))) {
+    stop("mismatch between splinefuns and samples")
   }
   traits <- names(splinefuns)
-     
+  
   ## g_i(phi_i) the spline estimate of model output for value of trait i
-  gi.phii <- t(laply(traits, 
-                    function(x) splinefuns[[x]](trait.samples[,x])))
+  gi.phii <- t(laply(traits, function(x) splinefuns[[x]](trait.samples[, x])))
   colnames(gi.phii) <- traits
   return(gi.phii)
-}
+} # get.gi.phii
+
 
 ##' Estimate model output based on univariate splines
 ##'
@@ -63,21 +66,19 @@ get.gi.phii <- function(splinefuns, trait.samples, maxn = NULL){
 ##' @param gi.phii matrix given as output from \code{\link{get.gi.phii}}
 ##' @param median median value around which variance will be calculated
 ##' @author David LeBauer
-spline.ensemble <- function(gi.phii, median){
+spline.ensemble <- function(gi.phii, median) {
   ## Calculate ensemble output for each parameter set (each row of trait.samples)
-  ## Equation 3
-  ##  1. calculate residuals  (g_i(phi_i,j) - g_i(median))
+  ## Equation 3 1. calculate residuals (g_i(phi_i,j) - g_i(median))
   residuals <- gi.phii - median
   ## 2. sum residuals by row, then truncate at 0:
-  spline.estimate <- sapply(rowSums(residuals),
-                            function(x) max(0, x + median))
+  spline.estimate <- sapply(rowSums(residuals), function(x) max(0, x + median))
   return(spline.estimate)
-}
+} # spline.ensemble
 
-vd.variance <- function(gi.phii){
+
+vd.variance <- function(gi.phii) {
   ## Calculate variance for each trait
-  var.phii    <- apply(gi.phii, 2, var)
+  var.phii <- apply(gi.phii, 2, var)
   sd.var.phii <- apply(gi.phii, 2, sd.var)
-  return(list(var = sum(var.phii),
-         sd  = sqrt(sum(sd.var.phii^2))))
-}
+  return(list(var = sum(var.phii), sd = sqrt(sum(sd.var.phii ^ 2))))
+} # vd.variance
