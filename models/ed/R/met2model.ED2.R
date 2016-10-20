1829d5112151dbf4069f21e7da3e2e59eb823e5d
Shawn P. Serbin <sserbin@bnl.gov>
2016-10-20 08:55:36 -0400

    Whoops, I didn't notice the defs at the beginngin.  Fixed

--- models/ed/R/met2model.ED2.R
+++ models/ed/R/met2model.ED2.R
@@ -88,9 +88,6 @@ met2model.ED2 <- function(in.path, in.prefix, outfolder, start_date, end_date, l
     ## open netcdf
     nc <- ncdf4::nc_open(ncfile)
     
-    # Import ncvar_get
-    ncvar_get <- ncdf4::ncvar_get    
-    
     # check lat/lon
     flat <- try(ncvar_get(nc, "latitude"), silent = TRUE)
     if (!is.numeric(flat)) {
