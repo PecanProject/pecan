a71a56bb31a1ea7b746f3793e92d2349452f1552
Betsy Cowdery <emcowdery@gmail.com>
2016-10-26 14:03:26 -0400

    create.BRR now called from within read.settings.RR assuming that the xml settings file may ony contain ensemble ids (and not BRR ids)

--- modules/benchmark/R/read.settings.RR.R
+++ modules/benchmark/R/read.settings.RR.R
@@ -25,10 +25,37 @@ read.settings.RR <- function(settings){
                        user     = settings$database$bety$user,
                        password = settings$database$bety$password)
   
-  settings <- tbl(bety,"reference_runs") %>% 
-    filter(id == settings$benchmark$reference_run_id) %>% 
-      dplyr::select(settings) %>% collect() %>% unlist() %>%
-      xmlToList(.,"pecan") %>% append(settings,.) %>% Settings()
+  if(!is.null(settings$benchmark$reference_run_id)){
+    BRR <- tbl(bety,"reference_runs") %>% filter(id == settings$benchmark$reference_run_id)
+  }else{
+    if(!is.null(settings$benchmark$ensemble_id)){
+      
+      # check if there is already a BRR for ensemble.id, otherwise make one
+      bm_ens <- tbl(bety,"benchmarks_ensembles") %>% rename(bm_ensemble_id = id) %>% 
+        filter(ensemble_id == settings$benchmark$ensemble_id) %>% collect()
+      
+      if(length(bm_ens) == 0){
+        # Get workflow id from ensemble id
+        ens_wf <- tbl(bety, 'ensembles') %>% filter(id == settings$benchmark$ensemble_id) %>% 
+          rename(ensemble_id = id) %>% 
+          left_join(.,tbl(bety, "workflows") %>% rename(workflow_id = id), by="workflow_id") %>% collect()
+        BRR <- create.BRR(ens_wf, con = bety$con)
+      }else if(dim(bm_ens)[1] == 1){
+        BRR <- tbl(bety,"reference_runs") %>% filter(id == bm_ens$reference_run_id) %>% 
+          rename(reference_run_id = id) %>% collect()
+      }else if(dim(bm_ens)[1] > 1){ # There shouldn't be more than one reference run per run
+        logger.error("There is more than one reference run in the database for this ensemble id. Review for duplicates. ")}
+      # add the ref_run id, remove the ensemble_id
+      settings$benchmark$reference_run_id <- BRR$reference_run_id
+      
+      # won't need this ensemble_id anymore, because the new run will make a new ensemble_id
+      if(as.logical(settings$benchmark$new_run)){settings$benchmark$ensemble_id <- NULL} 
+      
+    }else{logger.error("Cannot find or create BRR")}
+  } 
+
+  settings <- BRR %>% dplyr::select(settings) %>% collect() %>% unlist() %>%
+    xmlToList(.,"pecan") %>% append(settings,.) %>% Settings()
   invisible(settings)
 }
 
