ebd9dc06e7309345e9da32d7fdf3c2653afd534b
tonygardella <tonygard@bu.edu>
2016-10-31 16:16:18 -0400

    initia changes to met

--- modules/data.atmosphere/R/met2cf.module.R
+++ modules/data.atmosphere/R/met2cf.module.R
@@ -1,5 +1,5 @@
 .met2cf.module <- function(raw.id, register, met, dir, machine, site.id, lat, lon, start_date, end_date, 
-                           con, host, overwrite = FALSE, format.vars) {
+                           str_ns, con, host, overwrite = FALSE, format.vars) {
   logger.info("Begin change to CF Standards")
   
   input.id   <- raw.id$input.id[1]
