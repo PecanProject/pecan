667861e09ada01b2eb1a9fad950fca3cb49c7bf1
Alexey Shiklomanov <ashiklom@bu.edu>
2016-11-01 16:40:53 -0400

    PEcAnRTM: Scale likelihood by eff. sample size
    Also, minor optimization of `invert.custom` by using Rcpp implementation
    of `dnorm`.

---
+++ modules/rtm/R/neff.R
@@ -0,0 +1,23 @@
+#' @title Effective sample size
+#'
+#' @description Calculate effective sample size of vector based on its 
+#' autocorrelation.
+#' @param x A vector or time series
+#' @export
+neff <- function(x) {
+  UseMethod("neff")
+}
+
+neff.default <- function(x) {
+    arout <- ar.yw(x, na.action = na.omit)
+    spec <- arout$var.pred/(1 - sum(arout$ar))^2
+    out <- length(x) * var(x) / spec
+    stopifnot(length(out) == 1)
+    return(out)
+}
+
+neff.matrix <- function(x) {
+  col_neff <- apply(x, 2, neff.default)
+  return(mean(col_neff))
+}
+
