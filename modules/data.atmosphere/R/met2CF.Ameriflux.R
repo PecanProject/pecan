8ad562a5b056c28bf9bd603dd0876dc107751eda
Mike Dietze <dietze@bu.edu>
2016-11-19 22:45:51 -0500

    turns out geonames library needs to be loaded to initialize correctly

--- modules/data.atmosphere/R/met2CF.Ameriflux.R
+++ modules/data.atmosphere/R/met2CF.Ameriflux.R
@@ -80,6 +80,7 @@ met2CF.Ameriflux <- function(in.path, in.prefix, outfolder, start_date, end_date
   
   #---------------- Load libraries. -----------------------------------------------------------------#
   library(PEcAn.utils)
+  library(geonames)  ## has to be loaded as a library
   #--------------------------------------------------------------------------------------------------#  
   
   ncvar_get <- ncdf4::ncvar_get
@@ -140,7 +141,7 @@ met2CF.Ameriflux <- function(in.path, in.prefix, outfolder, start_date, end_date
       lst <- tdimunit[length(tdimunit)]  #already in definition, leave it alone
     } else {
       options(geonamesUsername = "carya")  #login to geoname server
-      lst <- geonames::GNtimezone(latlon[1], latlon[2], radius = 0)$gmtOffset
+      lst <- GNtimezone(latlon[1], latlon[2], radius = 0)$gmtOffset
       if (lst >= 0) {
         lststr <- paste("+", lst, sep = "")
       } else {
