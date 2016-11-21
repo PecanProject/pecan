6c8f25f65be5ca457a7a74836a84a4a01d20fcc6
jam2767 <jam2767@gmail.com>
2016-11-21 15:52:40 -0500

    First attempts at using BDFiddle to convert excel files into csv. Using example adult tree data file from Harvard forest.

--- utils/R/utils.R
+++ utils/R/utils.R
@@ -660,6 +660,32 @@ convert.expr <- function(expression) {
   return(list(variable.drv = deri.var, variable.eqn = list(variables = variables, expression = deri.eqn)))
 }
 
+## Convert a file using Brown Dog Conversion service
+##' @param bds: The URL to the Brown Dog Server to use
+##' @param input_filename: The input filename
+##' @param output: The output format extension
+##' @param output_path: The path for the created output file. May contain different filename
+##' @param token: Brown Dog access token
+##' @param wait: The amount of time to wait for the DAP service to respond. Default is 60
+##' @return: The output filename 
+
+# browndog.convert = function (bds, input_filename, output, output_path, token, wait=60, download=TRUE){
+#   convert_api <- paste0("https://", bds,"/dap/convert/", output, "/") 
+#   #convert_api <- paste0(bds,"/dap/convert/", output, "/")  # for localhost
+#   httpheader <- c(Accept="text/plain", Authorization = token)
+#   curloptions <- list(httpheader = httpheader)
+#   result_bds <- postForm(convert_api,"file"= fileUpload(input_filename),.opts = curloptions)
+#   url             <- gsub('.*<a.*>(.*)</a>.*', '\\1', result_bds)
+#   inputbasename   <- strsplit(basename(input_filename),'\\.')
+#   outputfile      <- paste0(output_path,inputbasename[[1]][1],".", output)
+#   if (download){
+#     output_filename <- browndog.download(url[1], outputfile, token, wait)
+#   }else{
+#     return(url[1]) 
+#   }
+#   return(output_filename)
+# }
+
 ####################################################################################################
 ### EOF.  End of R script file.              
 ####################################################################################################
