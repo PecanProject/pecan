320efd1816066b2ca17521ef136fe4eb2c85b913
Betsy Cowdery <emcowdery@gmail.com>
2016-11-28 04:27:01 -0500

    Updating met2CF to fix some errors - this function is not complient with the current version of met.process.

--- modules/data.atmosphere/R/met2CF.FACE.R
+++ modules/data.atmosphere/R/met2CF.FACE.R
@@ -8,7 +8,7 @@
 ##' @param convert FACE files to CF files
 ##' @author Elizabeth Cowdery
 ##' 
-met2CF.FACE <- function(in.path, in.prefix, outfolder, start_date, end_date, ...) {
+met2CF.FACE <- function(in.path, in.prefix, outfolder, start_date, end_date, bety, ...) {
   
   library(ncdf4.helpers)
   library(PEcAn.utils)
@@ -18,17 +18,22 @@ met2CF.FACE <- function(in.path, in.prefix, outfolder, start_date, end_date, ...
   ncatt_get <- ncdf4::ncatt_get
   ncvar_add <- ncdf4::ncvar_add
   ncvar_put <- ncdf4::ncvar_put
+  nc_open   <- ncdf4::nc_open
+  nc_create <- ncdf4::nc_create
+  
+  nc.get.variable.list <- ncdf4.helpers::nc.get.variable.list
   
   files <- dir(in.path, in.prefix)
   file <- files[grep(pattern = "*.nc", files)]
   if (!(length(file) == 1)) {
     return(NULL)
   }
-  f <- file.path(in.path, file)
+  f <- gsub("//","/",file.path(in.path, file))
   
   for (treatment in c("a", "e")) {
     
     t.outfolder <- paste(unlist(strsplit(outfolder, "FACE")), collapse = paste0("FACE_", treatment))
+    # t.outfolder <- paste0(outfolder,"_",treatment)
     if (!file.exists(t.outfolder)) {
       dir.create(t.outfolder)
     }
@@ -38,11 +43,11 @@ met2CF.FACE <- function(in.path, in.prefix, outfolder, start_date, end_date, ...
       
       # paste('ncks -x -v', paste0(rm.vars,collapse = ','), f.cf, f.cf)
       
-      # Change to CF variable names
+      #---------------------------------------------------------------------#
+      # Latitude and Longitude
       
-      nc1 <- ncdf4::nc_open(f, write = TRUE)
-      nc.vars <- nc.get.variable.list(nc1)
-      
+      nc1 <- nc_open(f, write = TRUE)
+
       lat <- ncdim_def(name = "latitude", units = "", vals = 1:1, create_dimvar = FALSE)
       lon <- ncdim_def(name = "longitude", units = "", vals = 1:1, create_dimvar = FALSE)
       time_units <- paste0("hours/2", unlist(strsplit(nc1$var$TIMEstp$units, "timesteps"))[2])
@@ -50,14 +55,15 @@ met2CF.FACE <- function(in.path, in.prefix, outfolder, start_date, end_date, ...
                         create_dimvar = TRUE, unlim = TRUE)
       dim <- list(lat, lon, time)
       
-      var <- ncvar_def(name = "latitude", units = "degree_north", dim = list(lat, lon), missval = as.numeric(-9999))
-      
-      nc2 <- ncdf4::nc_create(filename = f.cf, vars = var, verbose = TRUE)
-      
+      # copy lat attribute to latitude
+      var <- ncvar_def(name = "latitude", units = "degree_north", 
+                       dim = list(lat, lon), missval = as.numeric(-9999))
+      nc2 <- nc_create(filename = f.cf, vars = var, verbose = TRUE)
       ncvar_put(nc = nc2, varid = "latitude", vals = ncvar_get(nc1, "nav_lat"))
       
       # copy lon attribute to longitude
-      var <- ncvar_def(name = "longitude", units = "degree_east", dim = list(lat, lon), missval = as.numeric(-9999))
+      var <- ncvar_def(name = "longitude", units = "degree_east", 
+                       dim = list(lat, lon), missval = as.numeric(-9999))
       nc2 <- ncvar_add(nc = nc2, v = var, verbose = TRUE)
       ncvar_put(nc = nc2, varid = "longitude", vals = ncvar_get(nc1, "nav_lon"))
       
@@ -75,56 +81,75 @@ met2CF.FACE <- function(in.path, in.prefix, outfolder, start_date, end_date, ...
       nc2 <- ncvar_add(nc = nc2, v = var, verbose = FALSE)
       ncvar_put(nc = nc2, varid = "northward_wind", vals = nw)
       
-      # # remove the unwanted treatment 
-      # if(treatment == 'a'){rm.vars <- c('eCO2', 'eO3') }
-      # else if(treatment == 'e'){rm.vars <- c('aCO2', 'aO3') 
-      # }else{logger.error('Need a CO2 levels treatment')} 
-      # vars <- setdiff(vars_all, rm.vars)
+      #---------------------------------------------------------------------#
+      # Loop through variables and convert 
       
-      # convert CO2 to mole_fraction_of_carbon_dioxide_in_air
-      copyvals(nc1 = nc1, var1 = paste0(treatment, "CO2"), nc2 = nc2, 
-               var2 = "mole_fraction_of_carbon_dioxide_in_air", units2 = "mole/mole", 
-               dim2 = dim, conv = function(x) { x * 1e+06 },
-               verbose = verbose)
+      localhost <- fqdn()
+      machineid <- dplyr::tbl(bety, "machines") %>% 
+        filter(hostname == localhost) %>% 
+        dplyr::select(id) %>% collect() %>% .[[1]]
+      input.id <- dplyr::tbl(bety, "dbfiles") %>% filter(file_path == in.path) %>% filter(machine_id == machineid) %>% select(container_id) %>% collect() %>% .[[1]]
+      format.id <- dplyr::tbl(bety, "inputs") %>% filter(id == input.id) %>% select(format_id) %>% collect() %>% .[[1]]
       
-      # deal with the rest of the variables
+      format <- query.format.vars(bety,input.id, format.id)
       
-      vars <- c("Rainf", "Tair", "RH", "VPD", "Qair", "Wind", "SWdown", "PAR", "LWdown", "Psurf", 
-                paste0(treatment, "O3"), "SolarElevation")
+      vars.used.index.all <- setdiff(seq_along(format$vars$variable_id), format$time.row)
+      nt <- setdiff(c("a","e"), treatment) 
+      exclude.treatment <- paste0(nt,c("CO2","O3"))
+      vars.used.index <- vars.used.index.all[!(format$vars$input_name[vars.used.index.all] %in% exclude.treatment)]
       
-      nvars <- c("precipitation_flux", "air_temperature", "relative_humidity", "water_vapor_saturation_deficit", 
-                 "specific_humidity", "wind_speed", "surface_downwelling_shortwave_flux_in_air", 
-                 "surface_downwelling_photosynthetic_radiative_flux_in_air", 
-                 "surface_downwelling_longwave_flux_in_air", "air_pressure", 
-                 "mass_concentration_of_ozone_in_air", 
-                 "solar_elevation_angle")
-      
-      if (!(length(nvars) == length(vars))) {
-        logger.error("Variable mismatch")
-      }
-      
-      l <- length(vars)
-      for (k in seq_len(l)) {
-        if (vars[k] %in% nc.vars) {
-          # nc <- tncar_rename(nc,vars[k],nvars[k])
-          
-          vals <- ncvar_get(nc1, vars[k])
-          
-          units <- ncatt_get(nc1, varid = vars[k], attname = "units", verbose = FALSE)$value
-          
-          var <- ncvar_def(name = nvars[k], units = units, dim = dim, verbose = FALSE)
-          nc2 <- ncvar_add(nc = nc2, v = var, verbose = TRUE)
-          ncvar_put(nc = nc2, varid = nvars[k], vals = vals)
-          
-          att <- ncatt_get(nc1, vars[k], "long_name")
-          if (att$hasatt) {
-            val <- att$value
-            ncatt_put(nc = nc2, varid = nvars[k], attname = "long_name", attval = val)
-          }
+      derp <- grep(paste0(treatment,"CO2"), format$vars$input_name[vars.used.index])
+      if(length(derp) >1){
+        for(i in 2:length(derp)){
+          vars.used.index <- vars.used.index[-derp[i]]
         }
       }
+      derp <- grep(paste0(treatment,"O3"), format$vars$input_name[vars.used.index])
+      if(length(derp) >1){
+        for(i in 2:length(derp)){
+          vars.used.index <- vars.used.index[-derp[i]]
+        }
+      }
+      vars_used <- format$vars[vars.used.index, ]
       
-      ncdf4::nc_close(nc2)
+      # begin loop
+      for (i in seq_len(nrow(vars_used))) {
+        vals <- ncvar_get(nc1, vars_used$input_name[i])
+        
+        if (vars_used$input_units[i] == vars_used$pecan_units[i]) {
+          print("match")
+        } else {
+          u1 <- vars_used$input_units[i]
+          u2 <- vars_used$pecan_units[i]
+          if (udunits2::ud.are.convertible(u1, u2)) {
+            print(sprintf("convert %s %s to %s %s",
+                          vars_used$input_name[i], vars_used$input_units[i], 
+                          vars_used$pecan_name[i], vars_used$pecan_units[i]))
+            vals <- udunits2::ud.convert(vals, u1, u2)
+          } else if (misc.are.convertible(u1, u2)) {
+            print(sprintf("convert %s %s to %s %s", 
+                          vars_used$input_name[i], u1, 
+                          vars_used$pecan_name[i], u2))
+            vals <- misc.convert(x, u1, u2)
+          } else {
+            PEcAn.utils::logger.error("Units cannot be converted")
+          } 
+        }
+        
+        var <- ncvar_def(name = vars_used$pecan_name[i], 
+                         units = vars_used$pecan_units[i], 
+                         dim = dim, verbose = FALSE)
+        nc2 <- ncvar_add(nc = nc2, v = var, verbose = TRUE)
+        ncvar_put(nc = nc2, varid = vars_used$pecan_name[i], vals = vals)
+        
+        att <- ncatt_get(nc1,vars_used$input_name[i], "long_name")
+        if (att$hasatt) {
+          val <- att$value
+          ncatt_put(nc = nc2, varid = vars_used$pecan_name[i], attname = "long_name", attval = val)
+        }
+      }
+      nc_close(nc2)
+    
       
       # Split into annual files
       
@@ -133,7 +158,7 @@ met2CF.FACE <- function(in.path, in.prefix, outfolder, start_date, end_date, ...
       n <- length(y)
       t <- -1
       for (j in seq_len(n)) {
-        new.file <- paste0(t.outfolder, in.prefix, ".", y[j], ".nc")
+        new.file <- file.path(t.outfolder, paste(in.prefix, y[j],"nc", sep ="."))
         if (!file.exists(new.file)) {
           s <- t + 1
           print(s)
@@ -146,11 +171,61 @@ met2CF.FACE <- function(in.path, in.prefix, outfolder, start_date, end_date, ...
         t <- e
       }
       print(paste("Treatment ", treatment, " done"))
-      ncdf4::nc_close(nc2)
-      
+
     } else {
       print(paste("Treatment ", treatment, " aleady done"))
     }  # end make new file
     
   }  # end loop over treatments
 } # met2CF.FACE
+
+
+
+
+
+# #####################################################################
+# # HOW I PREVIOUSLY DID CONVERSIONS (I think it contains errors)
+# # convert CO2 to mole_fraction_of_carbon_dioxide_in_air
+# copyvals(nc1 = nc1, var1 = paste0(treatment, "CO2"), nc2 = nc2, 
+#          var2 = "mole_fraction_of_carbon_dioxide_in_air", units2 = "mole/mole", 
+#          dim2 = dim, conv = function(x) { x * 1e+06 },
+#          verbose = verbose)
+# 
+# # deal with the rest of the variables
+# 
+# vars <- c("Rainf", "Tair", "RH", "VPD", "Qair", "Wind", "SWdown", "PAR", "LWdown", "Psurf", 
+#           paste0(treatment, "O3"), "SolarElevation")
+# 
+# nvars <- c("precipitation_flux", "air_temperature", "relative_humidity", "water_vapor_saturation_deficit", 
+#            "specific_humidity", "wind_speed", "surface_downwelling_shortwave_flux_in_air", 
+#            "surface_downwelling_photosynthetic_radiative_flux_in_air", 
+#            "surface_downwelling_longwave_flux_in_air", "air_pressure", 
+#            "mass_concentration_of_ozone_in_air", 
+#            "solar_elevation_angle")
+# 
+# if (!(length(nvars) == length(vars))) {
+#   logger.error("Variable mismatch")
+# }
+# 
+# l <- length(vars)
+# for (k in seq_len(l)) {
+#   if (vars[k] %in% nc.vars) {
+#     # nc <- tncar_rename(nc,vars[k],nvars[k])
+#     
+#     vals <- ncvar_get(nc1, vars[k])
+#     
+#     units <- ncatt_get(nc1, varid = vars[k], attname = "units", verbose = FALSE)$value
+#     
+#     var <- ncvar_def(name = nvars[k], units = units, dim = dim, verbose = FALSE)
+#     nc2 <- ncvar_add(nc = nc2, v = var, verbose = TRUE)
+#     ncvar_put(nc = nc2, varid = nvars[k], vals = vals)
+#     
+#     att <- ncatt_get(nc1, vars[k], "long_name")
+#     if (att$hasatt) {
+#       val <- att$value
+#       ncatt_put(nc = nc2, varid = nvars[k], attname = "long_name", attval = val)
+#     }
+#   }
+# }
+# 
+# ncdf4::nc_close(nc2)
