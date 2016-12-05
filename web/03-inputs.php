b3087db2b2b98d0367f8c4116584c1d376676390
Rob Kooper <kooper@illinois.edu>
2016-12-05 12:48:50 -0600

    update web folder

--- web/03-inputs.php
+++ web/03-inputs.php
@@ -199,7 +199,7 @@ $stmt->closeCursor();
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 <link rel="stylesheet" type="text/css" href="sites.css" />
-<script type="text/javascript" src="jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="jquery-1.10.2.min.js"></script>
 <?php if (!$offline) {?>
 <script type="text/javascript" src="//www.google.com/jsapi"></script>
 <?php }?>
