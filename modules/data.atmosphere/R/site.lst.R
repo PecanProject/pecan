0169a60b21e3663e70a2eb848344e08d1c3d3086
tonygardella <tonygard@bu.edu>
2016-12-09 13:54:17 -0600

    change as.numeric to as.character

--- modules/data.atmosphere/R/site.lst.R
+++ modules/data.atmosphere/R/site.lst.R
@@ -10,8 +10,9 @@ site.lst <- function(site.id, con) {
   library(geonames)
   
   time.zone <- db.query(paste("SELECT time_zone from SITES where id =", site.id), con)
-  if (!is.na(time.zone) && !is.na(as.numeric(time.zone))) {
-    lst <- as.numeric(time.zone)
+  
+  if (!is.na(time.zone) && !is.na(as.character(time.zone))) {
+    lst <- as.character(time.zone)
   } else {
     site <- db.query(paste("SELECT ST_X(ST_CENTROID(geometry)) AS lon, ST_Y(ST_CENTROID(geometry)) AS lat", 
       "FROM sites WHERE id =", site.id), con)
