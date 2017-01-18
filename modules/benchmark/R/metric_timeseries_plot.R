63608e0e44a4996ab6149c8079997afcf5f1600e
Betsy Cowdery <emcowdery@gmail.edu>
2017-01-18 17:24:09 -0500

    Cleanup

--- modules/benchmark/R/metric_timeseries_plot.R
+++ modules/benchmark/R/metric_timeseries_plot.R
@@ -3,7 +3,7 @@
 ##' @export
 ##' @param metric_dat dataframe
 ##' @param var character
-##' 
+##' @importFrom ggplot2 ggplot labs geom_path geom_point
 ##' @author Betsy Cowdery
 
 metric_timeseries_plot <- function(metric_dat, var, filename = NA, draw.plot = is.na(filename)) {
@@ -17,12 +17,12 @@ metric_timeseries_plot <- function(metric_dat, var, filename = NA, draw.plot = i
     metric_dat$time <- date.time
   }
   
-  p <- ggplot2::ggplot(data = metric_dat, aes(x = time)) 
-  p <- p + ggplot2::labs(title = var, y = "") 
-  p <- p + ggplot2::geom_path(aes(y = model, colour = "Model"), size = 2) 
-  p <- p + ggplot2::geom_point(aes(y = model, colour = "Model"), size = 4) 
-  p <- p + ggplot2::geom_path(aes(y = obvs, colour = "Observed"), size = 2) 
-  p <- p + ggplot2::geom_point(aes(y = obvs, colour = "Observed"), size = 4)
+  p <- ggplot(data = metric_dat, aes(x = time)) 
+  p <- p + labs(title = var, y = "") 
+  p <- p + geom_path(aes(y = model, colour = "Model"), size = 2) 
+  p <- p + geom_point(aes(y = model, colour = "Model"), size = 4) 
+  p <- p + geom_path(aes(y = obvs, colour = "Observed"), size = 2) 
+  p <- p + geom_point(aes(y = obvs, colour = "Observed"), size = 4)
   
   if (!is.na(filename)) {
     pdf(filename, width = 10, height = 6)
