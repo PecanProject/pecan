1602819e360cec358aaf0bae129adec663b53403
Tempest Mccabe <tmccabe@pecan-test.bu.edu>
2017-01-23 13:14:28 -0500

    commit after got the benchmarking code to run, but can't find the output in a file

--- modules/benchmark/inst/scripts/benchmark.workflow.R
+++ modules/benchmark/inst/scripts/benchmark.workflow.R
@@ -1,40 +1,10 @@
-##' Benchmarking workflow
-##' Only use this if using and existing run (ie settings$new_run == FALSE)
-##' Author: Betsy Cowdery
+settings <- read.settings("modules/benchmark/inst/scripts/bm.1var.1metric.1site.1model.xml")
 
-## ----- setup: these are not necessary but make testing easier ----- ##
-rm(list = setdiff(ls(), lsf.str()))  # clear environment except for sourced functions
-# rm(list= ls()[!(ls() %in% c('objects you want to save'))] # clear environment except for ...
-for (i in dbListConnections(PostgreSQL())) db.close(i) #close any stray database connections
-options(digits = 10) # just to make things easier to read
-## ----- setup: these are not necessary but make testing easier ----- ##
+d <- settings$database$bety[c("dbname", "password", "host", "user")]
+bety <- src_postgres(host = d$host, user = d$user, password = d$password, dbname = d$dbname)
+settings$host$name <- "localhost"
 
-library(PEcAn.all)
-
-bety <- betyConnect("web/config.php")
-
-# Start with a benchmarking settings xml file that would be created though the interface 
-
-# ----- Pick a settings file -----#
-# WORKING: 1 variable, 1 metric, 1 site, 1 model 
-settings.file <- "modules/benchmark/inst/scripts/bm.1var.1metric.1site.1model.xml"
-
-# Test all the metrics
-# settings.file <- "modules/benchmark/inst/scripts/bm.1var.ALLmetric.1site.1model.xml"
-
-
-# WORKING: 2 variables, 2 metric, 1 site, 1 model 
-# settings.file <- "modules/benchmark/inst/scripts/bm.2var.2metric.1site.1model.xml"
-
-# NOT WORKING: 2 variables, 2 metric, 2 site, 1 model (multisettings object)
-# settings.file <- "modules/benchmark/inst/scripts/bm.2var.2metric.2site.1model.xml"
-
-# Ryan's multisite as an example
-# settings.file <- "/fs/data2/rykelly/mandifore/testruns/028_mandSE_test_08_n2/pecan.xml"
-# --------------------------------#
-
-settings <- read.settings(settings.file)
-bm.settings <- define_benchmark(settings$benchmarking,bety)
+bm.settings <- define_benchmark(settings,bety)
 
 # For testing (make sure new_run is FALSE)
 str(bm.settings)
@@ -47,7 +17,7 @@ add_workflow_info <- function(settings){
   if(!as.logical(settings$benchmarking$new_run)){
     settings$workflow$id <- tbl(bety,"ensembles") %>% 
       filter(id == settings$benchmarking$ensemble_id) %>% 
-      select(workflow_id) %>% collect %>% .[[1]]
+      dplyr::select(workflow_id) %>% collect %>% .[[1]]
     wf <- tbl(bety, 'workflows') %>% filter(id == settings$workflow$id) %>% collect()
     settings$rundir <- file.path(wf$folder, "run")
     settings$modeloutdir <- file.path(wf$folder, "out")
@@ -62,11 +32,20 @@ bm_settings2pecan_settings <- function(bm.settings){
   if (is.MultiSettings(bm.settings)) {
     return(papply(bm.settings, bm_settings2pecan_settings))
   }
-  return(append(bm.settings["reference_run_id"],
-                bm.settings$benchmark[which(names(bm.settings$benchmark) == "benchmark_id")]))
+  out <- bm.settings["reference_run_id"]
+  for(i in grep("benchmark", names(bm.settings))){
+    print(bm.settings[i]$benchmark$benchmark_id)
+    out <- append(out, list(benchmark_id = bm.settings[i]$benchmark$benchmark_id))
+  }
+  return(out)
 } 
 
 settings$benchmarking <- bm_settings2pecan_settings(bm.settings)
+
+############################################################################################################
+
+sprintf("MODEL: %s", settings$model$type)
+
 
 if(bm.settings$new_run){
   write.settings(settings,pecan.xml,outputdir = settings$outdir)
@@ -80,7 +59,7 @@ if(bm.settings$new_run){
   settings <- prepare.settings(settings)
   results <- papply(settings, function(x) calc_benchmark(x, bety))
 }
-  
+
 # 
 # # This may just be for testing or something that will ultimately be used with Shiny
 # rmarkdown::render(system.file("scripts/Benchmarking.Report.Rmd", package = "PEcAn.benchmark"), 
