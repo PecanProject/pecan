20a2b5da0e608f357ae144a8b77f3ae9b3b06c53
Tempest Mccabe <tmccabe@pecan-test.bu.edu>
2017-01-25 13:21:25 -0500

    Added custom xml to Betsy's updated code

--- modules/benchmark/inst/scripts/benchmark.workflow.R
+++ modules/benchmark/inst/scripts/benchmark.workflow.R
@@ -1,5 +1,5 @@
 
-settings <- read.settings(settings.file)
+settings <- read.settings("modules/benchmark/inst/scripts/tess.xml")
 
 d <- settings$database$bety[c("dbname", "password", "host", "user")]
 bety <- src_postgres(host = d$host, user = d$user, password = d$password, dbname = d$dbname)
