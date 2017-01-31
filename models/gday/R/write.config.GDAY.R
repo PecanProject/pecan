573957f5a8a694f59874ab8ba28238beccf7e45b
tonygardella <tonygard@bu.edu>
2017-01-31 10:55:23 -0600

    sorting out paths and hard linkns

--- models/gday/R/write.config.GDAY.R
+++ models/gday/R/write.config.GDAY.R
@@ -57,6 +57,12 @@ write.config.GDAY <- function(defaults, trait.values, settings, run.id) {
         hostteardown <- paste(hostteardown, sep = "\n", paste(settings$host$postrun, collapse = "\n"))
     }
     
+    # Create Python Run script
+    runpy<- readLines(con = system.file("run_simulations.py", package = "PEcAn.GDAY"), n = -1)
+    
+    runpy <- gsub("@PATH_SCRIPTS@",settings$model$binary
+    runpy <- gsub("@PATHTOGDAY@",
+    runpy <- gsub("@PATH_PARAMS@",
     # create job.sh
     jobsh <- gsub("@HOST_SETUP@", hostsetup, jobsh)
     jobsh <- gsub("@HOST_TEARDOWN@", hostteardown, jobsh)
