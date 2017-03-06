766f5808560d66b9dfb4377328fbdc4abb954961
tonygardella <tonygard@bu.edu>
2017-03-06 17:52:36 -0500

    fix permisssion and give travis ability to build

--- book_source/deploy.sh
+++ book_source/deploy.sh
@@ -2,9 +2,15 @@
 
 USER=$(git remote show origin | grep -Po '[^:/]+(?=/pecan)' | head -n1)
 
+# configure your name and email if you have not done so
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+# clone the repository to the book-output directory
 if [ ! -d "../book_output" ]; then 
 git clone -b gh-pages \
-git@github.com:$USER/pecan \
+#git@github.com:$USER/pecan \
+https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
 ../book_output
 fi
 
