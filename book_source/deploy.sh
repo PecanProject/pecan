74568c5292cc6e9cf5e065ec0c53eb94e904ce7f
tonygardella <tonygard@bu.edu>
2017-03-08 17:02:10 -0500

    changes to allow push to seperate repository

--- book_source/deploy.sh
+++ book_source/deploy.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-USER=$(git remote show origin | grep -Po '[^:/]+(?=/pecan)' | head -n1)
+set -e
+
+#USER=$(git remote show origin | grep -Po '[^:/]+(?=/pecan)' | head -n1)
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
 # configure your name and email if you have not done so
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
+git config --global user.email "tonygard@bu.edu"
+git config --global user.name "Tony Gardella"
 
 # clone the repository to the book-output directory
-if [ ! -d "../book_output" ]; then 
-git clone -b gh-pages \
+#if [ ! -d "../book_output" ]; then 
+#git clone -b gh-pages \
 #git@github.com:$USER/pecan \
 #https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
 #git@github.com:${GITHUB_PAT}/pecan_bookdown.git \
-https://github.com/PecanProject/pecan_bookdown.git 
-../book_output
-fi
+#https://github.com/PecanProject/pecan_bookdown.git 
+#fi
 
-pushd ../book_output
+git clone https://${GITHUB_PAT}@github.com/pecan_bookdown.git book_output
 
-cp -r ../book/* ./
-  
+cd ../book_output
+
+
+cp -r ../_book/* ./
 git add --all *
-  
-git commit -m "Update the Book `date`"
+git commit -m"Update the book `date`" || true
+git push -q origin master
 
-git push -q origin gh-pages
