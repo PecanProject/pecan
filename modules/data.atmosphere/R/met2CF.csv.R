2ad8a133fc9642f464d4913b92d60e75e96452ca
Mike Dietze <dietze@bu.edu>
2017-03-23 18:06:10 -0400

    met2CF.csv: datetime_units parsing wasn't calling lubridate package

--- modules/data.atmosphere/R/met2CF.csv.R
+++ modules/data.atmosphere/R/met2CF.csv.R
@@ -45,7 +45,6 @@
 met2CF.csv <- function(in.path, in.prefix, outfolder, start_date, end_date, format, lat = NULL, lon = NULL, 
                        nc_verbose = FALSE, overwrite = FALSE, ...) {
 
-  
   start_year <- lubridate::year(start_date)
   end_year   <- lubridate::year(end_date)
   if (!file.exists(outfolder)) {
@@ -163,6 +162,9 @@ met2CF.csv <- function(in.path, in.prefix, outfolder, start_date, end_date, form
       datetime_units <- format$vars$input_units[datetime_index]  #lubridate function to call such as ymd_hms
       if (datetime_units == "") {
         datetime_units <- "ymd_hm"
+      }
+      if(length(grep("lubridate",datetime_units))==0){
+        datetime_units = paste0("lubridate::",datetime_units)
       }
       datetime_raw <- alldat[, format$vars$input_name[datetime_index]]
       alldatetime <- do.call(datetime_units, list(datetime_raw))  #convert to POSIXct convention
