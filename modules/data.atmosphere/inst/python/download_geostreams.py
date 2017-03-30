bbf2ff1984195657cc918dcf41a26e1414a47889
harshagrawal28 <harshagrawal28@gmail.com>
2017-03-30 12:25:55 -0500

    Added comments

--- modules/data.atmosphere/inst/python/download_geostreams.py
+++ modules/data.atmosphere/inst/python/download_geostreams.py
@@ -1,6 +1,18 @@
 import requests
 import json
 
+"""
+host_url - clowder url (https://terreref.ncsa.illinois.edu/clowder)
+site_name - name of location whose datapoints should be downloaded
+start_time, end_time - format "YYYY-mm-DDTHH:MM::SSZ"
+Example - So, if you want data from January 1st 2016 to January 6th 2016, 
+start_time = "2016-01-01T06:45:00Z" , end_time = "2016-01-06T06:45:00Z".
+This will get data from 2016-01-01 midnight to 2016-01-06 midnight.
+"06:45:00" is needed to account for time offset of 7 hours.
+"""
+
+
+
 
 def download_geostreams(host_url, site_name, start_time, end_time, outfile):
     # https://terraref.ncsa.illinois.edu/clowder
