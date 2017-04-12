4bbff7e7022e6ef7880d4987068881169dc84c03
Alexey Shiklomanov <ashiklom@bu.edu>
2017-04-12 14:24:14 -0400

    Skip db tests if on travis

--- db/tests/testthat/db.setup.R
+++ db/tests/testthat/db.setup.R
@@ -1,11 +1,16 @@
+# Check if running on continuous integration
+# If yes, skip this test
+is_ci <- Sys.getenv('CI') != ''
 con <- NULL
-try({
-    if(fqdn() == "pecan2.bu.edu") {
-        con <- db.open(list(host="psql-pecan.bu.edu", driver = "PostgreSQL", user = "bety", dbname = "bety", password = "bety"))
-    } else {
-        con <- db.open(list(host="localhost", driver = "PostgreSQL", user = "bety", dbname = "bety", password = "bety"))
-    }
-}, silent=T)
+if (!is_ci) {
+    try({
+        if(fqdn() == "pecan2.bu.edu") {
+            con <- db.open(list(host="psql-pecan.bu.edu", driver = "PostgreSQL", user = "bety", dbname = "bety", password = "bety"))
+        } else {
+            con <- db.open(list(host="localhost", driver = "PostgreSQL", user = "bety", dbname = "bety", password = "bety"))
+        }
+    }, silent=T)
+}
 
 if(is.null(con)) {
     skip("Can't get a valid test connection right now. Skipping test. ")
