c772a6fbe717447b85a4b92662eabc2c8b1cac68
Alexey Shiklomanov <ashiklom@bu.edu>
2017-04-12 14:53:13 -0400

    Fix skipping db tests on travis

--- db/tests/testthat/test.query.base.R
+++ db/tests/testthat/test.query.base.R
@@ -11,10 +11,10 @@ source('db.setup.R')
 context("test db.query")
 
 test_that("db.query can execute a trivial SQL statement and return results",{  
+    con <- check_db_test()
     ans <- db.query("select count(*) from traits;", con = con)
     expect_is(ans, "data.frame")
     expect_is(ans[,1], "numeric")
     expect_true(length(ans) == 1)
+    try(db.close(con))
 })
-
-db.close(con = con)
