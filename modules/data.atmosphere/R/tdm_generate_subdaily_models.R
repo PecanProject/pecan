fab4e3755db3caec1ed65d329c75d8a32741a418
jsimkins2 <jsimkins2@wisc.edu>
2017-04-17 16:20:37 -0500

    Temporally Downscale Meteorology (TDM family) scripts tidy sourced to 70 width with CF convention throughout

---
+++ modules/data.atmosphere/R/tdm_generate_subdaily_models.R
@@ -0,0 +1,226 @@
+##' Create statistical models to predict subdaily meteorology from daily means
+##' @family tdm - Temporally Downscale Meteorology
+##' @title gen_subdaily_models
+##' @param outfolder - directory where models will be stored *** storage required varies by size of training dataset, but prepare for >100 GB
+##' @param dat.train_file - train_data file
+##' @param in.prefix 
+##' @param tdf_file - tdm_temporal_downscale_functions.R filepath that can be sourced i.e. '~/scripts/temporal_downscale_functions.R'
+##' @param n.beta - number of betas to save from linear regression model
+##' @param resids - logical stating whether to pass on residual data or not
+##' @param parallel - logical stating whether to run temporal_downscale_functions.R in parallel 
+##' @param n.cores - deals with parallelization
+##' @param day.window - integer specifying number of days around a particular day you want to use data from for that 
+##'                     specific hours coefficients
+##' @param overwrite
+##' @param verbose
+
+##' @author Christy Rollinson, James Simkins
+
+#----------------------------------------------------------------------
+# Begin Script
+#----------------------------------------------------------------------
+
+gen_subdaily_models <- function(outfolder, dat.train_file, in.prefix, tdf_file, 
+    n.beta, day.window, resids = F, parallel = F, n.cores = NULL, overwrite = TRUE, 
+    verbose = FALSE) {
+
+    pb.index <- 1
+    pb <- txtProgressBar(min = 1, max = 8, style = 3)
+    
+    # ----- 1.0 Read data & Make time stamps ---------- Load the data
+    
+    vars.info <- data.frame(CF.name = c("date", "year", "doy", "hour", 
+        "air_temperature", "precipitation_flux", "air_temperature_max", 
+        "air_temperature_min", "surface_downwelling_shortwave_flux_in_air", 
+        "surface_downwelling_longwave_flux_in_air", "air_pressure", "specific_humidity", 
+        "eastward_wind", "northward_wind", "wind_speed"))
+    dat.train <- list()
+    tem <- ncdf4::nc_open(dat.train_file)
+    dim <- tem$dim
+    for (j in seq_along(vars.info$CF.name)) {
+        if (exists(as.character(vars.info$CF.name[j]), tem$var) == FALSE) {
+            dat.train[[j]] <- NA
+        } else {
+            dat.train[[j]] <- ncdf4::ncvar_get(tem, as.character(vars.info$CF.name[j]))
+        }
+    }
+    names(dat.train) <- vars.info$CF.name
+    dat.train <- data.frame(dat.train)
+    
+    dat.train$date <- strptime(paste(dat.train$year, dat.train$doy + 1, 
+        dat.train$hour, sep = "-"), "%Y-%j-%H", tz = "GMT")
+    dat.train$time.hr <- as.numeric(difftime(dat.train$date, paste0((min(dat.train$year) - 
+        1), "-12-31 23:00:00"), tz = "GMT", units = "hour"))
+    dat.train$time.day <- as.numeric(difftime(dat.train$date, paste0((min(dat.train$year) - 
+        1), "-12-31 23:00:00"), tz = "GMT", units = "day")) - 1/24
+    dat.train$time.day2 <- as.integer(dat.train$time.day + 1/(48 * 2)) + 
+        1  # Offset by half a time step to get time stamps to line up
+    dat.train <- dat.train[order(dat.train$time.hr), ]
+    
+    
+    # ----- 1.1 Coming up with the daily means that are what we can
+    # use as predictors ----------
+    
+    train.day <- aggregate(dat.train[, c("air_temperature", "precipitation_flux", 
+        "surface_downwelling_shortwave_flux_in_air", "surface_downwelling_longwave_flux_in_air", 
+        "air_pressure", "specific_humidity", "wind_speed")], by = dat.train[, 
+        c("year", "doy")], FUN = mean)
+    names(train.day)[3:9] <- c("air_temperature_mean.day", "precipitation_flux.day", 
+        "surface_downwelling_shortwave_flux_in_air.day", "surface_downwelling_longwave_flux_in_air.day", 
+        "air_pressure.day", "specific_humidity.day", "wind_speed.day")
+    train.day$air_temperature_max.day <- aggregate(dat.train[, c("air_temperature")], 
+        by = dat.train[, c("year", "doy")], FUN = max)$x
+    train.day$air_temperature_min.day <- aggregate(dat.train[, c("air_temperature")], 
+        by = dat.train[, c("year", "doy")], FUN = min)$x
+    summary(train.day)
+    
+    
+    dat.train <- merge(dat.train[, ], train.day, all.x = T, all.y = T)
+    summary(dat.train)
+    
+    # ----- 1.2 Setting up a 1-hour lag -- smooth transitions at
+    # midnight NOTE: because we're filtering from the present back through
+    # the past, -1 will associate the closest hour that we've already done
+    # (midnight) with the day we're currently working on ----------
+    vars.hour <- c("air_temperature", "precipitation_flux", "surface_downwelling_shortwave_flux_in_air", 
+        "surface_downwelling_longwave_flux_in_air", "air_pressure", "specific_humidity", 
+        "wind_speed")
+    vars.lag <- c("lag.air_temperature", "lag.precipitation_flux", "lag.surface_downwelling_shortwave_flux_in_air", 
+        "lag.surface_downwelling_longwave_flux_in_air", "lag.air_pressure", 
+        "lag.specific_humidity", "lag.wind_speed")
+    lag.day <- dat.train[dat.train$hour == 23, c("year", "doy", "time.day2", 
+        vars.hour)]
+    names(lag.day)[4:10] <- vars.lag
+    
+    lag.day <- aggregate(lag.day[, vars.lag], by = lag.day[, c("year", 
+        "doy", "time.day2")], FUN = mean)
+    lag.day$lag.air_temperature_min <- aggregate(dat.train[, c("air_temperature")], 
+        by = dat.train[, c("year", "doy", "time.day2")], FUN = min)[, "x"]  # Add in a lag for the next day's min temp
+    lag.day$lag.air_temperature_max <- aggregate(dat.train[, c("air_temperature")], 
+        by = dat.train[, c("year", "doy", "time.day2")], FUN = max)[, "x"]  # Add in a lag for the next day's min temp
+    lag.day$time.day2 <- lag.day$time.day2 + 1  # +1 for forward filtering downscale
+    
+    
+    dat.train <- merge(dat.train, lag.day[, c("time.day2", vars.lag, "lag.air_temperature_min", 
+        "lag.air_temperature_max")], all.x = T)
+    
+    # ----- 1.3 Setting up a variable to 'preview' the next day's mean
+    # to help get smoother transitions NOTE: because we're filtering from
+    # the present back through the past, +1 will associate the mean for the
+    # next day we're going to model with the one we're currently working on
+    # ----------
+    vars.day <- c("air_temperature_mean.day", "air_temperature_max.day", 
+        "air_temperature_mean.day", "precipitation_flux.day", "surface_downwelling_shortwave_flux_in_air.day", 
+        "surface_downwelling_longwave_flux_in_air.day", "air_pressure.day", 
+        "specific_humidity.day", "wind_speed.day")
+    vars.next <- c("next.air_temperature_mean", "next.air_temperature_max", 
+        "next.air_temperature_min", "next.precipitation_flux", "next.surface_downwelling_shortwave_flux_in_air", 
+        "next.surface_downwelling_longwave_flux_in_air", "next.air_pressure", 
+        "next.specific_humidity", "next.wind_speed")
+    
+    next.day <- dat.train[c("year", "doy", "time.day2", vars.day)]
+    names(next.day)[4:12] <- vars.next
+    next.day <- aggregate(next.day[, vars.next], by = next.day[, c("year", 
+        "doy", "time.day2")], FUN = mean)
+    next.day$time.day2 <- next.day$time.day2 - 1
+    
+    dat.train <- merge(dat.train, next.day[, c("time.day2", vars.next)], 
+        all.x = T)
+    
+    # ----- 1.4 calculate air_temperature_min & air_temperature_max as
+    # departure from mean; order data ---------- Lookign at max & min as
+    # departure from mean
+    dat.train$max.dep <- dat.train$air_temperature_max.day - dat.train$air_temperature_mean.day
+    dat.train$min.dep <- dat.train$air_temperature_min.day - dat.train$air_temperature_mean.day
+    summary(dat.train)
+    
+    # Order the data just to help with my sanity when visualizing
+    dat.train <- dat.train[order(dat.train$time.hr), ]
+    summary(dat.train)
+    
+    # ------------------------------------------ 2 Train the models for
+    # each variable and save them to be read in as needed
+    # ------------------------------------------
+    source(tdf_file)
+    
+    
+    # ----- 2.1 Generating all the daily models, save the output as
+    # .Rdata files, then clear memory Note: Could save Betas as .nc files
+    # that we pull from as needed to save memory; but for now just leaving
+    # it in the .Rdata file for eas Note: To avoid propogating too much
+    # wonkiness in hourly data, any co-variates are at the daily level
+    # Note: If mod.precipitation_flux.doy doesn't run, try increasing the
+    # day.window for this variable. The lack of non-zero values makes it
+    # difficult for the linear regression model to calculate coefficients
+    # sometimes ---------
+    setTxtProgressBar(pb, pb.index)
+    
+    #---------------------------Air Temperature
+    mod.air_temperature.doy <- model.air_temperature(dat.train = dat.train[, 
+        ], resids = resids, parallel = parallel, path.out = paste0(outfolder, 
+        in.prefix, "/air_temperature"), n.cores = n.cores, n.beta = n.beta, 
+        day.window = day.window)
+    rm(mod.air_temperature.doy)
+    
+    pb.index <- pb.index + 1
+    setTxtProgressBar(pb, pb.index)
+    
+    #----------------------------Precipitation Flux
+    mod.precipitation_flux.doy <- model.precipitation_flux(dat.train = dat.train[, 
+        ], resids = resids, parallel = parallel, path.out = paste0(outfolder, 
+        in.prefix, "/precipitation_flux"), n.cores = n.cores, n.beta = n.beta, 
+        day.window = day.window)
+    rm(mod.precipitation_flux.doy)
+    
+    pb.index <- pb.index + 1
+    setTxtProgressBar(pb, pb.index)
+    
+    #----------------------------Shortwave
+    mod.surface_downwelling_shortwave_flux_in_air.doy <- model.surface_downwelling_shortwave_flux_in_air(dat.train = dat.train[, 
+        ], resids = resids, parallel = parallel, path.out = paste0(outfolder, 
+        in.prefix, "/surface_downwelling_shortwave_flux_in_air"), n.cores = n.cores, 
+        n.beta = n.beta, day.window = day.window)
+    rm(mod.surface_downwelling_shortwave_flux_in_air.doy)
+    
+    pb.index <- pb.index + 1
+    setTxtProgressBar(pb, pb.index)
+    
+    #----------------------------Longwave
+    mod.surface_downwelling_longwave_flux_in_air.doy <- model.surface_downwelling_longwave_flux_in_air(dat.train = dat.train[, 
+        ], resids = resids, parallel = parallel, path.out = paste0(outfolder, 
+        in.prefix, "/surface_downwelling_longwave_flux_in_air"), n.cores = n.cores, 
+        n.beta = n.beta, day.window = day.window)
+    rm(mod.surface_downwelling_longwave_flux_in_air.doy)
+    
+    pb.index <- pb.index + 1
+    setTxtProgressBar(pb, pb.index)
+    
+    #---------------------------Air Pressure
+    mod.air_pressure.doy <- model.air_pressure(dat.train = dat.train[, 
+        ], resids = resids, parallel = parallel, path.out = paste0(outfolder, 
+        in.prefix, "/air_pressure"), n.cores = n.cores, n.beta = n.beta, 
+        day.window = day.window)
+    rm(mod.air_pressure.doy)
+    
+    pb.index <- pb.index + 1
+    setTxtProgressBar(pb, pb.index)
+    
+    #---------------------------Specific Humidity
+    mod.specific_humidity.doy <- model.specific_humidity(dat.train = dat.train[, 
+        ], resids = resids, parallel = parallel, path.out = paste0(outfolder, 
+        in.prefix, "/specific_humidity"), n.cores = n.cores, n.beta = n.beta, 
+        day.window = day.window)
+    rm(mod.specific_humidity.doy)
+    
+    pb.index <- pb.index + 1
+    setTxtProgressBar(pb, pb.index)
+    
+    #--------------------------- Wind speed
+    mod.wind_speed.doy <- model.wind_speed(dat.train = dat.train[, ], resids = resids, 
+        parallel = parallel, path.out = paste0(outfolder, in.prefix, "/wind_speed"), 
+        n.cores = n.cores, n.beta = n.beta, day.window = day.window)
+    rm(mod.wind_speed.doy)
+    
+    pb.index <- pb.index + 1
+    setTxtProgressBar(pb, pb.index)
+}
---
+++ modules/data.atmosphere/R/tdm_nc2dat.train.R
@@ -0,0 +1,196 @@
+##' Parses multiple netCDF files into one central document for temporal downscaling procedure
+##' @family tdm - Temporally Downscale Meteorology
+##' @title nc2dat.train
+##' @param outfolder - directory where output will be stored
+##' @param in.path - path of coarse model (e.g. GCM output)
+##' @param in.prefix - prefix of model string as character (e.g. IPSL.r1i1p1.rcp85)
+##' @param start_date 
+##' @param end_date
+##' @param lat.in
+##' @param lon.in
+##' @param upscale - # Upscale can either be set for FALSE (leave alone) or to the temporal resolution you want to aggregate to
+# options are: year, doy (day of year), or hour
+##' @param overwrite
+##' @param verbose
+
+##' @author James Simkins, Christy Rollinson
+
+#----------------------------------------------------------------------
+# Begin Script
+#----------------------------------------------------------------------
+nc2dat.train <- function(outfolder, in.path, in.prefix, start_date, end_date, 
+    upscale = FALSE, overwrite = FALSE, verbose = FALSE, ...) {
+    
+    start_date <- as.POSIXlt(start_date, tz = "UTC")
+    end_date <- as.POSIXlt(end_date, tz = "UTC")
+    
+    start_year <- lubridate::year(start_date)
+    end_year <- lubridate::year(end_date)
+    
+    yr_seq <- seq(start_year, end_year)
+
+    #------------ Read in the data
+    
+    input_met <- list()
+    for (j in seq_along(yr_seq)) {
+        input_met[[j]] <- file.path(in.path, paste0(in.prefix, ".", yr_seq[j], 
+            ".nc", sep = ""))
+    }
+    
+    
+    vars.info <- data.frame(CF.name = c("air_temperature", "air_temperature_max", 
+        "air_temperature_min", "surface_downwelling_longwave_flux_in_air", 
+        "air_pressure", "surface_downwelling_shortwave_flux_in_air", "eastward_wind", 
+        "northward_wind", "specific_humidity", "precipitation_flux"), units = c("Kelvin", 
+        "Kelvin", "Kelvin", "W/m2", "Pascal", "W/m2", "m/s", "m/s", "g/g", 
+        "kg/m2/s"))
+    
+    stepby <- list()  # list of time stepbys just in case they vary
+    
+    # Have to do 1 go first to initialize the dataframe
+    raw_train_data <- list()
+    tem <- ncdf4::nc_open(input_met[[1]])
+    dim <- tem$dim
+    for (j in seq_along(vars.info$CF.name)) {
+        if (exists(as.character(vars.info$CF.name[j]), tem$var) == FALSE) {
+            raw_train_data[[j]] <- NA
+        } else {
+            raw_train_data[[j]] <- ncdf4::ncvar_get(tem, as.character(vars.info$CF.name[j]))
+        }
+    }
+    names(raw_train_data) <- vars.info$CF.name
+    train_df <- data.frame(raw_train_data)
+    
+    raw_train_data <- data.frame(raw_train_data)
+    if ((nrow(raw_train_data) == 17520) | (nrow(raw_train_data) == 17568)) {
+        stepby[1] <- 2
+    }
+    if ((nrow(raw_train_data) == 8760) | (nrow(raw_train_data) == 8784)) {
+        stepby[1] <- 1
+    }
+    
+    # Add a time stamp
+    start_time <- as.POSIXlt(paste0(yr_seq[1], "-01-01"), tz = "UTC")
+    end_time <- as.POSIXlt(paste0(yr_seq[1] + 1, "-01-01"), tz = "UTC")
+    train_df$date <- seq.POSIXt(from = start_time, by = 60 * 60/stepby[[1]], 
+        length.out = nrow(train_df))
+    summary(train_df)
+    
+    lat_raw_train_data <- as.numeric(ncdf4::ncvar_get(tem, "latitude"))
+    lon_raw_train_data <- as.numeric(ncdf4::ncvar_get(tem, "longitude"))
+    ncdf4::nc_close(tem)
+    
+    # Loop through remaining files and format Just a safe guard in case we
+    # run this for a single year
+    if (length(input_met) > 1) {
+        for (i in 2:length(input_met)) {
+            if (file.exists(input_met[[i]])) {
+                raw_train_data <- list()
+                tem <- ncdf4::nc_open(input_met[[i]])
+                dim <- tem$dim
+                for (j in seq_along(vars.info$CF.name)) {
+                  if (exists(as.character(vars.info$CF.name[j]), tem$var) == 
+                    FALSE) {
+                    raw_train_data[[j]] <- NA
+                  } else {
+                    raw_train_data[[j]] <- ncdf4::ncvar_get(tem, as.character(vars.info$CF.name[j]))
+                  }
+                }
+                names(raw_train_data) <- vars.info$CF.name
+                raw_train_data <- data.frame(raw_train_data)
+                
+                if ((nrow(raw_train_data) == 17520) | (nrow(raw_train_data) == 
+                  17568)) {
+                  stepby[i] <- 2
+                }
+                if ((nrow(raw_train_data) == 8760) | (nrow(raw_train_data) == 
+                  8784)) {
+                  stepby[i] <- 1
+                }
+                
+                # Add a time stamp
+                start_time <- as.POSIXlt(paste0(yr_seq[i], "-01-01"), tz = "UTC")
+                end_time <- as.POSIXlt(paste0(yr_seq[i] + 1, "-01-01"), 
+                  tz = "UTC")
+                raw_train_data$date <- seq.POSIXt(from = start_time, by = 60 * 
+                  60/stepby[[i]], length.out = nrow(raw_train_data))
+                summary(raw_train_data)
+                
+                train_df <- rbind(train_df, raw_train_data)
+            } else {
+                stepby[i] <- NA
+            }
+            ncdf4::nc_close(tem)
+        }  # End year loop
+    }
+    
+    # Quick & dirty way of gap-filling any lingering NAs
+    for (i in 1:ncol(train_df)) {
+        train_df[is.na(train_df[, i]), i] <- mean(train_df[, i], na.rm = TRUE)
+    }
+    
+    # Getting additional time stamps
+    train_df$year <- lubridate::year(train_df$date)
+    train_df$doy <- lubridate::yday(train_df$date)
+    train_df$hour <- lubridate::hour(train_df$date)
+    
+    if (upscale == FALSE) {
+        dat.train <- train_df
+    } else {
+        # Figure out which temporal variables we're aggregating over
+        time.vars <- c("year", "doy", "hour")
+        agg.ind <- which(time.vars == upscale)
+        time.vars <- time.vars[1:agg.ind]
+        
+        dat.train <- aggregate(train_df[, names(train_df)[!names(train_df) %in% 
+            c("year", "doy", "hour")]], by = train_df[, time.vars], FUN = mean, 
+            na.rm = F)
+        dat.train <- dat.train[order(dat.train$date), ]
+    }
+    # ---------------------------------
+    
+    # Add in wind speed & dataset name
+    dat.train$wind_speed <- sqrt(dat.train$eastward_wind^2 + dat.train$northward_wind^2)
+    dat.train$dataset <- paste0(in.prefix)
+    
+    # creating the columns required for downscaling functions
+    dat.train <- dat.train[, c("date", "year", "doy", "hour", "air_temperature", 
+        "precipitation_flux", "air_temperature_max", "air_temperature_min", 
+        "surface_downwelling_shortwave_flux_in_air", "surface_downwelling_longwave_flux_in_air", 
+        "air_pressure", "specific_humidity", "eastward_wind", "northward_wind", 
+        "wind_speed")]
+    
+    units_dat.train <- append(c("date", "year", "day of year", "hour"), 
+        as.character(vars.info$units))
+    rm(raw_train_data)
+    rm(train_df)
+    
+    # Create dimensions for NC file
+    ntime <- nrow(dat.train)
+    lat <- ncdf4::ncdim_def(name = "latitude", units = "degree_north", 
+        vals = lat_raw_train_data, create_dimvar = TRUE)
+    lon <- ncdf4::ncdim_def(name = "longitude", units = "degree_east", 
+        vals = lon_raw_train_data, create_dimvar = TRUE)
+    time <- ncdf4::ncdim_def(name = "time", units = "sec", vals = (1:ntime) * 
+        3600, create_dimvar = TRUE, unlim = TRUE)
+    dim <- list(lat, lon, time)
+    
+    # Create var.list for the NC file
+    var.list <- list()
+    for (j in seq_along(colnames(dat.train))) {
+        var.list[[j]] <- ncdf4::ncvar_def(name = as.character(colnames(dat.train[j])), 
+            units = as.character(units_dat.train[j]), dim = dim, missval = -9999, 
+            verbose = verbose)
+    }
+    
+    # Create NC file
+    dir.create(outfolder, showWarnings = FALSE, recursive = TRUE)
+    
+    loc.file <- file.path(outfolder, paste0(in.prefix, "_dat.train.nc"))
+    loc <- ncdf4::nc_create(filename = loc.file, vars = var.list, verbose = verbose)
+    
+    for (j in colnames(dat.train)) {
+        ncdf4::ncvar_put(nc = loc, varid = as.character(j), vals = dat.train[[j]][seq_len(nrow(dat.train))])
+    }
+    ncdf4::nc_close(loc)
+}
