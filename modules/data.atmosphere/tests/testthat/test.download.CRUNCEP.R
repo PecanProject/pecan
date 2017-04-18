4586c515375b0a3c800e1bc73f6e01176d16bce9
Chris Black <chris@ckblack.org>
2017-04-17 22:19:53 -0400

    respect overwrite parameter

--- modules/data.atmosphere/tests/testthat/test.download.CRUNCEP.R
+++ modules/data.atmosphere/tests/testthat/test.download.CRUNCEP.R
@@ -1,6 +1,8 @@
 context("Checking CRUNCEP download")
 
-tmpdir = tempdir()
+logger.setLevel("WARN")
+
+tmpdir <- tempdir()
 on.exit(unlink(tmpdir, recursive = TRUE))
 
 result <- download.CRUNCEP(outfolder = tmpdir,
@@ -16,4 +18,19 @@ ncdf4::nc_close(cf)
 
 test_that("reference times are present", {
   expect_equal(cf_units, "days since 2000-01-01")
+})
+
+test_that("overwrite argument is respected", {
+  # Skip message comes from logger.error,
+  # which writes to stderr but does not use message().
+  # If it did, this test would reduce to expect_message(download.CRUNCEP(...), "foo")
+  msg <- capture.output(download.CRUNCEP(outfolder = tmpdir,
+                                         start_date = "2000-01-01",
+                                         end_date = "2000-12-31",
+                                         site_id = 753,
+                                         lat.in = 40,
+                                         lon.in = -88,
+                                         overwrite = FALSE),
+                        type = "message")
+  expect_match(paste(msg, collapse="\n"), "already exists. Skipping")
 })
