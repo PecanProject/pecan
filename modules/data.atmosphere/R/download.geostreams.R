a625daeea36eaaa51c16decaf9f0b1b431f7fbc4
Chris Black <chris@ckblack.org>
2017-04-24 21:58:08 -0400

    read Clowder auth info from file if not given in call
    Defaults to reading ~/.pecan.clowder.xml, attempts unauthenticated download if not found.
    Sample file format for .pecan.clowder.xml:
    <?xml version="1.0"?>
    <pecan>
      <clowder>
        <hostname>terraref.illinois.edu</hostname>
        <user>black11@illinois.edu</user>
        <password>fake</password>
      </clowder>
    </pecan>

--- modules/data.atmosphere/R/download.geostreams.R
+++ modules/data.atmosphere/R/download.geostreams.R
@@ -4,7 +4,15 @@
 #' @param sitename character. Should match a geostreams sensor_name
 #' @param start_date,end_date datetime
 #' @param url base url for Clowder host
+#' @param key,user,pass authentication info for Clowder host.
 #' @param ... other arguments passed as query parameters
+#' @details Depending on the setup of your Clowder host, authentication may be by 
+#'   username/password, by API key, or skipped entirely. \code{download.Geostreams}
+#'   looks first in its call arguments for an API key, then a username and password, 
+#'   then if these are NULL it looks in the user's home directory for a file named 
+#'   `~/.pecan.clowder.xml`, and finally if no keys or passwords are found there it
+#'   attempts to connect unauthenticated.
+#'
 #' @export
 #' @importFrom PEcAn.utils logger.severe logger.info
 #' @author Harsh Agrawal, Chris Black
@@ -17,14 +25,19 @@
 download.Geostreams <- function(outfolder, sitename, 
                                 start_date, end_date,
                                 url = "https://terraref.ncsa.illinois.edu/clowder/api/geostreams",
+                                key NULL,
+                                user = NULL,
+                                pass = NULL,
                                 ...){
-  # TODO Handle auth explicitly. Currently just passing key as part of ...
-  
+
   start_date = lubridate::parse_date_time(start_date, orders = c("ymd", "ymdHMS", "ymdHMSz"), tz = "UTC")
   end_date = lubridate::parse_date_time(end_date, orders = c("ymd", "ymdHMS", "ymdHMSz"), tz = "UTC")
 
-  sensor_result <- httr::GET(paste0(url, "/sensors"),
-                           query = list(sensor_name = sitename, ...))
+  auth <- get_clowderauth(key, user, pass, url)
+
+  sensor_result <- httr::GET(url = paste0(url, "/sensors"),
+                             query = list(sensor_name = sitename, key = auth$key, ...),
+                             config = auth$userpass)
   httr::stop_for_status(sensor_result, "look up site info in Clowder")
   sensor_info <- jsonlite::fromJSON(httr::content(sensor_result, as = "text", encoding = "UTF-8"))
   sensor_mintime = lubridate::parse_date_time(sensor_info$min_start_time,
@@ -39,8 +52,10 @@ download.Geostreams <- function(outfolder, sitename,
   }
 
   sensor_id <- sensor_info$id
-  query_args <- list(sensor_id = sensor_id, since = start_date, until = end_date, ...)
-  met_result <- httr::GET(paste0(url, "/datapoints"), query = query_args)
+  query_args <- list(sensor_id = sensor_id, since = start_date, until = end_date, key = auth$key, ...)
+  met_result <- httr::GET(url = paste0(url, "/datapoints"),
+                          query = query_args,
+                          config = auth$userpass)
   logger.info(met_result$url)
   httr::stop_for_status(met_result, "download met data from Clowder")
 
@@ -57,4 +72,37 @@ download.Geostreams <- function(outfolder, sitename,
                     enddate = end_date,
                     dbfile.name = paste("Clowder", sitename, sep = "."),
                     stringsAsFactors = FALSE))
+}
+
+#' Authentication lookup helper
+#' 
+#' @param key,user,pass passed unchanged from \code{\link{get.Geostreams}} call, possibly null
+#' @param url matched against \code{<hostname>} in authfile, ignored if authfile contains no hostname.
+#' @param authfile path to a PEcAn-formatted XML settings file; must contain a \code{<clowder>} key
+#'
+get_clowderauth <- function(key, user, pass, url, authfile="~/.pecan.clowder.xml") {
+  if (!is.null(key)) {
+    return(list(key = key))
+  } else if (!is.null(user) && !is.null(pass)) {
+    return(list(userpass = httr::authenticate(user = user, password = pass)))
+  } else {
+    if (file.exists(authfile)) {
+      auth_file <- XML::xmlToList(XML::xmlParse(authfile))$clowder
+      if (!is.null(auth_file$hostname) && auth_file$hostname != httr::parse_url(url)$host) {
+        # auth in file isn't for this host; exit
+        return(NULL)
+      }
+      if (!is.null(auth_file$key)) {
+        return(list(key=key))
+      } else {
+        # allow for cases where one of user/pass given as argument and other is in file
+        if (is.null(user)) { user <- auth_file$user }
+        if (is.null(pass)) { pass <- auth_file$password }
+        if (xor(is.null(user), is.null(pass))) { return(NULL) }
+        return(list(userpass = httr::authenticate(user = user, password = pass)))
+      }
+    } else {
+      return(NULL)
+    }
+  }
 }
