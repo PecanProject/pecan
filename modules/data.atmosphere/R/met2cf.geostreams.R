2e47759ae71a64b30866e2068097f4596783d428
Chris Black <chris@ckblack.org>
2017-05-02 23:34:30 -0400

    better CF variable validation

--- modules/data.atmosphere/R/met2cf.geostreams.R
+++ modules/data.atmosphere/R/met2cf.geostreams.R
@@ -15,41 +15,28 @@ met2CF.Geostreams <- function(in.path, in.prefix, outfolder,
                     start_date, end_date, 
                     overwrite = FALSE, verbose = FALSE, ...) {
 
-  # TODO confirm all units / variable names match geostreams spec,
-  #   and that output names / units match CF convention
-  vars <- read.csv(
-    text = "name, units
-      air_temperature, K
-      relative_humidity, %
-      wind_speed, m/s
-      precipitation_rate, mm/s
-      air_pressure, Pa
-      surface_downwelling_photosynthetic_photon_flux_in_air, mol m-2 s-1
-      northward_wind, degrees
-      eastward_wind, degrees",
-    header=TRUE,
-    strip.white = TRUE,
-    stringsAsFactors = FALSE)
+  met.lookup = read.csv(system.file("/data/met.lookup.csv", package = "PEcAn.data.atmosphere"),
+                        header = TRUE, stringsAsFactors = FALSE)
 
   start_date <- as.POSIXct(start_date, tz="UTC")
   end_date <- as.POSIXct(end_date, tz="UTC")
 
   in_file <- file.path(in.path, paste(in.prefix, start_date, end_date, "json", sep = "."))
-  dat <- jsonlite::read_json(in_file, simplifyVector = TRUE, flatten = FALSE)
+  dat <- jsonlite::read_json(in_file, simplifyVector = TRUE, flatten = TRUE)
+  vars <- unlist(dat$sensor_info$parameters)
+  # TODO future versions of geostreams API may add units for each param
+  # if/when they are available, add code to check them
+  dat <- dat$data
+  names(dat) <- sub("^properties\\.", "", names(dat))
 
-  
-  dat$start_time <- as.POSIXct(dat$start_time, tz="UTC")
-  dat$end_time <- as.POSIXct(dat$end_time, tz="UTC")
+  dat$start_time <- lubridate::parse_date_time(dat$start_time, orders = "ymdHMSz", tz = "UTC")
+  dat$end_time <- lubridate::parse_date_time(dat$end_time, orders = "ymdHMSz", tz = "UTC")
   if (start_date < min(dat$start_time) | end_date > max(dat$end_time)) {
-    logger.severe("Requested dates", start_date, "--", end_date,
-                  "fall outside available data", min(dat$start_time), "--", max(dat$end_time))
+   logger.severe("Requested dates", start_date, "--", end_date,
+                 "fall outside available data", min(dat$start_time), "--", max(dat$end_time))
   }
 
-  if (isTRUE(all.equal(dat$start_time, dat$start_time))) {
-    dat$mid_time = dat$start_time
-  } else {
-    dat$mid_time <- dat$start_time + (dat$end_time - dat$start_time)/2
-  }
+  dat$mid_time <- dat$start_time + (dat$end_time - dat$start_time)/2
   dat <- dat[dat$start_time >= start_date & dat$end_time <= end_date,]
   secs_elapsed <- unclass(dat$mid_time) - unclass(start_date)
   days_elapsed <- udunits2::ud.convert(secs_elapsed, "sec", "days")
@@ -58,14 +45,14 @@ met2CF.Geostreams <- function(in.path, in.prefix, outfolder,
   time <- ncdf4::ncdim_def(name = "time", units = paste("days since", ref_time_str),
                            vals = days_elapsed, create_dimvar = TRUE, unlim = TRUE)
 
-  if (length(unique(dat$geometry$coordinates)) == 1) {
+  if (length(unique(dat$geometry.coordinates)) == 1) {
     # all lat/lons are are identical-- no need to store extra copies
-    raw_lat <- dat$geometry$coordinates[[1]][[1]]
-    raw_lon <- dat$geometry$coordinates[[1]][[2]]
+    raw_lat <- dat$geometry.coordinates[[1]][[1]]
+    raw_lon <- dat$geometry.coordinates[[1]][[2]]
   } else {
     # multiple coords in same file -- keep lat and lon as full-length vectors
-    raw_lat <- sapply(dat$geometry$coordinates, function(x)x[[1]])
-    raw_lon <- sapply(dat$geometry$coordinates, function(x)x[[2]])
+    raw_lat <- sapply(dat$geometry.coordinates, function(x)x[[1]])
+    raw_lon <- sapply(dat$geometry.coordinates, function(x)x[[2]])
   }
   
   lat <- ncdf4::ncdim_def(name = "latitude", units = "degrees_north", vals = raw_lat, create_dimvar = TRUE)
@@ -90,27 +77,37 @@ met2CF.Geostreams <- function(in.path, in.prefix, outfolder,
   }
 
   nc.file <- file.path(outfolder, paste(in.prefix, nc_date, "nc", sep = "."))
+  dir.create(outfolder, recursive = TRUE, showWarnings = FALSE)
   if (!overwrite &&  file.exists(nc.file)) {
-    logger.severe("Refusing to overwrite existing file", nc.file, " -- If you're sure, set overwrite=FALSE")
+    logger.severe("Refusing to overwrite existing file", nc.file, " -- If you're sure, set overwrite=TRUE")
   }
 
-  var_list = list()
-  for (i in seq_len(nrow(vars))) {
-    # TODO check file for any vars NOT in predefined list. How to handle?
-    var_list[[i]] <- ncdf4::ncvar_def(name = vars$name[[i]], 
-                                      units = vars$units[[i]], 
-                                      dim = cf_dims, 
-                                      missval = -999, 
-                                      verbose = verbose)
+  vars <- vars[vars != "source_file"] # This var is metadata, not met data
+  if ("precipitation_rate" %in% vars) {
+    # rate is in mm/sec, flux is in kg m^-2 sec^-1, conversion factor is 1.
+    vars <- sub("precipitation_rate", "precipitation_flux", vars)
+    names(dat) <- sub("precipitation_rate", "precipitation_flux", names(dat))
   }
+
+  make_ncvar <- function(name){
+    if (! name %in% met.lookup$CF_standard_name) {
+      logger.severe("Don't know how to convert parameter", name, "to CF standard format")
+    }
+    unit <- met.lookup[met.lookup$CF_standard_name == name, "units"]
+    ncdf4::ncvar_def(name = name,
+                     units = unit,
+                     dim = cf_dims,
+                     missval = -999,
+                     verbose = verbose)
+  }
+  var_list = lapply(vars, make_ncvar)
   
   cf <- ncdf4::nc_create(filename = nc.file, vars = var_list, verbose = verbose)
 
-  for (i in seq_len(nrow(vars))) {
-    vi = vars$name[[i]]
+  for (var in var_list) {
     ncdf4::ncvar_put(nc = cf,
-                     varid = vi,
-                     vals = dat$properties[[vi]],
+                     varid = var,
+                     vals = get(var$name, dat),
                      verbose = verbose)
   }
   ncdf4::nc_close(cf)
