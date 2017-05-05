8659038fbb0d89ee639f1632f3db7e1f80267bdb
Chris Black <chris@ckblack.org>
2017-05-05 13:33:35 -0400

    ppfd is in umols not mols

--- models/biocro/R/met2model.BIOCRO.R
+++ models/biocro/R/met2model.BIOCRO.R
@@ -131,7 +131,7 @@ cf2biocro <- function(met, longitude = NULL, zulu2solarnoon = FALSE) {
   }
   if (!"ppfd" %in% colnames(met)) {
     if ("surface_downwelling_photosynthetic_photon_flux_in_air" %in% colnames(met)) {
-      ppfd <- met$surface_downwelling_photosynthetic_photon_flux_in_air
+      ppfd <- udunits2::ud.convert(met$surface_downwelling_photosynthetic_photon_flux_in_air, "mol", "umol")
     } else if ("surface_downwelling_shortwave_flux_in_air" %in% colnames(met)) {
       par <- sw2par(met$surface_downwelling_shortwave_flux_in_air)
       ppfd <- par2ppfd(par)
