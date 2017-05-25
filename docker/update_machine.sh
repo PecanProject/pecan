4c4fbf777968fb49d1ddc2dbf418fb92f21e480d
Amanskywalker <ak47su30ac@gmail.com>
2017-05-25 19:30:50 +0530

    Added the Dockerfile supporting Scripts to install PEcAn and needed packages

---
+++ docker/update_machine.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+. /build/install_pecan_preprocessor.sh
+
+# actual install/update
+echo "######################################################################"
+echo "UPDATING MACHINE"
+echo "######################################################################"
+mkdir /home/carya/
+chmod 755 /home/carya/
+case "$OS_VERSION" in
+  RH_*)
+     yum update -y
+    if [ "$SETUP_VM" != "" ]; then
+       sed -i -e "s/^127.0.0.1 .*\$/127.0.0.1 ${HOSTNAME}.pecan ${HOSTNAME} localhost localhost.localdomain localhost4 localhost4.localdomain4/" /etc/hosts
+    fi
+    ;;
+  Ubuntu)
+     apt-get -qq -y update
+     apt-get -y dist-upgrade
+     apt-get -y purge --auto-remove
+    if [ "$SETUP_VM" != "" ]; then
+       sed -i -e "s/^127.0.0.1 .*\$/127.0.0.1 ${HOSTNAME}.pecan ${HOSTNAME} localhost/" /etc/hosts
+    fi
+    ;;
+  *)
+    echo "Unknown OS"
+    exit 1
+    ;;
+esac
