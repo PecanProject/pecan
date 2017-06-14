9e3815d1e8455192b8c046c321491d19b50e0d4b
Amanskywalker <ak47su30ac@gmail.com>
2017-06-14 22:38:36 +0530

    Removed pecan web codes from the core
    	Removed the pecan web codes from the core in docker/install_pecan.sh
    	Renamed the pecan-image to pecan-core in docker-compose.yml

--- docker/install_pecan.sh
+++ docker/install_pecan.sh
@@ -12,27 +12,3 @@ fi
 cd ${HOME}/pecan
 git pull
 make
-
- curl -o /var/www/html/pecan.pdf https://www.gitbook.com/download/pdf/book/pecan/pecan-documentation
- rm /var/www/html/index.html
- ln -s  ${HOME}/pecan/documentation/index_vm.html /var/www/html/index.html
-if [ ! -e ${HOME}/pecan/web/config.php ]; then
-  sed -e "s#browndog_url=.*#browndog_url=\"${BROWNDOG_URL}\";#" \
-      -e "s#browndog_username=.*#browndog_username=\"${BROWNDOG_USERNAME}\";#" \
-      -e "s#browndog_password=.*#browndog_password=\"${BROWNDOG_PASSWORD}\";#" \
-      -e "s#googleMapKey=.*#googleMapKey=\"${GOOGLE_MAP_KEY}\";#" \
-      -e "s/carya/$USER/g" ${HOME}/pecan/web/config.example.php > ${HOME}/pecan/web/config.php
-fi
-
-if [ ! -e ${HTTP_CONF}/pecan.conf ]; then
-  cat > /tmp/pecan.conf << EOF
-Alias /pecan ${HOME}/pecan/web
-<Directory ${HOME}/pecan/web>
-  DirectoryIndex index.php
-  Options +ExecCGI
-  Require all granted
-</Directory>
-EOF
-   cp /tmp/pecan.conf ${HTTP_CONF}/pecan.conf
-  rm /tmp/pecan.conf
-fi
