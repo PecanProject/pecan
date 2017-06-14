6332eb74744b98b85affe38c357f621b8e75a120
Ann Raiho <araiho@pecan-test.bu.edu>
2017-06-14 15:22:52 -0400

    making loop for when pfts are missing from a year

--- modules/assim.sequential/inst/paleon_sda.R
+++ modules/assim.sequential/inst/paleon_sda.R
@@ -1,13 +1,17 @@
 
-#LINKAGES
+library(PEcAn.all)
+library(PEcAn.assim.sequential)
+
+#LINKAGES #AGB.pft #Harvard Forest
 setwd('/fs/data2/output//PEcAn_1000003314/')
+
 #SIPNET
 #setwd('/fs/data2/output//PEcAn_1000003356')
 
-#---------------- Load PEcAn settings file. -------------------------------------------------------#
+#---------------- Load PEcAn settings file. --------------------------------#
 # Open and read in settings file for PEcAn run.
-settings <- read.settings("pecan.SDA.xml") 
+settings <- read.settings("pecan.SDA.xml")
 
-obs.list <- load_data_paleon_sda(settings = settings)
+obs.list <- PEcAn.assim.sequential::load_data_paleon_sda(settings = settings)
 
-sda.enkf(settings, obs.mean = obs.list$obs.mean, obs.cov = obs.list$obs.cov)
+sda.enkf(settings, obs.mean = obs.list$obs.mean, obs.cov = obs.list$obs.cov, IC <- NULL)
