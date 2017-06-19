b3cc614faba0899112e51522f7209071f5d159a4
David LeBauer <dlebauer@gmail.com>
2017-06-19 16:38:01 -0500

    minor changes to PEcAn.BIOCRO: version specific handling in read.biocro.config

--- models/biocro/R/read.biocro.config.R
+++ models/biocro/R/read.biocro.config.R
@@ -1,14 +1,16 @@
 ##' Read BioCro config file
 ##'
-##' @title Read BioCro Config 
+##' @title Read BioCro Config
 ##' @param config.file Path to XML file
 ##' @return list of run configuration parameters for PEcAn
 ##' @export
 ##' @author David LeBauer
 read.biocro.config <- function(config.file = "config.xml") {
-  config <- XML::xmlToList(XML::xmlTreeParse(file = config.file, 
+  config <- XML::xmlToList(XML::xmlTreeParse(file = config.file,
                                    handlers = list(comment = function(x) { NULL }),
                                    asTree = TRUE))
-  config$pft$canopyControl$mResp <- unlist(strsplit(config$pft$canopyControl$mResp, split = ","))
+  if(packageVersion('BioCro') < 1.0){
+    config$pft$canopyControl$mResp <- unlist(strsplit(config$pft$canopyControl$mResp, split = ","))
+  }
   return(config)
 }  # read.biocro.config
--- models/biocro/R/write.configs.BIOCRO.R
+++ models/biocro/R/write.configs.BIOCRO.R
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------
 # Copyright (c) 2012 University of Illinois, NCSA.
 # All rights reserved. This program and the accompanying materials
-# are made available under the terms of the 
+# are made available under the terms of the
 # University of Illinois/NCSA Open Source License
 # which accompanies this distribution, and is available at
 # http://opensource.ncsa.illinois.edu/license.html
@@ -11,7 +11,7 @@ PREFIX_XML <- "<?xml version=\"1.0\"?>\n<!DOCTYPE config SYSTEM \"biocro.dtd\">\
 
 ##------------------------------------------------------------------------------------------------#
 ##' convert parameters from PEcAn database default units to biocro defaults
-##' 
+##'
 ##' Performs model specific unit conversions on a a list of trait values,
 ##' such as those provided to write.config
 ##' @name convert.samples.BIOCRO
@@ -21,7 +21,7 @@ PREFIX_XML <- "<?xml version=\"1.0\"?>\n<!DOCTYPE config SYSTEM \"biocro.dtd\">\
 ##' @export
 ##' @author David LeBauer
 convert.samples.BIOCRO <- function(trait.samples) {
-  
+
   if (is.list(trait.samples)) {
     trait.samples <- as.data.frame(trait.samples)
   }
@@ -37,12 +37,12 @@ convert.samples.BIOCRO <- function(trait.samples) {
   trait.names[trait.names == "extinction_coefficient_diffuse"] <- "kd"
   trait.names[trait.names == "chi_leaf"] <- "chi.l"
   trait.names[trait.names == "quantum_efficiency"] <- "alpha"
-  
+
   colnames(trait.samples) <- trait.names
   ## Partitioning coefficients: especially leaf phenology
-  
+
   ## iRhizome iStem ifrRhizome ifrStem
-  
+
   ## transform values with different units cuticular conductance - BETY default is
   ## umol; BioCro uses mol
   if ("b0" %in% trait.names) {
@@ -59,7 +59,7 @@ convert.samples.BIOCRO <- function(trait.samples) {
     ## HAAAACK
     trait.samples <- transform(trait.samples, Rd = Rd)
   }
-  
+
   # kd = k*omega from $e^{-kL\omega}$, if (all(c('kd', 'clumping') %in%
   # trait.names)) { trait.samples <- transform(trait.samples, kd = clumping * kd,
   # clumping = NULL) }
@@ -68,20 +68,20 @@ convert.samples.BIOCRO <- function(trait.samples) {
 
 
 ##' Writes a configuration files for the biocro model
-##' 
+##'
 ##' @name write.config.BIOCRO
 ##' @title Write configuration files for the biocro model
-##' @param defaults named list with default model parameter values 
+##' @param defaults named list with default model parameter values
 ##' @param trait.values named list (or dataframe of trait values)
 ##'  can either be a data.frame or named list of traits, e.g.
 ##' \code{data.frame(vmax = 1, b0 = 2)} or \code{list(vmax = 1, b0 = 2)}
 ##' @param settings pecan settings file configured for BioCro
 ##' @param run.id integer; a unique identifier for the run.
 ##' @export
-##' @return nothing, writes configuration file as side effect 
+##' @return nothing, writes configuration file as side effect
 ##' @author David LeBauer
 write.config.BIOCRO <- function(defaults = NULL, trait.values, settings, run.id) {
-  
+
   ## find out where to write run/ouput
   rundir <- file.path(settings$host$rundir, as.character(run.id))
   outdir <- file.path(settings$host$outdir, as.character(run.id))
@@ -89,12 +89,12 @@ write.config.BIOCRO <- function(defaults = NULL, trait.values, settings, run.id)
     rundir <- file.path(settings$rundir, as.character(run.id))
     outdir <- file.path(settings$modeloutdir, as.character(run.id))
   }
-  
+
   ## create launch script (which will create symlink)
-  writeLines(c("#!/bin/bash", 
+  writeLines(c("#!/bin/bash",
                paste(settings$model$job.sh),
-               paste("mkdir -p", outdir), 
-               paste("cd", rundir), 
+               paste("mkdir -p", outdir),
+               paste("cd", rundir),
                paste(settings$model$binary, normalizePath(rundir, mustWork = FALSE), normalizePath(outdir, mustWork = FALSE)),
                "if [ $? -ne 0 ]; then",
                "    echo ERROR IN MODEL RUN >&2",
@@ -103,16 +103,16 @@ write.config.BIOCRO <- function(defaults = NULL, trait.values, settings, run.id)
                paste("cp ", file.path(rundir, "README.txt"), file.path(outdir, "README.txt"))),
              con = file.path(settings$rundir, run.id, "job.sh"))
   Sys.chmod(file.path(settings$rundir, run.id, "job.sh"))
-  
+
   ## write configuration file
   traits <- convert.samples.BIOCRO(trait.samples = trait.values[[settings$pfts$pft$name]])
-  
+
   species <- utils::read.csv(file.path(settings$pfts$pft$outdir, "species.csv"))
   genus <- unique(species$genus)
   if (length(genus) > 1) {
     logger.severe("BioCro can not combine multiple genera")
   }
-  
+
   ### Set defaults
   defaults.file <- defaults$pft$constants$file
   if (!is.null(defaults.file)) {
@@ -126,15 +126,15 @@ write.config.BIOCRO <- function(defaults = NULL, trait.values, settings, run.id)
       defaults.file <- NULL
     }
   } else if (is.null(defaults.file)) {
-    defaults.file <- system.file(file.path("extdata/defaults", paste0(tolower(genus), ".xml")), 
+    defaults.file <- system.file(file.path("extdata/defaults", paste0(tolower(genus), ".xml")),
                                  package = "PEcAn.BIOCRO")
   }
 
   if (file.exists(defaults.file)) {
-    if (packageVersioni('BioCro') >= 1.0) {
-        if (genus == 'Miscanthus') {
-            defaults <- list(initial_values=willow_initial_state, parameters=willow_parameters, modules=willow_modules)
-            defaults = append(list(type = list(photosynthesis = 'C4', genus = 'Miscanthus', name='miscanthus')), defaults)
+    if (packageVersion('BioCro') >= 1.0) {
+        if (genus == 'Sorghum') {
+            defaults <- list(initial_values=sorghum_initial_state, parameters=sorghum_parameters, modules=sorghum_modules)
+            defaults = append(list(type = list(photosynthesis = 'C4', genus = 'Sorghum')), defaults)
         }
     } else {
         defaults <- XML::xmlToList(XML::xmlParse(defaults.file))
@@ -142,11 +142,11 @@ write.config.BIOCRO <- function(defaults = NULL, trait.values, settings, run.id)
   } else {
     logger.severe("no defaults file given and ", genus, "not supported in BioCro")
   }
-  
+
   if (is.null(defaults)) {
     logger.error("No defaults values set")
   }
-  
+
   traits.used <- sapply(defaults, is.null)
   for (parm.type in names(defaults)) {
     for (parm in names(defaults[[parm.type]])) {
@@ -156,46 +156,46 @@ write.config.BIOCRO <- function(defaults = NULL, trait.values, settings, run.id)
       }
     }
   }
-  
+
   defaults$type$name <- settings$pfts$pft$name
-  
+
   ### Replace Defaults with meta-analysis results
   unused.traits <- !traits.used
   ## a clunky way to only use logger for MEDIAN rather than all runs
-  if (any(grepl("MEDIAN", scan(file = file.path(settings$rundir, run.id, "README.txt"), 
-                               character(0), 
-                               sep = ":", 
+  if (any(grepl("MEDIAN", scan(file = file.path(settings$rundir, run.id, "README.txt"),
+                               character(0),
+                               sep = ":",
                                strip.white = TRUE)))) {
     if (sum(unused.traits) > 0) {
-      logger.warn("the following traits parameters are not added to config file:", 
+      logger.warn("the following traits parameters are not added to config file:",
                   vecpaste(names(unused.traits)[unused.traits == TRUE]))
     }
   }
-  
+
   ## this is where soil parms can be set defaults$soilControl$FieldC <-
-  
+
   ### Put defaults and other parts of config file together
   parms.xml <- listToXml(defaults, "pft")
-  location.xml <- listToXml(list(latitude = settings$run$site$lat, 
-                                 longitude = settings$run$site$lon), 
+  location.xml <- listToXml(list(latitude = settings$run$site$lat,
+                                 longitude = settings$run$site$lon),
                             "location")
-  run.xml <- listToXml(list(start.date = settings$run$start.date, 
-                            end.date = settings$run$end.date, 
+  run.xml <- listToXml(list(start.date = settings$run$start.date,
+                            end.date = settings$run$end.date,
                             met.path = settings$run$inputs$met$path,
-                            soil.file = settings$run$inputs$soil$path), 
+                            soil.file = settings$run$inputs$soil$path),
                        "run")
-  
+
   slashdate <- function(x) substr(gsub("-", "/", x), 1, 10)
-  simulationPeriod.xml <- listToXml(list(dateofplanting = slashdate(settings$run$start.date), 
+  simulationPeriod.xml <- listToXml(list(dateofplanting = slashdate(settings$run$start.date),
                                          dateofharvest = slashdate(settings$run$end.date)),
                                     "simulationPeriod")
-  
+
   config.xml <- XML::xmlNode("config")
   config.xml <- XML::append.xmlNode(config.xml, run.xml)
   config.xml <- XML::append.xmlNode(config.xml, location.xml)
   config.xml <- XML::append.xmlNode(config.xml, simulationPeriod.xml)
   config.xml <- XML::append.xmlNode(config.xml, parms.xml)
-  
+
   XML::saveXML(config.xml, file = file.path(settings$rundir, run.id, "config.xml"),
           indent = TRUE)
 }  # write.config.BIOCRO
@@ -207,12 +207,12 @@ write.config.BIOCRO <- function(defaults = NULL, trait.values, settings, run.id)
 ##' @name remove.config.BIOCRO
 ##' @title Clear out previous biocro config and parameter files.
 ##' @param main.outdir Primary PEcAn output directory (will be depreciated)
-##' @param settings PEcAn settings file 
+##' @param settings PEcAn settings file
 ##' @return nothing, removes config files as side effect
 ##' @export
 ##' @author Shawn Serbin, David LeBauer
 remove.config.BIOCRO <- function(main.outdir, settings) {
-  
+
   ### Remove files on localhost
   if (settings$host$name == "localhost") {
     files   <- paste0(settings$outdir, list.files(path = settings$outdir, recursive = FALSE))  # Need to change this to the run folder when implemented
@@ -223,7 +223,7 @@ remove.config.BIOCRO <- function(main.outdir, settings) {
     files <- files[-grep(pft.dir, files)]  # Keep pft folder
     # file.remove(files,recursive=TRUE)
     system(paste("rm -r ", files, sep = "", collapse = " "), ignore.stderr = TRUE)  # remove files/dirs
-    
+
     ### On remote host
   } else {
     print("*** WARNING: Removal of files on remote host not yet implemented ***")
