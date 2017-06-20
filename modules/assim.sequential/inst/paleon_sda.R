10846a2a13123192b8416a896c9f56c8eee4471b
Ann Raiho <araiho@pecan-test.bu.edu>
2017-06-20 19:06:48 -0400

    adding SIPNET stuff

--- modules/assim.sequential/inst/paleon_sda.R
+++ modules/assim.sequential/inst/paleon_sda.R
@@ -1,12 +1,13 @@
 
 library(PEcAn.all)
+library(PEcAn.SIPNET)
 library(PEcAn.assim.sequential)
 
 #LINKAGES #AGB.pft #Harvard Forest
-setwd('/fs/data2/output//PEcAn_1000003314/')
+#setwd('/fs/data2/output//PEcAn_1000003314/')
 
 #SIPNET
-#setwd('/fs/data2/output//PEcAn_1000003356')
+setwd('/fs/data2/output//PEcAn_1000003356')
 
 #---------------- Load PEcAn settings file. --------------------------------#
 # Open and read in settings file for PEcAn run.
@@ -14,6 +15,12 @@ settings <- read.settings("pecan.SDA.xml")
 
 obs.list <- PEcAn.assim.sequential::load_data_paleon_sda(settings = settings)
 
-save(obs.list,file='obs.list.Rdata')
+status.start("IC")
+ne <- as.numeric(settings$state.data.assimilation$n.ensemble)
+IC <- sample.IC.SIPNET(ne, state = c('AGB','NPP'))
+status.end()
 
-sda.enkf(settings, obs.mean = obs.list$obs.mean, obs.cov = obs.list$obs.cov, IC = NULL)
+sda.enkf(settings, obs.mean = obs.list$obs.mean, obs.cov = obs.list$obs.cov, IC = IC)
+
+
+
