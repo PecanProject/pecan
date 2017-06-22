3b3525591a96a3cf19b86d9d2dfb7f929f624188
Ann Raiho <araiho@pecan-test.bu.edu>
2017-06-22 13:18:32 -0400

    making some changes to be able to redo years in sda without any hassle. Now, .nc files will be remade instead of skipped if they already exist (might want to look back into if that's an okay thing to do). And the restart files will look for old output if the new output isn't there. (might want to think about always looking for the output related to time)

--- models/linkages/R/model2netcdf.LINKAGES.R
+++ models/linkages/R/model2netcdf.LINKAGES.R
@@ -26,7 +26,7 @@ model2netcdf.LINKAGES <- function(outdir, sitelat, sitelon, start_date = NULL, e
   # , PFTs) { logger.severe('NOT IMPLEMENTED')
   
   library(PEcAn.utils)
-  
+
   ### Read in model output in linkages format
   load(file.path(outdir, "linkages.out.Rdata"))
   # linkages.output.dims <- dim(linkages.output)
@@ -39,9 +39,9 @@ model2netcdf.LINKAGES <- function(outdir, sitelat, sitelon, start_date = NULL, e
   
   ### Loop over years in linkages output to create separate netCDF outputs
   for (y in seq_along(years)) {
-    if (file.exists(file.path(outdir, paste(years[y], "nc", sep = ".")))) {
-      next
-    }
+    # if (file.exists(file.path(outdir, paste(years[y], "nc", sep = "."))) & overwrite ==FALSE) {
+    #   next
+    # }
     print(paste("---- Processing year: ", years[y]))  # turn on for debugging
     
     ## Subset data for processing sub.linkages.output <- subset(linkages.output, year == y)
