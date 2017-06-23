648e22b540879025c1e3eb9843f73c0a6b4a45f4
Ann Raiho <araiho@pecan-test.bu.edu>
2017-06-23 00:15:38 -0400

    added ensemble adjustment

--- modules/assim.sequential/inst/paleon_sda.R
+++ modules/assim.sequential/inst/paleon_sda.R
@@ -12,11 +12,14 @@ ciEnvelope <- function(x,ylo,yhi,...){
 }
 
 #LINKAGES #AGB.pft #Harvard Forest
-setwd('/fs/data2/output//PEcAn_1000003314/')
+#setwd('/fs/data2/output//PEcAn_1000003314/')
+#TO DO: Having problem with running proc.var == TRUE because nimble isn't keeping the toggle sampler in the function environment.
+#TO DO: Intial conditions for linkages are messed up. Need to calibrate.
+
 
 #SIPNET
-#setwd('/fs/data2/output//PEcAn_1000003356')
-#TO DO: Normalize state vector because NPP is too small.
+setwd('/fs/data2/output//PEcAn_1000003356')
+#TO DO: Skip ensemble members that fail or are missing in read.restart
 #See talk with with Mike on 6/21/17
 
 #---------------- Load PEcAn settings file. --------------------------------#
@@ -27,15 +30,13 @@ obs.list <- PEcAn.assim.sequential::load_data_paleon_sda(settings = settings)
 
 IC <- NULL
 
-# status.start("IC")
-# ne <- as.numeric(settings$state.data.assimilation$n.ensemble)
-# IC <- sample.IC.SIPNET(ne, state = c('AGB','NPP'))
-# status.end()
+status.start("IC")
+ne <- as.numeric(settings$state.data.assimilation$n.ensemble)
+IC <- sample.IC.SIPNET(ne, state = c('AGB','NPP'))
+status.end()
 
-#TO DO: Having problem with running proc.var == TRUE because nimble isn't keeping the toggle sampler in the function environment.
-#TO DO: Intial conditions for linkages are messed up. Need to calibrate.
 
-sda.enkf(settings, obs.mean = obs.list$obs.mean, obs.cov = obs.list$obs.cov, IC = IC)
+PEcAn.assim.sequential::sda.enkf(settings, obs.mean = obs.list$obs.mean, obs.cov = obs.list$obs.cov, IC = IC)
 
 
 
