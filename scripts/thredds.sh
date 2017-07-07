3ad0e8820e446514810dee86568a7f6db1e4ebb1
Rob Kooper <kooper@illinois.edu>
2017-07-07 15:57:44 -0500

    Add cronjob, fix pull request
    Added a simple script to run a cron job. This will run the thredds script, dump the database and sync the database.

--- scripts/thredds.sh
+++ scripts/thredds.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# some variables (set in cron.sh)
+FQDN=${FQDN:-"$(hostname -f)"}
+MYSITE=${MYSITE:-"99"}
+MY_START_ID=${MY_START_ID:-"99000000001"}
+MY_LAST_ID=${MY_LAST_ID:-"99999999999"}
+DATABASE=${DATABASE:-"bety"}
+PG_OPT=${PG_OPT:-""}
+
 cat << EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <catalog name="THREDDS Server Default Catalog : You must change this to fit your server!"
@@ -24,22 +32,10 @@ cat << EOF
 
 EOF
 
-FQDN=$(hostname -f)
-HOST_ID=$(sudo -u postgres psql -d bety -t -c "SELECT id FROM machines WHERE hostname='${FQDN}';")
-HOST_START=$(sudo -u postgres psql -d bety -t -c "SELECT sync_start FROM machines WHERE hostname='${FQDN}';")
-HOST_START="${HOST_START//[[:space:]]/}"
-HOST_END=$(sudo -u postgres psql -d bety -t -c "SELECT sync_end FROM machines WHERE hostname='${FQDN}';")
-HOST_END="${HOST_END//[[:space:]]/}"
-if [ "${HOST_START}" == "" -o "${HOST_END}" == "" ]; then
-    HOST_ID=99
-    HOST_START="99000000001"
-    HOST_END="99999999999"
-fi
-
 # INPUTS
 echo '    <dataset name="Inputs" ID="inputs">'
 IFS=$'\n'
-for X in $(sudo -u postgres psql -d bety -t -c "SELECT dbfiles.id, file_path || '/' || file_name FROM dbfiles, inputs, formats WHERE dbfiles.id BETWEEN ${HOST_START} AND ${HOST_END} AND dbfiles.container_type='Input' AND dbfiles.container_id=inputs.id AND inputs.format_id=formats.id AND formats.mimetype_id=1093;"); do
+for X in $(sudo -u postgres psql ${PG_OPT} -d ${DATABASE} -t -c "SELECT dbfiles.id, file_path || '/' || file_name FROM dbfiles, inputs, formats WHERE dbfiles.id BETWEEN ${MY_START_ID} AND ${MY_LAST_ID} AND dbfiles.container_type='Input' AND dbfiles.container_id=inputs.id AND inputs.format_id=formats.id AND formats.mimetype_id=1093;"); do
     # remove leading whitespace, and extract information
     X="${X//[[:space:]]/}"
     ID="$(echo $X | cut -d "|" -f 1)"
@@ -66,7 +62,7 @@ echo '    </dataset>'
 
 # WORKFLOWS
 echo '    <dataset name="Workflows" ID="workflows">'
-for X in $(sudo -u postgres psql -d bety -t -c "SELECT id, folder FROM workflows WHERE workflows.id BETWEEN ${HOST_START} AND ${HOST_END};"); do
+for X in $(sudo -u postgres psql ${PG_OPT} -d ${DATABASE} -t -c "SELECT id, folder FROM workflows WHERE workflows.id BETWEEN ${MY_START_ID} AND ${MY_LAST_ID};"); do
     # remove leading whitespace, and extract information
     X="${X//[[:space:]]/}"
     WF_ID="$(echo $X | cut -d "|" -f 1)"
@@ -75,12 +71,12 @@ for X in $(sudo -u postgres psql -d bety -t -c "SELECT id, folder FROM workflows
 
     # create workflow
     echo "      <dataset name=\"workflow - ${WF_ID}\" ID=\"workflow_${WF_ID}\">"
-    for Y in $(sudo -u postgres psql -d bety -t -c "SELECT id, runtype FROM ensembles WHERE workflow_id=${WF_ID};"); do
+    for Y in $(sudo -u postgres psql ${PG_OPT} -d ${DATABASE} -t -c "SELECT id, runtype FROM ensembles WHERE workflow_id=${WF_ID};"); do
         Y="${Y//[[:space:]]/}"
         ENS_ID="$(echo $Y | cut -d "|" -f 1)"
         ENS_TYPE="$(echo $Y | cut -d "|" -f 2)"
         echo "        <dataset name=\"${ENS_TYPE} - ${ENS_ID}\" ID=\"ensembles_${ENS_ID}\">"
-        for RUN_ID in $(sudo -u postgres psql -d bety -t -c "SELECT id FROM runs WHERE ensemble_id=${ENS_ID};"); do
+        for RUN_ID in $(sudo -u postgres psql ${PG_OPT} -d ${DATABASE} -t -c "SELECT id FROM runs WHERE ensemble_id=${ENS_ID};"); do
             RUN_ID="${RUN_ID//[[:space:]]/}"
             echo "          <datasetScan name=\"run - ${RUN_ID}\" ID=\"runs_${RUN_ID}\" location=\"${WF_FOLDER}/out/${RUN_ID}\" path=\"${RUN_ID}\">"
             echo '            <metadata inherited="true">'
