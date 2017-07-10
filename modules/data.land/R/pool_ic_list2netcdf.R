349465d59fc6daf36859441446570b97477c0193
annethomas <annethomas239@gmail.com>
2017-07-10 09:34:05 -0400

    copy pool_ic_list2netcdf.R from branch pool-ic-workflow

---
+++ modules/data.land/R/pool_ic_list2netcdf.R
@@ -0,0 +1,56 @@
+##' @name pool_ic_list2netcdf
+##' @title pool_ic_list2netcdf
+##' @export
+##'
+##' @param input list with two elements: list of netcdf dimensions (dims, with named values) and list of variables (vals, with named values)
+##' @param outdir directory to write netcdf file
+##' @param siteid site id
+##' @author Anne Thomas
+
+pool_ic_list2netcdf <- function(input, outdir,siteid){
+  ##define dimensions available for netcdf
+  #assuming additional dim names aren't necessary; could use process similar to ncvars if so
+  lon <- ncdf4::ncdim_def("lon", "degrees_east", vals = input$dims$lon, 
+                          longname = "station_longitude")
+  lat <- ncdf4::ncdim_def("lat", "degrees_north", vals = input$dims$lat, 
+                          longname = "station_latitude")
+  depth <- ncdf4::ncdim_def(name = "depth", units = "cm", vals = input$dims$nsoil, 
+                            longname = "depth to bottom of layer")
+  
+  dims <- list(lon = lon, lat = lat, nsoil = depth) #nsoil soon to be replaced in standard table with depth
+  
+  #lookup table
+  #standard_vars <- data(mstmip_vars,package = "PEcAn.utils") #standard_vars replacing mstmip_vars; not yet merged
+  standard_vars <- read.csv("/fs/data3/aet4612/pecan/utils/data/mstmip_vars.csv",sep = ';')
+  ###function to lapply to all variables in input list and return a list of ncvars
+  to_ncvar <- function(varname,standard_vars,dims){
+    #print(varname)
+    var <- standard_vars[which(standard_vars$Variable.Name == varname),]
+    #add: check var exists
+    dimset <- var[,c("dim1","dim2","dim3","dim4")]
+    dim <- dims[which(names(dims) %in% dimset)] #subset list of all dims for this variable
+    #add: check that dim isn't 0
+    units = as.character(var$Units) #if the units are a factor the function fails
+    ncvar <- ncdf4::ncvar_def(name = varname, units = units, dim = dim, -999, prec = "double") #also add longname?
+    if (!is.na(var$Long.name)) {
+      ncvar$longname <- as.character(var$Long.name)
+    }
+    return(ncvar)
+  }
+  
+  ncvars = lapply(names(input$vals),to_ncvar,standard_vars,dims)
+  
+  #create nc file
+  str_ns <- paste0(siteid %/% 1e+09, "-", siteid %% 1e+09)
+  outfile <- file.path(outdir, paste0("IC_site_", str_ns,".nc"))
+  nc  <- ncdf4::nc_create(outfile, ncvars)
+  
+  #put variables in nc file
+  for (i in seq(ncvars)) {
+    varname <- ncvars[[i]]$name
+    ncdf4::ncvar_put(nc, ncvars[[i]], input$vals[[varname]])
+  }
+  
+  #close file
+  ncdf4::nc_close(nc)
+} #pool_ic_list2netcdf
