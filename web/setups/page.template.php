6c7dddbd7e95cdbd129de3059b7b7c903a7df3cf
Amanskywalker <ak47su30ac@gmail.com>
2017-07-16 14:43:32 +0530

    Improved the UI added bootstrap framework to make it responsive

--- web/setups/page.template.php
+++ web/setups/page.template.php
@@ -14,35 +14,48 @@
 <html lang="en">
 <head>
 <title>PEcAn Configurations</title>
-<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+<link rel="shortcut icon" type="image/x-icon" href="../favicon.ico" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 <link rel="stylesheet" type="text/css" href="../sites.css" />
+<!-- Fonts -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+
+<!-- Styles -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
+<style>
+    body {
+        font-family: 'Lato';
+    }
+
+    .fa-btn {
+        margin-right: 6px;
+    }
+</style>
 </head>
 <body>
 <div id="wrap">
     <div id="stylized">
-        <form id="formprev" method="POST" action="history.php">
-        </form>
-        <form id="formnext" method="POST" action="02-modelsite.php">
-            <h1>Introduction</h1>
-            <p>On this page you will find the required fields to setup the particular configurations or edit it.</p>
-
-            <div class="spacer"></div>
-        </form>
-<p>
-  <h1>List of available configurations</h1>
-  <a href="edit.php?key=database" >Database</a> <br>
-  <a href="edit.php?key=browndog" >Browndog</a> <br>
-  <a href="edit.php?key=fiadb" >FIA Database</a> <br>
-  <a href="edit.php?key=googleMapKey" >Google MapKey</a> <br>
-</p>
-<p>
-  <a href="https://pecan.gitbooks.io/pecan-documentation/content/" target="_blank">Documentation</a>
-<br>
-  <a href="https://gitter.im/PecanProject/pecan" target="_blank">Chat Room</a>
-<br>
-  <a href="https://github.com/PecanProject/pecan/issues/new" target="_blank">Bug Report</a>
-</p>
+      <h1>Introduction</h1>
+      <p>This is the Admin Pages.
+        <h1>List of available configurations</h1>
+        <a href="edit.php?key=database" >Database</a> <br>
+        <a href="edit.php?key=browndog" >Browndog</a> <br>
+        <a href="edit.php?key=fiadb" >FIA Database</a> <br>
+        <a href="edit.php?key=googleMapKey" >Google MapKey</a> <br>
+      </p>
+        <div class="checkbox disabled">
+          <h1>Automatic Sync</h1> <input type="checkbox" disabled data-toggle="toggle">
+        </div>
+      <p></p>
+      <p>
+        <a href="https://pecan.gitbooks.io/pecan-documentation/content/" target="_blank">Documentation</a>
+      <br>
+        <a href="https://gitter.im/PecanProject/pecan" target="_blank">Chat Room</a>
+      <br>
+        <a href="https://github.com/PecanProject/pecan/issues/new" target="_blank">Bug Report</a>
+      </p>
     </div>
     <div id="output">
