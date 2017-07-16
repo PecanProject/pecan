8f9d9e8cc4e8fa8ff313e30cef80539dddbe0b13
Amanskywalker <ak47su30ac@gmail.com>
2017-07-16 14:42:47 +0530

    Added script to update the user password via web interface

---
+++ web/setups/chpasswd.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Copyright (c) 2017 University of Illinois, NCSA.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the
+ * University of Illinois/NCSA Open Source License
+ * which accompanies this distribution, and is available at
+ * http://opensource.ncsa.illinois.edu/license.html
+ */
+
+// change password script location
+$shellscript = "sudo chpasswd.sh";
+
+$username = $_POST['username'];
+$oldpasswd = $_POST['oldpasswd'];
+$newpasswd = $_POST['newpasswd'];
+
+# used as a flag to determine the action to be taken according to the request
+$callchpasswd = false;
+
+if (isset($username) && isset($oldpasswd) && isset($newpasswd)) {
+  if (!empty($username) && !empty($oldpasswd) && !empty($newpasswd)) {
+    $callchpasswd = true;
+  }
+}
+
+// include the page template
+include 'page.template.php';
+
+if ($callchpasswd) {
+  // generate the command to execute
+  $cmd="$shellscript " . $username. " " . $newpasswd;
+  // execute the command
+  exec($cmd,$output,$status);
+
+  if ($status == 0) {
+    echo "password chaged sucessfully";
+  } else {
+    echo "ERROR : error changing password";
+    echo "<br>".$output;
+  }
+}else {
+?>
+<div class="container">
+    <div class="row">
+                <h1>Password Change</h1>
+                  <form class="form-horizontal" role="form" method="POST" action="add.php" enctype="multipart/form-data">
+                      <div class="form-group">
+                          <label for="username" class="col-md-4 control-label">Username :</label>
+                          <div class="col-md-6">
+                              <input id="username" type="text" class="form-control transparent-input" name="username" value="">
+                          </div>
+                      </div>
+
+                      <div class="form-group">
+                          <label for="password" class="col-md-4 control-label">old Password:</label>
+                          <div class="col-md-6">
+                              <input id="password" type="password" class="form-control transparent-input" name="oldpasswd" value="">
+                          </div>
+                      </div>
+
+                      <div class="form-group">
+                          <label for="password" class="col-md-4 control-label">New Password:</label>
+                          <div class="col-md-6">
+                              <input id="password" type="password" class="form-control transparent-input" name="newpasswd" value="">
+                          </div>
+                      </div>
+
+                      <div class="form-group">
+                          <label for="password" class="col-md-4 control-label">New Password (again):</label>
+                          <div class="col-md-6">
+                              <input id="password" type="password" class="form-control transparent-input" name="newpasswdre" value="">
+                          </div>
+                      </div>
+
+                     <div class="form-group">
+                         <div class="col-md-6 col-md-offset-4">
+                             <button type="submit" class="btn btn-primary">
+                                 <i class="fa fa-btn fa-user"></i> Update
+                             </button>
+                         </div>
+                     </div>
+                  </form>
+    </div>
+</div>
+
+<?php
+}
+
+include 'pagefooter.template.php';
+?>
