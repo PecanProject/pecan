890490d7dc1548ee39cd3554987aafac83426eeb
Amanskywalker <ak47su30ac@gmail.com>
2017-07-20 23:27:10 +0530

    Added instructions and link to change the user password

--- web/setups/page.template.php
+++ web/setups/page.template.php
@@ -45,6 +45,7 @@
         <a href="edit.php?key=browndog" >Browndog</a> <br>
         <a href="edit.php?key=fiadb" >FIA Database</a> <br>
         <a href="edit.php?key=googleMapKey" >Google MapKey</a> <br>
+        <a href="chpasswd.php" >Change Password</a> <br>
       </p>
         <div class="checkbox disabled">
           <h1>Automatic Sync</h1> <input type="checkbox" disabled data-toggle="toggle">
