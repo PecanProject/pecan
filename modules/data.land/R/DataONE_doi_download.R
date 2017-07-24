d3a87d338688d91079f657cfc543862c88ed3b4c
LiamBurke24 <liam.burke24@gmail.com>
2017-07-24 19:59:42 -0400

    Added Roxygen2 Format

--- modules/data.land/R/DataONE_doi_download.R
+++ modules/data.land/R/DataONE_doi_download.R
@@ -1,46 +1,54 @@
 ##' Functions to determine if data can be found by doi in R
 ##' Author: Liam Burke
-##' Code draws heavily on dataone r package for communication with the dataONE federation
+##' Code draws heavily on dataoneR package for communication with the DataONE federation 
 
-#--------------------------------------------------------------------------------#
-#          1. format.identifier -- convert doi or id into solrQuery format       #
-#--------------------------------------------------------------------------------#
-
+#' format.identifier
+#'
+#' @param id the doi or other identifier linked to the package in DataONE
+#'
+#' @return returns the id in the proper format for querying the DataONE Federation (using solrQuery syntax)
+#' @export 
+#'
+#' @author Liam P Burke, \email{lpburke@bu.edu}
+#' 
+#' @examples 
 format.identifier = function(id){ 
   doi.template <- 'id:"_"' # solr format
   doi1 <<- base::gsub("_", id, doi.template) # replace "_" with the doi or id and store in global environment
   return(doi1) 
 } # end function
 
-#--------------------------------------------------------------------------------#
-#            2. id.resolveable -- Is doi/ id available in dataONE?               #
-#--------------------------------------------------------------------------------#
 
-##' Arguments
-#' id: doi or dataone id 
-#' CNode: usually "PROD" 
-#' return_result: boolean that returns or suppresses result of query
-
+#' id.resolveable 
+#'
+#' @param id the doi or other identifier linked to the package in DataONE  
+#' @param CNode usually "PROD"
+#' @param return_result boolean that returns or suppresses result of query
+#'
+#' @return returns message indicating wether or not the id resolves to data in the DataONE federation
+#' @export
+#'
+#' @examples
 id.resolveable = function(id, CNode, return_result){
   format.identifier(id) # reformat the id in solr format
   
-  cn <- dataone::CNode("PROD") 
+  cn <- DataONE::CNode("PROD") 
   queryParams <- list(q=doi1, rows="5") 
-  result <- dataone::query(cn, solrQuery = queryParams, as = "data.frame") # return query results as a data.frame
+  result <- DataONE::query(cn, solrQuery = queryParams, as = "data.frame") # return query results as a data.frame
   
   if(return_result == TRUE){ # option that displays data.frame of query
     print(result) 
     
     if(is.null(result[1,1])){ # if there is no data available, result[1,1] will return a NULL value
-      return("doi does not resolve in the DataOne federation and therefore cannot be retrieved by doi. 
-             Either download this data locally and import using PEcAn's drag and drop feature, or search DataOne manually for another data identifier. Thank you for your patience.")
+      return("doi does not resolve in the DataONE federation and therefore cannot be retrieved by doi. 
+             Either download this data locally and import using PEcAn's drag and drop feature, or search DataONE manually for another data identifier. Thank you for your patience.")
     } else{
       return("data can be found in D1 federation")
     }
   }   else{ # option that does not display data.frame of query (return_result == FALSE)
         if(is.null(result[1,1])){ 
-        return("doi does not resolve in the DataOne federation and therefore cannot be retrieved by doi. 
-               Either download this data locally and import using PEcAn's drag and drop feature, or search DataOne manually for another data identifier (e.g. pid or resource_map) Thank you for your patience.")
+        return("doi does not resolve in the DataONE federation and therefore cannot be retrieved by doi. 
+               Either download this data locally and import using PEcAn's drag and drop feature, or search DataONE manually for another data identifier (e.g. pid or resource_map) Thank you for your patience.")
     }   else{
         return("data can be found in D1 federation")
     }
@@ -48,56 +56,56 @@ id.resolveable = function(id, CNode, return_result){
   
 } # end function
 
-#--------------------------------------------------------------------------------#
-#                         Get resource_map from doi                              #
-#--------------------------------------------------------------------------------#
-
-##' Arguments: 
-#' id: doi or dataone id 
-#' CNode: usually "PROD"
-
+#' get.resource.map
+#'
+#' @param id the doi or other identifier linked to the package in DataONE  
+#' @param CNode usually "PROD"
+#' 
+#' @return return the resource_map or a message indicating that there is no corresponding resource_map for the given id
+#' @export
+#'
+#' @examples
 get.resource.map = function(id, CNode){
-  cn <- dataone::CNode("PROD") 
-  locations <- dataone::resolve(cn, pid = id) 
+  cn <- DataONE::CNode("PROD") 
+  locations <- DataONE::resolve(cn, pid = id) 
   mnId <<- locations$data[1,"nodeIdentifier"] # store mnId in global environment
-  mn <<- dataone::getMNode(cn, mnId) # store mn in global environment
+  mn <<- DataONE::getMNode(cn, mnId) # store mn in global environment
   
   format.identifier(id) # format the identifier in solr Query format
   queryParamList <- list(q=doi1, fl="resourceMap") # custom query for the resourceMap
-  resource_map_df <- dataone::query(cn, solrQuery = queryParamList, as="data.frame") 
+  resource_map_df <- DataONE::query(cn, solrQuery = queryParamList, as="data.frame") 
   resource_map <<- resource_map_df[1,1] # store resource map in global env. resource map is always in resource_map_df[1,1]
   
   if (is.null(resource_map_df[1,1])){ # inform user if id/ doi has a corresponding resource_map or if this needs to be found manually
-    print("doi does not resolve a resource_map. Please manually search for the resource_map in DataONE search: https://search.dataone.org/#data")
+    print("doi does not resolve a resource_map. Please manually search for the resource_map in DataONE search: https://search.DataONE.org/#data")
   } else{
     print("Continue to next phase to complete download")
     return(resource_map)
   }
 } # end function
 
-#--------------------------------------------------------------------------------#
-#                     download package using resource_map                        #
-#--------------------------------------------------------------------------------#
-
-### Arguments: 
-#' resource_map: can be entered manually or can be called from the get.resource.map fn result 
-#' CNode: usually "PROD"
-#' download_format: format of download defaulted to "application/bagit-097" -- other possible formats unknown
-#' overwrite_directory: boolean
-#' directory: indicates the destination directory for the BagItFile
-
-
-
+#' download.packages
+#'
+#' @param resource_map the resource map that corresponds to the given data package
+#' @param CNode typically "PROD"
+#' @param download_format typically "application/bagit-097". Other possible formats currently unknown.
+#' @param overwrite_directory boolean that indicates whether or not the function should overwrite the directory
+#' @param directory location that download.packages places the data
+#'
+#' @return results of download
+#' @export
+#'
+#' @examples
 download.package.rm = function(resource_map, CNode, download_format = "application/bagit-097", 
                                overwrite_directory = TRUE, directory){
   # Finding the mnId (query)
-  cn <- dataone::CNode("PROD") 
-  locations <- dataone::resolve(cn, pid = resource_map) 
+  cn <- DataONE::CNode("PROD") 
+  locations <- DataONE::resolve(cn, pid = resource_map) 
   mnId <<- locations$data[1,"nodeIdentifier"]
   
   # download the bagitFile 
-  mn <<- dataone::getMNode(cn, mnId)
-  bagitFile <<- dataone::getPackage(mn, id = resource_map, format = download_format)
+  mn <<- DataONE::getMNode(cn, mnId)
+  bagitFile <<- DataONE::getPackage(mn, id = resource_map, format = download_format)
   bagitFile
   
 
