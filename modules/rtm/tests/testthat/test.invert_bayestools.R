c115bf81ac3b6994ab838a7c003f5526788adb81
Alexey Shiklomanov <ashiklom@bu.edu>
2017-08-02 16:59:52 -0400

    Cleanup of BayesianTools inversion in PEcAnRTM

--- modules/rtm/tests/testthat/test.invert_bayestools.R
+++ modules/rtm/tests/testthat/test.invert_bayestools.R
@@ -3,16 +3,20 @@ library(PEcAnRTM)
 library(testthat)
 context('Inversion using BayesianTools')
 
-true_params <- defparam('prospect_5')
-model <- function(x) prospect(x, 5)[,1]
-observed <- model(true_params) + generate.noise()
-prior <- prospect_bt_prior(5)
-custom_settings <- list()
-samples <- invert_bt(observed = observed, model = model, prior = prior,
-                     custom_settings = list())
+if (Sys.getenv('CI') == 'true') {
+    message('Skipping inversion tests on CI system')
+} else {
+    true_params <- defparam('prospect_5')
+    model <- function(x) prospect(x, 5)[,1]
+    observed <- model(true_params) + generate.noise()
+    prior <- prospect_bt_prior(5)
+    custom_settings <- list()
+    samples <- invert_bt(observed = observed, model = model, prior = prior,
+                         custom_settings = list())
 
-samples_burned <- PEcAn.assim.batch::autoburnin(BayesianTools::getSample(samples, coda = TRUE), method = 'gelman.plot')
-mean_estimates <- do.call(cbind, summary(samples_burned)[c('statistics', 'quantiles')])
+    samples_burned <- PEcAn.assim.batch::autoburnin(BayesianTools::getSample(samples, coda = TRUE), method = 'gelman.plot')
+    mean_estimates <- do.call(cbind, summary(samples_burned)[c('statistics', 'quantiles')])
 
-test_that('Mean estimates are within 10% of true values', 
-          expect_equal(true_params, mean_estimates[seq_along(true_params),'Mean'], tol = 0.1))
+    test_that('Mean estimates are within 10% of true values', 
+              expect_equal(true_params, mean_estimates[seq_along(true_params),'Mean'], tol = 0.1))
+}
