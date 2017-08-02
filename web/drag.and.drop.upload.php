573a9f1f6df65dfd18c82e217fab5bd40cdb2e08
LiamBurke24 <liam.burke24@gmail.com>
2017-08-02 12:03:17 -0400

    Updated comments from '#' to '/'

--- web/drag.and.drop.upload.php
+++ web/drag.and.drop.upload.php
@@ -23,28 +23,29 @@ if (get_page_acccess_level() > $min_upload_level) {
   exit;
 }
 
-# drag and drop window. 
-*Copyright (c) 2010 Remy Sharp, http://html5demos.com
+// drag and drop window. 
+/**
+ *Copyright (c) 2010 Remy Sharp, http://html5demos.com
  
-*Permission is hereby granted, free of charge, to any person obtaining
-*a copy of this software and associated documentation files (the
-*"Software"), to deal in the Software without restriction, including
-*without limitation the rights to use, copy, modify, merge, publish,
-*distribute, sublicense, and/or sell copies of the Software, and to
-*permit persons to whom the Software is furnished to do so, subject to
-*the following conditions:
+ *Permission is hereby granted, free of charge, to any person obtaining
+ *a copy of this software and associated documentation files (the
+ *"Software"), to deal in the Software without restriction, including
+ *without limitation the rights to use, copy, modify, merge, publish,
+ *distribute, sublicense, and/or sell copies of the Software, and to
+ *permit persons to whom the Software is furnished to do so, subject to
+ *the following conditions:
  
-*The above copyright notice and this permission notice shall be
-*included in all copies or substantial portions of the Software.
- 
-*THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-*EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-*MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-*NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-*LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-*OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-*WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+ *The above copyright notice and this permission notice shall be
+ *included in all copies or substantial portions of the Software.
+  
+ *THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ *LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ *OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ *WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 <title>Drag and drop, automatic upload</title>
 <style>
@@ -81,7 +82,7 @@ var holder = document.getElementById('holder'),
       progress: document.getElementById('progress')
     },
     
-    # This could be problematic: What generic mimetypes should we include? 
+    // This could be problematic: What generic mimetypes should we include? 
     acceptedTypes = {
       'image/png': true,
       'image/jpeg': true,
