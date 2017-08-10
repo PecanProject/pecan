ee73815c16ded1bcc77fd103e279f7002320b3a5
Amanskywalker <ak47su30ac@gmail.com>
2017-08-10 22:16:57 +0530

    Added corn job script

---
+++ web/setups/synccorn.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright (c) 2017 University of Illinois, NCSA.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the
+ * University of Illinois/NCSA Open Source License
+ * which accompanies this distribution, and is available at
+ * http://opensource.ncsa.illinois.edu/license.html
+ */
+
+// cornjob
+
+if (file_exists ("syncflag.txt") == true){
+  include 'sync.php';
+}
+
+?>
