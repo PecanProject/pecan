4599871ea287724337d1c657722ba6f794135257
Amanskywalker <ak47su30ac@gmail.com>
2017-08-18 21:59:52 +0530

    Added condition to check if the host_id is 99 then change it to 100 as it is reserved for the VM

--- web/setups/serversyncscript.php
+++ web/setups/serversyncscript.php
@@ -35,8 +35,33 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
 if ($row == false) {
   // means no data exist in database
-  $host_id = 25;
-  $id = 0;
+
+  // finding the max sync_id
+  $stmt = $pdo->prepare("SELECT max(sync_host_id) FROM machines;",array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+  if (!$stmt->execute()) {
+    echo json_encode(array('status' => 'ERROR',
+                          'errormessage' => 'Invalid query : [' . error_database() . ']'  . $pdo->errorInfo()));
+    die();
+  }
+  $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  $host_id = $row+1;
+
+  // if the host_id == 99 then changing it to 100 as 99 is reserved for the VM
+  if ($host_id == 99)
+    $host_id++;
+
+  // finding max id 
+  $stmt = $pdo->prepare("SELECT max(id) FROM machines;",array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+  if (!$stmt->execute()) {
+    echo json_encode(array('status' => 'ERROR',
+                          'errormessage' => 'Invalid query : [' . error_database() . ']'  . $pdo->errorInfo()));
+    die();
+  }
+  $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  $id = $row+1;
+
   $date = date("Y-m-d H:i:s");
   $stmt = $pdo->prepare("INSERT
     INTO machines (id, hostname, created_at, updated_at , sync_host_id, sync_url, sync_contact, sync_start, sync_end)
