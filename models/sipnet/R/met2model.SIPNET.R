3bdafb2dcf9042c60fc2fc0576b7de36bc8bd5b6
annethomas <annethomas239@gmail.com>
2017-08-22 19:46:56 -0400

    Debugging messages

--- models/sipnet/R/met2model.SIPNET.R
+++ models/sipnet/R/met2model.SIPNET.R
@@ -213,14 +213,17 @@ met2model.SIPNET <- function(in.path, in.prefix, outfolder, start_date, end_date
     
     ##filter out days not included in start or end date
     if(year == start_year){
+      print(start_date)
       extra.days <- length(as.Date(paste0(start_year, "-01-01")):as.Date(start_date)) #extra days length includes the start date
       if (extra.days > 1){
+        PEcAn.logger::logger.info("Subsetting SIPNET met to match start date")
         start.row <-  ((extra.days - 1) * 86400 / dt) + 1 #subtract to include start.date, add to exclude last half hour of day before
         tmp <- tmp[start.row:nrow(tmp),]
       }
     } else if (year == end_year){
       extra.days <- length(as.Date(end_date):as.Date(paste0(end_year, "-12-31"))) #extra days length includes the end date
       if (extra.days > 1){
+        PEcAn.logger::logger.info("Subsetting SIPNET met to match end date")
         end.row <-  nrow(tmp) - ((extra.days - 1) * 86400 / dt)  #subtract to include end.date
         tmp <- tmp[1:end.row,]
       }
