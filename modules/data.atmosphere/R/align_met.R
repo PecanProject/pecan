737234abf5721472b6215df39bdfa9d5c00776a4
Christy Rollinson <crollinson@gmail.com>
2017-08-22 12:12:09 -0500

    Initial De-biasing scripts
    Translation of PalEON debiasing (spatial downscaling & bias-correction)
    workflow into scripts that can be used throughout Pecan!
    
    align.met function takes a training dataset and makes the dataset to be
    downscaled on the same temporal resolution.  This *should* work for
    both this bias correction step & the temporal downscaling.
    
    debias.met.regression is a beast of a function that does the bias
    correction and attempts to take into account the covariance among
    parameters by doing variables in a specific order and then adding
    predictors/complexity as we go along.  There are special cases for if
    we’re working with two empirical(-ish) datasets where we can pair the
    anomalies as well as some additional flags for CRUNCEP.  There are some
    improvements that can/will be made (e.g. possibly transforming
    zero-truncated variables when used as predictors & not just responses),
    but this is what I have so far.

--- modules/data.atmosphere/R/align_met.R
+++ modules/data.atmosphere/R/align_met.R
@@ -16,25 +16,205 @@
 # -----------------------------------
 ##' @param train.path - path to the dataset to be used to downscale the data
 ##' @param source.path - data to be bias-corrected aligned with training data (from align.met)
-##' @param pair.mems - logical stating whether ensemble members should be paired
+##' @param yrs.train - (optional) specify a specific years to be loaded for the training data; 
+##'                    prevents needing to load the entire dataset.  If NULL, all available years 
+##'                    will be loaded. If not null, should be a vector of numbers (so you can skip
+##'                    problematic years)
 ##' @param n.ens  - number of ensemble members to generate and save
+##' @param pair.mems - logical stating whether ensemble members should be paired
+##' @param seed - specify seed so that random draws can be reproduced
 ##' @param verbose
 ##' @export
 # -----------------------------------
 # Workflow
 # -----------------------------------
-# 1. Determine if training data is a single series or ensemble
-#    - note: assumes training data is at the temporal resolution you want to work with
-# 2. If not already, coerce training data into the size of the desired output ensemble
-# 3. Aggregate or coerce source data into temporal resolution of training data
-#    - note: source data should remain a single time series
-# 4. export data (stored in memory) for input into the debiasing or temporal downscaling workflow
+# 1. Read in & format the training data
+#    1.1. Determine if training data is a single series or ensemble
+#       - note: assumes training data is at the temporal resolution you want to work with
+#    1.2. If not already, coerce training data into the size of the desired output ensemble
+# 2. Read in & format the source data to match the temporal resolution of training data
+#    - Note: for now this is only going to work with a single time series & not an ensemble of source data
+#    - Note: end dimensions should match that of the training data
+# 3. export data (stored in memory) for input into the debiasing or temporal downscaling workflow
+# 
+# Returns a list called met.out with 2 levels that are matched in temporal resolution & number of ensembles
+# List Layers
+#  1. dat.train
+#  2. dat.source
+# Sublist Layers: time, met variables
+# -----------------------------------
+# Notes
+# -----------------------------------
+# 1. This assumes that both the trian and source data are in *at least* daily resolution
 # -----------------------------------
 
 #----------------------------------------------------------------------
 # Begin Function
 #----------------------------------------------------------------------
-align.met <- function(train.path, source.path, pair.mems = TRUE, verbose = FALSE) {
-  # 
+align.met <- function(train.path, source.path, yrs.train=NULL, n.ens=1, pair.mems = TRUE, seed=Sys.Date(), verbose = FALSE) {
+  # Load required libraries
+  library(ncdf4)
+  library(lubridate)
+  
+  met.out <- list() # where the aligned data will be stored
+
+  # ---------------
+  # 1. Read in & organize training data
+  # ---------------
+  met.out[["dat.train"]] <- list()
+  # 1.a. Determine if we have an ensemble in the training path or if it's a single time series
+  if(length(dir(train.path, ".nc"))>0){ # we have a single time series
+    n.trn = 1 # Ignore how many input ensemble members we asked for, we only actually have 1 here
+    
+    files.train <- dir(train.path, ".nc")
+    
+    yrs.file <- strsplit(files.train, "[.]")
+    yrs.file <- matrix(unlist(yrs.file), ncol=length(yrs.file[[1]]), byrow=T)
+    yrs.file <- as.numeric(yrs.file[,2])
+    
+    if(!is.null(yrs.train)){
+      files.train <- files.train[which(yrs.file %in% yrs.train)]
+      yrs.file <- yrs.file[which(yrs.file %in% yrs.train)]
+    }
+    
+    # Loop through the .nc files putting everything into a list
+    print("Processing Training Data")
+    pb <- txtProgressBar(min=0, max=length(files.train), style=3)
+    for(i in 1:length(files.train)){
+      yr.now <- yrs.file[i]
+      
+      ncT <- nc_open(file.path(train.path, files.train[i]))
+      
+      # Set up the time data frame to help index
+      nday <- ifelse(leap_year(yr.now), 366, 365)
+      ntime <- length(ncT$dim$time$vals)
+      step.day <- nday/ntime
+      step.hr  <- step.day*24
+      stamps.hr <- seq(step.hr/2, by=step.hr, length.out=1/step.day) # Time stamps centered on period
+      
+      # Create a data frame with all the important time info
+      # center the hour step
+      df.time <- data.frame(Year=yr.now, DOY=rep(1:nday, each=1/step.day), Hour=rep(stamps.hr, ntime))
+      df.time$Date <- strptime(paste(df.time$Year, df.time$DOY, df.time$Hour, sep="-"), format=("%Y-%j-%H"), tz="UTC")
+      met.out$dat.train[["time"]] <- rbind(met.out$dat.train$time, df.time)
+      
+      # Extract the met info, making matrices with the appropriate number of ensemble members
+      for(v in names(ncT$var)){
+        df.tem <- matrix(rep(ncvar_get(ncT, v), n.trn), ncol=n.trn, byrow=F)
+        
+        met.out$dat.train[[v]] <- rbind(met.out$dat.train[[v]], df.tem)
+      }
+      
+      nc_close(ncT)
+      
+      setTxtProgressBar(pb, i)
+    } # End looping through training data files
+  } else { # we have an ensemble we need to deal with
+    stop("Training ensemble mode not implemented yet!")
+  } # End loading & formatting training data
+  print(" ")
+  # ---------------
+  
+  # ---------------
+  # Read in & format the source data
+  # ---------------
+  met.out[["dat.source"]] <- list()
+  if(length(dir(source.path, ".nc"))>0){ # we have a single time series
+    n.src = 1 # we only have 1 time series so
+    
+    # Get a list of the files we'll be downscaling
+    files.source <- dir(source.path, ".nc")
+    
+    # create a vector of the years
+    yrs.file <- strsplit(files.source, "[.]")
+    yrs.file <- matrix(unlist(yrs.file), ncol=length(yrs.file[[1]]), byrow=T)
+    yrs.file <- as.numeric(yrs.file[,2])
+    
+    # Getting the day & hour timesteps from the training data
+    day.train <- round(365/length(unique(met.out$dat.train$time$DOY)))
+    hr.train  <- 24/length(unique(met.out$dat.train$time$Hour))
+  
+    # Loop through the .nc files putting everything into a list
+    print("Processing Source Data")
+    pb <- txtProgressBar(min=0, max=length(files.train), style=3)
+    for(i in 1:length(files.source)){
+      yr.now <- yrs.file[i]
+      
+      ncT <- nc_open(file.path(source.path, files.source[i]))
+      
+      # Set up the time data frame to help index
+      nday <- ifelse(leap_year(yr.now), 366, 365)
+      ntime <- length(ncT$dim$time$vals)
+      step.day <- nday/ntime
+      step.hr  <- step.day*24
+      
+      # -----
+      # Making time stamps to match the training data
+      # For coarser time step than the training data, we'll duplicate in the loop
+      # -----
+      # Making what the unique time stamps should be to match the training data
+      stamps.hr <- seq(hr.train/2, by=hr.train, length.out=1/day.train) 
+     
+      if(step.hr < hr.train){  # Finer hour increment --> set it up to aggregate
+        align = "aggregate"
+        stamps.src <- rep(stamps.hr, each=24/step.hr)
+      } else if(step.hr > hr.train) { # Set the flag to duplicate the data
+        align = "repeat"
+      } else { # things are aligned, so we're fine
+        align = "aligned"
+      }
+      # -----
+      
+      # Create a data frame with all the important time info
+      # center the hour step
+      df.time <- data.frame(Year=yr.now, DOY=rep(1:nday, each=1/day.train), Hour=rep(stamps.hr, length.out=nday/day.train))
+      df.time$Date <- strptime(paste(df.time$Year, df.time$DOY, df.time$Hour, sep="-"), format=("%Y-%j-%H"), tz="UTC")
+      met.out$dat.source[["time"]] <- rbind(met.out$dat.source$time, df.time)
+  
+      src.time <- data.frame(Year=yr.now, DOY=rep(1:nday, each=1/step.day), Hour=rep(stamps.src, length.out=ntime))
+      src.time$Date <- strptime(paste(df.time$Year, df.time$DOY, df.time$Hour, sep="-"), format=("%Y-%j-%H"), tz="UTC")
+      
+      # Extract the met info, making matrices with the appropriate number of ensemble members
+      for(v in names(ncT$var)){
+        dat.tem <- ncvar_get(ncT, v)
+        
+        if(align=="repeat"){ # if we need to coerce the time step to be repeated to match temporal resolution, do it here
+          dat.tem <- rep(dat.temp, each=stamps.hr)
+        }
+        df.tem <- matrix(rep(dat.tem, n.src), ncol=n.src, byrow=F)
+        
+        # If we need to aggregate the data to align it, do it now to save memory
+        if(align == "aggregate"){
+          df.tem <- cbind(src.time, data.frame(df.tem))
+  
+          df.agg <- aggregate(df.tem[,(4+1:n.src)], by=df.tem[,c("Year", "DOY", "Hour")], FUN=mean)
+          met.out$dat.source[[v]] <- rbind(met.out$dat.source[[v]], as.matrix(df.agg[,(3+1:n.src)]))
+          
+          # if workign wiht air temp, also find the max & min
+          if(v=="air_temperature"){
+            tmin <- aggregate(df.tem[,(4+1:n.src)], by=df.tem[,c("Year", "DOY", "Hour")], FUN=min)
+            tmax <- aggregate(df.tem[,(4+1:n.src)], by=df.tem[,c("Year", "DOY", "Hour")], FUN=max)
+            
+            met.out$dat.source[["air_temperature_minimum"]] <- rbind(met.out$dat.source[["air_temperature_minimum"]], as.matrix(tmin[,(3+1:n.src)]))
+            met.out$dat.source[["air_temperature_maximum"]] <- rbind(met.out$dat.source[["air_temperature_maximum"]], as.matrix(tmax[,(3+1:n.src)]))
+          } 
+        } 
+        
+        # If met doesn't need to be aggregated, just copy it in
+        if(align %in% c("repeat", "align")) {
+          met.out$dat.source[[v]] <- rbind(met.out$dat.source[[v]], as.matrix(df.tem, ncol=n.src))
+        }
+      }
+      nc_close(ncT)
+      setTxtProgressBar(pb, i)
+    } # End looping through source met files
+    print("")
+  } else { # we have an ensemble we need to deal with
+    stop("Source ensemble mode not implemented yet!")
+  } # End loading & formatting training data
+  # ---------------
+  
+  
+  return(met.out)
 }
   
