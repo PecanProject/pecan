6b0631b9470e818fae888c5f7541dc4e63e8fae5
Tobey Carman <tobeycarman@gmail.com>
2017-08-23 17:28:08 -0500

    Add empty model2netcdf function/file

---
+++ models/dvmdostem/R/model2netcdf.dvmdostem.R
@@ -0,0 +1,28 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2016 NCSA.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the 
+# University of Illinois/NCSA Open Source License
+# which accompanies this distribution, and is available at
+# http://opensource.ncsa.illinois.edu/license.html
+#-------------------------------------------------------------------------------
+
+##' @name model2netcdf.dvmdostem
+##' @title Code to convert dvmdostem netcdf output into into CF standard
+##'
+##' @param outdir Location of dvmdostem model output
+##' 
+##' @examples  
+##' \dontrun{
+##' # example code here?
+##' }
+##' 
+##' @export
+##'
+##' @author Tobey Carman, Shawn Serbin
+##' @importFrom ncdf4 ncdim_def ncvar_def ncatt_get ncvar_add
+model2netcdf.dvmdostem <- function(outdir) {
+  
+} # model2netcdf.dvmdostem
+
+
