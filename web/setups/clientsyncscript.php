19cc0a9b9f596897f9522a92a6563b4037d85baa
Amanskywalker <ak47su30ac@gmail.com>
2017-08-23 22:13:29 +0530

    Typo fix in clientsyncscript.php

--- web/setups/clientsyncscript.php
+++ web/setups/clientsyncscript.php
@@ -32,7 +32,7 @@ $curl_response = curl_exec($curl);
 if ($curl_response === false) {
     $info = curl_getinfo($curl);
     curl_close($curl);
-    die('error occured during curl exec. Additioanl info: ' . var_export($info));
+    die('error occured during curl exec. Additional info: ' . var_export($info));
 }
 
 // close curl
