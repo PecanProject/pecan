2798a083af20ad91b645c09fa8e01d367e28ce1e
Tony Gardella <tonygard@bu.edu>
2017-08-30 15:13:11 -0400

    switch cime output variable

--- models/fates/inst/create_1x1_ref_case.sh
+++ models/fates/inst/create_1x1_ref_case.sh
@@ -59,8 +59,8 @@ echo "*** Modifying xmls  ***"
 ./xmlchange -file env_mach_pes.xml -id MAX_TASKS_PER_NODE -val 1
 ./xmlchange -file env_mach_pes.xml -id TOTALPES -val 1
 # Modifying : env_build.xml
-#Updated variable of CESMSCRATCHROOT?#./xmlchange -file env_build.xml -id CIME_OUTPUT_ROOT -val ${CASEROOT}
-./xmlchange -file env_build.xml -id CESMSCRATCHROOT -val ${CASEROOT}
+./xmlchange -file env_build.xml -id CIME_OUTPUT_ROOT -val ${CASEROOT}
+#Outdated Var ./xmlchange -file env_build.xml -id CESMSCRATCHROOT -val ${CASEROOT}
 ./xmlchange -file env_build.xml -id GMAKE -val make
 #./xmlchange -file env_build.xml -id MPILIB -val openmpi
 #./xmlchange -file env_build.xml -id OS -val Linux
