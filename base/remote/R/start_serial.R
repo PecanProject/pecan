563770edc978f2208ad54c8bc84b4f9a6059493b
Alexey Shiklomanov <ashiklom@bu.edu>
2017-09-14 11:32:14 -0500

    Remote bugfixes, and some met2model.SIPNET cleanup

--- base/remote/R/start_serial.R
+++ base/remote/R/start_serial.R
@@ -4,7 +4,7 @@
 #'
 #' @return Output of execution command, as a character (see [remote.execute.cmd()]).
 #' @export
-start_serial <- function(host, rundir, host_rundir, job_script) {
+start_serial <- function(run, host, rundir, host_rundir, job_script) {
   run_id_string <- format(run, scientific = FALSE)
   if (is.localhost(host)) {
     out <- system2(file.path(rundir, run_id_string, job_script), stdout = TRUE, stderr = TRUE)
