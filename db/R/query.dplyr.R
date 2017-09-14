f0f6ef2dd7c9d01b9ff65acae89b90e23dba786b
LiamBurke24 <liam.burke24@gmail.com>
2017-09-14 15:17:34 -0400

    updated `query.dyplr.R` to use read_web_config (per alexey's request to not have duplicate code

--- db/R/query.dplyr.R
+++ db/R/query.dplyr.R
@@ -1,17 +1,10 @@
 #' Connect to bety using current PEcAn configuration
 #' @param php.config Path to `config.php`
 #' @export
+#' 
 betyConnect <- function(php.config = "../../web/config.php") {
   ## Read PHP config file for webserver
-  config <- scan(php.config, what = "character", sep = "\n")
-  config <- config[grep("^\\$", config)]  ## find lines that begin with $ (variables)
-  config <- sub("$", "", config, fixed = TRUE)  ## remove $
-  config <- sub(";", "", config, fixed = TRUE)  ## remove ;
-  config <- sub("false", "FALSE", config, fixed = TRUE)  ##  Boolean capitalization
-  config <- sub("true", "TRUE", config, fixed = TRUE)  ##  Boolean capitalization
-  config <- config[-grep("$", config, fixed = TRUE)]  ## lines with variable references fail
-  config <- config[-grep("exec", config, fixed = TRUE)]  ## lines 'exec' fail
-  config.list <- eval(parse(text = paste("list(", paste0(config[1:14], collapse = ","), ")")))
+  config.list <- PEcAn.utils::read_web_config(php.config)
   
   ## Database connection
   src_postgres(dbname = config.list$db_bety_database, 
