efa0138f6a5fcf6068f5611581a4ea7174f82e3d
Tony Gardella <tonygard@bu.edu>
2017-10-23 15:34:41 -0400

    delte slash

--- modules/meta.analysis/tests/testthat/test.approx.posterior.R
+++ modules/meta.analysis/tests/testthat/test.approx.posterior.R
@@ -9,7 +9,7 @@
 context("test approx.posterior function")
 
 data("trait.mcmc", package = "PEcAn.utils")
-load("/data/prior.distns.RData")
+load("data/prior.distns.RData")
 
 test_that("test data are as expected", {
   expect_equal(names(trait.mcmc),
