7ce8fe9b95d5785291cb169c57b71be98c060121
araiho <ann.raiho@gmail.com>
2017-11-18 09:26:22 -0500

    saving changes from pecan2 here while pecan2 runs sda run

--- modules/assim.sequential/R/sda.enkf.R
+++ modules/assim.sequential/R/sda.enkf.R
@@ -174,11 +174,11 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
     
     ## Write Configs
     do.call(what = my.write.config, args = list(defaults = NULL, 
-                                         trait.values = params[[i]], 
-                                         settings = settings, 
-                                         run.id = run.id[[i]], 
-                                         inputs = inputs[[i]], 
-                                         IC = IC[i, ]))
+                                                trait.values = params[[i]], 
+                                                settings = settings, 
+                                                run.id = run.id[[i]], 
+                                                inputs = inputs[[i]], 
+                                                IC = IC[i, ]))
     
     ## write a README for the run
     cat("runtype     : sda.enkf\n",
@@ -211,7 +211,7 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
   PEcAn.remote::start.model.runs(settings, settings$database$bety$write)
   save(list = ls(envir = environment(), all.names = TRUE), 
        file = file.path(outdir, "sda.initial.runs.Rdata"), envir = environment())
-
+  
   
   
   ###-------------------------------------------------------------------###
@@ -298,10 +298,10 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
     X[1:N]  ~ dmnorm(X.mod[1:N], prec = q[1:N,1:N])
     
     #agb linear
-    #y_star[1:N,1:N] <- X[1:N,1:N] #[choose]
+    #y_star[1:YN,1:YN] <- X[1:YN,1:YN] #[choose]
     
     #f.comp non linear
-    #y_star <- X[1:9] / sum(X[1:9])
+    #y_star[1:YN] <- X[1:YN] / sum(X[1:YN])
     
     ## Analysis
     y.censored[1:YN] ~ dmnorm(X[1:YN], prec = r[1:YN,1:YN]) #is it an okay assumpution to just have X and Y in the same order?
@@ -310,7 +310,7 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
     #remove y.censored samplers and only assign univariate samplers on NAs
     
     for(i in 1:YN){
-      y.ind[i] ~ dconstraint(y.censored[i] > 0)
+      y.ind[i] ~ dinterval(y.censored[i], 0)
     }
     
   })
@@ -319,7 +319,7 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
     for(i in 1:N){
       y.censored[i,1:J] ~ dmnorm(muf[1:J], cov = pf[1:J,1:J])
       for(j in 1:J){
-        y.ind[i,j] ~ dconstraint(y.censored[i,j] > 0)
+        y.ind[i,j] ~ dinterval(y.censored[i,j], 0)
       }
     }
     
@@ -345,7 +345,7 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
   ###-------------------------------------------------------------------###
   ### loop over time                                                    ###
   ###-------------------------------------------------------------------###  
-  for(t in seq_len(nt)) {
+  for(t in 3:50) {
     
     ###-------------------------------------------------------------------###
     ### read restart                                                      ###
@@ -516,7 +516,7 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
                                           name = 'space')
           ## Adding X.mod,q,r as data for building model.
           conf_tobit2space <- configureMCMC(tobit2space_pred, thin = 10, print=TRUE)
-          conf_tobit2space$addMonitors(c("pf", "muf")) 
+          conf_tobit2space$addMonitors(c("pf", "muf","y.censored")) 
           ## [1] conjugate_dmnorm_dmnorm sampler: X[1:5]
           ## important!
           ## this is needed for correct indexing later
@@ -548,22 +548,28 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
         }else{
           Cmodel_tobit2space$y.ind <- x.ind
           Cmodel_tobit2space$y.censored <- x.censored
-          Cmodel_tobit2space$lambda_0 <- diag(10,length(mu.f)) #enkf.params[[t-1]]$Pa
-          Cmodel_tobit2space$mu_0 <- rep(0,length(mu.f)) #enkf.params[[t-1]]$mu.a
+          #Cmodel_tobit2space$lambda_0 <- diag(10,length(mu.f)) #enkf.params[[t-1]]$Pa
+          #Cmodel_tobit2space$mu_0 <- rep(0,length(mu.f)) #enkf.params[[t-1]]$mu.a
+          
+          inits.tobit2space = list(pf = Pf, muf = colMeans(X))
+          
+          Cmodel_tobit2space$setInits(inits.tobit2space)
           
           for(i in seq_along(X)) {
-              ## ironically, here we have to "toggle" the value of y.ind[i]
-              ## this specifies that when y.ind[i] = 1,
-              ## indicator variable is set to 0, which specifies *not* to sample
-              valueInCompiledNimbleFunction(Cmcmc_tobit2space$samplerFunctions[[samplerNumberOffset_tobit2space+i]], 'toggle', 1-x.ind[i])
+            ## ironically, here we have to "toggle" the value of y.ind[i]
+            ## this specifies that when y.ind[i] = 1,
+            ## indicator variable is set to 0, which specifies *not* to sample
+            valueInCompiledNimbleFunction(Cmcmc_tobit2space$samplerFunctions[[samplerNumberOffset_tobit2space+i]], 'toggle', 1-x.ind[i])
           }
           
         }
-         
+        
         set.seed(0)
         dat.tobit2space <- runMCMC(Cmcmc_tobit2space, niter = 50000, progressBar=TRUE)
         
-        assessParams(dat = dat.tobit2space[1000:5000,], Xt = X, mu_f_TRUE = colMeans(X), P_f_TRUE = Pf)
+        pdf(file.path(outdir,paste0('assessParams',t,'.pdf')))
+        assessParams(dat = dat.tobit2space[1000:5000,], Xt = X)
+        dev.off()
         
         ## update parameters
         dat.tobit2space  <- dat.tobit2space[1000:5000, ]
@@ -572,11 +578,13 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
         iPf   <- grep("pf", colnames(dat.tobit2space))
         Pf <- matrix(colMeans(dat.tobit2space[, iPf]),ncol(X),ncol(X))
         
+        iycens <- grep("y.censored",colnames(dat.tobit2space))
+        X.new <- matrix(colMeans(dat.tobit2space[,iycens]),nens,ncol(X))
+        
         #plot(dat.tobit2space[,16])
         
+        #if(sum(diag(Pf)-diag(cov(X))) > 10 | sum(diag(Pf)-diag(cov(X))) < -10) logger.severe('Increase Sample Size')
         
-        if(sum(diag(Pf)-diag(cov(X))) > 10 | sum(diag(Pf)-diag(cov(X))) < -10) logger.severe('Increase Sample Size')
-      
         ###-------------------------------------------------------------------###
         ### Generalized Ensemble Filter                                       ###
         ###-------------------------------------------------------------------###
@@ -612,7 +620,7 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
           constants.tobit = list(N = ncol(X), YN = length(y.ind)) #, nc = 1
           dimensions.tobit = list(X = ncol(X), X.mod = ncol(X), Q = c(ncol(X),ncol(X))) #  b = dim(inits.pred$b),
           
-          data.tobit = list(muf = as.vector(mu.f), pf = Pf, aq = aqq[t,,], bq = bqq[t],
+          data.tobit = list(muf = as.vector(mu.f), pf = solve(Pf), aq = aqq[t,,], bq = bqq[t],
                             y.ind = y.ind,
                             y.censored = y.censored,
                             r = solve(R))
@@ -660,7 +668,7 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
           Cmodel$aq <- aqq[t,,]
           Cmodel$bq <- bqq[t]
           Cmodel$muf <- mu.f
-          Cmodel$pf <- Pf
+          Cmodel$pf <- solve(Pf)
           Cmodel$r <- solve(R)
           
           for(i in 1:length(y.ind)) {
@@ -730,8 +738,13 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
         
         aqq[t + 1, , ]   <- V
         bqq[t + 1]       <- n
+        par(mfrow=c(1,1))
+        plot(bqq[1:(t+1)])
         enkf.params[[t]] <- list(mu.f = mu.f, Pf = Pf, mu.a = mu.a, 
                                  Pa = Pa, q.bar = q.bar, n = n)
+        
+        var.df1 <- data.frame(diag(Pa),diag(Pf),diag(R),diag(solve(q.bar)))
+        var.df1[order(var.df1$diag.Pa.),]
       }
       
     } else {
@@ -765,7 +778,7 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
       ## normalize
       Z <- X*0
       for(i in seq_len(nens)){
-        Z[i,] <- 1/sqrt(L_f) * t(V_f)%*%(X[i,]-mu.f)
+        Z[i,] <- 1/sqrt(L_f) * t(V_f)%*%(X.new[i,]-mu.f)
       }
       Z[is.na(Z)]<-0
       
@@ -790,6 +803,10 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
     
     colnames(analysis) <- colnames(X)
     
+    hist(rnorm(1000,colMeans(X)[12],sd = sqrt(cov(X)[12,12])),freq=F,xlim=c(-.05,5))
+    lines(density(rnorm(1000,Y[12],sd = sqrt(R[12,12]))),col='green',lwd=2)
+    lines(density(rnorm(1000,mu.a[12],sd = sqrt(Pa[12,12]))),col='pink',lwd=2)
+    
     ##### Mapping analysis vectors to be in bounds of state variables
     if(processvar==TRUE){
       for(i in 1:ncol(analysis)){
@@ -805,7 +822,7 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
     new.params <- params
     
     ANALYSIS[[t]] <- analysis
-    
+    #pdf('ts-tree.rings-sda.pdf')
     if (interactive() & t > 1) { #
       t1 <- 1
       names.y <- unique(unlist(lapply(obs.mean[t1:t], function(x) { names(x) })))
@@ -818,18 +835,21 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
       }))
       
       if(any(obs)){
-      Y.order <- na.omit(pmatch(colnames(X), colnames(Ybar)))
-      Ybar <- Ybar[,Y.order]
-      Ybar[is.na(Ybar)] <- 0
-      YCI <- t(as.matrix(sapply(obs.cov[t1:t], function(x) {
-        if (length(x)<2) {
-          rep(NA, length(names.y))
-        }
-        sqrt(diag(x))
-      })))
-      
-      YCI <- YCI[,Y.order]
-      YCI[is.na(YCI)] <- 0
+        Y.order <- na.omit(pmatch(colnames(X), colnames(Ybar)))
+        Ybar <- Ybar[,Y.order]
+        Ybar[is.na(Ybar)] <- 0
+        Ybar <- Ybar
+        
+        YCI <- t(as.matrix(sapply(obs.cov[t1:t], function(x) {
+          if (length(x)<2) {
+            rep(NA, length(names.y))
+          }
+          sqrt(diag(x))
+        })))
+        
+        YCI <- YCI[,Y.order]
+        YCI[is.na(YCI)] <- 0
+        
       }else{
         YCI <- matrix(NA,nrow=length(t1:t), ncol=length(names.y))
       }
@@ -883,7 +903,7 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
         legend('topright',c('Forecast','Data','Analysis'),col=c(alphablue,alphagreen,alphapink),lty=1,lwd=5)
       }
     }
-    
+    #dev.off()
     ###-------------------------------------------------------------------###
     ### forecast step                                                     ###
     ###-------------------------------------------------------------------### 
@@ -975,14 +995,21 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
   
   YCI <- YCI[,Y.order]
   Xsum <- plyr::laply(FORECAST, function(x) { mean(rowSums(x[,1:length(names.y)], na.rm = TRUE)) })[t1:t]
+  Xasum <- plyr::laply(ANALYSIS, function(x) { mean(rowSums(x[,1:length(names.y)], na.rm = TRUE)) })[t1:t]
   
   for (i in seq_len(ncol(X))) {
     Xbar <- plyr::laply(FORECAST[t1:t], function(x) { mean(x[, i], na.rm = TRUE) })
     Xci <- plyr::laply(FORECAST[t1:t], function(x) { quantile(x[, i], c(0.025, 0.975)) })
     Xci[is.na(Xci)]<-0
     
+    Xbar <- Xbar
+    Xci <- Xci
+    
     Xa <- plyr::laply(ANALYSIS[t1:t], function(x) { mean(x[, i]) })
     XaCI <- plyr::laply(ANALYSIS[t1:t], function(x) { quantile(x[, i], c(0.025, 0.975)) })
+    
+    Xa <- Xa
+    XaCI <- XaCI
     
     plot(as.Date(obs.times[t1:t]),
          Xbar, 
@@ -1014,6 +1041,7 @@ sda.enkf <- function(settings, obs.mean, obs.cov, IC = NULL, Q = NULL, adjustmen
     legend('topright',c('Forecast','Data','Analysis'),col=c(alphablue,alphagreen,alphapink),lty=1,lwd=5)
     
   }
+  
   dev.off()
   ###-------------------------------------------------------------------###
   ### bias diagnostics                                                  ###
