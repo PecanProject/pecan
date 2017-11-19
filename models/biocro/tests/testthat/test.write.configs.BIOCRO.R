1c9efc4110c718a9811276674205a2f64838d6a7
Chris Black <chris@ckblack.org>
2017-11-19 18:13:42 -0500

    update write.config.BIOCRO tests for BioCro 1.0 support

--- models/biocro/tests/testthat/test.write.configs.BIOCRO.R
+++ models/biocro/tests/testthat/test.write.configs.BIOCRO.R
@@ -10,18 +10,31 @@ samples <- list(biocro.saof = (data.frame(
   stomatal_slope.BB = c(2.7, 3.3, 3.9),
   row.names = c("15.866", "50", "84.134"))))
 
-test_that("convert.samples.BIOCRO works", {
-  biocro.parms <- convert.samples.BIOCRO(samples$biocro.saof)
+test_that("convert.samples.BIOCRO works for BioCro 0.9", {
+  biocro.parms <- convert.samples.BIOCRO(samples$biocro.saof, 0.9)
   expect_equal(dim(biocro.parms), dim(samples$biocro.saof))
+  expect_null(biocro.parms[["vmax1"]])
   expect_equal(biocro.parms$vmax, samples$biocro.saof$Vcmax)
   expect_equal(biocro.parms$b0, samples$biocro.saof$cuticular_cond/1e+06)
   expect_equal(biocro.parms$SLA, samples$biocro.saof$SLA)
   expect_equal(biocro.parms$Rd, samples$biocro.saof$leaf_respiration_rate_m2)
   expect_equal(biocro.parms$b1, samples$biocro.saof$stomatal_slope.BB)
+})
+
+test_that("convert.samples.BIOCRO works for BioCro 1.0", {
+  biocro.parms <- convert.samples.BIOCRO(samples$biocro.saof, 1.0)
+  expect_equal(dim(biocro.parms), dim(samples$biocro.saof))
+  expect_null(biocro.parms[["vmax"]]) # [[ instead of $ to avoid partial matching to "vmax1"
+  expect_equal(biocro.parms$vmax1, samples$biocro.saof$Vcmax)
+  expect_equal(biocro.parms$b0, samples$biocro.saof$cuticular_cond/1e+06)
+  expect_equal(biocro.parms$iSp, samples$biocro.saof$SLA)
+  expect_equal(biocro.parms$Rd, samples$biocro.saof$leaf_respiration_rate_m2)
+  expect_equal(biocro.parms$b1, samples$biocro.saof$stomatal_slope.BB)
+
   
   ## re-create bug #1491
   test.list <- list(vmax = 1, b0 = 2)
-  convert.samples.BIOCRO(test.list)  ## this should work
+  convert.samples.BIOCRO(test.list, 0.9)  ## this should work
 })
 
 test_that("write.configs.BIOCRO produces expected output", {
@@ -53,7 +66,7 @@ test_that("write.configs.BIOCRO produces expected output", {
     
     config.xml <- XML::xmlParse(config)
     config.list <- XML::xmlToList(config.xml)
-    biocro.trait.values <- convert.samples.BIOCRO(trait.values[[settings$pfts$pft$name]])
+    biocro.trait.values <- convert.samples.BIOCRO(trait.values[[settings$pfts$pft$name]], 0.9)
     expect_equal(biocro.trait.values[["vmax"]], as.numeric(config.list$pft$photoParms[["vmax"]]))
     expect_equal(biocro.trait.values[["b0"]], as.numeric(config.list$pft$photoParms[["b0"]]))
     expect_equal(biocro.trait.values[["b1"]], as.numeric(config.list$pft$photoParms[["b1"]]))
