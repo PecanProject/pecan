cccfb01800a7254f96bd14f3462fff31fff6b822
Alexey Shiklomanov <ashiklom@bu.edu>
2017-12-18 15:03:10 -0500

    S3: Add `spectra` S3 class
    Key features:
    
    - Subset and assign by wavelength with `sp[[400:700]]`
    - Automatically `plot` and `matplot` against wavelength
    - Combine multiple spectra objects by wavelength
    - Cleaner printing -- only print head and tail by default

---
+++ modules/rtm/tests/testthat/test.spectra.R
@@ -0,0 +1,56 @@
+library(PEcAnRTM)
+library(testthat)
+context("Spectra S3 class")
+
+data(testspec, package = "PEcAnRTM")
+
+rawspec1 <- testspec_ACRU[, 1]
+rawspec2 <- testspec_ACRU[, 6:8]
+rawspec3 <- testspec_ACRU[1:100, 9:10]
+
+spec1 <- spectra(rawspec1, 400:2500)
+spec2 <- spectra(rawspec2, 400:2500)
+spec3 <- spectra(rawspec3, 401:500)
+
+wl1 <- 400
+i1 <- wl1 - 399
+wl2 <- 500:520
+i2 <- wl2 - 399
+
+wl1_NA <- 1500:1800
+i1_NA <- wl1_NA - 399
+spec1[[wl1_NA]] <- NA
+
+test_that(
+  "Spectra subsetting and assignment works as expected",
+  {
+    expect_equivalent(as.numeric(spec1[[wl1]]), rawspec1[i1])
+    expect_equivalent(as.numeric(spec2[[wl2]]), rawspec2[i2, ])
+    expect_equal(sum(is.na(spec1)), length(wl1_NA))
+    expect_true(all(is.na(spec1[[wl1_NA]])))
+    expect_true(all(is.na(spec1[i1_NA])))
+  }
+)
+
+N_col <- sum(ncol(spec1), ncol(spec2), ncol(spec3))
+correct_wl <- 400:2500
+N_wl <- length(correct_wl)
+spec123 <- cbind(spec1, spec2, spec3)
+
+test_that(
+  "Combining spectra by wavelength works as expected",
+  {
+    expect_equal(nrow(spec123), N_wl)
+    expect_equal(ncol(spec123), N_col)
+    expect_equivalent(spec123[, 1], spec1[, 1])
+  }
+)
+
+if (interactive()) {
+  plot(spec1)
+  matplot(spec2)
+}
+
+if (interactive()) {
+  matplot(sp, lty = "solid", lwd = 2)
+}
