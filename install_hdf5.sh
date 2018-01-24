601611997183e3ff49f7c19be88784a27085fab8
Alexey Shiklomanov <ashiklom@bu.edu>
2018-01-24 13:35:06 -0500

    Add explicit prefix to HDF5 install script

--- install_hdf5.sh
+++ install_hdf5.sh
@@ -6,6 +6,6 @@ HDF5_RELEASE_URL="https://support.hdfgroup.org/ftp/HDF5/releases"
 wget "$HDF5_RELEASE_URL/hdf5-${HDF5_VERSION%.*}/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz"
 tar -xzf "hdf5-$HDF5_VERSION.tar.gz"
 pushd "hdf5-$HDF5_VERSION"
-./configure
+./configure --prefix=/usr
 make install
 popd
