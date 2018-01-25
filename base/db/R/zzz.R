051c415d8657cbf7e69a866986c80976b982deff
Chris Black <chris@ckblack.org>
2018-01-25 14:28:58 +0100

    declare imports on package documentation page

---
+++ base/db/R/zzz.R
@@ -0,0 +1,10 @@
+#' Database functions for PEcAn, the Predictive Ecosyten Analyzer
+#' 
+#' longer message here
+#' 
+#' 
+#' @docType package
+#' @name PEcAn.DB
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+NULL
