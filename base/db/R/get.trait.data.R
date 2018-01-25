6f35ecf5dac8d9c6ef1e0b3c3d8a87a0a7298adc
Chris Black <chris@ckblack.org>
2018-01-25 14:49:41 +0100

    support cultivars.csv in check.lists helper

--- base/db/R/get.trait.data.R
+++ base/db/R/get.trait.data.R
@@ -15,14 +15,21 @@
 ##' @title Compares two lists
 ##' @param x first list
 ##' @param y second list
+##' @param filename one of "species.csv" or "cultivars.csv"
 ##' @return true if two list are the same
 ##' @author Rob Kooper
 ##'
-check.lists <- function(x, y) {
+check.lists <- function(x, y, filename = "species.csv") {
   if (nrow(x) != nrow(y)) {
     return(FALSE)
   }
-  cols <- c('id', 'genus', 'species', 'scientificname')
+  if(filename == "species.csv"){
+    cols <- c('id', 'genus', 'species', 'scientificname')
+  } else if (filename == "cultivars.csv") {
+    cols <- c('id', 'specie_id', 'species_name', 'cultivar_name')
+  } else {
+    return(FALSE)
+  }
   xy_match <- vapply(cols, function(i) identical(as.character(x[[i]]), as.character(y[[i]])), logical(1))
   return(all(unlist(xy_match)))
 }
