170982a4e8c07ad1a810aa6351b4a99b01e133d6
Alexey Shiklomanov <ashiklom@bu.edu>
2018-01-28 13:32:46 -0500

    Update hdf5r installer script

--- scripts/install_hdf5r.R
+++ scripts/install_hdf5r.R
@@ -3,9 +3,18 @@
 # This script is used to install the `hdf5r` package on Travis, using custom 
 # HDF5 binaries located in the `hdf5_bin_trusty` directory.
 
-install.packages(
-  "hdf5r",
-  configure.args = c(
-    "--with-hdf5=hdf5_bin_trusty"
-  )
-)
+hdf5_dir <- list.files(".", "hdf5_bin")
+
+if (length(hdf5_dir) > 1) {
+  stop("Multiple HDF5 directories found")
+} else if (length(hdf5_dir) < 1) {
+  stop("No HDF5 directories found")
+}
+
+hdf5_full_dir <- normalizePath(hdf5_dir)
+
+Sys.setenv("HDF5_DIR" = hdf5_full_dir)
+
+hdf_full_path <- file.path(hdf5_full_dir, "bin", "h5cc")
+hdf_config_arg <- paste0("--with-hdf5=", hdf_full_path)
+install.packages("hdf5r", lib = "test_library", configure.args = hdf_config_arg)
