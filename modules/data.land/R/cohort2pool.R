30b6174e747ccd4f64e39d98f0e290ed3f720a32
saloni <sbs2019@bu.edu>
2018-02-01 10:01:06 -0500

    clean up of cohort2pool.R

--- modules/data.land/R/cohort2pool.R
+++ modules/data.land/R/cohort2pool.R
@@ -2,55 +2,51 @@
 ##' @title cohort2pool
 ##' @export
 ##' @param veg_file standard cohort veg_file
-##' @param allom_param parameters for allometric equation, a and b
+##' @param allom_param parameters for allometric equation, a and b. Based on base-10 log-log linear model (power law)
 ##' @author Saloni Shah
-
+##' @example 
+##' \dontrun{
+##' veg_file <- "~/downloads/FFT_site_1-25665/FFT.2008.veg.rds"
+##' cohort2pool(veg_File = veg_file, allom_param = NULL)
+##' }
 cohort2pool <- function(veg_file,allom_param =NULL) {
   
-  dat <- readRDS(veg_file)
-  
-  #Building Site ID from past directories
-  
+  ## Building Site ID from past directories
   path <- dirname(veg_file)
   last_dir <- basename(path)
   nums_id <- strsplit(last_dir,"[^[:digit:]]")
   base_id <- nums_id[[1]][length(nums_id[[1]])]
   suffix <- nums_id[[1]][(length(nums_id[[1]])-1)]
-  if (suffix == 1) {
-    siteid <- as.numeric(paste0(suffix, "0000", base_id))
-  } else if (siteid ==0) {
-    siteid <- as.numeric(base_id)
-  }
+  siteid = as.numeric(suffix)*1e9 + as.numeric(base_id)
   
-  #Grab DBH
+  ## load data
   
+  dat <- readRDS(veg_file)
+  
+  ## Grab DBH
   dbh <- dat[[2]]$DBH
+  
+  ## Grab allometry
   if(is.null(allom_param)){
-    a = 2                        
-    b= 0.3
+    a <- 2                        
+    b <- 0.3
+  } else {
+    print("user provided allometry parameters not yet supported")
+    return(NULL)
   }
   
   #Calculate AGB
-  
-  for (i in 1: length(dbh)) {
-    biomass = a + b*log(dbh)  
-  }
-  
+  biomass = 10^(a + b*log10(dbh))
   tot_biomass <- sum(biomass)
   
   #Prep Arguments for pool_ic function
-  
-  input <- list()
   dims <- list(time =1)
   variables <-list(AGB=tot_biomass)
-  input$dims <-dims
-  input$vals <-variables
+  input <- list(dims = dims,
+                vals = variables)
   
   # Execute pool_ic function
-  
   result <- PEcAn.data.land::pool_ic_list2netcdf(input = input, outdir = path, siteid = siteid)
-  
   
   return(result)
 }
-
