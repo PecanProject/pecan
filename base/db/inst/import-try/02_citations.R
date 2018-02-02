1e9515ba588eb49770101ccf9f09d0d8e853ca64
Alexey Shiklomanov <ashiklom@bu.edu>
2018-02-02 08:57:39 -0600

    DB: Miscellaneous bugfixes to last few commits

--- base/db/inst/import-try/02_citations.R
+++ base/db/inst/import-try/02_citations.R
@@ -11,13 +11,16 @@ logger.setLevel("DEBUG")
 sqlite_file <- "try.sqlite"
 
 trydb <- dbConnect(SQLite(), sqlite_file)
-references <- trydb %>%
-  tbl("datasets") %>%
-  distinct(Reference) %>%
-  pull(Reference)
+reference_dat <- tbl(trydb, "datasets") %>%
+  distinct(Reference, ReferenceID) %>%
+  collect()
 
-try_refs <- search_references(references, min_score = 1)
+refs_proc <- reference_dat %>%
+  mutate(cr_df = map(Reference, search_references, min_score = 40)) %>%
+  unnest()
 
 source("inst/import-try/bety_connect.R")
+bety_refs <- db_merge_into(refs_proc, "citations", bety, "doi") %>%
+  collect()
 
 # TODO: Use object `bety`, table `citations`
