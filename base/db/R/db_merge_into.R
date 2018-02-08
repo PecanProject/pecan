1a1524e383deaaadf8e46fe28e5b4a19d949f1b3
Alexey Shiklomanov <ashiklom@bu.edu>
2018-02-08 12:04:06 -0500

    DB: Address @infotroph PR #1848 comments
    Notable changes:
    - Use `glue::glue_sql` instead of `dbplyr::build_sql` for generating SQL
    query. The syntax is much cleaner. This also fixes the breaking tests by
    improving the way column names are quoted.
    - Remove the `.here` file
    - In tests and examples, use `":memory:"` SQLite database instead of a
    temporary file
    - Use explicit `package::function` syntax in tests, to test for
    NAMESPACE issues

--- base/db/R/db_merge_into.R
+++ base/db/R/db_merge_into.R
@@ -1,21 +1,19 @@
 #' Merge local data frame into SQL table
 #'
 #' @inheritParams insert_table
+#' @inheritDotParams insert_table
 #' @param by Character vector of columns by which to perform merge. Defaults to all columns in `values`
 #' @return Data frame: Inner join of SQL table and input data frame (as unevaluated "lazy query" table)
 #' @export
 #' @examples
-#' library(dplyr)
-#' library(RSQLite)
-#' irisfile <- tempfile(fileext = ".sqlite")
-#' irisdb <- dbConnect(SQLite(), irisfile)
-#' copy_to(irisdb, iris[1:10,], name = "iris", overwrite = TRUE)
+#' irisdb <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
+#' dplyr::copy_to(irisdb, iris[1:10,], name = "iris", overwrite = TRUE)
 #' db_merge_into(iris[1:12,], "iris", irisdb)
-#' tbl(irisdb, "iris") %>% count()
-db_merge_into <- function(values, table, con, by = NULL) {
-  values_fixed <- match_dbcols(values, table, con)
+#' dplyr::tbl(irisdb, "iris") %>% dplyr::count()
+db_merge_into <- function(values, table, con, by = NULL, drop = FALSE, ...) {
+  values_fixed <- match_dbcols(values, table, con, drop = FALSE)
   if (is.null(by)) {
-    by <- colnames(values_fixed)
+    by <- match_colnames(values, table, con)
   }
   sql_tbl <- dplyr::tbl(con, table)
   values_merge <- dplyr::anti_join(values_fixed, sql_tbl, by = by, copy = TRUE)
@@ -24,7 +22,7 @@ db_merge_into <- function(values, table, con, by = NULL) {
       "Input table for merge is empty."
     )
   } else {
-    insert <- insert_table(values_merge, table, con)
+    insert <- insert_table(values_merge, table, con, ...)
   }
   dplyr::tbl(con, table) %>%
     dplyr::inner_join(values_fixed, copy = TRUE)
