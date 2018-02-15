883447ec3e483ad0ec556ef23eef1c10979f02fe
mccabete <tmccabe@pecan-test.bu.edu>
2018-02-15 10:50:40 -0500

    parameter style fix

--- base/utils/R/seconds_in_year.R
+++ base/utils/R/seconds_in_year.R
@@ -7,8 +7,9 @@
 #' seconds_in_year(2000)  # Leap year -- 366 x 24 x 60 x 60 = 31622400
 #' seconds_in_year(2001)  # Regular year -- 365 x 24 x 60 x 60 = 31536000
 #' seconds_in_year(2000:2005)  # Vectorized over year
+#' @inheritParams days_in_year
 #' @export
-seconds_in_year <- function(year, leap_year = TRUE) {
+seconds_in_year <- function(year, ...) {
     diy <- days_in_year(year, leap_year)
     siy <- udunits2::ud.convert(diy, 'days', 'seconds')
     return(siy)
