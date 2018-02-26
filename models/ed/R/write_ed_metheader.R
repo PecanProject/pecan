68ecfe489c67af9340b13d1d59dc6e8cccf9ac0e
Alexey Shiklomanov <ashiklom@bu.edu>
2018-02-25 22:23:30 -0600

    ED: Complete draft of `write_ed_metheader` fun

---
+++ models/ed/R/write_ed_metheader.R
@@ -0,0 +1,31 @@
+#' Write ED meteorlogy header
+#'
+#' Write ED met driver header from R met driver list object
+#'
+#' @param ed_metheader ED meteorlogy header object (see [read_ed_metheader])
+#' @param filename Full file name (including path) of ED met header
+#' @export
+write_ed_metheader <- function(ed_metheader, filename) {
+  nformats <- length(ed_metheader)
+  blocks <- vector("list", nformats)
+  for (i in seq_len(nformats)) {
+    metformat <- ed_metheader[[i]]
+    block_lines <- character(6)
+    block_lines[1] <- metformat$path_prefix
+    block_lines[2] <- paste(
+      metformat$nlon,
+      metformat$nlat,
+      metformat$dx,
+      metformat$dy,
+      metformat$xmin,
+      metformat$ymin
+    )
+    block_lines[3] <- nrow(metformat$variables)
+    block_lines[4] <- paste(metformat$variables$variable, collapse = " ")
+    block_lines[5] <- paste(metformat$variables$update_frequency, collapse = " ")
+    block_lines[6] <- paste(metformat$variables$flag, collapse = " ")
+    blocks[[i]] <- block_lines
+  }
+  file_lines <- c(as.character(nformats), Reduce(c, blocks))
+  writeLines(file_lines, filename)
+}
