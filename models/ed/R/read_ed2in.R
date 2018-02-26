f6aa7eda1c5cca16aa2a0da56476f56def272b24
Alexey Shiklomanov <ashiklom@bu.edu>
2018-02-26 10:25:16 -0700

    ED: Complete draft of `write_ed2in` function

--- models/ed/R/read_ed2in.R
+++ models/ed/R/read_ed2in.R
@@ -8,13 +8,25 @@
 #' @export
 read_ed2in <- function(filename) {
   raw_file <- readLines(filename)
+
+  # Extract tag-value pairs
+  ed2in_tag_rxp <- paste0(
+    "^[[:blank:]]*NL%([[:graph:]]+)[[:blank:]]+=",
+    "[[:blank:]]*([[:digit:].-]+(,[[:blank:]]*[[:digit:].-]+)*",
+    "|'[[:graph:][:blank:]]*')[[:blank:]]*!?.*"
+  )
   tag_lines <- grep(ed2in_tag_rxp, raw_file, perl = TRUE)
+  sub_file <- raw_file[tag_lines]
   tags <- gsub(ed2in_tag_rxp, "\\1", sub_file, perl = TRUE)
   values <- gsub(ed2in_tag_rxp, "\\2", sub_file, perl = TRUE)
 
+  # Extract comments. They will be stored in the object attributes.
+  all_lines <- seq_along(raw_file)
+  comment_linenos <- all_lines[!all_lines %in% tag_lines]
+  comment_values <- raw_file[comment_linenos]
+
   # Convert to a list to allow storing of multiple data types
   values_list <- as.list(values)
-  names(values_list) <- tags
 
   numeric_values <- !is.na(suppressWarnings(as.numeric(values)))
   values_list[numeric_values] <- lapply(values_list[numeric_values], as.numeric)
@@ -45,15 +57,34 @@ read_ed2in <- function(filename) {
     replacement = ""
   )
 
-  values_list
+  structure(
+    values_list,
+    names = tags,
+    class = c("ed2in", "list"),
+    comment_linenos = comment_linenos,
+    comment_values = comment_values,
+    value_linenos = tag_lines
+  )
 }
 
-#' Regular expressions for ED2IN tags and values
+#' Print method for `ed2in`
 #'
-#' Regular expressions used to match ED2IN tags (first captured group) and 
-#' values (second captured group).
-ed2in_tag_rxp <- paste0(
-  "^[[:blank:]]*NL%([[:graph:]]+)[[:blank:]]+=",
-  "[[:blank:]]*([[:digit:].-]+(,[[:blank:]]*[[:digit:].-]+)*",
-  "|'[[:graph:][:blank:]]*')[[:blank:]]*!?.*"
-)
+#' Sets attributes to `NULL` before printing, so the output isn't as messy.
+#'
+#' @inheritParams base::print
+#' 
+#' @export
+print.ed2in <- function(x, ...) {
+  attributes(x) <- attributes(x)["names"]
+  print.default(x, ...)
+}
+
+#' Check if object is `ed2in`
+#'
+#' Simple test if object inheirts from class `"ed2in"`.
+#'
+#' @param x Object to be tested
+#' @export
+is.ed2in <- function(x) {
+  inherits(x, "ed2in")
+}
