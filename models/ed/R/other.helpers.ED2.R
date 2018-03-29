e6cac94a0b58328f4b5fddcf400a33571d0ca477
istfer <fer.istem@gmail.com>
2018-03-29 15:58:37 -0400

    aggregation and derivation

--- models/ed/R/other.helpers.ED2.R
+++ models/ed/R/other.helpers.ED2.R
@@ -13,19 +13,18 @@ list.files.nodir <- function(path, ...) {
 #' Function translating pecan vars to ED vars
 #' var.names <- c("DBH", "AGB", "AbvGrndWood")
 #' @export 
-translate_vars_ed <- function(var.names) {
+translate_vars_ed <- function(varnames) {
   
   var.list <- list()
-  ed_derivations <- rep(NA, length(var.names))
-  
-  data("standard_vars", package = "PEcAn.utils")
-  for(n in seq_along(var.names)){
-    longname          <- standard_vars$Long.name[standard_vars$Variable.Name == var.names[n]]
-    edvarout          <- ed.var(longname)
+  ed_derivations <- rep(NA, length(varnames))
+ 
+  for(n in seq_along(varnames)){
+    edvarout          <- ed.var(varnames[n])
     var.list[[n]]     <- edvarout$readvar
     ed_derivations[n] <- edvarout$expr
   }
   
-  var.names <- unique(unlist(var.list))
-  return(list(vars = var.names, expr = ed_derivations))
+  varnames <- unique(unlist(var.list))
+  return(list(vars = varnames, expr = ed_derivations))
 }
+
