f02cf8e0dd6ada34f4a12e194e5fd4d2629f3630
Betsy Cowdery <emcowdery@gmail.com>
2018-04-03 00:23:43 -0400

    Changing when data is loaded and what it's called
    To help with redundancy and sanity.

--- shiny/workflowPlot/ui_files/sidebar_UI.R
+++ shiny/workflowPlot/ui_files/sidebar_UI.R
@@ -2,10 +2,10 @@ sidebarPanel(
   h3("Load Model Output"),
   wellPanel(
     p("Please select the workflow IDs to continue. You can select multiple IDs"),
-    selectizeInput("all_workflow_id", "Mutliple Workflow IDs", c(),multiple=TRUE),
+    selectizeInput("all_workflow_id", "Mutliple Workflow IDs", c(), multiple=TRUE),
     p("Please select the run IDs. You can select multiple IDs"),
     selectizeInput("all_run_id", "Mutliple Run IDs", c(), multiple=TRUE),
-    actionButton("load", "Load Model outputs")
+    actionButton("load_model", "Load Model outputs")
   ),
   h3("Load External Data"),
   wellPanel(
