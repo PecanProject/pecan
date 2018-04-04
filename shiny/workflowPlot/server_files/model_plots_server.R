f0f1fa28267c389d1421f18aae910d137f0d5d95
Betsy Cowdery <emcowdery@gmail.com>
2018-04-04 13:52:59 -0400

    Adding unit conversion

--- shiny/workflowPlot/server_files/model_plots_server.R
+++ shiny/workflowPlot/server_files/model_plots_server.R
@@ -12,17 +12,49 @@ output$modelPlot <- renderPlotly({
              size = 10, color = "grey")
 })
 
+# Update units every time a variable is selected
+observeEvent(input$var_name_model, {
+  model.df <- load.model()
+  default.unit <- model.df %>% filter(var_name == input$var_name_model) %>% pull(ylab) %>% unique()
+  updateTextInput(session, "units_model", value = default.unit)
+})
+
+# Check that new units are parsible and can be used for conversion
+observeEvent(input$units_model,{
+  parseable <- ifelse(udunits2::ud.is.parseable(input$units_model), "can", "cannot")
+  if(udunits2::ud.is.parseable(input$units_model)){
+    model.df <- load.model()
+    default.unit <- model.df %>% filter(var_name == input$var_name_model) %>% pull(ylab) %>% unique()
+    if(udunits2::ud.are.convertible(default.unit, input$units_model)){
+      output$unit_text <-  renderText({"Conversion possible"})
+    }else{
+      output$unit_text <-  renderText({"Units are parsible but conversion is not possible"})
+    }
+  }else{
+    output$unit_text <-  renderText({"Units are not parsible, please type units in udunits2 compatible format"})
+  }
+})
+
+
 observeEvent(input$ex_plot_model,{
+  req(input$units_model)
+  
   output$modelPlot <- renderPlotly({
     input$ex_plot_model
     isolate({
-      masterDF <- load.model()
-      df <- dplyr::filter(masterDF, var_name == input$var_name_model)
-      
+      df <- dplyr::filter(load.model(), var_name == input$var_name_model)
+     
       updateSliderInput(session,"smooth_n_model", min = 0, max = nrow(df))
+      
       title <- unique(df$title)
       xlab <- unique(df$xlab)
       ylab <- unique(df$ylab)
+      
+      unit <- ylab
+      if(input$units_model != unit & udunits2::ud.are.convertible(unit, input$units_model)){
+        df$vals <- udunits2::ud.convert(df$vals,unit,input$units_model)
+        ylab <- input$units_model
+      }
       
       data_geom <- switch(input$plotType_model, point = geom_point, line = geom_line)
       
