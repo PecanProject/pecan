5da52edb76383f564c7b2b6a9be379d31367673b
Betsy Cowdery <emcowdery@gmail.com>
2018-04-04 17:26:37 -0400

    Adding a documentation page for the app
    Work in progress. Mostly linking to other places in the pecan documentation.

--- shiny/workflowPlot/ui.R
+++ shiny/workflowPlot/ui.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(plotly)
 library(shinythemes)
-# Helper allows to load functions and variables that could be shared both by server.R and ui.R 
+library(knitr)
 
 source("ui_utils.R", local = TRUE)
 
@@ -23,7 +23,11 @@ ui <- fluidPage(theme = shinytheme("united"),
                                                   source_ui("benchmarking_scores_UI.R"),
                                                   source_ui("benchmarking_plots_UI.R")
                                                 )
-                                       ))
+                                       ),
+                                       tabPanel(h4("Documentation"),
+                                                withMathJax(includeMarkdown("markdown/workflowPlot_doc.Rmd"))
+                                                )
+                                       )
                             
                   )
                 ))
