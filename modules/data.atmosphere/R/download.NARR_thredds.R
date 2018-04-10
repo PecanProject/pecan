3b20aaa63e2d2c760e6aa025a00cefbbd4501289
Alexey Shiklomanov <ashiklom@bu.edu>
2018-04-10 19:49:10 -0400

    data.atmosphere: Download NARR using THREDDS
    Closes #1476.

---
+++ modules/data.atmosphere/R/download.NARR_thredds.R
@@ -0,0 +1,260 @@
+#' Retrieve NARR data using thredds
+#'
+#' @param start_date Start date for meteorology
+#' @param end_date End date for meteorology
+#' @param lat.in Latitude coordinate
+#' @param lon.in Longitude coordinate
+#' @param progress Whether or not to show a progress bar (default = `TRUE`)
+#' @return `tibble` containing time series of NARR data for the given site
+#' @author Alexey Shiklomanov
+#' @examples
+#' dat <- get_NARR_thredds("2008-01-01", "2008-01-15", 43.3724, -89.9071)
+get_NARR_thredds <- function(start_date, end_date, lat.in, lon.in,
+                             progress = TRUE) {
+
+  PEcAn.logger::severeifnot(
+    length(start_date) == 1,
+    msg = paste("Start date must be a scalar, but has length", length(start_date))
+  )
+
+  PEcAn.logger::severeifnot(
+    length(end_date) == 1,
+    msg = paste("End date must be a scalar, but has length", length(end_date))
+  )
+
+  PEcAn.logger::severeifnot(
+    length(lat.in) == 1,
+    msg = paste("Latitude must be a scalar, but has length", length(latitude))
+  )
+
+  PEcAn.logger::severeifnot(
+    length(lon.in) == 1,
+    msg = paste("Longitude must be a scalar, but has length", length(longitude))
+  )
+
+  narr_start <- lubridate::ymd("1979-01-01")
+  narr_end <- lubridate::ymd("2018-03-31")
+
+  start_date <- lubridate::as_date(start_date)
+  end_date <- lubridate::as_date(end_date)
+
+  PEcAn.logger::severeifnot(
+    start_date >= narr_start,
+    msg = paste0(
+      "Start date ", start_date,
+      " is before NARR start date ", narr_start
+    )
+  )
+
+  PEcAn.logger::severeifnot(
+    end_date <= narr_end,
+    msg = paste0(
+      "End date ", end_date,
+      "is after NARR end date ", narr_end
+    )
+  )
+
+  dates <- seq(start_date, end_date, by = "1 day")
+  flx_df <- generate_narr_url(dates, TRUE)
+  sfc_df <- generate_narr_url(dates, FALSE)
+
+  # Load dimensions, etc. from first netCDF file
+  nc1 <- ncdf4::nc_open(flx_df$url[1])
+  on.exit(ncdf4::nc_close(nc1))
+  xy <- latlon2narr(nc1, lat.in, lon.in)
+
+  # Retrieve remaining variables by iterating over URLs
+  npb <- nrow(flx_df) * nrow(narr_flx_vars) +
+    nrow(sfc_df) * nrow(narr_sfc_vars)
+  if (progress) {
+    pb <- progress::progress_bar$new(total = npb)
+  } else {
+    pb <- NULL
+  }
+
+  flx_data_raw <- flx_df %>%
+    dplyr::mutate(
+      data = purrr::map(
+        url,
+        get_narr_url,
+        xy = xy,
+        flx = TRUE,
+        pb = pb
+      )
+    )
+
+  sfc_data_raw <- sfc_df %>%
+    dplyr::mutate(
+      data = purrr::map(
+        url,
+        get_narr_url,
+        xy = xy,
+        flx = FALSE,
+        pb = pb
+      )
+    )
+
+  # Set times
+  flx_data <- post_process(flx_data_raw) %>% dplyr::rename(flx_url = url)
+  sfc_data <- post_process(sfc_data_raw) %>% dplyr::rename(sfc_url = url)
+  met_data <- dplyr::full_join(flx_data, sfc_data, by = "datetime")
+
+  met_data
+}
+
+#' Post process raw NARR downloaded data frame
+post_process <- function(dat) {
+  dat %>%
+    tidyr::unnest(data) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(datetime = startdate + lubridate::hours(dhours)) %>%
+    dplyr::select(-startdate, -startday, -endday, -dhours, -year, -month) %>%
+    dplyr::select(datetime, dplyr::everything()) %>%
+    dplyr::select(-url, url)
+}
+
+#' Generate NARR url from a vector of dates
+#'
+#' Figures out file names for the given dates, based on NARR's convoluted and 
+#' inconsistent naming scheme.
+#'
+#' @param dates Vector of dates for which to generate URL
+#' @param flx (Logical) If `TRUE`, format for `flx` variables. Otherwise, 
+#' format for `sfc` variables. See [narr_flx_vars].
+#' @author Alexey Shiklomanov
+generate_narr_url <- function(dates, flx) {
+  ngroup <- if (flx) 8 else 10
+  tag <- if (flx) "flx" else "sfc"
+  base_url <- paste(
+    # Have to login, so use Alexey Shiklomanov's account
+    "http://ashiklom%40bu.edu:Thisis4theNARR@rda.ucar.edu",
+    "thredds", "dodsC", "files", "g", "ds608.0", "3HRLY",
+    sep = "/"
+  )
+  tibble::tibble(date = dates) %>%
+    dplyr::mutate(
+      year = lubridate::year(date),
+      month = lubridate::month(date),
+      mday = lubridate::mday(date),
+      # FLX files: groups of 8: 0108, 0916, 1724, ...
+      # SFC files: groups of 10, but the first is shorter: 0109, 1019, 2029
+      daygroup = (mday - as.integer(flx)) %/% ngroup
+    ) %>%
+    dplyr::group_by(year, month, daygroup) %>%
+    dplyr::summarize(
+      # Weird logic to match file-grouping scheme described above
+      endday = (unique(daygroup) + 1) * ngroup - as.integer(!flx),
+      startday = max(1, 1 + endday - ngroup),
+      startdate = as.POSIXct(ISOdatetime(unique(year), unique(month), startday,
+                                         0, 0, 0, tz = "UTC")),
+      url = sprintf("%6$s/%1$d/NARR%5$s_%1$d%2$02d_%3$02d%4$02d.tar",
+                      unique(year), unique(month), startday, endday, tag, base_url),
+    ) %>%
+    dplyr::select(year, month, startday, endday, startdate, url)
+}
+
+#' Retrieve NARR data from a given URL
+#'
+#' @param url Full URL to NARR thredds file
+#' @param xy Vector length 2 containing NARR coordinates
+#' @param pb Progress bar R6 object (default = `NULL`)
+#' @inheritParams generate_narr_url
+#' @author Alexey Shiklomanov
+get_narr_url <- function(url, xy, flx, pb = NULL) {
+  stopifnot(length(xy) == 2, length(url) == 1, is.character(url))
+  nc <- ncdf4::nc_open(url)
+  on.exit(ncdf4::nc_close(nc))
+  timevar <- if (flx) "time" else "time3"
+  dhours <- ncdf4::ncvar_get(nc, timevar)
+  narr_vars <- if (flx) narr_flx_vars else narr_sfc_vars
+  result <- purrr::pmap(
+    narr_vars %>% dplyr::select(variable = NARR_name, unit = units),
+    read_narr_var,
+    nc = nc, xy = xy, flx = flx, pb = pb
+  )
+  names(result) <- narr_vars$CF_name
+  dplyr::bind_cols(dhours = dhours, result)
+}
+
+#' Read a specific variable from a NARR NetCDF file
+#'
+#' @param nc `ncdf4` connection object
+#' @param variable NARR name of variable to retrieve
+#' @param unit Output unit of variable to retrieve
+#' @inheritParams get_narr_url
+#' @author Alexey Shiklomanov
+read_narr_var <- function(nc, xy, variable, unit, flx, pb = NULL) {
+  if (flx) {
+    # Third dimension is height above ground -- first index is 2m above ground
+    start <- c(xy, 1, 1)
+    count <- c(1, 1, 1, -1)
+  } else {
+    # Third dimension is reference time; only has one index
+    start <- c(xy, 1, 1)
+    count <- c(1, 1, -1, -1)
+  }
+  nc_unit <- ncdf4::ncatt_get(nc, variable, "units")$value
+  out <- ncdf4::ncvar_get(nc, variable, start = start, count = count)
+  # Precipitation is a special case -- unit is actually precipitation per 3 hours
+  # So, divide by seconds in 3 hours and change unit accordingly
+  if (variable == "Total_precipitation_surface_3_Hour_Accumulation") {
+    nc_unit <- paste0(nc_unit, "/s")
+    out <- out / udunits2::ud.convert(3, "hours", "seconds")
+  }
+  final <- udunits2::ud.convert(out, nc_unit, unit)
+  if (!is.null(pb)) pb$tick()
+  final
+}
+
+#' NARR flux and sfc variables
+narr_flx_vars <- tibble::tribble(
+  ~CF_name, ~NARR_name, ~units,
+  "air_temperature", "Temperature_height_above_ground", "Kelvin",
+  "air_pressure", "Pressure_height_above_ground", "Pascal",
+  "eastward_wind", "u-component_of_wind_height_above_ground", "m/s",
+  "northward_wind", "v-component_of_wind_height_above_ground", "m/s",
+  "specific_humidity", "Specific_humidity_height_above_ground", "g/g"
+)
+
+#' @rdname narr_flx_vars
+narr_sfc_vars <- tibble::tribble(
+  ~CF_name, ~NARR_name, ~units,
+  "surface_downwelling_longwave_flux_in_air", "Downward_longwave_radiation_flux_surface_3_Hour_Average", "W/m2",
+  "surface_downwelling_shortwave_flux_in_air", "Downward_shortwave_radiation_flux_surface_3_Hour_Average", "W/m2",
+  "precipitation_flux", "Total_precipitation_surface_3_Hour_Accumulation", "kg/m2/s",
+)
+
+#' Convert latitude and longitude coordinates to NARR indices
+#'
+#' @inheritParams read_narr_var
+#' @inheritParams get_NARR_thredds
+#' @return Vector length 2 containing NARR `x` and `y` indices, which can be 
+#' used in `ncdf4::ncvar_get` `start` argument.
+#' @author Alexey Shiklomanov
+latlon2narr <- function(nc, lat.in, lon.in) {
+  narr_x <- ncdf4::ncvar_get(nc, "x")
+  narr_y <- ncdf4::ncvar_get(nc, "y")
+  ptrans <- latlon2lcc(lat.in, lon.in)
+  x_ind <- which.min((ptrans$x - narr_x) ^ 2)
+  y_ind <- which.min((ptrans$y - narr_y) ^ 2)
+  c(x = x_ind, y = y_ind)
+}
+
+#' Convert latitude and longitude to x-y coordinates (in km) in Lambert 
+#' conformal conic projection (used by NARR)
+#'
+#' @inheritParams get_NARR_thredds
+#' @return `sp::SpatialPoints` object containing transformed x and y 
+#' coordinates, in km, which should match NARR coordinates
+#' @author Alexey Shiklomanov
+#' @export
+latlon2lcc <- function(lat.in, lon.in) {
+  pll <- sp::SpatialPoints(list(x = lon.in, y = lat.in), sp::CRS("+proj=longlat +datum=WGS84"))
+  CRS_narr_string <- paste(
+    "+proj=lcc +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96",
+    "+x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=km +no_defs"
+  )
+  CRS_narr <- sp::CRS(CRS_narr_string)
+  ptrans <- sp::spTransform(pll, CRS_narr)
+  ptrans
+}
