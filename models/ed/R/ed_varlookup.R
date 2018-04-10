1f77ce4189d90a350ec8750b5e93c84849ce8f11
istfer <fer.istem@gmail.com>
2018-04-10 07:24:58 -0400

    move else to next line

--- models/ed/R/ed_varlookup.R
+++ models/ed/R/ed_varlookup.R
@@ -6,43 +6,43 @@ ed.var <- function(varname) {
     out = list(readvar = "AGB_CO", 
                type = 'co', units = "kgC/plant", 
                drelated = NULL,                    # other deterministically related vars?
-               expr    = "AGB_CO")                                                 } else
-  if(varname == "TotLivBiom") {
+               expr    = "AGB_CO")                                                 
+  } else if(varname == "TotLivBiom") {
     out = list(readvar = c("BALIVE"),
                type = 'co', units = "kgC/plant", 
                drelated = NULL,
-               expr    = "BALIVE")                                                 } else
-  if(varname == "BA") {
+               expr    = "BALIVE")                                                 
+  } else if(varname == "BA") {
     out = list(readvar = "BA_CO",
                type = 'co', units = "cm2/plant", 
                drelated = NULL,
-               expr    = "BA_CO")                                                  } else
-  if(varname == "DBH") {
+               expr    = "BA_CO")                                                  
+  } else if(varname == "DBH") {
     out = list(readvar = "DBH",
                type = 'co', units = "cm/plant",
                drelated = NULL,
-               expr    = "DBH")                                                    } else
-  if(varname == "AbvGrndWood") {
+               expr    = "DBH")                                                    
+  } else if(varname == "AbvGrndWood") {
     out = list(readvar = c("AGB_CO", "BLEAF"), 
                type = 'co', units = "kgC/plant", 
                drelated = NULL, 
-               expr    = "AGB_CO-BLEAF")                                           } else
-  if(varname == "leaf_carbon_content") {
+               expr    = "AGB_CO-BLEAF")                                           
+  } else if(varname == "leaf_carbon_content") {
     out = list(readvar = "BLEAF", 
                type = 'co', units = "kgC/plant", 
                drelated = NULL, 
-               expr    = "BLEAF")                                                  } else
-  if(varname == "root_carbon_content") {
+               expr    = "BLEAF")                                                  
+  } else if(varname == "root_carbon_content") {
     out = list(readvar = "BROOT", 
                type = 'co', units = "kgC/plant", 
                drelated = NULL, 
-               expr    = "BROOT")                                                  } else
-  if(varname == "GWBI") {
+               expr    = "BROOT")                                                  
+  } else if(varname == "GWBI") {
     out = list(readvar = "DLNAGB_DT", # this is actually rate of change in AGB 
                type = 'co', units = "1/yr", 
                drelated = NULL,
-               expr    = "DLNAGB_DT")                                              } else
-  { # No Match!
+               expr    = "DLNAGB_DT")                                              
+  } else { # No Match!
     warning(paste0("Couldn't find varname ", varname, "!"))
     out = NULL
   }
