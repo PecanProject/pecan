2b59e914f98dff6906ef308c51cc52e319edf104
istfer <fer.istem@gmail.com>
2018-04-17 15:57:10 -0400

    use remote execute for remote met sampling

--- modules/assim.sequential/R/met_filtering_helpers.R
+++ modules/assim.sequential/R/met_filtering_helpers.R
@@ -1,11 +1,16 @@
 
-sample_met <- function(settings,nens=1){
-  
+sample_met <- function(settings, nens=1){
+
   path <- settings$run$inputs$met[["path"]]
 
-  ens_members <- list.files(path, recursive = TRUE)
+  if(settings$host$name == "localhost"){
+    ens_members <- list.files(path, recursive = TRUE)
+  }else{
+    # remote
+    ens_members <- PEcAn.remote::remote.execute.cmd(host, paste0('ls -d -1 ', path, "/*.*"))
+  }
   
-  
+
   start_date <- as.POSIXlt(strptime(settings$run$site$met.start, "%Y/%m/%d"))
   end_date   <- as.POSIXlt(strptime(settings$run$site$met.end, "%Y/%m/%d"))
   start_date$zone <- end_date$zone <- NULL
@@ -15,8 +20,17 @@ sample_met <- function(settings,nens=1){
   tmp_members <- gsub(paste0(".", end_date),   "", tmp_members) 
   member_names <- unique(dirname(ens_members))
   
-  #this will change from model to model, generalize later
-  ens_ind <- unlist(sapply(paste0(member_names, ".clim"), grep, tmp_members))
+  # this will change from model to model, generalize later
+  # This function is temporary but if we will continue to use this approach for met ensembles (instead of met process workflow)
+  # it might not be a bad idea to have sample_met.model
+  if(settings$model$type == "ED2"){
+    ens_members <- file.path(ens_members, "ED_MET_DRIVER_HEADER")
+    ens_ind     <- seq_along(ens_members)
+  }else if(settings$model$type == "SIPNET"){
+    ens_ind <- unlist(sapply(paste0(member_names, ".clim"), grep, tmp_members))
+  }
+  
+
   # ens_members[ens_ind]
   ens_input <- list()
   for(i in seq_len(nens)){
