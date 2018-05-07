bb01656ad761c799cd239a0459b6ce2f64b7c7d1
Tony Gardella <tonygard@bu.edu>
2018-05-07 14:09:52 -0400

    sibcasa

--- models/sibcasa/R/model2netcdf.SIBCASA.R
+++ models/sibcasa/R/model2netcdf.SIBCASA.R
@@ -22,6 +22,25 @@
 ##'
 ##' @author Tony Gardella
 model2netcdf.SIBCASA <- function(outdir, sitelat, sitelon, start_date, end_date) {
-  PEcAn.logger::logger.severe("NOT IMPLEMENTED")
+    
+  PEcAn.logger::logger.severe("NOT IMPLEMENTED") 
+
+    ## Loop over years  
+        ## read in and rename variables and units
+    [1] "lonindex"    "latindex"    "year"        "month"       "DOM"         "HOD"         "DOY"         "seconds"    
+    [9] "ventmf"      "ustar"       "gt"          "tcan"        "ev"          "snow_depth"  "snow_can"    "runoff"     
+    [17] "gl"          "testvar1"    "testvar2"    "testvar3"    "gs"          "ra"          "rb"          "rc"         
+    [25] "rd"          "snow_bulk"   "d_active"    "d_freeze"    "snow_area"   "snow_mass"   "evt"         "hfss"       
+    [33] "fws"         "chf"         "shf"         "hfc"         "hfg"         "hfect"       "hfeci"       "hfegs"      
+    [41] "hfegi"       "ea"          "ta"          "em"          "hura"        "leaf_frac"   "root_frac"   "wood_frac"  
+    [49] "snow_nsl"    "grnd_liq"    "canopy_liq"  "mrtsoil"     "pco2ap"      "pco2s"       "pco2i"       "pco2c"      
+    [57] "spdmsib"     "pssib"       "dlsprsib"    "dcuprsib"    "tssib"       "tsib3"       "sh_sib"      "radvbc"     
+    [65] "radvdc"      "radnbc"      "radndc"      "sw_dwn"      "dlwbotsib"   "carb_live"   "carb_dead"   "carb_litter"
+    [73] "carb_soil"   "carb_awood"  "carb_tot"    "c_flux"      "assimn"      "gpp"         "NEE_1"       "NEE_2"      
+    [81] "npp"         "resp_grnd"   "resp_tot"    "resp_auto"   "resp_het"    "rstfac1"     "rstfac2"     "rstfac3"    
+    [89] "carb_above"  "pco2m"       "zlt"         "LAI"         "lai_chi_sqr" "lai_err"     "aparc"       "fpar"       
+    [97] "carb_al"     "dperm_top"   "OLT"         "resp_perm"   "resp_al"     "carb_perm"   "carb_froz"   "carb_thaw"  
+    [105] "resp_meth"   "soil_frz"    
+  
   
 } # model2netcdf.SIBCASA
