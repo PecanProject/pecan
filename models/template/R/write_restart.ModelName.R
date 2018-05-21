521467f8b9e023fb243207e212dba7201c6e22a7
istfer <fer.istem@gmail.com>
2018-05-21 12:43:27 -0400

    update template documentation for write_restart

--- models/template/R/write_restart.ModelName.R
+++ models/template/R/write_restart.ModelName.R
@@ -5,6 +5,9 @@
 #' @param start.time   Time of current assimilation step 
 #' @param stop.time    Time of next assimilation step
 #' @param new.state    Analysis state matrix returned by \code{sda.enkf}
+#' @param RENAME       flag to either rename output file or not
+#' @param new.params   optional, additionals params to pass write.configs that are deterministically related to the parameters updated by the analysis 
+#' @param inputs       new input paths updated by the SDA workflow, will be passed to write.configs
 #' @inheritParams read.restart.ModelName
 #' 
 #' @description Write restart files for model
@@ -15,4 +18,7 @@ write_restart.ModelName <- function(outdir,
                                     start.time,
                                     stop.time,
                                     settings,
-                                    new.state) {}
+                                    new.state,
+                                    RENAME,
+                                    new.params,
+                                    inputs) {}
