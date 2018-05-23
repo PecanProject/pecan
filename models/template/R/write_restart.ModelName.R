86ee9171a09e4138b9ea552bb9eee26c2f39303d
istfer <fer.istem@gmail.com>
2018-05-23 17:25:13 -0400

    revise template args

--- models/template/R/write_restart.ModelName.R
+++ models/template/R/write_restart.ModelName.R
@@ -2,13 +2,15 @@
 #' 
 #' @author Alexey Shiklomanov
 #' 
+#' @param outdir
+#' @param runid
 #' @param start.time   Time of current assimilation step 
 #' @param stop.time    Time of next assimilation step
+#' @param settings
 #' @param new.state    Analysis state matrix returned by \code{sda.enkf}
 #' @param RENAME       flag to either rename output file or not
 #' @param new.params   optional, additionals params to pass write.configs that are deterministically related to the parameters updated by the analysis 
 #' @param inputs       new input paths updated by the SDA workflow, will be passed to write.configs
-#' @inheritParams read_restart.ModelName
 #' 
 #' @description Write restart files for model
 #' 
