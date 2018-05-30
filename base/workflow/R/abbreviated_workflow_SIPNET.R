f81a49bc009311eed3a64a1a947c1a4229bdb059
Tony Gardella <tonygard@bu.edu>
2018-05-30 10:15:20 -0400

    script from saloni

---
+++ base/workflow/R/abbreviated_workflow_SIPNET.R
@@ -0,0 +1,25 @@
+
+#AbbreviatedWorkflow_Sipnet
+#Variables :
+library(PEcAn.all)
+library(PEcAn.utils)
+library(RCurl)
+variable_S <- c("GPP ", "NPP", "TotalResp","AutoResp", "HeteroResp", "SoilResp", "NEE", "QLE", "leaf_carbon_content", "GWBI", "TotSoilCarb", "course_root_carbon_content", "fine_root_carbon_content", "litter_carbon_content", "Transp", "TotLivBiom", "LAI", "AGB", "SoilMoist", "SoilMoistFracSWE", "AbvGrndWood")
+
+for (i in 1:length(variable_S)) {
+  settings <- PEcAn.settings::read.settings("pecan.CHECKED.xml") 
+  settings$sensitivity.analysis$variable_S <- variable_S
+  print(settings$sensitivity.analysis$variable)
+  
+  # Get results of model runs
+  
+    runModule.get.results(settings)
+    
+  # Run sensitivity analysis and variance decomposition on model output
+
+    runModule.run.sensitivity.analysis(settings)
+
+
+  
+}
+
