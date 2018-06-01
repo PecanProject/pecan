c90e182cf5af45df8cb144c7627ec75c6f2b7dfd
Chaitanya Reddy <chavalichaithanyachinna@gmail.com>
2018-06-01 19:48:56 +0530

    loading animations on plot recalculations

--- shiny/workflowPlot/ui_files/benchmarking_plots_UI.R
+++ shiny/workflowPlot/ui_files/benchmarking_plots_UI.R
@@ -1,4 +1,20 @@
 tabPanel("Plots",
          uiOutput("bm_plots"),
-         plotlyOutput("bmPlot")
+         div(id = "plot-container",
+             div(class = "plotlybars-wrapper",
+                 div( class="plotlybars",
+                      div(class="plotlybars-bar b1"),
+                      div(class="plotlybars-bar b2"),
+                      div(class="plotlybars-bar b3"),
+                      div(class="plotlybars-bar b4"),
+                      div(class="plotlybars-bar b5"),
+                      div(class="plotlybars-bar b6"),
+                      div(class="plotlybars-bar b7")
+                 ),
+                 div(class="plotlybars-text",
+                     p("Updating the plot. Hold tight!")
+                 )
+             ),
+             plotlyOutput("bmPlot")
+         )
 )
