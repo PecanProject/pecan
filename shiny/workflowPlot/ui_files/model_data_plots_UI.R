4eab20df2c5f67e5ec255e515660a9e65b727d68
Chaitanya Reddy <chavalichaithanyachinna@gmail.com>
2018-06-12 21:46:47 +0530

    toggle plots for model data plots

--- shiny/workflowPlot/ui_files/model_data_plots_UI.R
+++ shiny/workflowPlot/ui_files/model_data_plots_UI.R
@@ -1,15 +1,36 @@
-tabPanel("Model-Data Plots",
-         column(12, load_anim_div(plot_div = "modelDataPlot")),
-         column(12, wellPanel( actionButton("ex_plot_modeldata", "Generate Plot"))),
-         column(12, wellPanel(
-           selectInput("var_name_modeldata", "Variable Name", ""),
-           textInput("units_modeldata", "Units",
-                     placeholder = "Type units in udunits2 compatible format"),
-           verbatimTextOutput("unit_text2"),
-           radioButtons("plotType_modeldata", "Plot Type (for Model Outputs)",
-                        c("Scatter Plot" = "point","Line Chart" = "line"),
-                        selected = "point"),
-           sliderInput("smooth_n_modeldata", "Value for smoothing:",
-                       min = 0, max = 100, value = 80))
-         )
+tabPanel(
+  "Model-Data Plots",
+  hidden(div(id = "model_data_plot_interactive", column(
+    12, load_anim_div(plot_div = "modelDataPlot")
+  ))),
+  div(id = "model_data_plot_static", column(
+    12, load_anim_div(plot_div = "modelDataPlotStatic")
+  )),
+  column(12, wellPanel(
+    actionButton("ex_plot_modeldata", "Generate Plot"),
+    div(actionButton("model_data_toggle_plot", "Toggle Plot"),
+        style = "float:right")
+  )),
+  column(
+    12,
+    wellPanel(
+      selectInput("var_name_modeldata", "Variable Name", ""),
+      textInput("units_modeldata", "Units",
+                placeholder = "Type units in udunits2 compatible format"),
+      verbatimTextOutput("unit_text2"),
+      radioButtons(
+        "plotType_modeldata",
+        "Plot Type (for Model Outputs)",
+        c("Scatter Plot" = "point", "Line Chart" = "line"),
+        selected = "point"
+      ),
+      sliderInput(
+        "smooth_n_modeldata",
+        "Value for smoothing:",
+        min = 0,
+        max = 100,
+        value = 80
+      )
+    )
+  )
 )
