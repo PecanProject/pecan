3d47f488dfa579c5b3c9268dff4d99da00cb354c
LiamBurke24 <liam.burke24@gmail.com>
2018-06-13 16:29:48 -0400

    Cleaned up code, now both download messages download data, display downloaded files, and move those files to dbfiles.

--- shiny/Data-Ingest/app.R
+++ shiny/Data-Ingest/app.R
@@ -3,9 +3,9 @@ library(PEcAn.data.land)
 library(PEcAn.utils)
 library(shinydashboard)
 library(dataone)
-library(shinyFiles)
 library(stringr)
-library(unixtools) #DEVELOPMENT VERSION! UNSURE IF THIS IS VIABLE FOR IMPLEMENTATION IN PECAN
+library(shinyFiles)
+library(DT)
 
 #############################################################################
 ################################## UI #######################################
@@ -55,12 +55,17 @@ ui <- dashboardPage(
                   label = h2("Import From DataONE"),
                   placeholder = "Enter doi or id here"
                 ),
-                p("Copy Paste the following DOI as an example: doi:10.6073/pasta/63ad7159306bc031520f09b2faefcf87"),
+                p("Copy and Paste the following example data sets:"),
+                p("doi:10.6073/pasta/63ad7159306bc031520f09b2faefcf87"),
+                p("doi:10.6073-pasta-f31b28b912e6051bf1d383ff1ef18987"),
                 actionButton(inputId = "D1Button", label = "Download"),
                 hr(),
-                tableOutput("identifier"),
+                DTOutput("identifier"),
                 hr(),
-                actionButton(inputId = "D1FinishButton", label = "Finish Download")
+                actionButton(inputId = "D1FinishButton", label = "Finish Download"),
+                hr(),
+                p("Location of Downloaded files:"),
+                verbatimTextOutput("D1dbfilesPath")
               )
             )
             ),
@@ -76,9 +81,12 @@ ui <- dashboardPage(
                   multiple = FALSE,
                   placeholder = "Drag and drop files here"
                 ),
-                h3("This feature is currently unavailable"),
                 tableOutput("contents"),
-                actionButton(inputId = "EmptyDirectoryButton", label = "Clear All")
+                actionButton(inputId = "LocalFinishButton", label = "Finish Download"),
+                hr(),
+                p("Location of Downloaded Files:"),
+                verbatimTextOutput("LocaldbfilesPath")
+                
               )
               )
             ),
@@ -101,58 +109,62 @@ ui <- dashboardPage(
 server <- function(input, output, session) {
   options(shiny.maxRequestSize = 30 * 1024 ^ 2) #maximum file input size
   
-  Shared.data<-reactiveValues(downloaded=NULL)
-  
+  ## Setup ##
+  Shared.data <- reactiveValues(downloaded=NULL)
   temp <- tempdir() 
   PEcAn_path <- PEcAn.utils::read_web_config("../../web/config.php")$dbfiles_folder
-  print(temp) #for testing only -- show in console only
+  # print(temp) #for testing only -- show in console only
   
-  # Create two sub-directories in the tempfile
+##################### DataONE Download #############################################  
+  ## Create two sub-directories in the tempfile ##
   d1_tempdir <- file.path(temp, "d1_tempdir")
     dir.create(d1_tempdir, showWarnings = F)
   local_tempdir <- file.path(temp, "local_tempdir")
     dir.create(local_tempdir, showWarnings = F)
     
-  print(list.files(temp))
+  # print(list.files(temp)) # for testing only -- show
 
    observeEvent(input$D1Button, {
      # run dataone_download with input from id on click
      PEcAn.data.land::dataone_download(trimws(input$id), filepath = d1_tempdir) # store files in tempfile
-     # newdir_D1 <<- "/tmp/Rtmp35jOEK/d1_tempdir/DataOne_doi:10.6073-pasta-63ad7159306bc031520f09b2faefcf87"
-        Filename <- list.files(newdir_D1)
-        D1_file_df <- as.data.frame(Filename)
-        list_of_d1_files <<- list.files(newdir_D1) # I call this later
+      #newdir_D1 <<- file.path(temp, "d1_tempdir/DataOne_doi:10.6073-pasta-f31b28b912e6051bf1d383ff1ef18987") # For Testing (so I don't have to re-download the data)
+     list_of_d1_files <<- list.files(newdir_D1)
+     D1_file_df <- as.data.frame(list_of_d1_files)
         
-        # Grab the name of the D1_file from newdir_D1
-        d1_dirname <<- base::sub("/tmp/Rtmp[[:alnum:]]{6}/d1_tempdir/", "", newdir_D1) # let users create their own filenames
         
-        Shared.data$downloaded <- D1_file_df # Reactive Variable
+      # Grab the name of the D1_file from newdir_D1
+      d1_dirname <<- base::sub("/tmp/Rtmp[[:alnum:]]{6}/d1_tempdir/", "", newdir_D1) # let users create their own filenames eventually
+       
+      Shared.data$downloaded <- D1_file_df # Reactive Variable 
   })
   
    # Display downloaded files in data.frame
-   output$identifier <- renderTable(rownames = TRUE, hover = TRUE, {Shared.data$downloaded})
+   output$identifier <- DT::renderDT({Shared.data$downloaded})
    
    # Move files to correct dbfiles location (make a custom function for this?)
    observeEvent(input$D1FinishButton, {
      # create the new directory in /dbfiles
      dir.create(paste0(PEcAn_path, d1_dirname))
      
-     n <- length(list_of_d1_files)
-     for (i in 1:n){
-       base::file.copy(file.path(newdir_D1, list_of_d1_files[i]), file.path(PEcAn_path, d1_dirname, list_of_d1_files[i]))
-     }
-     
+     ### Return a warning if the directory already exists ### Not working yet
+   # if (!file.exists(file.path(PEcAn_path, d1_dirname))){
+     # print("File Already Exists")
+    #}else{
+     n <- length(list_of_d1_files) 
+        for (i in 1:n){
+          base::file.copy(file.path(newdir_D1, list_of_d1_files[i]), file.path(PEcAn_path, d1_dirname, list_of_d1_files[i]))
+        }
+     output$D1dbfilesPath <- renderText({paste0(PEcAn_path, d1_dirname)}) # Print path to data
+   # }
    })
-   
    
   ######### FileInput ########################################
   output$contents <- renderTable({
-   #localdownload <- eventReactive({
     inFile <- input$file
     n <- length(inFile$name)
     names <- inFile$name
 
-     if (is.null(inFile))  # Do I need this here?
+     if (is.null(inFile))  
        return(NULL)
 
     splits <- list()
@@ -166,39 +178,31 @@ server <- function(input, output, session) {
         print(oldpath[i])
         print(list.files(temp)[i])
         print(file.path(temp, inFile[i, "name"]))
-       base::file.rename(oldpath[i], file.path(temp, inFile[i, "name"])) # rename the file to include the original filename
+       base::file.rename(oldpath[i], file.path(temp, "local_tempdir", inFile[i, "name"])) # rename the file to include the original filename
        base::unlink(dirname(oldpath[i]), recursive = TRUE) # remove the file with the userhostile name
     }
-     return(list.files(paste(temp, "local_temp", sep = '/'))) # I think I should move this too
+     return(list.files(file.path(temp, "local_tempdir"))) # I think I should move this too
   })
-
-  # This doesn't work yet
-  # output$contents <- renderTable({
-  #   localdownload()
-  #
-  #   if (is.null(inFile))
-  #      return(NULL)
-  #
-  #   return(list.files(temp))
-  # })
-
-
-############### Empty Directory Button (for testing) ########################
-  observeEvent(input$"EmptyDirectoryButton", {
-    tempfiles <- list.files(temp)
-    nfiles <- length(tempfiles)
-    # print(temp)
-    # print(tempfiles)
-
-    for (i in 1:nfiles) {
-      filepath <- file.path(temp, tempfiles[i])
-    #  base::file.remove(filepath[i])
-
-    }
-     lapply(tempfiles,unlink)
-     print(list.files(temp))
-  })
-
+   
+   # Move files to correct dbfiles location (make a custom function for this?)
+   observeEvent(input$LocalFinishButton, {
+     # create the new directory in /dbfiles
+     local_dirname <- "TESTDIR_LOCAL_DOWNLOAD" # I will replace this with a naming field in the next PR
+     dir.create(file.path(PEcAn_path, local_dirname))
+     
+     path_to_local_tempdir <- file.path(local_tempdir)
+     list_of_local_files <- list.files(path_to_local_tempdir) 
+     
+     ### Return a warning if the directory already exists ### Not working yet
+     # if (!file.exists(file.path(PEcAn_path, d1_dirname))){
+     # print("File Already Exists")
+     #}else{
+     n <- length(list_of_d1_files)
+     for (i in 1:n){
+       base::file.copy(file.path(path_to_local_tempdir, list_of_local_files[i]), file.path(PEcAn_path, local_dirname, list_of_local_files[i]))
+     }
+     output$LocaldbfilesPath <- renderText({paste0(PEcAn_path, local_dirname)}) # Print path to paper
+   })
 }
 
 # Run the application
