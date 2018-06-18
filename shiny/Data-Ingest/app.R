76d3bfe21c1f7930807845d7ed730736c5089ee7
LiamBurke24 <liam.burke24@gmail.com>
2018-06-18 12:10:06 -0400

    Added a functioning conditional spinner and message to the dataONE download pannel.

--- shiny/Data-Ingest/app.R
+++ shiny/Data-Ingest/app.R
@@ -61,14 +61,14 @@ ui <- dashboardPage(
                 p("Copy and Paste the following example data sets:"),
                 p("doi:10.6073/pasta/63ad7159306bc031520f09b2faefcf87"),
                 p("doi:10.6073-pasta-f31b28b912e6051bf1d383ff1ef18987"),
-                 actionButton(inputId = "D1Button", label = "Download"),
+                  actionButton(inputId = "D1Button", label = "Download"),
+                #  actionButton(inputId = "CancelD1Download", label = "Cancel Download"), This is WAY tricky. Not sure if I can add this functionality... 
                 hr(),
                 conditionalPanel(condition="$('html').hasClass('shiny-busy')",
                                  tags$div(id="loadmessage",
-                                          HTML(paste0("<div style='background-color:lightgreen'> <h3>Download in Progress...</h3> <p>This download may take a couple of minutes. This is a placeholder for a spinning loader.</p> </div>"))
-                                           )), 
-                DTOutput("identifier"), 
-                hr(),
+                                          HTML(paste0("<div> <h3>Download in Progress.</h3> <p>This download may take a couple of minutes.</p> <img src=\'http://www.lettersmarket.com/uploads/lettersmarket/blog/loaders/common_green/ajax_loader_green_64.gif' height=\"64\" width=\"64\"> </div>"))
+                                           )), #"<div style='background-color:lightgreen'> <h3>Download in Progress...</h3> <p>This download may take a couple of minutes. This is a placeholder for a spinning loader.</p> </div>"
+                DTOutput("identifier"), # http://www.lettersmarket.com/uploads/lettersmarket/blog/loaders/common_green/ajax_loader_green_64.gif
                 actionButton(inputId = "D1FinishButton", label = "Finish Download"),
                 hr(),
                 p("Location of Downloaded files:"),
