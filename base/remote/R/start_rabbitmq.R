e11f5eff6fc050beab9870a884fa445600ff5172
Rob Kooper <kooper@illinois.edu>
2018-06-18 13:12:01 -0500

    no need to export start_rabbitmq, add documentation

--- base/remote/R/start_rabbitmq.R
+++ base/remote/R/start_rabbitmq.R
@@ -1,7 +1,6 @@
 #' Start model execution using rabbitmq
 #'
 #' @return Output of execution command, as a character (see [remote.execute.cmd()]).
-#' @export
 start_rabbitmq <- function(folder, rabbitmq_uri, rabbitmq_queue) {
   out <- system2('python3', c('/work/sender.py', rabbitmq_uri, rabbitmq_queue, folder), stdout = TRUE, stderr = TRUE)
   return(out)
