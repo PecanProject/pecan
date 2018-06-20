1c384828cb2694cd41abd881238e88f9aef1069d
LiamBurke24 <liam.burke24@gmail.com>
2018-06-20 14:10:21 -0400

    substitute sidebar and d1 ui for source calls

--- shiny/Data-Ingest/app.R
+++ shiny/Data-Ingest/app.R
@@ -8,6 +8,8 @@
  library(DT)
  library(shinyjs)
  library(shiny)
+ 
+ source("ui_utils.R", local = TRUE)
 
 #############################################################################
 ################################## UI #######################################
@@ -15,66 +17,14 @@
 
 ui <- dashboardPage(
   dashboardHeader(title = "Data Ingest"), 
-  dashboardSidebar(sidebarMenu(
-    # menuItem(
-    #   "Select Data Acquisition Method",
-    #   tabName = "importDataONE",
-    #   icon = icon("file")
-    # ),
-    menuItem(
-      "Import from DataONE",
-      tabName = "importDataONE",
-      icon = icon("file")
-    ),
-    menuItem(
-      "Upload Local Files",
-      tabName = "uploadLocal",
-      icon = icon("file")
-    ),
-    menuItem(
-      "Step 2 -- dbfiles record",
-      tabName = "step2",
-      icon = icon("cog")
-    ),
-    menuItem(
-      "Step 3 -- format record",
-      tabName = "step3",
-      icon = icon("cog")
-    ),
-    menuItem("Step 4 -- etc.", 
-             tabName = "step4", 
-             icon = icon("cog")
-    )
-  )
+  dashboardSidebar(
+    source_ui("sidebar_ui.R")
   ),
   dashboardBody(
     tabItems(
     ################ Tab 1 -- DataONE download ##################################
     tabItem(tabName = "importDataONE",
-            fluidRow(
-              box(
-                textInput(
-                  "id",
-                  label = h2("Import From DataONE"),
-                  placeholder = "Enter doi or id here"
-                ),
-                p("Copy and Paste the following example data sets:"),
-                p("doi:10.6073/pasta/63ad7159306bc031520f09b2faefcf87"),
-                p("doi:10.6073-pasta-f31b28b912e6051bf1d383ff1ef18987"),
-                  actionButton(inputId = "D1Button", label = "Download"),
-                #  actionButton(inputId = "CancelD1Download", label = "Cancel Download"), This is WAY tricky. Not sure if I can add this functionality... 
-                hr(),
-                conditionalPanel(condition="$('html').hasClass('shiny-busy')",
-                                 tags$div(id="loadmessage",
-                                          HTML(paste0("<div> <h3>Download in Progress.</h3> <p>This download may take a couple of minutes.</p> <img src=\'http://www.lettersmarket.com/uploads/lettersmarket/blog/loaders/common_green/ajax_loader_green_64.gif' height=\"64\" width=\"64\"> </div>"))
-                                           )), 
-                DTOutput("identifier"), 
-                actionButton(inputId = "D1FinishButton", label = "Finish Download"),
-                hr(),
-                p("Location of Downloaded files:"),
-                verbatimTextOutput("D1dbfilesPath")
-              )
-            )
+            source_ui("d1_download_ui.R")
             ),
     ################ Tab 2 -- Local File Upload ##################################
     tabItem(tabName = "uploadLocal",
