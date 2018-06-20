95470e540182f47b04bea2e88432ee8627a7438a
Christy Rollinson <crollinson@gmail.com>
2018-06-20 13:29:44 -0500

    update bounds to 0 & 365 per @mdietze suggestion

--- modules/data.atmosphere/R/debias_met_regression.R
+++ modules/data.atmosphere/R/debias_met_regression.R
@@ -185,8 +185,8 @@ debias.met.regression <- function(train.data, source.data, n.ens, vars.debias=NU
       
       # Hard-coding in some sort of max for precipitaiton
       rain.max <- max(train.data$precipitation_flux) + sd(train.data$precipitation_flux)
-      rainless.min <- ifelse(min(rainless)-sd(rainless)>0, min(rainless)-sd(rainless), max(min(rainless)-sd(rainless)/2, 1))
-      rainless.max <- ifelse(max(rainless)+sd(rainless)<365, max(rainless)+sd(rainless), min(max(rainless)+sd(rainless)/2, 364))
+      rainless.min <- ifelse(min(rainless)-sd(rainless)>=0, min(rainless)-sd(rainless), max(min(rainless)-sd(rainless)/2, 0))
+      rainless.max <- ifelse(max(rainless)+sd(rainless)<=365, max(rainless)+sd(rainless), min(max(rainless)+sd(rainless)/2, 365))
     }
     # -------------
     
