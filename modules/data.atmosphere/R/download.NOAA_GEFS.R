4f7218ce7b8b10c617f3e50559c037eb074d56f6
Luke-Dramko <luke.dramko@gmail.com>
2018-06-22 15:54:08 -0400

    Updated data.atmosphere package documentation

--- modules/data.atmosphere/R/download.NOAA_GEFS.R
+++ modules/data.atmosphere/R/download.NOAA_GEFS.R
@@ -41,32 +41,7 @@
 ##' @examples 
 ##' 
 ##' \dontrun {
-##'  #This program is to test the R script donload.NOAA.R during development.
-##'
-##' test_no = 1
-##' args = commandArgs(trailingOnly = TRUE)
-##' if (length(args) > 0) {
-##'  test_no = as.integer(args[1])
-##'}
-##'
-##' source("~/pecan/modules/data.atmosphere/R/download.NOAA_GEFS.R")
-##'
-##'
-##' if (test_no == 1) { #Default case
 ##'  download.NOAA_GEFS("~/Working/results", lat.in= 46.2420, lon.in = -89.3476, "willow creek")
-##'} else if (test_no == 2) { #Should be an Error - date out of bounds
-##'  download.NOAA_GEFS("~/Working/results",lat.in= 46.2420, lon.in = -89.3476, "willow creek", Sys.time()  - lubridate::days(12), Sys.time(), verbose = FALSE)
-##' } else if (test_no == 3) { #Should work
-##'  download.NOAA_GEFS("~/Working/results", lat.in= 46.2420, lon.in = -89.3476, "willow creek", Sys.time() - lubridate::days(4), verbose = FALSE)
-##' } else if (test_no == 4) { #Should work - 1 day's worth of data
-##'   download.NOAA_GEFS("~/Working/results", lat.in= 46.2420, lon.in = -89.3476, "willow creek", Sys.time() - lubridate::days(8), Sys.time() - lubridate::days(7),  verbose = FALSE)
-##' } else if (test_no == 5) { #Should be an error - date out of bounds 
-##'   download.NOAA_GEFS("~/Working/results", lat.in= 46.2420, lon.in = -89.3476, "willow creek", Sys.Date() + lubridate::days(1), verbose = FALSE)
-##' } else if (test_no == 6) { #Should crash - timespan not large enough
-##'  download.NOAA_GEFS("~/Working/results", lat.in= 46.2420, lon.in = -89.3476, "willow creek", Sys.time(), Sys.time(), verbose = FALSE)
-##' } else if (test_no == 7) { #Should work, but have the timespan shrunk by one day.  Output should be identical to default case.
-##'  download.NOAA_GEFS("~/Working/results", lat.in= 46.2420, lon.in = -89.3476, "willow creek", Sys.time(), Sys.time() + lubridate::days(17), verbose = FALSE)
-##'}
 ##' }
 ##' 
 ##' @author Luke Dramko
