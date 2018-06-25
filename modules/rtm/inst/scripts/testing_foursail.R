1f965388da395b81aaa828527059ea62e87f55cf
Shawn P. Serbin <sserbin@bnl.gov>
2018-06-25 16:07:04 -0400

    Updates. Cleanup

--- modules/rtm/inst/scripts/testing_foursail.R
+++ modules/rtm/inst/scripts/testing_foursail.R
@@ -6,19 +6,18 @@ rsoil <- read.table(rsoil,header = F)
 #str(rsoil)
 rsoil <- as.vector(unlist(rsoil[1,]))
 length(rsoil)
-rsoil <- c(rsoil,rsoil[2100])
+rsoil <- c(rsoil,rsoil[2100]) # make soil reflectance the correct length
 plot(seq(400,2500,1), rsoil, type = "l")
 
 # define some sail params
-LIDFa <- -0.35 # -1 #-0.35  
-LIDFb <- -0.15 # 0 #-0.15
+LIDFa <- -0.35 
+LIDFb <- -0.15
 TypeLIDF <- 1
 LAI <- 4
 q <- 0.1
-tts <- 48  # Flight 6 2300, 48 deg or 57
-tto <- 0   # assuming nadir, I think
-psi <- 234 #F6: 234 # need to confirm, sun azimuth?
-#psoil=0.9 # soil "wetness" factor, 1 very wet, 0 very dry
+tts <- 48
+tto <- 0 
+psi <- 234
 params <- c(LIDFa,LIDFb,TypeLIDF,TypeLIDF,LAI,q,tts,tto,psi)
 
 # get leaf refl/trans
@@ -30,3 +29,5 @@ tran <- LRT[,2]
 
 
 sail_sepc <- foursail(refl, tran, rsoil, params)
+
+
