ac2627ebdff0af8f530d701d5e471ab1a42b7977
Luke-Dramko <luke.dramko@gmail.com>
2018-06-29 11:12:36 -0400

    Added forecast and ensemble arguments to download.raw.met.module.R

--- modules/data.atmosphere/R/download.raw.met.module.R
+++ modules/data.atmosphere/R/download.raw.met.module.R
@@ -6,8 +6,23 @@
   pkg <- "PEcAn.data.atmosphere"
   fcn <- paste0("download.", met)
   
+  #Some data products can be forecasts instead of real time data.  Others can be ensembles of data instead of a single source.  Some can be both.
+  #Not all of the registration.xml files for each data source contains a <forecast> or <ensemble> tag; therefore, we must check for their 
+  #existence first.
+  forecast = FALSE
+  ensemble = FALSE
+  if (!is.null(register$forecast)) {
+    forecast = as.logical(register$forecast)
+  }
+  if (!is.null(register$ensemble) && !is.na(as.integer(register$ensemble)) && as.integer(register$ensemble) > 1) {
+    ensemble = as.integer(register$ensemble) #No ensembles is given by FALSE, while the presence of ensembles is given by the number of ensembles.
+    ifelse(is.na(ensemble), FALSE, ensemble) #If ensemble happens to be a character vector or something it can't convert, as.integer will evaluate to NA.
+  }
+  
+  ### source("~/pecan/base/utils/R/convert.input.R")
+  
   if (register$scale == "regional") {
-    raw.id <- PEcAn.utils::convert.input(input.id = NA, 
+    raw.id <- PEcAn.utils::convert.input(input.id = NA,   ### Change back to PEcAn.utils:: when done debugging!!!
                             outfolder = outfolder, 
                             formatname = register$format$name, 
                             mimetype = register$format$mimetype,
@@ -35,6 +50,8 @@
                             fcn = fcn, 
                             con = con, host = host, browndog = NULL, 
                             write = TRUE, overwrite = overwrite, 
+                            forecast = forecast,
+                            ensemble = ensemble,
                             sitename = site$name, 
                             username = username)
     
