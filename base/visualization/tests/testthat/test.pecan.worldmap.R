c4ece380a0dc8fe2d0902c00452cf44099d969bd
LiamBurke24 <liam.burke24@gmail.com>
2018-07-10 16:36:04 -0400

    Depricating the test.pecan.worldmap Test

--- base/visualization/tests/testthat/test.pecan.worldmap.R
+++ base/visualization/tests/testthat/test.pecan.worldmap.R
@@ -1,11 +1,11 @@
-
-context("World map")
-
-test_that("pecan.worldmap runs with example input", {
-  
-  data(yielddf, package = "PEcAn.visualization")
-  outfile <- file.path(tempdir(), "worldmap.png")
-  pecan.worldmap(yielddf, outfile = outfile)
-  expect_true(file.exists(outfile))
-  
-})
+############### This test is being depricated. ###################
+# context("World map")
+# 
+# test_that("pecan.worldmap runs with example input", {
+#   
+#   data(yielddf, package = "PEcAn.visualization")
+#   outfile <- file.path(tempdir(), "worldmap.png")
+#   pecan.worldmap(yielddf, outfile = outfile)
+#   expect_true(file.exists(outfile))
+#   
+# })
