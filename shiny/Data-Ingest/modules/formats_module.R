2af82c3af5742c4a2f0fcc860960fb9cb703eaae
LiamBurke24 <liam.burke24@gmail.com>
2018-07-10 23:43:43 -0400

    hide new formats record tab until button click. Add href link to BETYdb website for New Formats

--- shiny/Data-Ingest/modules/formats_module.R
+++ shiny/Data-Ingest/modules/formats_module.R
@@ -2,46 +2,47 @@
 formatsRecordUI <- function(id){
   ns <- NS(id)
   
-  box(title = h2("3. Format Record"), width = 3, collapsible = TRUE, collapsed = TRUE,
-      hr(),
-      selectizeInput(ns("MimetypeName"), label = "Mimetype *", choices = NULL,
-                     options = list(
-                       placeholder = 'Please search inputs by name or site',
-                       onInitialize = I('function() { this.setValue(""); }')
-                     )),
-      hr(),
-      textInput(
-        ns("NewMimeType"),
-        label = "New Mime Type *",
-        placeholder = "Create a New Mimetype"),
-      hr(),
-      textInput(
-        ns("NewFormatName"),
-        label = "New Format Name *",
-        placeholder = "Create a New Format Name"),
-      hr(),
-      radioButtons(
-        ns("HeaderBoolean"),
-        label = "Is There a Header ?", 
-        choices =  c("Yes", "No")
-      ),
-      hr(),
-      textInput( # I should Render UI only if Header = TRUE
-        ns("SkipLines"),
-        label = "Skip",
-        placeholder = "Enter number of header lines to skip."),
-      hr(),
-      textAreaInput(
-        ns("FormatNotes"),
-        label = "Notes",
-        height = '150px'
-      ),
-      actionButton(ns("createFormatRecord"), label = "Create Format Record"),
-      p("* Denotes a Required Field"),
-      hr(),
-      verbatimTextOutput(ns("FormatRecordOut"))
+  shinyjs::hidden(
+    div(id = "formatbox",
+      box(title = h2("3. Create New Format"),
+          width = 4, collapsible = TRUE, collapsed = FALSE, solidHeader = TRUE, status = "warning",
+          selectizeInput(ns("MimetypeName"), label = "Mimetype *", choices = NULL,
+                         options = list(
+                           placeholder = 'Please search inputs by name or site',
+                           onInitialize = I('function() { this.setValue(""); }')
+                         )),
+          p("or"),
+          # uiOutput(ns("tab")), ## Link to BETYdb
+          a(id = ns("betyURL"), "Create New Mimetype", href = "https://www.betydb.org/formats/new"),
+          hr(),
+          textInput(
+            ns("NewFormatName"),
+            label = "New Format Name *",
+            placeholder = "Create a New Format Name"),
+          hr(),
+          radioButtons(
+            ns("HeaderBoolean"),
+            label = "Is There a Header ?", 
+            choices =  c("Yes", "No")
+          ),
+          hr(),
+          textInput( # I should Render UI only if Header = TRUE
+            ns("SkipLines"),
+            label = "Skip",
+            placeholder = "Enter number of header lines to skip."),
+          hr(),
+          textAreaInput(
+            ns("FormatNotes"),
+            label = "Notes",
+            height = '150px'
+          ),
+          actionButton(ns("createFormatRecord"), label = "Create Format Record"),
+          p("* Denotes a Required Field"),
+          hr(),
+          verbatimTextOutput(ns("FormatRecordOut"))
+      )
+    )
   )
-  
 }
 
 #### Server #####
@@ -59,7 +60,7 @@ formatsRecord <- function(input, output, session){
     FormatRecordList$mimetypeID <- mimetype_sub %>% dplyr::filter(type_string %in% FormatRecordList$mimetypeName) %>% pull(id)
     
     ## Everything else
-    FormatRecordList$NewMimeType <- input$NewMimeType
+    # FormatRecordList$NewMimeType <- input$NewMimeType
     FormatRecordList$NewFormatName <- input$NewFormatName
     FormatRecordList$HeaderBoolean <- input$HeaderBoolean
     FormatRecordList$SkipLines <- input$SkipLines #This should appear only if header = TRUE
@@ -67,8 +68,14 @@ formatsRecord <- function(input, output, session){
     
     output$FormatRecordOut <- renderPrint({print(FormatRecordList)})
     
+    # url <- a("Google Homepage", href="https://www.google.com/")
+    # output$tab <- renderUI({
+    #   tagList("URL link:", url)
+    # })
+    
+   # shinyjs::onclick("betyURL", shinyjs::
+    
   })
   
-  
-  
+
 }
