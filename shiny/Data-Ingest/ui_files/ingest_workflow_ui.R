b7e1f63bebba782ca919022bcdd7bd9f06f2b69a
LiamBurke24 <liam.burke24@gmail.com>
2018-07-14 17:19:57 -0400

    Delete Duplicate Formats record UI

--- shiny/Data-Ingest/ui_files/ingest_workflow_ui.R
+++ shiny/Data-Ingest/ui_files/ingest_workflow_ui.R
@@ -106,48 +106,5 @@ box(
   hr(),
   verbatimTextOutput("summInputs")
   )
-
-# 3. Formats (optional)
-#shinyjs::hidden(
-#  div(id = "formatbox",
-      # box(title = h2("3. Create New Format"),
-      #     width = 4, collapsible = TRUE, collapsed = FALSE, solidHeader = TRUE, status = "warning",
-      #     selectizeInput("MimetypeName", label = "Mimetype *", choices = NULL,
-      #                    options = list(
-      #                      placeholder = 'Please search inputs by name or site',
-      #                      onInitialize = I('function() { this.setValue(""); }')
-      #                    )),
-      #     p("or"),
-      #     # uiOutput("tab"), ## Link to BETYdb
-      #     a(id = "betyURL", "Create New Mimetype", href = "https://www.betydb.org/formats/new", target = "_blank"),
-      #     hr(),
-      #     textInput(
-      #       "NewFormatName",
-      #       label = "New Format Name *",
-      #       placeholder = "Create a New Format Name"),
-      #     hr(),
-      #     radioButtons(
-      #       "HeaderBoolean",
-      #       label = "Is There a Header ?",
-      #       choices =  c("Yes", "No")
-      #     ),
-      #     hr(),
-      #     textInput( # I should Render UI only if Header = TRUE
-      #       "SkipLines",
-      #       label = "Skip",
-      #       placeholder = "Enter number of header lines to skip."),
-      #     hr(),
-      #     textAreaInput(
-      #       "FormatNotes",
-      #       label = "Notes",
-      #       height = '150px'
-      #     ),
-      #     actionButton("createFormatRecord", label = "Create Format Record"),
-      #     p("* Denotes a Required Field"),
-      #     hr(),
-      #     verbatimTextOutput("FormatRecordOut")
-      # )
-#  )
-#)
 )
 ## 4. Formats-Variables
