0b5096d6669a637f308a1b99ed4e37d96c00a558
hamzed <hamzed@bu.edu>
2018-07-16 14:52:18 -0400

    If there was no tag for met or param then replicate the first one

--- base/utils/R/ensemble.R
+++ base/utils/R/ensemble.R
@@ -33,7 +33,7 @@ read.ensemble.output <- function(ensemble.size, pecandir, outdir, start.year, en
       stop(samples.file, "not found required by read.ensemble.output")
     }
   }
-
+  
   expr <- variable$expression
   variables <- variable$variables
   
@@ -70,11 +70,13 @@ read.ensemble.output <- function(ensemble.size, pecandir, outdir, start.year, en
 ##' @param pft.samples random samples from parameter distribution, e.g. from a MCMC chain  
 ##' @param env.samples env samples
 ##' @param method the method used to generate the ensemble samples. Random generators: uniform, uniform with latin hypercube permutation. Quasi-random generators: halton, sobol, torus. Random generation draws random variates whereas quasi-random generation is deterministic but well equidistributed. Default is uniform. For small ensemble size with relatively large parameter number (e.g ensemble size < 5 and # of traits > 5) use methods other than halton. 
+##' @param param.names a list of parameter names that were fitted either by MA or PDA, important argument, if NULL parameters will be resampled independently
+##' 
 ##' @return matrix of (quasi-)random samples from trait distributions
 ##' @export
-##' @author David LeBauer
+##' @author David LeBauer, Istem Fer
 get.ensemble.samples <- function(ensemble.size, pft.samples, env.samples, 
-                                 method = "uniform", ...) {
+                                 method = "uniform", param.names = NULL, ...) {
   
   if (is.null(method)) {
     PEcAn.logger::logger.info("No sampling method supplied, defaulting to uniform random sampling")
@@ -95,8 +97,10 @@ get.ensemble.samples <- function(ensemble.size, pft.samples, env.samples,
       pft2col <- c(pft2col, rep(i, length(pft.samples[[i]])))
     }
     
+    
     total.sample.num <- sum(sapply(pft.samples, length))
     random.samples <- NULL
+    
     
     if (method == "halton") {
       PEcAn.logger::logger.info("Using ", method, "method for sampling")
@@ -130,15 +134,32 @@ get.ensemble.samples <- function(ensemble.size, pft.samples, env.samples,
                                total.sample.num)
     }
     
+    
     ensemble.samples <- list()
+    
     
     col.i <- 0
     for (pft.i in seq(pft.samples)) {
       ensemble.samples[[pft.i]] <- matrix(nrow = ensemble.size, ncol = length(pft.samples[[pft.i]]))
+      
+      # meaning we want to keep MCMC samples together
+      if(length(pft.samples[[pft.i]])>0 & !is.null(param.names)){ 
+        # TODO: for now we are sampling row numbers uniformly
+        # stop if other methods were requested 
+        if(method != "uniform"){
+          PEcAn.logger::logger.severe("Only uniform sampling is available for joint sampling at the moment. Other approaches are not implemented yet.")
+        }
+        same.i <- sample.int(length(pft.samples[[pft.i]][[1]]), ensemble.size)
+      }
+      
       for (trait.i in seq(pft.samples[[pft.i]])) {
         col.i <- col.i + 1
-        ensemble.samples[[pft.i]][, trait.i] <- stats::quantile(pft.samples[[pft.i]][[trait.i]],
-                                                         random.samples[, col.i])
+        if(names(pft.samples[[pft.i]])[trait.i] %in% param.names[[pft.i]]){ # keeping samples
+          ensemble.samples[[pft.i]][, trait.i] <- pft.samples[[pft.i]][[trait.i]][same.i]
+        }else{
+          ensemble.samples[[pft.i]][, trait.i] <- stats::quantile(pft.samples[[pft.i]][[trait.i]],
+                                                                  random.samples[, col.i])
+        }
       }  # end trait
       ensemble.samples[[pft.i]] <- as.data.frame(ensemble.samples[[pft.i]])
       colnames(ensemble.samples[[pft.i]]) <- names(pft.samples[[pft.i]])
@@ -163,11 +184,12 @@ get.ensemble.samples <- function(ensemble.size, pft.samples, env.samples,
 ##' @param clean remove old output first?
 ##' @return list, containing $runs = data frame of runids, and $ensemble.id = the ensemble ID for these runs. Also writes sensitivity analysis configuration files as a side effect
 ##' @export
-##' @author David LeBauer, Carl Davidson
+##' @author David LeBauer, Carl Davidson, Hamze Dokoohaki
 write.ensemble.configs <- function(defaults, ensemble.samples, settings, model, 
-                                   clean = FALSE, write.to.db = TRUE) {
+                                   clean = FALSE, write.to.db = TRUE,restart=NULL) {
   
   my.write.config <- paste("write.config.", model, sep = "")
+  my.write_restart <- paste0("write_restart.", model)
   
   if (is.null(ensemble.samples)) {
     return(list(runs = NULL, ensemble.id = NULL))
@@ -191,36 +213,65 @@ write.ensemble.configs <- function(defaults, ensemble.samples, settings, model,
   } else {
     workflow.id <- -1
   }
-  
-  # create an ensemble id
-  if (!is.null(con)) {
-    # write ensemble first
-    ensemble.id <- PEcAn.DB::db.query(paste0(
-      "INSERT INTO ensembles (runtype, workflow_id) ",
-      "VALUES ('ensemble', ", format(workflow.id, scientific = FALSE), ")",
-      "RETURNING id"), con = con)[['id']]
-
-    for (pft in defaults) {
-      PEcAn.DB::db.query(paste0(
-        "INSERT INTO posteriors_ensembles (posterior_id, ensemble_id) ",
-        "values (", pft$posteriorid, ", ", ensemble.id, ")"), con = con)
-    }
-  } else {
-    ensemble.id <- NA
-  }
-  
-  # find all inputs that have an id
-  inputs <- names(settings$run$inputs)
-  inputs <- inputs[grepl(".id$", inputs)]
-  
-  # write configuration for each run of the ensemble
-  runs <- data.frame()
-  for (counter in seq_len(settings$ensemble$size)) {
+  #------------------------------------------------- if this is a new fresh run------------------  
+  if (is.null(restart)){
+    # create an ensemble id
     if (!is.null(con)) {
-      paramlist <- paste("ensemble=", counter, sep = "")
-      run.id <- PEcAn.DB::db.query(paste0(
-        "INSERT INTO runs (model_id, site_id, start_time, finish_time, outdir, ensemble_id, parameter_list) ",
-        "values ('", 
+      # write ensemble first
+      ensemble.id <- PEcAn.DB::db.query(paste0(
+        "INSERT INTO ensembles (runtype, workflow_id) ",
+        "VALUES ('ensemble', ", format(workflow.id, scientific = FALSE), ")",
+        "RETURNING id"), con = con)[['id']]
+      
+      for (pft in defaults) {
+        PEcAn.DB::db.query(paste0(
+          "INSERT INTO posteriors_ensembles (posterior_id, ensemble_id) ",
+          "values (", pft$posteriorid, ", ", ensemble.id, ")"), con = con)
+      }
+    } else {
+      ensemble.id <- NA
+    }
+    #-------------------------generating met/param/soil/veg/... for all ensumbles----
+    settings$ensemble$samplingspace->samp 
+    #finding who has a parent
+    parents<-lapply(samp,'[[','parent')
+    #order parents based on the need of who has to be first
+    names(samp)[lapply(parents,function(tr) which(names(samp)%in%tr))%>%unlist()]->order
+    #new ordered sampling space
+    samp[c(order,names(samp)[!(names(samp)%in%order)])]->samp.ordered
+    #performing the sampling
+    samples<-list()
+    for(i in seq_along(samp.ordered)){
+      
+      myparent<-samp.ordered[[i]]$parent # do I have a parent ?
+      #call the function responsible for generating the ensemble
+      do.call(paste0(names(samp.ordered[i]),".ens.gen"),
+              args = list(settings=settings,
+                          method=samp.ordered[[i]]$method,
+                          parenids=if(!is.null(myparent)) samples[[myparent]], # if I have parent then give me their ids - this is where the ordering matters making sure the parent is done before it's asked
+                          ensemble.samples)
+      )->samples[[names(samp.ordered[i])]]
+      
+    }
+    
+   
+    # if no ensemble piece was in the xml I replicare n times the first element in met and params
+    if (is.null(samples$met)) samples$met$samples<-rep(settings$run$inputs$met$path[1],settings$ensemble$size)
+    if (is.null(samples$parameters)) samples$parameters$samples<-ensemble.samples%>%purrr::map(~.x[rep(1,,settings$ensemble$size),])
+    #-some cheking if the met paths are there and params are fine
+    #------------------------End of generating ensembles-----------------------------------
+    # find all inputs that have an id
+    inputs <- names(settings$run$inputs)
+    inputs <- inputs[grepl(".id$", inputs)]
+    
+    # write configuration for each run of the ensemble
+    runs <- data.frame()
+    for (i in seq_len(settings$ensemble$size)) {
+      if (!is.null(con)) {
+        paramlist <- paste("ensemble=", i, sep = "")
+        run.id <- PEcAn.DB::db.query(paste0(
+          "INSERT INTO runs (model_id, site_id, start_time, finish_time, outdir, ensemble_id, parameter_list) ",
+          "values ('", 
           settings$model$id, "', '", 
           settings$run$site$id, "', '", 
           settings$run$start.date, "', '", 
@@ -228,59 +279,190 @@ write.ensemble.configs <- function(defaults, ensemble.samples, settings, model,
           settings$run$outdir, "', ", 
           ensemble.id, ", '", 
           paramlist, "') ",
-        "RETURNING id"), con = con)[['id']]
-      
-      # associate inputs with runs
-      if (!is.null(inputs)) {
-        for (x in inputs) {
-          PEcAn.DB::db.query(paste0("INSERT INTO inputs_runs (input_id, run_id) ",
-                          "values (", settings$run$inputs[[x]], ", ", run.id, ")"), 
-                   con = con)
+          "RETURNING id"), con = con)[['id']]
+        
+        # associate inputs with runs
+        if (!is.null(inputs)) {
+          for (x in inputs) {
+            PEcAn.DB::db.query(paste0("INSERT INTO inputs_runs (input_id, run_id) ",
+                                      "values (", settings$run$inputs[[x]], ", ", run.id, ")"), 
+                               con = con)
+          }
         }
+        
+      } else {
+        run.id <- get.run.id("ENS", left.pad.zeros(i, 5))
       }
+      runs[i, "id"] <- run.id
       
-    } else {
-      run.id <- get.run.id("ENS", left.pad.zeros(counter, 5))
+      # create folders (cleaning up old ones if needed)
+      if (clean) {
+        unlink(file.path(settings$rundir, run.id))
+        unlink(file.path(settings$modeloutdir, run.id))
+      }
+      dir.create(file.path(settings$rundir, run.id), recursive = TRUE)
+      dir.create(file.path(settings$modeloutdir, run.id), recursive = TRUE)
+      # write run information to disk
+      cat("runtype     : ensemble\n",
+          "workflow id : ", workflow.id, "\n",
+          "ensemble id : ", ensemble.id, "\n",
+          "run         : ", i, "/", settings$ensemble$size, "\n",
+          "run id      : ", run.id, "\n",
+          "pft names   : ", as.character(lapply(settings$pfts, function(x) x[['name']])), "\n",
+          "model       : ", model, "\n",
+          "model id    : ", settings$model$id, "\n",
+          "site        : ", settings$run$site$name, "\n",
+          "site  id    : ", settings$run$site$id, "\n",
+          "met data    : ", samples$met$samples[[i]], "\n",
+          "start date  : ", settings$run$start.date, "\n",
+          "end date    : ", settings$run$end.date, "\n",
+          "hostname    : ", settings$host$name, "\n",
+          "rundir      : ", file.path(settings$host$rundir, run.id), "\n",
+          "outdir      : ", file.path(settings$host$outdir, run.id), "\n",
+          file = file.path(settings$rundir, run.id, "README.txt"))
+      
+      do.call(my.write.config, args = list( defaults = defaults, 
+                                            trait.values = lapply(samples$parameters$samples, function(x, n) { x[n, , drop=FALSE] }, n=i), # this is the params
+                                            settings = settings, 
+                                            run.id = run.id,
+                                            inputs = list(met=list(path=samples$met$samples[[i]]))
+      )
+      )
+      cat(run.id, file = file.path(settings$rundir, "runs.txt"), sep = "\n", append = TRUE)
     }
-    runs[counter, "id"] <- run.id
+    return(invisible(list(runs = runs, ensemble.id = ensemble.id, samples=samples)))
+    #------------------------------------------------- if we already have everything ------------------        
+  }else{
+    #reading retsrat inputs
+    inputs<-restart$inputs
+    run.id<-restart$runid
+    new.params<-restart$new.params
+    new.state<-restart$new.state
+    ensemble.id<-restart$ensemble.id
     
-    # create folders (cleaning up old ones if needed)
-    if (clean) {
-      unlink(file.path(settings$rundir, run.id))
-      unlink(file.path(settings$modeloutdir, run.id))
+    for (i in seq_len(settings$ensemble$size)) {
+      do.call(my.write_restart, 
+              args =  list(outdir = settings$host$outdir, 
+                           runid = run.id[[i]], 
+                           start.time = restart$start.time,
+                           stop.time =restart$stop.time, 
+                           settings = settings,
+                           new.state = new.state[i, ], 
+                           new.params = new.params[[i]], 
+                           inputs =NULL, 
+                           RENAME = TRUE)
+      )
     }
-    dir.create(file.path(settings$rundir, run.id), recursive = TRUE)
-    dir.create(file.path(settings$modeloutdir, run.id), recursive = TRUE)
-    
-    # write run information to disk
-    cat("runtype     : ensemble\n",
-        "workflow id : ", workflow.id, "\n",
-        "ensemble id : ", ensemble.id, "\n",
-        "run         : ", counter, "/", settings$ensemble$size, "\n",
-        "run id      : ", run.id, "\n",
-        "pft names   : ", as.character(lapply(settings$pfts, function(x) x[['name']])), "\n",
-        "model       : ", model, "\n",
-        "model id    : ", settings$model$id, "\n",
-        "site        : ", settings$run$site$name, "\n",
-        "site  id    : ", settings$run$site$id, "\n",
-        "met data    : ", settings$run$site$met, "\n",
-        "start date  : ", settings$run$start.date, "\n",
-        "end date    : ", settings$run$end.date, "\n",
-        "hostname    : ", settings$host$name, "\n",
-        "rundir      : ", file.path(settings$host$rundir, run.id), "\n",
-        "outdir      : ", file.path(settings$host$outdir, run.id), "\n",
-        file = file.path(settings$rundir, run.id, "README.txt"))
-    
-    do.call(my.write.config, args = list(
-      defaults = defaults, 
-      trait.values = lapply(
-        ensemble.samples, function(x, n) { x[n, , drop=FALSE] }, n=counter
-      ), 
-      settings = settings, 
-      run.id = run.id)
-    )
-    cat(run.id, file = file.path(settings$rundir, "runs.txt"), sep = "\n", append = TRUE)
+    params<-new.params
+    return(invisible(list(runs = data.frame(id=run.id), ensemble.id = ensemble.id)))
   }
-
-  return(invisible(list(runs = runs, ensemble.id = ensemble.id)))
+  
+  
+  
 } # write.ensemble.configs
+
+
+
+#' Function for reurning the met ensuble based on sampling method, parent or etc
+#'
+#' @param settings list of PEcAn settings
+#' @param method Method for sampling - For now looping or sampling with replacement is implemented
+#' @param parenids If met's ids are read from a parent what are the ids
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+met.ens.gen<-function(settings,method="sampling",parenids=NULL,...){
+  #-- reading the dots and exposing them to the inside of the function
+  samples<-list()
+  dots<-list(...)
+  if (length(dots)>0) lapply(names(dots),function(name){assign(name,dots[[name]], pos=1 )})
+  #-- assing the sample ids based on different scenarios
+  if(!is.null(parenids)) {
+    samples$ids<-parenids$ids  
+    sample$ids[samples$ids>settings$run$inputs$met$path%>%length]->out.of.sample.size
+    #sample for those that our outside the param size - forexample, parent id may send id number 200 but we have only100 sample for param
+    samples(settings$run$inputs$met$path%>%seq_along(),out.of.sample.size,replace = T)->samples$ids[samples$ids%in%out.of.sample.size]
+  }else if(method=="sampling") {
+    samples$ids<-sample(settings$run$inputs$met$path%>%seq_along(),settings$ensemble$size,replace = T)  
+  }else if(method=="looping"){
+    samples$ids<-rep_len(settings$run$inputs$met$path%>%seq_along(), length.out=settings$ensemble$size)
+  }
+  #using the sample ids
+  samples$samples<-settings$run$inputs$met$path[samples$ids]
+  return(samples)
+}
+
+#' function for generating params ens
+#'
+#' @param settings list of PEcAn settings
+#' @param method  Method for sampling - For now looping or sampling with replacement is implemented
+#' @param parenids If params's id are read from a parent what are the ids
+#' @param ...  ensemble samples needs to be passed to this sample. This object is made by function get.parameter.samples
+#'
+#' @return
+#' @export
+#'
+#' @examples
+parameters.ens.gen<-function(settings,method="sampling",parenids=NULL,...){
+  #-- reading the dots and exposing them to the inside of the function
+  samples<-list()
+  dots<-list(...)
+  if (length(dots)>0) lapply(names(dots),function(name){assign(name,dots[[name]], pos=1 )})
+  
+  if(!is.null(parenids)) { # if you have a parent - ids are sent by the parent
+    #ids are sent based on the total sample size of the parent we need to make sure that the child also has as many samples
+    samples$ids<-parenids$ids
+    samples$ids[samples$ids>ensemble.samples[[1]]%>%nrow]->out.of.sample.size
+    #sample for those that our outside the param size - forexample, parent id may send id number 200 but we have only100 sample for param
+    sample(ensemble.samples[[1]]%>%nrow,length(out.of.sample.size),replace = T)->samples$ids[samples$ids%in%out.of.sample.size]
+  }else if (method=="sampling"){
+    samples$ids<-sample(ensemble.samples[[1]]%>%nrow,settings$ensemble$size,replace = T)
+  }else if(method=="looping"){
+    samples$ids<-rep_len(ensemble.samples[[1]]%>%nrow%>%vector(length=.)%>%seq_along, length.out=settings$ensemble$size)
+  }
+  samples$samples<-ensemble.samples%>%purrr::map(~.x[samples$ids,])
+  #browser()
+  #if(is.null(parenids) & method=="sampling") samples<-sample(settings$run$inputs$met%>%seq_along(),settings$ensemble$size,replace = T)
+  return(samples)
+}
+
+#' generating params for Veg parameters - needs to be implemented 
+#'
+#' @param settings 
+#' @param method 
+#' @param parenids 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+vegetation.ens.gen<-function(settings,method="sampling",parenids=NULL,...){
+  #-- reading the dots and exposing them to the inside of the function
+  dots<-list(...)
+  if (length(dots)>0) lapply(names(dots),function(name){assign(name,dots[[name]], pos=1 )})
+  samples<-list()
+  return(samples)
+}
+
+#' generating params for soil parameters - needs to be implemented 
+#'
+#' @param settings 
+#' @param method 
+#' @param parenids 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+soil.ens.gen<-function(settings,method="",parenids=NULL,...){
+  #-- reading the dots and exposing them to the inside of the function
+  dots<-list(...)
+  if (length(dots)>0) lapply(names(dots),function(name){assign(name,dots[[name]], pos=1 )})
+  samples<-list()
+  return(samples)
+}
