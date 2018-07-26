4aba03b5472ad1f2a0b8206ed962ec945aa3666c
Alexey Shiklomanov <ashiklom@bu.edu>
2018-07-26 09:41:51 -0400

    data.atmosphere: Add CF var table URL building function

--- modules/data.atmosphere/R/get_cf_variables_table.R
+++ modules/data.atmosphere/R/get_cf_variables_table.R
@@ -1,11 +1,11 @@
 #' Retrieve the current CF variables table from `cfconventions.org`
 #' and convert it into a `data.frame`
 #'
-#' @param cf_url URL of CF variables table XML
+#' @param cf_url URL of CF variables table XML. See also [build_cf_variables_table_url].
 #' @return CF variables table, as a `tibble`
 #' @author Alexey Shiklomanov
 #' @export
-get_cf_variables_table <- function(cf_url = "http://cfconventions.org/Data/cf-standard-names/57/src/cf-standard-name-table.xml") {
+get_cf_variables_table <- function(cf_url = build_cf_variables_table_url(57)) {
   raw_xml <- XML::xmlParse(cf_url)
   list_xml <- XML::xmlToList(raw_xml)
   entries <- list_xml[names(list_xml) == "entry"]
@@ -21,4 +21,20 @@ get_cf_variables_table <- function(cf_url = "http://cfconventions.org/Data/cf-st
       description,
       dplyr::everything()
     )
+}
+#' Construct a URL to a specific version of the CF variables table XML
+#'
+#' This uses [sprintf] to construct the URL with the version number as
+#' the first argument.
+#'
+#' @param version CF variables table version number (integer/numeric)
+#' @param url_format_string A format string passed to [sprintf]. This
+#'   should contain the entire target URL with the version number
+#'   replaced by `"%d"`, and _no other string substitutions_.
+#' @return Complete URL, as a string
+#' @author Alexey Shiklomanov
+#' @export
+build_cf_variables_table_url <- function(version,
+                                         url_format_string = "http://cfconventions.org/Data/cf-standard-names/%d/src/src-cf-standard-name-table.xml") {
+  sprintf(url_format_string, version)
 }
