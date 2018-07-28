db6a06e352a66148f5339b507fad0abf75320bf9
LiamBurke24 <liam.burke24@gmail.com>
2018-07-27 21:57:55 -0400

    insert.format.vars.R: Added detailed param descriptions, fixed strings/factors bug, make query dataframe with formatID for formats-variables query, and return formatID.

--- base/db/R/insert.format.vars.R
+++ base/db/R/insert.format.vars.R
@@ -1,49 +1,67 @@
 #' Insert Format and Format-Variable Records 
 #'
-#' @param con Bety connection object
-#' @param format_notes Character string used to 
-#' @param format_name 
-#' @param header 
-#' @param skip 
-#' @param mimetype_id 
+#' @param con SQL connection to BETYdb
+#' @param format_notes Character string that describes format.
+#' @param format_name Character string 
+#' @param header Boolean that indicates the presence of a header in the format 
+#' @param skip Integer that indicates the number of lines to skip in the header. Defaults to "" 
+#' @param mimetype_id Integer 
 #' @param formats_variables_df A 'data.frame' consisting of entries that correspond to columns in the formats-variables table
-#'
+#' @details It is very important that the formats_variables_df 'data.frame' be structured in a specific format so that the SQL query functions properly. First, all arguments should be passed as vectors so that each entry will correspond with a specific row.
+#' Variables: 
+#' \enumerate{
+#' \item variable_id: vector of integers
+#' \item name: vector of character strings
+#' \item unit: vector of character strings
+#' \item storage_type: vector of character strings
+#' \item column_number: vector of integers
+#' }
 #' @author Liam Burke (liam.burke24@gmail.com)
-#' @return format id
+#' @return format_id
 #' @export
 #' @examples
 #' \dontrun{
 #' bety <- betyConnect
-#'   insert.format.vars(con = bety$con, formats_df, formats_variables_df)
+#' 
+#' formats_variables_df <- tibble::tibble(
+#'        variable_id = c(327, 2.98e+02), # integer 
+#'        name = c("New Name", "Other New Name"), # 
+#'        unit = c("Yoonits", "Other Units"),
+#'        storage_type = c("%j", "%Y"),
+#'        column_number = c(as.integer(22), as.integer(24)),
+#'  )
+#'   insert.format.vars(con = bety$con, format_notes = "Info about format", format_name = "New Format", header = TRUE, skip = 2, mimetype_id = 1090, formats_variables_df)
 #' }
-insert.format.vars <- function(con, format_notes, format_name, header, skip, mimetype_id, formats_variables_df = NULL){
+insert.format.vars <- function(con, format_notes, format_name, header, skip = "", mimetype_id, formats_variables_df = NULL){
   
-    formats_df <- data.frame(
-      header = header,
+    formats_df <- tibble::tibble(
+      header = as.character(header),
       skip = skip,
       mimetype_id = mimetype_id,
       notes = format_notes,
-      name = format_name
+      name = format_name,
+      stringsAsFactors = FALSE
     )
 
     ## Insert format record
     inserted_formats <- db_merge_into(formats_df, "formats", con = con, by = c("name", "mimetype_id")) ## Make sure to include a 'by' argument
-      inserted_formats
+    format_id <- dplyr::pull(inserted_formats, id)
     
   if(!is.null(formats_variables_df)){
+    ## Insert format_id into 
+    n <- nrow(formats_variables_df)
+    format_id_df <- matrix(data = NA, nrow = n, ncol = 1)
+    for(i in 1:n){
+      format_id_df[i,1] <- format_id
+    }
+    colnames(format_id_df) <- "format_id"
+    
+    ## Make query data.frame
+    formats_variables_input <- cbind(format_id_df, formats_variables_df) 
+    
     ## Insert Format-Variable record
-    inserted_formats_variables <- db_merge_into(formats_variables_df, "formats_variables", con = con,  by = c("variable_id", "name"))
-      return(inserted_formats, inserted_formats_variables)
+    inserted_formats_variables <- db_merge_into(formats_variables_input, "formats_variables", con = con,  by = c("variable_id", "name"))
   }
+    return(format_id)
     
 }
-
-### For Testing###
-# bety <- betyConnect()
-# con = bety$con
-# format_notes = "Testing 1,2,3,4,5,6"
-# format_name = "New Name 1235678"
-# header = as.character(FALSE)
-# skip = ""
-# mimetype_id = 1090
-#######
