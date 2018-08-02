99598acdd0de2b05a33cf052af6bd632f7aed5e6
Luke-Dramko <luke.dramko@gmail.com>
2018-08-02 15:41:06 -0400

    Better description for workflow.R path in run.gefs.sipnet.EXAMPLE.sh

--- modules/assim.sequential/inst/NEFI/run.gefs.sipnet.EXAMPLE.sh
+++ modules/assim.sequential/inst/NEFI/run.gefs.sipnet.EXAMPLE.sh
@@ -5,7 +5,7 @@
 # REPLACE < username > WITH YOUR USERNAME
 # If running from a CRON job, these paths MUST be absolute paths.  This is because CRON assumes that the directory it is in is the working directory.
 xmlfile="./gefs.sipnet.source.xml" #Path to, and name of, the base xml file.
-workflow_path="./../../../../scripts/"                     #Path to workflow.R.
+workflow_path="./../../../../scripts/"                     #Path to workflow.R (in pecan/web for the standard version or pecan/scripts for the custom version).
 output_path="/fs/data3/< username >/output/"               #Path to the directory where all PEcAn output is put.
 xmlscript="./generate.gefs.xml.R"                          #Path to, and name of, the script that modifies the xml.
 # Could also be just workflow.R in pecan/web
