c5dc0636a6504084f16a589b461dd8107da49e04
Tony Gardella <tonygard@bu.edu>
2018-08-31 11:23:57 -0400

    initial base script

---
+++ base/workflow/inst/batch_runs.R
@@ -0,0 +1,100 @@
+## Function to Create and execute pecan xml
+create_exec_test_xml <- function(run_list){
+  
+  model_id <- run_list[1]
+  met <- run_list[2]
+  site_id<- run_list[3]
+  start_date<- run_list[4]
+  end_date<- run_list[5]
+  pecan_path<- run_list[6]
+  user_id<- run_list[7]
+  pft_name<- run_list[8]
+  
+  config.list <- PEcAn.utils::read_web_config(paste0(pecan_path,"/web/config.php"))
+  bety <- betyConnect(paste0(pecan_path,"/web/config.php"))
+  con <- bety$con
+  settings <- list()
+  # Info
+  settings$info$notes <- paste0("Test_Run")
+  settings$info$userid <- user_id
+  settings$info$username <- "None"
+  settings$info$dates <- Sys.Date()
+  #Outdir
+  model.new <- tbl(bety, "models") %>% filter(model_id == id) %>% collect()
+  outdir_base<-config.list$output_folder
+  outdir_pre <- paste(model.new$model_name,format(as.Date(start_date), "%Y-%m"),format(as.Date(end_date), "%Y-%m"),met,site_id,"_test_runs",sep="",collapse =NULL)
+  outdir <-  paste0(outdir_base,outdir_pre)
+  dir.create(outdir)
+  settings$outdir <- outdir
+  #Database BETY
+  settings$database$bety$user <- config.list$db_bety_username
+  settings$database$bety$password <- config.list$db_bety_password
+  settings$database$bety$host <- config.list$db_bety_hostname
+  settings$database$bety$dbname <- config.list$db_bety_database
+  settings$database$bety$driver <- "PostgreSQL"
+  settings$database$bety$write <- FALSE
+  #Database dbfiles
+  settings$database$dbfiles <- config.list$dbfiles_folder
+  #PFT
+  if (is.na(pft_name)){
+    pft <- tbl(bety, "pfts") %>% filter(modeltype_id == model.new$modeltype_id) %>% collect()
+    pft_name <- pft$name[1]
+  }
+  settings$pfts$pft$name <- pft_name
+  settings$pfts$pft$constants$num <- 1
+  #Meta Analysis
+  settings$meta.analysis$iter <- 3000
+  settings$meta.analysis$random.effects <- FALSE
+  #Ensemble
+  settings$ensemble$size <- 1
+  settings$ensemble$variable <- "GPP"
+  settings$ensemble$samplingspace$met$method <- "sampling"
+  settings$ensemble$samplingspace$parameters$method <- "uniform"
+  #Model
+  settings$model$id <- model.new$id
+  #Worflow
+  settings$workflow$id <- paste0("Test_run_","_",model.new$model_name)
+  settings$run$site$id <- site_id
+  settings$run$site$met.start <- start_date
+  settings$run$site$met.end <- end_date
+  settings$run$inputs$met$source <- met
+  settings$run$inputs$met$output <- model.new$model_name
+  settings$run$inputs$met$username <- "pecan" 
+  settings$run$start.date <- start_date
+  settings$run$end.date <- end_date
+  settings$host$name <-config.list$db_bety_hostname
+  
+  #create file and Run
+  saveXML(listToXml(settings, "pecan"), file=paste0(outdir,"/","pecan.xml"))
+  file.copy(paste0(config.list$pecan_home,"web/","workflow.R"),to = outdir)
+  setwd(outdir)
+  source("workflow.R")
+  
+}
+
+
+
+
+##Create Run Args
+pecan_path <- "~/pecan"
+config.list <- PEcAn.utils::read_web_config(paste0(pecan_path,"/web/config.php"))
+bety <- betyConnect(paste0(pecan_path,"/web/config.php"))
+con <- bety$con
+machineid <- 99000000001 #ID of the VM
+model_ids <- tbl(bety, "dbfiles") %>% filter(machine_id == machineid) %>% 
+  filter(container_type == "Model")%>% select(container_id) %>% collect()
+
+
+models <- model_ids$container_id
+met_name <- c("CRUNCEP","AmerifluxLBL")
+site_id <- "772"
+startdate<-"2004/01/01"
+enddate<-"2004/12/31"
+
+#Create permutations of arg combinations
+run_table <- expand.grid(models,met_name,site_id, startdate,enddate,pecan_path,scientific =FALSE)
+
+#Apply function by row of run_table. Return NA if error occurs
+apply(run_table,MARGIN = 1,FUN = purrr::possibly(create_exec_test_xml,otherwise = NA))
+
+
