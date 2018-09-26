1292a084247a191bd3ffc05b7737b29bc9ffee5a
Tony Gardella <tonygard@bu.edu>
2018-09-26 13:18:46 -0400

    clean

--- scripts/build_staticdocs.R
+++ scripts/build_staticdocs.R
@@ -8,15 +8,6 @@ pkg_list<- list.dirs(path = ".", full.names = FALSE, recursive = TRUE) %>%
            grep("man$",., value = TRUE) %>%
            dirname(.)
 pkg_list <- pkg_list[-1]
-#as.data.frame(.) 
-#%>%
-#  t <- filter(pkg_list,grepl(paste(pkg_patterns, collapse="$"),.))
-#%>%
-#dirname(as.vector(t$.))
-#pat <- "PEcAn"
-#packages <- paste0(installed.packages()[,2],"/",installed.packages()[,1])
-#pecan_pkg <- grep(pat,packages,value = TRUE)
-
 
 lapply(pkg_list, function(x){
   print(x)
