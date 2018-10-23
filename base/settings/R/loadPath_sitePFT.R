bc771b938a59f1ea491a5185e22b1fbd1776789d
hamzed <hamzed@bu.edu>
2018-10-23 10:34:34 -0400

    path loader

--- base/settings/R/loadPath_sitePFT.R
+++ base/settings/R/loadPath_sitePFT.R
@@ -14,7 +14,7 @@ loadPath.sitePFT<-function(settings, Path){
   
   if (ext=="csv" | ext=="txt"){
     # reading in the links
-    links <- read.table(file.path(settings$outdir,Path), header = T, sep = ",")
+    links <- read.table(file.path(Path), header = T, sep = ",")
     #check to make sure the input file is what we expect it.
     if (nrow(links)==0 | ncol(links)==0 | ncol(links)!=2) PEcAn.logger::logger.severe("There is a problem with reading the file. Either row number, column number is zero or your file does not have a two columns. ")
     
