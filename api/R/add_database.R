107810c1ebcf90a77ffa08e5bb77d314317c8da4
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2018-11-08 10:50:03 -0500

    API: Add dbfiles to add_database

--- api/R/add_database.R
+++ api/R/add_database.R
@@ -2,11 +2,12 @@
 #'
 #' @param settings Input settings list (list)
 #' @param host Database server hostname (character, default =
-#'   "postgres", so it works with Docker) 
+#'   "postgres", so it works with Docker)
 #' @param user Database user name (character, default = "bety")
 #' @param password Database password (character, default = "bety")
 #' @param dbname Database name (character, default = "bety")
 #' @param driver Database driver (character, default = "PostgreSQL")
+#' @param dbfiles Path to `dbfiles` directory (character, default = "/data/dbfiles")
 #' @param write Whether or not to write to the database (logical,
 #'   default = `TRUE`)
 #' @param overwrite Whether or not to overwrite any already existing
@@ -21,12 +22,20 @@ add_database <- function(settings,
                          password = "bety",
                          dbname = "bety",
                          driver = "PostgreSQL",
+                         dbfiles = "/data/dbfiles/",
                          write = TRUE,
                          overwrite = FALSE,
                          ...) {
-  bety_list <- list(host = host, user = user, password = password,
-                  dbname = dbname, driver = driver, write = write, ...)
-  db_list <- list(database = list(bety = bety_list))
+  bety_list <- list(
+    host = host,
+    user = user,
+    password = password,
+    dbname = dbname,
+    driver = driver,
+    write = write,
+    ...
+  )
+  db_list <- list(database = list(bety = bety_list, dbfiles = dbfiles))
   new_settings <- modifyList(settings, db_list)
   if (!overwrite) new_settings <- modifyList(new_settings, settings)
   new_settings
