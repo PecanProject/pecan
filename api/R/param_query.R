c144c2d7914b066efe3abb7abd19942250f76f3e
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2018-11-08 10:49:33 -0500

    API: Export param_query and add param_statement

--- api/R/param_query.R
+++ api/R/param_query.R
@@ -5,6 +5,7 @@
 #' @param params Query parameters (unnamed list)
 #' @return Query result, as `data.frame`
 #' @author Alexey Shiklomanov
+#' @export
 param_query <- function(con, query, params) {
   stopifnot(
     class(con) == "PqConnection",
@@ -16,4 +17,23 @@ param_query <- function(con, query, params) {
   res <- DBI::dbBind(qry, params)
   on.exit(DBI::dbClearResult(res))
   DBI::dbFetch(res)
+}
+
+#' Run prepared statement (e.g. `insert`, `delete`, `update`)
+#'
+#' @inheritParams param_query
+#' @return Summary of operation (output of [DBI::dbBind])
+#' @author Alexey Shiklomanov
+#' @export
+param_statement <- function(con, query, params) {
+  stopifnot(
+    class(con) == "PqConnection",
+    is.character(query),
+    length(query) == 1,
+    is.list(params)
+  )
+  qry <- DBI::dbSendStatement(con, query)
+  res <- DBI::dbBind(qry, params)
+  return(res)
+  on.exit(DBI::dbClearResult(res))
 }
