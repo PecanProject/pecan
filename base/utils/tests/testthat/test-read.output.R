a686e965543d03042762d2da991acf39373a4d37
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-01-09 17:57:49 -0500

    UTILS: Suppress `read.output` test output for cleaner testthat log
    This output is stored as a character, to which we can add more tests later.

--- base/utils/tests/testthat/test-read.output.R
+++ base/utils/tests/testthat/test-read.output.R
@@ -8,13 +8,17 @@ teardown(unlink(testdir, recursive = TRUE))
 test_that("returns a list or dataframe as requested", {
 	times_to_netcdf(0:364, "days since 2001-01-01", testdir, "2001.nc")
 	
-	listres <- read.output(runid = "", outdir = testdir, variables = "Y", dataframe = FALSE)
+        out_log <- capture.output(type = "message", {
+	  listres <- read.output(runid = "", outdir = testdir, variables = "Y", dataframe = FALSE)
+        })
 	expect_is(listres, "list")
 	expect_length(listres, 1)
 	expect_setequal(names(listres), "Y")
 	expect_length(listres$Y, 365)
 
-	dfres <- read.output(runid = "", outdir = testdir, variables = "Y", dataframe = TRUE)
+	out_log <- capture.output(type = "message", {
+          dfres <- read.output(runid = "", outdir = testdir, variables = "Y", dataframe = TRUE)
+        })
 	expect_s3_class(dfres, "data.frame")
 	expect_equal(dim(dfres), c(365, 3))
 	expect_setequal(names(dfres), c("posix", "year", "Y"))
@@ -28,11 +32,13 @@ test_that("accepts start and end years as string, number, datetime", {
 	times_to_netcdf(0:364, "days since 2002-01-01", testdir, "2002.nc")
 	times_to_netcdf(0:364, "days since 2003-01-01", testdir, "2003.nc")
 
-	res_all <- read.output(runid = "", outdir = testdir, variables = "Y", dataframe = TRUE)
-	res_str <- read.output(runid = "", outdir = testdir, variables = "Y", start.year = "2001", end.year = "2003", dataframe = TRUE)
-	res_num <- read.output(runid = "", outdir = testdir, variables = "Y", start.year = 2001, end.year = 2003, dataframe = TRUE)
-	res_date <- read.output(runid = "", outdir = testdir, variables = "Y", start.year = as.Date("2001-01-01"), end.year = as.Date("2003-06-23"), dataframe = TRUE)
-	res_posix <- read.output(runid = "", outdir = testdir, variables = "Y", start.year = as.POSIXct("2001-01-01 08:22:33"), end.year = as.POSIXct("2003-12-16 23:22:53"), dataframe = TRUE)
+        out_log <- capture.output(type = "message", {
+          res_all <- read.output(runid = "", outdir = testdir, variables = "Y", dataframe = TRUE)
+          res_str <- read.output(runid = "", outdir = testdir, variables = "Y", start.year = "2001", end.year = "2003", dataframe = TRUE)
+          res_num <- read.output(runid = "", outdir = testdir, variables = "Y", start.year = 2001, end.year = 2003, dataframe = TRUE)
+          res_date <- read.output(runid = "", outdir = testdir, variables = "Y", start.year = as.Date("2001-01-01"), end.year = as.Date("2003-06-23"), dataframe = TRUE)
+          res_posix <- read.output(runid = "", outdir = testdir, variables = "Y", start.year = as.POSIXct("2001-01-01 08:22:33"), end.year = as.POSIXct("2003-12-16 23:22:53"), dataframe = TRUE)
+        })
 
 	expect_equivalent(res_all, res_str)
 	expect_equivalent(res_all, res_num)
@@ -57,7 +63,9 @@ test_that("handles arbitrary time offsets", {
 	times_to_netcdf(365:730, "days since 2003-01-01", testdir, "2004.nc")
 	times_to_netcdf( ((0:364)+916) * 24, "hours since 2002-06-30", testdir, "2005.nc")
 
-	mixedres = read.output(runid = "", outdir = testdir, variables = "Y", dataframe = TRUE, start.year = 2004, end.year = 2006)
+	out_log <- capture.output(type = "message", {
+          mixedres = read.output(runid = "", outdir = testdir, variables = "Y", dataframe = TRUE, start.year = 2004, end.year = 2006)
+        })
 
 	expect_length(mixedres$posix, 731)
 	
