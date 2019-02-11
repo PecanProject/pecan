a3334865b6f82082a770ad766e29505932daec5e
Rob Kooper <kooper@illinois.edu>
2019-02-11 11:19:47 -0600

    move install to script from travis

---
+++ scripts/travis/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+. $( dirname $0 )/func.sh
+
+# INSTALLING SIPNET
+(
+    travis_time_start "install_sipnet" "Installing SIPNET for testing"
+
+    curl -o sipnet_unk.tar.gz http://isda.ncsa.illinois.edu/~kooper/EBI/sipnet_unk.tar.gz
+    tar zxf sipnet_unk.tar.gz
+    cd sipnet_unk
+    make
+
+    travis_time_end
+)
+
+# INSTALLING BIOCRO
+(
+    travis_time_start "install_biocro" "Installing BioCro"
+
+    curl -sL https://github.com/ebimodeling/biocro/archive/0.95.tar.gz | tar zxf -
+    cd biocro-0.95
+    rm configure
+    R CMD INSTALL .
+
+    travis_time_end
+)
