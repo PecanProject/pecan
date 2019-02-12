19ac929b114fa9331abfc001d2661cc7779e9de3
Rob Kooper <kooper@illinois.edu>
2019-02-11 23:43:43 -0600

    Update install.sh

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -7,6 +7,7 @@ set -e
 (
     travis_time_start "install_sipnet" "Installing SIPNET for testing"
 
+    cd ${HOME}
     curl -o sipnet_unk.tar.gz http://isda.ncsa.illinois.edu/~kooper/EBI/sipnet_unk.tar.gz
     tar zxf sipnet_unk.tar.gz
     cd sipnet_unk
@@ -16,7 +17,10 @@ set -e
     travis_time_end
 )
 
+echo ${PWD}
 echo ${HOME}
+ls -l ${PWD}/sipnet_unk/sipnet
+echo ${PWD}/sipnet_unk/sipnet
 ls -l /home/travis/sipnet_unk/sipnet
 if [ ! -e /home/travis/sipnet_unk/sipnet ]; then
     echo "ERROR NO SIPNET!"
@@ -27,6 +31,7 @@ fi
 (
     travis_time_start "install_biocro" "Installing BioCro"
 
+    cd ${HOME}
     curl -sL https://github.com/ebimodeling/biocro/archive/0.95.tar.gz | tar zxf -
     cd biocro-0.95
     rm configure
