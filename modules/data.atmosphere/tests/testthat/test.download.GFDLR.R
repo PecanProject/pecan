c1cac24dd54b038b70f41fc6330a9d2a9ed04ed5
Chris Black <chris@ckblack.org>
2019-02-12 23:02:28 +0100

    nest other tests inside server availability check
    If no connection available, will skip whole file.
    But it still waits >60s for a timeout before skipping -- need a faster check.

--- modules/data.atmosphere/tests/testthat/test.download.GFDLR.R
+++ modules/data.atmosphere/tests/testthat/test.download.GFDLR.R
@@ -4,7 +4,7 @@ tmpdir <- tempfile(pattern = "GFDLtest")
 dir.create(tmpdir, showWarnings = FALSE)
 teardown(unlink(tmpdir, recursive = TRUE))
 
-test_that("URL is openable", {
+test_that("GFDL server is reachable", {
   skip_on_travis()
 
   test_url <- paste0("http://nomads.gfdl.noaa.gov:9192/opendap/",
@@ -14,50 +14,50 @@ test_that("URL is openable", {
   test_nc <- tryCatch(
     ncdf4::nc_open(test_url, suppress_dimvals = TRUE),
     error = function(e) {
+      # NB this skips whole file-- that's why other tests are nested inside this one.
       skip("Skipping test because unable to reach NOAA GFDL download server.")
     }
   )
   teardown(ncdf4::nc_close(test_nc))
-
   expect_s3_class(test_nc, "ncdf4")
-})
 
-test_that("download works and returns a valid CF file", {
-  # Download is too slow for Travis -- please run locally before committing!
-  skip_on_travis()
+  test_that("download works and returns a valid CF file", {
+    # Download is too slow for Travis -- please run locally before committing!
+    skip_on_travis()
 
 
-  PEcAn.logger::logger.setLevel("WARN")
+    PEcAn.logger::logger.setLevel("WARN")
 
-  result <- download.GFDL(
-    outfolder = tmpdir,
-    start_date = "2007-01-01",
-    end_date = "2007-12-31",
-    site_id = 753,
-    lat.in = 40,
-    lon.in = -88
-  )
-  cf <- ncdf4::nc_open(result$file)
-  teardown(ncdf4::nc_close(cf))
-
-  # Expect that reference times are present and set to start date
-  cf_units <- cf$dim$time$units
-  expect_equal(cf_units, "seconds since 2007-01-01 00:00:00")
-
-  expect_equal(cf$dim$latitude$len, 1)
-  expect_equal(cf$dim$longitude$len, 1)
-  expect_equal(cf$dim$time$len, 365*(24/3)) # one year at 3-hr interval, leap days ignored
-  expect_equal(cf$nvar, 8)
-
-  # Expect that overwrite argument is respected
-  expect_log(
-    download.GFDL(
+    result <- download.GFDL(
       outfolder = tmpdir,
       start_date = "2007-01-01",
       end_date = "2007-12-31",
       site_id = 753,
       lat.in = 40,
-      lon.in = -88,
-      overwrite = FALSE),
-    "already exists. Skipping")
+      lon.in = -88
+    )
+    cf <- ncdf4::nc_open(result$file)
+    teardown(ncdf4::nc_close(cf))
+
+    # Expect that reference times are present and set to start date
+    cf_units <- cf$dim$time$units
+    expect_equal(cf_units, "seconds since 2007-01-01 00:00:00")
+
+    expect_equal(cf$dim$latitude$len, 1)
+    expect_equal(cf$dim$longitude$len, 1)
+    expect_equal(cf$dim$time$len, 365*(24/3)) # one year at 3-hr interval, leap days ignored
+    expect_equal(cf$nvar, 8)
+
+    # Expect that overwrite argument is respected
+    expect_log(
+      download.GFDL(
+        outfolder = tmpdir,
+        start_date = "2007-01-01",
+        end_date = "2007-12-31",
+        site_id = 753,
+        lat.in = 40,
+        lon.in = -88,
+        overwrite = FALSE),
+      "already exists. Skipping")
+  })
 })
