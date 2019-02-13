6d34928b1b9d04d2470dca485c2824d02d7e12bd
hamzed <hamzed@bu.edu>
2019-02-13 13:06:42 -0500

    if there is no con then use the local pft

--- modules/uncertainty/R/get.parameter.samples.R
+++ modules/uncertainty/R/get.parameter.samples.R
@@ -16,8 +16,8 @@ get.parameter.samples <- function(settings,
   pft.names <- list()
   outdirs   <- list()
   ## Open database connection
-  con <- PEcAn.DB::db.open(settings$database$bety)
-  on.exit(PEcAn.DB::db.close(con))
+  con <- try(PEcAn.DB::db.open(settings$database$bety), silent = TRUE)
+  on.exit(try(PEcAn.DB::db.close(con), silent = TRUE))
   
   for (i.pft in seq_along(pfts)) {
     pft.names[i.pft] <- settings$pfts[[i.pft]]$name
@@ -66,7 +66,7 @@ get.parameter.samples <- function(settings,
     }
     
     ### Load trait mcmc data (if exists, either from MA or PDA)
-    if (!is.null(settings$pfts[[i]]$posteriorid)) { # first check if there are any files associated with posterior ids
+    if (!is.null(settings$pfts[[i]]$posteriorid) & !inherits(con, "try-error")) {# first check if there are any files associated with posterior ids
       files <- PEcAn.DB::dbfile.check("Posterior",
                                       settings$pfts[[i]]$posteriorid, 
                                       con, settings$host$name, return.all = TRUE)
