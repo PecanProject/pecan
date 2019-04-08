f068046b579986dba0a5aca49981b431254996c2
bmorrison@bnl.gov <bmorrison@modex.bnl.gov>
2019-04-08 11:37:45 -0400

    added qc filter

--- modules/data.remote/R/call_MODIS.R
+++ modules/data.remote/R/call_MODIS.R
@@ -28,183 +28,131 @@
 ##' 
 ##' @author Bailey Morrison
 ##'  
-call_MODIS <- function(outfolder = ".", start_date, end_date, lat, lon, size = 0, product, band, band_qc = "", band_sd = "", siteID = NULL, package_method = "MODISTools") {
+call_MODIS <- function(outfolder = NULL, start_date, end_date, lat, lon, size = 0, product, band, band_qc = "", band_sd = "", siteID = NULL, package_method = "MODISTools", QC_filter = F) {
   
   # makes the query search for 1 pixel and not for rasters for now. Will be changed when we provide raster output support.
   size <- 0
   
-#  # For PEcAn users: finds lat lon based on a site id and also uses this to name the output file
-#  if (!(is.null(siteID)))
-#  {
-#    siteID = as.numeric(siteID)
-#    bety <- list(user='bety', password='bety', host='localhost',
-#                dbname='bety', driver='PostgreSQL',write=TRUE)
-#    con <- PEcAn.DB::db.open(bety)
-#    bety$con <- con
-#    sites <- PEcAn.DB::query.site(siteID,con)
-#    lat = sites$lat
-#    lon = sites$lon
-#  }  
-  # reformat start and end date if they are in YYYYMMDD format instead of YYYYJJJ
+
+  # reformat start and end date if they are in YYYY/MM/DD format instead of YYYYJJJ
   if (grepl("/", start_date) == T)
-  {
-    start_date = as.Date(paste0(lubridate::year(start_date), spatial.tools::add_leading_zeroes(lubridate::yday(start_date), 3)), format = "%Y%j")
-  }
+    {
+      start_date = as.Date(paste0(lubridate::year(start_date), spatial.tools::add_leading_zeroes(lubridate::yday(start_date), 3)), format = "%Y%j")
+    }
   
   if (grepl("/", end_date) == T)
-  {
-    end_date = as.Date(paste0(lubridate::year(end_date), spatial.tools::add_leading_zeroes(lubridate::yday(end_date), 3)), format = "%Y%j")
+    {
+      end_date = as.Date(paste0(lubridate::year(end_date), spatial.tools::add_leading_zeroes(lubridate::yday(end_date), 3)), format = "%Y%j")
   }
+  start_date = as.Date(start_date, format = "%Y%j")
+  end_date = as.Date(end_date, format = "%Y%j")
   
-  # set start and end dates to correct format
-  if (package_method == "MODISTools"){
-    
- products = MODISTools::mt_products()
-    if (!(product %in% products$product))
+#################### if package_method == MODISTools option ####################
+  if (package_method == "MODISTools")
     {
-      print(products)
-      stop("Product not available for MODIS API. Please chose a product from the list above.")
-    } else {
-      print("Check #1: Product exists!")
-    }
-    
-    # checks if start and end dates are within all or partial range of data available from MODIS product date range
-    dates <- MODISTools::mt_dates(product = product, lat = lat, lon = lon)$modis_date
-    dates <- as.Date(as.character(substr(dates, 2, nchar(dates))), format = "%Y%j")
-    #list total range of dates available for product
-    print(paste("Range of dates for product are ", dates[1], " - ", dates[length(dates)], sep = ""))
-    
-    #case where user only wants one date:
-    if (start_date == end_date & as.numeric(start_date) >= dates[1] & as.numeric(end_date) <= dates[length(dates)])
-    {
+  
+#################### FUNCTION PARAMETER PRECHECKS #################### 
+    #1. check that modis product is available
+      products = MODISTools::mt_products()
+      if (!(product %in% products$product))
+        {
+          print(products)
+          stop("Product not available for MODIS API. Please chose a product from the list above.")
+        } 
+ 
+     #2. check that modis produdct band is available
       bands <- MODISTools::mt_bands(product = product)
       if (!(band %in% bands$band))
+        {
+          print(bands$band)
+          stop("Band selected is not avialable. Please selected from the bands listed above that correspond with the data product.")
+        } 
+ 
+      #3. check that dates asked for in function parameters are fall within dates available for modis product/bands.
+      dates <- MODISTools::mt_dates(product = product, lat = lat, lon = lon)$modis_date
+      dates <- as.Date(as.character(substr(dates, 2, nchar(dates))), format = "%Y%j")
+
+      ########## Date case 1:  user only wants one date ##########
+      if (start_date == end_date)
       {
-        print(bands$band)
-        stop("Band selected is not avialable. Please selected from the bands listed above that correspond with the data product.")
-      } else {
-        print("Check #3: Band Exists!")
+        if (as.numeric(start_date) >= dates[1] & as.numeric(end_date) <= dates[length(dates)])
+        {
+          print("Extracting data")
+          
+          start <- as.Date(start_date, "%Y%j")
+          end <- as.Date(end_date, "%Y%j")
+        }
+        ########## For Date case 1: if only one date is asked for, but date is not within modis data prodate date range ##########
+        if (as.numeric(start_date)< dates[1] | as.numeric(end_date)> dates[length(dates)])
+        {
+          print(start)
+          stop("start or end date are not within MODIS data product date range. Please choose another date.")
+        }
+      } else { 
+      ########## Date case 2: user want a range of dates ##########
+        # Best case scenario: Start and end date asked for fall with available date range of modis data product.
+        if (as.numeric(start_date)>=dates[1] & as.numeric(end_date)<=dates[length(dates)])
+        {
+          print("Check #2: All dates are available!")
+        }
+        
+        # Okay scenario: Some MODIS data is available for parameter start_date and end_date range, but either the start_date or end_date falls outside the range of availble 
+        # MODIS data dates
+        if (as.numeric(start_date)<=dates[1] & as.numeric(end_date)>=dates[1])
+        {
+          start_date = dates[1]
+          print("WARNING: Dates are only partially available. Start date before modis data product is available.")
+        } 
+        if (as.numeric(end_date)>=dates[length(dates)] & as.numeric(start_date) <= dates[length(dates)])
+        {
+          end_date = dates[length(dates)]
+          print("WARNING: Dates are only partially available. End date befire modis data product is available.")
+        } 
+        
+        # Unacceptable scenario: start_date and end_date does not fall within the availa MODIS data product date range. There is no data to extract in this scenario.
+        if ((as.numeric(start_date)<dates[1] & as.numeric(end_date <dates[1])) | (as.numeric(start_date)>dates[length(dates)] & as.numeric(end_date)>dates[length(dates)]))
+        {
+          stop("No MODIS data available start_date and end_date parameterized.")
+        }
+        
+        start <- as.Date(start_date, "%Y%j")
+        end <- as.Date(end_date, "%Y%j")
       }
       
-      
       print("Extracting data")
-      
-      start <- as.Date(start_date, "%Y%j")
-      end <- as.Date(end_date, "%Y%j")
+      cat(paste("Product =", product, "\n", "Band =", band, "\n", "Date Range =", start, "-", end, "\n", "Latitude =", lat, "\n", "Longitude =", lon, sep = " "))
       
       # extract main band data from api
       dat <- MODISTools::mt_subset(lat=lat, lon=lon, product=product, band=band,
-                                   start=start, end=end, km_ab=size, km_lr=size)
+                                     start=start_date, end=end_date, km_ab=size, km_lr=size)
+         
       # extract QC data
-      if(band_qc != ""){
-        qc <- MODISTools::mt_subset(lat=lat, lon=lon, product=product, band=band_qc,
+      if(band_qc != "")
+        {
+          qc <- MODISTools::mt_subset(lat=lat, lon=lon, product=product, band=band_qc,
                                     start=start, end=end, km_ab=size, km_lr=size)
-      }
-      
+        }
+        
       # extract stdev data
-      if(band_sd != ""){
-        sd <- MODISTools::mt_subset(lat=lat, lon=lon, product=product, band=band_sd,
+      if(band_sd != "")
+        {
+          sd <- MODISTools::mt_subset(lat=lat, lon=lon, product=product, band=band_sd,
                                     start=start, end=end, km_ab=size, km_lr=size)
-      }
-      
-      
+        }
+    
       if (band_qc == "")
-      {
-        QC <- rep("nan", nrow(dat))
-      } else {
-        QC <- as.numeric(qc$value)
-      }
-      
+        {
+          QC <- rep("nan", nrow(dat))
+        } else {
+          QC <- as.numeric(qc$value)
+        }
+    
       if (band_sd == "")
-      {
-        SD <- rep("nan", nrow(dat))
-      } else {
-        SD <- as.numeric(sd$value) * as.numeric(sd$scale) #formatC(sd$data$data*scale, digits = 2, format = 'f')
-      }
-      
-      output <- as.data.frame(cbind(dat$modis_date, dat$calendar_date, dat$band, dat$tile, dat$latitude, dat$longitude, dat$pixel, dat$value, QC, SD), stringsAsFactors = F)
-      names(output) <- c("modis_date", "calendar_date", "band", "tile", "lat", "lon", "pixels", "data", "qc", "sd")
-      
-      output[,5:10] <- lapply(output[,5:10], as.numeric)
-      
-      # scale the data + stdev to proper units
-      output$data <- output$data * (as.numeric(dat$scale))
-      output$sd <- output$sd * (as.numeric(dat$scale))
-      output$lat <- round(output$lat, 4)
-      output$lon <- round(output$lon, 4)
-      
-      fname <- paste(product, "_", band, "output_", start_date, "_", end_date, "_", lat, "_", lon, ".csv", sep = "")
-      fname <- paste0(outfolder, "/", fname)
-      write.csv(output, fname, row.names = F)
-      return(output)
-    } else {
-    # Best case scenario: the start_date and end_date parameters fall within available MODIS data dates
-    if (as.numeric(start_date)>=dates[1] & as.numeric(end_date)<=dates[length(dates)])
-    {
-      print("Check #2: All dates are available!")
-    }
-    # Okay scenario: Some MODIS data is available for parameter start_date and end_date range, but either the start_date or end_date falls outside the range of availble 
-    # MODIS data dates
-    if (as.numeric(start_date)<=dates[1] & as.numeric(end_date)>=dates[1])
-    {
-      start_date = dates[1]
-      print("WARNING: Dates are only partially available. Look at list of available dates for MODIS data product.")
-    } 
-    if (as.numeric(end_date)>=dates[length(dates)] & as.numeric(start_date) <= dates[length(dates)])
-    {
-      end_date = dates[length(dates)]
-      print("WARNING: Dates are only partially available. Look at list of available dates for MODIS data product.")
-    } 
-    # Unacceptable scenario: start_date and end_date does not fall within the availa MODIS data product date range. There is no data to extract in this scenario.
-    if ((as.numeric(start_date)<dates[1] & as.numeric(end_date <dates[1])) | (as.numeric(start_date)>dates[length(dates)] & as.numeric(end_date)>dates[length(dates)]))
-    {
-      stop("No MODIS data available start_date and end_date parameterized.")
-    }
-    
-    bands <- MODISTools::mt_bands(product = product)
-    if (!(band %in% bands$band))
-    {
-      print(bands$band)
-      stop("Band selected is not avialable. Please selected from the bands listed above that correspond with the data product.")
-    } else {
-      print("Check #3: Band Exists!")
-    }
-    
-    
-    print("Extracting data")
-    
-    start <- as.Date(start_date, "%Y%j")
-    end <- as.Date(end_date, "%Y%j")
-    
-    # extract main band data from api
-    dat <- MODISTools::mt_subset(lat=lat, lon=lon, product=product, band=band,
-                                 start=start, end=end, km_ab=size, km_lr=size)
-    # extract QC data
-    if(band_qc != ""){
-      qc <- MODISTools::mt_subset(lat=lat, lon=lon, product=product, band=band_qc,
-                                  start=start, end=end, km_ab=size, km_lr=size)
-    }
-    
-    # extract stdev data
-    if(band_sd != ""){
-      sd <- MODISTools::mt_subset(lat=lat, lon=lon, product=product, band=band_sd,
-                                  start=start, end=end, km_ab=size, km_lr=size)
-    }
-    
-    
-    if (band_qc == "")
-    {
-      QC <- rep("nan", nrow(dat))
-    } else {
-      QC <- as.numeric(qc$value)
-    }
-    
-    if (band_sd == "")
-    {
-      SD <- rep("nan", nrow(dat))
-    } else {
-      SD <- as.numeric(sd$value) * as.numeric(sd$scale) #formatC(sd$data$data*scale, digits = 2, format = 'f')
-    }
+        {
+          SD <- rep("nan", nrow(dat))
+        } else {
+          SD <- as.numeric(sd$value) * as.numeric(sd$scale) #formatC(sd$data$data*scale, digits = 2, format = 'f')
+        }
     
     output <- as.data.frame(cbind(dat$modis_date, dat$calendar_date, dat$band, dat$tile, dat$latitude, dat$longitude, dat$pixel, dat$value, QC, SD), stringsAsFactors = F)
     names(output) <- c("modis_date", "calendar_date", "band", "tile", "lat", "lon", "pixels", "data", "qc", "sd")
@@ -217,12 +165,31 @@ call_MODIS <- function(outfolder = ".", start_date, end_date, lat, lon, size = 0
     output$lat <- round(output$lat, 4)
     output$lon <- round(output$lon, 4)
     
-    fname <- paste(product, "_", band, "output_", start_date, "_", end_date, "_", lat, "_", lon, ".csv", sep = "")
-    fname <- paste0(outfolder, "/", fname)
-    write.csv(output, fname, row.names = F)
-    return(output)
-    }
+    if (QC_filter == T)
+      {
+        output$qc == as.character(output$qc)
+        for (i in 1:nrow(output))
+         {
+            convert = paste(binaryLogic::as.binary(as.integer(output$qc[i]), n = 8), collapse = "")
+            output$qc[i] = substr(convert, nchar(convert)-2, nchar(convert))
+        }
+      good = which(output$qc == "000" | output$qc == "001")
+      if (length(good) > 0 | !(is.null(good)))
+      {
+        output = output[good,]
+      } else {
+        print("All QC values are bad. No data to output with QC filter == TRUE.")
+      }
+        }
     
+    if (!(is.null(outfolder)))
+        {
+          fname <- paste(product, "_", band, "output_", start_date, "_", end_date, "_", lat, "_", lon, ".csv", sep = "")
+          fname <- paste0(outfolder, "/", fname)
+          write.csv(output, fname, row.names = F)
+         }
+    
+    return(output)
     }
 
   
