c61ff81d7123d30b16342a1cf98b273ab98fb601
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-05-17 16:48:29 -0400

    Start cleaning up batch test file

--- base/workflow/inst/batch_runs.R
+++ base/workflow/inst/batch_runs.R
@@ -13,123 +13,156 @@
 ## D. Manually Define Met, right now CRUNCEP and AMerifluxlbl
 ## E. Manually Define start and end date
 ## F. Manually Define Output var
-## G. Manually Define Sensitivity and enesmble 
+## G. Manually Define Sensitivity and enesmble
 ## H. Find sites not associated with any inputs(meaning data needs to be downloaded), part of ameriflux network, and have data for start-end year range.
 ## Available ameriflux data is found by parsing the notes section of the sites table where ameriflux sites have a year range.
 ## Part 3 - Create Run table
 ## A. Create table that contains combinations of models,met_name,site_id, startdate, enddate, pecan_path,out.var, ensemble, ens_size, sensitivity args that the function above will use to do runs.
-## Part 4 - Run the Function across the table 
+## Part 4 - Run the Function across the table
 ## A. Use the pmap function to apply the function across each row of arguments and append a pass or fail outcome column to the original table of runs
-## Part 5 - (In progress) Turn output table into a table 
+## Part 5 - (In progress) Turn output table into a table
 
-create_execute_test_xml <- function(run_list){
-  library(PEcAn.DB)
-  library(dplyr)
-  library(PEcAn.utils)
-  library(XML)
-  library(PEcAn.settings)
-  
-  #Read in Table 
-  model_id <- run_list[[1]]
-  met <- run_list[[2]]
-  site_id<- run_list[[3]]
-  start_date<- run_list[[4]]
-  end_date<- run_list[[5]]
-  pecan_path<- run_list[[6]]
-  out.var<- run_list[[7]]
-  ensemble<- run_list[[8]]
-  ens_size<- run_list[[9]]
-  sensitivity<- run_list[[10]]
-  user_id<- NA
-  pft_name<- NA
-  
-  config.list <-read_web_config(paste0(pecan_path,"/web/config.php"))
-  bety <- betyConnect(paste0(pecan_path,"/web/config.php"))
+#' .. title/description ..
+#'
+#' @param model_id Model ID (from `models` table)
+#' @param met Meteorology input source (e.g. "CRUNCEP")
+#' @param site_id Site ID (from `sites` table)
+#' @param start_date Run start date
+#' @param end_date Run end date
+#' @param pecan_path Path to PEcAn source code. Default is current
+#'   working directory.
+#' @param user_id
+#' @param output_folder
+#' @param dbfiles_folder
+#' @param pft_name
+#' @param ensemble_size
+#' @param ensemble_variable
+#' @param sensitivity
+#' @param db_bety_username
+#' @param db_bety_password
+#' @param db_bety_hostname
+#' @param db_bety_driver
+#' @return 
+#' @author Alexey Shiklomanov
+create_execute_test_xml <- function(model_id,
+                                    met,
+                                    site_id,
+                                    start_date,
+                                    end_date,
+                                    dbfiles_folder,
+                                    user_id,
+                                    output_folder = "batch_test_output",
+                                    pecan_path = getwd(),
+                                    pft_name = NULL,
+                                    ensemble_size = 1,
+                                    ensemble_variable = "NPP",
+                                    sensitivity = FALSE,
+                                    db_bety_username = "bety",
+                                    db_bety_password = "bety",
+                                    db_bety_hostname = "localhost",
+                                    db_bety_driver = "PostgreSQL") {
+
+  php_file <- file.path(pecan_path, "web", "config.php")
+  config.list <- read_web_config(php_file)
+  bety <- betyConnect(php_file)
   con <- bety$con
-  settings <- list()
-  
-  # Info
-  settings$info$notes <- paste0("Test_Run")
-  settings$info$userid <- user_id
-  settings$info$username <- "None"
-  settings$info$dates <- Sys.Date()
+
+  settings <- list(
+    info = list(notes = "Test_Run",
+                userid = user_id,
+                username = "None",
+                dates = Sys.Date())
+  )
+
   #Outdir
-  model.new <- tbl(bety, "models") %>% filter(model_id == id) %>% collect()
-  outdir_base<-config.list$output_folder
-  outdir_pre <- paste(model.new$model_name,format(as.Date(start_date), "%Y-%m"),
-                      format(as.Date(end_date), "%Y-%m"),
-                      met,site_id,"test_runs",
-                      sep="_",collapse =NULL)
-  outdir <-  paste0(outdir_base,outdir_pre)
-  dir.create(outdir)
-  settings$outdir <- outdir
+  model.new <- tbl(bety, "models") %>%
+    filter(model_id == id) %>%
+    collect()
+  outdir_pre <- paste(
+    model.new[["model_name"]],
+    format(as.Date(start_date), "%Y-%m"),
+    format(as.Date(end_date), "%Y-%m"),
+    met, site_id, "test_runs",
+    sep = "_"
+  )
+  outdir <-  file.path(output_folder, outdir_pre)
+  dir.create(outdir, showWarnings = FALSE, recursive = TRUE)
+  settings[["outdir"]] <- outdir
+
   #Database BETY
-  settings$database$bety$user <- config.list$db_bety_username
-  settings$database$bety$password <- config.list$db_bety_password
-  settings$database$bety$host <- "localhost"
-  settings$database$bety$dbname <- config.list$db_bety_database
-  settings$database$bety$driver <- "PostgreSQL"
-  settings$database$bety$write <- FALSE
-  #Database dbfiles
-  settings$database$dbfiles <- config.list$dbfiles_folder
+  settings[["database"]] <- list(
+    bety = list(user = db_bety_username,
+                password = db_bety_password,
+                host = db_bety_hostname,
+                dbname = "bety",
+                driver = db_bety_driver,
+                write = FALSE),
+    dbfiles = dbfiles_folder
+  )
+
   #PFT
-  if (is.na(pft_name)){
-    pft <- tbl(bety, "pfts") %>% filter(modeltype_id == model.new$modeltype_id) %>% collect()
-    pft_name <- pft$name[1]
+  if (is.null(pft_name)){
+    # Select the first PFT in the model list.
+    pft <- tbl(bety, "pfts") %>%
+      filter(modeltype_id == model.new$modeltype_id) %>%
+      collect()
+    pft_name <- pft$name[[1]]
   }
-  settings$pfts$pft$name <- pft_name
-  settings$pfts$pft$constants$num <- 1
+  settings[["pfts"]] <- list(
+    pft = list(name = pft_name,
+               constants = list(num = 1))
+  )
+
   #Meta Analysis
-  settings$meta.analysis$iter <- 3000
-  settings$meta.analysis$random.effects <- FALSE
+  settings[["meta.analysis"]] <- list(iter = 3000, random.effects = FALSE)
+
   #Ensemble
-  if(ensemble){
-    settings$ensemble$size <- ens_size
-    settings$ensemble$variable <- out.var
-    settings$ensemble$samplingspace$met$method <- "sampling"
-    settings$ensemble$samplingspace$parameters$method <- "uniform"
-  }else{
-    settings$ensemble$size <- 1
-    settings$ensemble$variable <- out.var
-    settings$ensemble$samplingspace$met$method <- "sampling"
-    settings$ensemble$samplingspace$parameters$method <- "uniform"
+  settings[["ensemble"]] <- list(
+    size = ensemble_size,
+    variable = ensemble_variable,
+    samplingspace = list(met = list(method = "sampling"),
+                         parameters = list(method = "uniform"))
+  )
+
+  #Sensitivity 
+  if (sensitivity) {
+    settings[["sensitivity.analysis"]] <- list(
+      quantiles = list(sigma1 = -2, sigma2 = -1, sigma3 = 1, sigma4 = 2)
+    )
   }
-  #Sensitivity
-  if(sensitivity){
-    settings$sensitivity.analysis$quantiles <-
-      settings$sensitivity.analysis$quantiles$sigma1 <--2
-    settings$sensitivity.analysis$quantiles$sigma2 <--1
-    settings$sensitivity.analysis$quantiles$sigma3 <- 1
-    settings$sensitivity.analysis$quantiles$sigma4 <- 2
-    names(settings$sensitivity.analysis$quantiles) <-c("sigma","sigma","sigma","sigma")
-  }
+
   #Model
-  settings$model$id <- model.new$id
-  #Worflow
-  settings$workflow$id <- paste0("Test_run_","_",model.new$model_name)
-  settings$run$site$id <- site_id
-  settings$run$site$met.start <- start_date
-  settings$run$site$met.end <- end_date
-  settings$run$inputs$met$source <- met
-  settings$run$inputs$met$output <- model.new$model_name
-  settings$run$inputs$met$username <- "pecan" 
-  settings$run$start.date <- start_date
-  settings$run$end.date <- end_date
-  settings$host$name <-"localhost"
-  
+  settings[[c("model", "id")]] <- model.new[["id"]]
+
+  #Workflow
+  settings[[c("workflow", "id")]] <- paste0("Test_run_","_",model.new$model_name)
+  settings[["run"]] <- list(
+    site = list(id = site_id, met.start = start_date, met.end = end_date),
+    inputs = list(met = list(source = met, output = model.new[["model_name"]],
+                             username = "pecan")),
+    start.date = start_date, end.date = end_date
+  )
+  settings[[c("host", "name")]] <- "localhost"
+
   #create file and Run
-  saveXML(listToXml(settings, "pecan"), file=paste0(outdir,"/","pecan.xml"))
-  file.copy(paste0(config.list$pecan_home,"web/","workflow.R"),to = outdir)
+  saveXML(listToXml(settings, "pecan"), file = file.path(outdir, "pecan.xml"))
+  file.copy(file.path(pecan_path, "web", "workflow.R"), outdir)
   setwd(outdir)
   ##Name log file
   #log <- file("workflow.Rout", open = "wt")
   #sink(log)
   #sink(log, type = "message")
-  
+
   system("./workflow.R 2>&1 | tee workflow.Rout")
   #source("workflow.R")
   #sink()
 }
+
+library(tidyverse)
+library(PEcAn.DB)
+library(PEcAn.utils)
+library(XML)
+library(PEcAn.settings)
 
 ##Create Run Args
 
@@ -138,7 +171,6 @@ pecan_path <- "/fs/data3/tonygard/work/pecan"
 config.list <- PEcAn.utils::read_web_config(paste0(pecan_path,"/web/config.php"))
 bety <- PEcAn.DB::betyConnect(paste0(pecan_path,"/web/config.php"))
 con <- bety$con
-library(tidyverse)
 
 ## Find name of Machine R is running on
 mach_name <- Sys.info()[[4]]
@@ -146,8 +178,8 @@ mach_id <- tbl(bety, "machines")%>% filter(grepl(mach_name,hostname)) %>% pull(i
 
 ## Find Models
 #devtools::install_github("pecanproject/pecan", subdir = "api")
-model_ids <- tbl(bety, "dbfiles") %>% 
-  filter(machine_id == mach_id) %>% 
+model_ids <- tbl(bety, "dbfiles") %>%
+  filter(machine_id == mach_id) %>%
   filter(container_type == "Model") %>%
   pull(container_id)
 
@@ -164,11 +196,11 @@ sensitivity <- FALSE
 ## Find Sites
 ## Site with no inputs from any machines that is part of Ameriflux site group and Fluxnet Site group
 site_id_noinput<- anti_join(tbl(bety, "sites"),tbl(bety, "inputs")) %>%
-  inner_join(tbl(bety, "sitegroups_sites") %>% 
+  inner_join(tbl(bety, "sitegroups_sites") %>%
                filter(sitegroup_id == 1),
              by = c("id" = "site_id")) %>%
   dplyr::select("id.x", "notes", "sitename") %>%
-  dplyr::filter(grepl("TOWER_BEGAN", notes)) %>% 
+  dplyr::filter(grepl("TOWER_BEGAN", notes)) %>%
   collect() %>%
   dplyr::mutate(
     # Grab years from string within the notes
@@ -191,8 +223,19 @@ site_name <- gsub(" ","_",site_id_noinput$sitename)
 
 #Create permutations of arg combinations
 options(scipen = 999)
-run_table <- expand.grid(models,met_name,site_id, startdate, enddate, 
-                         pecan_path,out.var, ensemble, ens_size, sensitivity, stringsAsFactors = FALSE)
+run_table <- expand.grid(
+  models,
+  met_name,
+  site_id,
+  startdate,
+  enddate,
+  pecan_path,
+  out.var,
+  ensemble,
+  ens_size,
+  sensitivity,
+  stringsAsFactors = FALSE
+)
 #Execute function to spit out a table with a column of NA or success
 
 tab <-run_table %>% mutate(outcome = purrr::pmap(.,purrr::possibly(function(...){
