c38d154db672c532b7af79e99d095a5bda610f40
PEcAn Demo User <carya@pecan.vm>
2019-06-07 01:16:12 -0500

    add options

--- shiny/workflowPlot/server.R
+++ shiny/workflowPlot/server.R
@@ -33,6 +33,9 @@ lapply(c( "shiny",
 # Maximum size of file allowed to be uploaded: 100MB
 options(shiny.maxRequestSize=100*1024^2)
 
+options(shiny.port = 6438)
+options(shiny.launch.browser = 'FALSE')
+
 # Define server logic
 server <- shinyServer(function(input, output, session) {
   bety <- betyConnect()
