36f7623c6be14a69e09edeb7db2fc5fffdbdba12
Hamze Dokoohaki <para2x@gmail.com>
2019-06-19 09:18:36 -0400

    Update modules/assim.sequential/R/sda.enkf_MultiSite.R
    Co-Authored-By: Shawn P. Serbin <sserbin@bnl.gov>

--- modules/assim.sequential/R/sda.enkf_MultiSite.R
+++ modules/assim.sequential/R/sda.enkf_MultiSite.R
@@ -108,7 +108,7 @@ sda.enkf.multisite <- function(settings,
     Start.Year <-(lubridate::year(settings$state.data.assimilation$start.date))
   }
   
-  End.Year <-   lubridate::year(settings$state.data.assimilation$end.date) # years that assimilations will be done for - obs will be subsetted based on this
+  End.Year <- lubridate::year(settings$state.data.assimilation$end.date) # years that assimilations will be done for - obs will be subsetted based on this
   assimyears <- Start.Year:End.Year
   obs.mean <- obs.mean[sapply(year(names(obs.mean)), function(obs.year) obs.year %in% (assimyears))]
   obs.cov <- obs.cov[sapply(year(names(obs.cov)), function(obs.year) obs.year %in% (assimyears))]
