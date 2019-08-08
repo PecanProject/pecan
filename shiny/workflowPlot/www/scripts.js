412fa592c4a84cd691082b97abdd1a73048c80da
Hamze Dokoohaki <para2x@gmail.com>
2019-08-08 16:24:37 -0400

    explorebtn

--- shiny/workflowPlot/www/scripts.js
+++ shiny/workflowPlot/www/scripts.js
@@ -3,3 +3,9 @@ Shiny.onInputChange('workflowselected',this.id);
 // to report changes on the same selectInput
 Shiny.onInputChange('workflowclassrand', Math.random());
 });
+
+$(document).on('click', '.expanclass', function () {
+Shiny.onInputChange('workflows_explor_selected',this.id);
+// to report changes on the same selectInput
+Shiny.onInputChange('workflow_explor_classrand', Math.random());
+});
