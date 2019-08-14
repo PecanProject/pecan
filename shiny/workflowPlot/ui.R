c49b7120cff931afa2d0b9148cc1eb2be218fd28
sl4397 <sl4397@columbia.edu>
2019-08-14 08:47:26 +0000

    move documentation ui to a seperate file

--- shiny/workflowPlot/ui.R
+++ shiny/workflowPlot/ui.R
@@ -88,47 +88,9 @@ ui <- fluidPage(theme = shinytheme("yeti"),
                                            tabPanel("Documentation",
                                                     icon = icon("book"),
                                                     #withMathJax(includeMarkdown("markdown/workflowPlot_doc.Rmd"))
-                                                    bs_accordion_sidebar(id = "documentation",
-                                                                         spec_side = c(width = 3, offset = 0)) %>%
-                                                      bs_append(
-                                                        title_side = "App Documentation", 
-                                                        content_side = NULL,
-                                                        content_main = withMathJax(includeMarkdown("markdown/app_documentation.Rmd"))
-                                                      ) %>%
-                                                      bs_append(
-                                                        title_side = "Setup page", 
-                                                        content_side = NULL,
-                                                        content_main = withMathJax(includeMarkdown("markdown/setup_page.Rmd"))
-                                                      ) %>%
-                                                      bs_append(
-                                                        title_side = "Exploratory Plots", 
-                                                        content_side = NULL,
-                                                        content_main = withMathJax(includeMarkdown("markdown/exploratory_plot.Rmd"))
-                                                      ) %>%
-                                                      bs_append(
-                                                        title_side = "Benchmarking", 
-                                                        content_side = NULL,
-                                                        content_main = 
-                                                          bs_accordion_sidebar(id = "benchmarking") %>%
-                                                          bs_append(
-                                                            title_side = "Settings",
-                                                            content_side = NULL,
-                                                            content_main = withMathJax(includeMarkdown("markdown/benchmarking_setting.Rmd"))
-                                                          ) %>%
-                                                          bs_append(
-                                                            title_side = "Scores",
-                                                            content_side = NULL,
-                                                            content_main = withMathJax(includeMarkdown("markdown/benchmarking_scores.Rmd"))
-                                                          ) %>% 
-                                                          bs_append(
-                                                            title_side = "Plots",
-                                                            content_side = NULL,
-                                                            content_main = withMathJax(includeMarkdown("markdown/benchmarking_plots.Rmd"))
-                                                          )
-                                                      ),
+                                                    source_ui("documentation_UI.R"),
                                                     use_bs_accordion_sidebar()
-           
-                                                    
+        
                                            )
                       )
                   )
