e567cf4aea0a3a67b7d9b6e38c6b5c7d2bb10e20
Hamze Dokoohaki <para2x@gmail.com>
2019-08-15 15:50:37 -0400

    cards

--- shiny/workflowPlot/server_files/select_data_server.R
+++ shiny/workflowPlot/server_files/select_data_server.R
@@ -55,27 +55,67 @@ observeEvent(input$load_model,{
                      select.df <- rbind(select.df, README.df)
                    }
                    
+                   #hide the into msg
+                   shinyjs::hide("intromsg")
+                   
                     select.data <- select.df %>% 
                       dlply(.(V1), function(x) x[[2]]) %>% 
                       as.data.frame() %>% 
                       dplyr::rename(site.id = site..id) %>% 
-                      select(runtype, workflow.id, ensemble.id, pft.name, quantile, trait, run.id, 
+                      dplyr::select(runtype, workflow.id, ensemble.id, pft.name, quantile, trait, run.id, 
                             model, site.id, start.date, end.date, hostname, timestep, rundir, outdir)
-                     
                     
-                   output$datatable <- DT::renderDataTable(
-                     DT::datatable(select.data,
-                                   options = list(
-                                     dom = 'ft',
-                                     scrollX = TRUE,
-                                     initComplete = DT::JS(
-                                       "function(settings, json) {",
-                                       "$(this.api().table().header()).css({'background-color': '#404040', 'color': '#fff'});",
-                                       "}")
-                                     )
-                                   )
-                     )
-                  
+                    output$runsui<-renderUI({
+                      seq_len(nrow(select.data)) %>%
+                        map(
+                          function(rown){
+
+                            HTML(paste0('
+                                <div class="alert alert-dismissible alert-',sample(c("warning","info"),1),'">
+                                 <h4 class="alert-heading">',select.data$workflow.id[rown],'</h4>
+                                 <table class="table table-condensed">
+
+                              <tbody>
+                                <tr>
+                                  <th scope="col"><h5>Runtype:</h5> </th>
+                                  <th scope="col">',select.data$runtype[rown],'</th>
+                                  <th scope="col"><h5>Ensemble.id:</h5> </th>
+                                  <th scope="col">',select.data$ensemble.id[rown],'</th>
+                                </tr>
+
+                                <tr>
+                                  <th scope="row"><h5>Pft.name</h5> </th>
+                                  <td>',select.data$pft.name[rown],'</td>
+                                  <td><h5>Run.id</h5> </td>
+                                  <td>',select.data$run.id[rown],'</td>
+                                </tr>
+                                <tr>
+                                  <th scope="row"><h5>Model</h5> </th>
+                                  <td>',select.data$model[rown],'</td>
+                                  <td><h5>Site.id</h5> </td>
+                                  <td>',select.data$site.id[rown],'</td>
+                                </tr>
+                                <tr>
+                                  <th scope="row"><h5>Start.date</h5> </th>
+                                  <td>',select.data$start.date[rown],'</td>
+                                  <td><h5>End.date</h5> </td>
+                                  <td>',select.data$end.date[rown],'</td>
+                                </tr>
+                                <tr>
+                                  <th scope="row"><h5>Hostname</h5> </th>
+                                  <td>',select.data$hostname[rown],'</td>
+                                  <td><h5>Outdir</h5> </td>
+                                  <td>',select.data$outdir[rown],'</td>
+                                </tr>
+                              </tbody>
+                            </table> 
+                               </div> <br>
+                                        
+                                        '))
+                          }
+                        )
+                    })
+
                    #output$README <- renderUI({HTML(paste(README.text, collapse = '<br/>'))})
                    
                    output$dim_message <- renderText({sprintf("This data has %.0f rows,\nthink about skipping exploratory plots if this is a large number...", dim(df)[1])})
