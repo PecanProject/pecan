3a78880e0ff765f5828cf691c1259e04a64f3211
Chris Black <chris@ckblack.org>
2019-09-17 17:46:28 +0200

    use local file for read_web_config test. Fixes #2417

--- base/utils/tests/testthat/test.read_web_config.R
+++ base/utils/tests/testthat/test.read_web_config.R
@@ -1,19 +1,28 @@
 context("Read web config")
 
-# `here` package needed to correctly set path relative to package
-skip_if_not_installed("here")
-pecan_root <- normalizePath(here::here("..", ".."))
+php_config_example <- file.path("data", "config.example.php")
 
 test_that("Read example config file", {
-  php_config_example <- file.path(pecan_root, "web", "config.example.php")
   cfg_example <- read_web_config(php_config_example)
   expect_equal(cfg_example[["output_folder"]], "/home/carya/output/")
   expect_equal(cfg_example[["dbfiles_folder"]], "/home/carya/output//dbfiles")
 })
 
-test_that("Read docker config file", {
-  php_config_docker <- file.path(pecan_root, "docker", "web", "config.docker.php")
-  cfg_docker <- read_web_config(php_config_docker)
-  expect_equal(cfg_docker[["output_folder"]], "/data/workflows")
-  expect_equal(cfg_docker[["dbfiles_folder"]], "/data/dbfiles")
+test_that("parse converts types", {
+  cfg_example <- read_web_config(php_config_example, parse = FALSE)
+  expect_type(cfg_example[["pagesize"]], "character")
+  expect_equal(cfg_example[["pagesize"]], "30")
+
+  # parse not currently working; uncomment when fixed
+  # cfg_example <- read_web_config(php_config_example, parse = TRUE)
+  # expect_type(cfg_example[["pagesize"]], "double")
+  # expect_equal(cfg_example[["pagesize"]], 30)
+})
+
+test_that("expand replaces output_folder", {
+  cfg_example <- read_web_config(php_config_example, expand = FALSE)
+  expect_equal(cfg_example[["dbfiles_folder"]], "$output_folder . /dbfiles")
+
+  cfg_example <- read_web_config(php_config_example, expand = TRUE)
+  expect_equal(cfg_example[["dbfiles_folder"]], "/home/carya/output//dbfiles")
 })
