005173f880614830c855392744559d1bd7878332
Chris Black <chris@ckblack.org>
2019-09-19 00:58:47 +0200

    replace hand-written Rd stub with Roxygen

---
+++ models/dalec/R/get.model.output.dalec.R
@@ -0,0 +1,6 @@
+#' Retrieve model output from local or remote server
+#'
+#' @export
+get.model.output.dalec <- function(){
+	# not yet implemented, just put here to go along with its Rd skeleton
+}
