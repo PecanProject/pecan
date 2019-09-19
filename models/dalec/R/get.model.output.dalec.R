2eac81f9dc842ed366c7b9d5ea7b3ac946cd3450
Chris Black <chris@ckblack.org>
2019-09-19 15:07:48 +0200

    remove dummy functions

--- models/dalec/R/get.model.output.dalec.R
+++
@@ -1,6 +0,0 @@
-#' Retrieve model output from local or remote server
-#'
-#' @export
-get.model.output.dalec <- function(){
-	# not yet implemented, just put here to go along with its Rd skeleton
-}
