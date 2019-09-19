4723b4b0062de9f0039292c9348df152b35d92aa
hamzed <hamzed@bu.edu>
2019-09-19 12:35:46 -0400

    Fall back to the ncss method

--- modules/data.atmosphere/R/download.CRUNCEP_Global.R
+++ modules/data.atmosphere/R/download.CRUNCEP_Global.R
@@ -24,9 +24,9 @@
 ##' @author James Simkins, Mike Dietze, Alexey Shiklomanov
 download.CRUNCEP <- function(outfolder, start_date, end_date, site_id, lat.in, lon.in,
                              overwrite = FALSE, verbose = FALSE, maxErrors = 10, sleep = 2,
-                             method = "opendap", ...) {
+                             method = "ncss", ...) {
 
-  if (is.null(method)) method <- "opendap"
+  if (is.null(method)) method <- "ncss"
   if (!method %in% c("opendap", "ncss")) {
     PEcAn.logger::logger.severe(glue::glue(
       "Bad method '{method}'. Currently, only 'opendap' or 'ncss' are supported."
@@ -220,7 +220,7 @@ download.CRUNCEP <- function(outfolder, start_date, end_date, site_id, lat.in, l
                                     "but got", min(dap_time), "..", max(dap_time))
       }
 
-
+      
       dat.list[[j]] <- PEcAn.utils::retry.func(
         ncdf4::ncvar_get(
           dap,
