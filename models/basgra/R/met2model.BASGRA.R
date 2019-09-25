9c76ce39f32c99ed182906238b629058c4c82d30
istfer <fer.istem@gmail.com>
2019-09-25 06:30:02 -0400

    start met2model

--- models/basgra/R/met2model.BASGRA.R
+++ models/basgra/R/met2model.BASGRA.R
@@ -11,26 +11,19 @@
 ##' Converts a met CF file to a model specific met file. The input
 ##' files are calld <in.path>/<in.prefix>.YYYY.cf
 ##'
-##' @name met2model.MODEL
-##' @title Write MODEL met files
+##' @name met2model.BASGRA
+##' @title Write BASGRA met files
 ##' @param in.path path on disk where CF file lives
 ##' @param in.prefix prefix for each file
 ##' @param outfolder location where model specific output is written.
+##' @param start_date beginning of the weather data
+##' @param end_date end of the weather data
 ##' @return OK if everything was succesful.
 ##' @export
-##' @author Rob Kooper
+##' @author Istem Fer
 ##-------------------------------------------------------------------------------------------------#
-met2model.MODEL <- function(in.path, in.prefix, outfolder, overwrite = FALSE) {
-  PEcAn.logger::logger.severe("NOT IMPLEMENTED")
+met2model.BASGRA <- function(in.path, in.prefix, outfolder, overwrite = FALSE, 
+                             start_date, end_date, ...) {
 
-  # Please follow the PEcAn style guide:
-  # https://pecanproject.github.io/pecan-documentation/master/coding-style.html
   
-  # Note that `library()` calls should _never_ appear here; instead, put
-  # packages dependencies in the DESCRIPTION file, under "Imports:".
-  # Calls to dependent packages should use a double colon, e.g.
-  #    `packageName::functionName()`.
-  # Also, `require()` should be used only when a package dependency is truly
-  # optional. In this case, put the package name under "Suggests:" in DESCRIPTION. 
-  
-} # met2model.MODEL
+} # met2model.BASGRA
