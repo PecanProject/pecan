6f6725d19e3a9e6c378129e23a1c691340113db8
Kah5 <kellyannheilman@gmail.com>
2019-10-29 15:49:38 -0700

    updating rdriver vice to only sample DBH measurements from plots where we also have tree core samples

--- modules/data.land/R/RDrivervice.R
+++ modules/data.land/R/RDrivervice.R
@@ -7,10 +7,7 @@
 #### including time-varying effects
 
 ### prep data files into jags objects
-setwd("/home/rstudio/")
-
-### load in the data for trees with increment cores (and 1 or 2 DBH measurements)
-AZ.PIPO <- read.delim("data/AZ_FIA_RWL_PRISM_allinone_04192017.txt", stringsAsFactors = F) ### 820 trees
+AZ.PIPO <- read.delim("FIA_inc_data/AZ_FIA_RWL_PRISM_allinone_04192017.txt", stringsAsFactors = F) ### 820 trees
 
 ### merge together three diameter columns
 AZ.PIPO$DIA <- ifelse(is.na(AZ.PIPO$TREE_DIA), AZ.PIPO$SITETREE_DIA, AZ.PIPO$TREE_DIA) # combine together FIADB diameter measurements for trees and site trees
@@ -28,7 +25,7 @@ temp1 <- AZ.PIPO[AZ.PIPO$PLOT_MEASYEAR-AZ.PIPO$DateEnd<2,] # 544 trees
 temp2 <- temp1[temp1$PLOT_MEASYEAR-temp1$DateEnd>-1,] # no change
 
 ### load in the data for trees without increment cores ("tree-to-tree" 2 DBH measurements)
-Tree2Tree <- read.csv("data/Tree2Tree.csv", stringsAsFactors = F)
+Tree2Tree <- read.csv("FIA_inc_data/Tree2Tree.csv", stringsAsFactors = F)
 
 ### limit analysis to those trees with second DBH measurement in =< year 2015
 ### this is because as of 7/2017, the available PRISM data (KNMI) go only to Dec 2015
@@ -38,23 +35,58 @@ Tree2Tree <- Tree2Tree[Tree2Tree$T2_MEASYR<=2015,]
 Tree2Tree <- Tree2Tree[!is.na(Tree2Tree$SICOND),]
 Tree2Tree <- Tree2Tree[!is.na(Tree2Tree$SDIc),]
 
+
+FIA.COND <- read.csv("/Users/kah/Documents/docker_pecan/pecan/FIA_inc_data/AZ_COND.csv", stringsAsFactors = F)
+Tree2Tree <- merge(FIA.COND[c("DSTRBYR1", "DSTRBYR2", "DSTRBYR3", "CN", "PLT_CN", "INVYR", "PLOT")], Tree2Tree, by.x = "PLT_CN", by.y = "T1_PLT_CN")
+unique(temp2$Plot) %in% unique(Tree2Tree$T1_PLOT)
+
+distrub.plot <- merge(temp2, unique(Tree2Tree[,c("T1_PLOT", "DSTRBYR1", "DSTRBYR2", "DSTRBYR3")]),  by.y = "T1_PLOT", by.x = "PlotNo", all.x = TRUE, all.y = FALSE)
+
+newtemp2 <- distrub.plot[!duplicated(distrub.plot),]
+
+#ggplot(distrub.plot, aes())
+
+# join trees to cond table
+
+# join cond table
+
 ### NOW go get function that makes jags objects out of the above
 ### setwd to github folder
-#setwd("modules/data.land/R/")
+setwd("/home/rstudio/pecan/modules/data.land/R")
 
+setwd("pecan/modules/data.land/R")
 
 ### read in function that creates jags objects from above data
-source("pecan/modules/data.land/R/BuildJAGSdataobject.R")
-jags.stuff.nocores <- buildJAGSdataobject(temp2, Tree2Tree, rnd.subset = 100, trunc.yr = 1966)
+source("modules/data.land/R/BuildJAGSdataobject.R")
+#jags.stuff <- buildJAGSdataobject(temp2, Tree2Tree, rnd.subset = 5000, trunc.yr = 1966)
 # if you don't have trees without cores, use the following line
 # or you wish to not include trees without cores
-jags.stuff <- buildJAGSdataobject(temp2, rnd.subset = 100, trunc.yr = 1966)
+T2T.nodup <- Tree2Tree[!duplicated(Tree2Tree),]
+
+# sample from DBH measurements that only exist in plots that were also cored
+
+Tree2Tree.incored.plots <- Tree2Tree[paste0(Tree2Tree$COUNTYCD, Tree2Tree$PLOT) %in% paste0(newtemp2$CountyNo, newtemp2$PlotNo) ,]
+
+# looks like there are 5794 dbh measurements from plots where we also have tree cores:
+# > length(Tree2Tree.incored.plots$PLT_CN)
+# [1] 5794
+
+# check that we only have plot ids with tree cores
+#unique(paste0(Tree2Tree.incored.plots$COUNTYCD, Tree2Tree.incored.plots$PLOT)) %in% unique(paste0(newtemp2$CountyNo, newtemp2$PlotNo))
+
+
+jags.stuff <- buildJAGSdataobject(temp2 = newtemp2, Tree2Tree = Tree2Tree.incored.plots, rnd.subset = 100, trunc.yr = 1966)
+
+
+# 
+#jags.stuff <- buildJAGSdataobject(newtemp2,  rnd.subset = 100, trunc.yr = 1966)
+
 data <- jags.stuff$data
 z0 <- jags.stuff$z0
 cov.data <- jags.stuff$cov.data
 time_data <- jags.stuff$time_data
 
-# why are these here
+
 data$a_dbh <- 512
 data$r_dbh <- 256
 
@@ -63,6 +95,33 @@ View(data$y)
 View(data$z)
 ### initial conditions state variable (DBH)
 View(z0)
+
+### read in function that makes/executes a jags model from lmer-like call of a linear model
+# note that Evans version of Dietze function comments out creation of state variable initial conditions (z0)
+# which is done in the function buildJAGSdataobject instead
+source("/Users/kah/Documents/docker_pecan/pecan/modules/data.land/R/InventoryGrowthFusion.R") 
+
+
+# linear model with DBH^2 removed for Precipitation and 500 cores
+ppt.noX2 <- InventoryGrowthFusion(data=data, cov.data=cov.data, time_data=time_data,
+                                  n.iter=40000, z0=z0,
+                                  n.chunk=100, save.state=TRUE, random="(1|PLOT[i])  + (1|FIRE[i])",
+                                  fixed = "~ X + SICOND + SDI + SDI*X + SICOND*X + X*wintP.wateryr[t] + SICOND*SDI",
+                                  time_varying = "wintP.wateryr + SDI*wintP.wateryr[t] + SICOND*wintP.wateryr[t]",
+                                  burnin_plot=FALSE, save.jags = "PPT.noX2.5000nocores.40000.txt", model.name = "PPT.noX2.5000nocores.40000.txt", 
+                                  output.folder = "IGF_PIPO_AZ_mcmc/", breakearly = FALSE)
+
+
+# linear model with DBH^2 removed for Precipitation + Tmax and 500 cores
+ppt.noX2 <- InventoryGrowthFusion(data=data, cov.data=cov.data, time_data=time_data,
+                                  n.iter=40000, z0=z0,
+                                  n.chunk=100, save.state=TRUE, random="(1|PLOT[i])",
+                                  fixed = "~ X + SICOND + SDI + SDI*X + SICOND*X + X*wintP.wateryr[t] + SICOND*SDI",
+                                  time_varying = "wintP.wateryr + SDI*wintP.wateryr[t] + SICOND*wintP.wateryr[t]",
+                                  burnin_plot=FALSE, save.jags = "PPT.noX2.5000nocores.40000.txt", model.name = "PPT.noX2.5000nocores.40000.txt", 
+                                  output.folder = "IGF_PIPO_AZ_mcmc/", breakearly = FALSE)
+
+
 
 ### read in function that makes/executes a jags model from lmer-like call of a linear model
 
@@ -591,7 +650,7 @@ Tree2Tree <- Tree2Tree[!is.na(Tree2Tree$SDIc),]
 
 
 FIA.COND <- read.csv("/Users/kah/Documents/docker_pecan/pecan/FIA_inc_data/AZ_COND.csv", stringsAsFactors = F)
-Tree2Tree <- merge(FIA.COND[c("DSTRBYR1", "DSTRBYR2", "DSTRBYR3", "CN", "PLT_CN", "INVYR", "STATECD", "UNITCD", "COUNTYCD", "PLOT")], Tree2Tree, by.x = "PLT_CN", by.y = "T1_PLT_CN")
+Tree2Tree <- merge(FIA.COND[c("DSTRBYR1", "DSTRBYR2", "DSTRBYR3", "CN", "PLT_CN", "INVYR", "PLOT")], Tree2Tree, by.x = "PLT_CN", by.y = "T1_PLT_CN")
 unique(temp2$Plot) %in% unique(Tree2Tree$T1_PLOT)
 
 distrub.plot <- merge(temp2, unique(Tree2Tree[,c("T1_PLOT", "DSTRBYR1", "DSTRBYR2", "DSTRBYR3")]),  by.y = "T1_PLOT", by.x = "PlotNo", all.x = TRUE, all.y = FALSE)
@@ -615,8 +674,25 @@ source("modules/data.land/R/BuildJAGSdataobject.R")
 #jags.stuff <- buildJAGSdataobject(temp2, Tree2Tree, rnd.subset = 5000, trunc.yr = 1966)
 # if you don't have trees without cores, use the following line
 # or you wish to not include trees without cores
-jags.stuff <- buildJAGSdataobject(newtemp2, Tree2Tree, rnd.subset = 100, trunc.yr = 1966)
-jags.stuff <- buildJAGSdataobject(newtemp2,  rnd.subset = 100, trunc.yr = 1966)
+T2T.nodup <- Tree2Tree[!duplicated(Tree2Tree),]
+
+# sample from DBH measurements that only exist in plots that were also cored
+
+Tree2Tree.incored.plots <- Tree2Tree[paste0(Tree2Tree$COUNTYCD, Tree2Tree$PLOT) %in% paste0(newtemp2$CountyNo, newtemp2$PlotNo) ,]
+
+# looks like there are 5794 dbh measurements from plots where we also have tree cores:
+# > length(Tree2Tree.incored.plots$PLT_CN)
+# [1] 5794
+
+# check that we only have plot ids with tree cores
+#unique(paste0(Tree2Tree.incored.plots$COUNTYCD, Tree2Tree.incored.plots$PLOT)) %in% unique(paste0(newtemp2$CountyNo, newtemp2$PlotNo))
+
+
+jags.stuff <- buildJAGSdataobject(temp2 = newtemp2, Tree2Tree = Tree2Tree.incored.plots, rnd.subset = 100, trunc.yr = 1966)
+
+
+# 
+#jags.stuff <- buildJAGSdataobject(newtemp2,  rnd.subset = 100, trunc.yr = 1966)
 
 data <- jags.stuff$data
 z0 <- jags.stuff$z0
