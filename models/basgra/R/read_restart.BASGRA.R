d56697a52458ee37d1aefcbf40463bbda8e63070
istfer <fer.istem@gmail.com>
2019-11-27 04:02:25 -0500

    read lai and soilc

--- models/basgra/R/read_restart.BASGRA.R
+++ models/basgra/R/read_restart.BASGRA.R
@@ -11,7 +11,36 @@
 ##' 
 read_restart.BASGRA <- function(outdir, runid, stop.time, settings, var.names, params) {
   
+  forecast <- list()
+
+  # Read ensemble output
+  ens <- read.output(runid = runid, 
+                     outdir = file.path(outdir, runid), 
+                     start.year = lubridate::year(stop.time), 
+                     end.year = lubridate::year(stop.time),
+                     variables = var.names)
   
+  last <- length(ens[[1]])
+  
+  params$restart <- c()
+  
+  #if("TotSoilCarb" %in% var.names){
+  #  # fast/slow pool fractions needed maybe extract here or in write_restart
+  #}
+  
+  if ("LAI" %in% var.names) {
+    forecast[[length(forecast) + 1]] <- ens$LAI[last] ## m2 m-2 
+    names(forecast[[length(forecast)]]) <- c("LAI")
+  }
+  
+  if ("TotSoilCarb" %in% var.names) {
+    forecast[[length(forecast) + 1]] <- ens$TotSoilCarb[last] # kg C m-2
+    names(forecast[[length(forecast)]]) <- c("TotSoilCarb")
+  }
+  
+  PEcAn.logger::logger.info(runid)
+  
+  X_tmp <- list(X = unlist(forecast), params = params)
   
   return(X_tmp)
 } # read_restart.BASGRA
