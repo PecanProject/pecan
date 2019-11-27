d2ab18019052e625c9614af36d5b629974fa0f19
istfer <fer.istem@gmail.com>
2019-11-27 03:48:19 -0500

    start read_restart function

---
+++ models/basgra/R/read_restart.BASGRA.R
@@ -0,0 +1,18 @@
+##' @title Read restart function for SDA with BASGRA
+##' 
+##' @author Istem Fer
+##' 
+##' @inheritParams PEcAn.ModelName::read_restart.ModelName
+##' 
+##' @description Read Restart for BASGRA
+##' 
+##' @return X.vec      vector of forecasts
+##' @export
+##' 
+read_restart.BASGRA <- function(outdir, runid, stop.time, settings, var.names, params) {
+  
+  
+  
+  return(X_tmp)
+} # read_restart.BASGRA
+}
