5cd6cbb61ddb76eea9a220e0a6750066a886cdef
istfer <fer.istem@gmail.com>
2019-12-12 07:59:06 -0500

    start stics package

---
+++ models/stics/R/model2netcdf.STICS.R
@@ -0,0 +1,29 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2012 University of Illinois, NCSA.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the 
+# University of Illinois/NCSA Open Source License
+# which accompanies this distribution, and is available at
+# http://opensource.ncsa.illinois.edu/license.html
+#-------------------------------------------------------------------------------
+
+##-------------------------------------------------------------------------------------------------#
+##' Convert STICS output into the NACP Intercomparison format (ALMA using netCDF)
+##' 
+##' @name model2netcdf.STICS
+##' @title Code to convert STICS' output into netCDF format
+##'
+##' @param outdir Location of model output
+##' @param sitelat Latitude of the site
+##' @param sitelon Longitude of the site
+##' @param start_date Start time of the simulation
+##' @param end_date End time of the simulation
+##' @export
+##'
+##' @author Istem Fer
+model2netcdf.STICS <- function(outdir, sitelat, sitelon, start_date, end_date) {
+  PEcAn.logger::logger.severe("NOT IMPLEMENTED")
+
+
+  
+} # model2netcdf.STICS
