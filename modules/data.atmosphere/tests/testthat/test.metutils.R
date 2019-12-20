ba61847ab5fe23d58966c2364ed3e329d49400ef
David LeBauer <dlebauer@email.arizona.edu>
2019-12-19 17:07:46 -0700

    fix get.rh, added tests

--- modules/data.atmosphere/tests/testthat/test.metutils.R
+++ modules/data.atmosphere/tests/testthat/test.metutils.R
@@ -8,5 +8,25 @@ test_that("sw2par, par2ppfd, sw2ppfd are consistent ",{
 })
 
 test_that("qair2rh is consistent",{
-#    qair2rh(qair = 1, temp = 10, press = 1013.25)
+  expect_equal(qair2rh(qair = 1, temp = 10, press = 1013.25), 1)
 })
+
+test_that("get.rh RH from dewpoint",{
+  # air temp fixed at 15C
+  getrhtest <- function(Td_test){
+    testrh <-  get.rh(T  = 273.15 + 15, 
+                      Td = 273.15 + Td_test)
+    return(round(testrh))# tolerance = +/- 0.5%
+  }
+  # air T = dewpoint
+  expect_equal(getrhtest(15), 100)
+  # air T < dewpoint
+  expect_equal(getrhtest(20), 100)
+  # air T > dewpoint
+  # compared to values at NOAA calculator
+  # https://www.wpc.ncep.noaa.gov/html/dewrh.shtml
+  expect_equal(getrhtest(12.5), 85)
+  expect_equal(getrhtest(4.6), 50)
+  expect_equal(getrhtest(0), 36)
+})
+  
