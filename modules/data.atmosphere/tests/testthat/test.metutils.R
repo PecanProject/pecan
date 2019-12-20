7c6c16bf109fab744c56f6613d4993988c0274ba
David LeBauer <dlebauer@email.arizona.edu>
2019-12-19 17:19:22 -0700

    added a few additional tests explicitly add tolerance

--- modules/data.atmosphere/tests/testthat/test.metutils.R
+++ modules/data.atmosphere/tests/testthat/test.metutils.R
@@ -13,20 +13,22 @@ test_that("qair2rh is consistent",{
 
 test_that("get.rh RH from dewpoint",{
   # air temp fixed at 15C
-  getrhtest <- function(Td_test){
-    testrh <-  get.rh(T  = 273.15 + 15, 
+  getrhtest <- function(T_Test, Td_test){
+    testrh <-  get.rh(T  = 273.15 + T_Test, 
                       Td = 273.15 + Td_test)
-    return(round(testrh))# tolerance = +/- 0.5%
+    return(testrh)
   }
   # air T = dewpoint
-  expect_equal(getrhtest(15), 100)
+  expect_equal(getrhtest(15, 15), 100)
   # air T < dewpoint
-  expect_equal(getrhtest(20), 100)
+  expect_equal(getrhtest(15, 20), 100)
   # air T > dewpoint
   # compared to values at NOAA calculator
   # https://www.wpc.ncep.noaa.gov/html/dewrh.shtml
-  expect_equal(getrhtest(12.5), 85)
-  expect_equal(getrhtest(4.6), 50)
-  expect_equal(getrhtest(0), 36)
+  expect_equal(getrhtest(15, 12.5), 85.02, tolerance = 0.2)
+  expect_equal(getrhtest(15, 4.6), 49.8, tolerance = 0.2)
+  expect_equal(getrhtest(15, 0), 35.88, tolerance = 0.2)
+  expect_equal(getrhtest(25, 10), 38.82, tolerance = 0.2)
+  expect_equal(getrhtest(0, -5), 69, tolerance = 0.2)
 })
   
