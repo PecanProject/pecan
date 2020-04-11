f75927fbd9f92874b4d1f928ce04883c4f555eb3
Rob Kooper <kooper@illinois.edu>
2020-04-11 13:53:07 -0500

    fix ed compile

--- docker.sh
+++ docker.sh
@@ -171,11 +171,12 @@ for version in 0.95; do
 done
 
 # build ed2
-for version in git; do
+for version in git 2.2.0; do
     ${DEBUG} docker build \
         --tag pecan/model-ed2-${version}:${IMAGE_VERSION} \
         --build-arg MODEL_VERSION="${version}" \
         --build-arg IMAGE_VERSION="${IMAGE_VERSION}" \
+        --build-arg BINARY_VERSION="2.2" \
         models/ed
 done
 
