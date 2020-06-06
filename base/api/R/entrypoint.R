e1736978c0218692033a7d7bacb9a7bdfb752af6
Tezan Sahu <tezansahu@gmail.com>
2020-06-06 15:18:25 -0500

    directory refactoring

--- base/api/R/entrypoint.R
+++
@@ -1,7 +0,0 @@
-root <- plumber::plumber$new()
-root$handle("GET", "/ping", pecanapi::ping)
-
-root$run(port=8000, swagger = function(pr, spec, ...) {
-  spec <- yaml::read_yaml("inst/pecan-api-spec.yml")
-  spec
-})
