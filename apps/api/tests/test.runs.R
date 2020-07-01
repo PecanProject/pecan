6dd0eb1dfbfe2565db7d1e18a0c1f5b338775fc5
Tezan Sahu <tezansahu@gmail.com>
2020-07-01 18:21:48 +0000

    added failure tests

--- apps/api/tests/test.runs.R
+++ apps/api/tests/test.runs.R
@@ -1,6 +1,6 @@
-context("Testing the /api/runs/ endpoint")
+context("Testing all runs endpoints")
 
-test_that("Calling /api/runs/ returns Status 200", {
+test_that("Calling /api/runs/ with a valid workflow id returns Status 200", {
   res <- httr::GET(
     "http://localhost:8000/api/runs/?workflow_id=1000009172",
     httr::authenticate("carya", "illinois")
@@ -9,12 +9,29 @@ test_that("Calling /api/runs/ returns Status 200", {
 })
 
 
-context("Testing the /api/runs/{id} endpoint")
 
-test_that("Calling /api/runs/{id} returns Status 200", {
+test_that("Calling /api/runs/{id} with a valid run id returns Status 200", {
   res <- httr::GET(
     "http://localhost:8000/api/runs/1002042201",
     httr::authenticate("carya", "illinois")
   )
   expect_equal(res$status, 200)
+})
+
+test_that("Calling /api/runs/ with a invalid workflow id returns Status 404", {
+  res <- httr::GET(
+    "http://localhost:8000/api/runs/?workflow_id=1000000000",
+    httr::authenticate("carya", "illinois")
+  )
+  expect_equal(res$status, 404)
+})
+
+
+
+test_that("Calling /api/runs/{id} with a invalid run id returns Status 404", {
+  res <- httr::GET(
+    "http://localhost:8000/api/runs/1000000000",
+    httr::authenticate("carya", "illinois")
+  )
+  expect_equal(res$status, 404)
 })
