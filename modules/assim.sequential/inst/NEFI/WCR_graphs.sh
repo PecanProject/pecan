adae1caff6b8e5dcf0aeff44302e49f284bad223
Katie Zarada <kzarada@pecan-test.bu.edu>
2020-07-06 16:53:01 -0400

    removing files that have been renamed and moved to proper folders

--- modules/assim.sequential/inst/NEFI/WCR_graphs.sh
+++
@@ -1,4 +0,0 @@
-Rscript -e "rmarkdown::render('/fs/data3/kzarada/NEFI/Willow_Creek/index.Rmd')"
-Rscript "/fs/data3/kzarada/NEFI/Willow_Creek/email.R"
-Rscript "/fs/data3/kzarada/NEFI/graph_fluxtowers.R"
-Rscript "/fs/data3/kzarada/NEFI/graph_SDA_fluxtowers.R"
