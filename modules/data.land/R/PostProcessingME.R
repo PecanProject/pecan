5caf3a26c66fee3a987affe17bacacd3a8e07a59
Kah5 <kellyannheilman@gmail.com>
2020-07-10 14:09:44 -0400

    edits to the post-processing

--- modules/data.land/R/PostProcessingME.R
+++ modules/data.land/R/PostProcessingME.R
@@ -3,22 +3,64 @@
 library(rjags)
 #library(PEcAn.data.land)
 jags.comb <- NULL
-for(i in 9:12){
-  load(paste0("C:/Users/mekevans/Documents/Cdrive/Bayes/DemogRangeMod/ProofOfConcept/treerings/FIAmetadata/ArizonaData/MergedDatabase/New/ppt5000nocores/IGF.0.",i,".RData"))
+file.base.name <- "X2_Xscaled_forecasted_2018."
+output.base.name <- "X2_Xscaled_forecasted_2018"
+stage2 <- TRUE
+workingdir <- "/home/rstudio/"
+#workingdir <- "/Users/kah/Documents/docker_pecan/pecan"
+climate <- "wintP.wateryr"
+cov.data = cov.data
+
+
+jags.comb <- NULL
+
+for(i in 175:200){ # note this model stopped early b/c convergence
+  load(paste0(workingdir,"/IGF_PIPO_AZ_mcmc/", file.base.name,i,".RData"))
+  new.out <- jags.out 
+  
   if(is.null(jags.comb)){
-    jags.comb <- jags.out
+    for(j in seq_along(new.out)){
+      x.cols <- grep("^x",colnames(new.out[[j]]))
+      
+      if(length(x.cols)>0){ 
+        jags.comb[[j]] <- new.out[[j]][,x.cols]
+      }else{
+        jags.comb[[j]] <- new.out[[j]]
+      }
+      
+    }
+    
   } else {
-    for(j in seq_along(jags.out)){
-      x.cols <- grep("^x",colnames(jags.out[[j]]))
-      if(length(x.cols)>0) jags.out[[j]] <- jags.out[[j]][,-x.cols]
-      jags.comb[[j]]  <- rbind(jags.comb[[j]],jags.out[[j]])
+    for(j in seq_along(new.out)){
+      x.cols <- grep("^x",colnames(new.out[[j]]))
+      
+      if(length(x.cols)>0){ 
+        new.out[[j]] <- new.out[[j]][,x.cols]
+      }else{
+        new.out[[j]] <- new.out[[j]]
+      }
+      
+      jags.comb[[j]]  <- rbind(jags.comb[[j]], new.out[[j]])
+      rm(jags.out)
     }
   }
 }
+
 for(i in 1:3){
   jags.comb[[i]] <- as.mcmc(jags.comb[[i]])
 }
 jags.comb <- as.mcmc.list(jags.comb)
-pdf("IGF.1.burn.pdf")
-InventoryGrowthFusionDiagnostics(jags.comb)
+#save(jags.comb,file="IGF.waterYear.PPT.RData")
+saveRDS(jags.comb,file=paste0("IGF_xvals_est",output.base.name,".rds"))
+
+
+# also need to load the data used to run the model:
+
+# check for convergence via gelman-rubin
+#gelman.diag(jags.comb)
+
+data$time <- 1966:2018 # should have changed before running model but for now change here
+source("pecan/modules/data.land/R/InventoryGrowthFusionDiagnostics_KH.R")
+pdf("IGF.X2_Xscaled_forecasted_2018.pred.obs.pdf")
+InventoryGrowthFusionDiagnostics2(model.out = jags.comb, getRsq = FALSE)
 dev.off()
