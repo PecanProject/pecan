96a02a980df1f330371936de83b8e54bad7d3f79
Juliette Bateman <jbateman@pecan-test.bu.edu>
2020-07-24 13:00:10 -0400

    Adding the SMAP download, and a minor change to gee2pecan_smap.py date output

--- modules/data.remote/inst/gee2pecan_smap.py
+++ modules/data.remote/inst/gee2pecan_smap.py
@@ -1,15 +1,14 @@
 """
 Downloads SMAP Global Soil Moisture Data from Google Earth Engine and saves it in a netCDF file.
 
-Data retrieved: ssm, susm, smp, ssma, susma
-
 Requires Python3
 
 Author: Ayush Prasad
 """
-from gee_utils import create_geo, get_sitecoord, get_sitename
+
 import ee
 import pandas as pd
+import geopandas as gpd
 import os
 import xarray as xr
 import datetime
@@ -17,7 +16,7 @@ import datetime
 ee.Initialize()
 
 
-def gee2pecan_smap(geofile, outdir, start, end):
+def gee2pecan_smap(geofile, outdir, start, end, var):
     """
     Downloads and saves SMAP data from GEE
 
@@ -31,15 +30,37 @@ def gee2pecan_smap(geofile, outdir, start, end):
 
     end (str) -- ending date areaof the data request in the form YYYY-MM-dd
 
+    var (str) -- one of ssm, susm, smp, ssma, susma
+
     Returns
     -------
     Nothing:
             output netCDF is saved in the specified directory
     """
 
-    geo = create_geo(geofile)
+    # read in the geojson file
+    df = gpd.read_file(geofile)
 
-    def smap_ts(geo, start, end):
+    if (df.geometry.type == "Point").bool():
+        # extract coordinates
+        lon = float(df.geometry.x)
+        lat = float(df.geometry.y)
+        # create geometry
+        geo = ee.Geometry.Point(lon, lat)
+
+    elif (df.geometry.type == "Polygon").bool():
+        # extract coordinates
+        area = [
+            list(df.geometry.exterior[row_id].coords) for row_id in range(df.shape[0])
+        ]
+        # create geometry
+        geo = ee.Geometry.Polygon(area)
+
+    else:
+        # if the input geometry type is not
+        raise ValueError("geometry type not supported")
+
+    def smap_ts(geo, start, end, var):
         # extract a feature from the geometry
         features = [ee.Feature(geo)]
         # create a feature collection from the features
@@ -52,7 +73,7 @@ def gee2pecan_smap(geofile, outdir, start, end):
                 ee.ImageCollection("NASA_USDA/HSL/SMAP_soil_moisture")
                 .filterBounds(area)
                 .filterDate(start, end)
-                .select(["ssm", "susm", "smp", "ssma", "susma"])
+                .select([var])
             )
 
             def smap_ts_image(img):
@@ -68,77 +89,47 @@ def gee2pecan_smap(geofile, outdir, start, end):
                     scale=scale,
                 )
                 # store data in an ee.Array
-                ssm = ee.Array(img.get("ssm"))
-                susm = ee.Array(img.get("susm"))
-                smp = ee.Array(img.get("smp"))
-                ssma = ee.Array(img.get("ssma"))
-                susma = ee.Array(img.get("susma"))
+                smapdata = ee.Array(img.get(var))
                 tmpfeature = (
                     ee.Feature(ee.Geometry.Point([0, 0]))
-                    .set("ssm", ssm)
-                    .set("susm", susm)
-                    .set("smp", smp)
-                    .set("ssma", ssma)
-                    .set("susma", susma)
+                    .set("smapdata", smapdata)
                     .set("dateinfo", dateinfo)
                 )
                 return tmpfeature
 
             # map tmpfeature over the image collection
             smap_timeseries = collection.map(smap_ts_image)
-            return (
-                feature.set("ssm", smap_timeseries.aggregate_array("ssm"))
-                .set("susm", smap_timeseries.aggregate_array("susm"))
-                .set("smp", smap_timeseries.aggregate_array("smp"))
-                .set("ssma", smap_timeseries.aggregate_array("ssma"))
-                .set("susma", smap_timeseries.aggregate_array("susma"))
-                .set("dateinfo", smap_timeseries.aggregate_array("dateinfo"))
-            )
+            return feature.set(
+                "smapdata", smap_timeseries.aggregate_array("smapdata")
+            ).set("dateinfo", smap_timeseries.aggregate_array("dateinfo"))
 
         # map feature over featurecollection
         featureCollection = featureCollection.map(smap_ts_feature).getInfo()
         return featureCollection
 
-    fc = smap_ts(geo=geo, start=start, end=end)
+    fc = smap_ts(geo=geo, start=start, end=end, var=var)
 
     def fc2dataframe(fc):
-        ssm_datalist = []
-        susm_datalist = []
-        smp_datalist = []
-        ssma_datalist = []
-        susma_datalist = []
-        date_list = []
+        smapdatalist = []
+        datelist = []
         # extract var and date data from fc dictionary and store in it in smapdatalist and datelist
-        for i in range(len(fc["features"][0]["properties"]["ssm"])):
-            ssm_datalist.append(fc["features"][0]["properties"]["ssm"][i][0])
-            susm_datalist.append(fc["features"][0]["properties"]["susm"][i][0])
-            smp_datalist.append(fc["features"][0]["properties"]["smp"][i][0])
-            ssma_datalist.append(fc["features"][0]["properties"]["ssma"][i][0])
-            susma_datalist.append(fc["features"][0]["properties"]["susma"][i][0])
-            date_list.append(
-                datetime.datetime.strptime(
+        for i in range(len(fc["features"][0]["properties"]["smapdata"])):
+            smapdatalist.append(fc["features"][0]["properties"]["smapdata"][i][0])
+            datelist.append(
+                str(datetime.datetime.strptime(
                     (fc["features"][0]["properties"]["dateinfo"][i]).split(".")[0],
                     "%Y-%m-%d",
-                )
+                ))
             )
-        fc_dict = {
-            "date": date_list,
-            "ssm": ssm_datalist,
-            "susm": susm_datalist,
-            "smp": smp_datalist,
-            "ssma": ssma_datalist,
-            "susma": susma_datalist,
-        }
+        fc_dict = {"date": datelist, var: smapdatalist}
         # create a pandas dataframe and store the data
-        fcdf = pd.DataFrame(
-            fc_dict, columns=["date", "ssm", "susm", "smp", "ssma", "susma"]
-        )
+        fcdf = pd.DataFrame(fc_dict, columns=["date", var])
         return fcdf
 
     datadf = fc2dataframe(fc)
 
-    site_name = get_sitename(geofile)
-    AOI = get_sitecoord(geofile)
+    site_name = df[df.columns[0]].iloc[0]
+    AOI = str(df[df.columns[1]].iloc[0])
 
     # convert the dataframe to an xarray dataset, used for converting it to a netCDF file
     tosave = xr.Dataset(
@@ -148,6 +139,7 @@ def gee2pecan_smap(geofile, outdir, start, end):
             "start_date": start,
             "end_date": end,
             "AOI": AOI,
+            "product": var,
         },
     )
 
@@ -155,6 +147,6 @@ def gee2pecan_smap(geofile, outdir, start, end):
     if not os.path.exists(outdir):
         os.makedirs(outdir, exist_ok=True)
 
+    file_name = "_" + var
     # convert to netCDF and save the file
-    tosave.to_netcdf(os.path.join(outdir, site_name + "_smap.nc"))
-
+    tosave.to_netcdf(os.path.join(outdir, site_name + file_name + ".nc"))
