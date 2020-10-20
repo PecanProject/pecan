PecanProject/pecan

commit 5ab53e2594ef6326e3db36b45809839d6623b5d4

remove `car` dependency

modules/data.atmosphere/R/extract_local_CMIP5.R
@@ -92,9 +92,8 @@ extract.local.CMIP5 <- function(outfolder, in.path, start_date, end_date, lat.in
   vars.gcm <- c(vars.gcm.day, vars.gcm.mo)
   
   # Rewriting the dap name to get the closest variable that we have for the GCM (some only give uss stuff at sea level)
-  library(car) # having trouble gettins stuff to work otherwise
-  if(!("huss" %in% vars.gcm)) var$DAP.name <- car::recode(var$DAP.name, "'huss'='hus'")
-  if(!("ps" %in% vars.gcm  )) var$DAP.name <- car::recode(var$DAP.name, "'ps'='psl'")
+  if(!("huss" %in% vars.gcm)) levels(var$DAP.name) <- sub("huss", "hus", levels(var$DAP.name))
+  if(!("ps" %in% vars.gcm  )) levels(var$DAP.name) <- sub("ps", "psl", levels(var$DAP.name))
 
   # Making sure we're only trying to grab the variables we have (i.e. don't try sfcWind if we don't have it)
   var <- var[var$DAP.name %in% vars.gcm,]

modules/data.atmosphere/DESCRIPTION
@@ -19,7 +19,6 @@ Depends:
     methods
 Imports:
     abind (>= 1.4.5),
-    car,
     data.table,
     dplyr,
     geonames (> 0.998),
@@ -39,6 +38,7 @@ Imports:
     PEcAn.remote,
     PEcAn.utils,
     purrr (>= 0.2.3),
+    raster,
     RCurl,
     REddyProc,
     reshape2,

modules/data.atmosphere/tests/Rcheck_reference.log
@@ -38,11 +38,10 @@ Use \uxxxx escapes for other characters.
 * checking whether the namespace can be unloaded cleanly ... OK
 * checking loading without being on the library search path ... OK
 * checking dependencies in R code ... WARNING
-'::' or ':::' import not declared from: ‘raster’
 'library' or 'require' calls not declared from:
-  ‘car’ ‘MASS’ ‘mgcv’
+  ‘MASS’ ‘mgcv’
 'library' or 'require' calls in package code:
-  ‘car’ ‘MASS’ ‘mgcv’
+  ‘MASS’ ‘mgcv’
   Please use :: or requireNamespace() instead.
   See section 'Suggested packages' in the 'Writing R Extensions' manual.
 Package in Depends field not imported from: ‘methods’
@@ -473,4 +472,4 @@ Package has no Sweave vignette sources and no VignetteBuilder field.
 * checking for unstated dependencies in ‘tests’ ... OK
 * checking tests ... SKIPPED
 * DONE
-Status: 9 WARNINGs, 2 NOTEs
+Status: 8 WARNINGs, 2 NOTEs

commit 4ba1c1be1d3024e283fa2317f46b7fce92914b17

Fix variable recoding
commit # 5ab53e2594ef6326e3db36b45809839d6623b5d4 broke improtant recoding causing air pressure and humidity to not be renamed if necessary.

modules/data.atmosphere/R/extract_local_CMIP5.R
@@ -101,10 +101,10 @@ extract.local.CMIP5 <- function(outfolder, in.path, start_date, end_date, lat.in
   vars.gcm.mo <- dir(path.mo) 
   # If our extraction bath is different from what we had, modify it
   if("atmos" %in% vars.gcm.day){
-	path.day <- file.path(in.path, "day", "atmos", "day", ensemble_member, "latest")
-	path.mo <- file.path(in.path, "mon", "atmos", "Amon", ensemble_member, "latest")
+	  path.day <- file.path(in.path, "day", "atmos", "day", ensemble_member, "latest")
+	  path.mo <- file.path(in.path, "mon", "atmos", "Amon", ensemble_member, "latest")
 
-	vars.gcm.day <- dir(path.day)
+	  vars.gcm.day <- dir(path.day)
   	vars.gcm.mo <- dir(path.mo)  	
   }
   vars.gcm.mo <- vars.gcm.mo[!vars.gcm.mo %in% vars.gcm.day]
@@ -112,8 +112,8 @@ extract.local.CMIP5 <- function(outfolder, in.path, start_date, end_date, lat.in
   vars.gcm <- c(vars.gcm.day, vars.gcm.mo)
   
   # Rewriting the dap name to get the closest variable that we have for the GCM (some only give uss stuff at sea level)
-  if(!("huss" %in% vars.gcm)) levels(var$DAP.name) <- sub("huss", "hus", levels(var$DAP.name))
-  if(!("ps" %in% vars.gcm  )) levels(var$DAP.name) <- sub("ps", "psl", levels(var$DAP.name))
+  if(!("huss" %in% vars.gcm)) var$DAP.name[var$DAP.name=="huss"] <- "hus"
+  if(!("ps" %in% vars.gcm)) var$DAP.name[var$DAP.name=="ps"] <- "psl"
 
   # Making sure we're only trying to grab the variables we have (i.e. don't try sfcWind if we don't have it)
   var <- var[var$DAP.name %in% vars.gcm,]
@@ -195,9 +195,17 @@ extract.local.CMIP5 <- function(outfolder, in.path, start_date, end_date, lat.in
       # date.origin <- as.Date(stringr::str_split(ncT$dim$time$units, " ")[[1]][splt.ind])
       nc.date <- date.origin + nc.time
       
-      if(as.Date(min(nc.date)) < as.Date(paste0(files.var[[var.now]][i,"first.year"], "-01-01"))){
-      	nc.date <- as.Date(paste0(files.var[[var.now]][i,"first.year"], "-01-01")) + nc.time
+      nc.min <- as.Date(min(nc.date))
+      date.ref <- as.Date(paste0(files.var[[var.now]][i,"first.year"], "-01-01"))
+      
+      # If things don't align with the specified origin, update it & try again
+      if(nc.min != date.ref){
+        date.off <- date.ref - nc.min
+        date.origin <- date.origin + date.off + 1
+        
+        nc.date <- nc.time + date.origin
       }
+
       date.leaps <- seq(as.Date(paste0(files.var[[var.now]][i,"first.year"], "-01-01")), as.Date(paste0(files.var[[var.now]][i,"last.year"], "-12-31")), by="day")
       # Figure out if we're missing leap dat
       no.leap <- ifelse(is.null(no.leap) & length(nc.date)!=length(date.leaps), TRUE, FALSE)