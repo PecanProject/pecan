efecc551b269cb182e7dc2048775041f1c51907e
David LeBauer <dlebauer@email.arizona.edu>
2020-10-30 10:53:02 -0700

    fix if / else nesting error

--- modules/photosynthesis/R/fitA.R
+++ modules/photosynthesis/R/fitA.R
@@ -170,11 +170,11 @@ sel <- seq_len(nrow(dat))  #which(dat$spp == s)
 if("Tleaf" %in% names(dat)){
   if(max(dat$Tleaf) < 100){ # if Tleaf in C, convert to K
     dat$Tleaf <- dat$Tleaf + 273.15
-  } else if (!"Tleaf" %in% names(dat)) {
+  } 
+} else if (!"Tleaf" %in% names(dat)) {
     dat$Tleaf <- 25 + 273.15 ## if no Tleaf, assume 25C in Kelvin
     warning("No Leaf Temperature provided, setting to 25C\n",
             "To change add a column named Tleaf to flux.data data frame")
-  }
 }
 
 mydat <- list(an = dat$Photo[sel], 
