2a20dc2370003cf231eaf13a6a5010a12b7890c7
Shashank Singh <73598347+moki1202@users.noreply.github.com>
2021-05-27 15:24:30 +0530

    Update benchmark.workflow.R

--- modules/benchmark/inst/scripts/benchmark.workflow.R
+++ modules/benchmark/inst/scripts/benchmark.workflow.R
@@ -1,4 +1,4 @@
-## Workflow assumes that you have already run 
+## Workflow assumes that you have already run
 ## settings <- read.settings("path_to_settings")
 #> settings <- read.settings("/fs/data2/output/PEcAn_1000008263/pecan.BENCH.xml")
 
@@ -9,7 +9,13 @@ library(lubridate)
 
 
 d <- settings$database$bety[c("dbname", "password", "host", "user")]
-bety <- src_postgres(host = d$host, user = d$user, password = d$password, dbname = d$dbname)
+bety <- DBI::dbConnect(
+  RPostgres::Postgres(),
+  host = d$host,
+  user = d$user,
+  password = d$password,
+  dbname = d$dbname
+)
 settings$host$name <- "localhost"
 
 bm.settings <- define_benchmark(settings,bety)
@@ -25,19 +31,18 @@ settings$benchmarking <- bm_settings2pecan_settings(bm.settings)
 ################################################################################
 
 if(bm.settings$new_run){
-  
+
   # This section isn't ready yet
   # write.settings(settings = settings, outputfile = "pecan.xml", outputdir = settings$outdir)
   # # Run the workflow! YAY
   # settings <- read.settings(file.path(settings$outdir,"pecan.CHECKED.xml"))
   # results <- load(file.path(settings$outdir,"benchmarking.output.Rdata"))
- 
+
 }else{
-  
+
   settings <- read_settings_BRR(settings)
   sprintf("MODEL: %s", settings$model$type)
-  
+
   settings <- prepare.settings(settings)
   results <- papply(settings, function(x) calc_benchmark(x, bety))
 }
-
