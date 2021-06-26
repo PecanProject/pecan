c29a2d7737edf172e31e06a60eb2c6c6ff159861
Ayush Prasad <ayush.prd@gmail.com>
2021-06-26 10:23:05 +0530

    add test for product url

--- modules/data.atmosphere/tests/testthat/test.download.Drought2018.R
+++ modules/data.atmosphere/tests/testthat/test.download.Drought2018.R
@@ -8,7 +8,8 @@ test_that("Drought 2018 download works", {
   start_date <- "2016-01-01"
   end_date <- "2017-01-01"
   sitename <- "FI-Sii"
-  print(outfolder)
+  res <- httr::GET("https://meta.icos-cp.eu/objects/a8OW2wWfAYqZrj31S8viVLUS")
+  expect_equal(200, res$status_code)
   dat <- download.Drought2018(sitename, outfolder, start_date, end_date, overwrite = TRUE)
   expect_true(file.exists(dat$file))
 })
