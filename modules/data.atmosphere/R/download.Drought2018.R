9632c823f5ac1d7b58bc8ed7717c53581798e9e3
Ayush Prasad <ayush.prd@gmail.com>
2021-06-27 13:34:31 +0530

    fix indentation

--- modules/data.atmosphere/R/download.Drought2018.R
+++ modules/data.atmosphere/R/download.Drought2018.R
@@ -11,26 +11,33 @@
 #' @param overwrite should existing files be overwritten. Default False.
 #' @return information about the output file
 #' @export
-#' @examples 
+#' @examples
 #' \dontrun{
 #' download.Drought2018("FI-Sii", "/home/carya/pecan", "2016-01-01", "2018-01-01")
 #' }
 #' @author Ayush Prasad
-#' 
+#'
 download.Drought2018 <-
   function(sitename,
            outfolder,
            start_date,
            end_date,
            overwrite = FALSE) {
-    
     stage_download <- TRUE
     stage_extract <- TRUE
     
     # construct output CSV file name
-    output_file_name <- paste0("FLX_", sitename, "_FLUXNET2015_FULLSET_HH_", as.character(format(as.Date(start_date),'%Y')), "-2018_beta-3.csv")
+    output_file_name <-
+      paste0(
+        "FLX_",
+        sitename,
+        "_FLUXNET2015_FULLSET_HH_",
+        as.character(format(as.Date(start_date), '%Y')),
+        "-2018_beta-3.csv"
+      )
     
-    if (file.exists(file.path(outfolder, output_file_name)) && !overwrite) {
+    if (file.exists(file.path(outfolder, output_file_name)) &&
+        !overwrite) {
       PEcAn.logger::logger.info("Output CSV file for the requested site already exists")
       stage_download <- FALSE
       stage_extract <- FALSE
@@ -49,7 +56,9 @@ download.Drought2018 <-
     
     if (as.Date(end_date) > as.Date("2018-12-31")) {
       PEcAn.logger::logger.severe(paste0(
-        "Requested end date ", end_date, " exceeds Drought 2018 availability period"
+        "Requested end date ",
+        end_date,
+        " exceeds Drought 2018 availability period"
       ))
     }
     
@@ -63,7 +72,7 @@ download.Drought2018 <-
       body <- "
       prefix cpmeta: <http://meta.icos-cp.eu/ontologies/cpmeta/>
       prefix prov: <http://www.w3.org/ns/prov#>
-      select ?dobj ?spec ?timeStart 
+      select ?dobj ?spec ?timeStart
       where {
       	VALUES ?spec {<http://meta.icos-cp.eu/resources/cpmeta/dought2018ArchiveProduct>}
       	?dobj cpmeta:hasObjectSpec ?spec .
@@ -78,12 +87,21 @@ download.Drought2018 <-
       response <- httr::content(response, as = "text")
       response <- jsonlite::fromJSON(response)
       dataset_url <- response$results$bindings$dobj$value
-      dataset_start_date <-  as.Date(strptime(response$results$bindings$timeStart$value, format = "%Y-%m-%dT%H:%M:%S"))
-      if(is.null(dataset_url)){
+      dataset_start_date <-
+        as.Date(
+          strptime(response$results$bindings$timeStart$value, format = "%Y-%m-%dT%H:%M:%S")
+        )
+      if (is.null(dataset_url)) {
         PEcAn.logger::logger.severe("Data is not available for the requested site")
       }
-      if(dataset_start_date > as.Date(start_date)){
-        PEcAn.logger::logger.severe(paste("Data is not available for the requested start date. Please try again with", dataset_start_date, "as start date.") )
+      if (dataset_start_date > as.Date(start_date)) {
+        PEcAn.logger::logger.severe(
+          paste(
+            "Data is not available for the requested start date. Please try again with",
+            dataset_start_date,
+            "as start date."
+          )
+        )
       }
       dataset_id <- sub(".*/", "", dataset_url)
       
@@ -94,60 +112,62 @@ download.Drought2018 <-
                '%22%5D')
       # Download the zip file
       file <-
-        httr::GET(url = download_url, httr::write_disk(
-          paste0(outfolder, "/Drought", sitename, ".zip"),
-          overwrite = TRUE
-        ), httr::progress())
+        httr::GET(url = download_url,
+                  httr::write_disk(
+                    paste0(outfolder, "/Drought", sitename, ".zip"),
+                    overwrite = TRUE
+                  ),
+                  httr::progress())
     }
     
-    if (stage_extract){
-    file_name <- paste0('FLX_', sitename, '_FLUXNET2015_FULLSET_HH')
-  
-    # extract only the hourly data file
-    zipped_csv_name <-
-      grep(
-        paste0('^', file_name),
-        utils::unzip(zip_file_name, list = TRUE)$Name,
-        ignore.case = TRUE,
-        value = TRUE
-      )
-    utils::unzip(file.path(outfolder, paste0('Drought', sitename, '.zip')),
-          files = zipped_csv_name,
-          exdir = outfolder)
-    `%>%` <- dplyr::`%>%`
-    # read in the output CSV file and select the variables required
-    df <- utils::read.csv(file.path(outfolder, zipped_csv_name))
-    df <-
-      subset(
-        df,
-        select = c(
-          "TIMESTAMP_START",
-          "TA_F",
-          "SW_IN_F",
-          "LW_IN_F",
-          "VPD_F",
-          "PA_F",
-          "P_F",
-          "WS_F",
-          "WD",
-          "RH",
-          "PPFD_IN",
-          "CO2_F_MDS",
-          "TS_F_MDS_1",
-          "TS_F_MDS_2",
-          "NEE_VUT_REF",
-          "LE_F_MDS",
-          "RECO_NT_VUT_REF",
-          "GPP_NT_VUT_REF"
+    if (stage_extract) {
+      file_name <- paste0('FLX_', sitename, '_FLUXNET2015_FULLSET_HH')
+      
+      # extract only the hourly data file
+      zipped_csv_name <-
+        grep(
+          paste0('^', file_name),
+          utils::unzip(zip_file_name, list = TRUE)$Name,
+          ignore.case = TRUE,
+          value = TRUE
         )
-      )
-    df <-
-      df %>% dplyr::filter((
-        as.Date(strptime(df$TIMESTAMP_START, format = "%Y%m%d%H%M")) >= as.Date(start_date) &
-          as.Date(strptime(df$TIMESTAMP_START, format = "%Y%m%d%H%M")) <= as.Date(end_date)
-      ))
-    # save the csv file
-    utils::write.csv(df, file.path(outfolder, output_file_name), row.names = FALSE)
+      utils::unzip(file.path(outfolder, paste0('Drought', sitename, '.zip')),
+                   files = zipped_csv_name,
+                   exdir = outfolder)
+      `%>%` <- dplyr::`%>%`
+      # read in the output CSV file and select the variables required
+      df <- utils::read.csv(file.path(outfolder, zipped_csv_name))
+      df <-
+        subset(
+          df,
+          select = c(
+            "TIMESTAMP_START",
+            "TA_F",
+            "SW_IN_F",
+            "LW_IN_F",
+            "VPD_F",
+            "PA_F",
+            "P_F",
+            "WS_F",
+            "WD",
+            "RH",
+            "PPFD_IN",
+            "CO2_F_MDS",
+            "TS_F_MDS_1",
+            "TS_F_MDS_2",
+            "NEE_VUT_REF",
+            "LE_F_MDS",
+            "RECO_NT_VUT_REF",
+            "GPP_NT_VUT_REF"
+          )
+        )
+      df <-
+        df %>% dplyr::filter((
+          as.Date(strptime(df$TIMESTAMP_START, format = "%Y%m%d%H%M")) >= as.Date(start_date) &
+            as.Date(strptime(df$TIMESTAMP_START, format = "%Y%m%d%H%M")) <= as.Date(end_date)
+        ))
+      # save the csv file
+      utils::write.csv(df, file.path(outfolder, output_file_name), row.names = FALSE)
     }
     
     rows    <- 1
@@ -162,7 +182,8 @@ download.Drought2018 <-
       stringsAsFactors = FALSE
     )
     
-    results$file[rows]       <- file.path(outfolder, output_file_name)
+    results$file[rows]       <-
+      file.path(outfolder, output_file_name)
     results$host[rows]       <- PEcAn.remote::fqdn()
     results$startdate[rows]  <- start_date
     results$enddate[rows]    <- end_date
