de75267b9ad848cbee12da73795d423cd91a9a1f
Christina Francis <cfranci1@pecan-test.bu.edu>
2021-07-23 13:28:58 -0400

    function pulling the soil moisture value from the eco forecast server

--- modules/data.land/R/Pulling_VSWC.R
+++ modules/data.land/R/Pulling_VSWC.R
@@ -43,75 +43,10 @@ filter.date.daily <- dplyr::filter(non_na_daily, time == max_time & siteID == si
 print(filter.date.daily)
 
 # save the lastest vswc value into object and turn this into a netCDF
-VSWC_dim <- ncdim_def(name = "VSWC", units = "mass fraction of unfrozen water in soil moisture" , vals = filter.date.daily$vswc)
-VSWC_def <- ncvar_def(name = "volumetric soil water content", units = "mass fraction of unfrozen water in soil moisture", dim = VSWC_dim, 
+depth <- ncdim_def(name = "depth", units = "meters" , vals = filter.date.daily$vswc)
+VSWC <- ncvar_def(name = "mass fraction of unfrozen water in soil_moisture", units = "volumetric soil water content", dim = depth, 
                       missval = -999, longname = paste0(site_ID, "_", "VSWC"), prec = "integer")
-soilmoisture.nc <- nc_create(paste0(site_ID, "_", "VSWC"), VSWC_def)
+soilmoisture.nc <- nc_create(paste0(site_ID, "_", "VSWC"), VSWC)
 print(soilmoisture.nc)
 
 }
-
-# terrestrial targets data produce the same vswc values at the daily and 30-minute target level
-############# doing the same process again, but at the 30-min terrestrial target level #############
-
-# reading in terrestrial targets data at 30-min level
-terr_30min <- read_csv("/projectnb/dietzelab/cfranci1/Targets/terrestrial/terrestrial_30min-targets.csv.gz", guess_max = 10000, 
-  col_types = cols(
-  time = col_datetime(format = ""),
-  siteID = col_character(),
-  nee = col_double(),
-  le = col_double(),
-  nee_sd_intercept = col_double(),
-  nee_sd_slopeP = col_double(),
-  nee_sd_slopeN = col_double(),
-  le_sd_intercept = col_double(),
-  le_sd_slopeP = col_double(),
-  le_sd_slopeN = col_double(),
-  vswc = col_double(),
-  vswc_sd = col_double()
-))
-
-# review 30-min targets
-summary(terr_30min)
-## view dates that contain vswc data
-non_na_30min <- terr_daily[complete.cases(terr_daily$vswc), ] %>% 
-  mutate(time = as.Date(non_na_daily$time))
-
-## find date range (most recent date) with vswc data for each site at the 30-min target level
-non_na_30min_konz <- non_na_30min %>% 
-  filter(siteID == "KONZ")
-range(non_na_30min_konz$time)
-
-KONZ = 2021-01-20
-
-non_na_30min_bart <- non_na_30min %>% 
-  filter(siteID == "BART")
-range(non_na_30min_bart$time)
-
-BART = 2021-06-28
-
-non_na_30min_srer <- non_na_30min %>% 
-  filter(siteID == "SRER")
-range(non_na_30min_srer$time)
-
-SRER = 2021-02-21
-
-non_na_30min_osbs <- non_na_30min %>% 
-  filter(siteID == "OSBS")
-range(non_na_30min_osbs$time)
-
-OSBS = 2020-12-08
-
-# extracting a VSWC value based on date and siteID columns
-filter.date.30min.KONZ <- dplyr::filter(non_na_30min, time == "2021-01-20" & siteID == "KONZ")
-filter.date.30min.KONZ
-
-filter.date.30min.BART <- dplyr::filter(non_na_30min, time == "2021-06-28" & siteID == "BART")
-filter.date.30min.BART 
-
-filter.date.30min.SRER <- dplyr::filter(non_na_30min, time == "2021-02-21" & siteID == "SRER")
-filter.date.30min.SRER
-
-filter.date.30min.OSBS <- dplyr::filter(non_na_30min, time == "2020-12-08" & siteID == "OSBS")
-filter.date.30min.OSBS 
-
