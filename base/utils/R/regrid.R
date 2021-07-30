982ca447db4e5604ab276b4f2823d72f0997ee81
Shashank Singh <73598347+moki1202@users.noreply.github.com>
2021-07-30 14:42:21 +0530

    years and yieldarray (No global variable)

--- base/utils/R/regrid.R
+++ base/utils/R/regrid.R
@@ -1,5 +1,5 @@
 ##' Regrid dataset to even grid
-##' 
+##'
 ##' @title regrid
 ##' @param latlon.data dataframe with lat, lon, and some value to be regridded
 ##' @return dataframe with regridded data
@@ -13,15 +13,15 @@ regrid <- function(latlon.data) {
   e <- raster::extent(spdf)
   ## Determine ratio between x and y dimensions
   ratio <- (e@xmax - e@xmin) / (e@ymax - e@ymin)
-  
+
   ## Create template raster to sample to
   r  <- raster::raster(nrows = 56, ncols = floor(56 * ratio), ext = raster::extent(spdf))
   rf <- raster::rasterize(spdf, r, field = "z", fun = mean)
-  
+
   # rdf <- data.frame( rasterToPoints( rf ) ) colnames(rdf) <-
   # colnames(latlon.data)
   arf <- as.array(rf)
-  
+
   # return(rdf)
   return(arf)
 } # regrid
@@ -30,7 +30,10 @@ regrid <- function(latlon.data) {
 ##' Write gridded data to netcdf file
 ##'
 ##' @title grid2netcdf
-##' @param grid.data 
+##' @param grid.data
+##' @param gdata
+##' @param Date as string or date object.
+##' @param outfile location where output will be stored.
 ##' @return writes netCDF file
 ##' @author David LeBauer
 grid2netcdf <- function(gdata, date = "9999-09-09", outfile = "out.nc") {
@@ -49,22 +52,24 @@ grid2netcdf <- function(gdata, date = "9999-09-09", outfile = "out.nc") {
       "please install `data.table`."
     )
   }
+  years <- NULL
   grid.data <- merge(latlons, gdata, by = c("lat", "lon", "date"), all.x = TRUE)
   lat       <- ncdf4::ncdim_def("lat", "degrees_east", vals = lats, longname = "station_latitude")
   lon       <- ncdf4::ncdim_def("lon", "degrees_north", vals = lons, longname = "station_longitude")
-  time      <- ncdf4::ncdim_def(name = "time", units = paste0("days since 1700-01-01"), 
+  time      <- ncdf4::ncdim_def(name = "time", units = paste0("days since 1700-01-01"),
                          vals = as.numeric(lubridate::ymd(paste0(years, "01-01")) - lubridate::ymd("1700-01-01")),
                          calendar = "standard",
                          unlim = TRUE)
-  
+
   yieldvar <- to_ncvar("CropYield", list(lat, lon, time))
   nc <- ncdf4::nc_create(filename = outfile, vars = list(CropYield = yieldvar))
-  
+
   ## Output netCDF data
+  yieldarray <- NULL
   #    ncvar_put(nc, varid = yieldvar, vals = grid.data[order(lat, lon, order(lubridate::ymd(date )))]$yield)
   #    ncvar_put(nc, varid = yieldvar, vals = grid.data[order(order(lubridate::ymd(date), lat, lon))]$yield)
   ncdf4::ncvar_put(nc, varid = yieldvar, vals = yieldarray)
-  
+
   ncdf4::ncatt_put(nc, 0, "description", "put description here")
   ncdf4::nc_close(nc)
 } # grid2netcdf
