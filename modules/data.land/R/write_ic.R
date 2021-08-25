c679c70c342b70b111d462cf14e984782ca9956d
Alexis Helgeson <ahelgeso@bu.edu>
2021-08-25 15:46:00 -0400

    "added cohort2pools call and veg2model.SIPNET call"

--- modules/data.land/R/write_ic.R
+++ modules/data.land/R/write_ic.R
@@ -9,8 +9,9 @@
 ##' @param pfts list settings$pfts. 
 ##' @param source Data source as saved in the BETY db
 ##' @param overwrite DEfault is FALSE. Option to overwrite existing files. 
+##' @param n.ensemble number of ensemble members
 ##' @param ... Additional parameters
-##'
+##' 
 ##' @name write_ic
 ##' @title write_ic
 ##' @export
@@ -28,15 +29,15 @@ write_ic <- function(in.path, in.name, start_date, end_date,
   #--------------------------------------------------------------------------------------------------#
   # Match PFTs
   
-  obs <- as.data.frame(veg_info[[2]], stringsAsFactors = FALSE)
-  
-  # NOTE : match_pft may return NAs for unmatched dead trees
-  pft.info <- PEcAn.data.land::match_pft(obs$bety_species_id, pfts, model = model)
-  
-  ### merge with other stuff
-  obs$pft <- pft.info$pft
-  
-  veg_info[[2]] <- obs
+  # obs <- as.data.frame(veg_info[[2]], stringsAsFactors = FALSE)
+  # 
+  # # NOTE : match_pft may return NAs for unmatched dead trees
+  # pft.info <- PEcAn.data.land::match_pft(bety_species_id = obs$bety_species_id, pfts = pfts, model = model, con = con)
+  # 
+  # ### merge with other stuff
+  # obs$pft <- pft.info$pft
+  # 
+  # veg_info[[2]] <- obs
   
 
 # Cohort2Pool -------------------------------------------------------------
@@ -46,34 +47,42 @@ write_ic <- function(in.path, in.name, start_date, end_date,
   register     <- PEcAn.data.atmosphere::read.register(register.xml, con)
   if (register$poolinitcond == "POOL") {
     poolinfo <- PEcAn.data.land::cohort2pool(veg_info = veg_info, allom_param = NULL, dbh_name = "DBH") #Figure out how to grab dbh_name from veg_info object?
-    ncdfinfo <- PEcAn.SIPNET::veg2model.SIPNET(input = poolinfo, outdir = outfolder, siteid = new_site$id, ens = n.ensemble)
+    out <- PEcAn.SIPNET::veg2model.SIPNET(input = poolinfo, outdir = outfolder, siteid = new_site$id, ens = n.ensemble) 
+    # Build results dataframe for convert.input
+    results <- data.frame(file = out$file, 
+                          host = host, 
+                          mimetype = out$mimetype, 
+                          formatname = out$formatname, 
+                          startdate = start_date, 
+                          enddate = end_date, 
+                          dbfile.name = out$dbfile.name, 
+                          stringsAsFactors = FALSE)
+  } else{
+    #--------------------------------------------------------------------------------------------------#
+    # veg2model
+    ## Set model-specific functions
+    pkg <- paste0("PEcAn.", model)
+    do.call("library", list(pkg))
+    fcnx <- paste("veg2model.", model, sep = "")
+    if (!exists(fcnx)) {
+      PEcAn.logger::logger.severe(paste(fcnx, "does not exist."))
+    }else{
+      fcn <- match.fun(fcnx)
+    }
+    
+    out <- fcn(outfolder, veg_info, start_date, new_site, source)
+    # Build results dataframe for convert.input
+    results <- data.frame(file = out$filepath, 
+                          host = c(PEcAn.remote::fqdn()), 
+                          mimetype = out$mimetype, 
+                          formatname = out$formatname, 
+                          startdate = start_date, 
+                          enddate = end_date, 
+                          dbfile.name = out$filename, 
+                          stringsAsFactors = FALSE)
   }
   
-  #--------------------------------------------------------------------------------------------------#
-  # veg2model
   
-  ## Set model-specific functions
-  pkg <- paste0("PEcAn.", model)
-  do.call("library", list(pkg))
-  fcnx <- paste("veg2model.", model, sep = "")
-  if (!exists(fcnx)) {
-    PEcAn.logger::logger.severe(paste(fcnx, "does not exist."))
-  }else{
-    fcn <- match.fun(fcnx)
-  }
-  
-  out <- fcn(outfolder, veg_info, start_date, new_site, source)
-  
-
-  # Build results dataframe for convert.input
-  results <- data.frame(file = out$filepath, 
-                        host = c(PEcAn.remote::fqdn()), 
-                        mimetype = out$mimetype, 
-                        formatname = out$formatname, 
-                        startdate = start_date, 
-                        enddate = end_date, 
-                        dbfile.name = out$filename, 
-                        stringsAsFactors = FALSE)
   
   ### return for convert.inputs
   return(invisible(results))
