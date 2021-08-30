25ea80dd183037f2aba65aedcacdc3ac9b9dc85d
Chris Black <chris@ckblack.org>
2021-08-30 14:25:50 +0200

    biocro needs to know where get.results moved

--- models/biocro/R/get.model.output.BIOCRO.R
+++ models/biocro/R/get.model.output.BIOCRO.R
@@ -18,7 +18,7 @@ get.model.output.BIOCRO <- function(settings) {
   
   ### Get model output on the localhost
   if (settings$host$name == "localhost") {
-    PEcAn.utils::get.results(settings = settings)
+    PEcAn.uncertainty::get.results(settings = settings)
   } else {
     print(paste("biocro model specific get.model.output not implemented for\n", 
                 "use on remote host; generic get.model.output under development"))
