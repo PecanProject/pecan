3a1c9264936eca3d1e625dad263a70c49b279b1d
Shashank Singh <moki1202@pecan-moki.ncsa.illinois.edu>
2021-09-14 15:24:44 +0000

    minor fix

--- base/remote/R/remote.copy.update.R
+++ base/remote/R/remote.copy.update.R
@@ -11,25 +11,25 @@
 #' @author Istem Fer
 #' @export
 remote.copy.update <- function(input_id, remote_dir, local_file_path, remote_file_name = NULL, host, con){
-
+  
   PEcAn.remote::remote.execute.cmd(host, "mkdir", c("-p", remote_dir))
-
+  
   if(is.null(remote_file_name)){
     remote_file_name <- basename(local_file_path)
   }
-
+  
   remote_file_path <- file.path(remote_dir, remote_file_name)
-
+  
   remote.copy.to(host, local_file_path, remote_file_path)
-
-  type <- db.query(paste("SELECT container_type from dbfiles where container_id =", putveg.id[[i]]), con)
+  
+  type <- PEcAn.DB::db.query(paste("SELECT container_type from dbfiles where container_id =", putveg.id[[i]]), con)
   
   # update DB record
   remote_id <- PEcAn.DB::dbfile.insert(in.path = remote_dir, in.prefix = remote_file_name,
-                type = unique(type), id = input_id$input.id,
-                con = con, hostname = host$name)
-
-
+                                       type = unique(type), id = input_id$input.id,
+                                       con = con, hostname = host$name)
+  
+  
   return(remote_id)
-
+  
 } # remote.copy.update
