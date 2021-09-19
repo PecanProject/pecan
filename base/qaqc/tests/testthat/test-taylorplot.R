f9faa4c8413c9800b89991b223bbb7b7d4b896b6
Chris Black <chris@ckblack.org>
2021-09-19 22:57:37 +0200

    use vdiffr for cross-OS reproducibility
    PNG snapshots had slightly different kerning on OS X and Linus
    
    NB vdiffr requires R >= 4.1, but will skip test gracefully on older R versions

--- base/qaqc/tests/testthat/test-taylorplot.R
+++ base/qaqc/tests/testthat/test-taylorplot.R
@@ -7,35 +7,6 @@
 # http://opensource.ncsa.illinois.edu/license.html
 #-------------------------------------------------------------------------------
 
-save_png <- function(code) {
-    path <- tempfile(fileext = ".png")
-    png(path, 400, 400)
-    on.exit(dev.off())
-    force(code)
-
-    path
-}
-
-expect_plot <- function(name, code) {
-
-    # 2021-09-17: PEcAn CI builds currently run testthat 3.0.2, but didn't want
-    #   to wait to add this very useful 3.0.4 feature:
-    # When test is skipped or fails with an error in the R code (but not when
-    #   test fails because file changed with no error), testthat normally
-    #   deletes the snapshot as "unused".
-    # That makes it hard to tell after error is fixed whether your fix for the
-    #   error has also changed the output.
-    # With announce_snapshot_file, both the existing snapshot and the new
-    #   version are saved for manual comparison.
-    if (packageVersion("testthat") >= "3.0.4") {
-        announce_snapshot_file(name = name)
-    }
-    fig <- save_png(code)
-
-    expect_snapshot_file(fig, name)
-}
-
-
 test_that("taylor diagram", {
 
     set.seed(1)
@@ -46,7 +17,7 @@ test_that("taylor diagram", {
         model1 = rnorm(6, 10, 3) + 2,
         model2 = rnorm(6, 11, 3) + 2)
 
-    expect_plot(
-        "taylor.png",
-        new.taylor(testdata, siteid = 1:3, runid = 1:2))
+    vdiffr::expect_doppelganger(
+        "taylor diagram",
+        function() new.taylor(testdata, siteid = 1:3, runid = 1:2))
 })
