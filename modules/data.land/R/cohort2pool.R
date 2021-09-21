ca8a1fc55edcde1f2b7d9e8533c23aedeeb00486
Alexis Helgeson <ahelgeso@bu.edu>
2021-09-21 13:00:08 -0400

    "removed /dontrun from Roxygen replaced with @example"

--- modules/data.land/R/cohort2pool.R
+++ modules/data.land/R/cohort2pool.R
@@ -10,11 +10,10 @@
 ##' @param veg_info veg_info object passed from write_ic
 ##'
 ##' @author Saloni Shah
-##'
-##' \dontrun{
+##' @example 
 ##' veg_file <- "~/downloads/FFT_site_1-25665/FFT.2008.veg.rds"
 ##' cohort2pool(veg_File = veg_file, allom_param = NULL)
-##' }
+##' 
 
 cohort2pool <- function(veg_info, allom_param = NULL, dbh_name="DBH") {
   
