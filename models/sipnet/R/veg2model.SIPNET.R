36956199060c206e8ffc11574e7aae8023732b7f
Alexis Helgeson <ahelgeso@bu.edu>
2021-09-22 12:33:31 -0400

    "removed @param source from function"

--- models/sipnet/R/veg2model.SIPNET.R
+++ models/sipnet/R/veg2model.SIPNET.R
@@ -2,12 +2,12 @@
 #' @name veg2model.SIPNET
 #' @title veg2model.SIPNET
 #'
-#' @param ens number of ensemble members
+#' 
 #' @param outfolder location to store ncdf files
 #' @param poolinfo object passed from write_ic contains output from cohort2pool function
 #' @param siteid object passed from write_ic contains site id
-#' @param source object passed from write_ic
-#'
+#' @param ens number of ensemble members
+#' 
 #' @return result object with filepaths to ncdf files
 #' @export
 #' @author Alexis Helgeson
