6aa01ade75931fe30cb73839cdd4b18b2978a4a6
Chris Black <chris@ckblack.org>
2021-10-07 12:17:08 +0200

    run styler

--- models/sibcasa/R/met2model.SIBCASA.R
+++ models/sibcasa/R/met2model.SIBCASA.R
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------
 # Copyright (c) 2012 University of Illinois, NCSA.
 # All rights reserved. This program and the accompanying materials
-# are made available under the terms of the 
+# are made available under the terms of the
 # University of Illinois/NCSA Open Source License
 # which accompanies this distribution, and is available at
 # http://opensource.ncsa.illinois.edu/license.html
@@ -24,12 +24,11 @@ met2model.SIBCASA <- function(in.path, in.prefix, outfolder, overwrite = FALSE) 
 
   # Please follow the PEcAn style guide:
   # https://pecanproject.github.io/pecan-documentation/master/coding-style.html
-  
+
   # Note that `library()` calls should _never_ appear here; instead, put
   # packages dependencies in the DESCRIPTION file, under "Imports:".
   # Calls to dependent packages should use a double colon, e.g.
   #    `packageName::functionName()`.
   # Also, `require()` should be used only when a package dependency is truly
   # optional. In this case, put the package name under "Suggests:" in DESCRIPTION.
-  
 }
--- models/sibcasa/R/write.config.SIBCASA.R
+++ models/sibcasa/R/write.config.SIBCASA.R
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------
 # Copyright (c) 2012 University of Illinois, NCSA.
 # All rights reserved. This program and the accompanying materials
-# are made available under the terms of the 
+# are made available under the terms of the
 # University of Illinois/NCSA Open Source License
 # which accompanies this distribution, and is available at
 # http://opensource.ncsa.illinois.edu/license.html
@@ -22,11 +22,11 @@
 #' @author Anthony Gardella, Rob Kooper
 write.config.SIBCASA <- function(defaults, trait.values, settings, run.id) {
 
-  
+
   # find out where to write run/ouput
   rundir <- file.path(settings$host$rundir, run.id)
   outdir <- file.path(settings$host$outdir, run.id)
-  
+
   #-----------------------------------------------------------------------
   # create launch script (which will create symlink)
   if (!is.null(settings$model$jobtemplate) && file.exists(settings$model$jobtemplate)) {
@@ -34,7 +34,7 @@ write.config.SIBCASA <- function(defaults, trait.values, settings, run.id) {
   } else {
     jobsh <- readLines(con = system.file("template.job", package = "PEcAn.SIBCASA"), n = -1)
   }
-  
+
   # create host specific setttings
   hostsetup <- ""
   if (!is.null(settings$model$prerun)) {
@@ -43,7 +43,7 @@ write.config.SIBCASA <- function(defaults, trait.values, settings, run.id) {
   if (!is.null(settings$host$prerun)) {
     hostsetup <- paste(hostsetup, sep = "\n", paste(settings$host$prerun, collapse = "\n"))
   }
-  
+
   hostteardown <- ""
   if (!is.null(settings$model$postrun)) {
     hostteardown <- paste(hostteardown, sep = "\n", paste(settings$model$postrun, collapse = "\n"))
@@ -51,31 +51,31 @@ write.config.SIBCASA <- function(defaults, trait.values, settings, run.id) {
   if (!is.null(settings$host$postrun)) {
     hostteardown <- paste(hostteardown, sep = "\n", paste(settings$host$postrun, collapse = "\n"))
   }
-  
+
   # create job.sh
   jobsh <- gsub("@HOST_SETUP@", hostsetup, jobsh)
   jobsh <- gsub("@HOST_TEARDOWN@", hostteardown, jobsh)
-  
+
   jobsh <- gsub("@SITE_LAT@", settings$run$site$lat, jobsh)
   jobsh <- gsub("@SITE_LON@", settings$run$site$lon, jobsh)
   jobsh <- gsub("@SITE_MET@", settings$run$site$met, jobsh)
-  
+
   jobsh <- gsub("@START_DATE@", settings$run$start.date, jobsh)
   jobsh <- gsub("@END_DATE@", settings$run$end.date, jobsh)
-  
+
   jobsh <- gsub("@OUTDIR@", outdir, jobsh)
   jobsh <- gsub("@RUNDIR@", rundir, jobsh)
-  
+
   jobsh <- gsub("@BINARY@", settings$model$binary, jobsh)
-  
+
   writeLines(jobsh, con = file.path(settings$rundir, run.id, "job.sh"))
   Sys.chmod(file.path(settings$rundir, run.id, "job.sh"))
-  
+
   #-----------------------------------------------------------------------
   ### Edit a templated config file for runs
   nl_sib <- readLines(con = system.file("namel_sibdrv", package = "PEcAn.SIBCASA"), n = -1)
-  
-  namel_sibdrv <- gsub("@SITE_LAT@", settings$run$site$lat,nl_sib)
+
+  namel_sibdrv <- gsub("@SITE_LAT@", settings$run$site$lat, nl_sib)
   namel_sibdrv <- gsub("@SITE_LON@", settings$run$site$lon, nl_sib)
   namel_sibdrv <- gsub("@SITE_MET@", settings$run$inputs$met$path, nl_sib)
   namel_sibdrv <- gsub("@START_DAY@", format(startdate, "%d"), nl_sib)
@@ -83,10 +83,9 @@ write.config.SIBCASA <- function(defaults, trait.values, settings, run.id) {
   namel_sibdrv <- gsub("@END_YEAR@", format(enddate, "%Y"), nl_sib)
   namel_sibdrv <- gsub("@OUTDIR@", settings$host$outdir, nl_sib)
   namel_sibdrv <- gsub("@OUTFILE@", paste0("out", run.id), nl_sib)
-  
+
   #-----------------------------------------------------------------------
   writeLines(namel_sibdrv, con = paste(, namel_sibdrv, sep = ""))
-  
-  PEcAn.logger::logger.warn("Parameters are currently not handled ")
 
+  PEcAn.logger::logger.warn("Parameters are currently not handled ")
 }
