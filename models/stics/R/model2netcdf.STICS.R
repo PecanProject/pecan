373fcb0dba45b719258137ec0f9004b0f378063f
istfer <fer.istem@gmail.com>
2022-03-29 14:34:39 +0300

    second pass at NEE

--- models/stics/R/model2netcdf.STICS.R
+++ models/stics/R/model2netcdf.STICS.R
@@ -29,7 +29,7 @@ model2netcdf.STICS <- function(outdir, sitelat, sitelon, start_date, end_date, o
   out_files <- list.files(outdir)
   
   stics_out_file <- file.path(outdir, out_files[grepl("mod_s.*", out_files)])
-  stics_output   <- lapply(stics_out_file, read.table, header = T, sep = ";")
+  stics_output   <- lapply(stics_out_file, read.table, header = TRUE, sep = ";")
   stics_output  <- do.call("rbind", stics_output)
   # probably already ordered, but order by year and DoY
   stics_output <-  stics_output[order(stics_output[,1], stics_output[,4]), ]
@@ -60,24 +60,81 @@ model2netcdf.STICS <- function(outdir, sitelat, sitelon, start_date, end_date, o
     outlist[[1]] <- stics_output[thisyear, "lai.n."]  # LAI in (m2 m-2)
     
     # daily amount of CO2-C emitted due to soil mineralisation (humus and organic residues) (kg ha-1 d-1)
-    HeteroResp <- stics_output[thisyear, "CO2sol"]  
-
-    #outlist[[2]] <- udunits2::ud.convert(HeteroResp, "ha-1 day-1", "m-2 s-1")
+    HeteroResp <- udunits2::ud.convert(stics_output[thisyear, "CO2sol"],  "ha-1 day-1", "m-2 s-1")
     
-    # need to study NEE in STICS, growth rate is calcultaed after photosynthesis and remobilized reserves
-    # also check the final utput is not shoot growth only
+    outlist[[2]] <- HeteroResp
     
-    # dltams(n): daily growth rate of the plant (t.ha-1.d-1)
-    dltams      <- stics_output[thisyear, "dltams.n."]   * 1000 * 0.48
+    # AutoResp = AbvResp + RootResp
     
+    # following a similar approach in LPJ-GUESS
+    # AbvResp = ABG * respcoeff * g(T) following Sun et al., 2007 doi: 10.1007/s00376-007-0055-4
+    # masec_kg_ha: Aboveground dry matter (kg ha-1)
+    # CNplante: N concentration in the aboveground plant (% dry weight)
+    
+    # assumptions ---
+    # respcoeff: need some sort of maintenance respiration coefficient parameter, there isn't one in STICS (double check), also should it differ between plant stages?
+    # also this coefficient should differ between plant stages, assuming this will be driven by N
+    # instead of using ABG, I'll use C_abv / CN_abv to account for this (similar to LPJ-GUESS?), so new eqn:*
+    # AbvResp = (C_abv / CN_abv) * respcoeff * g(T)
+    # using leaf_maintenance_respiration_mass variable from BetyDB (umol [CO2] kg-1 s-1) for this respcoeff
+    # no MR rate parameter really have kg N-1 in units but as I'm including N effect here and assume units
+    leaf_maintenance_respiration_mass <- 10000 # hardcoding for development, need a prior on it, note that prior will need to be higher than normal (x ~100)
+    # or pass the normal prior vals but scale it here somehow (maybe with another prior)?
+    
+    # g(T): respiration temperature response function, some Q10 equation, is there something similar in STICS?
+    # gtemp = Q10 ^ ([T-25]/10) for testing using Sun et al.
+    # Q10: using leaf_respiration_Q10 from BetyDB
+    # tcult: crop surface temperature (daily average) degreeC
+    leaf_respiration_Q10 <- 2 # hardcoding for development, need a prior on it
+    gtemp <- leaf_respiration_Q10 ^ ((stics_output[thisyear, "tcult"] - 25) / 10)
+    leafC <- 0.48 # is this calculated by STICS? there is something like Crac: amount of C in roots at harvest
+    AbvResp <- (stics_output[thisyear, "masec_kg_ha"] * leafC * (stics_output[thisyear, "CNplante"])/100) * leaf_maintenance_respiration_mass * gtemp
+    # (kg ha-1) * (umol [CO2] kg-1 s-1) 
+    #  now in umol CO2 ha-1 s-1, pecan units are in kg C m-2 s-1  
+    AbvResp <- udunits2::ud.convert((AbvResp / (44/12)), "ha-1", "m-2")
+    # now umol C to kg C
+    AbvResp <- PEcAn.utils::misc.convert(AbvResp, "umol C m-2 s-1", "kg C m-2 s-1")
+    
+    RootResp <- AbvResp * 1.5 # factor from Moureaux for now
+    ## RootResp = (C_root/ CN_root) * r * g(Tsoil) can use something like this explicitly
+    
+    MaintResp <- AbvResp + RootResp # kg C m-2 s-1  
+    
+    # deriving GPP:  (GPP - MR)*0.72 = NPP
+    
+    # dltams(n): daily growth rate of the plant (t.ha-1.d-1) 
+    dltams      <- udunits2::ud.convert(stics_output[thisyear, "dltams.n."], "ton", "kg") * 0.48 # ton to kgC
     # dltaremobil: daily amount of perennial reserves remobilised (t.ha-1.d-1)
-    dltaremobil <- stics_output[thisyear, "dltaremobil"] * 1000 * 0.48
-    
-    # is it? is autotrophic respiration accounted for?
+    dltaremobil <- udunits2::ud.convert(stics_output[thisyear, "dltaremobil"], "ton", "kg") * 0.48 # ton to kgC
+
     NPP <- dltams - dltaremobil # kgC ha-1 d-1
-    #outlist[[3]] <- udunits2::ud.convert(NPP, "ha-1 day-1", "m-2 s-1")
+    NPP[NPP<0] <- 0
     
-    outlist[[2]] <- -1 * udunits2::ud.convert(NPP - HeteroResp, "ha-1 day-1", "m-2 s-1")
+    # double checking that this is all NPP (above and below)
+    ## this
+    #stics_output[thisyear, "dltams.n."]  # t.ha-1.d-1
+    ##should be roughly equal to this
+    #diff(stics_output[thisyear, "masec.n."])+ diff(stics_output[thisyear, "msrac.n."]) # t.ha-1
+    
+    NPP <- udunits2::ud.convert(NPP, "ha-1 day-1", "m-2 s-1") # kg C m-2 s-1  
+    outlist[[3]] <- NPP
+    
+    GPP <- (NPP / 0.72) + MaintResp
+    outlist[[4]] <- GPP
+    
+    # deriving Growth respiration:  (GPP - MR)*0.28 = GR where 0.28 comes from ORCHIDEE-STICS 10.1016/j.agee.2016.04.017
+    GrowthResp <- (GPP - MaintResp)*0.28
+    
+    AutoResp <- MaintResp + GrowthResp
+    outlist[[5]] <- AutoResp
+    
+    TotalResp <- AutoResp + HeteroResp
+    outlist[[6]] <- TotalResp
+    
+    NEE <- -1*(GPP-TotalResp)
+    outlist[[7]] <- NEE
+      
+
     
     # ******************** Declare netCDF dimensions and variables ********************#
     t <- ncdf4::ncdim_def(name = "time", 
@@ -94,7 +151,12 @@ model2netcdf.STICS <- function(outdir, sitelat, sitelon, start_date, end_date, o
     
     nc_var <- list()
     nc_var[[1]] <- PEcAn.utils::to_ncvar("LAI", dims)
-    nc_var[[2]] <- PEcAn.utils::to_ncvar("NEE", dims)
+    nc_var[[2]] <- PEcAn.utils::to_ncvar("HeteroResp", dims)
+    nc_var[[3]] <- PEcAn.utils::to_ncvar("NPP", dims)
+    nc_var[[4]] <- PEcAn.utils::to_ncvar("GPP", dims)
+    nc_var[[5]] <- PEcAn.utils::to_ncvar("AutoResp", dims)
+    nc_var[[6]] <- PEcAn.utils::to_ncvar("TotalResp", dims)
+    nc_var[[7]] <- PEcAn.utils::to_ncvar("NEE", dims)
     
     # ******************** Declare netCDF variables ********************#
     
