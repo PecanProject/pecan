b90600c936a11095571f1aea4fe7314bc10f5fe9
Alexis Helgeson <ahelgeso@bu.edu>
2022-05-26 15:07:39 -0400

    "updates to SDA fcns"

--- modules/assim.sequential/inst/WillowCreek/SDA_Workflow.R
+++ modules/assim.sequential/inst/WillowCreek/SDA_Workflow.R
@@ -19,7 +19,7 @@ plan(multisession)
 #------------------------------------------Prepared SDA Settings -----
 # ----------------------------------------------------------------------------------------------
 
-outputPath <- "/projectnb/dietzelab/kzarada/US_WCr_SDA_output/"
+outputPath <- "/projectnb/dietzelab/ahelgeso/SDA/Wcr_SDA_Output/"
 nodata <- FALSE #use this to run SDA with no data
 restart <- FALSE#flag to start from previous run or not
 days.obs <- 3 #how many of observed data *BY HOURS* to include -- not including today
@@ -46,47 +46,46 @@ c(
 #------------------------------------------------------------------------------------------------
 
 #reading xml
-settings <- read.settings("/fs/data3/kzarada/pecan/modules/assim.sequential/inst/WillowCreek/testing.xml")
+settings <- read.settings("/projectnb/dietzelab/ahelgeso/SDA/testing.xml")
 
 #connecting to DB
 con <-try(PEcAn.DB::db.open(settings$database$bety), silent = TRUE)
 
-#Find last SDA Run to get new start date 
+#Find last SDA Run to get new start date
+sda.start <- NA
 all.previous.sims <- list.dirs(outputPath, recursive = F)
-if (length(all.previous.sims) > 0 & !inherits(con, "try-error")) {
-  
-  tryCatch({
-    # Looking through all the old simulations and find the most recent
-    all.previous.sims <- all.previous.sims %>%
-      map(~ list.files(path = file.path(.x, "SDA"))) %>%
-      setNames(all.previous.sims) %>%
-      discard( ~ !"sda.output.Rdata" %in% .x) # I'm throwing out the ones that they did not have a SDA output
-    
-    last.sim <-
-      names(all.previous.sims) %>%
-      map_chr( ~ strsplit(.x, "_")[[1]][5]) %>%
-      map_dfr(~ db.query(
-        query = paste("SELECT * FROM workflows WHERE id =", .x),
-        con = con
-      ) %>%
-        mutate(ID=.x)) %>%
-      mutate(start_date = as.Date(start_date)) %>%
-      arrange(desc(start_date), desc(ID)) %>%
-      head(1)
-    # pulling the date and the path to the last SDA
-    restart.path <-grep(last.sim$ID, names(all.previous.sims), value = T)
-    sda.start <- last.sim$start_date+ lubridate::days(3)
-  },
-  error = function(e) {
-    restart.path <- NULL
-    sda.start <- Sys.Date() - 9
-    PEcAn.logger::logger.warn(paste0("There was a problem with finding the last successfull SDA.",conditionMessage(e)))
-  })
-  
-  # if there was no older sims
-  if (is.na(sda.start))
-    sda.start <- Sys.Date() - 9
-}
+# if (length(all.previous.sims) > 0) {
+#     # Looking through all the old simulations and find the most recent
+#     all.previous.sims <- all.previous.sims %>%
+#       map(~ list.files(path = file.path(all.previous.sims, "SDA"))) %>%
+#       setNames(all.previous.sims) %>%
+#       discard( ~ !"sda.output.Rdata" %in% all.previous.sims) # I'm throwing out the ones that they did not have a SDA output
+#     
+#     last.sim <-
+#       names(all.previous.sims) %>%
+#       map_chr( ~ strsplit(names(all.previous.sims), "_")[[1]][5]) %>%
+#       map_dfr(~ db.query(
+#         query = paste("SELECT * FROM workflows WHERE id =", names(all.previous.sims)),
+#         con = con
+#       ) %>%
+#         mutate(ID=names(all.previous.sims))) %>%
+#       mutate(start_date = as.Date(settings$run$start.date)) %>%
+#       arrange(desc(start_date), desc(ID)) %>%
+#       head(1)
+#     # pulling the date and the path to the last SDA
+#     restart.path <-grep(last.sim$ID, names(all.previous.sims), value = T)
+#     sda.start <- last.sim$start_date+ lubridate::days(3)
+# 
+#   error = function(e) {
+#     restart.path <- NULL
+#     sda.start <- Sys.Date() - 9
+#     PEcAn.logger::logger.warn(paste0("There was a problem with finding the last successfull SDA.",conditionMessage(e)))
+#   }
+#   
+#   # if there was no older sims
+#   if (is.na(sda.start))
+#     sda.start <- Sys.Date() - 9
+# }
 #to manually change start date 
 sda.start <- Sys.Date() - lubridate::days(15)
 sda.end <- sda.start + lubridate::days(5)
@@ -108,7 +107,7 @@ prep.data  <- prep.data.assim(
   data.len = days.obs, 
   sda.start)
 
-obs.raw <-prep.data$rawobs
+Axobs.raw <-prep.data$rawobs
 prep.data<-prep.data$obs
 
 # if there is infinte value then take it out - here we want to remove any that just have one NA in the observed data 
@@ -152,7 +151,6 @@ site_info <- list(
   lon = -90.07961,
   time_zone = "UTC")
 
-tryCatch({
   lai <- call_MODIS(outdir = NULL,
                     var = 'lai', 
                     site_info = site_info, 
@@ -164,16 +162,15 @@ tryCatch({
                     package_method = "MODISTools", 
                     QC_filter = TRUE,
                     progress = TRUE)
-  lai <- lai %>% filter(qc == "000")}, 
-  error = function(e) {
-    lai <- NULL
-    PEcAn.logger::logger.warn(paste0("MODIS Data not available for these dates",conditionMessage(e)))
+  
+#filter for good resolution data
+  lai <- lai %>% filter(qc == "000") 
+
+  if(dim(lai)[1] < 1){
+    lai = NA
+    PEcAn.logger::logger.warn(paste0("MODIS mean Data not available for these dates, initialzing NA"))
   }
-)
-if(!exists('lai')){lai = NULL}
 
-
-tryCatch({
   lai_sd <- call_MODIS(outdir = NULL,
                        var = 'lai', 
                        site_info = site_info, 
@@ -185,16 +182,16 @@ tryCatch({
                        package_method = "MODISTools", 
                        QC_filter = TRUE,
                        progress = TRUE)
-  lai_sd <- lai_sd %>% filter(qc == "000")}, 
-  error = function(e) {
-    lai_sd <- NULL
-    PEcAn.logger::logger.warn(paste0("MODIS Data not available for these dates",conditionMessage(e)))
+ 
+#filter for good resolution data
+  lai_sd <- lai_sd %>% filter(qc == "000")
+  
+  if(dim(lai_sd)[1] < 1){
+    lai_sd = NA
+    PEcAn.logger::logger.warn(paste0("MODIS standard deviation Data not available for these dates, initialzing NA"))
   }
-)
-if(!exists('lai_sd')){lai_sd = NULL}
-
+  
 ###### Pad Observed Data to forecast ############# 
-
 date <-
   seq(
     from = lubridate::force_tz(as.POSIXct(last(names(prep.data)), format = "%Y-%m-%d %H:%M:%S"), tz = "UTC") + lubridate::hours(1),
@@ -202,7 +199,7 @@ date <-
     by = "1 hour"
   )
 
-pad.prep <- obs.raw %>%
+pad.prep <- Axobs.raw %>%
   tidyr::complete(Date = date) %>%
   filter(Date %in% date) %>% 
   mutate(means = NA, covs = NA) %>%
@@ -211,13 +208,11 @@ pad.prep <- obs.raw %>%
 
 names(pad.prep) <-date
 
-
 #Add in LAI info 
-
-if(is.null(lai)){index <- rep(FALSE, length(names(prep.data)))}else{
+if(is.na(lai)){
+  index <- rep(FALSE, length(names(prep.data)))}else{
   index <- as.Date(names(prep.data)) %in% as.Date(lai$calendar_date)
 }
-
 
 for(i in 1:length(index)){
   
@@ -238,7 +233,6 @@ for(i in 1:length(index)){
 prep.data = c(prep.data, pad.prep)
 
 #split into means and covs 
-
 obs.mean <- prep.data %>%
   map('means') %>% 
   setNames(names(prep.data))
@@ -250,11 +244,7 @@ obs.cov <- prep.data %>% map('covs') %>% setNames(names(prep.data))
 #-----------------------------------------------------------------------------------------------
 #------------------------------------------ Fixing the settings --------------------------------
 #-----------------------------------------------------------------------------------------------
-#unlink existing IC files
-sapply(paste0("/projectnb/dietzelab/pecan.data/dbfiles/BADM_site_0-676/IC_site_0-676_", 1:100, ".nc"), unlink)
 #Using the found dates to run - this will help to download mets
-settings$run$start.date <- as.character(met.start)
-settings$run$end.date <- as.character(met.end)
 settings$run$site$met.start <- as.character(met.start)
 settings$run$site$met.end <- as.character(met.end)
 #info
@@ -271,7 +261,7 @@ settings <- PEcAn.settings::prepare.settings(settings, force=FALSE)
 setwd(settings$outdir)
 ggsave(
   file.path(settings$outdir, "Obs_plot.pdf"),
-  ploting_fluxes(obs.raw) ,
+  ploting_fluxes(Axobs.raw) ,
   width = 16,
   height = 9
 )
@@ -283,137 +273,65 @@ statusFile <- file.path(settings$outdir, "STATUS")
 if (length(which(commandArgs() == "--continue")) == 0 && file.exists(statusFile)) {
   file.remove(statusFile)
 }
-# Do conversions
 
-######### Check for input files and insert paths #############
-  con <-try(PEcAn.DB::db.open(settings$database$bety), silent = TRUE)
 
-#checks for .nc files for NOAA GEFS
-  input_check <- PEcAn.DB::dbfile.input.check(
-    siteid=settings$run$site$id %>% as.character(),
-    startdate = settings$run$start.date %>% as.Date,
-    enddate = settings$run$end.date %>% as.Date,
-    parentid = NA,
-    mimetype="application/x-netcdf",
-    formatname="CF Meteorology",
-    con,
-    hostname = PEcAn.remote::fqdn(),
-    exact.dates = TRUE,
-    pattern = "NOAA_GEFS",
-    return.all=TRUE
-  )
- 
-#new NOAA GEFS files were going through gapfilled so the parent
-#files of the clim files were the CF_gapfilled nc files
-  input_check_2 <- list()
-  
-  for(i in 1:length(input_check$id)){
-  input_check_2[i] <- PEcAn.DB::dbfile.input.check(
-    siteid=settings$run$site$id %>% as.character(),
-    startdate = settings$run$start.date %>% as.Date,
-    enddate = settings$run$end.date %>% as.Date,
-    parentid = input_check$container_id[i],
-    mimetype="application/x-netcdf",
-    formatname="CF Meteorology",
-    con,
-    hostname = PEcAn.remote::fqdn(),
-    exact.dates = TRUE,
-    pattern = "NOAA_GEFS",
-    return.all=TRUE
-  )$container_id
-  }
+#manually add in clim files 
+con <-try(PEcAn.DB::db.open(settings$database$bety), silent = TRUE)
 
-#this  if statement deals with the NOAA GEFS files that 
-#were gapfilled, and allows for us to find the clim files of the ones that weren't
-#the problem here is that when we get GEFS nc files -> clim files, 
-#the GEFS nc files are the parent ids for finding the clim files 
-#but with GEFS nc -> gapfilled nc -> clim, the gapfilled files are the parent ids for the clim files 
-if(length(input_check_2)>1){
-input_check_2 = unlist(input_check_2)
-
-clim_check = list()
-for(i in 1:length(input_check$id)){
-  clim_check[[i]] = file.path(PEcAn.DB::dbfile.input.check(
+input_check <- PEcAn.DB::dbfile.input.check(
   siteid=settings$run$site$id %>% as.character(),
-  startdate = settings$run$start.date %>% as.Date,
-  enddate = settings$run$end.date %>% as.Date,
-  parentid = input_check_2[i],
+  startdate = settings$run$site$met.start %>% as.Date,
+  enddate = settings$run$site$met.end %>% as.Date,
+  parentid = NA,
   mimetype="text/csv",
   formatname="Sipnet.climna",
-  con,
+  con = con,
   hostname = PEcAn.remote::fqdn(),
+  pattern = "NOAA_GEFS", 
   exact.dates = TRUE,
-  pattern = "NOAA_GEFS",
   return.all=TRUE
-  )$file_path, PEcAn.DB::dbfile.input.check(
-    siteid=settings$run$site$id %>% as.character(),
-    startdate = settings$run$start.date %>% as.Date,
-    enddate = settings$run$end.date %>% as.Date,
-    parentid = input_check_2[i],
-    mimetype="text/csv",
-    formatname="Sipnet.climna",
-    con,
-    hostname = PEcAn.remote::fqdn(),
-    exact.dates = TRUE,
-    pattern = "NOAA_GEFS",
-    return.all=TRUE
-  )$file_name)}}else{
-    for(i in 1:length(input_check$id)){
-      clim_check[[i]] = file.path(PEcAn.DB::dbfile.input.check(
-        siteid=settings$run$site$id %>% as.character(),
-        startdate = settings$run$start.date %>% as.Date,
-        enddate = settings$run$end.date %>% as.Date,
-        parentid = input_check$container_id[i],
-        mimetype="text/csv",
-        formatname="Sipnet.climna",
-        con,
-        hostname = PEcAn.remote::fqdn(),
-        exact.dates = TRUE,
-        pattern = "NOAA_GEFS",
-        return.all=TRUE
-      )$file_path, PEcAn.DB::dbfile.input.check(
-        siteid=settings$run$site$id %>% as.character(),
-        startdate = settings$run$start.date %>% as.Date,
-        enddate = settings$run$end.date %>% as.Date,
-        parentid = input_check$container_id[i],
-        mimetype="text/csv",
-        formatname="Sipnet.climna",
-        con,
-        hostname = PEcAn.remote::fqdn(),
-        exact.dates = TRUE,
-        pattern = "NOAA_GEFS",
-        return.all=TRUE
-      )$file_name)}
-  }#end if/else look for making clim file paths 
+)
 
-  #If INPUTS already exists, add id and met path to settings file
+#If INPUTS already exists, add id and met path to settings file
 
-  if(length(input_check$id) > 0){
-    index_id = list()
-    index_path = list()
-    for(i in 1:length(input_check$id)){
-      index_id[[i]] = as.character(dbfile.id(type = "Input",
-                                             file = file.path(input_check$file_path,
-                                                              input_check$file_name)[i], con = con))#get ids as list
+if(length(input_check$id) > 0){
+  #met paths 
+  clim_check = list()
+  for(i in 1:length(input_check$file_path)){
+    
+    clim_check[[i]] <- file.path(input_check$file_path[i], input_check$file_name[i])
+  }#end i loop for creating file paths 
+  #ids
+  index_id = list()
+  index_path = list()
+  for(i in 1:length(input_check$id)){
+    index_id[[i]] = as.character(input_check$id[i])#get ids as list
+    
+  }#end i loop for making lists
+  names(index_id) = sprintf("id%s",seq(1:length(input_check$id))) #rename list
+  names(clim_check) = sprintf("path%s",seq(1:length(input_check$id)))
+  
+  settings$run$inputs$met$id = index_id
+  settings$run$inputs$met$path = clim_check
+}else{PEcAn.utils::logger.error("No met file found")}
+#settings <- PEcAn.workflow::do_conversions(settings, T, T, T)
 
-    }#end i loop for making lists
-    names(index_id) = sprintf("id%s",seq(1:length(input_check$id))) #rename list
-    names(clim_check) = sprintf("path%s",seq(1:length(input_check$id)))
-
-    settings$run$inputs$met$id = index_id
-    settings$run$inputs$met$path = clim_check
-  }
-
+if(is_empty(settings$run$inputs$met$path) & length(clim_check)>0){
+  settings$run$inputs$met$id = index_id
+  settings$run$inputs$met$path = clim_check
+}
+PEcAn.DB::db.close(con)
 #still want to run this to get the IC files 
-settings <- PEcAn.workflow::do_conversions(settings)          #end if loop for existing inputs  
+#settings <- PEcAn.workflow::do_conversions(settings)          #end if loop for existing inputs  
 
- # if(is_empty(settings$run$inputs$met$path) & length(clim_check)>0){
+# if(is_empty(settings$run$inputs$met$path) & length(clim_check)>0){
 #   settings$run$inputs$met$id = index_id
 #   settings$run$inputs$met$path = clim_check
 # }
 
 
 # PEcAn.DB::db.close(con)
+
 # Query the trait database for data and priors
 if (PEcAn.utils::status.check("TRAIT") == 0) {
   PEcAn.utils::status.start("TRAIT")
@@ -432,8 +350,10 @@ if (!is.null(settings$meta.analysis)) {
     PEcAn.utils::status.end()
   }
 }
+
 #sample from parameters used for both sensitivity analysis and Ens
 get.parameter.samples(settings, ens.sample.method = settings$ensemble$samplingspace$parameters$method)
+
 # Setting dates in assimilation tags - This will help with preprocess split in SDA code
 settings$state.data.assimilation$start.date <-as.character(first(names(obs.mean)))
 settings$state.data.assimilation$end.date <-as.character(last(names(obs.mean)))
@@ -548,18 +468,7 @@ if(restart == TRUE){
 #--------------------------------- Run state data assimilation -------------------------------------
 # --------------------------------------------------------------------------------------------------
 
-settings$host$name <- "geo.bu.edu"
-settings$host$user <- 'kzarada'
-settings$host$folder <- "/projectnb/dietzelab/kzarada/US_WCr_SDA_output"
-settings$host$job.sh <- "module load udunits/2.2.26 R/3.5.1" 
-settings$host$qsub <- 'qsub -l h_rt=24:00:00 -V -N @NAME@ -o @STDOUT@ -e @STDERR@'
-settings$host$qsub.jobid <- 'Your job ([0-9]+) .*'
-settings$host$qstat <- 'qstat -j @JOBID@ || echo DONE'
-settings$host$tunnel <- '/tmp/tunnel'
-settings$model$binary = "/usr2/postdoc/istfer/SIPNET/1023/sipnet"
-
-
-source('/fs/data3/kzarada/pecan/modules/assim.sequential/R/Nimble_codes.R')
+source('/projectnb/dietzelab/ahelgeso/pecan/modules/assim.sequential/R/Nimble_codes.R')
 
 
 if(restart == FALSE) unlink(c('run','out','SDA'), recursive = T)
@@ -570,7 +479,7 @@ if ('state.data.assimilation' %in% names(settings)) {
     PEcAn.utils::status.start("SDA")
     PEcAn.assim.sequential::sda.enkf(
       settings, 
-      restart=restart,
+      restart=FALSE,
       Q=0,
       obs.mean = obs.mean,
       obs.cov = obs.cov,
