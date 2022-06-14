b93704b2e57b2d5434a562ed03542cd7b85405db
DongchenZ <zhangdc@bu.edu>
2022-06-14 11:54:29 -0400

    updated this function

--- scripts/pft.add.spp.R
+++ scripts/pft.add.spp.R
@@ -17,77 +17,100 @@
 ##' @param con Database connection object.  If not provided will use \code{\link{query.bety.con}} to connect to BETY-db.
 ##' @param ... optional arguements for connecting to database (e.g. password, user name, database)
 ##' @return Function does not return a value but does print out diagnostic statements.
-##' @author Michael C. Dietze
+##' @author Michael C. Dietze, Dongchen Zhang
 pft.add.spp <- function(pft, acronym, test = TRUE, con = NULL, ...) {
   
   ## establish database connection
+  # default points to psql-pecan.bu.edu.
   if (is.null(con)) {
-    con <- query.bety.con(...)
+    bety <- dplyr::src_postgres(dbname   = "bety",
+                                host     = "psql-pecan.bu.edu",
+                                user     = "bety",
+                                password = "bety")
+    con <- bety$con
+  }
+  
+  #detect if acronym is ID or Symbol
+  if(is.character(acronym[1])){
+    ID_flag <- F
+  }else if(is.numeric(acronym[1])){
+    ID_flag <- T
+  }else{
+    print("can't detect the type of Acronym, please check it!!!")
+    return(0)
   }
   
   ## look up pfts.id based on pfts.name
-  q <- dbSendQuery(con, paste0("select * from pfts where name =\"", pft, "\""))
-  my.pft <- fetch(q, n = -1)
+  pft_qry <- glue::glue_sql(paste0("select * from pfts where name = '", pft, "'"), .con = con)
+  my.pft <- PEcAn.DB::db.query(con = con, query = pft_qry)
   
   ## if pfts.name does not exist, stop and send error
-  if (nrow(my.pft) != 1) {
-    print("PFT did not match uniquely.  Match=")
-    print(my.pft)
-    print("Similar pfts")
-    q <- dbSendQuery(con, paste0("select * from pfts where name like \"%", pft, "%\""))
-    print(fetch(q, n = -1))
-    return()
-  } else {
-    print("using PFT:")
-    print(my.pft)
+  if(nrow(my.pft) > 1){
+    print("More than one pft matched!!! Please check your pft name and make sure it is unique to all other pfts!!!")
+    return(my.pft)
+  }else if(nrow(my.pft) == 0){
+    print("No pft founded that matched the name!!! Please check your pft name!!!")
+    return(0)
   }
+  
+  #initialize inputid to store IDs
+  inputid <- c()
+  
+  #initialize bad to store any problematic items.
+  bad_species <- c()
+  bad_pft_speceis <- c()
   
   ## loop over acronyms
   for (acro in acronym) {
-    
-    ## look up plant_id based on acronyms
-    q <- dbSendQuery(con, paste0("select id, ScientificName,Symbol from plants where Symbol = '", acro, "'"))
-    my.plant <- fetch(q, n = -1)
-    if (nrow(my.plant) != 1) {
-      print(c("ACRONYM not matched", acro))
-      print(my.plant)
-      next
+    ## look up species based on acronyms. (can be either Symbols or IDs)
+    if(ID_flag){
+      species_qry <- glue::glue_sql(paste0("select * from species where \"id\" = '", acro, "'"), .con = con)
+    }else{
+      species_qry <- glue::glue_sql(paste0("select * from species where \"Symbol\" = '", acro, "'"), .con = con)
     }
-    
-    ## look up species.id based on plant_id
-    q <- dbSendQuery(con, paste0("select * from species where plant_id = '", my.plant$id, "'"))
-    my.species <- fetch(q, n = -1)
+    my.species <- PEcAn.DB::db.query(con = con, query = species_qry)
+
+    #if species not matched with bety records
     if (nrow(my.species) != 1) {
-      print(c("PLANT_ID not matched", acro, my.plant$id))
+      print(c("ACRONYM not matched", acro))
       print(my.species)
+      bad_species <- c(bad_species, acro)
       next
     }
     
     ## look up pfts_species.specie_id to check for duplicates
-    q <- dbSendQuery(con, paste0("select * from pfts_species where pft_id = '", my.pft$id, "' and specie_id = '", my.species$id, 
-                                 "'"))
-    my.pft2spp <- fetch(q, n = -1)
-    if (nrow(my.pft2spp) > 0) {
+    species_pft_qry <- glue::glue_sql(paste0("select * from pfts_species where pft_id = '", my.pft$id,"' and specie_id = '",my.species$id ,"'"), .con = con)
+    pft_species <- PEcAn.DB::db.query(con = con, query = species_pft_qry)
+    
+    #if record already exists
+    if (nrow(pft_species) > 0) {
       print(c("Species already exists for PFT", acro))
-      print(my.species)
+      print(pft_species)
+      bad_pft_speceis <- c(bad_pft_speceis, paste0("Specie_ID: ", my.species$id, " . pft_ID: ", my.pft$id, "."))
       next
     }
     
     ## give list of species
     print(c("ADDING", acro))
-    # print(my.plant) print(my.species)
+    # print(my.species) print(my.species)
     if (test) {
-      # print('TEST ONLY')
+      print('TEST ONLY')
+      if(nrow(pft_species) == 0 && nrow(my.species) == 1 && nrow(my.pft) == 1){
+        print("pft exists and unique; specie exists and unique; pft_species does not exists; Therefore, it's ready to go!!!")
+        print(acro)
+      }
       next
     }
     
     ## if a species is not already in the pft, add
-    q <- dbSendQuery(con, paste0("insert into pfts_species set pft_id = '", my.pft$id, "', specie_id = '", my.species$id, "'"))
+    cmd <- paste0(
+      "INSERT INTO pfts_species ",
+      "(pft_id, specie_id) VALUES ('",my.pft$id, "', '", my.species$id, "') RETURNING id"
+    )
+    # This is the id that we just registered
+    inputid <- c(PEcAn.DB::db.query(query = cmd, con = con), inputid)
     
   }  ## end loop over acronyms
-  
-  if (test) {
-    print("THIS WAS A TEST, NO SPECIES WERE ADDED. SET 'test = FALSE' TO COMMIT CHANGES")
-  }
+  return(list(input_ID = inputid, bad_species = bad_species, bad_pft_speceis = bad_pft_speceis))
   
 } # pft.add.spp
