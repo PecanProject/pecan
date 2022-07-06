202837995ccc9ff97a966a89b02cc18110a3370a
Alexis Helgeson <ahelgeso@bu.edu>
2022-07-06 09:37:48 -0400

    "unexpected else flagged by make"

--- modules/assim.sequential/R/sda.enkf_MultiSite.R
+++ modules/assim.sequential/R/sda.enkf_MultiSite.R
@@ -251,8 +251,6 @@ sda.enkf.multisite <- function(settings,
       
       #build X using previous forecast output
       X <- build_X(runid, settings, new.params, nens = length(runid), read_restart_times, sim.time, outdir = paste0(old.dir, "out/"))
-  }else{
-    sim.time<-seq_len(nt)
   }
   
   ###-------------------------------------------------------------------###
