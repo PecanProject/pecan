1e706a8aeba2b4ee5f7d37217937d29cb8d1828b
jbowers20 <bowerj79@uwosh.edu>
2022-07-15 12:23:54 -0400

    Revert "Made changes to download_SMAP"
    This reverts commit 5f6d6937242853e5fe0f762357042949cea13096.

--- modules/data.remote/inst/SoilMoistModelData.R
+++ modules/data.remote/inst/SoilMoistModelData.R
@@ -1,23 +1,3 @@
-##'@name SoilMoistModelData
-##'@description: This script collects ensemble model data from a specified file location, 
-##'analyzes that data to produce two exploratory graphs, and finally compares the
-##'model data to actual soil moisture data collected from NASA's SMAP satellite to
-##'produce an additional 5 graphs and a set of comparative statistics including: pbias, 
-##'RMSE, CRPS, Correlation, and r-squared values.
-##'
-##'This script is broken into 4 functions: extractSoilMoist, extractEnsambles, 
-##'runAnalysis, and main. The functions can be used separately, however, it is 
-##'meant for the main method to be run to with a series of site id number and the
-##'desired date ranges for those sites.  
-##'
-##'
-##'@author Joshua Bowers
-##'
-##'@examples
-##'
-##'
-
-# load necessary packages
 library(tidyverse)
 library(plotly)
 library(patchwork)
@@ -33,27 +13,60 @@ library(hydroGOF)
 library(Metrics)
 library(verification)
 
+sipnet.out <- "/projectnb/dietzelab/dongchen/All_NEON_SDA/SDA/out/ENS-00001-1000004927/sipnet.2012-07-16.out"
+sipnet.out1 <- "/projectnb/dietzelab/hamzed/SDA/ProductionRun/50Sites/SDA_50Sites_1000008768/out/ENS-00001-769/sipnet.out"
+text <- readLines(sipnet.out)[-1]
+csv <- read.csv(textConnection(text), sep = "")
+
+
+plot(csv)
+
+site_pft <- read.csv("/projectnb/dietzelab/hamzed/SDA/ProductionRun/50Sites/SDA_50Sites_1000008768/out/ENS-00001-769/sipnet.out")
+
+plot(csv$day,csv$LAI)
+plot(csv$day,csv$gpp)
+
+clim.path <- "/projectnb/dietzelab/dongchen/Multi-site/download_500_sites/SDA/run/ENS-00001-646/sipnet.clim"
+text.clim <- readLines(clim.path)
+csv.clim <- read.csv(textConnection(text.clim), sep="", header = F)
+plot(csv.clim$V3, csv.clim$V10)
+
+sipnet.out1 <- "/projectnb/dietzelab/hamzed/SDA/ProductionRun/200Sites/SDA_200Sites_1000008769/out/ENS-00001-622/sipnet.2004-07-16.out"
+
+text1 <- readLines(sipnet.out1)[-1]
+csv1 <- read.csv(textConnection(text1), sep = "")
+#csv1_1986 <- csv1[csv1$year==1986,]
+plot(csv1$day, csv1$nee)
+
+#csv_2001 <- csv[csv$year==2001,]
+plot(csv$day, csv$LAI)
+
+
+param.path <- "/projectnb/dietzelab/dongchen/Multi-site/download_500_sites/SDA/run/ENS-00001-646/sipnet_2001_2002.param"
+text.param <- readLines(param.path)
+csv.param <- read.csv(textConnection(text.param), sep="", header = F)
+
+
+
+
+
+
+
+#############################################################################################################################
+
+sipnet.out <- "/projectnb/dietzelab/dongchen/All_NEON_SDA/NEON42/SDA_testrun/out/ENS-00025-1000004921/sipnet.2015-07-16.out"
+text <- readLines(sipnet.out)[-1]
+csv <- read.csv(textConnection(text), sep = "")
+plot(csv$day, csv$soilWater)
+
+
+
+
 #check on nc file
+## Harvard Forest also = 1000004921
 #################### Extracts Soil Moisture by Year ####################
 setwd('/projectnb2/dietzelab/dongchen/All_NEON_SDA/NEON42/SDA_testrun/out')
 
-#' @title extractSoilMoist
-#' @description extracts a single ensamble member during the given year range from a specified file location
-#'
-#' @param betySiteId bety site id for the desired location (String)
-#' @param yearStart starting year YYYY as (date) or (numeric)
-#' @param yearEnd ending year YYYY as (date) or (numeric)
-#' @param ENS ensamble member front padded with 0's to five values (String)
-#'
-#' @return A list containing soil moisture and time values
-#'
-#' @examples 
-#' extractSoilMoist(betySiteId = '1000004945', yearStart = 2016, yearEnd = 2017, ENS = '25')
-#' 
-#' This extracts soil moisture values and time values from the 25th ensamble member netCDF file 
-#' in a specified file directory for the Harvard Forest ('1000004945') site during the years 
-#' 2016 and 2017.
-#' 
 extractSoilMoist <- function(betySiteId, yearStart, yearEnd, ENS){
 
   for (x in 0:(yearEnd - yearStart)) {
@@ -71,26 +84,7 @@ extractSoilMoist <- function(betySiteId, yearStart, yearEnd, ENS){
   return(list(time, soilMoist))
 }
 
-
-#' @title extractEnsamlbes
-#' @description utilizes extractSoilMoist function to extract an entire ensamble of a specified
-#' over the course of a specified date range.
-#'
-#' @param betySiteId bety site id for the desired location as (String)
-#' @param start starting date YYYY-mm-dd as (date)
-#' @param end ending date YYYY-mm-dd as (date)
-#' @param ENSnum number of members in the ensamble as (numeric)
-#'
-#' @return a data frame consisting of the soil mositure as well as their date-time values 
-#' during the specified date range
-#' 
-#'
-#' @examples 
-#' extractEnsambles(betySiteId = '1000004945', start = as.Date('2016-01-02'), end = as.Date('2016-07-01'))
-#' 
-#' Extracts all 25 ensample members (default) from the Harvard Forest (1000004945) site between the
-#' '2016-01-02' - '2016-07-01' date range.
-#' 
+#################### Collects Multiple Ensembles and Plots ####################
 extractEnsambles <- function(betySiteId, start, end, ENSnum=25){
   for (x in 1:ENSnum) {
     newEns <- extractSoilMoist(betySiteId = betySiteId, yearStart = as.numeric(format(start, '%Y')), 
@@ -111,26 +105,7 @@ extractEnsambles <- function(betySiteId, start, end, ENSnum=25){
   return(vtr)
 }
 
-
-#' @title runAnalysis
-#' 
-#' @description Runs multiple analyses on the specified site during a given date range
-#' and has the functionality to compare those values to actual soil moisture values
-#' collected from SMAP
-#'
-#' @param betySiteId bety site id for the desired location as (String)
-#' @param start starting date YYYY-mm-dd as (date)
-#' @param end ending date YYYY-mm-dd as (date)
-#' @param compare (boolean) to turn on/off comparison to actual smap data
-#'
-#' @return returns a list consisting of all of the analysis plots and statistics
-#'
-#' @examples 
-#' var <- runAnalysis('1000004945', '2016-01-02', '2016-07-16', TRUE)
-#' This runs an analysis on the Harvard Forest (1000004945) site with results between
-#' the date range of '2016-01-02' and '2016-07-16'
-#' 
-runAnalysis <- function(betySiteId, start, end, compare = TRUE){
+runAnalysis <- function(betySiteId, start, end, compare){
   
   moistureFrame <- extractEnsambles(betySiteId, as.Date(start), as.Date(end))
   
@@ -156,7 +131,7 @@ runAnalysis <- function(betySiteId, start, end, compare = TRUE){
     ggtitle('Hex Distribution of Model Data') +
     geom_line(data = mean_per_ENS, aes(x = time, y = mean, color = 'Mean of Model Data')) + 
     labs(colour = '')
-  print(p1)
+  
   
   ## Min/Max Values with 95% Quantile Range ## Still has messed up color legend
   p2 <- ggplot(grouped_ENS, aes(x = time, y = moisture)) + theme_bw() + 
@@ -166,10 +141,11 @@ runAnalysis <- function(betySiteId, start, end, compare = TRUE){
     geom_line(data = mean_per_ENS, aes(x = time, y = mean, color = 'Mean')) +
     geom_line(data = quantiles_lower, aes(x = time, y = q_lower)) +
     geom_line(data = quantiles_upper, aes(x = time, y = q_upper))
-  print(p2)
   
   if (compare) {
     ## Analyses Requiring Actual SMAP Data ##
+    ## Download SMAP data ##
+    source('~/pecan/modules/data.remote/inst/download_SMAP.R')
     smap_data <- all_smap_data[[betySiteId]]
     
       
@@ -177,24 +153,27 @@ runAnalysis <- function(betySiteId, start, end, compare = TRUE){
     plot(smap_data$Date, smap_data$ssm, main='SMAP ssm (Surface Soil Moisture) vs Time')
     p3 <- recordPlot()
     
+    ## ggplot Version with Linear Regression Line ##
+    p4 <- ggplot(smap_data, aes(x = Date, y = ssm)) + geom_point() +
+      geom_smooth(data = smap_data, aes(x = Date, y = ssm), method='lm', formula= y~x) +
+      ggtitle('ggplot Version with Linear Regression Line') +
+      theme(plot.title = element_text(hjust = 0.5))
     
     ## Hex distribution of Model Data with Actual Values as well as Mean and Quantile Range (Way too much) ##
-    p4 <- ggplot(moistureFrame, aes(x = time, y = moisture)) + geom_hex() + theme_bw() + 
+    p5 <- ggplot(moistureFrame, aes(x = time, y = moisture)) + geom_hex() + theme_bw() + 
       geom_line(data = mean_per_ENS, aes(x = time, y = mean, color = 'Mean of Model Data')) +
       geom_line(data = quantiles_lower, aes(x = time, y = q_lower, color = '95% Quantile Range')) + 
       geom_line(data = quantiles_upper, aes(x = time, y = q_upper, color = '95% Quantile Range')) +
       geom_line(data = smap_data, aes(x = as_datetime(Date), y = ssm, color = 'Actual SMAP Values')) +
       ggtitle('Way too Much...')
-    print(p4)
     
     ## Actual Data with Mean and 95% quantile range of Model Data ##
-    p5 <- ggplot() + theme_bw() + 
+    p6 <- ggplot() + theme_bw() + 
       geom_line(data = mean_per_ENS, aes(x = time, y = mean, color = 'Mean of Model Data')) +
       geom_line(data = quantiles_lower, aes(x = time, y = q_lower, color = '95% Quantile Range')) + 
       geom_line(data = quantiles_upper, aes(x = time, y = q_upper, color = '95% Quantile Range')) +
       geom_line(data = smap_data, aes(x = as_datetime(Date), y = ssm, color = 'Actual SMAP Values')) + 
       ggtitle('Actual SMAP vs Mean and 95% Quantile Range of Model Data')
-    print(p5)
     
     ## Collapsing Temporal Scale to Match SMAP Output Data ##
     vec_time <- vector()
@@ -223,7 +202,7 @@ runAnalysis <- function(betySiteId, start, end, compare = TRUE){
     mod <- lm(smap_data$ssm ~ df$vec_mean)
     plot(df$vec_mean, smap_data$ssm, main= 'Actual SMAP vs Mean Model Data\nw/ Linear Regression Line') + 
       abline(mod)
-    p6 <- recordPlot()
+    p7 <- recordPlot()
     
     ## Calculating Bias ##
     pbias <- pbias(df['vec_mean'], smap_data['ssm'], rm.NA=TRUE)
@@ -246,12 +225,12 @@ runAnalysis <- function(betySiteId, start, end, compare = TRUE){
     ## Quick Plot of Residuals vs. Time ##
     plot(df$vec_time, mod$residuals, main='Residuals vs. Time') +
       abline(h = 0, lty = 2)
-    p7 <- recordPlot()
+    p8 <- recordPlot()
     
-    vtr <- list(p1, p2, p3, p4, p5, p6, p7, pbias, RMSE, CRPS, correlation, r.squared)
+    vtr <- list(p1, p2, p3, p4, p5, p6, p7, p8, pbias, RMSE, CRPS, correlation, r.squared)
     
     names(vtr) <-  c('Plot1', 'Plot2', 'Plot3', 'Plot4', 'Plot5', 'Plot6', 'Plot7',
-                     'pbias','RMSE', 'CRPS', 'correlation', 'r.squared')
+                     'Plot8', 'pbias','RMSE', 'CRPS', 'correlation', 'r.squared')
     
   } else {
     vtr <- list(p1, p2)
@@ -261,27 +240,36 @@ runAnalysis <- function(betySiteId, start, end, compare = TRUE){
   return(vtr)
 }
 
-
-## Download SMAP data ##
-source('~/pecan/modules/data.remote/inst/download_SMAP.R')
 all_smap_data <- download_SMAP('2016-01-02', '2016-07-16')
 
 ## Test Zone ##
+run1 <- runAnalysis('1000004945', '2016-01-02', '2016-07-16', TRUE)
+run1$Plot5
+run2 <- runAnalysis('1000004931', '2016-01-02', '2016-07-16', TRUE)
+run2$Plot5
 
-# Test single
-var <- runAnalysis('1000004945', '2016-01-02', '2016-07-16') # harvard
-var1 <- runAnalysis('1000004876', '2016-01-02', '2016-07-16') # santa rita
-var2 <- runAnalysis('1000004927', '2016-01-02', '2016-07-16') # konza
-var3 <- runAnalysis('1000004916', '2016-01-02', '2016-07-16') # ordway
+run3 <- runAnalysis('1000004890', '2016-01-02', '2016-07-16', TRUE)
+run3$Plot5
+run4 <- runAnalysis('1000004924', '2016-01-02', '2016-07-16', TRUE)
+run4$Plot5
 
-
-# Test all
-all_sites <- PEcAn.settings::read.settings("~/pecan.xml")
-
-for (index in 1:length(all_sites)) {
-  if (index != 4 & index != 33 & index != 37){
-    runAnalysis(all_sites[[index]]$run$site$id, '2016-01-02', '2016-07-16', TRUE)
-  }
-}
+run5 <- runAnalysis('1000004918', '2016-01-02', '2016-07-16', TRUE)
+run5$Plot5
+run6 <- runAnalysis('1000004882', '2016-01-02', '2016-07-16', TRUE)
+run6$Plot5
 
 ##^ Test Zone ^##
+
+#############################################################################################################################
+  
+  
+  
+  
+#check on the param files
+param1.path <- "/projectnb/dietzelab/dongchen/All_NEON_SDA/SDA/run/ENS-00001-1000004927/sipnet.param"
+param2.path <- "/projectnb/dietzelab/dongchen/All_NEON_SDA/SDA/run/ENS-00001-646/sipnet.param"
+#sipnet 2402
+param1 <- readLines(param1.path)
+param1 <- read.csv(textConnection(param1), sep="", header = F)
+param2 <- readLines(param2.path)
+param2 <- read.csv(textConnection(param2), sep="", header = F)
