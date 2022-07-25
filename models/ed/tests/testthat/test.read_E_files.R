cff37de9378c90ff6dfb98e63c02bd577364f1fa
Eric R. Scott <scottericr@gmail.com>
2022-07-25 16:01:35 -0400

    fix tests for read_E_files

--- models/ed/tests/testthat/test.read_E_files.R
+++ models/ed/tests/testthat/test.read_E_files.R
@@ -1,19 +1,29 @@
-
-context("test possible inputs to read_E_file")
-
 outfolder <- tempdir()
-h5_file <- "analysis-E-2004-04-00-000000-g01.h5"
+h5_file <- "analysis-E-2004-07-00-000000-g01.h5"
 h5_path <- system.file("tests/testthat/data", h5_file, package = "PEcAn.ED2")
 file.copy(h5_path, file.path(outfolder, h5_file))
+settings_file <- "pecan_checked.xml"
+settings_path <- system.file("tests/testthat/data", settings_file, package = "PEcAn.ED2")
+file.copy(settings_path, file.path(outfolder, settings_file))
+test_settings <- PEcAn.settings::read.settings(file.path(outfolder, "pecan_checked.xml"))
+test_settings$outdir <- outfolder
 
 test_that("read E files without settings arg or ED2 pft number", {
   year <- 2004
   year_files <- 2004
   start_date <- "2004/04/01"
   end_date <- "2004/06/01"
-  pft_without_number <- list(pft = list(name = 'ebifarm.forb'))
+  pfts_without_number <- list(
+    pft = list(
+      name = 'SetariaWT',
+      ed2_pft_number = "1"
+    ),
+    pft = list(
+      name = 'ebifarm.c3grass'
+    )
+  )
   result <- read_E_files(year, year_files, h5_file, outfolder, start_date, 
-                         end_date, pft_without_number)
+                         end_date, pfts_without_number)
   expect_type(result, "list")
   expect_equal(length(result), 4)
 })
@@ -33,11 +43,6 @@ test_that("read E files without settings arg and with ED2 pft number", {
 test_that("read E files without only settings arg", {
   year <- 2004
   year_files <- 2004
-
-  test_settings <- list(outdir = outfolder, 
-                           run = list(start.date = "2004/04/01", end.date = "2004/06/01",
-                                      site = list(lat = 40.0637, lon = -88.202)), 
-                           pfts = list(pft = list(name = "SetariaWT", ed2_pft_number = "1")))
   result <- read_E_files(year, year_files, h5_file, settings = test_settings)
   expect_type(result, "list")
   expect_equal(length(result), 4)
