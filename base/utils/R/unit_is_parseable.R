038be6672f169ada7a814f7981bd52a8ef0c7384
Tanishq Jain <tanishqjain010@gmail.com>
2022-08-05 22:20:48 +0530

    Update base/utils/R/unit_is_parseable.R
    Co-authored-by: Chris Black <chris@ckblack.org>

--- base/utils/R/unit_is_parseable.R
+++ base/utils/R/unit_is_parseable.R
@@ -3,13 +3,18 @@
 ##' Function will replace the now-unmaintained `udunits2::ud.is.parseable`
 ##' @author Tanishq Jain
 ##' 
-##' @param u1 A character string representing a type of units
+##' @param unit A character string representing a type of units
 ##' 
 ##' @return TRUE if the units is parseable, FALSE otherwise.
+##'
+##' @examples
+##'   unit_is_parseable("g/sec^2")
+##'   unit_is_parseable("kiglometters")
+##'
 ##' @export
-unit_is_parseable <- function(u1){
+unit_is_parseable <- function(unit){
   tryCatch({
-    if(units::as_units(u1))
+    if(units::as_units(unit))
       return(TRUE)
   },
   error = function(e) FALSE
