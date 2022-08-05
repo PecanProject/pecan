143dd2e1202407a19c5ec74122694e703584bcbd
Tanishq Jain <tanishqjain010@gmail.com>
2022-08-05 22:25:22 +0530

    Update base/utils/tests/testthat/test-unit_is_parseable.R
    Co-authored-by: Chris Black <chris@ckblack.org>

--- base/utils/tests/testthat/test-unit_is_parseable.R
+++ base/utils/tests/testthat/test-unit_is_parseable.R
@@ -6,12 +6,17 @@ test_that("parseable unit", {
   expect_true(unit_is_parseable("kg"))
 })
 
-test_that("Non-paresable unit", {
+test_that("Non-parseable unit", {
   expect_false(unit_is_parseable("fake"))
-  expect_false(unit_is_parseable("gk"))
+  expect_false(unit_is_parseable("kg / fake"))
   expect_false(unit_is_parseable(NULL))
-  expect_false(unit_is_parseable("Not parseable"))
-  expect_false(unit_is_parseable("Loading"))
+
+  # Note: This behavior differs from `udunits2::ud.is.parseable("")`
+  #  (which returns TRUE), but is better aligned with PEcAn's usage
+  #  of "parseable" to mean "will work when passed to ud_convert".
+  # Since ud_convert does not support any useful conversions of "",
+  #  we report it as unparseable.
+  expect_false(unit_is_parseable(""))
 })
 
 # This differ from `udunits2::ud.is.parseable`
