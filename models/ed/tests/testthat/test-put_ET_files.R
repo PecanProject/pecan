07d50b5d1100a708664ee8edcbb7cc546e9704ca
Eric R. Scott <scottericr@gmail.com>
2022-08-29 15:40:36 -0400

    rename test

--- models/ed/tests/testthat/test-put_ET_files.R
+++ models/ed/tests/testthat/test-put_ET_files.R
@@ -2,18 +2,27 @@ outdir <- tempfile()
 withr::defer(unlink(outdir, recursive = TRUE))
 unzip("data/ed2_run_output.zip", exdir = outdir)
 file.copy("data/pecan_checked.xml", file.path(outdir, "pecan_checked.xml"))
-h5_file <- "analysis-E-2004-07-00-000000-g01.h5"
+e_file <- "analysis-E-2004-07-00-000000-g01.h5"
+t_file <- "analysis-T-2004-00-00-000000-g01.h5"
 
 test_settings <-
   PEcAn.settings::read.settings(file.path(outdir, "pecan_checked.xml"))
 test_settings$outdir <- outdir
 year <- 2004
 year_files <- 2004
-var_list <-
+var_list_E <-
   read_E_files(
     yr = year,
     yfiles = year_files,
-    efiles = h5_file,
+    h5_files = e_file,
+    settings = test_settings
+  )
+
+var_list_T <-
+  read_T_files(
+    yr = year,
+    yfiles = year_files,
+    h5_files = t_file,
     settings = test_settings
   )
 # create lat/long nc variables
@@ -32,12 +41,33 @@ test_that("put_E_values() runs", {
     put_E_values(
       yr = year,
       nc_var = list(),
-      var_list = var_list,
+      var_list = var_list_E,
       lat = lat,
       lon = lon,
+      start_date = lubridate::ymd(settings$run$start.date),
+      end_date = lubridate::ymd(settings$run$end.date),
       settings = test_settings
     ),
     "list"
   )
 })
 
+test_that("put_T_values() runs", {
+  expect_is(
+    put_T_values(
+      yr = year,
+      nc_var = list(),
+      var_list = var_list_T,
+      lat = lat,
+      lon = lon,
+      start_date = lubridate::ymd(settings$run$start.date),
+      end_date = lubridate::ymd(settings$run$end.date),
+      settings = test_settings
+    ),
+    "list"
+  )
+})
+
+#TODO: test if all vars are in output
+#TODO: test if dimensions are correct and consistent
+#TODO: test behvaior when nc_var is not empty
