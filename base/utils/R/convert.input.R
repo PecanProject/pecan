84063f90f3d124dcd195e0cd9ee0a1b5a3f31300
Tanishq Jain <tanishqjain010@gmail.com>
2022-08-31 19:49:09 +0000

    moved temporarily to utils

--- base/utils/R/convert.input.R
+++ base/utils/R/convert.input.R
@@ -65,7 +65,7 @@
 ##' @export
 ##' @author Betsy Cowdery, Michael Dietze, Ankur Desai, Tony Gardella, Luke Dramko
 
-convert_input <-
+convert.input <-
   function(input.id,
            outfolder,
            formatname,
