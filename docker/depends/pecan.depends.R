0f7bdba80d155b4f7345006c45f1d0b3f2aae6a4
Eric R. Scott <scottericr@gmail.com>
2022-09-06 10:56:18 -0400

    add debug to check if token exists

--- docker/depends/pecan.depends.R
+++ docker/depends/pecan.depends.R
@@ -8,7 +8,7 @@ rlib <- Sys.getenv('R_LIBS_USER', '/usr/local/lib/R/site-library')
 Sys.setenv(RLIB = rlib)
 
 # install remotes first in case packages are references in dependencies
-lapply(c(
+remotes::install_github(c(
 'araiho/linkages_package',
 'chuhousen/amerifluxr',
 'ebimodeling/biocro@0.951',
@@ -17,7 +17,10 @@ lapply(c(
 'r-lib/vdiffr@v1.0.2',
 'ropensci/geonames',
 'ropensci/nneo'
-), remotes::install_github, lib = rlib)
+), lib = rlib)
+
+#temporary debug
+print(gh::gh_token())
 
 # install all packages (depends, imports, suggests)
 wanted <- c(
