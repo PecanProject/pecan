09a3c7d229f827fc7b5231979262b24570853f33
Chris Black <chris@ckblack.org>
2022-09-20 21:51:57 -0700

    added stub function convert.input in utils

---
+++ base/utils/R/Defunct.R
@@ -0,0 +1,8 @@
+##' Stub Function for convert.input which is moved in PEcAn.DB package
+##' 
+##' @param ... any argument
+##' @export
+
+convert.input <- function(...){
+  .Defunct("PEcAn.DB::convert_input", package = NULL , "This function is moved in PEcAn.DB with a new name convert_input")
+} # convert.input()
