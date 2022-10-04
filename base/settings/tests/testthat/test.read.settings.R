209610d202cc078137619567d5edb62edfe06857
Eric R. Scott <scottericr@gmail.com>
2022-10-04 16:15:28 -0400

    attempt to fix test

--- base/settings/tests/testthat/test.read.settings.R
+++ base/settings/tests/testthat/test.read.settings.R
@@ -24,7 +24,6 @@ test_that("read.settings() strips comments", {
 
 test_that("read.settings() warns if named input file doesn't exist (but pecan.xml does)", {
   old_setting <- PEcAn.logger::logger.setLevel("DEBUG")
-  
   #hacky way to check for errors b/c PEcAn.logger errors are non-standard and
   #not captured by testthat::expect_message() or expect_error()
   x <- capture.output(
@@ -34,6 +33,12 @@ test_that("read.settings() warns if named input file doesn't exist (but pecan.xm
   expect_true(any(grepl("WARN", x)))
   expect_true(any(grepl("blahblahblah.xml not found", x)))
   PEcAn.logger::logger.setLevel(old_setting)
+})
+
+test_that("read settings returns error if no settings file found (#1124)", {
+  withr::with_tempdir({
+    expect_error(read.settings("nofile.xml"), "Could not find a pecan.xml file")
+  })
 })
 
 # ANS: Many of these functions seem like they shouldn't require a
@@ -50,10 +55,6 @@ test_that("read.settings returned correctly", {
   skip("Tests failing due to multisite?")
   expect_true(file.exists(s$outdir))
   expect_true(file.info(s$outdir)$isdir)
-})
-
-test_that("read settings returns error if no settings file found (#1124)", {
-  expect_error(read.settings("nofile.xml"), "Could not find a pecan.xml file")
 })
 
 
