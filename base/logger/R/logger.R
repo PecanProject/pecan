448123358240ea071ead96167ac6e52a33ff0cc3
Eric R. Scott <scottericr@gmail.com>
2022-10-04 11:03:08 -0400

    document @return value

--- base/logger/R/logger.R
+++ base/logger/R/logger.R
@@ -180,6 +180,8 @@ logger.message <- function(level, msg, ..., wrap = TRUE) {
 ##'
 ##' @param level the level of the message (ALL, DEBUG, INFO, WARN, ERROR, OFF)
 ##' @export
+##' @return When logger level is set, the previous level is returned invisibly.
+##'   This can be passed to `logger.setLevel()` to restore the previous level.
 ##' @author Rob Kooper
 ##' @examples
 ##' \dontrun{
