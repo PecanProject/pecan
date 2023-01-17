be86d5336fcd3545532e86eadfdcbb07bd028048
Dongchen Zhang <zhangdc@scc-wn3.scc.bu.edu>
2023-01-17 17:06:32 -0500

    remove example for GitHub debug

--- modules/assim.sequential/R/SDA_OBS_Assembler.R
+++ modules/assim.sequential/R/SDA_OBS_Assembler.R
@@ -11,10 +11,6 @@
 #' @author Dongchen Zhang
 #'
 #' @examples
-#' settings_dir <- "/projectnb/dietzelab/dongchen/All_NEON_SDA/NEON42/SDA/pecan.xml"
-#' OutDir <- "/projectnb/dietzelab/dongchen/All_NEON_SDA/test_OBS"
-#' Var <- c("SMP", "LAI", "AGB")
-#' OBS <- PEcAnAssimSequential::SDA_OBS_Assembler(settings_dir, Var, OutDir)
 
 SDA_OBS_Assembler <- function(settings_dir, Var, OutDir, Obs_Prep = NULL, skip_buffer = TRUE){
   #read settings
