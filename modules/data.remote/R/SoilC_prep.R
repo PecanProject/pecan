5e8d639c182b54bfd85ee811bdf320f42e28dcf0
Dongchen Zhang <zhangdc@scc-wn3.scc.bu.edu>
2023-01-17 15:50:12 -0500

    Soil prep function.

---
+++ modules/data.remote/R/SoilC_prep.R
@@ -0,0 +1,52 @@
+#' Prepare Soilgrids SoilC data for the SDA workflow.
+#'
+#' @param Site_Info Bety list of site info including site_id, lon, and lat.
+#' @param Start_Date Start date of SDA workflow.
+#' @param End_Date End date of SDA workflow.
+#' @param Time_Step A list containing time step and number of time step, which allows time step to be any years or days.
+#' @param OutDir Where the final CSV file will be stored.
+#'
+#' @return A data frame containing AGB median and sd for each site and each time step.
+#' @export
+#'
+#' @examples
+#' @author Dongchen Zhang
+SoilC_prep <- function(Site_Info, Start_Date, End_Date, Time_Step = list(unit="year", num=1), 
+                           OutDir = NULL){
+  #if we export CSV but didn't provide any path
+  if(is.null(OutDir)){
+    PEcAn.logger::logger.info("Please provide the input dir!")
+    return(0)
+  }
+  
+  #calculate time points given start, end date, and time step.
+  if(Time_Step$unit == "year"){
+    years <- seq(0, (lubridate::year(End_Date) - lubridate::year(Start_Date)), as.numeric(Time_Step$num))#how many years between start and end date
+    time_points <- as.Date(Start_Date) %m+% years(years)
+  }else if(Time_Step$unit == "day"){
+    days <- seq(0, (lubridate::yday(End_Date) - lubridate::yday(Start_Date)), as.numeric(Time_Step$num))#how many days between start and end date
+    time_points <- as.Date(Start_Date) %m+% days(days)
+  }
+  
+  #if we have previous extracted soilgrids csv file.
+  if(file.exists(file.path(OutDir, "soilgrids_soilC_data.csv"))){
+    Previous_CSV <- as.data.frame(read.csv(file.path(OutDir, "soilgrids_soilC_data.csv")))
+    SoilC_Output <- matrix(NA, length(Site_Info$site_id), 2*length(time_points)+1) %>% 
+      `colnames<-`(c("site_id", paste0(time_points, "_SoilC"), paste0(time_points, "_SD"))) %>% as.data.frame()#we need: site_id, agb, sd, target time point.
+    SoilC_Output$site_id <- Site_Info$site_id
+    
+    #loop over time and site
+    for (i in 1:length(time_points)) {
+      t <- time_points[i]
+      for (id in Site_Info$site_id) {
+        site_SoilC <- Previous_CSV[which(Previous_CSV$Site_ID == id),]
+        SoilC_Output[which(SoilC_Output$site_id==id), paste0(t, "_SoilC")] <- site_SoilC$Total_soilC_0.200cm
+        SoilC_Output[which(SoilC_Output$site_id==id), paste0(t, "_SD")] <- site_SoilC$Std_soilC_0.200cm
+      }
+    }
+  }else{
+    PEcAn.logger::logger.info("The soilgrids prep function needs the SoilC.CSV file to be prepared before hand!")
+    return(0)
+  }
+  list(SoilC_Output = SoilC_Output, time_points = time_points, var = "SoilC")
+}
