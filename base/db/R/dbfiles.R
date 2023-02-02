e25540bf43840e3521ff2a5088c0e58f81ce5bf2
Alexis Helgeson <ahelgeso@bu.edu>
2023-02-01 19:51:23 -0500

    "issue with machine.check when files exist"

--- base/db/R/dbfiles.R
+++ base/db/R/dbfiles.R
@@ -324,7 +324,7 @@ dbfile.input.check <- function(siteid, startdate = NULL, enddate = NULL, mimetyp
           container.id = inputs$id,
           con = con,
           hostname = hostname,
-          machine.check = TRUE,
+          machine.check = TRUE, 
           return.all = return.all
         )
 
@@ -350,7 +350,7 @@ dbfile.input.check <- function(siteid, startdate = NULL, enddate = NULL, mimetyp
         container.id = inputs$id,
         con = con,
         hostname = hostname,
-        machine.check = TRUE,
+        machine.check = FALSE, #issues with hostid not matching and resulting in error message dispite files existing on host machine
         return.all = return.all
       )
 
