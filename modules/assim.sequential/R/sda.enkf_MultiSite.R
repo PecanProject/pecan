404dba01be5c0b04877a9226a0ec138a42341fc0
Alexis Helgeson <ahelgeso@bu.edu>
2023-02-03 09:28:33 -0500

    "remove debugging hacks"

--- modules/assim.sequential/R/sda.enkf_MultiSite.R
+++ modules/assim.sequential/R/sda.enkf_MultiSite.R
@@ -160,13 +160,13 @@ sda.enkf.multisite <- function(settings,
 
   #--get model specific functions
   #switch to depreciated version for debugging purposes
-  do.call("library", list(paste0("PEcAn.", model)))
-  my.write_restart <- paste0("write_restart.", model)
-  my.read_restart <- paste0("read_restart.", model)
-  my.split_inputs  <- paste0("split_inputs.", model)
-  # my.write_restart <- paste0("PEcAn.", model, "::write_restart.", model)
-  # my.read_restart <- paste0("PEcAn.", model, "::read_restart.", model)
-  # my.split_inputs  <- paste0("PEcAn.", model, "::split_inputs.", model)
+  # do.call("library", list(paste0("PEcAn.", model)))
+  # my.write_restart <- paste0("write_restart.", model)
+  # my.read_restart <- paste0("read_restart.", model)
+  # my.split_inputs  <- paste0("split_inputs.", model)
+  my.write_restart <- paste0("PEcAn.", model, "::write_restart.", model)
+  my.read_restart <- paste0("PEcAn.", model, "::read_restart.", model)
+  my.split_inputs  <- paste0("PEcAn.", model, "::split_inputs.", model)
   #- Double checking some of the inputs
   if (is.null(adjustment)) adjustment <- TRUE
   # models that don't need split_inputs, check register file for that
@@ -369,7 +369,7 @@ sda.enkf.multisite <- function(settings,
         if (control$debug) browser()
         #if restart then use restart.list, include site for debugging purposes
         if(restart_flag){
-          restart.arg = restart.list$`646` #debugging hack site specific
+          restart.arg = restart.list #$`646`debugging hack site specific
         }else{
           restart.arg = NULL
         }
