84e7e0e993fb328fa5d251ac2e7c54ed3e121f1c
Dongchen Zhang <zhangdc@scc-wh2.scc.bu.edu>
2023-02-16 16:23:17 -0500

    github debug

--- modules/assim.sequential/R/sda_weights_site.R
+++ modules/assim.sequential/R/sda_weights_site.R
@@ -16,32 +16,32 @@ sda_weights_site <- function(FORECAST, ANALYSIS, t, ens){
   site.ids <- attr(FORECAST[[1]],'Site') %>% unique() 
   
   #calculate weights for each ensemble member of each site at time point t.
-  Weights.new <- pmap(list(ANALYSIS[t],
-                           FORECAST[t],
-                           names(FORECAST)[t]),
-                      function(ANALYSIS.r, FORECAST.r, Year.applid.weight) {
-                        #loop over each site
-                        site.ids %>%
-                          future_map_dfr(function(one.site){
-                            #match site id
-                            site.ind <- which( attr(FORECAST[[1]],'Site') %in% one.site)
-                            #match date
-                            ind <- which( names(FORECAST) %in% Year.applid.weight)
-                            #calculate analysis mean value
-                            mu.a <- apply(ANALYSIS.r[,site.ind],2 ,mean)
-                            #calculate analysis covariance matrix
-                            Pa <- cov(ANALYSIS.r[,site.ind])
-                            #calculate weights
-                            w <- emdbook::dmvnorm(FORECAST.r[,site.ind], mu.a, Pa, TRUE)
-                            #return outputs
-                            data.frame(
-                              ens = 1:ens,
-                              raw_weight=w,
-                              Site= one.site,
-                              Relative_weight=abs(w)/sum(abs(w)),
-                              Year=lubridate::year(Year.applid.weight)
-                            )
-                          }, .progress = TRUE)
-                      })
+  Weights.new <- purrr::pmap(list(ANALYSIS[t],
+                                  FORECAST[t],
+                                  names(FORECAST)[t]),
+                             function(ANALYSIS.r, FORECAST.r, Year.applid.weight) {
+                               #loop over each site
+                               site.ids %>%
+                                 future_map_dfr(function(one.site){
+                                   #match site id
+                                   site.ind <- which( attr(FORECAST[[1]],'Site') %in% one.site)
+                                   #match date
+                                   ind <- which( names(FORECAST) %in% Year.applid.weight)
+                                   #calculate analysis mean value
+                                   mu.a <- apply(ANALYSIS.r[,site.ind],2 ,mean)
+                                   #calculate analysis covariance matrix
+                                   Pa <- stats::cov(ANALYSIS.r[,site.ind])
+                                   #calculate weights
+                                   w <- emdbook::dmvnorm(FORECAST.r[,site.ind], mu.a, Pa, TRUE)
+                                   #return outputs
+                                   data.frame(
+                                     ens = 1:ens,
+                                     raw_weight=w,
+                                     Site= one.site,
+                                     Relative_weight=abs(w)/sum(abs(w)),
+                                     Year=lubridate::year(Year.applid.weight)
+                                   )
+                                 }, .progress = TRUE)
+                             })
   Weights.new
 }
