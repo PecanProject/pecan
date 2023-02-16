87d05d3cc375146b42ad6b0c5249f0d75cadbf66
meetagrawal09 <agrawalmeet91@gmail.com>
2023-02-16 20:32:00 +0530

    updated access

--- models/sipnet/R/model2netcdf.SIPNET.R
+++ models/sipnet/R/model2netcdf.SIPNET.R
@@ -287,9 +287,9 @@ model2netcdf.SIPNET <- function(outdir, sitelat, sitelon, start_date, end_date, 
       nc      <- ncdf4::nc_create(file.path(outdir, paste("current", "nc", sep = ".")), nc_var)
       ncdf4::ncatt_put(nc, "time", "bounds", "time_bounds", prec=NA)
       varfile <- file(file.path(outdir, paste(y, "nc", "var", sep = ".")), "w")
-      for (i in seq_along(nc_var)) {
-        ncdf4::ncvar_put(nc, nc_var[[i]], output[[i]])
-        cat(paste(nc_var[[i]]$name, nc_var[[i]]$longname), file = varfile, sep = "\n")
+      for (key in names(nc_var)) {
+        ncdf4::ncvar_put(nc, nc_var[[key]], output[[key]])
+        cat(paste(nc_var[[key]]$name, nc_var[[key]]$longname), file = varfile, sep = "\n")
       }
       close(varfile)
       ncdf4::nc_close(nc)
