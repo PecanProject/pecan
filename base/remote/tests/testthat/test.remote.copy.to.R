6e01d29de48e061917fbd51ead4db9358c4197b5
meetagrawal09 <agrawalmeet91@gmail.com>
2023-02-25 19:28:39 +0530

    remote.copy.to() : added tests

---
+++ base/remote/tests/testthat/test.remote.copy.to.R
@@ -0,0 +1,4 @@
+test_that("`remote.copy.to()` raises errors for false inputs",{
+  expect_error(remote.copy.to(NULL, "path/to/local/file", "path/to/remote/file"), "`host` object passed to the function is NULL : Try passing a valid host object")
+  expect_error(remote.copy.to(host = list(name = "pecan",user="test_user", tunnel="test_tunnel"), "path/to/local/file", "path/to/remote/file"), "Could not find tunnel")
+})
