afd830bb151cbbd421bfd0d77abe77f29a6e7017
meetagrawal09 <agrawalmeet91@gmail.com>
2023-02-25 09:34:20 +0530

    added fqdn() tests

--- base/remote/tests/testthat/test-fqdn.R
+++ base/remote/tests/testthat/test-fqdn.R
@@ -1,3 +1,14 @@
 test_that("fqdn() returns exactly one result", {
   expect_length(fqdn(), 1)
 })
+
+test_that("`fqdn()` returns expected `FQDN` value", {
+  
+  #set FQDN
+  Sys.setenv(FQDN = "pecan_host")
+  expect_equal(fqdn(), "pecan_host")
+  
+  #unset FQDN
+  Sys.unsetenv("FQDN")
+  expect_equal(fqdn(), as.character(Sys.info()["nodename"]))
+})
