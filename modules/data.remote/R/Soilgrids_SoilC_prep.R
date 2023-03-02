e678f5ef6d7ea6e7575808c007c5eedb6468031e
Dongchen Zhang <zhangdc@scc-wi1.scc.bu.edu>
2023-03-02 18:31:57 -0500

    update logic and use Cherry's soilgrids download function.

--- modules/data.remote/R/Soilgrids_SoilC_prep.R
+++ modules/data.remote/R/Soilgrids_SoilC_prep.R
@@ -5,6 +5,7 @@
 #' @param end_date End date of SDA workflow.
 #' @param time_points A vector contains each time point within the start and end date.
 #' @param outdir Where the final CSV file will be stored.
+#' @param export_csv Decide if we want to export the CSV file.
 #'
 #' @return A data frame containing AGB median and sd for each site and each time step.
 #' @export
@@ -13,15 +14,19 @@
 #' @author Dongchen Zhang
 #' @importFrom magrittr %>%
 Soilgrids_SoilC_prep <- function(site_info, start_date, end_date, time_points, 
-                           outdir = NULL){
+                           outdir = NULL, export_csv = FALSE){
   #if we export CSV but didn't provide any path
-  if(is.null(outdir)){
-    PEcAn.logger::logger.info("Please provide the input dir!")
+  if(as.logical(export_csv) && is.null(outdir)){
+    PEcAn.logger::logger.info("If you want to export CSV file, please ensure input the outdir!")
     return(0)
-  }
-  
-  #if we have previous extracted soilgrids csv file.
-  if(file.exists(file.path(outdir, "soilgrids_soilC_data.csv"))){
+  }else if(as.logical(export_csv) && !file.exists(file.path(outdir, "soilgrids_soilC_data.csv"))){
+    #if we want to export the csv file for soilgrids data.
+    Previous_CSV <- PEcAn.data.land::soilgrids_soilC_extract(site_info, outdir)
+  }else if(!as.logical(export_csv && !file.exists(file.path(outdir, "soilgrids_soilC_data.csv")))){
+    #if we don't want to export the csv file.
+    Previous_CSV <- PEcAn.data.land::soilgrids_soilC_extract(site_info)
+  }else if(file.exists(file.path(outdir, "soilgrids_soilC_data.csv"))){
+    #if we have previous extracted soilgrids csv file.
     Previous_CSV <- as.data.frame(utils::read.csv(file.path(outdir, "soilgrids_soilC_data.csv")))
     SoilC_Output <- matrix(NA, length(site_info$site_id), 2*length(time_points)+1) %>% 
       `colnames<-`(c("site_id", paste0(time_points, "_TotSoilCarb"), paste0(time_points, "_SD"))) %>% as.data.frame()#we need: site_id, agb, sd, target time point.
@@ -36,9 +41,6 @@ Soilgrids_SoilC_prep <- function(site_info, start_date, end_date, time_points,
         SoilC_Output[which(SoilC_Output$site_id==id), paste0(t, "_SD")] <- site_SoilC$Std_soilC_0.200cm
       }
     }
-  }else{
-    PEcAn.logger::logger.info("The soilgrids prep function needs the SoilC.CSV file to be prepared before hand!")
-    return(0)
   }
   PEcAn.logger::logger.info("Soilgrids SoilC Prep Completed!")
   list(SoilC_Output = SoilC_Output, time_points = time_points, var = "TotSoilCarb")
