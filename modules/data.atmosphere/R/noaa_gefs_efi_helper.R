9630c2f77a1c45c82a8faffb6dff3e0ed5027af9
Alexis Helgeson <ahelgeso@bu.edu>
2023-05-26 16:07:01 -0400

    "added arrow import"

--- modules/data.atmosphere/R/noaa_gefs_efi_helper.R
+++ modules/data.atmosphere/R/noaa_gefs_efi_helper.R
@@ -9,6 +9,8 @@
 #' @param endpoint the EFI host address (leave as default)
 #' @param verbose logical, displays or hides messages
 #' @param start_date forecast start date yyyy-mm-dd format
+#' 
+#' @import arrow
 #'
 #' @author Alexis Helgeson (taken from neon4cast package)
 noaa_stage2 <- function(cycle = 0,
