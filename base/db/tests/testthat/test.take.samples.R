2f99ed472304bd3a34d0fcb3991e45a2bb744d0a
meetagrawal09 <agrawalmeet91@gmail.com>
2023-06-07 21:04:58 +0530

    moved around tests

--- base/db/tests/testthat/test.take.samples.R
+++ base/db/tests/testthat/test.take.samples.R
@@ -7,4 +7,9 @@ test_that("`take.samples` returns a vector of length sample.size for given summa
   summary = list(mean = 10, stat = 10)
   sample.size = 10
   expect_equal(length(take.samples(summary = summary, sample.size = sample.size)), sample.size)
+
+  # Testing for exact return values for a simple example
+  test.sample <- take.samples(summary = data.frame(mean = 1, stat = 1),
+                               sample.size = 2)
+  expect_equal(test.sample, c(2.26295428488079, 0.673766639294351))
 })
