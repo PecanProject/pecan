a4e1421ef02db76865d273eaf59e20f28bbbe307
Rohan Sasne <rohansasne30@gmail.com>
2023-06-15 04:42:34 +0530

    Set Test to Every Monday and Add Template for cURL Request

--- tests/Docker-Stack-Test.sh
+++ tests/Docker-Stack-Test.sh
@@ -16,3 +16,32 @@ sleep 200
 
 curl -v -L -H "Host: pecan.localhost" \
 'http://172.17.0.1/pecan/dataset.php?workflowid=99000000001&type=file&name=out/99000000001/sipnet.out'
+
+
+# -----------------------------------------------
+#               cURL Request Template
+#-----------------------------------------------
+
+# Be sure to use the -H header and also use the bridge IP address of the Docker host (172.17.0.1) and not localhost
+
+
+# curl -v -L -X POST -H "Host: pecan.localhost" \
+#     -F 'hostname=' \
+#     -F 'modelid=' \
+#     -F 'sitegroupid=' \
+#     -F 'siteid=' \
+#     -F 'sitename=' \
+#     -F 'pft[]=' \
+#     -F 'start=' \
+#     -F 'end=' \
+#     -F 'input_met=' \
+#     -F 'email=' \
+#     -F 'notes=' \
+# 'http://172.17.0.1/pecan/04-runpecan.php'
+
+# Adjust the sleep time according to the model you are running
+
+# Append the workflowid and ID after `out/` by 1 for every consecutive run
+
+# curl -v -L -H "Host: pecan.localhost" \
+# 'http://172.17.0.1/pecan/dataset.php?workflowid=(ID)&type=file&name=out/(ID)/sipnet.out'
