f27780a9889da912b7c545e0d6542ac42ee08105
Rohan Sasne <rohansasne30@gmail.com>
2023-06-15 03:13:02 +0530

    Update File Permission

--- tests/Docker-Stack-Test.sh
+++ tests/Docker-Stack-Test.sh
@@ -3,7 +3,7 @@ curl -v -L -X POST -H "Host: pecan.localhost" \
     -F 'modelid=1000000014' \
     -F 'sitegroupid=1' \
     -F 'siteid=756' \
-    -F 'sitename=Southern Pine (Subcategory), Duke Forest Pine ' \
+    -F 'sitename=Duke Forest - loblolly pine (US-Dk3)' \
     -F 'pft[]=temperate.coniferous' \
     -F 'start=2004/01/01' \
     -F 'end=2004/12/31' \
