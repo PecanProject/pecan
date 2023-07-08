33b8f2a78e3315dd08f9e9615cfc763a22751aa0
meetagrawal09 <agrawalmeet91@gmail.com>
2023-07-08 12:59:32 +0530

    tested datetime2cf, datetime2doy, cf2doy

--- base/utils/tests/testthat/test.cf2date.R
+++ base/utils/tests/testthat/test.cf2date.R
@@ -6,3 +6,18 @@ test_that("`cf2datetime()` able to convert CF-style date-time to POSIXct date-ti
   # leap year
   expect_equal(cf2datetime(365, "days since 2000-01-01 12:00:00 -05:00"), as.POSIXct("2000-12-31 17:00:00", tz = "UTC"))
 })
+
+test_that("`datetime2cf()` able to convert POSIXct date-time to CF-style date-time", {
+  expect_equal(datetime2cf("1990-10-05", "days since 1990-01-01", tz = "UTC"), 277)
+  expect_equal(datetime2cf("1963-01-03 17:27:00", "minutes since 1963-01-03 12:00:00 -05:00", tz = "UTC"), 27)
+})
+
+test_that("`datetime2doy()` and `cf2doy()` able to extract Julian day from POSIXct or CF date-times respectively(cf2doy internally converts CF to POSIXct and calls datetime2doy)", {
+  
+  # POSIXct date-times
+  expect_equal(datetime2doy("2010-01-01"), 1)
+  expect_equal(datetime2doy("2010-01-01 12:00:00"), 1.5)
+
+  # CF date-times
+  expect_equal(cf2doy(0, "days since 2007-01-01"), 1)
+})
