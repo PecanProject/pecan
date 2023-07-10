5512ae7f4061a792c277eaa5167eaeac667339c4
Mike Dietze <dietze@bu.edu>
2023-07-10 15:27:16 -0400

    fix run.write.configs to allow reuse of previous ensemble samples. Added so that parameters are not resampled site-by-site in a multi-site run.

--- base/workflow/R/runModule.run.write.configs.R
+++ base/workflow/R/runModule.run.write.configs.R
@@ -2,16 +2,17 @@
 #'
 #' @param settings a PEcAn Settings or MultiSettings object
 #' @param overwrite logical: Replace config files if they already exist?
+#' @param use.existing.samples FALSE (default), TRUE, or filename
 #' @return A modified settings object, invisibly
 #' @export
-runModule.run.write.configs <- function(settings, overwrite = TRUE) {
+runModule.run.write.configs <- function(settings, overwrite = TRUE, use.existing.samples = TRUE) {
 
   if (PEcAn.settings::is.MultiSettings(settings)) {
     if (overwrite && file.exists(file.path(settings$rundir, "runs.txt"))) {
       PEcAn.logger::logger.warn("Existing runs.txt file will be removed.")
       unlink(file.path(settings$rundir, "runs.txt"))
     }
-    return(PEcAn.settings::papply(settings, runModule.run.write.configs, overwrite = FALSE))
+    return(PEcAn.settings::papply(settings, runModule.run.write.configs, overwrite = overwrite,use.existing.samples=use.existing.samples))
   } else if (PEcAn.settings::is.Settings(settings)) {
     write <- settings$database$bety$write
     # double check making sure we have method for parameter sampling
@@ -27,7 +28,12 @@ runModule.run.write.configs <- function(settings, overwrite = TRUE) {
       }) %>%
       unlist()
     
-    return(PEcAn.workflow::run.write.configs(settings, write, ens.sample.method, posterior.files = posterior.files, overwrite = overwrite))
+    return(PEcAn.workflow::run.write.configs(settings, 
+                                             write, 
+                                             ens.sample.method, 
+                                             posterior.files = posterior.files, 
+                                             overwrite = overwrite, 
+                                             use.existing.samples = use.existing.samples))
   } else {
     stop("runModule.run.write.configs only works with Settings or MultiSettings")
   }
