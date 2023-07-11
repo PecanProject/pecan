7cb18ca76fc1c647dc1586574aa6568a28d9c3df
meetagrawal09 <agrawalmeet91@gmail.com>
2023-07-11 14:58:06 +0530

    rolled back test

--- base/db/tests/testthat/test.utils_db.R
+++ base/db/tests/testthat/test.utils_db.R
@@ -1,15 +1,15 @@
-test_that("`db.print.connections()` able to log out details about connections", {
-  PEcAn.logger::logger.setUseConsole(TRUE, FALSE)
-  on.exit(PEcAn.logger::logger.setUseConsole(TRUE, TRUE), add = TRUE)
-  expect_output(
-    db.print.connections(), 
-    paste0(
-      ".* Created 0 connections and executed 0 queries .* ",
-      "Created 0 connections and executed 0 queries.*",
-      "No open database connections."
-    )
-  )
-})
+# test_that("`db.print.connections()` able to log out details about connections", {
+#   PEcAn.logger::logger.setUseConsole(TRUE, FALSE)
+#   on.exit(PEcAn.logger::logger.setUseConsole(TRUE, TRUE), add = TRUE)
+#   expect_output(
+#     db.print.connections(), 
+#     paste0(
+#       ".* Created 0 connections and executed 0 queries .* ",
+#       "Created 0 connections and executed 0 queries.*",
+#       "No open database connections."
+#     )
+#   )
+# })
 
 test_that("`db.showQueries()` and `db.getShowQueries()` able to set and get the value of the .db.utils$showquery variable respectively", {
   showquery_old <- db.getShowQueries()
