7acb7aeaddf545f5ae9fd02f2e1509e60a43289a
meetagrawal09 <agrawalmeet91@gmail.com>
2023-07-13 15:46:06 +0530

    updated tests

--- base/qaqc/tests/testthat/test.cull_database_entries.R
+++ base/qaqc/tests/testthat/test.cull_database_entries.R
@@ -14,11 +14,12 @@ test_that("`cull_database_entries()` gives errors for faulty inputs",{
 })
 
 test_that("`cull_database_entries()` able to correctly add logs to the output file", {
-  mockery::stub(cull_database_entries, 'PEcAn.DB::db.query', 'test_log')
-  dir <- tempdir()
-  on.exit(unlink(dir, recursive = TRUE))
-  cull_database_entries(table = data.frame(id = 1), table_name = 'test', con = 1, outdir = dir)
-  expect_true(file.exists(paste0(dir, "/deletion_log_of_test")))
-  file_data <- readLines(paste0(dir, "/deletion_log_of_test"))
-  expect_equal(grepl("test_log", file_data), c(TRUE, TRUE))
+  withr::with_dir(tempdir(), {
+    mockery::stub(cull_database_entries, 'PEcAn.DB::db.query', 'test_log')
+    dir <- getwd()
+    cull_database_entries(table = data.frame(id = 1), table_name = 'test', con = 1, outdir = dir)
+    expect_true(file.exists(paste0(dir, "/deletion_log_of_test")))
+    file_data <- readLines(paste0(dir, "/deletion_log_of_test"))
+    expect_equal(grepl("test_log", file_data), c(TRUE, TRUE))
+  })
 })
