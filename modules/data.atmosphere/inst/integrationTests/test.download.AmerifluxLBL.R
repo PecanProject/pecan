acb23b4e9f73db64ad97467d15fe2b903c9865b1
meetagrawal09 <agrawalmeet91@gmail.com>
2023-08-23 16:21:06 +0530

    moved test to function, using withr for tempfile

--- modules/data.atmosphere/inst/integrationTests/test.download.AmerifluxLBL.R
+++ modules/data.atmosphere/inst/integrationTests/test.download.AmerifluxLBL.R
@@ -1,31 +1,43 @@
-# putting logger to debug mode
-PEcAn.logger::logger.setUseConsole(TRUE, FALSE)
-on.exit(PEcAn.logger::logger.setUseConsole(TRUE, TRUE), add = TRUE)
-PEcAn.logger::logger.setLevel("DEBUG")
+library(testthat)
 
+test_download_AmerifluxLBL <- function(start_date, end_date, sitename, lat.in, lon.in) {
+  # putting logger to debug mode
+  PEcAn.logger::logger.setUseConsole(TRUE, FALSE)
+  on.exit(PEcAn.logger::logger.setUseConsole(TRUE, TRUE), add = TRUE)
+  PEcAn.logger::logger.setLevel("DEBUG")
 
-# mocking functions
-mockery::stub(PEcAn.DB::convert_input, 'dbfile.input.check', data.frame())
-mockery::stub(PEcAn.DB::convert_input, 'db.query', data.frame(id = 1))
+  # mocking functions
+  mockery::stub(PEcAn.DB::convert_input, 'dbfile.input.check', data.frame())
+  mockery::stub(PEcAn.DB::convert_input, 'db.query', data.frame(id = 1))
 
-# calling download function
-PEcAn.DB::convert_input(
-  input.id = NA,
-  outfolder = "testAmerifluxData",
-  formatname = NULL,
-  mimetype = NULL,
-  site.id = 1,
+  withr::with_dir(tempdir(), {
+    tmpdir <- getwd()
+    # calling download function
+    PEcAn.DB::convert_input(
+      input.id = NA,
+      outfolder = tmpdir,
+      formatname = NULL,
+      mimetype = NULL,
+      site.id = 1,
+      start_date = start_date,
+      end_date = end_date,
+      pkg = 'PEcAn.data.atmosphere',
+      fcn = 'download.AmerifluxLBL',
+      con = NULL,
+      host = data.frame(name = "localhost"),
+      browndog = NULL,
+      write = FALSE,
+      lat.in = lat.in,
+      lon.in = lon.in,
+      sitename = sitename
+    )
+  })
+}
+
+test_download_AmerifluxLBL(
   start_date = "2011-01-01",
   end_date = "2011-12-31",
-  pkg = 'PEcAn.data.atmosphere',
-  fcn = 'download.AmerifluxLBL',
-  con = NULL,
-  host = data.frame(name = "localhost"),
-  browndog = NULL,
-  write = FALSE,
+  sitename = 'US-Akn',
   lat.in = 40,
-  lon.in = -88,
-  sitename = 'US-Akn'
+  lon.in = -88
 )
-
-# checking the downloaded files
