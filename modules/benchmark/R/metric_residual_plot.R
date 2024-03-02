742fcc36a5a06d05b4ef559b70b1b3c6b1ec22c8
Chris Black <chris@ckblack.org>
2024-03-01 21:58:09 -0800

    Update modules/benchmark/R/metric_residual_plot.R

--- modules/benchmark/R/metric_residual_plot.R
+++ modules/benchmark/R/metric_residual_plot.R
@@ -14,7 +14,7 @@ metric_residual_plot <- function(metric_dat, var, filename = NA, draw.plot = is.
   metric_dat$diff <- abs(metric_dat$model - metric_dat$obvs)
   metric_dat$zeros <- rep(0, length(metric_dat$time))
   
-    p <- ggplot2::ggplot(data = metric_dat, ggplot2::aes(x = .data$time)) 
+  p <- ggplot2::ggplot(data = metric_dat, ggplot2::aes(x = .data$time)) 
   p <- p + ggplot2::geom_path(ggplot2::aes(y = .data$zeros), colour = "#666666", size = 2, linetype = 2, lineend = "round") 
   p <- p + ggplot2::geom_point(ggplot2::aes(y = .data$diff), size = 4, colour = "#619CFF") 
   p <- p + ggplot2::labs(title = var, x = "years", y = "abs(model - observation)")
