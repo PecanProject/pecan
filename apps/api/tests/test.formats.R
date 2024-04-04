908de176e31cb20c3bdc9fa89f3da4df29743d81
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2024-04-05 03:46:35 +0530

    Update API endpoint URLs

--- apps/api/tests/test.formats.R
+++ apps/api/tests/test.formats.R
@@ -2,7 +2,7 @@ context("Testing all formats related endpoints")
 
 test_that("Calling /api/formats/ with valid parameters returns Status 200", {
   res <- httr::GET(
-    "http://localhost:8000/api/formats/?format_name=ameriflux&mimetype=csv&ignore_case=TRUE",
+    "http://pecan.localhost/api/formats/?format_name=ameriflux&mimetype=csv&ignore_case=TRUE",
     httr::authenticate("carya", "illinois")
   )
   expect_equal(res$status, 200)
@@ -10,7 +10,7 @@ test_that("Calling /api/formats/ with valid parameters returns Status 200", {
 
 test_that("Calling /api/formats/ with invalid parameters returns Status 404", {
   res <- httr::GET(
-    "http://localhost:8000/api/formats/?format_name=random&mimetype=random&ignore_case=TRUE",
+    "http://pecan.localhost/api/formats/?format_name=random&mimetype=random&ignore_case=TRUE",
     httr::authenticate("carya", "illinois")
   )
   expect_equal(res$status, 404)
@@ -18,7 +18,7 @@ test_that("Calling /api/formats/ with invalid parameters returns Status 404", {
 
 test_that("Calling /api/formats/{format_id} returns Status 200", {
   res <- httr::GET(
-    "http://localhost:8000/api/formats/19",
+    "http://pecan.localhost/api/formats/19",
     httr::authenticate("carya", "illinois")
   )
   expect_equal(res$status, 200)
@@ -26,7 +26,7 @@ test_that("Calling /api/formats/{format_id} returns Status 200", {
 
 test_that("Calling /api/formats/{format_id} with invalid parameters returns Status 404", {
   res <- httr::GET(
-    "http://localhost:8000/api/formats/0",
+    "http://pecan.localhost/api/formats/0",
     httr::authenticate("carya", "illinois")
   )
   expect_equal(res$status, 404)
