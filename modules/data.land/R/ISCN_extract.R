5645ceb5b79a5d309c5723ea9d923972e71c138d
Dongchen Zhang <zhangdc@scc-gp2.scc.bu.edu>
2024-04-26 20:52:11 -0400

    Update ISCN extract function based on new Rdata file.

--- modules/data.land/R/ISCN_extract.R
+++ modules/data.land/R/ISCN_extract.R
@@ -13,22 +13,17 @@
 IC_ISCN_SOC <- function(site_info, ens = 100, ecoregion.path = NULL) {
   iscn_soc <- PEcAn.data.land::iscn_soc
   site_eco <- PEcAn.data.land::EPA_ecoregion_finder(site_info$lat, site_info$lon, ecoregion.path)
-  soc <- iscn_soc %>%
-    dplyr::filter(
-      .data$NA_L2CODE %in% site_eco$L2
-    ) %>%
-    dplyr::select("soc (g cm-2)", "NA_L2CODE") %>%
-    na.omit()
+  soc <- iscn_soc[,which(colnames(iscn_soc) %in% site_eco$L2)]
   ic_sample_soc <- data.frame(matrix(NA, ens, length(site_info$site_id))) %>%
     `colnames<-`(site_info$site_id)
   for (i in seq_along(site_eco$L2)) {
     if (is.na(site_eco$L2[i])) {
       next
     }
-    if (length(soc$`soc (g cm-2)`[which(soc$NA_L2CODE == site_eco$L2[i])]) == 0) {
+    if (!site_eco$L2[i] %in% colnames(soc)) {
       next
     }
-    ic_sample_soc[,i] <- PEcAn.utils::ud_convert(sample(soc$`soc (g cm-2)`[which(soc$NA_L2CODE == site_eco$L2[i])], ens, replace = T), "g cm-2", "kg m-2")
+    ic_sample_soc[,i] <- PEcAn.utils::ud_convert(sample(soc[,site_eco$L2[i]], ens, replace = T), "g cm-2", "kg m-2")
   }
   return(ic_sample_soc)
 }
