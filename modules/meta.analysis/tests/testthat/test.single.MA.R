2da04b29f339495f7f401d0d14b928a68395e959
meetagrawal09 <agrawalmeet91@gmail.com>
2024-05-27 18:35:33 +0530

    added test dummies

---
+++ modules/meta.analysis/tests/testthat/test.single.MA.R
@@ -0,0 +1,5 @@
+test_that("`single.MA` gives expected result for example inputs", {
+  ## need to calculate x
+  ## x <- singleMA(....)
+  #expect_equal(round(summary(x)$statistics["beta.o", "Mean"]), 5)
+})
