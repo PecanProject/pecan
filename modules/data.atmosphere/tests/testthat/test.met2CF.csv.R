2298a9bc77a47b8a50d216ded193e82d21d6e43e
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2024-05-30 16:52:30 +0530

    Refactor csv import format in test.met2CF.csv.R for debugging

--- modules/data.atmosphere/tests/testthat/test.met2CF.csv.R
+++ modules/data.atmosphere/tests/testthat/test.met2CF.csv.R
@@ -1,13 +1,13 @@
 context("testing csv import using met2CF.csv")
-
+source("../../R/met2CF.csv.R")
 format <- list(
        header = 1,
-       time_zone = "America/New_York",
-       time.row = list(1),
+       time_zone = "GMT",
+       time.row = 1,
        orig = c(
-              "Corrected.Date.Time", "Solar.Radiation..W.m2.", "Temp..C.",
-              "PAR_.umol_m.2_s.1.", "PAR_.mol_m.2_h.1.", "RH....", "Wind.Speed..m.s.",
-              "VPD", "Rain..mm."
+              "Corrected Date/Time", "Solar Radiation (W/m2)", "Temp (C)",
+              "PAR_(umol_m-2_s-1)", "PAR_(mol_m-2_h-1)", "RH (%)", "Wind Speed (m/s)",
+              "VPD", "Rain (mm)"
        ),
        units = c(
               "mdy_hm", "W m-2", "celsius",
@@ -19,7 +19,21 @@ format <- list(
        ),
        skip = 0,
        unit.row = TRUE,
-       na.strings = NA
+       na.strings = NA,
+       vars = list(
+              input_name = c(
+                     "Corrected Date/Time", "Solar Radiation (W/m2)", "Temp (C)",
+                     "PAR_(umol_m-2_s-1)", "PAR_(mol_m-2_h-1)", "RH (%)", "Wind Speed (m/s)",
+                     "VPD", "Rain (mm)"
+              ),
+              bety_name = c(
+                     "datetime", "solar_radiation", "airT",
+                     "PAR", NA, "relative_humidity", "Wspd", NA, "precipitation_rate"
+              )
+       ),
+       storage_type = c(
+              "%m/%d/%y %H:%M", NA, NA, NA, NA, NA, NA, NA, NA
+       )
 )
 # met2CF.csv(in.path = "data", in.file = "met2CF.csv.csv", outfolder = tempdir(),
 #            format = format,
@@ -34,11 +48,11 @@ lat <- 42 + 47 / 60 + 30 / 6000
 lon <- 76 + 7 / 60 + 20 / 6000
 
 
-start_date <- lubridate::mdy_hm("03/26/2013 18:00")
-end_date <- lubridate::mdy_hm("03/27/2013 17:00")
+start_date <- lubridate::mdy_hm("03/01/13 18:00")
+end_date <- lubridate::mdy_hm("03/27/13 17:00")
 
 test_that("met2CF.csv function works correctly", {
-       output <- PEcAn.data.atmosphere::met2CF.csv(
+       output <- met2CF.csv(
               in.path = in.path, in.prefix = in.file, outfolder = outfolder, start_date = start_date,
               end_date = end_date, format = format, lat = lat, lon = lon,
        )
