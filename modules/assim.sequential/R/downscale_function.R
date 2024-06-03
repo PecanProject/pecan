71040be72a8fec0a5bf2f1701cb2f5a7bf1c9761
Michael Dietze <dietze@bu.edu>
2024-06-03 16:18:08 -0400

    Update modules/assim.sequential/R/downscale_function.R

--- modules/assim.sequential/R/downscale_function.R
+++ modules/assim.sequential/R/downscale_function.R
@@ -28,7 +28,6 @@ NA_downscale <- function(data, coords, date, C_pool, covariates){
   
   # Extract predictors from covariates raster using site coordinates
   predictors <- as.data.frame(terra::extract(covariates, site_coordinates,ID = FALSE)) 
-  predictors <- dplyr::select(predictors, -ID)
   
   # Combine each ensemble member with all predictors
   ensembles <- list()
