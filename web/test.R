7b9d4d95b5549b1e88f4ee1d12bfaeeaf3161844
meetagrawal09 <agrawalmeet91@gmail.com>
2024-06-16 12:18:20 +0530

    bug fix

--- web/test.R
+++ web/test.R
@@ -1,3 +1,5 @@
+library("PEcAn.all")
+
 args <- PEcAn.settings::get_args()
 settings <- PEcAn.settings::read.settings(args$settings)
 settings <- PEcAn.settings::prepare.settings(settings, force = FALSE)
