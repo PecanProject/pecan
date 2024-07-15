392ecaff615046f9f8949d6decf77ced47e27ffb
Harunobu Ishii <harunobuishii547@gmail.com>
2024-07-15 16:12:42 -0400

    docs generated

--- modules/assim.sequential/R/aggregate.R
+++ modules/assim.sequential/R/aggregate.R
@@ -1,41 +1,39 @@
-##' @title Aggregation Function
-##' @name aggregate
-##' @author Harunobu Ishii
-##'
-##' @param downscale_output  Raster file output from downscale_function.R. Read file in this way if stored locally: \code{downscale_output <- readRDS("xxx.rds")}
-##' @param polygon_data A spatial polygon object (e.g., an `sf` object) that defines the spatial units for aggregation. 
-##'                     This data should be in a coordinate reference system compatible with the raster data (e.g., "EPSG:4326").
-##'                     Example of state-level aggregation: 
-##'                     \code{
-##'                     us_states <- readRDS("polygon/us_states.rds")
-##'                     state <- "MA"
-##'                     polygon_data <- st_transform(us_states[us_states$STUSPS == state, ], crs = "EPSG:4326")
-##'                     }
-##' @details This function will aggregate previously downscaled carbon flux amount to a spatial unit of choice 
-##'
-##' @return It returns the `polygon_data` with added columns for mean and sum values of the aggregated raster data for each ensemble member.
+#' @title Aggregation Function
+#' @name aggregate
+#' @author Harunobu Ishii
+#'
+#' @param downscale_output  Raster file output from downscale_function.R. Read file in this way if stored locally: \code{downscale_output <- readRDS("xxx.rds")}
+#' @param polygon_data A spatial polygon object (e.g., an `sf` object) that defines the spatial units for aggregation. 
+#'                     This data should be in a coordinate reference system compatible with the raster data (e.g., "EPSG:4326").
+#'                     Example of state-level aggregation: 
+#'                     \code{
+#'                     us_states <- readRDS("polygon/us_states.rds")
+#'                     state <- "MA"
+#'                     polygon_data <- st_transform(us_states[us_states$STUSPS == state, ], crs = "EPSG:4326")
+#'                     }
+#' @param func A character string specifying the aggregation function to use (e.g., 'mean', 'sum').
+#' @details This function will aggregate previously downscaled carbon flux amount to a spatial unit of choice 
+#'
+#' @return It returns the `polygon_data` with added columns for mean and sum values of the aggregated raster data for each ensemble member.
+#' @import sf
+#' @import exactextractr
+#' @import raster
+#' @export
 
+aggregate <- function(downscale_output, polygon_data, func = 'mean'){
+  grand_TTL <- 0
 
-library(exactextractr)
-library(terra)
-library(sf)
-
-aggregate <- function(downscale_output, polygon_data){
-  grand_total <- 0
-  overall_ave <- 0
-  
   # Perform spatial operations on each raster
   for (name in names(downscale_output$maps)) {
     raster_data <- downscale_output$maps[[name]]
-    mean_values <- exact_extract(raster_data, polygon_data, fun = 'mean')
-    sum_values <- exact_extract(raster_data, polygon_data, fun = 'sum')
-    polygon_data[[paste0(name, "_mean")]] <- mean_values
-    polygon_data[[paste0(name, "_sum")]] <- sum_values
-    grand_total = grand_total + sum_values
-    overall_ave = grand_total + mean_values
+    agg_values <- exactextractr::exact_extract(raster_data, polygon_data, fun = func)
+    
+    polygon_data[[paste0(name, "_", func)]] <- agg_values
+    grand_TTL = grand_TTL + agg_values
   }
-  overall_ave = overall_ave/length(downscale_output$maps)
-  polygon_data[["grand_total"]] <- grand_total
-  polygon_data[["overall_mean"]] <- overall_ave
+  if(func == 'mean'){
+    grand_TTL = grand_TTL/length(downscale_output$maps)
+  }
+  polygon_data[[paste0("TTL_", func)]] <- grand_TTL
   return (polygon_data)
 }
