1e6a4845e1e10edeec54c59a5f91033684d293d1
Sambhav Dixit <94298612+sambhavnoobcoder@users.noreply.github.com>
2024-07-24 20:32:57 +0530

    Update modules/assim.sequential/R/downscale_function.R
    Co-authored-by: Michael Dietze <dietze@bu.edu>

--- modules/assim.sequential/R/downscale_function.R
+++ modules/assim.sequential/R/downscale_function.R
@@ -79,7 +79,6 @@ SDA_downscale_preprocess <- function(data_path, coords_path, date, carbon_pool) 
 ##' @return A list containing the training and testing data sets, models, predicted maps for each ensemble member, and predictions for testing data.
 
 SDA_downscale <- function(preprocessed, date, carbon_pool, covariates, model_type = "rf", seed = NULL) {
-  input_data <- preprocessed$input_data
   carbon_data <- preprocessed$carbon_data
   
   # Convert site coordinates to SpatVector
