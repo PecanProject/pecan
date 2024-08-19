ab262cc6fd9d06f54cce940bbd205782a3795eea
Yisreal <huyucong34@gmail.com>
2024-08-20 03:37:53 +0800

    Only machine configuration was left

--- models/fates/R/write.configs.FATES.R
+++ models/fates/R/write.configs.FATES.R
@@ -22,6 +22,19 @@
 ##-------------------------------------------------------------------------------------------------# # nolint
 
 # example trait.values list
+trait.values <- list()
+trait.values$temperate.coniferous <- list()
+trait.values$env <- list()
+
+tc_traits <- as.data.frame(matrix(c(9999, 8888), nrow=1, ncol=2))
+colnames(tc_traits) <- c("leaf_turnover_rate", "veg_respiration_Q10")
+# Fates names for these params are "fates_leaf_long", "q10_mr"
+
+trait.values$temperate.coniferous <- tc_traits
+settings <- '/pecan/models/fates/R/test.fates.xml'
+run.id <- 'test_oneyear'
+
+# example trait.values list
 
 write.config.FATES <- function(defaults, trait.values, settings, run.id){
 
@@ -32,12 +45,12 @@ write.config.FATES <- function(defaults, trait.values, settings, run.id){
    # find out where things are
    local.rundir <- file.path(settings$rundir, run.id) ## this is on local machine for staging
    rundir       <- file.path(settings$host$rundir, run.id)  ## this is on remote machine for execution
-   casedir      <- file.path(rundir,site.id) ## /ctsm-api/resources/cases/case_SOD1, change2: 'case' -> site.id
+   casedir      <- file.path(rundir,run.id) ## /ctsm-api/resources/cases/case_SOD1, change2: 'case' -> site.id
    outdir       <- file.path(settings$host$outdir, run.id)
    refcase      <- settings$model$binary # question1: refcase==casedir?
    bld          <- file.path(refcase,"bld")
    binary       <- file.path(bld,"cesm.exe")
-   indir        <- file.path(rundir,"input") ## input directory
+   indir        <- file.path(rundir,"data/shared") ## input directory
    default      <- settings$run$inputs$default$path ## reference inputs file structure
    site_name    <- paste0(site.id, "_c", run.id) ## change3 
    
@@ -56,50 +69,50 @@ write.config.FATES <- function(defaults, trait.values, settings, run.id){
 
    ## SITE INFO --> DOMAIN FILE (lat/lon)
    #  - should also store this in the refcase directory for PEcAn so we can grab from there, and not the PEcAn package
-   gridres <- 0.125  ## ultimately this should be a variable
-   lat <- as.numeric(site$lat)
-   lon <- (as.numeric(site$lon) + 360) %% 360 ## make sure coords in 0-360 range, not negative
-   domain.default <- system.file("domain.lnd.360x720_gswp3.0v1.c170606.nc",package="PEcAn.FATES")
-   domain.file <- file.path(local.rundir,paste0("domain.lnd.fv0.9x1.25_gx1v7_",site_name,".nc"))
-   file.copy(domain.default,domain.file)
-   domain.nc <- ncdf4::nc_open(domain.file,write=TRUE)
-   ncdf4::ncvar_put(nc=domain.nc, varid='xc', vals=lon)
-   ncdf4::ncvar_put(nc=domain.nc, varid='yc', vals=lat)
-   ncdf4::ncvar_put(nc=domain.nc, varid='xv', vals=lon+c(-1,1,1,-1)*gridres)
-   ncdf4::ncvar_put(nc=domain.nc, varid='yv', vals=lat+c(-1,-1,1,1)*gridres)
-   ncdf4::ncvar_put(nc=domain.nc, varid='area', vals=(2*gridres*pi/180)^2)   
-   ncdf4::nc_close(domain.nc)
+   #gridres <- 0.125  ## ultimately this should be a variable
+   #lat <- as.numeric(site$lat)
+   #lon <- (as.numeric(site$lon) + 360) %% 360 ## make sure coords in 0-360 range, not negative
+   #domain.default <- system.file("domain.lnd.360x720_gswp3.0v1.c170606.nc",package="PEcAn.FATES")
+   #domain.file <- file.path(local.rundir,paste0("domain.lnd.fv0.9x1.25_gx1v7_",site_name,".nc"))
+   #file.copy(domain.default,domain.file)
+   #domain.nc <- ncdf4::nc_open(domain.file,write=TRUE)
+   #ncdf4::ncvar_put(nc=domain.nc, varid='xc', vals=lon)
+   #ncdf4::ncvar_put(nc=domain.nc, varid='yc', vals=lat)
+   #ncdf4::ncvar_put(nc=domain.nc, varid='xv', vals=lon+c(-1,1,1,-1)*gridres)
+   #ncdf4::ncvar_put(nc=domain.nc, varid='yv', vals=lat+c(-1,-1,1,1)*gridres)
+   #ncdf4::ncvar_put(nc=domain.nc, varid='area', vals=(2*gridres*pi/180)^2)   
+   #ncdf4::nc_close(domain.nc)
    
    ## SURF - should also store this in the refcase directory for PEcAn so we can grab from there, and not the PEcAn package
-   surf.default <- system.file("surfdata_0.9x1.25_hist_16pfts_Irrig_CMIP6_simyr2000_c190214.nc",package = "PEcAn.FATES")
-   surf.file    <- file.path(local.rundir,paste0("surfdata_0.9x1.25_hist_16pfts_Irrig_CMIP6_simyr2000_",site_name, ".nc"))
-   file.copy(surf.default,surf.file)
-   Sys.chmod(surf.file)
-   surf.nc <- ncdf4::nc_open(surf.file,write=TRUE)
-   ncdf4::ncvar_put(nc=surf.nc, varid='LONGXY', vals=lon)
-   ncdf4::ncvar_put(nc=surf.nc, varid='LATIXY', vals=lat)
-   ncdf4::nc_close(surf.nc)   
+   #surf.default <- system.file("surfdata_0.9x1.25_hist_16pfts_Irrig_CMIP6_simyr2000_c190214.nc",package = "PEcAn.FATES")
+   #surf.file    <- file.path(local.rundir,paste0("surfdata_0.9x1.25_hist_16pfts_Irrig_CMIP6_simyr2000_",site_name, ".nc"))
+   #file.copy(surf.default,surf.file)
+   #Sys.chmod(surf.file)
+   #surf.nc <- ncdf4::nc_open(surf.file,write=TRUE)
+   #ncdf4::ncvar_put(nc=surf.nc, varid='LONGXY', vals=lon)
+   #ncdf4::ncvar_put(nc=surf.nc, varid='LATIXY', vals=lat)
+   #ncdf4::nc_close(surf.nc)   
    
    ## DATM & DATM Stream??
    ## MET HEADERS
-   if(!is.null(settings$run$inputs$met)){
+   #if(!is.null(settings$run$inputs$met)){
 
      ## DATM HEADER: datm_atm_in
-     datm <- readLines(con=system.file("datm_atm_in.template",package = "PEcAn.FATES"),n=-1)
-     datm <- gsub('@DOMAIN@', file.path(indir,"share/domains/domain.clm",basename(domain.file)), datm)
-     datm <- gsub('@START_YEAR@',lubridate::year(start_date), datm)
-     datm <- gsub('@END_YEAR@',lubridate::year(end_date), datm)
-     writeLines(datm, con=file.path(local.rundir, "datm_atm_in"))
+     #datm <- readLines(con=system.file("datm_atm_in.template",package = "PEcAn.FATES"),n=-1)
+     #datm <- gsub('@DOMAIN@', file.path(indir,"share/domains/domain.clm",basename(domain.file)), datm)
+     #datm <- gsub('@START_YEAR@',lubridate::year(start_date), datm)
+     #datm <- gsub('@END_YEAR@',lubridate::year(end_date), datm)
+     #writeLines(datm, con=file.path(local.rundir, "datm_atm_in"))
      
-     ## DATM STREAM MET
-     met <- readLines(con=system.file("datm.streams.txt.PEcAn_met.template",package = "PEcAn.FATES"),n=-1)
-     met <- gsub('@INDIR@',indir, met)
+     ### DATM STREAM MET
+     #met <- readLines(con=system.file("datm.streams.txt.PEcAn_met.template",package = "PEcAn.FATES"),n=-1)
+     #met <- gsub('@INDIR@',indir, met)
      #domain.file.name <- paste0("domain.lnd.",site_name,".nc")
      #met <- gsub('@DOMAIN@',domain.file.name, met)  # attempting to provide correct domain file name
-     met <- gsub('@MET_PATH@',settings$run$inputs$met$path, met)
-     met.files <- dir(settings$run$inputs$met$path,"*.nc")
-     met <- gsub('@MET_FILES@',paste(met.files,collapse = "\n"), met)
-     writeLines(met, con=file.path(local.rundir, "datm.streams.txt.PEcAn_met"))
+     #met <- gsub('@MET_PATH@',settings$run$inputs$met$path, met)
+     #met.files <- dir(settings$run$inputs$met$path,"*.nc")
+     #met <- gsub('@MET_FILES@',paste(met.files,collapse = "\n"), met)
+     #writeLines(met, con=file.path(local.rundir, "datm.streams.txt.PEcAn_met"))
      
    }
 #   ... need to set this up so that if MET is blank it can run with default CLM met
@@ -121,21 +134,21 @@ write.config.FATES <- function(defaults, trait.values, settings, run.id){
    }
    
  # create host specific settings
-   hostsetup <- ""
-   if (!is.null(settings$model$prerun)) {
-     hostsetup <- paste(hostsetup, sep="\n", paste(settings$model$prerun, collapse="\n"))
-   }
-   if (!is.null(settings$host$prerun)) {
-     hostsetup <- paste(hostsetup, sep="\n", paste(settings$host$prerun, collapse="\n"))
-   }
+  # hostsetup <- ""
+  # if (!is.null(settings$model$prerun)) {
+  #   hostsetup <- paste(hostsetup, sep="\n", paste(settings$model$prerun, collapse="\n"))
+  # }
+  # if (!is.null(settings$host$prerun)) {
+  #   hostsetup <- paste(hostsetup, sep="\n", paste(settings$host$prerun, collapse="\n"))
+  # }
 
-   hostteardown <- ""
-   if (!is.null(settings$model$postrun)) {
-     hostteardown <- paste(hostteardown, sep="\n", paste(settings$model$postrun, collapse="\n"))
-   }
-   if (!is.null(settings$host$postrun)) {
-     hostteardown <- paste(hostteardown, sep="\n", paste(settings$host$postrun, collapse="\n"))
-   }
+  # hostteardown <- ""
+  # if (!is.null(settings$model$postrun)) {
+  #   hostteardown <- paste(hostteardown, sep="\n", paste(settings$model$postrun, collapse="\n"))
+  # }
+  # if (!is.null(settings$host$postrun)) {
+  #   hostteardown <- paste(hostteardown, sep="\n", paste(settings$host$postrun, collapse="\n"))
+  # }
 
 # create job.sh
    jobsh <- gsub('@HOST_SETUP@', hostsetup, jobsh)
@@ -143,7 +156,6 @@ write.config.FATES <- function(defaults, trait.values, settings, run.id){
    
    ## Machine configs
    #   ./create_newcase –case A –res @RES@ –compset @COMPSET@ –driver @DRIVER@ –machine @MACHINE@ –run-unsupported
-   ## --run-unspported??
    if (!is.null(settings$model$machine)) {
      machine <- paste(settings$model$machine, collapse="\n") 
    } else {
@@ -191,7 +203,7 @@ write.config.FATES <- function(defaults, trait.values, settings, run.id){
    
    writeLines(jobsh, con=file.path(settings$rundir, run.id, "job.sh"))
    Sys.chmod(file.path(settings$rundir, run.id, "job.sh"))
-#   
+
 #   ## Write PARAMETER file
    
    ## COPY AND OPEN DEFAULT PARAMETER FILES
@@ -202,65 +214,65 @@ write.config.FATES <- function(defaults, trait.values, settings, run.id){
    #clm.param.default <- system.file("clm5_params.c171117.nc",package="PEcAn.FATES")
    #clm.param.file <- file.path(local.rundir,paste0("clm_params.",run.id,".nc"))
    ## Position of parameters file?
-   clm.param.default <- file.path(refcase,"clm5_params.c171117.nc") # probably need to allow custom param file names here (pecan.xml?)
-   clm.param.file <- file.path(local.rundir,paste0("clm_params.",run.id,".nc"))
-   file.copy(clm.param.default,clm.param.file)
-   clm.param.nc <- ncdf4::nc_open(clm.param.file,write=TRUE)
+   #clm.param.default <- file.path(refcase,"clm5_params.c171117.nc") # probably need to allow custom param file names here (pecan.xml?)
+   #clm.param.file <- file.path(local.rundir,paste0("clm_params.",run.id,".nc"))
+   #file.copy(clm.param.default,clm.param.file)
+   #clm.param.nc <- ncdf4::nc_open(clm.param.file,write=TRUE)
    
    # FATES
    #fates.param.default <- system.file("fates_params_2troppftclones.c171018_sps.nc",package="PEcAn.FATES")
    # above is a temporary param file corrected for the tropics by lowering freezing tolerace parameters
-   fates.param.default <- file.path(refcase,"fates_params_2troppftclones.c171018_sps.nc") # probably need to allow custom param file names here (pecan.xml?)
-   fates.param.file <- file.path(local.rundir,paste0("fates_params.",run.id,".nc"))
-   file.copy(fates.param.default,fates.param.file)
-   fates.param.nc <- ncdf4::nc_open(fates.param.file,write=TRUE)
+   #fates.param.default <- file.path(refcase,"fates_params_2troppftclones.c171018_sps.nc") # probably need to allow custom param file names here (pecan.xml?)
+   #fates.param.file <- file.path(local.rundir,paste0("fates_params.",run.id,".nc"))
+   #file.copy(fates.param.default,fates.param.file)
+   #fates.param.nc <- ncdf4::nc_open(fates.param.file,write=TRUE)
    
    ## Loop over PFTS
-   npft <- length(trait.values)
-   PEcAn.logger::logger.debug(npft)
-   PEcAn.logger::logger.debug(dim(trait.values))
-   PEcAn.logger::logger.debug(names(trait.values))
+   #npft <- length(trait.values)
+   #PEcAn.logger::logger.debug(npft)
+   #PEcAn.logger::logger.debug(dim(trait.values))
+   #PEcAn.logger::logger.debug(names(trait.values))
    #pftnames <- stringr::str_trim(tolower(ncvar_get(param.nc,"pftname"))) 
-   pftnames <- stringr::str_trim(tolower(ncdf4::ncvar_get(clm.param.nc,"pftname")))
-   PEcAn.logger::logger.debug(paste0("CLM PFT names: "),pftnames)
-   for (i in seq_len(npft)) {
-     pft <- trait.values[[i]]
-     print(c("PFT",i))
-     PEcAn.logger::logger.info(pft)
-     pft.name <- names(trait.values)[i]
-     if(is.null(pft.name) | is.na(pft.name)){
-       PEcAn.logger::logger.error("pft.name missing")
-     } else {
-       PEcAn.logger::logger.info(paste("PFT =",pft.name))
-       PEcAn.logger::logger.debug(paste0("fates-clm PFT number: ",which(pftnames==pft.name)))
-     }
-     if(pft.name == 'env') next   ## HACK, need to remove env from default
+   #pftnames <- stringr::str_trim(tolower(ncdf4::ncvar_get(clm.param.nc,"pftname")))
+   #PEcAn.logger::logger.debug(paste0("CLM PFT names: "),pftnames)
+   #for (i in seq_len(npft)) {
+     #pft <- trait.values[[i]]
+     #print(c("PFT",i))
+     #PEcAn.logger::logger.info(pft)
+     #pft.name <- names(trait.values)[i]
+     #if(is.null(pft.name) | is.na(pft.name)){
+       #PEcAn.logger::logger.error("pft.name missing")
+     #} else {
+       #PEcAn.logger::logger.info(paste("PFT =",pft.name))
+       #PEcAn.logger::logger.debug(paste0("fates-clm PFT number: ",which(pftnames==pft.name)))
+     #}
+     #if(pft.name == 'env') next   ## HACK, need to remove env from default
      
      ## Match PFT name to COLUMN
-     ipft <- match(tolower(pft.name),pftnames)
-     PEcAn.logger::logger.debug(paste0("ipft: ",ipft))
+     #ipft <- match(tolower(pft.name),pftnames)
+     #PEcAn.logger::logger.debug(paste0("ipft: ",ipft))
                                       
-     if(is.na(ipft)){
-       PEcAn.logger::logger.severe(paste("Unmatched PFT",pft.name,
-                          "in FATES. PEcAn does not yet support non-default PFTs for this model"))
-     }
+     #if(is.na(ipft)){
+       #PEcAn.logger::logger.severe(paste("Unmatched PFT",pft.name,
+                          #"in FATES. PEcAn does not yet support non-default PFTs for this model"))
+     #}
      
      # hard code hack until we can use more than 2 pfts in FATES 
-     ipft <- 2
-     PEcAn.logger::logger.debug(paste0("*** PFT number hard-coded to ", ipft," in fates. This will be updated when FATES allows more PFTs"))
+     #ipft <- 2
+     #PEcAn.logger::logger.debug(paste0("*** PFT number hard-coded to ", ipft," in fates. This will be updated when FATES allows more PFTs"))
      
      ## Special variables used in conversions
 #     leafC <- pft['leafC']/100  ## percent to proportion
-     leafC <- NA
-     if(is.na(leafC)) leafC <- 0.48
+     #leafC <- NA
+     #if(is.na(leafC)) leafC <- 0.48
      
      # determine photo pathway
-     photo_flag <- ncdf4::ncvar_get(fates.param.nc,varid="fates_c3psn", start = ipft, count = 1)
-     PEcAn.logger::logger.debug(paste0("Photosynthesis pathway flag value: ", photo_flag))
+     #photo_flag <- ncdf4::ncvar_get(fates.param.nc,varid="fates_c3psn", start = ipft, count = 1)
+     #PEcAn.logger::logger.debug(paste0("Photosynthesis pathway flag value: ", photo_flag))
      
      ## Loop over VARIABLES
-     for (v in seq_along(pft)) {
-       var <- names(pft)[v]
+     #for (v in seq_along(pft)) {
+       #var <- names(pft)[v]
 
        ## THESE NEED SOME FOLLOW UP
        
@@ -342,10 +354,10 @@ write.config.FATES <- function(defaults, trait.values, settings, run.id){
        
        
        ## ALLPFT indexed (size = 1) 
-       if(var == "veg_respiration_Q10"){            ## Q10 for maintenance respiration. CLM param. q10_mr(allpfts)
-         ncdf4::ncvar_put(nc=clm.param.nc, varid='q10_mr', start = 1, count = 1,
-                   vals=pft[v])
-       }
+       #if(var == "veg_respiration_Q10"){            ## Q10 for maintenance respiration. CLM param. q10_mr(allpfts)
+         #ncdf4::ncvar_put(nc=clm.param.nc, varid='q10_mr', start = 1, count = 1,
+                   #vals=pft[v])
+       #}
        
        ## NONE indexed
        ##   -- FIRE
@@ -374,14 +386,14 @@ write.config.FATES <- function(defaults, trait.values, settings, run.id){
        ## NCWD dimensioned       Size:4
        
        
-     } ## end loop over VARIABLES
-   } ## end loop over PFTs
+     #} ## end loop over VARIABLES
+   #} ## end loop over PFTs
    #ncdf4::nc_close(param.nc)
-   ncdf4::nc_close(clm.param.nc)
-   ncdf4::nc_close(fates.param.nc)
+   #ncdf4::nc_close(clm.param.nc)
+   #ncdf4::nc_close(fates.param.nc)
    
 #   ## Write SETTINGS file
 #     
-}
+#}
 #---------------------------------------------------------------------------------------------------------------------#
 ### EOF
