3f4928fa018801d1dabff1d08ba271d4ea74dfc8
Chris Black <chris@ckblack.org>
2024-09-04 00:12:50 -0700

    relicense PEcAn.visualization as BSD3 with agreement of all contributors

--- base/visualization/R/plots.R
+++ base/visualization/R/plots.R
@@ -1,12 +1,3 @@
-#-------------------------------------------------------------------------------
-# Copyright (c) 2012 University of Illinois, NCSA.
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the 
-# University of Illinois/NCSA Open Source License
-# which accompanies this distribution, and is available at
-# http://opensource.ncsa.illinois.edu/license.html
-#-------------------------------------------------------------------------------
-
 ##' Variable-width (diagonally cut) histogram
 ##'
 ##' When constructing a histogram, it is common to make all bars the same width.
