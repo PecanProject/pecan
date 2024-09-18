75f02eb7be698300f629c977b639bd9fffd812d8
Dongchen Zhang <zhangdc@scc-yr1.scc.bu.edu>
2024-09-18 16:19:27 -0400

    Add qsub functions for parallel SDA workflows.

---
+++ modules/assim.sequential/R/sda.qsub.functions.R
@@ -0,0 +1,355 @@
+#' @description This function provides an option for submitting `write.configs` jobs to cluster.
+#' @title parallel.write.configs
+#' @param settings  PEcAn settings object.
+#' @param ensemble.samples Lists: Sampled parameters for each plant functional types.
+#' @param restart.list Lists: restart lists of each site from previous runs.
+#' @author Dongchen Zhang.
+parallel.write.configs <- function (settings, ensemble.samples, restart.list) {
+  L <- length(settings)
+  batch.folder <- file.path(settings$outdir, "batch")
+  if (file.exists(batch.folder)){
+    unlink(batch.folder, recursive = T)
+  } 
+  dir.create(batch.folder)
+  # grab info from settings.
+  num.folder <- as.numeric(settings$state.data.assimilation$batch.settings$write.config$folder.num)
+  cores <- as.numeric(settings$state.data.assimilation$batch.settings$write.config$cores)
+  num.per.folder <- ceiling(L/num.folder)
+  folder.paths <- c()
+  for (i in 1:num.folder) {
+    # create folder for each set of pixels.
+    head.num <- (i-1)*num.per.folder + 1
+    # if the site number can not be evenly divided.
+    if (i*num.per.folder > L) {
+      tail.num <- L
+    } else {
+      tail.num <- i*num.per.folder
+    }
+    folder.name <- paste0("From_", head.num, "_to_", tail.num)
+    folder.path <- file.path(batch.folder, folder.name)
+    folder.paths <- c(folder.paths, folder.path)
+    if (dir.exists(folder.path)) {
+      unlink(x = file.path(folder.path, c("stderr.log", "stdout.log")))
+    } else {
+      dir.create(folder.path)
+      # write parameters.
+      configs <- list(setting = settings[head.num:tail.num],
+                      outdir = file.path(settings$outdir, "out"),
+                      ensemble.samples = ensemble.samples,
+                      restart.list = restart.list[head.num:tail.num])
+      save(configs, file = file.path(folder.path, "configs.Rdata"))
+    }
+    jobsh <- c("#!/bin/bash -l", 
+               "module load R/4.1.2", 
+               "echo \"require (PEcAnAssimSequential)", 
+               "      require (foreach)",
+               "      write.configs.fun('@FOLDER_PATH@', '@CORES@', '@LIB@')", 
+               "    \" | R --no-save")
+    jobsh <- gsub("@FOLDER_PATH@", folder.path, jobsh)
+    jobsh <- gsub("@CORES@", cores, jobsh)
+    jobsh <- gsub("@LIB@", paste0("PEcAn.",settings$model$type), jobsh)
+    writeLines(jobsh, con = file.path(folder.path, "job.sh"))
+    # qsub command.
+    qsub <- "qsub -l h_rt=48:00:00 -l buyin -pe omp @CORES@ -V -N @NAME@ -o @STDOUT@ -e @STDERR@ -S /bin/bash"
+    qsub <- gsub("@CORES@", cores, qsub)
+    qsub <- gsub("@NAME@", paste0("Job-", i), qsub)
+    qsub <- gsub("@STDOUT@", file.path(folder.path, "stdout.log"), qsub)
+    qsub <- gsub("@STDERR@", file.path(folder.path, "stderr.log"), qsub)
+    qsub <- strsplit(qsub, " (?=([^\"']*\"[^\"']*\")*[^\"']*$)", perl = TRUE)
+    cmd <- qsub[[1]]
+    out <- system2(cmd, file.path(folder.path, "job.sh"), stdout = TRUE, stderr = TRUE)
+  }
+  # check outputs.
+  PEcAn.logger::logger.info("Checking outputs.")
+  l <- length(list.files(batch.folder, pattern = "out.configs.Rdata", recursive = T))
+  pb <- utils::txtProgressBar(min = 0, max = num.folder, style = 3)
+  while(l < num.folder) {
+    Sys.sleep(60)
+    l <- length(list.files(batch.folder, pattern = "out.configs.Rdata", recursive = T))
+    utils::setTxtProgressBar(pb, l)
+  }
+  Sys.sleep(10)
+  # assemble results.
+  PEcAn.logger::logger.info("Assembling results.")
+  out.configs <- c()
+  for (path in folder.paths) {
+    res_env <- new.env()
+    load(file.path(path, "out.configs.Rdata"), envir = res_env)
+    out.configs <- c(out.configs, res_env$out.configs)
+  }
+  return(out.configs)
+}
+
+#' @description This function provides an option for splitting met files using cluster.
+#' @title parallel.split.met
+#' @param settings  PEcAn settings object.
+#' @param my.split_inputs Character: Function to be called for splitting met.
+#' @param conf.settings.before.split Lists: includes lists of configs for each site.
+#' @param start.time Character: start time for cut met files.
+#' @param stop.time Character: stop time for cut met files.
+#' @param inputs List: physical paths to met files for each site.
+#' @author Dongchen Zhang.
+parallel.split.met <- function (settings, my.split_inputs, outdir, conf.settings.before.split, start.time, stop.time, inputs) {
+  L <- length(settings)
+  batch.folder <- file.path(outdir, "batch")
+  if (file.exists(batch.folder)){
+    unlink(batch.folder, recursive = T)
+  } 
+  dir.create(batch.folder)
+  # grab info from settings.
+  num.folder <- as.numeric(settings$state.data.assimilation$batch.settings$met.split$folder.num)
+  cores <- as.numeric(settings$state.data.assimilation$batch.settings$met.split$cores)
+  num.per.folder <- ceiling(L/num.folder)
+  folder.paths <- c()
+  for (i in 1:num.folder) {
+    # create folder for each set of pixels.
+    head.num <- (i-1)*num.per.folder + 1
+    # if the site number can not be evenly divided.
+    if (i*num.per.folder > L) {
+      tail.num <- L
+    } else {
+      tail.num <- i*num.per.folder
+    }
+    folder.name <- paste0("From_", head.num, "_to_", tail.num)
+    folder.path <- file.path(batch.folder, folder.name)
+    folder.paths <- c(folder.paths, folder.path)
+    if (dir.exists(folder.path)) {
+      unlink(x = file.path(folder.path, c("stderr.log", "stdout.log")))
+    } else {
+      dir.create(folder.path)
+      # write parameters.
+      configs <- list(my.split_inputs = my.split_inputs,
+                      outdir = file.path(outdir, "out"),
+                      config.settings = conf.settings.before.split[head.num:tail.num],
+                      start.time = start.time,
+                      stop.time = stop.time,
+                      inputs = inputs[head.num:tail.num],
+                      nens = as.numeric(conf.settings.before.split[[1]]$ensemble$size))
+      save(configs, file = file.path(folder.path, "configs.Rdata"))
+    }
+    jobsh <- c("#!/bin/bash -l", 
+               "module load R/4.1.2", 
+               "echo \"require (PEcAnAssimSequential)", 
+               "      require (foreach)",
+               "      met.split.fun('@FOLDER_PATH@', '@CORES@', '@LIB@')", 
+               "    \" | R --no-save")
+    jobsh <- gsub("@FOLDER_PATH@", folder.path, jobsh)
+    jobsh <- gsub("@CORES@", cores, jobsh)
+    jobsh <- gsub("@LIB@", paste0("PEcAn.",settings$model$type), jobsh)
+    writeLines(jobsh, con = file.path(folder.path, "job.sh"))
+    # qsub command.
+    qsub <- "qsub -l h_rt=48:00:00 -l buyin -pe omp @CORES@ -V -N @NAME@ -o @STDOUT@ -e @STDERR@ -S /bin/bash"
+    qsub <- gsub("@CORES@", cores, qsub)
+    qsub <- gsub("@NAME@", paste0("Job-", i), qsub)
+    qsub <- gsub("@STDOUT@", file.path(folder.path, "stdout.log"), qsub)
+    qsub <- gsub("@STDERR@", file.path(folder.path, "stderr.log"), qsub)
+    qsub <- strsplit(qsub, " (?=([^\"']*\"[^\"']*\")*[^\"']*$)", perl = TRUE)
+    cmd <- qsub[[1]]
+    out <- system2(cmd, file.path(folder.path, "job.sh"), stdout = TRUE, stderr = TRUE)
+  }
+  # check outputs.
+  PEcAn.logger::logger.info("Checking outputs.")
+  l <- length(list.files(batch.folder, pattern = "splits.Rdata", recursive = T))
+  pb <- utils::txtProgressBar(min = 0, max = num.folder, style = 3)
+  while(l < num.folder) {
+    Sys.sleep(60)
+    l <- length(list.files(batch.folder, pattern = "splits.Rdata", recursive = T))
+    utils::setTxtProgressBar(pb, l)
+  }
+  Sys.sleep(10)
+  # assemble results.
+  PEcAn.logger::logger.info("Assembling results.")
+  inputs.split <- c()
+  for (path in folder.paths) {
+    res_env <- new.env()
+    load(file.path(path, "splits.Rdata"), envir = res_env)
+    inputs.split <- c(inputs.split, res_env$splits)
+  }
+  names(inputs.split) <- seq_along(conf.settings.before.split)
+  return(inputs.split)
+}
+
+#' @description This function provides an option for reading sda outputs using cluster.
+#' @title parallel.read.sda
+#' @param settings  PEcAn settings object.
+#' @param my.read_restart Character: Function to be called for reading sda outputs.
+#' @param outdir Character: physical path to which model outputs are stored.
+#' @param out.configs Lists: outputs returned by `parallel.write.configs` function.
+#' @param stop.time Character: stop time for reading NC files.
+#' @param var.names Character: names of variables to be read.
+#' @param new.params Lists: outputs returned by `sda_matchparam` function.
+#' @author Dongchen Zhang.
+parallel.read.sda <- function(settings, my.read_restart, outdir, out.configs, stop.time, var.names, new.params) {
+  L <- length(settings)
+  batch.folder <- file.path(outdir, "batch")
+  if (file.exists(batch.folder)){
+    unlink(batch.folder, recursive = T)
+  } 
+  dir.create(batch.folder)
+  # grab info from settings.
+  num.folder <- as.numeric(settings$state.data.assimilation$batch.settings$sda.read$folder.num)
+  cores <- as.numeric(settings$state.data.assimilation$batch.settings$sda.read$cores)
+  num.per.folder <- ceiling(L/num.folder)
+  folder.paths <- c()
+  for (i in 1:num.folder) {
+    # create folder for each set of pixels.
+    head.num <- (i-1)*num.per.folder + 1
+    # if the site number can not be evenly divided.
+    if (i*num.per.folder > L) {
+      tail.num <- L
+    } else {
+      tail.num <- i*num.per.folder
+    }
+    folder.name <- paste0("From_", head.num, "_to_", tail.num)
+    folder.path <- file.path(batch.folder, folder.name)
+    folder.paths <- c(folder.paths, folder.path)
+    if (dir.exists(folder.path)) {
+      unlink(x = file.path(folder.path, c("stderr.log", "stdout.log")))
+    } else {
+      dir.create(folder.path)
+      # write parameters.
+      configs <- list(my.read_restart = my.read_restart,
+                      outdir = file.path(outdir, "out"),
+                      out.configs = out.configs[head.num:tail.num],
+                      stop.time = stop.time,
+                      var.names = var.names,
+                      new.params = new.params[head.num:tail.num],
+                      nens = dim(out.configs[[1]]$runs)[1])
+      save(configs, file = file.path(folder.path, "configs.Rdata"))
+    }
+    jobsh <- c("#!/bin/bash -l", 
+               "module load R/4.1.2", 
+               "echo \"require (PEcAnAssimSequential)", 
+               "      require (foreach)",
+               "      read.fun('@FOLDER_PATH@', '@CORES@', '@LIB@')", 
+               "    \" | R --no-save")
+    jobsh <- gsub("@FOLDER_PATH@", folder.path, jobsh)
+    jobsh <- gsub("@CORES@", cores, jobsh)
+    jobsh <- gsub("@LIB@", paste0("PEcAn.",settings$model$type), jobsh)
+    writeLines(jobsh, con = file.path(folder.path, "job.sh"))
+    # qsub command.
+    qsub <- "qsub -l h_rt=48:00:00 -l buyin -pe omp @CORES@ -V -N @NAME@ -o @STDOUT@ -e @STDERR@ -S /bin/bash"
+    qsub <- gsub("@CORES@", cores, qsub)
+    qsub <- gsub("@NAME@", paste0("Job-", i), qsub)
+    qsub <- gsub("@STDOUT@", file.path(folder.path, "stdout.log"), qsub)
+    qsub <- gsub("@STDERR@", file.path(folder.path, "stderr.log"), qsub)
+    qsub <- strsplit(qsub, " (?=([^\"']*\"[^\"']*\")*[^\"']*$)", perl = TRUE)
+    cmd <- qsub[[1]]
+    out <- system2(cmd, file.path(folder.path, "job.sh"), stdout = TRUE, stderr = TRUE)
+  }
+  # check outputs.
+  PEcAn.logger::logger.info("Checking outputs.")
+  l <- length(list.files(batch.folder, pattern = "reads.Rdata", recursive = T))
+  pb <- utils::txtProgressBar(min = 0, max = num.folder, style = 3)
+  while(l < num.folder) {
+    Sys.sleep(60)
+    l <- length(list.files(batch.folder, pattern = "reads.Rdata", recursive = T))
+    utils::setTxtProgressBar(pb, l)
+  }
+  Sys.sleep(10)
+  # assemble results.
+  PEcAn.logger::logger.info("Assembling results.")
+  reads <- c()
+  for (path in folder.paths) {
+    res_env <- new.env()
+    load(file.path(path, "reads.Rdata"), envir = res_env)
+    reads <- c(reads, res_env$reads)
+  }
+  names(reads) <- seq_along(out.configs)
+  return(reads)
+}
+
+#' @description This function provides an option for executing model or removing files using cluster.
+#' @title job.sub
+#' @param settings  PEcAn settings object.
+#' @param outdir Character: physical path to which model outputs are stored.
+#' @param rm.file Boolean: if we want to delete files.
+#' the model execution will be submitted if it's `FALSE`.
+#' @param only.nc Boolean: if we want to only delete NC files.
+#' @author Dongchen Zhang.
+job.sub <- function(settings, rm.file = T, only.nc = T) {
+  outdir <- settings$outdir
+  L <- length(settings)
+  batch.folder <- file.path(outdir, "batch")
+  if (file.exists(batch.folder)){
+    unlink(batch.folder, recursive = T)
+  } 
+  dir.create(batch.folder)
+  # grab info from settings.
+  num.folder <- as.numeric(settings$state.data.assimilation$batch.settings$general.job$folder.num)
+  cores <- as.numeric(settings$state.data.assimilation$batch.settings$general.job$cores)
+  num.per.folder <- ceiling(L/num.folder)
+  for (i in 1:num.folder) {
+    # create folder for each set of pixels.
+    head.num <- (i-1)*num.per.folder + 1
+    # if the site number can not be evenly divided.
+    if (i*num.per.folder > L) {
+      tail.num <- L
+    } else {
+      tail.num <- i*num.per.folder
+    }
+    folder.name <- paste0("From_", head.num, "_to_", tail.num)
+    folder.path <- file.path(batch.folder, folder.name)
+    if (dir.exists(folder.path)) {
+      unlink(x = file.path(folder.path, c("stderr.log", "stdout.log")))
+    } else {
+      dir.create(folder.path)
+      # write parameters.
+      # iteration, sza, vza, psi, observed, wavelengths.
+      folder.runs <- run.lists[head.num:tail.num,]
+      if (rm.file) {
+        folder.runs <- file.path(outdir, "out", folder.runs)
+      } else {
+        folder.runs <- file.path(outdir, "run", folder.runs)
+      }
+      save(folder.runs, file = file.path(folder.path, "runs.Rdata"))
+    }
+    # create job file.
+    if (rm.file) {
+      if (only.nc) {
+        jobsh <- c("#!/bin/bash -l", 
+                   "module load R/4.1.2", 
+                   "echo \"require (PEcAnAssimSequential)", 
+                   "      require (foreach)",
+                   "      rm.files('@FOLDER_PATH@', '@CORES@')", 
+                   "    \" | R --no-save")
+      } else {
+        jobsh <- c("#!/bin/bash -l", 
+                   "module load R/4.1.2", 
+                   "echo \"require (PEcAnAssimSequential)", 
+                   "      require (foreach)",
+                   "      rm.files('@FOLDER_PATH@', '@CORES@', 'FALSE')", 
+                   "    \" | R --no-save")
+      }
+    } else {
+      jobsh <- c("#!/bin/bash -l", 
+                 "module load R/4.1.2", 
+                 "echo \"require (PEcAnAssimSequential)", 
+                 "      require (foreach)",
+                 "      parallel.job.execution('@FOLDER_PATH@', '@CORES@')", 
+                 "    \" | R --no-save")
+    }
+    jobsh <- gsub("@FOLDER_PATH@", folder.path, jobsh)
+    jobsh <- gsub("@CORES@", cores, jobsh)
+    writeLines(jobsh, con = file.path(folder.path, "job.sh"))
+    # qsub command.
+    qsub <- "qsub -l h_rt=48:00:00 -l buyin -pe omp @CORES@ -V -N @NAME@ -o @STDOUT@ -e @STDERR@ -S /bin/bash"
+    qsub <- gsub("@CORES@", cores, qsub)
+    qsub <- gsub("@NAME@", paste0("Job-", i), qsub)
+    qsub <- gsub("@STDOUT@", file.path(folder.path, "stdout.log"), qsub)
+    qsub <- gsub("@STDERR@", file.path(folder.path, "stderr.log"), qsub)
+    qsub <- strsplit(qsub, " (?=([^\"']*\"[^\"']*\")*[^\"']*$)", perl = TRUE)
+    cmd <- qsub[[1]]
+    out <- system2(cmd, file.path(folder.path, "job.sh"), stdout = TRUE, stderr = TRUE)
+  }
+  # check outputs.
+  PEcAn.logger::logger.info("Checking outputs.")
+  l <- length(list.files(batch.folder, pattern = "result.txt", recursive = T))
+  pb <- utils::txtProgressBar(min = 0, max = num.folder, style = 3)
+  while(l < num.folder) {
+    Sys.sleep(60)
+    l <- length(list.files(batch.folder, pattern = "result.txt", recursive = T))
+    utils::setTxtProgressBar(pb, l)
+  }
+  Sys.sleep(10)
+}
