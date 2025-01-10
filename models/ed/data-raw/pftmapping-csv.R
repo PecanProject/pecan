13fff49ac2c2910e800e216dadc2f3bce6e2b525
sdeherto <64660693+sdeherto@users.noreply.github.com>
2025-01-10 20:48:12 +0100

    Apply pftmapping-csv.R script to implement changes

--- models/ed/data-raw/pftmapping-csv.R
+++ models/ed/data-raw/pftmapping-csv.R
@@ -1,6 +1,6 @@
 ## code to prepare `pftmapping.csv` dataset goes here
-pftmapping <- read.csv2("./models/ed/data-raw/pftmapping.csv")
+pftmapping <- read.csv2("C:/Users/stevdher/OneDrive - UGent/Documenten/postdoc/research/LBNL/GitHub/inventory/pecan/models/ed/data-raw/pftmapping.csv")
 usethis::with_project(
-  "./models/ed/",
+  "C:/Users/stevdher/OneDrive - UGent/Documenten/postdoc/research/LBNL/GitHub/inventory/pecan/models/ed/",
   usethis::use_data(pftmapping, overwrite = TRUE)
 )
