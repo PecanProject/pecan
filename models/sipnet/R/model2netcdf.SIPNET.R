2ea39cc3e7d243a0c0947993a26a9abb387a3398
Cami Webb <cwebb16@bu.edu>
2025-09-17 13:44:24 -0400

    Update model2netcdf.SIPNET.R
    Reverted line 99 to original for better clarity

--- models/sipnet/R/model2netcdf.SIPNET.R
+++ models/sipnet/R/model2netcdf.SIPNET.R
@@ -96,8 +96,7 @@ sipnet2datetime <- function(sipnet_tval, base_year, base_month = 1,
 ##' @param overwrite Flag for overwriting nc files or not
 ##' @param conflict Flag for dealing with conflicted nc files, if T we then will merge those, if F we will jump to the next.
 ##' @param prefix prefix to read the output files
-##' @param delete.raw logical: remove sipnet.out files after converting?
-##'
+##' @param delete.raw Flag to remove sipnet.out files, FALSE = do not remove files TRUE = remove files
 ##' @export
 ##' @author Shawn Serbin, Michael Dietze
 model2netcdf.SIPNET <- function(outdir, sitelat, sitelon, start_date, end_date, delete.raw = FALSE, revision, prefix = "sipnet.out",
