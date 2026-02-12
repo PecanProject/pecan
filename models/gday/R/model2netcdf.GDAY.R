136db35ae3c5e076fe84fd20a750d65cc026c03f
ayushman1210 <ayushmangupta892@gmail.com>
2026-02-12 11:15:52 +0530

    Clarify GDAY unit conversion: daily Mg/ha/day not per-year

--- models/gday/R/model2netcdf.GDAY.R
+++ models/gday/R/model2netcdf.GDAY.R
@@ -41,7 +41,11 @@ model2netcdf.GDAY <- function(outdir, sitelat, sitelon, start_date, end_date) {
     output <- list()
     
     ## standard variables: C-Fluxes
-    # GDAY outputs in Mg/ha/day, convert directly to kgC/m2/s (ud_convert returns per-second)
+    ## NOTE: GDAY outputs daily accumulated values in Mg/ha/day (NOT per year!)
+    ## timestep.s = 86400 seconds = 1 day
+    ## Unit conversion: 1 Mg/ha/day = 0.1 kg/m2 ÷ 86400 s = 1.157407e-6 kg/m2/s
+    ## Reference: https://github.com/PecanProject/pecan/issues/XXXX
+    ## The ud_convert function handles the unit transformation to per-second basis
     output[[1]] <- PEcAn.utils::ud_convert(sub.GDAY.output[, "auto_resp"], "Mg/ha/day", "kg/m2/s")
     output[[2]] <- PEcAn.utils::ud_convert(sub.GDAY.output[, "hetero_resp"], "Mg/ha/day", "kg/m2/s")
     output[[3]] <- PEcAn.utils::ud_convert(sub.GDAY.output[, "auto_resp"] + sub.GDAY.output[, "hetero_resp"], "Mg/ha/day", "kg/m2/s")
