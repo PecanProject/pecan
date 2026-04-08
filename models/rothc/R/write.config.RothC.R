bc7ec4a31e68991db6ba2bd7a8eb0112d0c8aec1
krupashah20 <krupashah175@gmail.com>
2026-04-08 21:24:38 +0530

    refactor: use write_job_sh helper in RothC model

--- models/rothc/R/write.config.RothC.R
+++ models/rothc/R/write.config.RothC.R
@@ -75,8 +75,7 @@ write.config.RothC <- function(defaults, trait.values, settings, run.id) {
 
   jobsh <- gsub("@BINARY@", settings$model$binary, jobsh)
 
-  writeLines(jobsh, con = file.path(settings$rundir, run.id, "job.sh"))
-  Sys.chmod(file.path(settings$rundir, run.id, "job.sh"))
+  PEcAn.utils::write_job_sh(settings$rundir, run.id, jobsh)
 
   #-----------------------------------------------------------------------
   ### Edit a templated config file for runs
