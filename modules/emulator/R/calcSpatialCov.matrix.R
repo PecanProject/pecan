1a058bb932cb8b99465ab58f6f70d59e0f068bc0
Man <kasetiyaman08@gmail.com>
2026-05-15 12:07:52 +0530

    fix(emulator): restore Roxygen documentation for calcSpatialCov.matrix

--- modules/emulator/R/calcSpatialCov.matrix.R
+++ modules/emulator/R/calcSpatialCov.matrix.R
@@ -1,3 +1,14 @@
+##' Currently assumes an exponential spatial dependency
+##'
+##' Can make gaussian by passing squared distance matrix
+##'
+##' @param d spatial distance matrix
+##' @param psi spatial corr
+##' @param tau spatial var
+##' @param ... additional arguments (currently unused)
+##' @return spatial covariance matrix
+##' @author Michael Dietze
+##' @export
 calcSpatialCov.matrix <- function(d, psi, tau, ...) {
-    return(tau * exp(-psi * d))
-  }
+  return(tau * exp(-psi * d))
+}
