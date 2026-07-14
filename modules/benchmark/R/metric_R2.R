3b719d34a5beb56bac378e0ae5d442bfcbc182ae
ayushman1210 <ayushmangupta892@gmail.com>
2026-07-14 17:10:30 +0530

    updated the pr as per the akash suggestion

--- modules/benchmark/R/metric_R2.R
+++ modules/benchmark/R/metric_R2.R
@@ -8,6 +8,10 @@
 ##' Computes R-squared using the correlation-based formula:
 ##' \eqn{R^2 = \left(\frac{\sum(obs - \bar{obs})(mod - \bar{mod})}
 ##' {\sqrt{\sum(obs - \bar{obs})^2} \cdot \sqrt{\sum(mod - \bar{mod})^2}}\right)^2}
+##' 
+##' Note: Because this is a correlation-based R2, it is invariant to bias and slope.
+##' A model that is perfectly correlated but biased (e.g., model = obs + 100) will still
+##' score 1. This is distinct from variance explained or Nash-Sutcliffe Efficiency (NSE).
 ##'
 ##' @author Betsy Cowdery
 metric_R2 <- function(metric_dat, ...) {
